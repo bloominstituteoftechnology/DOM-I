7016ad47af2fd962e6416ab05f8793b96f89a4d9
Joshua Goode <jgoode.business@gmail.com>
2018-02-26 17:59:15 -0500

    Updated w/function template, added additional pseudocode

--- digital_timer/index.js
+++ digital_timer/index.js
@@ -10,18 +10,39 @@ const digits = document.querySelector(".digits");
 clockCounter();
 
 function clockCounter () {
+    init();
 
-    let totalMs = 0;
+    function init () {
+        let totalMs = 0;
+        ms10s.innerHTML = "0";
+        ms100s.innerHTML = "0";
+        second1s.innerHTML = "0";
+        second10s.innerHTML = "0";
+
+    }
 
     const clockInterval = window.setInterval(() => {
         totalMs += 10;
         //if totalMs === 10000 -- go to stop function
         //else go to updater function
 
-
     }, 10);
+
+    function increment () {
+        //incremements numbers -- called from updateTimer?
+    }
+
+    function endTimer () {
+        //stop function - once 10,000 ms is reached within setInterval, stop function will run clearInterval and set each element to red by adding .redDigit to digit.classList
+
+    }
+
+    function updateTimer () {
+        //updater function - take totalMs and updates .innerHTML for each element based on total ms that has passed, using if statements?
+
+    }
 
 }
 
-//updater function - take totalMs and update .innerHTML for each element based on total ms that has passed?
-//stop function - once 10,000 ms is reached within setInterval, stop function will run clearInterval and set each element to red by adding .redDigit to digit.classList
+
+
