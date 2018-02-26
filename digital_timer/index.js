fe87c059dc8126ba381e888b3d8a6a07ba9efcc2
Courtney Seitz <courtneyelizabethseitz@gmail.com>
2018-02-26 17:52:37 -0500

    trying to fix js innerHTML 'null' error, still

--- digital_timer/index.js
+++ digital_timer/index.js
@@ -2,8 +2,8 @@
 const digits = document.getElementsByClassName(".digits");
 const tensSec = document.getElementById("#secondTens");
 const onesSec = document.getElementById("#secondOnes");
-const hundredsMs = document.getElementById("#msHundreds");
-const tensMs = document.getElementById("#msTens");
+const hundredsMil = document.getElementById("#msHundreds");
+const tensMil = document.getElementById("#msTens");
 
 myTimer();
 
@@ -13,8 +13,8 @@ function myTimer () {
   function init () {
     tensSec.innerHTML = "0";
     onesSec.innerHTML = "0";
-    hundredsMs.innerHTML = "0";
-    tensMs.innerHTML = "0";
+    hundredsMil.innerHTML = "0";
+    tensMil.innerHTML = "0";
 
 //set interval that calls update timer as well as calls endtimer after 10 seconds
 
