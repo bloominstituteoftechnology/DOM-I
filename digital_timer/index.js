903e38a81803299e07e226498243f9793029b156
Courtney Seitz <courtneyelizabethseitz@gmail.com>
2018-02-26 17:24:20 -0500

    trying to fix js innerHTML 'null' error

--- digital_timer/index.js
+++ digital_timer/index.js
@@ -1,15 +1,16 @@
 
-const digits = document.getElementsByClassName('.digits');
-const secondTens = document.getElementById('#secondTens');
-const secondOnes = document.getElementById('#secondOnes');
-const msHundreds = document.getElementById('#msHundreds');
-const msTens = document.getElementById('#msTens');
+const digits = document.getElementsByClassName(".digits");
+const Tens = document.getElementById("#secondTens");
+const Ones = document.getElementById("#secondOnes");
+const msHundreds = document.getElementById("#msHundreds");
+const msTens = document.getElementById("#msTens");
 
 
-secondTens.innerHTML = '0';
-secondOnes.innerHTML = '0';
-msHundreds.innerHTML = '0';
-msTens.innerHTML = '0';
+
+Tens.innerHTML = "0";
+Ones.innerHTML = "0";
+msHundreds.innerHTML = "0";
+msTens.innerHTML = "0";
 
 //increment milliseconds...
 
