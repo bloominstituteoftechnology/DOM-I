3e217c67a72e4564ba1cce55b9f014e8ff7c9461
kait-schorr <kaitlynschorr@gmail.com>
2018-02-26 15:46:01 -0600

    Created variables for the digits

--- digital_timer/index.js
+++ digital_timer/index.js
@@ -1,5 +1,20 @@
-let counter = 0;
-setInterval(function(){
-    counter += 10;
-},10);
-let digits = document.getElementsByClassName("digits");
+//Get the digits divs from the DOM
+let secondTens = document.getElementById("secondTens").innerHTML;
+let secondOnes = document.getElementById("secondOnes").innerHTML;
+let msHundreds = document.getElementById("msHundreds").innerHTML;
+let msTens = document.getElementById("msTens").innerHTML;
+//Counter
+//Inside counter: increment the digits
+//Stop at 10 seconds
+
+var ms = 0;
+let seconds = document.getElementById("secondOnes");
+var id = setInterval(function () {
+    if (ms <10000) {
+    ms += 10; 
+    console.log(ms);
+    seconds.innerHTML = ms;
+    }
+}, 10);
+
+
