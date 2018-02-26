d5643aac1d60097debed8aa0f457f8be42e882df
Nick Coultas <nncoultas@yahoo.com>
2018-02-26 14:59:59 -0800

    initial PR/Starting incrementing

---
+++ digital_timer/index.js
@@ -0,0 +1,27 @@
+const secondTens = document.getElementById("secondTens");
+const secondOnes = document.getElementById("secondOnes");
+const msHundreds = document.getElementById("msHundreds");
+const msTens = document.getElementById("msTens");
+
+timer(10);
+
+function timer (number) {
+    init();
+    function  init () {
+        secondTens.innerHTML = "0";
+        secondOnes.innerHTML = "0";
+        msHundreds.innerHTML = "0";
+        msTens.innerHTML = "0";
+    }
+
+    function increment () {
+    }
+    
+    function endTimer () {
+    }
+
+    function updateTimer () {
+        
+    }
+    
+}
