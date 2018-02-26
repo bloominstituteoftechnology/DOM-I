f145a8b5b75680e675686514aa02d02a89ff11fe
Justin Kaseman <justinkaseman@live.com>
2018-02-26 15:13:09 -0800

    JK Complete

--- digital_timer/index.js
+++ digital_timer/index.js
@@ -2,33 +2,50 @@ window.onload = function startTimer() {
     const digitSelector = document.getElementsByClassName('digit');
     const secondTen = digitSelector[0];
     const secondOne = digitSelector[1];
-    const msHundred = digitSelector[2]
+    const colon = digitSelector[2];
+    const msHundred = digitSelector[4]
     const msTen = digitSelector[3];
 
-    (function countSecondsTens(){
-        //every 10 seconds increment
-        window.setTimeout(increment(), 10000);
-
-        function increment() {
-            secondTen.innerHTML = "1";
-        }
+    (function init(){
+        msTen.innerHTML = "0";
+        msHundred.innerHTML = "0";
+        secondOne.innerHTML = "0";
+        secondTen.innerHTML = "0";
     }());
 
-    function countSecondsOnes(){
+    let myInt = function() {
+        if (msHundred.innerHTML === "9") {
+            msHundred.innerHTML = "0";
+            incrementMsTens();
+        } else msHundred.innerHTML = ++msHundred.innerHTML;
+    };
+
+    var go = setInterval( function(){ myInt()}, 10);
+
+    function incrementMsTens(){
+        if(msTen.innerHTML === "9") {
+            msTen.innerHTML = "0";
+            incrementSecondsOnes();
+        } else msTen.innerHTML = ++msTen.innerHTML;
+        //every 10th second increment
+    }
+
+    function incrementSecondsOnes(){
         //every second increment
+        if(secondOne.innerHTML === "9") {
+            secondOne.innerHTML = "0";
+            incrementSecondsTens();
+        } else secondOne.innerHTML = ++secondOne.innerHTML;
     }
 
-    function countMsTens(){
-        //every 10th of a second increment
+    function incrementSecondsTens(){
+        //every 10 seconds increment
+        secondTen.innerHTML = "1";
+        clearInterval(go);
+        msHundred.style.color = "red";
+        msTen.style.color = "red";
+        secondTen.style.color = "red";
+        secondOne.style.color = "red";
+        colon.style.color = "red";
     }
-
-    function countMsHundreds(){
-        //every 100th of a second increment
-        window.setInterval(cb, )
-    }
-
-    function endTimer() {
-        
-    }
-
 };
