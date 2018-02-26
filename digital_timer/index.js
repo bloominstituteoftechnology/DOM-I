d9f1a3afa1c9beaf5c05393296dfb204f8e17474
hrehman000@citymail.cuny.edu <hrehman000@citymail.cuny.edu>
2018-02-26 18:17:42 -0500

    almost done

--- digital_timer/index.js
+++ digital_timer/index.js
@@ -5,6 +5,7 @@ let secondTens = document.getElementById("secondTens");
 let secondOnes = document.getElementById("secondOnes");
 let msHundreds = document.getElementById("msHundreds");
 let msTens = document.getElementById("msTens");
+let digits = document.getElementsByClassName("digits"); 
 //Counter
 //Inside counter: increment the digits
 //Stop at 10 seconds
@@ -14,34 +15,30 @@ let vmsTens = 0;
 let vsecondOnes = 0;
 let vsecondTens = 0;
 var id = setInterval(function () {
-    vmsHundreds++;
-    msHundreds.innerHTML = vmsHundreds;
-    if (vmsHundreds === 10) {
-        vmsTens++;
-        msTens.innerHTML = vmsTens;
-        vmsHundreds = 0;
+    if (vsecondTens < 1){
+        vmsHundreds++;
+        msHundreds.innerHTML = vmsHundreds;
+        if (vmsHundreds === 10) {
+            vmsTens++;
+            msTens.innerHTML = vmsTens;
+            vmsHundreds = msHundreds.innerHTML = 0;
+        }
+        if (vmsTens === 10) {
+            vsecondOnes++;
+            secondOnes.innerHTML = vsecondOnes;
+            vmsTens = msTens.innerHTML = 0;            
+        }
+        if (vsecondOnes === 10) {
+            vsecondTens++;
+            secondTens.innerHTML = vsecondTens;
+            vsecondOnes = secondOnes.innerHTML = 0;
+        }
+        if (vsecondTens === 1) {
+            let newTens = document.getElementById("secondTens");
+            newTens.innerHTML = 1;
+            for (let i = 0; i < ; i++)
+            digits.classList.add("redDigit");
+        }
     }
-    if (vmsTens === 10) {
-        vsecondOnes++;
-        secondOnes.innerHTML = vsecondOnes;
-        vmsTens = 0;
-    }
-    if (vsecondOnes === 10) {
-        vsecondTens++;
-        secondTens.innerHTML = vsecondTens;
-        vsecondOnes = 0;
-    }
-    if (vsecondTens === 1) {
-        let newTens = document.getElementById("secondTens");
-        newTens.innerHTML = 1;
-    }
-
-
-    //increment the hundreth ms
-    //if the hundreth ms is equal to 10 => increment the tenth ms
-    //if the tenth ms is equal to 10 => increment the second ones
-    //if the second ones is equal to 10 => increment the tenth seconds
 }
 , 10);
-
-
