603865eacbc357c4417a106204bef8d0accbf653
Juber Ahmed <a.juber@hotmail.com>
2018-02-26 16:29:52 -0500

    Displaying Start of Counter

--- digital_timer/index.js
+++ digital_timer/index.js
@@ -1,2 +1,5 @@
 
-
+document.getElementById("secondTens").innerHTML = "0";
+document.getElementById("secondOnes").innerHTML = "0";
+document.getElementById("msHundreds").innerHTML = "0";
+document.getElementById("msTens").innerHTML = "0";
