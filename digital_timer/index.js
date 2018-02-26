0edc560919332200f3127e7b7554309c034edf81
Abraham Andujo <foxtara00@gmail.com>
2018-02-26 14:34:36 -0800

    Abe, Harrison, Sumayyah PR

---
+++ digital_timer/index.js
@@ -0,0 +1,37 @@
+let secondTens = document.querySelector("#secondTens");
+let secondOnes = document.querySelector("#secondOnes");
+let msHundreds = document.querySelector("#msHundreds");
+let msTens = document.querySelector("#msTens");
+let stopwatch = document.querySelector(".digits");
+
+let msCounter = 0;
+let secCounter = 0;
+let maxSeconds = 0;
+secondTens.innerHTML = 0;
+secondOnes.innerHTML = 0;
+msHundreds.innerHTML = 0;
+msTens.innerHTML = 0;
+
+var timer = window.setInterval(function () {
+    msTens.innerHTML++;
+    msCounter++;
+    secCounter++;
+
+    if (msCounter === 10) {
+        msHundreds.innerHTML++;
+        msTens.innerHTML = 0;
+        msCounter = 0;
+    }
+    if (secCounter === 100) {
+        secondOnes.innerHTML++;
+        msHundreds.innerHTML = 0;
+        secCounter = 0;
+        maxSeconds++;
+    }
+    if (maxSeconds === 10) {
+        secondOnes.innerHTML = 0;
+        secondTens.innerHTML = 1;
+        stopwatch.style.color = "red";
+        clearInterval(timer);
+    }
+}, 10)
