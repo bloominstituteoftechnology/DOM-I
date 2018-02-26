e342bd81d1d6ff0c4feb6719ccb96acb2b976c6b
liz <elizabethbraggisgreat@gmail.com>
2018-02-26 16:18:09 -0500

    added variables to index.js and changed page title

---
+++ digital_timer/index.js
@@ -0,0 +1,13 @@
+// create variable for digits
+// creative indiv variables for second placeholders (const)
+
+const ms10s = document.getElementById("msTens");
+const ms100s = document.getElementById("msHundreds");
+const second1s = document.getElementById("secondOnes");
+const second10s = document.getElementById("secondTens");
+const digits = document.querySelector(".digits");
+
+
+
+
+
