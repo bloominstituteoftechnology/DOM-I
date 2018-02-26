3378aaf48d703ed15c4e249f2889f176f36fff0e
Joshua Goode <jgoode.business@gmail.com>
2018-02-26 17:01:43 -0500

    started on setInterval, pseudocode'd potential functions

--- digital_timer/index.js
+++ digital_timer/index.js
@@ -7,7 +7,21 @@ const second1s = document.getElementById("secondOnes");
 const second10s = document.getElementById("secondTens");
 const digits = document.querySelector(".digits");
 
+clockCounter();
+
+function clockCounter () {
+
+    let totalMs = 0;
+
+    const clockInterval = window.setInterval(() => {
+        totalMs += 10;
+        //if totalMs === 10000 -- go to stop function
+        //if not === 10000 then go to update function
 
 
+    }, 10);
 
+}
 
+//updater function - take totalMs and update .innerHTML for each element based on total ms that has passed?
+//stop function - once 10,000 ms is reached within setInterval, stop function will run clearInterval and set each element to red by adding .redDigit to digit.classList
