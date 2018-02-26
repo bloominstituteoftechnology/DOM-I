b375a69de4f23b542507846202435bf91d4949d2
Megan Williamson <meganawilliamson@gmail.com>
2018-02-26 14:56:29 -0800

    post Q&A

---
+++ digital_timer/index.js
@@ -0,0 +1,30 @@
+const secondTens = document.getElementById('secondTens');
+const secondOnes = document.getElementById('secondOnes');
+const msHundreds = document.getElementById('msHundreds');
+const msTens = document.getElementById('msTens');
+
+timer();
+
+function timer () {
+    init();
+
+    function init () {
+        secondTens.innerHTML = '0';
+        secondOnes.innerHTML = '0';
+        msHundreds.innerHTML = '0';
+        msTens.innerHTML = '0';
+    }
+    // set an interval that calls updateTimer and calls endTimer after 10 seconds
+}
+
+function increment () {
+    // helper function that gets called by updateTimer
+}
+
+function endTimer () {
+
+}
+
+function updateTimer () {
+
+}
