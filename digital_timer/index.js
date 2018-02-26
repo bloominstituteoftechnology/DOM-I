8c5e09f2233ea76487e403b0f4942368189c007c
Johnathan Huggett <johnathanhuggett@Johnathans-MacBook-Pro.local>
2018-02-26 18:07:34 -0500

    Johnathan Huggett Bredban Meredith

--- digital_timer/index.js
+++ digital_timer/index.js
@@ -1,1 +1,15 @@
-console.log('test');
+
+// variables for the timer
+
+let secondTens = document.getElementById("secondTens");
+let secondOnes = document.getElementById("secondOnes");
+let msHundreds = document.getElementById("msHundreds");
+let msTens = document.getElementById("msTens");
+
+secondTens.innerHTML = "0";
+secondOnes.innerText = "0";
+msHundreds.innerHTML = "0";
+msTens.innerHTML = "0";
+
+
+
