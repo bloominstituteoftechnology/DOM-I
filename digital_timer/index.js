f93baa057254386cbd97ee684879ce5fd31fa40b
Courtney Seitz <courtneyelizabethseitz@gmail.com>
2018-02-26 16:29:09 -0500

    storing variable names for each element id, etc

--- digital_timer/index.js
+++ digital_timer/index.js
@@ -1,3 +1,13 @@
+
+const digits = document.getElementsByClassName('.digits');
+const secondTens = document.getElementsById('#secondTens');
+const secondOnes = document.getElementsById('#secondOnes');
+const msHundreds = document.getElementsById('#msHundreds');
+const msTens = document.getElementsById('#msTens');
+
+
+
+
 function myTimer() {
 document.getElementsById("secondTens");
 
