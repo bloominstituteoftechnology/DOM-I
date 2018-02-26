1cecf309ef5488ba44f93397cd866114fa6b0708
kait-schorr <kaitlynschorr@gmail.com>
2018-02-26 16:02:41 -0600

    Trying to implement counter

--- digital_timer/index.js
+++ digital_timer/index.js
@@ -1,7 +1,9 @@
 //Get the digits divs from the DOM
+//declare four variables to hold the time interval counter for each digit part
+
 let secondTens = document.getElementById("secondTens").innerHTML;
 let secondOnes = document.getElementById("secondOnes").innerHTML;
-let msHundreds = document.getElementById("msHundreds").innerHTML;
+let msHundreds = document.getElementById("msHundreds");
 let msTens = document.getElementById("msTens").innerHTML;
 //Counter
 //Inside counter: increment the digits
@@ -10,10 +12,29 @@ let msTens = document.getElementById("msTens").innerHTML;
 var ms = 0;
 let seconds = document.getElementById("secondOnes");
 var id = setInterval(function () {
-    if (ms <10000) {
-    ms += 10; 
-    console.log(ms);
-    seconds.innerHTML = ms;
+    vmsHundreds ++;
+    msHundreds.innerHTML = vmsHundreds;
+    if (vmsHundreds === 10) {
+        vmsTens++;
+        vmsHundreds = 0;
+    }
+    if (vmsTens === 10) {
+        vsecondOnes++;
+        vmsTens = 0;
+    }
+    if (vsecondOnes === 10) {
+        vsecondTens++;
+        vsecondOnes = 0;
+        break;
+    }
+    
+        
+        //increment the hundreth ms
+        //if the hundreth ms is equal to 10 => increment the tenth ms
+        //if the tenth ms is equal to 10 => increment the second ones
+        //if the second ones is equal to 10 => increment the tenth seconds
+        console.log(ms);
+        seconds.innerHTML = ms;
     }
 }, 10);
 
