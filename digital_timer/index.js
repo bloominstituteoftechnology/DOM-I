05e8fada92fdd085b4695af375c16f2c700c9e95
Jeremy Jones <jeremyjones@Macintosh-58b035f2f989.local>
2018-02-26 18:56:54 -0800

    Updated / Cleaned up

--- digital_timer/index.js
+++ digital_timer/index.js
@@ -3,10 +3,6 @@ const secondOnes = document.getElementById("secondOnes");
 const msHundreds = document.getElementById("msHundreds");
 const msTens = document.getElementById("msTens");
 
-
-
-
-
 timer()
 function timer () {
     let counter = 0;
@@ -16,12 +12,9 @@ function timer () {
        secondOnes.innerHTML = "0";
        msHundreds.innerHTML = "0";
        msTens.innerHTML = "0";
-
-       // set an interval that calls updateTimer as well as calls endTimer after 10 seconds
-       
     }
-    var setTimer = setInterval(function(){ updateTimer() }, 1);
-
+    
+    var setTimer = setInterval(function(){ updateTimer() }, 10);
 
     function increment (divIncrement) {
         // helper function that gets called by updateTimer
@@ -31,19 +24,18 @@ function timer () {
     }
 
     function updateTimer () {
-       
-       
         increment(msTens) 
         ++counter;
+
         if (counter % 10 === 0) {
             increment(msHundreds);
-        } 
+        }
         if (counter % 100 === 0) {
             increment(secondOnes);
         }
         if (counter % 1000 === 0) {
             increment(secondTens);
-        } 
+        }
         if (counter === 1000) {
             clearInterval(setTimer);
             
@@ -51,15 +43,6 @@ function timer () {
                 for (let i = 0; i < element.length; i++) {
                 element[i].className += " redDigit";
                 }
-        
         }
-        // if (counter % )
-        // console.log(counter);
     }
-}
-
-// So we're using setInterval as a loop with 10ms intervals,  
-// updating a counter each time it runs,  
-// each time is runs it calls a function-- 
-// until a condition is reached -- 
-// once it's reached another function is called that uses clearInterval?
+};
