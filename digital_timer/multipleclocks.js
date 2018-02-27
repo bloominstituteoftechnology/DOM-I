896df03cea66db82f0f66bb6d875f99bab5e7d3c
DragonRydrz <DragonRydrz@me.com>
2018-02-26 19:39:34 -0500

    creating new clocks

--- digital_timer/multipleclocks.js
+++ digital_timer/multipleclocks.js
@@ -1,6 +1,3 @@
-
-
-
 let clockNumber = 1;
 
 const createClock = () => {
@@ -9,26 +6,40 @@ const createClock = () => {
 
   const secondsTens = document.createElement("div");
   secondsTens.classList.add("digit", `secondTens${clockNumber}`);
+  
+  secondsTens.innerHTML = '-';
 
   const seconds = document.createElement("div");
   seconds.classList.add("digit", `second${clockNumber}`);
   
+  seconds.innerHTML = '-';
+  
   const colon = document.createElement("div");
   colon.classList.add("digit", `colon${clockNumber}`);
+  
+  colon.innerHTML = ':';
   
   const msHundreds = document.createElement("div");
   msHundreds.classList.add("digit", `msHundreds${clockNumber}`);
   
+  msHundreds.innerHTML = '-';
+  
   const msTens = document.createElement("div");
   msTens.classList.add("digit", `msTens${clockNumber}`);
+  
+  msTens.innerHTML = '-';
   
   newDiv.appendChild(secondsTens);
   newDiv.appendChild(seconds);
   newDiv.appendChild(colon);
   newDiv.appendChild(msHundreds);
   newDiv.appendChild(msTens);
-
-  document.querySelector("body").appendChild("newDiv");
   console.log(newDiv);
+  document.querySelector('body').appendChild(newDiv);
+  clockNumber++;
 }
+createClock();
+createClock();
+createClock();
+createClock();
 createClock();
