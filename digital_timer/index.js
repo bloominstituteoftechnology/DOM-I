404a9caeb840f9ec77e1f7396e8f9f032fea7866
Robert Rak <robertrak@hotmail.com>
2018-02-26 19:40:15 -0500

    Project start

--- digital_timer/index.js
+++ digital_timer/index.js
@@ -1,8 +1,8 @@
 // Countdown clock
-const msHundreds = document.querySelector('#msHundreds');
-const msTens = document.querySelector('#msTens');
-const secondTens = document.querySelector('#secondTens');
-const secondOnes = document.querySelector('#secondOnes');
+const msHundreds = document.getElementById('msHundreds');
+const msTens = document.getElementById('msTens');
+const secondTens = document.getElementById('secondTens');
+const secondOnes = document.getElementById('secondOnes');
 
 timer();
 
@@ -15,17 +15,20 @@ function timer () {
     msHundreds.innerHTML = "0";
     msTens.innerHTML = "0";
 
+
     // set an interval that calls updateTimer and calls endTimer after 10 seconds
 
     let myInterval = window.setInterval(() => {
       console.log('10ms has passed');
       updateTimer();
     }, 10);
-  }
+}
 
   function increment (number) {
     // helper function that gets called by updateTimer
-    for (let i = 0; i <= 9; i++) {
+    for (let i = 0; i < 9; i++) {
+      return number.innerHTML = i;
+    }
 
     }
   }
@@ -35,7 +38,5 @@ function timer () {
   }
 
   function updateTimer () {
-
+    increment(msTens);
   }
-
-}
