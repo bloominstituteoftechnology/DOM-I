9e2e622af93ad36bae5b5ceabef6e37b05844ba7
Viveca Valdez <vivecavaldez@Vivecas-MBP.fios-router.home>
2018-02-26 19:34:19 -0500

    Nick Coultas & Vivi Valdez Final

--- digital_timer/index.js
+++ digital_timer/index.js
@@ -3,25 +3,29 @@ const secondOnes = document.getElementById("secondOnes");
 const msHundreds = document.getElementById("msHundreds");
 const msTens = document.getElementById("msTens");
 
-timer(10);
+secondTens.innerHTML = "0";
+secondOnes.innerHTML = "0";
+msHundreds.innerHTML = "0";
+msTens.innerHTML = "0";
 
-function timer (number) {
-    init();
-    function  init () {
-        secondTens.innerHTML = "0";
-        secondOnes.innerHTML = "0";
-        msHundreds.innerHTML = "0";
-        msTens.innerHTML = "0";
+function countUp() {
+    if (msTens.innerHTML < 9) {
+        ++msTens.innerHTML;
+    } else {
+        msTens.innerHTML = 0;
+        if (msHundreds.innerHTML < 9) {
+            ++msHundreds.innerHTML;
+        } else {
+            msHundreds.innerHTML = 0;
+            if (secondOnes.innerHTML < 9) {
+                ++secondOnes.innerHTML;
+            } else {
+                secondOnes.innerHTML = 0;
+                clearInterval(count);
+                ++secondTens.innerHTML;
+                document.getElementsByClassName("digits")[0].classList.add("redDigit");
+            }
+        }
     }
-
-    function increment () {
-    }
-    
-    function endTimer () {
-    }
-
-    function updateTimer () {
-        
-    }
-    
 }
+const count = setInterval(function() { countUp() }, 10);
