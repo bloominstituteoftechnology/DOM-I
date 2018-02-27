01b3127a25e916b0dd2962f74c5efb3e1d38560f
RiaClay <sherria.rydyn@gmail.com>
2018-02-26 19:11:22 -0600

    fixed code. Timer was stopping at 9:90.

--- digital_timer/index.js
+++ digital_timer/index.js
@@ -1,3 +1,5 @@
+// Sherria's Code
+
 window.onload = function startTimer() {
     const digitSelector = document.getElementsByClassName('digit');
     const secondTen = digitSelector[0];
@@ -5,6 +7,73 @@ window.onload = function startTimer() {
     const colon = digitSelector[2];
     const msHundred = digitSelector[4]
     const msTen = digitSelector[3];
+    console.log(secondTen);
+
+       
+    let milliSec = 0;
+    secondTen.innerHTML = "0";
+    secondOne.innerHTML = "0";
+    msHundred.innerHTML = "0";
+    msTen.innerHTML = "0";
+
+        const digitalTimer = setInterval (() => {
+            milliSec += 10;
+            if (milliSec === 10000) {
+               endTimer(digitalTimer); 
+            }
+            updateTimer(milliSec);
+        }, 10);
+     
+
+        function increment (string) {
+            let number = Number(string) + 1;
+            return number.toString();
+        }
+     
+        function endTimer (stopId) {
+            clearInterval(stopId);
+            Array.from(digitSelector).forEach(digit => {
+                digit.classList.add("redDigit");
+            })
+        }
+
+        function updateTimer (milliSec) {
+           if(milliSec === 10000) {
+              secondTen.innerHTML = "1";
+              secondOne.innerHTML = "0";
+              msHundred.innerHTML ="0";
+              msTen.innerHTML = "0";
+           } else if (milliSec % 1000 === 0) {
+               secondOne.innerHTML = increment(secondOne.innerHTML);
+               msHundred.innerHTML = "0";
+               msTen.innerHTML = "0";
+           } else if (milliSec % 100 === 0) {
+               msHundred.innherHTML = increment(msHundred.innerHTML);
+               msTen.innerHTML = "0";
+           } else {
+               msTen.innerHTML = increment(msTen.innerHTML);
+           }
+        }
+           
+    }
+        
+// Justin's code
+
+window.onload = function startTimer() {
+    const digitSelector = document.getElementsByClassName('digit');
+    const secondTen = digitSelector[0];
+    const secondOne = digitSelector[1];
+    const colon = digitSelector[2];
+    const msHundred = digitSelector[4]
+    const msTen = digitSelector[3];
+    console.log(secondTen);
+
+       
+    let milliSec = 0;
+    secondTen.innerHTML = "0";
+    secondOne.innerHTML = "0";
+    msHundred.innerHTML = "0";
+    msTen.innerHTML = "0";
 
     (function init(){
         msTen.innerHTML = "0";
@@ -48,4 +117,4 @@ window.onload = function startTimer() {
         secondOne.style.color = "red";
         colon.style.color = "red";
     }
-};
+}
