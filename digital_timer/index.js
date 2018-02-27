177a3c3734f367cae34cfd5c47fcef409a17fa4b
ubuntu <irishseanvaldivia@gmail.com>
2018-02-27 00:52:29 -0600

    Working on the night shift...

--- digital_timer/index.js
+++ digital_timer/index.js
@@ -5,3 +5,16 @@ const msHundreds = document.getElementById("msHundreds");
 const msTens = document.getElementById("msTens");
 const msOnes = document.getElementById("msOnes");
 
+timer();
+
+function timer () {
+  init();
+
+  function init () {
+    let ms = 0;
+    secondTens.innerHTML = "0";
+    secondOnes.innerHTML = "0";
+    msHundreds.innerHTML = "0";
+    msTens.innerHTML = "0";
+  }
+}
