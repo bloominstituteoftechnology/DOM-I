37e3f40f1feb090e4ef39236401d95dbd3ebffed
unknown <adfaris@gmail.com>
2018-02-26 19:07:35 -0500

    It turns red after 10 sec but doesn't count down

--- digital_timer/index.js
+++ digital_timer/index.js
@@ -22,11 +22,11 @@ function clockCounter () {
         const clockInterval = window.setInterval(() => {
             totalMs += 10;
             //if totalMs === 10000 -- go to stop function
-            if (ms === 10000) {
-                endTimer();
+            if (totalMs === 10000) {
+                endTimer(clockInterval);
             }
             //else go to updater function
-            updateTimer();
+            updateTimer(totalMs);
     
         }, 10);
 
@@ -34,15 +34,45 @@ function clockCounter () {
 
     function increment () {
         //incremements numbers -- called from updateTimer?
+        let time = Number(num + 1);
+        // let time = Number(num) + 1;
+        return time;
     }
 
-    function endTimer () {
+    function endTimer (intervalID) {
         //stop function - once 10,000 ms is reached within setInterval, stop function will run clearInterval and set each element to red by adding .redDigit to digit.classList
-
+        clearInterval(intervalID);
+        digits.classList.add("redDigit")
     }
 
-    function updateTimer () {
+    function updateTimer (totalMs) {
         //updater function - take totalMs and updates .innerHTML for each element based on total ms that has passed, using if statements?
+        if( totalMs === 10000){
+            ms10s.innerHTML = "0";
+            ms100s.innerHTML = "0";
+            second1s.innerHTML = "0";
+            second10s.innerHTML = "1";
+        } 
+        else if (totalMs % 1000 === 0) {
+            ms10s.innerHTML = "0";
+            ms100s.innerHTML = "0";
+            second1s.innerHTML = increment(second1s.innerHTML);
+            // second10s.innerHTML = "1";
+        }
+        
+        else if (totalMs %100 === 0) {
+            ms10s.innerHTML = "0";
+            ms100s.innerHTML = increment(ms100s.innerHTML);
+            // second1s.innerHTML = "0";
+            // second10s.innerHTML = "1";
+        }
+
+        else {
+            ms10s.innerHTML = increment(ms10s.innerHTML);
+            // ms100s.innerHTML = "0";
+            // second1s.innerHTML = "0";
+            // second10s.innerHTML = "1";
+        }
 
     }
 
