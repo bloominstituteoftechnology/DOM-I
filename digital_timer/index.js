bf01b63e3547e4cc04675450dc9df587d68ebd6c
ubuntu <irishseanvaldivia@gmail.com>
2018-02-27 00:49:56 -0600

    Still going...

---
+++ digital_timer/index.js
@@ -0,0 +1,7 @@
+const digits = document.querySelector(".digits");
+const secondTens = document.getElementById("secondTens");
+const secondOnes = document.getElementById("secondOnes");
+const msHundreds = document.getElementById("msHundreds");
+const msTens = document.getElementById("msTens");
+const msOnes = document.getElementById("msOnes");
+
