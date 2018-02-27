9fe27603725d5bd310ae0746a0000cd7ba0dae7d
Carlos Andrade <carlos.andrade86@outlook.com>
2018-02-26 20:37:48 -0500

    Robert Rak & Carlos Andrade--DOM-I--COMPLETED PROJECT

--- digital_timer/index.js
+++ digital_timer/index.js
@@ -1,42 +1,64 @@
-// Countdown clock
-const msHundreds = document.getElementById('msHundreds');
-const msTens = document.getElementById('msTens');
-const secondTens = document.getElementById('secondTens');
-const secondOnes = document.getElementById('secondOnes');
+const secondTens = document.getElementById("secondTens");
+const secondOnes = document.getElementById("secondOnes");
+const msHundreds = document.getElementById("msHundreds");
+const msTens = document.getElementById("msTens");
 
 timer();
 
 function timer () {
+  
   init();
+  const myInterval = window.setInterval(updateTimer, 10);
 
-  function init () {
-    secondTens.innerHTML = "0";
-    secondOnes.innerHTML = "0";
-    msHundreds.innerHTML = "0";
-    msTens.innerHTML = "0";
-
-
-    // set an interval that calls updateTimer and calls endTimer after 10 seconds
-
-    let myInterval = window.setInterval(() => {
-      console.log('10ms has passed');
-      updateTimer();
-    }, 10);
+  function init() {
+    secondTens.innerHTML = '0';
+    secondOnes.innerHTML = '0';
+    msHundreds.innerHTML = '0';
+    msTens.innerHTML = '0';
+    
+    // remove red color in text.
+    secondTens.parentElement.classList.remove("redDigit");
 }
 
-  function increment (number) {
-    // helper function that gets called by updateTimer
-    for (let i = 0; i < 9; i++) {
-      return number.innerHTML = i;
+    let counter = 0;
+    let last = 0;
+    let second = 0;
+    let first = 0;
+
+    function increment() {
+        counter++;
     }
 
+    function endTimer () {
+        // convert the color to red
+        secondTens.parentElement.classList.add("redDigit");
+      
+        clearInterval(myInterval);
     }
-  }
 
-  function endTimer () {
-    return clearInterval(myInterval);
-  }
+    function updateTimer() {
+       increment();
 
-  function updateTimer () {
-    increment(msTens);
-  }
+        if (counter === 10){
+            counter = 0;
+            first++;
+        }
+
+        if (first === 10){
+            first = 0;
+            second++;
+        }
+        
+        if (second === 10){
+            second = 0;
+            last++;
+        }
+        
+        secondTens.innerHTML = last;
+        secondOnes.innerHTML = second;
+        msHundreds.innerHTML = first;
+        msTens.innerHTML = counter;
+        
+       if(last === 1) endTimer();// base case
+    }
+}
