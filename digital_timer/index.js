ba8592aa87152bfe120ad9c0494afd363767e12c
Shaun Kelly <Shaun@redskelly.local>
2018-02-26 21:17:42 -0500

    updated counters

--- digital_timer/index.js
+++ digital_timer/index.js
@@ -3,28 +3,71 @@ const secondOnes = document.getElementById('secondOnes');
 const msHundreds = document.getElementById('msHundreds');
 const msTens = document.getElementById('msTens');
 
-timer();
+let secondTensCounter = 0;
+let secondOnesCounter = 0;
+let msHundredsCounter = 0;
+let msTensCounter = 0;
 
-function timer () {
-    init();
-
-    function init () {
-        secondTens.innerHTML = '0';
-        secondOnes.innerHTML = '0';
-        msHundreds.innerHTML = '0';
+let timer = setInterval(function() {
+    if (msTens.innerHTML !== '9') {
+        msTensCounter++;
+        msTens.innerHTML = msTensCounter.toString();
+    } else {
+        msTensCounter = 0;
         msTens.innerHTML = '0';
+        if (msHundreds.innerHTML !== '9') {
+            msHundredsCounter++;
+            msHundreds.innerHTML = msHundredsCounter.toString();
+        }
     }
-    // set an interval that calls updateTimer and calls endTimer after 10 seconds
-}
+}, 50)
 
-function increment () {
-    // helper function that gets called by updateTimer
-}
 
-function endTimer () {
 
-}
 
-function updateTimer () {
 
-}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// timer();
+
+// function timer () {
+//     init();
+
+//     function init () {
+//         secondTens.innerHTML = '0';
+//         secondOnes.innerHTML = '0';
+//         msHundreds.innerHTML = '0';
+//         msTens.innerHTML = '0';
+//     }
+//     // set an interval that calls updateTimer and calls endTimer after 10 seconds
+// }
+
+// function increment () {
+//     // helper function that gets called by updateTimer
+// }
+
+// function endTimer () {
+
+// }
+
+// function updateTimer () {
+
+// }
