53e530ce590c3162036164f8c6c729b25e9f5bf7
Jonathan Reeves <jonathanedwardreeves1988@gmail.com>
2018-02-26 18:02:18 -0600

    Tens place stuck at 9

---
+++ digital_timer/index.js
@@ -0,0 +1,31 @@
+const secondTens = document.getElementById("secondTens");
+const secondOnes = document.getElementById("secondOnes");
+const msHundreds = document.getElementById("msHundreds");
+const msTens = document.getElementById("msTens");
+
+secondTens.innerHTML = "0";
+secondOnes.innerHTML = "0";
+msHundreds.innerHTML = "0";
+msTens.innerHTML = "0";
+
+function timer() {
+
+    if (msTens.innerHTML < 9) {
+        ++msTens.innerHTML;
+    } else {
+        msTens.innerHTML = 0;
+        if (msHundreds.innerHTML < 9) {
+            ++msHundreds.innerHTML;
+        } else {
+            msHundreds.innertHTML = 0;
+            if (secondOnes.innerHTML < 9) {
+                ++secondOnes.innerHTML;
+            } else {
+                secondOnes.innerHTML = 0;
+                clearInterval(countToTen);
+                ++secondTens.innerHTMLdocument.getElementsByClassName("digits")[0].classList.add("redDigit");
+            }
+        }
+    }
+}
+const countToTen = setInterval(function() { timer() }, 10);
