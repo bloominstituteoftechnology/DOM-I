a82531cc50206eb621220e56cdfc2639b655fbe8
Edward Manda <lilkups@yahoo.com>
2018-02-26 18:22:23 -0600

    DOM I done, refactoring next!

--- digital_timer/index.js
+++ digital_timer/index.js
@@ -1,26 +1,85 @@
-const digits = document.getElementsByClassName('digits');
 
 //  set each id as it's own variable
 const secondTens = document.getElementById("secondTens");
-console.log(secondTens);
-
 const secondOnes = document.getElementById("secondOnes");
-console.log(secondOnes);
-
 const msHundreds = document.getElementById("msHundreds");
-console.log(msHundreds);
-
 const msTens = document.getElementById("msTens");
-console.log(msTens);
 
-let count = 0;
+function timer () {
+    let counter = 0;
+    let a = 0, b = 0, c = 0;
 
-const timeChks = () => {
-    count++;
-    return count;
-}; 
+    function increment() {
+        counter++;
+    }
 
-var timer = window.setInterval(timeChks(), 1000);
-console.log(timer);
+    function endTimer () {
+        // change color to red
+        secondTens.parentElement.style.color = 'red';
+        // clearInterval
+        clearInterval(time);
+    }
 
+    function updateTimer() {
+       // call increment
+       increment();
 
+        if (counter === 10){
+            counter = 0;
+            c++;
+        }
+
+        if (c === 10){
+            c = 0;
+            b++;
+        }
+        
+        if (b === 10){
+            b = 0;
+            a++;
+        }
+        
+        secondTens.innerHTML = a;
+        secondOnes.innerHTML = b;
+        msHundreds.innerHTML = c;
+        msTens.innerHTML = counter;
+        
+       if(a === 1) endTimer();
+    }
+
+    function init() {
+        secondTens.innerHTML = '0';
+        secondOnes.innerHTML = '0';
+        msHundreds.innerHTML = '0';
+        msTens.innerHTML = '0';
+        
+    }
+
+    // initialize variables, html display
+    init();
+    const time = window.setInterval(updateTimer, 10);
+}
+
+// initialize timer
+timer();
+/*// start time function
+const timer = (current) => {
+    console.log('timer start ', current);
+    count++
+
+    console.log('inside the timers' , count);
+    if(count > 3) stopTimer();
+};
+
+// stop timer function, change color to red also stop timer at 10 sec
+const stopTimer = () => {
+    //change color;
+    clearInterval(timeInterval);
+};
+
+const startTimer = (counter) => {
+    timeInterval = setInterval(timer, 1000, counter);
+};
+
+//startTimer(counter);
+*/
