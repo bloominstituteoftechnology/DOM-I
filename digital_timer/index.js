a1bbf240ea9ad2987dab3f968ec39a334a3c4bd1
JPEmery <clearkeyemery@gmail.com>
2018-03-06 22:35:23 -0600

    JP Emery: good ol college try

--- digital_timer/index.js
+++ digital_timer/index.js
@@ -1,49 +1,36 @@
 
-// set all === to 0 
 
-// ten second interval
-// setTimeout(
-//     function(){ 
-//         // .innerHTML = 1 
-//      },
-//     10000);
+let count = 0
 
-// set a one second interval with a break at 10 
-let i = 0
-let val = setInterval(
-        function(){
-            i++;
-            if(i === 10) {    
-                clearInterval(val);
-                document.querySelector("#secondOnes").innerHTML = `0`;
-                return; 
-            } 
-            document.querySelector("#secondOnes").innerHTML = `${i}`
-              
+const timer = setInterval( () => masterFunction(), 10);
+
+const masterFunction = () => {
+    ++count;
+        if(count % 1000 === 0) {
+         
+        document.querySelector("#secondTens").innerHTML = `1`;
         }
-      
-    
-      , 1000);
+
+        if(count % 100 == 0) {
+            let newCount = count / 100;
+            if(newCount > 9) 
+            {
+            newCount = 0;
+            }
+            document.querySelector("#secondOnes").innerHTML = `${newCount}`;
+        }
+
+        if(count % 10 == 0) {
+            let newCount = count / 10;
+            if(newCount > 9) 
+            {
+            newCount = 0;
+            }
+            document.querySelector("#msHundreds").innerHTML = `${newCount}`;
+        }
 
 
-    //   setInterval(function(){
-    //     let i = 0; 
-    //     while(i<10) {
-    //         i++
-    //         document.querySelector("#secondOnes").innerHTML = `${i}`
-    //     }
-    // }, 1000);
-
-
-  // if( i === 10 ) ;{
-        //      clearInterval()
-        //         //.innerHTML = 0
-        //     }
-        // set to innerHTML and stop at 9sec
-        // secondOnes
+}
 
 
 
-
-
-// the reast
