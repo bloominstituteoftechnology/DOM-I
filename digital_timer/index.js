f419e1a814a0ab21281b8f432bf9778f59034b2d
Andrew Jarrett <ahrjarrett@gmail.com>
2018-03-07 12:32:42 -0700

    bug fix: pass timer to clearInterval instead of ms

--- digital_timer/index.js
+++ digital_timer/index.js
@@ -1,35 +1,34 @@
-const secondTens  = document.querySelector('#secondTens')
-const secondOnes  = document.querySelector('#secondOnes')
-const msHundreds  = document.querySelector('#msHundreds')
-const msTens      = document.querySelector('#msTens')
-const digits      = document.querySelector('.digits')
-const startButton = document.querySelector('.start-button')
-const stopButton = document.querySelector('.stop-button')
-const resetButton = document.querySelector('.reset-button')
-const enterMs = document.querySelector('.enter-ms')
+const secondTens  = document.querySelector('#secondTens');
+const secondOnes  = document.querySelector('#secondOnes');
+const msHundreds  = document.querySelector('#msHundreds');
+const msTens      = document.querySelector('#msTens');
+const digits      = document.querySelector('.digits');
+const startButton = document.querySelector('.start-button');
+const stopButton  = document.querySelector('.stop-button');
+const resetButton = document.querySelector('.reset-button');
+const enterMs     = document.querySelector('.enter-ms');
 
-let timer;
-let counter;
-let ms;
+let timer,
+    counter,
+    ms;
 
 const reset = () => {
-    secondTens.innerHTML = 0
-    secondOnes.innerHTML = 0
-    msHundreds.innerHTML = 0
-    msTens.innerHTML     = 0
-    digits.style.color = 'black';
-    counter = 0;
-    
+  secondTens.innerHTML = 0;
+  secondOnes.innerHTML = 0;
+  msHundreds.innerHTML = 0;
+  msTens.innerHTML     = 0;
+  digits.style.color = 'black';
+  counter = 0;
 }
+
+const incrementString = str => (Number(str) + 1).toString();
 
 reset();
 
-const incrementString = str => (Number(str) + 1).toString()
-
 //deleted stopTime bc its not needed
-function createTimer(ms) {
+const createTimer = ms => {
 
-   timer = setInterval(() => {
+  timer = setInterval(() => {
     counter = counter + 10;
 
     if (counter % 10 === 0) {
@@ -50,42 +49,34 @@ function createTimer(ms) {
       secondOnes.innerHTML = 0;
       secondTens.innerHTML = incrementString(secondTens.innerHTML);
     }
-  }, 10)
+  }, 10);
 
-
-
-setTimeout(() => {
-    clearInterval(ms);
+  setTimeout(() => {
+    clearInterval(timer);
     digits.style.color = 'red';
   }, ms);
-    
-    
-    
-    
-    
 }
 
 startButton.addEventListener('click', (event) => {
     ms = enterMs.value;
     createTimer(ms);
   // we can also make setTimeout a method and call it here
-    //if the counter is not 0, counter needs to be the difference of ms and the time already in the counter.
-//    if(counter != 0 null, ){
-//    timer = ms - counter;
-//    }
-})
+  //if the counter is not 0, counter needs to be the difference of ms and the time already in the counter.
+  //    if(counter != 0 null, ){
+  //    timer = ms - counter;
+  //    }
+});
 
 stopButton.addEventListener('click', (event) => {
-    clearTimeout(timer);
-
-})
+  clearTimeout(timer);
+});
 
 resetButton.addEventListener('click', (event) => {
-    reset();
-    clearTimeout(timer);
-    clearInterval(timer);
+  reset();
+  clearTimeout(timer);
+  clearInterval(timer);
     
-    enterMs.innerHTML = 0;
-    //must also delete the stored history
-})
+  enterMs.innerHTML = 0;
+  //must also delete the stored history
+});
 
