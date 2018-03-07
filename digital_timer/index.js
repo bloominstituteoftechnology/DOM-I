e84ff1885b72330d06582726b342fc6b29e77392
jnandez0586 <jnandez0586@gmail.com>
2018-03-06 21:31:58 -0600

    finished assignment, now starting stretch goal

--- digital_timer/index.js
+++ digital_timer/index.js
@@ -2,57 +2,88 @@ let tens;
 let ones;
 let miliH;
 let miliT;
-let count1 = 1;
+let count1 = 0;
 let count2 = 0;
 let count3 = 0;
 let count4 = 0;
 
 function timer(){
-    tens = window.setInterval(sec1, 10000);
-    ones = window.setInterval(sec2, 1000);
-    miliH = window.setInterval(sec3, 100);
-    miliT = window.setInterval(sec4, 10);
-}
+    // tens = window.setInterval(sec1, 10000);
+    miliH = window.setInterval(sec2, 100);
+    ones = window.setInterval(sec1, 1000);
     
+    // miliT = window.setInterval(sec3, 10);
+}
+
+// let i = 0;
+// const cb = () => {
+//     i++
+//     console.log(i);
+//     if(i === 10){
+//         clearInterval()
+//     }
+// }
+    
+// function sec1(){
+//     let sT = document.querySelector("#secondTens");
+//     if(count1 < 10){
+        
+//         count1++;
+//     }else{
+//         clearInterval(tens);
+//     }
+// }
+
 function sec1(){
-    let sT = document.querySelector("#secondTens");
-    if(count1 < 10){
-        sT.innerHTML = count1;
-        count1++;
-    }else{
-        count1 = 0;
+    let sT1 = document.querySelector("#secondTens");
+    let sT2 = document.querySelector("#secondOnes");
+    let sT3 = document.querySelector("#msHundreds");
+    let sT4 = document.querySelector("#msTens");
+    let colon = document.querySelector("#colon");
+    // if(count2 < 10){
+    //     sT2.innerHTML = count2;
+    //     count2++;
+    count2++;
+    sT2.innerHTML = count2;
+    sT1.innerHTML = "";
+    if(count2 === 10){
+        sT1.innerHTML = 1;
+        sT2.innerHTML = 0;
+        sT3.innerHTML = 0;
+        sT4.innerHTML = 0;
+        clearInterval(ones);
+        clearInterval(miliH);
+        // clearInterval(miliT);
+        sT1.style.color = "red";
+        sT2.style.color = "red";
+        sT3.style.color = "red";
+        sT4.style.color = "red";
+        colon.style.color = "red";
     }
+    //else{
+    //     sT1.innerHTML = 1;
+    //     sT2.innerHTML = 0;
+    //     clearInterval(ones);
+    //     clearInterval(miliH);
+    //     clearInterval(miliT);
+    // }
 }
 
 function sec2(){
-    let sT = document.querySelector("#secondOnes");
-    if(count2 < 10){
-        sT.innerHTML = count2;
-        count2++;
-    }else{
-        count2 = 0;
-    }
+    let sT1 = document.querySelector("#msHundreds");
+    let sT2 = document.querySelector("#msTens");
+    sT2.innerHTML = "";
+    count3++;
+    sT1.innerHTML = count3;
 }
 
-function sec3(){
-    let sT = document.querySelector("#msHundreds");
-    if(count3 < 10){
-        sT.innerHTML = count3;
-        count3++;
-    }else{
-        count3 = 0;
-    }
-}
-
-function sec4(){
-    let sT = document.querySelector("#msTens");
-    if(count4 < 10){
-        sT.innerHTML = count4;
-        count4++;
-    }else{
-        count4 = 0;
-    }
-}
+// function sec3(){
+//     let sT = document.querySelector("#msTens");
+//     count4 += 1;
+//     console.log(count4);
+    
+//     sT.innerHTML = count4;
+// }
 
 // timer(change("#secondTens"), 1000);
 // console.log(sT.innerHTML);
