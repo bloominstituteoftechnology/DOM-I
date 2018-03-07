5f4a64158774aa16e3e3da0fdd7b9bc996c3337b
Charlie <ccariste@gmail.com>
2018-03-06 19:42:29 -0800

    CRC: fixed variable names so make code clearer

--- digital_timer/index.js
+++ digital_timer/index.js
@@ -3,11 +3,13 @@ let currentTens = 0;
 let currentSec = 0;
 let currentHs = 0;
 let currentMs = 0;
+let digitOne = document.getElementById('secondTens');
+let digitTwo = document.getElementById('secondOnes');
+let digitThree = document.getElementById('msHundreds');
+let digitFour = document.getElementById('msTens');
 
-const secondOnes = window.setInterval( () => {
-    secondOne = document.getElementById('secondOnes');
-    secondTens = document.getElementById('secondTens');
-    secondOne.innerHTML = current;
+const d2 = window.setInterval( () => {
+    digitTwo.innerHTML = current;
     current = ++current;
     if (current === 10) {
         current = 0;
@@ -15,47 +17,44 @@ const secondOnes = window.setInterval( () => {
     }
 }, 1000);
 
-const secondTimes = window.setInterval( () => {
-    // secondTime = document.getElementById('secondTens');
+const d1 = window.setInterval( () => {
     if (currentHs === 9) {
         currentSec = ++currentSec;
     }
-    secondTens.innerHTML = currentSec;
+    digitOne.innerHTML = currentSec;
 }, 10000);
 
-const hsTimes = window.setInterval( () => {
-    hsTime = document.getElementById('msHundreds');
-    hsTime.innerHTML = currentHs;
-    currentHs = ++currentHs;
-    if (currentHs === 10) {
-        currentHs = 0;
-    }
-    hsTime.innerHTML = currentHs;
-}, 100);
-
-const msTimes = window.setInterval( () => {
-    msTime = document.getElementById('msTens');
-    msTime.innerHTML = currentMs;
+const d4 = window.setInterval( () => {
+    digitFour.innerHTML = currentMs;
     currentMs = ++currentMs;
     if (currentMs === 10) {
         currentMs = 0;
     }
-    msTime.innerHTML = currentMs;
+    digitFour.innerHTML = currentMs;
     if (currentSec === 1) {
-        clearInterval(msTimes);
-        msTime.innerHTML = 0;
-        msTime.style.color = 'red';
+        clearInterval(d4);
+        digitFour.innerHTML = 0;
     }
 }, 10);
 
+const d3 = window.setInterval( () => {
+    digitThree.innerHTML = currentHs;
+    currentHs = ++currentHs;
+    if (currentHs === 10) {
+        currentHs = 0;
+    }
+    digitThree.innerHTML = currentHs;
+}, 100);
+
 const stop = window.setTimeout( () => {
-    clearInterval(hsTimes);
-    clearInterval(secondOnes);
+    clearInterval(d3);
+    clearInterval(d2);
     // let digits = document.getElementsByClassName('digits');
-    // digits.style.color = 'red';
-    hsTime.style.color = 'red';
-    secondOne.style.color = 'red';
-    secondTens.style.color = 'red';
+    // digits.style.color = 'red';  // how to get class group to change together?
+    digitFour.style.color = 'red';
+    digitThree.style.color = 'red';
+    digitTwo.style.color = 'red';
+    digitOne.style.color = 'red';
     (document.getElementById('colon')).style.color = 'red';
 
 }, 10001);
