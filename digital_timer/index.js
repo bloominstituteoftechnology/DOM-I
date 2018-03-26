64eeb87fa8f673e5199a1d743a4dd4e5a4ebec7e
nickbrenn <nbren@tutanota.com>
2018-03-26 18:06:44 -0400

    Fixed start button and implemented a reset button

--- digital_timer/index.js
+++ digital_timer/index.js
@@ -1,5 +1,3 @@
-startButton.onclick = function () { incrementTimer(); };
-
 let ms = 0;
 const msLimit = 10000;
 
@@ -8,22 +6,49 @@ document.getElementById('secondOnes').innerHTML = 0;
 document.getElementById('msHundreds').innerHTML = 0;
 document.getElementById('msTens').innerHTML = 0;
 
-function incrementTimer() {
-  ms += 10;
+function resetTimer() {
+  let ms = 0;
+  document.getElementById('secondTens').innerHTML = 0;
+  document.getElementById('secondOnes').innerHTML = 0;
+  document.getElementById('msHundreds').innerHTML = 0;
+  document.getElementById('msTens').innerHTML = 0;
+  document.querySelector('.digits').style = 'color: black;'
+}
 
-  document.getElementById('secondTens').innerHTML = parseInt(ms / 10000) % 10;
-  document.getElementById('secondOnes').innerHTML = parseInt(ms / 1000) % 10;
-  document.getElementById('msHundreds').innerHTML = parseInt(ms / 100) % 10;
-  document.getElementById('msTens').innerHTML = parseInt(ms / 10) % 10;
+function startTimer() {
+  document.getElementById('secondTens').innerHTML = 0;
+  document.getElementById('secondOnes').innerHTML = 0;
+  document.getElementById('msHundreds').innerHTML = 0;
+  document.getElementById('msTens').innerHTML = 0;
 
-  if (ms === msLimit) {
-    stopTimer();
-    document.querySelector('.digits').style = 'color: red;'
+  function incrementTimer() {
+    ms += 10;
+
+    document.getElementById('secondTens').innerHTML = parseInt(ms / 10000) % 10;
+    document.getElementById('secondOnes').innerHTML = parseInt(ms / 1000) % 10;
+    document.getElementById('msHundreds').innerHTML = parseInt(ms / 100) % 10;
+    document.getElementById('msTens').innerHTML = parseInt(ms / 10) % 10;
+
+    if (ms === msLimit) {
+      stopTimer();
+      document.querySelector('.digits').style = 'color: red;'
+    }
   }
+
+  function stopTimer() {
+    ms = 0;
+    clearInterval(msTimer);
+  }
+
+  let msTimer = setInterval(incrementTimer, 10);
 }
 
-function stopTimer() {
-  clearInterval(msTimer);
-}
+const startButton = document.createElement('button');
+document.body.appendChild(startButton);
+startButton.innerHTML = 'Start';
+startButton.onclick = startTimer;
 
-let msTimer = setInterval(incrementTimer, 10);
+const resetButton = document.createElement('button');
+document.body.appendChild(resetButton);
+resetButton.innerHTML = 'Reset';
+resetButton.onclick = resetTimer;
