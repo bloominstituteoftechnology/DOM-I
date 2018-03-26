98f341417ab3c94f4d102acce8223e7fb2622579
Kyle Kauzlarich <kauzlarich.kyle@gmail.com>
2018-03-26 15:25:14 -0700

    got it counting up automaticly now

--- digital_timer/index.js
+++ digital_timer/index.js
@@ -1,26 +1,28 @@
-let tenMs = 9;
-let hundredsMs = 9;
-let oneSeconds = 9;
+let tenMs = 0;
+let hundredsMs = 0;
+let oneSeconds = 0;
 let tenSeconds = 0;
 document.getElementById('msTens').innerHTML = tenMs;
 document.getElementById('msHundreds').innerHTML = hundredsMs;
 document.getElementById('secondOnes').innerHTML = oneSeconds;
 document.getElementById('secondTens').innerHTML = tenSeconds;
-tenMs++;
-if (tenMs === 10) {
-  hundredsMs++;
-  tenMs = 0;
-}
-if (hundredsMs === 10) {
-  oneSeconds++;
-  hundredsMs = 0;
-}
-
-if (oneSeconds === 10) {
-  tenSeconds++;
-  oneSeconds = 0;
-}
-document.getElementById('msTens').innerHTML = tenMs;
-document.getElementById('msHundreds').innerHTML = hundredsMs;
-document.getElementById('secondOnes').innerHTML = oneSeconds;
-document.getElementById('secondTens').innerHTML = tenSeconds;
+let test = function() {
+    for (i = 0; i < 1; i++) { tenMs++}
+  if (tenMs === 10) {
+    hundredsMs++;
+    tenMs = 0;
+  }
+  if (hundredsMs === 10) {
+    oneSeconds++;
+    hundredsMs = 0;
+  }
+  if (oneSeconds === 10) {
+    tenSeconds++;
+    oneSeconds = 0;
+  }
+  document.getElementById('msTens').innerHTML = tenMs;
+  document.getElementById('msHundreds').innerHTML = hundredsMs;
+  document.getElementById('secondOnes').innerHTML = oneSeconds;
+  document.getElementById('secondTens').innerHTML = tenSeconds;
+  }
+  setInterval(test, 10);
