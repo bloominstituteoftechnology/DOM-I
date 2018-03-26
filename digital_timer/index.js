d37064ab58caec9ad7ddf80d9a7b3b637c591f5a
Cruise Brantley <cruise.brantley@gmail.com>
2018-03-26 17:38:52 -0500

    Works, time seems slightly off - investigating!

--- digital_timer/index.js
+++ digital_timer/index.js
@@ -1,35 +1,34 @@
-let hundredths = document.getElementById('mstens');
+let hundredths = document.getElementById('msHundreds');
 let tenths = document.getElementById('msTens');
 let ones = document.getElementById('secondOnes');
 let tens = document.getElementById('secondTens');
 
-let hundredthsCount = 0;
-let tenthsCount = 0;
-let onesCount = 0;
-let tensCount = 0;
+let hundredthsCount = 1;
+let tenthsCount = 1;
+let onesCount = 1;
+let tensCount = 1;
 
 int = setInterval(() => {
-    if (tenthsCount <= 9) {
-        tenthsCount.innerHTML = tenthsCount++;
-    } else if (hundredthsCount <= 9) {
-        tenthsCount = 0;
-        hundredthsCount.innerHTML = hundredthsCount++;
-    } else if (onesCount <= 9) {
+    if (hundredthsCount <= 9) {
+        hundredths.innerHTML = hundredthsCount++;
+    } else if (tenthsCount <= 9) {
         hundredthsCount = 0;
-        onesCount.innerHTML = onesCount++;
+        tenths.innerHTML = tenthsCount++;
+    } else if (onesCount <= 9) {
+        tenthsCount = 0;
+        ones.innerHTML = onesCount++;
     } else {
-        onesCount = 0;
-        tensCount.innerHTML = tensCount++;
-    }
-    if (tensCount === 1) {
         clearInterval(int);
         hundredths.innerHTML = '0';
         tenths.innerHTML = '0';
         ones.innerHTML = '0';
         tens.innerHTML = '1';
+        document.getElementsByClassName('digits') = 
     }
-}
 }, 10);
+
+
+//---------------- Failed Attempts! ---------------//
 
 // int1 = setInterval(() => {
 //     if(hundredths.innerHTML < 10)
@@ -54,5 +53,3 @@ int = setInterval(() => {
 //         tens.innerHTML++;
 //     tens.innerHTML = 1;
 // }, 10000);
-
-// if(tens)
