d9ec8eab44d5084fc41e29d61de80ff2eed39f47
Devon Bacon <devon.bacon@gmail.com>
2018-03-26 14:27:16 -0700

    Refactor phase 1

--- digital_timer/index.js
+++ digital_timer/index.js
@@ -12,28 +12,26 @@ msHundreds.innerHTML = 0;
 msTens.innerHTML = 0;
 
 tenSecondTimer = () => {
-  // Start timer by incrementing milliseconds
-  if (msTens.innerHTML < 9) {
-    msTens.innerHTML++;
-  // Once 9 milliseconds have passed
-  } else if (msTens.innerHTML % 9 === 0 && msHundreds.innerHTML < 9) {
+  msTens.innerHTML++;
+  // Once 90 milliseconds have passed
+  if (msTens.innerHTML === '9') {
     // Reset msTens value and increment msHundreds
     msTens.innerHTML = 0;
     msHundreds.innerHTML++;
-  // Once 100 milliseconds have passed
-  } else if (msHundreds.innerHTML % 9 === 0 && secondOnes.innerHTML < 9) {
+  } 
+  // Once 1000 milliseconds have passed
+  if (msHundreds.innerHTML === '9') {
     // Reset msHundreds value and increment secondOnes
     msHundreds.innerHTML = 0;
     secondOnes.innerHTML++;
+  }
   // Once 10 seconds have passed
-  } else if (secondOnes.innerHTML % 9 === 0) {
-    // Stop timer
-    clearInterval(timer);
-    // Reset Values
+  if (secondOnes.innerHTML === '9') {
     secondOnes.innerHTML = 0;
-    msTens.innerHTML = 0;
-    msHundreds.innerHTML = 0;
     secondTens.innerHTML++;
+  }
+  if (secondTens.innerHTML === '1') {
+    clearInterval(timer);
     // Turn digits color red
     document.querySelector(".digits").className += " redDigit";
   }
