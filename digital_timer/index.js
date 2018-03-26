a935d7ce72b1b209d754c18e19926b598a017d3b
Justin M <jekm321@gmail.com>
2018-03-26 19:03:12 -0400

    did a little testing on my own and commented the result out, also added comments

--- digital_timer/index.js
+++ digital_timer/index.js
@@ -6,12 +6,22 @@ const msHun = document.getElementById('msHundreds');
 const msTen = document.getElementById('msTens');
 const startbtn = document.getElementById('start');
 const resetbtn = document.getElementById('reset');
+// const stopbtn = document.getElementById('stop');
+
+// these are all constants since value will not be changed, will be used to 
+// access the document and make changes to the css/html
 
 startbtn.addEventListener('click', timer);
 resetbtn.addEventListener('click', reset);
+// stopbtn.addEventListener('click', stop);
+
+// this is where the eventListeners are added, allowing buttons to have a funciton
 let counter = 0;
 let counts = '';
 let ready = true;
+let interval;
+
+// these are global variables to be accessed in multiple functions
 
 
 function timer() {
@@ -20,13 +30,16 @@ function timer() {
         startbtn.classList.add('disabled');
         resetbtn.disabled = true;
         startbtn.disabled = true;
-        let interval = window.setInterval(myFunc, 10);
+        interval = window.setInterval(myFunc, 10);
+        // ready = false;
+
+        //prevents button interference while timer is running
 
         function myFunc() {
-            counter += 10;
+            counter++;
             let counts = counter.toString();
 
-            while (counts.length < 5) {
+            while (counts.length < 4) {
                 counts = '0' + counts;
             }
 
@@ -34,7 +47,7 @@ function timer() {
             msHun.innerHTML = `${counts[2]}`;
             secOne.innerHTML = `${counts[1]}`;
             secTen.innerHTML = `${counts[0]}`;
-            if (counter === 10000) {
+            if (counter === 1000) {
                 window.clearInterval(interval);
                 resetbtn.classList.remove('disabled');
                 resetbtn.disabled = false;
@@ -42,6 +55,9 @@ function timer() {
                     digits[i].style.color = 'red';
                 }
                 ready = false;
+
+                // activates reset button at the end
+
                 // msTen.style.color = 'red';
                 // msHun.style.color = 'red';
                 // colon.style.color = 'red';
@@ -61,8 +77,34 @@ function reset() {
     ready = true;
     startbtn.classList.remove('disabled');
     startbtn.disabled = false;
-        
+
+    // resets appearance of timer
+
     for (let i = 0; i < digits.length; i++) {
         digits[i].style.color = 'black';
     }
+
+    // returns color of timer back to black
 }
+
+// function stop() {
+//     if (!ready) {
+//         window.clearInterval(interval);
+//         resetbtn.classList.remove('disabled');
+//         resetbtn.disabled = false;
+//         for (let i = 0; i < digits.length; i++) {
+//             digits[i].style.color = 'black';
+//         }
+
+//         msTen.innerHTML = `0`;
+//         msHun.innerHTML = `0`;
+//         secOne.innerHTML = `0`;
+//         secTen.innerHTML = `0`;
+//         counter = 0;
+//         ready = true;
+//         startbtn.classList.remove('disabled');
+//         startbtn.disabled = false;         
+//     }
+// }
+
+// for fun added a stop button xD
