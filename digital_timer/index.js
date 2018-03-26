938bef4e8e29c5e02109551c16518226809c3cf5
Tylar Pierson <tylarpiersonbusiness@gmail.com>
2018-03-26 13:05:41 -0700

    added favorite animal

--- digital_timer/index.js
+++ digital_timer/index.js
@@ -1,3 +1,3 @@
 // In order to see changes, run `npx serve digital_timer` from main directory
 
-// In collaboration Axhon Ruiz 🐧, Kaitlyn Flynn👍, Tylar Pierson
+// In collaboration Axhon Ruiz 🐧, Kaitlyn Flynn👍, Tylar Pierson 🐘
