443946b2a1d5604b8ca528be8e3214c71179aadf
totsuya <supertatortots@gmail.com>
2018-03-26 16:32:47 -0400

    added variables

--- digital_timer/index.js
+++ digital_timer/index.js
@@ -1,5 +1,9 @@
 // For the timer, the milisecond section of the timer should count to 1000ms
 // Every time it reaches 1000ms, it should increase the second timer by second by one
 // When the seconds section hit 10 seconds, the timer should turn red and stop
-
-const msTens = document.getElementByID('msTens');
+// Variables
+const secondTens = document.getElementById('secondTens');
+const secondOnes = document.getElementById('secondOnes');
+const colon = document.getElementById('colon');
+const msHundreds = document.getElementById('msHundreds');
+const msTens = document.getElementById('msTens');
