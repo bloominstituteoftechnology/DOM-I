03da65cc705eb6af0903cd53b99ccaeee32f813a
Axhon <axhonruiz@gmail.com>
2018-03-26 10:21:39 -1000

    added button and styled

--- digital_timer/index.js
+++ digital_timer/index.js
@@ -1,3 +1,8 @@
 // In order to see changes, run `npx serve digital_timer` from main directory
 
 // In collaboration Axhon Ruiz 🐧, Kaitlyn Flynn👍, Tylar Pierson 🐘
+
+const secondTens = Document.getElementById('secondTens')
+const secondOnes = Document.getElementById('secondOnes')
+const msHundreds = Document.getElementById('msHundreds')
+const msTens = Document.getElementById('msTens')
