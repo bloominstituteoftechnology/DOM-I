d36060697f97429d0a5e8c56f5ac03dbc18f6d4e
Devon Bacon <devon.bacon@gmail.com>
2018-03-26 14:43:50 -0700

    Refactor phase 2

--- digital_timer/index.js
+++ digital_timer/index.js
@@ -1,11 +1,4 @@
-// Set references for DOM elements
-let secondOnes = document.querySelector("#secondOnes");
-let secondTens = document.querySelector("#secondTens");
-let msHundreds = document.querySelector("#msHundreds");
-let msTens = document.querySelector("#msTens");
-let digits = document.querySelector(".digits");
-
-// Initialize all variables to 0
+// Initialize all variables to 0 by referencing DOM element IDs
 secondOnes.innerHTML = 0;
 secondTens.innerHTML = 0;
 msHundreds.innerHTML = 0;
@@ -13,24 +6,25 @@ msTens.innerHTML = 0;
 
 tenSecondTimer = () => {
   msTens.innerHTML++;
-  // Once 90 milliseconds have passed
-  if (msTens.innerHTML === '9') {
+  // Once 100 milliseconds have passed
+  if (msTens.innerHTML === '10') {
     // Reset msTens value and increment msHundreds
     msTens.innerHTML = 0;
     msHundreds.innerHTML++;
   } 
   // Once 1000 milliseconds have passed
-  if (msHundreds.innerHTML === '9') {
+  if (msHundreds.innerHTML === '10') {
     // Reset msHundreds value and increment secondOnes
     msHundreds.innerHTML = 0;
     secondOnes.innerHTML++;
   }
   // Once 10 seconds have passed
-  if (secondOnes.innerHTML === '9') {
+  if (secondOnes.innerHTML === '10') {
     secondOnes.innerHTML = 0;
     secondTens.innerHTML++;
   }
   if (secondTens.innerHTML === '1') {
+    // Stop timer
     clearInterval(timer);
     // Turn digits color red
     document.querySelector(".digits").className += " redDigit";
