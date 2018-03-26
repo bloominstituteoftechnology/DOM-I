95fadf07984820e2fd3346b591f9009927831648
Jared Cuffe <jcuffe@gmail.com>
2018-03-26 12:30:55 -0700

    Added documentation

--- digital_timer/index.js
+++ digital_timer/index.js
@@ -1,48 +1,52 @@
 class Timer {
   constructor() {
+    // Initialize at 00:00 and not running
     this.count = 0;
     this.running = false;
 
+    // Gather the control elements into properties
     this.startButton = document.querySelector('button#start');
     this.stopButton = document.querySelector('button#stop');
     this.resetButton = document.querySelector('button#reset');
 
+    // Attach click event handlers to each button
     this.startButton.addEventListener('click', (event) => timer.start());
     this.stopButton.addEventListener('click', (event) => timer.stop());
     this.resetButton.addEventListener('click', (event) => timer.reset());
   }
-  init() {
+  init() { // Add the tick() function to the window object on a 10ms interval
     window.setInterval(this.tick.bind(this), 10);
   }
-  start() {
+  start() { // Start the timer and disable the start button
     this.running = true;
     this.startButton.setAttribute('disabled', 'disabled');
   }
-  stop() {
+  stop() { // Stop the timer and enable the start button
     this.running = false;
     this.startButton.removeAttribute('disabled');
   }
-  tick() {
-    if (this.count < 1000 && this.running) {
+  tick() { // Updates current time
+    if (this.count < 1000 && this.running) { // Only update count while running and not at max
       this.count += 1;
     }
     this.update();
   }
-  update() {
-    let timerString = this.count.toString().padStart(4, '0');
-    const digits = Array.from(document.querySelectorAll('.digit:not(#colon)'));
-    digits.forEach((element, index) => element.innerHTML = timerString[index]);
+  update() { // Controls InnerHTML of digits and their display classes
+    let timerString = this.count.toString().padStart(4, '0'); // Pad the count if it is not yet 1000
+    const digits = Array.from(document.querySelectorAll('.digit:not(#colon)')); // Grab all the non-colon digits
+    digits.forEach((element, index) => element.innerHTML = timerString[index]); // Assign each digit of the count sequentially
     if (timerString == '1000') {
       console.log('done');
-      digits.push(document.querySelector('#colon'));
-      digits.forEach((element) => element.classList.add('redDigit'));
+      digits.push(document.querySelector('#colon')); // Add colon back to the collection
+      digits.forEach((element) => element.classList.add('redDigit')); // Turn them all red
     }
   }
-  reset() {
+  reset() { // Start the count back at 0
     this.count = 0;
     this.update();
   }
 }
 
+// Make it all happen
 let timer = new Timer();
 timer.init();
