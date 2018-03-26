569d207c2a85504158684dac495097f96872cb34
Axhon <axhonruiz@gmail.com>
2018-03-26 11:00:53 -1000

    created counter

--- digital_timer/index.js
+++ digital_timer/index.js
@@ -13,15 +13,39 @@ const blue = event => {
     : handlesCountdownstart()
 }
 
-const handleError = err => console.log('oops', err.message)
+const handleError = err => console.error('oops', err)
 
 const handlesCountdownstart = () => {
-  setInterval(countdownTick(), 10)
+  return setInterval(countdownTick(), 10)
 }
+
+const handleCountdownStop = () => {}
+// create a counter
+// 0000
+// 0010 = 10 ms
+let c = 0
+class Counter {
+  constructor(c = 0) {
+    this.c = c
+  }
+  checkIfDone() {
+    return this.c > 9999 ? handleCountdownStop() : false
+  }
+  increment() {
+    this.checkIfDone()
+    this.c += 10
+  }
+}
+const countdownCounter = new Counter()
+
+const render = num => console.log(num)
+
 function countdownTick() {
   return () => {
     // update our counter
+    countdownCounter.increment()
     // handle the trash
     // render our changes
+    render(countdownCounter.c)
   }
 }
