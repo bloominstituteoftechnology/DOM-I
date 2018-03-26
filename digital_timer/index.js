4761a3f8c1d7cb9b2222a3c3dd32e5599a2ecca9
Natalie Schennum <natalieschennum@gmail.com>
2018-03-26 14:04:12 -0700

    Index.js

---
+++ digital_timer/index.js
@@ -0,0 +1,24 @@
+
+let count1 = 0; 
+ 
+let sT1 = document.querySelector("#msHundreds");
+let sT2 = document.querySelector("#msTens");
+
+function timer() {
+    sT1 = window.setInterval(ms10, 10);
+    sT2 = window.setInterval(tenSec, 10000);
+    
+    const msTensTimer = setInterval(() => {
+        for (count1 <= 10000) ( {
+        msTens+=ms10, 
+        msTens.innerHTML = tens.toString(),
+    }, 10000);
+    },
+
+    if (count1 >= st2) {
+        digits.style.redDigit; 
+        clearInterval(tens); 
+        clearInterval(milis); 
+
+    }
+};
