6cc8e0a47549069aa1df7d7dbe51dbd5f364c0ac
JulianAlexander <julian.pineyro@gmail.com>
2018-03-27 01:30:33 +0300

    Added timer

--- digital_timer/index.js
+++ digital_timer/index.js
@@ -16,3 +16,23 @@ secondOnes.innerHTML = '0';
 msHundreds.innerHTML = '0';
 msTens.innerHTML = '0';
 
+function timer() {
+    if (msTens.innerHTML < 9 ) {
+        ++msTens.innerHTML;
+    }else{
+        msTens.innerHTML = 0;
+        if(msHundreds.innerHTML < 9 ) {
+            ++msHundreds.innerHTML;
+        }else{
+            msHundreds.innerHTML = 0;
+            if(secondOnes.innerHTML < 9 ) {
+                ++secondOnes.innerHTML;
+            }else{
+                secondOnes.innerHTML = 0;
+                ++secondTens.innerHTML;
+                clearInterval(counter);
+            }
+        }
+    }
+}
+
