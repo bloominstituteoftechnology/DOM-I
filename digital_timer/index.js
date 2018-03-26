4bc12874f98bb6429fe6327e310fe39cee685ad2
jaivonmassena <jaivonmassena@gmail.com>
2018-03-26 16:24:14 -0400

    trying to get js working

---
+++ digital_timer/index.js
@@ -0,0 +1,17 @@
+const counter = 0;
+const digits = [];
+// const time = window.setInterval();
+// if counter = 10000{ color red};
+// 10ms = digit++
+//
+// array = all digits;
+//
+// loop through array change every digit 10ms;
+digits = document.querySelectorAll('.digits')
+digits = digits.childNodes;
+let test = 'hi';
+window.setInterval(function(){ alert("Hello"); }, 3000);
+window.alert(digits);
+ for(;counter < 10000; counter++){
+
+ }
