78c121127cbb38ef5ab3c90beeceb434f6341663
jeffrey <jeffreyneilflynn@gmail.com>
2018-03-26 19:53:12 -0400

    finished challenge

--- digital_timer/index.js
+++ digital_timer/index.js
@@ -1,8 +1,9 @@
-// const secondsTen = document.getElementById('secondTens');
-// const secondsOne = document.getElementById('secondOnes');
-// const msHundos   = document.getElementById('msHundreds');
-// const msTens     = document.getElementById('msTens');
+const secondsTen = document.getElementById('secondTens');
+const secondsOne = document.getElementById('secondOnes');
+const msHundos   = document.getElementById('msHundreds');
+const msTens     = document.getElementById('msTens');
 /*************************************************************************/
+const digits = document.getElementsByClassName('digits')[0];
 let countSeconds = 0,
 	count10seconds = 0,
 	ms10s = 0,
@@ -10,36 +11,28 @@ let countSeconds = 0,
 
 let interval = setInterval(function() {
 	ms10s++;
-	document.getElementById('msTens').innerHTML = ms10s;
+	msTens.innerHTML = ms10s;
 
 	if (ms10s === 10) {
 		ms10s = 0;
 		ms100s++;
-		document.getElementById('msTens').innerHTML = ms10s;
-		document.getElementById('msHundreds').innerHTML = ms100s;
+		msTens.innerHTML = ms10s;
+		msHundos.innerHTML = ms100s;
 
 		if (ms100s === 10) {
 			ms100s = 0;
 			countSeconds++;
-			document.getElementById('msHundreds').innerHTML = ms100s;
-			document.getElementById('secondOnes').innerHTML = countSeconds;
+			msHundos.innerHTML = ms100s;
+			secondsOne.innerHTML = countSeconds;
 
 			if (countSeconds === 10) {
 				clearInterval(interval);
 				countSeconds = 0;
 				count10seconds++;
-				document.getElementById('secondOnes').innerHTML = countSeconds;
-				document.getElementById('secondTens').innerHTML = count10seconds;
-				red();
+				secondsOne.innerHTML = countSeconds;
+				secondsTen.innerHTML = count10seconds;
+				digits.setAttribute('class', 'redDigit');
 			}
 		}
 	}
 }, 10);
-
-function red() {
-	document.getElementById('secondTens').style.color = 'red';
-	document.getElementById('secondOnes').style.color = 'red';
-	document.getElementById('colon').style.color = 'red';
-	document.getElementById('msHundreds').style.color = 'red';
-	document.getElementById('msTens').style.color = 'red';
-}
