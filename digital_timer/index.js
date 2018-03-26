edf29bfcf66587d24ed54878452174aa63fb0754
Justin M <jekm321@gmail.com>
2018-03-26 16:08:40 -0400

    starting on index.js

---
+++ digital_timer/index.js
@@ -0,0 +1,9 @@
+$(document).ready(() => {
+    const digits = document.getElementsByClassName('digit');
+    const secTen = document.getElementById('secondTens');
+    const secOne = document.getElementById('secondOnes');
+    const msHun = document.getElementById('msHundreds');
+    const msTen = document.getElementById('msTens');
+
+    
+})
