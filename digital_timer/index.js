0d491e40b6dbafca2a79a75657403735afee03f0
Justin M <jekm321@gmail.com>
2018-03-26 17:12:26 -0400

    added button, allowed reusability by resetting  counter and reverting color upon click, disabled during timer re-enabled after

--- digital_timer/index.js
+++ digital_timer/index.js
@@ -4,34 +4,51 @@ const secOne = document.getElementById('secondOnes');
 const colon = document.getElementById('colon');
 const msHun = document.getElementById('msHundreds');
 const msTen = document.getElementById('msTens');
+const myButton = document.getElementById('button');
+
+myButton.addEventListener('click', timer);
+
 let counter = 0;
 let counts = '';
 
 
-let interval = window.setInterval(myFunc, 10);
-
-function myFunc() {
-    counter += 10;
-    let counts = counter.toString();
-
-    while (counts.length < 5) {
-        counts = '0' + counts;
+function timer() {
+    msTen.innerHTML = `-`;
+    msHun.innerHTML = `-`;
+    secOne.innerHTML = `-`;
+    secTen.innerHTML = `-`;
+    counter = 0;
+    for (let i = 0; i < digits.length; i++) {
+        digits[i].style.color = 'black';
     }
 
-    msTen.innerHTML = `${counts[3]}`;
-    msHun.innerHTML = `${counts[2]}`;
-    secOne.innerHTML = `${counts[1]}`;
-    secTen.innerHTML = `${counts[0]}`;
-    if (counter === 10000) {
-        window.clearInterval(interval);
-        for (let i = 0; i < digits.length; i++) {
-            digits[i].style.color = 'red';
+    myButton.disabled = true;
+    let interval = window.setInterval(myFunc, 10);
+
+    function myFunc() {
+        counter += 10;
+        let counts = counter.toString();
+
+        while (counts.length < 5) {
+            counts = '0' + counts;
         }
-        // msTen.style.color = 'red';
-        // msHun.style.color = 'red';
-        // colon.style.color = 'red';
-        // secOne.style.color = 'red';
-        // secTen.style.color = 'red';
+
+        msTen.innerHTML = `${counts[3]}`;
+        msHun.innerHTML = `${counts[2]}`;
+        secOne.innerHTML = `${counts[1]}`;
+        secTen.innerHTML = `${counts[0]}`;
+        if (counter === 10000) {
+            window.clearInterval(interval);
+            myButton.disabled = false;
+            for (let i = 0; i < digits.length; i++) {
+                digits[i].style.color = 'red';
+            }
+
+            // msTen.style.color = 'red';
+            // msHun.style.color = 'red';
+            // colon.style.color = 'red';
+            // secOne.style.color = 'red';
+            // secTen.style.color = 'red';
+        }
     }
 }
-
