bfc2e8fcc0211e60679176b8b21ef4d3a040a32d
cgoez <goez2001@gmail.com>
2018-03-26 13:31:52 -0700

    added variables

---
+++ digital_timer/index.js
@@ -0,0 +1,24 @@
+// variables
+let msTens = document.getElementById('msTens');
+let msHuns = document.getElementById('msHundreds');
+let secOnes = document.getElementById('secondOnes');
+let secTens = docment.getElementById('secondTens');
+
+// set times to 0
+msTens.innerHTML = 0;
+msHuns.innerHTML = 0;
+secOnes.innerHTML = 0;
+secTens.innerHTML = 0;
+
+// timer function
+function timer() {
+    if (msTens.innerHTML < 9) {
+        msTens.innerHTML++;
+    } else {
+
+        if (msHuns.innerHTML < 9)
+    }
+}
+
+
+ 
