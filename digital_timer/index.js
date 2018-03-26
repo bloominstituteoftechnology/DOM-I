80bb4e2163fc9a359fa0d5cc5051caf68966c22b
Shawn Stewart <s.stewart0220@gmail.com>
2018-03-26 17:51:18 -0500

    Completion of stretch goals.

--- digital_timer/index.js
+++ digital_timer/index.js
@@ -8,6 +8,8 @@ function startTimer() {
   document.getElementById('secondOnes').innerHTML = 0;
   document.getElementById('msHundreds').innerHTML = 0;
   document.getElementById('msTens').innerHTML = 0;
+  myStartButton.style.background = 'lightgray';
+  myStartButton.disabled = true;
 
   function incrementTimer() {    
     ms += 10;
@@ -18,19 +20,40 @@ function startTimer() {
     document.getElementById('msTens').innerHTML = parseInt((ms / 10) % 10)
 
     if (ms === LIMIT) {
-      stopTimer();
+      clearInterval(msTimer);
       document.querySelector('.digits').style = 'color: red;'
+      myResetButton.style.backgroundColor = 'wheat';
+      myResetButton.disabled = false;
     }
-  }
-
-  function stopTimer() {
-    clearInterval(msTimer);
   }
 
   let msTimer = setInterval(incrementTimer, 10);
 }
 
+function resetTimer() {
+  ms = 0;
+  document.querySelector('.digits').style = 'color: black;'
+  document.getElementById('secondTens').innerHTML = 0;
+  document.getElementById('secondOnes').innerHTML = 0;
+  document.getElementById('msHundreds').innerHTML = 0;
+  document.getElementById('msTens').innerHTML = 0;
+  myStartButton.style.backgroundColor = 'wheat';
+  myStartButton.disabled = false;
+  myResetButton.style.backgroundColor = 'lightgray'
+  myResetButton.disabled = true;
+}
+
+document.body.style.flexDirection = 'column';
+
 const myStartButton = document.createElement('button');
 myStartButton.innerHTML = 'Start';
 myStartButton.onclick = startTimer;
+myStartButton.style = 'margin: 10px; width: 100px; height: 30px; background-color: wheat; border: 2px solid black; border-radius: 25px;'
 document.body.appendChild(myStartButton);
+
+const myResetButton = document.createElement('button');
+myResetButton.innerHTML = 'Reset';
+myResetButton.disabled = true;
+myResetButton.onclick = resetTimer;
+myResetButton.style = 'margin: 10px; width: 100px; height: 30px; background-color: lightgray; border: 2px solid black; border-radius: 25px;'
+document.body.appendChild(myResetButton);
