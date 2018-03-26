c2ae32b33af8053845677dc12d0d9c1ecd6f57e7
Robert Hogan <roberthogan@Roberts-MacBook-Pro.local>
2018-03-26 18:01:47 -0400

    Robert Hogan, Paul Fang. Additional Commit. Additional work on incrememting time.

--- digital_timer/index.js
+++ digital_timer/index.js
@@ -3,12 +3,40 @@ const secondOnes = document.getElementById('secondOnes');
 const msHundreds = document.getElementById('msHundreds');
 const msTens = document.getElementById('msTens');
 
-function timerCall() {
-    document.getElementById('secondTens').innerHTML = "0";
-    document.getElementById('secondOnes').innerHTML = "0";
-    document.getElementById('msHundreds').innerHTML = "0";
-    document.getElementById('msTens').innerHTML = "0";
 
-    
-};
-timerCall();
+    document.getElementById('secondOnes').innerHTML = 0;
+    document.getElementById('secondTens').innerHTML = 0;
+    document.getElementById('msHundreds').innerHTML = 0;
+    document.getElementById('msTens').innerHTML = 0;
+
+let timer = setInterval(function() {
+    if (msTens.innerHTML < 9) {
+        msTens.innerHTML++;
+    } else {
+        msTens.innerHTML === 0;
+    }
+        if (msHundreds.innerHTML < 9) {
+            msHundreds.innerHTML++;
+        } else {
+            msHundreds.innerHTML === 0;
+        }
+        if (secondTens.innerHTML < 9) {
+            secondTens.innerHTML++;
+        } else {
+            secondTens.innerHTML === 0;
+        }
+            if (secondOnes.innerHTML < 9) {
+                secondOnes.innerHTML++;
+            } else {
+                secondOnes.innerHTML === 0;
+            }
+
+                clearInterval();
+})
+
+
+
+
+//10ms = 1/10 second
+//1,000ms = 1 second
+// 10,000ms = 10 seconds
