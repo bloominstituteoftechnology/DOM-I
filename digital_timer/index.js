9b1d6da1d5cde48baf990dc3ec25754dae8504ba
totsuya <supertatortots@gmail.com>
2018-03-26 16:13:10 -0400

    updated comments

--- digital_timer/index.js
+++ digital_timer/index.js
@@ -1,5 +1,5 @@
-// For the timer, the milisecond section of the timer should count to 10ms
-// Every time it reaches 10ms, it should increase the second timer by second by one
+// For the timer, the milisecond section of the timer should count to 1000ms
+// Every time it reaches 1000ms, it should increase the second timer by second by one
 // When the seconds section hit 10 seconds, the timer should turn red and stop
 
 const msTens = document.getElementByID('msTens');
