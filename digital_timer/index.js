92a0d890d24cd3a8b5f0c04f99385a7d36760c73
jeffrey <jeffreyneilflynn@gmail.com>
2018-03-26 19:16:14 -0400

    turns red at 10.00

--- digital_timer/index.js
+++ digital_timer/index.js
@@ -4,11 +4,11 @@
 // const msTens     = document.getElementById('msTens');
 /*************************************************************************/
 let countSeconds = 0,
-	countMinutes = 0,
+	count10seconds = 0,
 	ms10s = 0,
 	ms100s= 0;
 
-let msOutput = setInterval(function() {
+let interval = setInterval(function() {
 	ms10s++;
 	document.getElementById('msTens').innerHTML = ms10s;
 
@@ -25,15 +25,21 @@ let msOutput = setInterval(function() {
 			document.getElementById('secondOnes').innerHTML = countSeconds;
 
 			if (countSeconds === 10) {
-				clearInterval(msOutput);
+				clearInterval(interval);
 				countSeconds = 0;
-				countMinutes++;
+				count10seconds++;
 				document.getElementById('secondOnes').innerHTML = countSeconds;
-				document.getElementById('secondTens').innerHTML = countMinutes;
-				//document.querySelector('div').style = {'color:red'};
+				document.getElementById('secondTens').innerHTML = count10seconds;
+				red();
 			}
 		}
 	}
 }, 10);
 
-/*************************************************************************/
+function red() {
+	document.getElementById('secondTens').style.color = 'red';
+	document.getElementById('secondOnes').style.color = 'red';
+	document.getElementById('colon').style.color = 'red';
+	document.getElementById('msHundreds').style.color = 'red';
+	document.getElementById('msTens').style.color = 'red';
+}
