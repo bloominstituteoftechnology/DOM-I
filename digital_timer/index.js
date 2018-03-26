3d40ff2c7c2d5947139ff1ed9c05ba0daeffc0d1
Jonathan Greene <greeneselectronics@gmail.com>
2018-03-26 17:51:14 -0600

    Added in stopping the counter at 0 and re running it' git push

--- digital_timer/index.js
+++ digital_timer/index.js
@@ -34,12 +34,21 @@
 
 
 function timeUp() {  
+    
     //let count = document.getElementById('secondOnes');
     count = parseInt(count, 10);
-    if (count <10) {
+    if (count < 9) {
+        
         count++;
+        document.getElementById("secondOnes").innerHTML = count;
     }
-    document.getElementById("secondOnes").innerHTML = count;
+    else 
+    {
+        document.getElementById('secondOnes').innerHTML = "0";
+        count = 0;
+        
+    }
+    
 };
 
 //console.log(count);
