b7544e4f6c440420b3118308b0843484b46d039e
K Haag <kh@Ks-MacBook-Pro.local>
2018-03-26 13:11:48 -0700

    DOM-I Push2

--- digital_timer/index.js
+++ digital_timer/index.js
@@ -1,11 +1,41 @@
-function updateTime() {
-    var time = 0;
-    setInterval(function() {
-        document.querySelector(".digits").value = time;
-        time++;
-        if (time > 10) {
-            time = 1;
+// function updateTime() {
+//     var time = 0;
+//     setInterval(function() {
+//         document.querySelector(".digits").value = time;
+//         time++;
+//         if (time > 10) {
+//             time = 1;
+//         }
+//     }, 10);
+// }
+// updateTime();
+
+
+// let num = window.setInterval( function() {
+//     console.log('Look at this');
+// }, 1000);
+
+
+var timer = {
+    currentTime: 1,
+    startTime: 1,
+    maxTime: 12,
+    interval: 200,
+    target: ".digits",
+    updateTime: function() {
+        document.querySelectorAll(this.target).value = this.currentTime;
+        this.currentTime++;
+        if (this.currentTime > this.maxTime) {
+            this.currentTime = this.startTime;
         }
-    }, 200);
-}
-updateTime();
+    },
+    start: function() {
+        this.currentInterval = setInterval(this.updateTime.bind(this), this.interval);
+    },
+    stop: function() {
+        clearInterval(this.currentInterval);
+    }
+};
+
+timer.start();
+timer.stop();
