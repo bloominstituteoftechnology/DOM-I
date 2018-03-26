61c2c71507db42849636c4e21d6b6991051b659a
Matthias Bakken <matthias.bakken@gmail.com>
2018-03-26 15:36:01 -0700

    count works. ms count counts backwards with negative symbol and we are attempting to debug

--- digital_timer/index.js
+++ digital_timer/index.js
@@ -2,7 +2,7 @@ let counter = 0;
 let msHun = 0;
 let msTen = 0;
 let second = 0;
-
+let subNum = 0;
 
 let secondTens = document.querySelector('#secondTens');
 let secondOnes = document.querySelector('#secondOnes');
@@ -10,39 +10,22 @@ let msHundreds = document.querySelector('#msHundreds');
 let msTens = document.querySelector('#msTens');
 
 let timer = window.setInterval(() => {
-    // keeps track of time in ms
-    // if (counter % 1000 === 0) {
-    //     document.getElementById('secondsOnes').innerHTML = counter;
-    //     counter++;
-    // } else if (counter % 100 === 0) {
-    //     document.getElementById('msTens').innerHTML = counter;
-    //     counter++;
-    // } else if (counter % 10 === 0) {
-    //     document.getElementById('msHundreds').innerHTML = counter;
-    //     counter++;
-    // } else {
-    //     clearInterval(timer);
-    // }
 
-    if (counter !== 1001) {
+    if (counter !== 1100) {
+        //document.getElementById('msTens').innerHTML = counter - subNum;
+        //document.getElementById('msHundreds').style.display = 'none';
         if (counter % 100 === 0) {
             document.getElementById('secondOnes').innerHTML = second;
-            second++
-            if (second === 10) {
+            second++;
+            counter++;
+            //subNum += 100;
+            if (second === 11) {
+                //document.getElementById('msTens').innerHTML = 00;
                 document.querySelector('#secondTens').style.display = 'none';
                 document.querySelector('.digits').style.color = 'red';
                 return second;
             }
-        } else if (counter % 10 === 0) {
-            document.getElementById('msTens').innerHTML = msTen;
-            msTen++
-            if (msTen === 10) {
-                msTen = 1;
-            }
         }
-        // document.getElementById('msHundreds').innerHTML = counter;
-        // document.getElementById('msTens').innerHTML = counter;
-        // document.getElementById('secondOnes').innerHTML = counter;
         counter++;
     }
 }, 10);
