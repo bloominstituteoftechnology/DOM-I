f0eca9ab7c3a751c71ecabea7dbf909c36faade2
Cruise Brantley <cruise.brantley@gmail.com>
2018-03-26 16:46:03 -0500

    Not functioning yet, logic in a couple of ways here

--- digital_timer/index.js
+++ digital_timer/index.js
@@ -1,9 +1,58 @@
-const hundredths = document.getElementsByClassName('msHundreds');
-const tenths = document.getElementsByClassName('msTens');
-const ones = document.getElementsByClassName('secondOnes');
-const hundreds = document.getElementsByClassName('secondTens');
+let hundredths = document.getElementById('mstens');
+let tenths = document.getElementById('msTens');
+let ones = document.getElementById('secondOnes');
+let tens = document.getElementById('secondTens');
 
-int1 = setInterval(() => {if(hundredths.innerHTML < 10) hundredths.innerHTML += 1; hundredths.innerHTML = 0}, 10);
-int2 = setInterval(() => {if(tenths.innerHTML < 10) tenths.innerHTML += 1; tenths.innerHTML = 0}, 100);
-int3 = setInterval(() => {if(ones.innerHTML < 10) ones.innerHTML += 1; ones.innerHTML = 0}, 1000);
-int4 = setInterval(() => {if(hundreds.innerHTML < 10) hundreds.innerHTML += 1; hundreds.innerHTML = 0}, 10000);
+let hundredthsCount = 0;
+let tenthsCount = 0;
+let onesCount = 0;
+let tensCount = 0;
+
+int = setInterval(() => {
+    if (tenthsCount <= 9) {
+        tenthsCount.innerHTML = tenthsCount++;
+    } else if (hundredthsCount <= 9) {
+        tenthsCount = 0;
+        hundredthsCount.innerHTML = hundredthsCount++;
+    } else if (onesCount <= 9) {
+        hundredthsCount = 0;
+        onesCount.innerHTML = onesCount++;
+    } else {
+        onesCount = 0;
+        tensCount.innerHTML = tensCount++;
+    }
+    if (tensCount === 1) {
+        clearInterval(int);
+        hundredths.innerHTML = '0';
+        tenths.innerHTML = '0';
+        ones.innerHTML = '0';
+        tens.innerHTML = '1';
+    }
+}
+}, 10);
+
+// int1 = setInterval(() => {
+//     if(hundredths.innerHTML < 10)
+//         hundredths.innerHTML++;
+//     hundredths.innerHTML = '0';
+// }, 10);
+
+// int2 = setInterval(() => {
+//     if(tenths.innerHTML < 10)
+//         tenths.innerHTML++;
+//     tenths.innerHTML = '0';
+// }, 100);
+
+// int3 = setInterval(() => {
+//     if(ones.innerHTML < 10)
+//         ones.innerHTML++;
+//     ones.innerHTML = '0';
+// }, 1000);
+
+// int4 = setInterval(() => {
+//     if(tens.innerHTML < 1)
+//         tens.innerHTML++;
+//     tens.innerHTML = 1;
+// }, 10000);
+
+// if(tens)
