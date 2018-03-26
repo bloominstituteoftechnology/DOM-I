87139a2a4f1c356b594e5d2b6f310e5385c6cb11
nickbrenn <nbren@tutanota.com>
2018-03-26 18:26:38 -0400

    Made the start button not work while it is active (using a weird way)

--- digital_timer/index.js
+++ digital_timer/index.js
@@ -1,5 +1,6 @@
 let ms = 0;
 const msLimit = 10000;
+let buttonEnabled = false;
 
 document.getElementById('secondTens').innerHTML = 0;
 document.getElementById('secondOnes').innerHTML = 0;
@@ -7,40 +8,46 @@ document.getElementById('msHundreds').innerHTML = 0;
 document.getElementById('msTens').innerHTML = 0;
 
 function resetTimer() {
-  let ms = 0;
-  document.getElementById('secondTens').innerHTML = 0;
-  document.getElementById('secondOnes').innerHTML = 0;
-  document.getElementById('msHundreds').innerHTML = 0;
-  document.getElementById('msTens').innerHTML = 0;
-  document.querySelector('.digits').style = 'color: black;'
+  if (buttonEnabled === false) {
+    let ms = 0;
+    document.getElementById('secondTens').innerHTML = 0;
+    document.getElementById('secondOnes').innerHTML = 0;
+    document.getElementById('msHundreds').innerHTML = 0;
+    document.getElementById('msTens').innerHTML = 0;
+    document.querySelector('.digits').style = 'color: black;';
+  }
 }
 
 function startTimer() {
-  document.getElementById('secondTens').innerHTML = 0;
-  document.getElementById('secondOnes').innerHTML = 0;
-  document.getElementById('msHundreds').innerHTML = 0;
-  document.getElementById('msTens').innerHTML = 0;
+  if (buttonEnabled === false) {
+    buttonEnabled = true;
+    document.getElementById('secondTens').innerHTML = 0;
+    document.getElementById('secondOnes').innerHTML = 0;
+    document.getElementById('msHundreds').innerHTML = 0;
+    document.getElementById('msTens').innerHTML = 0;
 
-  function incrementTimer() {
-    ms += 10;
+    function incrementTimer() {
+      ms += 10;
 
-    document.getElementById('secondTens').innerHTML = parseInt(ms / 10000) % 10;
-    document.getElementById('secondOnes').innerHTML = parseInt(ms / 1000) % 10;
-    document.getElementById('msHundreds').innerHTML = parseInt(ms / 100) % 10;
-    document.getElementById('msTens').innerHTML = parseInt(ms / 10) % 10;
+      document.getElementById('secondTens').innerHTML = parseInt(ms / 10000) % 10;
+      document.getElementById('secondOnes').innerHTML = parseInt(ms / 1000) % 10;
+      document.getElementById('msHundreds').innerHTML = parseInt(ms / 100) % 10;
+      document.getElementById('msTens').innerHTML = parseInt(ms / 10) % 10;
 
-    if (ms === msLimit) {
-      stopTimer();
-      document.querySelector('.digits').style = 'color: red;'
+      if (ms === msLimit) {
+        stopTimer();
+        document.querySelector('.digits').style = 'color: red;';
+      }
     }
-  }
 
-  function stopTimer() {
-    ms = 0;
-    clearInterval(msTimer);
-  }
+    function stopTimer() {
+      ms = 0;
+      buttonEnabled = false;
+      clearInterval(msTimer);
+    }
 
-  let msTimer = setInterval(incrementTimer, 10);
+    let msTimer = setInterval(incrementTimer, 10);
+  }
 }
 
 const startButton = document.createElement('button');
