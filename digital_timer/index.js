fdcdb99702a9ccf1bfba096accecfeaf22ada389
Robert Hogan <roberthogan@Roberts-MacBook-Pro.local>
2018-03-26 16:22:36 -0400

    Robert Hogan, Paul Fang, secondary commit. Variables set, innerHTML.

---
+++ digital_timer/index.js
@@ -0,0 +1,11 @@
+let timeSecTen = document.getElementById('secondTens');
+let timeSecOne = document.getElementById('secondOnes');
+let timeSecMsHun = document.getElementById('msHundreds');
+let timeSecMsTen = document.getElementById('msTens');
+
+ timeSecTen = 0;
+ timeSecOne = 0; 
+ timeSecMsHun = 0; 
+ timeSecMsTen = 0; 
+
+ timeSecTen.innerHTML = "0";
