a61fc4c888fed19b724a1ef9203a32a8f554eb1b
Justin M <jekm321@gmail.com>
2018-03-26 16:48:46 -0400

    got the timer to count to 10 seconds and stop at 10 seconds

--- digital_timer/index.js
+++ digital_timer/index.js
@@ -1,9 +1,29 @@
-$(document).ready(() => {
-    const digits = document.getElementsByClassName('digit');
-    const secTen = document.getElementById('secondTens');
-    const secOne = document.getElementById('secondOnes');
-    const msHun = document.getElementById('msHundreds');
-    const msTen = document.getElementById('msTens');
+const digits = document.getElementsByClassName('digit');
+const secTen = document.getElementById('secondTens');
+const secOne = document.getElementById('secondOnes');
+const msHun = document.getElementById('msHundreds');
+const msTen = document.getElementById('msTens');
+let counter = 0;
+let counts = '';
 
-    
-})
+
+let interval = window.setInterval(myFunc, 10);
+
+function myFunc() {
+    counter += 10;
+    let counts = counter.toString();
+
+    while (counts.length < 5) {
+        counts = '0' + counts;
+    }
+
+    msTen.innerHTML = `${counts[3]}`;
+    msHun.innerHTML = `${counts[2]}`;
+    secOne.innerHTML = `${counts[1]}`;
+    secTen.innerHTML = `${counts[0]}`;
+    if (counter === 10000) {
+        window.clearInterval(interval);
+        digits.style.color = 'red';
+    }
+}
+
