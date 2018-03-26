9d10cece6baa4054547661ea72d601240f318196
tanking <tanking@github.com>
2018-03-26 18:20:50 -0400

    Set timer to 0

--- digital_timer/index.js
+++ digital_timer/index.js
@@ -1,6 +1,7 @@
 // For the timer, the milisecond section of the timer should count to 1000ms
 // Every time it reaches 1000ms, it should increase the second timer by second by one
 // When the seconds section hit 10 seconds, the timer should turn red and stop
+
 // Variables
 const secondTens = document.getElementById('secondTens');
 const secondOnes = document.getElementById('secondOnes');
@@ -8,5 +9,9 @@ const colon = document.getElementById('colon');
 const msHundreds = document.getElementById('msHundreds');
 const msTens = document.getElementById('msTens');
 
+// Sets timer to 0 
+secondTens.innerHTML = '0';
+secondOnes.innerHTML = '0';
+msHundreds.innerHTML = '0';
+msTens.innerHTML = '0';
 
-//Test comment
