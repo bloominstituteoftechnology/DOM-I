34f2682a3e91987e41fccae5d19c3fd4d8b94350
cgoez <goez2001@gmail.com>
2018-03-26 15:53:44 -0700

    working on timer logic, figuring out clearint vs timeout

--- digital_timer/index.js
+++ digital_timer/index.js
@@ -1,24 +1,42 @@
+/*eslint-env browser*/
 // variables
-let msTens = document.getElementById('msTens');
-let msHuns = document.getElementById('msHundreds');
-let secOnes = document.getElementById('secondOnes');
-let secTens = docment.getElementById('secondTens');
+let msTens = document.querySelector('#msTens');
+let msHuns = document.querySelector('#msHundreds');
+let secOnes = document.querySelector('#secondOnes');
+let secTens = document.querySelector('#secondTens');
+
+const digits = document.querySelector(".digits"); // for red at 10s
 
 // set times to 0
-msTens.innerHTML = 0;
-msHuns.innerHTML = 0;
-secOnes.innerHTML = 0;
-secTens.innerHTML = 0;
+msTens.innerHTML = '0';
+msHuns.innerHTML = '0';
+secOnes.innerHTML = '0';
+secTens.innerHTML = '0';
 
 // timer function
-function timer() {
+timer = () => {
     if (msTens.innerHTML < 9) {
         msTens.innerHTML++;
     } else {
-
-        if (msHuns.innerHTML < 9)
+        msTens.innerHTML = 0;
+        if (msHuns.innerHTML < 9) {
+            msHuns.innerHTML++;
+        } else {
+            msHuns.innerHTML = 0;
+            if (secOnes.innerHTML < 9) {
+                secOnes.innerHTML++;
+            } else {
+                secOnes.innerHTML = 0;
+                // clearInterval(count); // stop
+                secTens.innerHTML++;
+                // digits.forEach(element => {
+                //     element.classList.add('redDigit');
+                // })
+            }
+        }
     }
 }
 
-
- 
+set 10ms increments
+let count = setInterval(function(){ timer() }, 10);
+// setTimeout(timer(), 10000);
