0950916b7ff752e96dcf5079b20f6353f4fb3458
Kaitlyn Flynn <kaitlynflynn@gmail.com>
2018-03-26 13:02:10 -0700

    Thumbs Up added

--- digital_timer/index.js
+++ digital_timer/index.js
@@ -1,3 +1,4 @@
 // In order to see changes, run `npx serve digital_timer` from main directory
 
-// In collaboration Axhon Ruiz, Kaitlyn Flynn, Tylar Pierson
+// In collaboration Axhon Ruiz, Kaitlyn Flynn👍, Tylar Pierson
+
