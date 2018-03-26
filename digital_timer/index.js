a93c64882a630dd318bff60c9a4926ed3491d40d
imsdhk <jappleleaf@gmail.com>
2018-03-26 18:31:35 -0400

    finished rest of the logic for timer

--- digital_timer/index.js
+++ digital_timer/index.js
@@ -18,3 +18,33 @@ function countUp() {
         }
     }
 }
+st secondTens = document.querySelector("#secondTens");
+const secondOnes = document.querySelector("#secondOnes");
+const msHundreds = document.querySelector("#msHundreds");
+const msTens = document.querySelector("#msTens");
+
+secondTens.innerHTML = "0";
+secondOnes.innerHTML = "0";
+msHundreds.innerHTML = "0";
+msTens.innerHTML = "0";
+
+function countUp() {
+    if (msTens.innerHTML < 9) {
+        ++msTens.innerHTML;
+    } else {
+        msTens.innerHTML = 0;
+        if (msHundreds.innerHTML < 9) {
+            ++msHundreds.innerHTML;
+        } else {
+            msHundreds.innerHTML = 0;
+            if (secondOnes.innerHTML < 9) {
+                ++secondOnes.innerHTML;
+            } else {
+                secondOnes.innerHTML = 0;
+                clearInterval(count);
+                secondTens.innerHTML = '1';
+                document.querySelector(".digits").classList.add("redDigit");
+            }
+        }
+    }
+}
