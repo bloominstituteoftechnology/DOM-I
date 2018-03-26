3d85c7d8dbfea0a162c909ebb64640b48c8871a2
imsdhk <jappleleaf@gmail.com>
2018-03-26 18:31:04 -0400

    set up countUp function to count

--- digital_timer/index.js
+++ digital_timer/index.js
@@ -1,48 +1,20 @@
-let counter = 0;
-let msHun = 0;
-let msTen = 0;
-let second = 0;
+st secondTens = document.querySelector("#secondTens");
+const secondOnes = document.querySelector("#secondOnes");
+const msHundreds = document.querySelector("#msHundreds");
+const msTens = document.querySelector("#msTens");
 
+secondTens.innerHTML = "0";
+secondOnes.innerHTML = "0";
+msHundreds.innerHTML = "0";
+msTens.innerHTML = "0";
 
-let secondTens = document.querySelector('#secondTens');
-let secondOnes = document.querySelector('#secondOnes');
-let msHundreds = document.querySelector('#msHundreds');
-let msTens = document.querySelector('#msTens');
-
-let timer = window.setInterval(() => {
-    // keeps track of time in ms
-    // if (counter % 1000 === 0) {
-    //     document.getElementById('secondsOnes').innerHTML = counter;
-    //     counter++;
-    // } else if (counter % 100 === 0) {
-    //     document.getElementById('msTens').innerHTML = counter;
-    //     counter++;
-    // } else if (counter % 10 === 0) {
-    //     document.getElementById('msHundreds').innerHTML = counter;
-    //     counter++;
-    // } else {
-    //     clearInterval(timer);
-    // }
-
-    if (counter !== 1001) {
-        if (counter % 100 === 0) {
-            document.getElementById('secondOnes').innerHTML = second;
-            second++
-            if (second === 10) {
-                document.querySelector('#secondTens').style.display = 'none';
-                document.querySelector('.digits').style.color = 'red';
-                return second;
-            }
-        } else if (counter % 10 === 0) {
-            document.getElementById('msTens').innerHTML = msTen;
-            msTen++
-            if (msTen === 10) {
-                msTen = 1;
-            }
+function countUp() {
+    if (msTens.innerHTML < 9) {
+        ++msTens.innerHTML;
+    } else {
+        msTens.innerHTML = 0;
+        if (msHundreds.innerHTML < 9) {
+            ++msHundreds.innerHTML;
         }
-        // document.getElementById('msHundreds').innerHTML = counter;
-        // document.getElementById('msTens').innerHTML = counter;
-        // document.getElementById('secondOnes').innerHTML = counter;
-        counter++;
     }
-}, 10);
+}
