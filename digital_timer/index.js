85751d64b9c3f6a63de903ca676a12bfe99d8e42
Devon Bacon <devon.bacon@gmail.com>
2018-03-26 13:44:53 -0700

    Set refences for DOM elements

---
+++ digital_timer/index.js
@@ -0,0 +1,7 @@
+// Set references for DOM elements
+let secondOnes = document.querySelector("#secondOnes");
+let secondTens = document.querySelector("#secondTens");
+let msHundreds = document.querySelector("#msHundreds");
+let msTens = document.querySelector("#msTens");
+let digits = document.querySelector(".digits");
+
