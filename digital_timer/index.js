e198904cc7243de4c400be80da3393637c5c254b
Mark Wong <fmarkwong@gmail.com>
2018-03-26 14:57:49 -0700

    start button

--- digital_timer/index.js
+++ digital_timer/index.js
@@ -1,32 +1,38 @@
-let secondTens = document.getElementById('secondTens');
-let secondOnes = document.getElementById('secondOnes');
-let msHundreds = document.getElementById('msHundreds');
-let msTens = document.getElementById('msTens');
+const secondTens = document.getElementById('secondTens');
+const secondOnes = document.getElementById('secondOnes');
+const msHundreds = document.getElementById('msHundreds');
+const msTens = document.getElementById('msTens');
 
-let digits = document.getElementsByClassName('digits')[0];
+const digits = document.getElementsByClassName('digits')[0];
+
+const start = document.getElementById('start');
+
+
 
 let msTensValue = 0;
 let msHundredsValue = 0;
 let secondOnesValue = 0;
 let secondTensValue = 0;
 
-const intervalId = setInterval(() => {
-  if (msTensValue <= 9) {
-    msTens.textContent = msTensValue++;  
-  } else if (msHundredsValue <= 9) {
-    msTensValue = 0;
-    msHundreds.textContent = msHundredsValue++;  
-  } else if (secondOnesValue <= 9) {
-    msHundredsValue = 0;
-    secondOnes.textContent = secondOnesValue++;  
-  } else {
-    secondTens.textContent = 1;
-    secondOnes.textContent = 0;
-    msHundreds.textContent = 0;
-    msTens.textContent = 0;
-    digits.classList.add('redDigit');
+start.onclick = e => {
+  const intervalId = setInterval(() => {
+    if (msTensValue <= 9) {
+      msTens.textContent = msTensValue++;  
+    } else if (msHundredsValue <= 9) {
+      msTensValue = 0;
+      msHundreds.textContent = msHundredsValue++;  
+    } else if (secondOnesValue <= 9) {
+      msHundredsValue = 0;
+      secondOnes.textContent = secondOnesValue++;  
+    } else {
+      secondTens.textContent = 1;
+      secondOnes.textContent = 0;
+      msHundreds.textContent = 0;
+      msTens.textContent = 0;
+      digits.classList.add('redDigit');
 
-    clearInterval(intervalId);
-  }
+      clearInterval(intervalId);
+    }
 
-}, 10);
+  }, 10);
+};
