dba173d7f1ac6aca61b46124b8433be5ac5bc6dc
2940cristian <2940cristian@gmail.com>
2018-03-26 22:12:19 -0700

    added ms functionality and clear interval

--- digital_timer/index.js
+++ digital_timer/index.js
@@ -3,8 +3,9 @@ let secondOnes = document.getElementById('secondOnes');
 let msHundreds = document.getElementById('msHundreds');
 let msTens = document.getElementById('msTens');
 
-let counter = 0;
-
+let counter = 0
+let counterMs = 0;
+let counterMsHundreds = 0;
 function timerSeconds() {
     if(counter<9) {
         counter++
@@ -19,5 +20,46 @@ function timerTen() {
     secondTens.innerHTML = "1";
 };
 
-// setInterval(timerSeconds, 1000);
-// setInterval(timerTen, 10000);
+let a = setInterval(timerSeconds, 1000);
+let b = setInterval(timerTen, 10000);
+let c = setInterval(timerMsTens, 10)
+let d = setInterval(timerMsHunreds, 100);
+
+// function timerMsTens() {
+//     if(counterMs < 100) {
+//         counterMs = counterMs+10;
+//         msTens.innerHTML = counterMs;
+//     } else {
+//         counterMs = 0;
+//     }
+// };
+
+function timerMsTens() {
+    if(counterMs < 90) {
+        counterMs = counterMs+10;
+        msTens.innerHTML = counterMs;
+    } else {
+        counterMs = 0;
+    }
+};
+
+
+
+function timerMsHunreds() {
+    if(counterMsHundreds < 9) {
+        counterMsHundreds++;
+        msHundreds.innerHTML = counterMsHundreds;
+    } else {
+        counterMsHundreds = 0;
+    }
+};
+
+function atTen () {
+    clearInterval(c);
+    clearInterval(d);
+    msHundreds.innerHTML = 0;
+    msTens.innerHTML = 0;
+
+};
+
+setInterval(atTen, 10000);
