b5595aa468d24de84c6d27956c33ce5ed7ec5b70
markstez05 <36493764+markstez05@users.noreply.github.com>
2018-03-26 23:08:10 -0500

    timer working not stopping or colors

--- digital_timer/index.js
+++ digital_timer/index.js
@@ -1,1 +1,23 @@
-setInterval(function(){ alert("Hello"); }, 3000);
+var tenSecondsLabel = document.getElementById("msHundreds");
+var secondsLabel = document.getElementById("msTens");
+var totalSeconds = 0;
+setInterval(setTime, 1000);
+
+function setTime() {
+  ++totalSeconds;
+  secondsLabel.innerHTML = pad(totalSeconds % 10);
+  tenSecondsLabel.innerHTML = pad(parseInt(totalSeconds / 10));
+}
+
+function pad(val) {
+  var valString = val + "";
+  if (valString.length < 1) {
+    return "0" + valString;
+  } else {
+    return valString;
+  }
+}
+
+function endTime(intervalId) {
+    clearInterval(intervalId);
+} 
