65904772f57044fe53d7f09fe1830f594e695114
Alex Myers <amyers88@outlook.com>
2018-03-27 14:01:33 -0600

    Auto stash before rebase of "origin/master"

---
+++ digital_timer/index.js
@@ -0,0 +1,24 @@
+const msHundreds = document.getElementById('msHundreds'),
+    msTens = document.getElementById('msTens'),
+    secondOnes = document.getElementById('secondOnes'),
+    secondTens = document.getElementById('secondTens'),
+    digitsClass = document.getElementsByClassName('digits');
+
+let counter = 0000;
+
+const digitalTimer = () => {
+    const stop = () => {
+        clearInterval(digitalTimer);
+    }
+    
+    counter++;
+
+    const counterArr = counter.toString().split('');
+
+    msHundreds.innerText = counterArr[counterArr.length - 1];
+    msTens.innerText = counterArr[counterArr.length - 2];
+    secondOnes.innerText = counterArr[counterArr.length - 3];
+    secondTens.innerText = counterArr[counterArr.length - 4];
+}
+
+setInterval(digitalTimer, 10);
