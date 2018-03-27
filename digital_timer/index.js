e3730deb1a6eb07d307ea4229a8ff704e96c6038
Robert Hogan <roberthogan@Roberts-MacBook-Pro.local>
2018-03-26 22:54:59 -0400

    Robert Hogan, Paul Fang. Final Commit. Assignment Completed.

--- digital_timer/index.js
+++ digital_timer/index.js
@@ -1,58 +1,48 @@
-const secondTens = document.getElementById('secondTens');
-const secondOnes = document.getElementById('secondOnes');
-const msHundreds = document.getElementById('msHundreds');
-const msTens = document.getElementById('msTens');
+const secondTens = document.getElementById("secondTens");
+const secondOnes = document.getElementById("secondOnes");
+const msHundreds = document.getElementById("msHundreds");
+const msTens = document.getElementById("msTens");
+let count = 0;
 
-
-    secondOnes.innerHTML = 0;
+const resetClock = () => {
     secondTens.innerHTML = 0;
+    secondOnes.innerHTML = 0;
     msHundreds.innerHTML = 0;
     msTens.innerHTML = 0;
+}
 
+const controller = () => {
+    ++count;
+    updateFunct();
+    stopClock();
+}
 
-    let msTensCounter = 0;
-    let msHundredsCounter = 0;
-    let secondsTensCounter = 0;
-    let secondsOnesCounter = 0;
+const updateFunct = ()  => {
+    if (count % 1000 == 0) addDigit(secondTens);
+    if (count % 100 == 0) addDigit(secondOnes);
+    if (count % 10 == 0) addDigit(msHundreds);
+}
 
+const addDigit = (element) => {
+    if (element.innerHTML == 9) element.innerHTML = 0;
+    else ++element.innerHTML;
+}
 
-    if (msTens.innerHTML < 9) {
-        msTensCounter++;
-        msTens.innerHTML = msTensCounter.toString();
-    } else {
-        msTens.innerHTML === 0;
-        msTensCounter === 0;
+const stopClock = () => {
+    if (secondTens.innerHTML == 1) {
+        clearInterval(clockTimer);
+        updateRed();
     }
+}
+const updateRed = () => {
+    let elements = document.querySelectorAll(".digit");
+    elements.forEach(element => {
+        element.classList.add('redDigit');
+    });
+}
 
-        if (msHundreds.innerHTML < 9) {
-            msHundredsCounter++;
-            msHundreds.innerHTML = msHundredsCounter.toString();
-        } else {
-            msHundreds.innerHTML === 0;
-            msHundredsCounter === 0;
-        }
-
-
-        if (secondTens.innerHTML < 9) {
-            secondTensCounter++;
-            secondTens.innerHTML = secondTensCounter.toString();
-        } else {
-            secondTens.innerHTML === 0;
-            secondTensCounter === 0; 
-        }
-
-
-            if (secondOnes.innerHTML < 9) {
-                secondOnesCounter++;
-                secondOnes.innerHTML = secondOnesCounter.toString();
-            } else {
-                secondOnes.innerHTML === 0;
-                secondOnesCounter === 0;
-            }
-            clearInterval();
-
-
-
+resetClock();
+const clockTimer = setInterval(() => controller(), 10);
 
 //10ms = 1/10 second
 //1,000ms = 1 second
