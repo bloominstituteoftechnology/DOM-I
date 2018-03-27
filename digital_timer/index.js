aa47e5027d6a56ee88e15d8315be2e1a4044299e
Mark Wong <fmarkwong@gmail.com>
2018-03-27 12:36:18 -0700

    clean up

--- digital_timer/index.js
+++ digital_timer/index.js
@@ -5,60 +5,26 @@ const msTens = document.getElementById('msTens');
 
 const digits = document.getElementsByClassName('digits')[0];
 
-const start = document.getElementById('start');
-
+const startButton = document.getElementById('startButton');
 
 
 let msTensValue = 1;
 let msHundredsValue = 1;
 let secondOnesValue = 1;
 
-start.onclick = e => {
-  // const intervalMsTensId = setInterval(() => {
-  //   if (msTensValue <= 9) {
-  //     msTens.textContent = msTensValue++;  
-  //
-  //
-  //   } else if (msHundredsValue <= 9) {
-  //     msTensValue = 0;
-  //     msHundreds.textContent = msHundredsValue++;  
-  //
-  //
-  //   } else if (secondOnesValue <= 9) {
-  //     msHundredsValue = 0;
-  //     secondOnes.textContent = secondOnesValue++;  
-  //
-  //
-  //   } else {
-  //     secondTens.textContent = 1;
-  //     secondOnes.textContent = 0;
-  //     msHundreds.textContent = 0;
-  //     msTens.textContent = 0;
-  //     digits.classList.add('redDigit');
-  //
-  //     clearInterval(intervalMsTensId);
-  //   }
-  //
-  // }, 10);
-
+startButton.onclick = e => {
   const intervalmsTensId = setInterval(() => {
-    if (msTensValue <= 9) {
-      msTens.textContent = msTensValue++;  
-    }
+    msTens.textContent = msTensValue++;  
   }, 10);
 
   const intervalmsHundredsId = setInterval(() => {
-    if (msHundredsValue <= 9) {
-      msTensValue = 0;
-      msHundreds.textContent = msHundredsValue++;
-    }
+    msTensValue = 0;
+    msHundreds.textContent = msHundredsValue++;
   }, 100);
 
   const intervalSecondOnesId = setInterval(() => {
-    if (secondOnesValue <= 9) {
-      msHundredsValue = 0;
-      secondOnes.textContent = secondOnesValue++;  
-    } 
+    msHundredsValue = 0;
+    secondOnes.textContent = secondOnesValue++;  
   }, 1000);
 
 
