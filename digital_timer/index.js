35386a9b0a10b88f814779696a7cb6e76f9bcf10
Breanna Lee <Breanna.Lee@outlook.com>
2018-03-26 21:56:37 -0400

    My current Progress
    Code seems to be broke now, but not sure why.. The Button to Start timer never works so no count down, and my button styling isn't showing up.. :(

---
+++ digital_timer/index.js
@@ -0,0 +1,65 @@
+const digits = document.querySelector(".digits");
+const secondTens = document.getElementById("secondTens");
+const secondOnes = document.getElementById("secondOnes");
+const msHundreds = document.getElementById("msHundreds");
+const msTens = document.getElementById("msTens");
+const msOnes = document.getElementById("msOnes");
+const button = document.getElementById('button');
+
+button.addEventListener('click', (e) => {
+timer();
+});
+
+function timer () {
+  init();
+
+  function init () {
+    let ms = 0;
+    secondTens.innerHTML = "0";
+    secondOnes.innerHTML = "0";
+    msHundreds.innerHTML = "0";
+    msTens.innerHTML = "0";
+
+    for (let i = 0; i < digits.children.length; i++) {
+      digits.children[i].classList.remove('redDigit');
+    }
+
+    const timerInterval = window.setInterval(() => {
+      ms += 10;
+      if (ms === 10000) {
+        endTimer(timerInterval);
+      }
+      updateTimer(ms);
+    }, 10);
+  }
+
+  function increment (string) {
+    let number = Number(string) + 1;
+    return number.toString();
+  }
+
+  function endTimer (intervalId) {
+    clearInterval(intervalId);
+    Array.from(digits.children).forEach(digit => {
+      digit.classList.add("redDigit");
+    })
+  }
+
+  function updateTimer (ms) {
+    if (ms === 10000) {
+      secondTens.innerHTML = "1";
+      secondOnes.innerHTML = "0";
+      msHundreds.innerHTML = "0";
+      msTens.innerHTML = "0";
+    } else if (ms % 1000 === 0) {
+      secondOnes.innerHTML = increment(secondOnes.innerHTML);
+      msHundreds.innerHTML = "0";
+      msTens.innerHTML = "0";
+    } else if (ms % 100 === 0) {
+      msHundreds.innerHTML = increment(msHundreds.innerHTML);
+      msTens.innerHTML = "0";
+    } else {
+      msTens.innerHTML = increment(msTens.innerHTML);
+    }
+  }
+}
