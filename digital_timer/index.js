af262f25b86f2948f3a9b46fe71579cbfeac9d97
Iqra Javed <iqrajaved@Iqras-MacBook-Air.local>
2018-03-26 19:26:35 -0500

    added comments to code

--- digital_timer/index.js
+++ digital_timer/index.js
@@ -4,57 +4,83 @@ let colon = document.querySelector("#colon");
 let msHundreds = document.querySelector("#msHundreds");
 let msTens = document.querySelector("#msTens");
 
+
+
+// let secTens = document.getElementById("secondTens");
+// let secOnes = document.getElementById("#secondOnes");
+// let colon = document.getElementById("colon");
+// let msHundreds = document.getElementById("msHundreds");
+// let msTens = document.getElementById("msTens");
+
+
 function timer() {
 
     function stopTimer() {
-        clearInterval();
-    }
-
-    function msTensPlace() {
-       
-        msTens.innerHTML = 0;
         
+        clearInterval(x);
+    }
+// Initialized digits   
+    msTens.innerHTML = 0;
+    msHundreds.innerHTML = 0;
+    secOnes.innerHTML = 0;
+    secTens.innerHTML = 0;
+
+
+//  MsTens Function 
+    function msTensPlace() {
         function incMsTens() {
             setInterval(() => {
                 while (msTens.innerHTML < 9) {
-                    return ++msTens.innerHTML;
+                     ++msTens.innerHTML;
                 }
             }, 10)
         }
-       
+
         incMsTens();
         msHundredsPlace();
-
     }
 
-
-
+//  MsHundreds Function 
     function msHundredsPlace() {
-        msHundreds.innerHTML = 0;
-
         function incMsHundreds() {
             setInterval(() => {
-                while (msHundreds.innerHTML < 9) {
-                    return ++msHundreds.innerHTML;
+                while (msHundreds.innerHTML < 1) {
+                     ++msHundreds.innerHTML;
                 }
             }, 100)
             }
-        incMsHundreds()
+
+        incMsHundreds();
+        secOnesPlace();
     }
 
-
+//  SecOnes Function 
     function secOnesPlace() {
-        secOnes.innerHTML = 0;
+    
+        
+        function incSecOnes() {
+            setInterval(() => {
+                while(secOnes.innerHTML < 9) {
+                    secOnes.innerHTML++;
+                }
+            }, 1000)
 
+        }
+        incSecOnes();
+        secTensPlace();
     }
 
-
+// SecTens Function 
     function secTensPlace() {
-        secTens.innerHTML = 0;
+        
+        let x = setInterval(() => {
+            while(secTens.innerHTML < 1) {
+                secTens.innerHTML++;
+            }
+        }, 10000)
+       stopTimer();
     }
-
-
-    msTensPlace()
+    msTensPlace();
 }
 
 
