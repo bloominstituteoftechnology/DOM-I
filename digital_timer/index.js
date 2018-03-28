6945241231881b7dd1725521c152d26130dea3ea
Alex Myers <amyers88@outlook.com>
2018-03-28 12:16:36 -0600

    Alex Myers - DOM-I Complete

--- digital_timer/index.js
+++ digital_timer/index.js
@@ -1,22 +1,52 @@
-const msHundreds = document.getElementById('msHundreds'),
-    msTens = document.getElementById('msTens'),
-    secondOnes = document.getElementById('secondOnes'),
-    secondTens = document.getElementById('secondTens'),
-    digitsClass = document.getElementsByClassName('digits');
+// Global variables
+const msHundreds = document.getElementById('msHundreds');
+const msTens = document.getElementById('msTens');
+const secondOnes = document.getElementById('secondOnes');
+const secondTens = document.getElementById('secondTens');
+const digitsClass = document.querySelector('.digits');
 
+// Create a counter
 let counter = 0;
 
-const digitalTimer = () => {
+// Set Initial values
+secondTens.innerHTML = '0';
+secondOnes.innerHTML = '0';
+msTens.innerHTML = '0';
+msHundreds.innerHTML = '0';
 
-    counter++;
+// Increment Function
+const increment = (string) => {
+  let number = Number(string) + 1;
+  return String(number);
+};
 
-    const counterArr = counter.toString().split('');
+// Update HTML Display function
+const updateDisplay = (ms) => {
+  if (ms === 10000) {
+    secondTens.innerHTML = '1';
+    secondOnes.innerHTML = '0';
+    msTens.innerHTML = '0';
+    msHundreds.innerHTML = '0';
+  } else if (ms % 1000 === 0) {
+    secondOnes.innerHTML = increment(secondOnes.innerHTML);
+    msTens.innerHTML = '0';
+    msHundreds.innerHTML = '0';
+  } else if (ms % 100 === 0) {
+    msTens.innerHTML = increment(msTens.innerHTML);
+    msHundreds.innerHTML = '0';
+  } else if (ms % 10 === 0) {
+    msHundreds.innerHTML = increment(msHundreds.innerHTML);
+  }
+};
 
-    msHundreds.innerText = counterArr[counterArr.length - 1];
-    msTens.innerText = counterArr[counterArr.length - 2];
-    secondOnes.innerText = counterArr[counterArr.length - 3];
-    secondTens.innerText = counterArr[counterArr.length - 4];
-
-}
-
-setInterval(digitalTimer, 10);
+// Invoke the setInterval
+const digitalTimer = setInterval(() => {
+  counter += 10;
+  if (counter === 10000) {
+    clearInterval(digitalTimer);
+    for (let i = 0; i < digitsClass.children.length; i++) {
+      digitsClass.children[i].classList.add('redDigit');
+    }
+  }
+  updateDisplay(counter);
+}, 10);
