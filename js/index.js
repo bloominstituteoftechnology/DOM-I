2bddc7e82f6b3aef5bfbce643b16a7484cad9eff
april <martinezapril56@yahoo.com>
2018-04-23 18:51:23 -0400

    commit

--- js/index.js
+++ js/index.js
@@ -39,3 +39,46 @@ const siteContent = {
 // Example: Update the img src for the logo
 let logo = document.getElementById("logo-img");
 logo.setAttribute('src', siteContent["nav"]["img-src"])
+
+let headerA = document.querySelectorAll("a");
+headerA[0].innerHTML = siteContent["nav"]["nav-item-1"];
+headerA[1].innerHTML = siteContent["nav"]["nav-item-2"];
+headerA[2].innerHTML = siteContent["nav"]["nav-item-3"];
+headerA[3].innerHTML = siteContent["nav"]["nav-item-4"];
+headerA[4].innerHTML = siteContent["nav"]["nav-item-5"];
+
+
+let header = document.getElementById("cta-img");
+header.setAttribute('src', siteContent["cta"]["img-src"]);
+
+
+
+let tagElements = document.querySelector('.cta-text h1' );
+tagElements.innerHTML = siteContent['cta']['h1'];
+
+let middle = document.getElementById("middle-img");
+middle.setAttribute('src', siteContent["main-content"]["middle-img-src"]);
+
+let main = document.querySelectorAll("h4","p");
+main[0].innerHTML = siteContent["main-content"]["features-h4"];
+main[0].innerHTML = siteContent["main-content"]["features-content"];
+main[1].innerHTML = siteContent["main-content"]["about-h4"];
+main[1].innerHTML = siteContent["main-content"]["about-content"];
+main[2].innerHTML = siteContent["main-content"]["services-h4"];
+main[2].innerHTML = siteContent["main-content"]["services-content"];
+main[3].innerHTML = siteContent["main-content"]["product-h4"];
+main[3].innerHTML = siteContent["main-content"]["product-content"];
+main[4].innerHTML = siteContent["main-content"]["vision-h4"];
+main[4].innerHTML = siteContent["main-content"]["vision-content"];
+
+
+
+let contact = document.querySelectorAll("h4","p");
+contact[0].innerHTML = siteContent["contact"]["contact-h4"];
+contact[1].innerHTML = siteContent["contact"]["address"];
+contact[2].innerHTML = siteContent["contact"]["phone"];
+contact[3].innerHTML = siteContent["contact"]["email"];
+
+let footer = document.querySelectorAll("p");
+footer.innerHTML = siteContent["footer"]["footer"];
+
