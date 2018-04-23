58a05c41d1f99c547cf25b44466f7ac6150e3733
Anne Courtney <annecourtney_1@yahoo.com>
2018-04-23 14:48:22 -0500

    Began adding first few elements to the page using getElementsByTagName selector

--- js/index.js
+++ js/index.js
@@ -39,3 +39,58 @@ const siteContent = {
 // Example: Update the img src for the logo
 let logo = document.getElementById("logo-img");
 logo.setAttribute('src', siteContent["nav"]["img-src"])
+
+//nav
+let nav1 = document.getElementsByTagName("a")
+nav1[0].innerHTML = "Services";
+
+let nav2 = document.getElementsByTagName("a")
+nav2[1].innerHTML = "Product";
+
+let nav3 = document.getElementsByTagName("a")
+nav3[2].innerHTML = "Vision";
+
+let nav4 = document.getElementsByTagName("a")
+nav4[3].innerHTML = "Feautures";
+
+let nav5 = document.getElementsByTagName("a")
+nav5[4].innerHTML = "Contact";
+
+
+//cta
+let ctah1 = document.querySelector("h1");
+ctah1.innerHTML = "DOM is Awesome";
+
+let ctaButton = document.querySelector("button");
+ctaButton.innerHTML = "Get Started";
+
+let headerImg = document.getElementById("cta-img");
+headerImg.setAttribute('src', "img/header-img.png")
+
+//main content
+let main1 = document.getElementsByTagName("h4")
+main1[0].innerHTML = "Features";
+let content1 = document.getElementsByTagName("p")
+content1[0].innerHTML = "Features content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.";
+
+
+let main2 = document.getElementsByTagName("h4")
+main2[1].innerHTML = "About";
+let content2 = document.getElementsByTagName("p")
+content2[1].innerHTML = "About content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.";
+
+let main3 = document.getElementsByTagName("h4")
+main3[2].innerHTML = "Services";
+let content3 = document.getElementsByTagName("p")
+content3[2].innerHTML = "Services content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.";
+
+let main4 = document.getElementsByTagName("h4")
+main4[3].innerHTML = "Product";
+let content4 = document.getElementsByTagName("p")
+content4[3].innerHTML = "Product content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.";
+
+
+let main5 = document.getElementsByTagName("h4")
+main5[4].innerHTML = "Vision";
+let content5 = document.getElementsByTagName("p")
+content5[4].innerHTML = "Vision content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.";
