c8578289ba44391de604210653dc34409b428183
NandoTheessen <nando.theessen@gmail.com>
2018-04-23 22:09:17 +0200

    Completed multiple tasks:
    Added proper navbar
    Added CTA Content
    Added Main Content

--- js/index.js
+++ js/index.js
@@ -13,29 +13,64 @@ const siteContent = {
     "img-src": "img/header-img.png"
   },
   "main-content": {
-    "features-h4":"Features",
+    "features-h4": "Features",
     "features-content": "Features content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.",
-    "about-h4":"About",
+    "about-h4": "About",
     "about-content": "About content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.",
     "middle-img-src": "img/mid-page-accent.jpg",
-    "services-h4":"Services",
+    "services-h4": "Services",
     "services-content": "Services content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.",
-    "product-h4":"Product",
+    "product-h4": "Product",
     "product-content": "Product content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.",
-    "vision-h4":"Vision",
+    "vision-h4": "Vision",
     "vision-content": "Vision content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.",
   },
   "contact": {
-    "contact-h4" : "Contact",
-    "address" : "123 Way 456 Street Somewhere, USA",
-    "phone" : "1 (888) 888-8888",
-    "email" : "sales@greatidea.io",
+    "contact-h4": "Contact",
+    "address": "123 Way 456 Street Somewhere, USA",
+    "phone": "1 (888) 888-8888",
+    "email": "sales@greatidea.io",
   },
   "footer": {
-    "copyright" : "Copyright Great Idea! 2018"
+    "copyright": "Copyright Great Idea! 2018"
   },
 };
 
 // Example: Update the img src for the logo
 let logo = document.getElementById("logo-img");
 logo.setAttribute('src', siteContent["nav"]["img-src"])
+
+let selNavItem = document.querySelectorAll("nav a");
+selNavItem.forEach((element, index) => {
+  if (index === 5) {
+    return;
+  }
+  element.innerHTML = siteContent["nav"]["nav-item-" + (index + 1)];
+})
+
+let ctaHeading = document.querySelector(".cta-text")
+ctaHeading.innerHTML = "DOM Is Awesome";
+
+let ctaLogo = document.querySelector("section img");
+ctaLogo.src = 'img/header-img.png';
+
+let topContent = document.querySelector(".top-content")
+topContent.children[0].firstElementChild.innerHTML = siteContent["main-content"]["features-h4"];
+topContent.children[0].lastElementChild.innerHTML = siteContent["main-content"]["features-content"];
+topContent.children[1].firstElementChild.innerHTML = siteContent["main-content"]["about-h4"];
+topContent.children[1].lastElementChild.innerHTML = siteContent["main-content"]["about-content"];
+
+let middleImg = document.querySelector("#middle-img");
+middleImg.src = siteContent["main-content"]["middle-img-src"];
+
+let bottomContent = document.querySelector(".bottom-content")
+bottomContent.children[0].firstElementChild.innerHTML = siteContent["main-content"]["services-h4"];
+bottomContent.children[0].lastElementChild.innerHTML = siteContent["main-content"]["services-content"];
+bottomContent.children[1].firstElementChild.innerHTML = siteContent["main-content"]["product-h4"];
+bottomContent.children[1].lastElementChild.innerHTML = siteContent["main-content"]["product-content"];
+bottomContent.children[2].firstElementChild.innerHTML = siteContent["main-content"]["vision-h4"];
+bottomContent.children[2].lastElementChild.innerHTML = siteContent["main-content"]["vision-content"];
+
+let
+// let topContent2h = document.querySelector(".text-content p");
+// topContent.innerHTML = "jbaoifoiasdjf";
