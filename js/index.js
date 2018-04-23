02a3f3aafc91286c0496c8827a9165e641959324
Brad Traversy <mercado7946@gmail.com>
2018-04-23 14:21:55 -0700

    working through

--- js/index.js
+++ js/index.js
@@ -38,4 +38,30 @@ const siteContent = {
 
 // Example: Update the img src for the logo
 let logo = document.getElementById("logo-img");
-logo.setAttribute('src', siteContent["nav"]["img-src"])
+logo.setAttribute('src', siteContent["nav"]["img-src"]);
+
+let navLinks = document.querySelectorAll('nav a');
+
+navLinks[0].innerText = siteContent.nav['nav-item-1'];
+navLinks[1].innerText = siteContent.nav['nav-item-2'];
+navLinks[2].innerText = siteContent.nav['nav-item-3'];
+navLinks[3].innerText = siteContent.nav['nav-item-4'];
+navLinks[4].innerText = siteContent.nav['nav-item-5'];
+
+let createAnchor1 = document.createElement('a');
+createAnchor1.innerHTML = 'About';
+
+let createAnchor2 = document.createElement('a');
+createAnchor2.innerHTML = 'What we Do';
+
+let nav = document.querySelector('nav');
+nav.prepend(createAnchor1);
+nav.appendChild(createAnchor2);
+
+navLinks = document.querySelectorAll('nav a');
+
+for (let i = 0; i < navLinks.length; i++) {
+  navLinks[i].style.color = 'green';
+}
+
+
