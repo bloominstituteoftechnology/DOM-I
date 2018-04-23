1b5044c88108bfdf0039453df8f16d11358d8d54
mister-corn <code@ronaldlibago.com>
2018-04-23 15:38:30 -0700

    3:36PM PDT: Stretch Goals Completed
    * Played with a few styles and observed effects in the HTML and the DOM as seen by the browser.
    * Added a button that shows contents and increments a button counter in the bottom of the contact div.

--- js/index.js
+++ js/index.js
@@ -31,6 +31,7 @@ const siteContent = {
     "address" : "123 Way 456 Street Somewhere, USA",
     "phone" : "1 (888) 888-8888",
     "email" : "sales@greatidea.io",
+    "count": 0,
   },
   "footer": {
     "copyright" : "Copyright Great Idea! 2018"
@@ -40,6 +41,24 @@ const siteContent = {
 // Example: Update the img src for the logo
 const logo = document.querySelector("#logo-img");
 logo.setAttribute('src', siteContent["nav"]["img-src"]);
+
+//---Stretch: Button
+// We'll hide a lot of the page until the button is clicked.
+document.querySelectorAll(".main-content, .contact").forEach(element => element.style.display = "none");
+// Now, to add an eventListener to the button and function that runs when button is clicked
+const button = document.querySelector(".cta-text button");
+button.addEventListener("click", function() {
+  let displayCheck = document.querySelector(".main-content").style.display;
+  if (displayCheck === "none") {
+    document.querySelectorAll(".main-content, .contact").forEach(element => element.style.display = "block");
+  } else {
+    document.querySelectorAll(".main-content, .contact").forEach(element => element.style.display = "none");
+  }
+  siteContent["contact"]["count"]++;
+  console.log(siteContent["contact"]["count"]);
+  document.querySelector("#buttonCounter").innerHTML = `The button has been clicked ${siteContent["contact"]["count"]} times.`
+;})
+//---------End Stretch: Button
 
 //---Header 
 // Insert Nav Items into Nav Bar
@@ -63,7 +82,10 @@ nav.appendChild(backElem);
 // To make all the <a> elements (light) green
 navItem = document.querySelectorAll("nav a");
 navItem.forEach(element => element.style.color = "lightgreen");
-//------End Header  
+// Let's add a background color to complement that green
+nav.style["background-color"] = "#369C9C";
+nav.style.padding = "16px";
+//---------End Header  
 
 //---CTA
 const ctaHeading = document.querySelectorAll(".cta-text")
@@ -72,11 +94,10 @@ ctaHeading[0].lastElementChild.innerHTML = siteContent.cta["button"];
 
 const ctaLogo = document.querySelector("section img");
 ctaLogo.src = 'img/header-img.png';
-//------End CTA 
+//---------End CTA 
 
 //---Main-Content 
 const topContent = document.querySelector(".top-content");
-console.log(topContent.children);
 topContent.children[0].firstElementChild.innerHTML = siteContent["main-content"]["features-h4"];
 topContent.children[0].lastElementChild.innerHTML = siteContent["main-content"]["features-content"];
 topContent.children[1].firstElementChild.innerHTML = siteContent["main-content"]["about-h4"];
@@ -92,7 +113,7 @@ bottomContent.children[1].firstElementChild.innerHTML = siteContent["main-conten
 bottomContent.children[1].lastElementChild.innerHTML = siteContent["main-content"]["product-content"];
 bottomContent.children[2].firstElementChild.innerHTML = siteContent["main-content"]["vision-h4"];
 bottomContent.children[2].lastElementChild.innerHTML = siteContent["main-content"]["vision-content"];
-//------End Main-Content 
+//---------End Main-Content 
 
 //---Contact 
 const contactHead = document.querySelector(".contact h4");
@@ -102,9 +123,14 @@ const contactBody = document.querySelectorAll(".contact p");
 contactBody[0].innerHTML = siteContent["contact"]["address"];
 contactBody[1].innerHTML = siteContent["contact"]["phone"];
 contactBody[2].innerHTML = siteContent["contact"]["email"];
-//------End Contact 
+
+const buttonCounter = newElem("p");
+buttonCounter.appendChild(newText(`The button yearns for your click.`));
+buttonCounter.setAttribute("id","buttonCounter");
+document.querySelector(".contact").appendChild(buttonCounter);
+//---------End Contact 
 
 //---Footer 
 const footer = document.querySelector("footer");
 footer.innerHTML = siteContent["footer"]["copyright"];
-//------End Footer 
+//---------End Footer 
