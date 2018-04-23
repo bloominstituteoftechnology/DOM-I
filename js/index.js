be66bb5f70664e942665e578cdc7463e087dda84
fireinjun <fireinjun291@gmail.com>
2018-04-23 15:30:29 -0400

    added image

--- js/index.js
+++ js/index.js
@@ -39,3 +39,6 @@ const siteContent = {
 // Example: Update the img src for the logo
 let logo = document.getElementById("logo-img");
 logo.setAttribute('src', siteContent["nav"]["img-src"])
+
+let headerimg = document.getElementById("cta-img");
+headerimg.setAttribute('src', siteContent["cta"]["img-src"])
