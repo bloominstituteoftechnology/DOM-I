54ffe1854a1b1f895b6250605c3187700cc53f68
mr-burton <taylormade800@gmail.com>
2018-04-23 12:55:19 -0700

    initial change for github

--- js/index.js
+++ js/index.js
@@ -39,3 +39,5 @@ const siteContent = {
 // Example: Update the img src for the logo
 let logo = document.getElementById("logo-img");
 logo.setAttribute('src', siteContent["nav"]["img-src"])
+
+// change made for inital github
