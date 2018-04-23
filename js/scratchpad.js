520be7b419a1f35ee87f8357da82a43210eab030
fireinjun <fireinjun291@gmail.com>
2018-04-23 13:37:55 -0400

    Added scratchpad for manipulation practice.

---
+++ js/scratchpad.js
@@ -0,0 +1,11 @@
+const id = document.querySelector('#id');
+console.log(id);
+
+const classes = document.querySelectorAll(".class");
+classes.forEach(element => {console.log(element)});
+
+const headings = document.querySelectorAll("h2");
+headings.forEach(element => {console.log(element)});
+
+const ps = document.querySelectorAll("p");
+ps.forEach(element => {console.log(element)});
