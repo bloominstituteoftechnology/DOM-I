3665c527e44b4961b02091ee7243bdb54924595a
Niloc92 <prestonresult@github.com>
2018-04-23 15:49:27 -0500

    adding nav

--- js/index.js
+++ js/index.js
@@ -13,29 +13,50 @@ const siteContent = {
     "img-src": "img/header-img.png"
   },
   "main-content": {
-    "features-h4":"Features",
+    "features-h4": "Features",
     "features-content": "Features content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.",
-    "about-h4":"About",
+    "about-h4": "About",
     "about-content": "About content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.",
     "middle-img-src": "img/mid-page-accent.jpg",
-    "services-h4":"Services",
+    "services-h4": "Services",
     "services-content": "Services content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.",
-    "product-h4":"Product",
+    "product-h4": "Product",
     "product-content": "Product content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.",
-    "vision-h4":"Vision",
+    "vision-h4": "Vision",
     "vision-content": "Vision content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.",
   },
   "contact": {
-    "contact-h4" : "Contact",
-    "address" : "123 Way 456 Street Somewhere, USA",
-    "phone" : "1 (888) 888-8888",
-    "email" : "sales@greatidea.io",
+    "contact-h4": "Contact",
+    "address": "123 Way 456 Street Somewhere, USA",
+    "phone": "1 (888) 888-8888",
+    "email": "sales@greatidea.io",
   },
   "footer": {
-    "copyright" : "Copyright Great Idea! 2018"
+    "copyright": "Copyright Great Idea! 2018"
   },
 };
 
 // Example: Update the img src for the logo
+
+let nav = document.getElementsByTagName("nav");
+nav[0].children[0].innerHTML = siteContent["nav"]["nav-item-1"]
+nav[0].children[1].innerHTML = siteContent["nav"]["nav-item-2"]
+nav[0].children[2].innerHTML = siteContent["nav"]["nav-item-3"]
+nav[0].children[3].innerHTML = siteContent["nav"]["nav-item-4"]
+
+
+console.log(nav[0].children[0].innerHTML)
+
 let logo = document.getElementById("logo-img");
-logo.setAttribute('src', siteContent["nav"]["img-src"])
+logo.setAttribute('src', siteContent["nav"]["img-src"]);
+
+let headering = document.getElementById("cta-img");
+headering.setAttribute('src', siteContent["cta"]["img-src"])
+
+
+
+let middleimg = document.getElementById("middle-img");
+middleimg.setAttribute('src', siteContent["main-content"]["middle-img-src"]);
+
+
+
---
+++ js/scratchpad.js
@@ -0,0 +1,28 @@
+// Scracth pad
+const id = document.querySelector('#id');
+console.log(id);
+
+const classes = document.querySelectorAll(".class");
+classes.forEach(element => { console.log(element) });
+
+const headings = document.querySelectorAll("h2");
+headings.forEach(element => { console.log(element) });
+
+const ps = document.querySelectorAll("p");
+ps.forEach(element => { console.log(element) });
+
+
+
+
+var elem = document.getElementById('id');
+console.log(elem);
+
+
+
+
+
+console.log(document.getElementByID('id').innerHTML)
+console.log(document.getElementsByClass('class')[0].innerHTML)
+console.log(document.getElementsByClassName('class')[1].innerHTML)
+
+
