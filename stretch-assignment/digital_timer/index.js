ca2934a6d07104b065945508f7f33162b4c3993c
NandoTheessen <nando.theessen@gmail.com>
2018-04-24 16:07:14 +0200

    Added Stretch Goals for the Stretch Challenge: - Added two Buttons for Starting and resetting the counter - Added onClickEvents for both buttons to fire / reset the counter - Added style Elements

--- stretch-assignment/digital_timer/index.js
+++ stretch-assignment/digital_timer/index.js
@@ -14,50 +14,70 @@ function initialise() {
     return digits;
 }
 
-var incrementVar = setInterval(increment, 10)
+function onClick() {
+    var incrementVar = setInterval(increment, 10)
 
-// function to increment our timer every 10ms
-let ms = 0;
-let hs = 0;
-let s = 0;
-let ns = 0;
+    // function to increment our timer every 10ms
+    let ms = 0;
+    let hs = 0;
+    let s = 0;
+    let ns = 0;
 
-function increment() {
+    function increment() {
 
-    if (digits[4].innerHTML !== "9") {
-        ms++;
-        digits[4].innerHTML = ms;
-    } else {
-        hs++;
-        ms = 0;
-        digits[4].innerHTML = ms;
-        if (digits[3].innerHTML !== "9") {
-            digits[3].innerHTML = hs;
+        if (digits[4].innerHTML !== "9") {
+            ms++;
+            digits[4].innerHTML = ms;
         } else {
-            s++;
-            hs = 0;
-            digits[3].innerHTML = hs;
-            if (digits[1].innerHTML !== "9") {
-                digits[1].innerHTML = s;
+            hs++;
+            ms = 0;
+            digits[4].innerHTML = ms;
+            if (digits[3].innerHTML !== "9") {
+                digits[3].innerHTML = hs;
             } else {
-                ns++;
-                s = 0;
-                digits[1].innerHTML = s;
-                digits[0].innerHTML = ns;
-                if (ns == 1) {
-                    for (let i = 0; i < digits.length; i++) {
-                        digits[i].style.color = "red";
+                s++;
+                hs = 0;
+                digits[3].innerHTML = hs;
+                if (digits[1].innerHTML !== "9") {
+                    digits[1].innerHTML = s;
+                } else {
+                    ns++;
+                    s = 0;
+                    digits[1].innerHTML = s;
+                    digits[0].innerHTML = ns;
+                    if (ns == 1) {
+                        for (let i = 0; i < digits.length; i++) {
+                            digits[i].style.color = "red";
+                        }
+                        clearInterval(incrementVar);
                     }
-                    clearInterval(incrementVar);
                 }
             }
         }
     }
 }
-
-
-
-// function myStopFunction() {
-//     clearInterval(incrementVar);
-// }
 initialise();
+
+// Create a Start button element
+
+const newButton = document.createElement('button')
+newButton.innerHTML = "PLEASE, PLEASE PRESS ME!"
+document.body.appendChild(newButton);
+
+// Add funcitonality to Start Button, Add functionality to disable the button for 10s after timer start
+newButton.addEventListener('click', function () {
+    document.querySelector("button").setAttribute("disabled", "")
+    onClick()
+    setTimeout(function () {
+        document.querySelector("button").removeAttribute("disabled")
+    }, 10000)
+});
+
+// add Button & functionality to Reset the timer 
+const newButtonReset = document.createElement('button')
+newButtonReset.innerHTML = "Reset the Timer!"
+document.body.appendChild(newButtonReset);
+
+newButtonReset.addEventListener('click', function () {
+    initialise()
+});
