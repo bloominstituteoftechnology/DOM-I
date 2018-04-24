f503bcbbe320d694ad0d7e57c25120fec232f194
CMVNK <Kopecky12112@gmail.com>
2018-04-24 16:09:34 -0500

    decluttered comments that were used to assist in writing code

--- js/index.js
+++ js/index.js
@@ -1,41 +1,39 @@
 const siteContent = { //this is an object with properties and values.
-  'nav': { //document.getElementByTagName('nav') = siteContent['nav']         
-    'nav-item-1': 'Services', //menu = siteContent['nav']['nav-item-1']
-    'nav-item-2': 'Product', //menu = siteContent['nav']['nav-item-2']
-    'nav-item-3': 'Vision', //menu = siteContent['nav']['nav-item-3']
-    'nav-item-4': 'Features', //menu = siteContent['nav']['nav-item-4']
-    'nav-item-5': 'About', //menu = siteContent['nav']['nav-item-5']
-    'nav-item-6': 'Contact', //menu = siteContent['nav']['nav-item-6']
-    'img-src': 'img/logo.png'//document.querySelector('#logo-img') = siteContent['nav']['img-src']
+  'nav': {       
+    'nav-item-1': 'Services', 
+    'nav-item-2': 'Product', 
+    'nav-item-3': 'Vision', 
+    'nav-item-4': 'Features', 
+    'nav-item-5': 'About', 
+    'nav-item-6': 'Contact', 
+    'img-src': 'img/logo.png'
   },
-  'cta': { //siteContent['cta']
-    'h1': 'DOM Is Awesome', //document.querySelector('.cta .cta-text h1') = siteContent['cta']['h1']
-    'button': 'Get Started',//document.getElementByTagName('button') = siteContent['cta'][button]
-    'img-src': 'img/header-img.png' //document.querySelector('#cta-img) = siteContent['cta']['img-src']
+  'cta': { 
+    'h1': 'DOM Is Awesome', 
+    'button': 'Get Started',
+    'img-src': 'img/header-img.png' 
   },
   'main-content': { 
-    //topContent 
-    'features-h4':'Features',//document.querySelectorAll('.top-content .text-content[0]') = siteContent['main-content']['features-h4']
-    'features-content': 'Features content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.', //document.querySelector('.top-content .text-content[0] p') = siteContent['main-content']['features-content']
-    'about-h4':'About', //document.querySelectorAll('.top-content .text-content[1]') = siteContent['main-content']['about-h4']
-    'about-content': 'About content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.', //document.querySelector('.top-content .text-content[1] p') = siteContent['main-content']['about-content']
-    'middle-img-src': 'img/mid-page-accent.jpg', //document.getElementById('middle-img') = siteContent['main-content']['middle-img-src']
-    //bottom-content
-    'services-h4':'Services',//document.querySelector('.bottom-content .text-content[0] h4')' = siteContent['main-content]['services-h4']
-    'services-content': 'Services content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.',//document.querySelector('.bottom-content .text-content[0] p') = siteContent['main-content']['services-content']
-    'product-h4':'Product', //document.querySelector('.bottom-content .text-content[1] h4') = siteContent['main-content']['product-h4']
-    'product-content': 'Product content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.',//document.querySelector('.bottom-content .text-content[1] p') = siteContent['main-content']['product-content']
-    'vision-h4':'Vision',//document.querySelector('.bottom-content .text-content[2] h4') = siteContent['main-content']['vision-h4']
-    'vision-content': 'Vision content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.',//document.querySelector('bottom-content .text-content[2] p') = siteContent['main-content']['vision-content']
+    'features-h4':'Features',
+    'features-content': 'Features content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.', 
+    'about-h4':'About', 
+    'about-content': 'About content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.',
+    'middle-img-src': 'img/mid-page-accent.jpg', 
+    'services-h4':'Services',
+    'services-content': 'Services content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.',
+    'product-h4':'Product', 
+    'product-content': 'Product content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.',
+    'vision-h4':'Vision',
+    'vision-content': 'Vision content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.',
   },
-  'contact': {//document.getElementsByClassName('contact') = siteContent['contact']
-    'contact-h4' : 'Contact',// document.querySelector('.contact h4') = siteContent['contact']['contact-h4']
-    'address' : '123 Way 456 Street Somewhere, USA',//document.querySelector('.contact p[1]') = siteContent['contact']['address']
-    'phone' : '1 (888) 888-8888',//document.querySelector('.contact p[2]') = siteContent['contact']['phone']
-    'email' : 'sales@greatidea.io',//document.querySelector('.contact p[3]') = siteContent['contact']['email']
+  'contact': {
+    'contact-h4' : 'Contact',
+    'address' : '123 Way 456 Street Somewhere, USA',
+    'phone' : '1 (888) 888-8888',
+    'email' : 'sales@greatidea.io',
   },
-  'footer': { //siteContent['footer']
-    'copyright' : 'Copyright Great Idea! 2018'//document.getElementByTagName('footer') = siteContent['footer']['copyright']
+  'footer': { 
+    'copyright' : 'Copyright Great Idea! 2018'
   },
 };
 
@@ -119,7 +117,7 @@ let footerPTag = pTag[8];
 let middleImg = document.querySelector('.middle-img'); 
   middleImg.setAttribute('src', siteContent['main-content']['middle-img-src']) 
 
-  //new navs
+//new navs
 
 //create text node to append to "a" element that will then append to
 //"nav" element.
@@ -128,7 +126,7 @@ let middleImg = document.querySelector('.middle-img');
 let navigator = document.querySelector('nav');
 let newNav = document.createElement('a');
 newNav.style.color = 'green';           
-let newNavText = document.createTextNode('Directions');    // Create a text node
+let newNavText = document.createTextNode('Directions');
 newNav.setAttribute('href', '#');
 newNav.appendChild(newNavText);
 navigator.appendChild(newNav);  
