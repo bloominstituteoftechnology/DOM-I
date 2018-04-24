565d4ad044b9fabf562a9219f6478d433de6e789
Niloc92 <prestonresult@github.com>
2018-04-24 08:29:04 -0500

    different approuch

--- js/index.js
+++ js/index.js
@@ -44,47 +44,113 @@ nav[0].children[0].innerHTML = siteContent["nav"]["nav-item-1"]
 nav[0].children[1].innerHTML = siteContent["nav"]["nav-item-2"]
 nav[0].children[2].innerHTML = siteContent["nav"]["nav-item-3"]
 nav[0].children[3].innerHTML = siteContent["nav"]["nav-item-4"]
+
+document.getElementById("logo-img").src = siteContent["nav"]["img-src"];
+
+
+
+document.getElementById("cta-img").src = siteContent['cta']['img-src'];
+
+
+document.getElementById("middle-img").src = siteContent["main-content"]["middle-img-src"];
+
 // console.log(nav[0].children[0].innerHTML)
-let logo = document.getElementById("logo-img");
-logo.setAttribute('src', siteContent["nav"]["img-src"]);
 
-let headering = document.getElementById("cta-img");
-headering.setAttribute('src', siteContent["cta"]["img-src"]);
+document.querySelector('.cta-text').firstElementChild.innerHTML = siteContent.cta.h1;
 
-document.querySelector('button').innerHTML = siteContent['cta']['button'];
+// Add cta button
+document.querySelector('.cta-text').getElementsByTagName('button')[0].innerHTML = siteContent.cta.button;
 
-
-
-const maincontent = Object.entries(siteContent['main-content'])
-const contact = Object.entries(siteContent.contact)
-const footer = Object.entries(siteContent.footer)
-// console.log('lets see', maincontent,'ok', contact,'ok', footer)
+// Add main content
+// Seperate into Top Content and Bottom Content
+// TODO Add a for()loop to seperate the content-text into various parts for the site.
 
 
+// Top 
+document.querySelector('.top-content').getElementsByTagName('h4')[0].innerHTML = siteContent["main-content"]["features-h4"];
+document.querySelector('.top-content').getElementsByTagName('p')[0].innerHTML = siteContent["main-content"]["features-content"];
+document.querySelector('.top-content').getElementsByTagName('h4')[1].innerHTML = siteContent["main-content"]["about-h4"];
+document.querySelector('.top-content').getElementsByTagName('p')[1].innerHTML = siteContent["main-content"]["about-content"];
+
+// Bottom Content
+document.querySelector('.bottom-content').getElementsByTagName('h4')[0].innerHTML = siteContent["main-content"]["services-h4"];
+document.querySelector('.bottom-content').getElementsByTagName('p')[0].innerHTML = siteContent["main-content"]["services-content"];
+document.querySelector('.bottom-content').getElementsByTagName('h4')[1].innerHTML = siteContent["main-content"]["product-h4"];
+document.querySelector('.bottom-content').getElementsByTagName('p')[1].innerHTML = siteContent["main-content"]["product-content"];
+document.querySelector('.bottom-content').getElementsByTagName('h4')[2].innerHTML = siteContent["main-content"]["vision-h4"];
+document.querySelector('.bottom-content').getElementsByTagName('p')[2].innerHTML = siteContent["main-content"]["vision-content"];
+
+// Add contact
+document.querySelector('.contact').getElementsByTagName('h4')[0].innerHTML = siteContent.contact["contact-h4"];
+document.querySelector('.contact').getElementsByTagName('p')[0].innerHTML = siteContent.contact["address"];
+document.querySelector('.contact').getElementsByTagName('p')[1].innerHTML = siteContent.contact["phone"];
+document.querySelector('.contact').getElementsByTagName('p')[2].innerHTML = siteContent.contact["email"];
+
+// Add footer
+document.querySelector('footer').getElementsByTagName('p')[0].innerHTML = siteContent.footer.copyright;
 
 
 
-// let middleimg = document.getElementById("middle-img");
-// middleimg.setAttribute('src', siteContent["main-content"]["middle-img-src"]);
 
 
-// console.log(Array.from(Object.entries(siteContent['main-content'])))
-
-// Array.from(Object.entries(siteContent['main-content'])).slice(4, 1)
-// Object.entries
-//longest way Lol
-
-// document.querySelector('h1').innerHTML = siteContent['cta']['h1'];
-
-// document.getElementById("top-content").innerHTML = siteContent["main-content"]["features-h4"];
-// // document.querySelector('p').innerHTML = siteContent["main-content"]["features-content"];
 
 
-// // console.log(document.querySelector("h4").innerHTML)
+
+// let logo = document.getElementById("logo-img");
+// logo.setAttribute('src', siteContent["nav"]["img-src"]);
+
+// let headering = document.getElementById("cta-img");
+// headering.setAttribute('src', siteContent["cta"]["img-src"]);
+
+// document.querySelector('button').innerHTML = siteContent['cta']['button'];
 
 
-// // document.getElementsByClass('h4').innerHTML = siteContent['contact']['contact-h4'];
-// // console.log(document.querySelector('h4').innerHTML)
+
+// const maincontent = Object.entries(siteContent['main-content'])
+// const contact = Object.entries(siteContent.contact)
+// const footer = Object.entries(siteContent.footer)
+// // console.log('lets see', maincontent,'ok', contact,'ok', footer)
+
+// let maincontentElements = Array.from(document.getElementsByClassName('main-content')[0].children);
+
+// maincontentElements.forEach((item, index) => {
+//   if (item.className === 'middle-img') {
+//     item.setAttribute('src', siteContent['main-content']['middle-img-src']);
+//   } else {
+//     let ind = 0;
+//     let h4s = Array.from(item.getElementsByTagName('h4'));
+//     let ps = Array.from(item.getElementsByTagName('p'));
+//     h4s.forEach(item => {
+//       item.innerHTML = mainContent[ind][i];
+//       ind++
+//     })  
+//   }
+// })
+
+
+
+
+// // let middleimg = document.getElementById("middle-img");
+// // middleimg.setAttribute('src', siteContent["main-content"]["middle-img-src"]);
+
+
+// // console.log(Array.from(Object.entries(siteContent['main-content'])))
+
+// // Array.from(Object.entries(siteContent['main-content'])).slice(4, 1)
+// // Object.entries
+// //longest way Lol
+
+// // document.querySelector('h1').innerHTML = siteContent['cta']['h1'];
+
+// // document.getElementById("top-content").innerHTML = siteContent["main-content"]["features-h4"];
+// // // document.querySelector('p').innerHTML = siteContent["main-content"]["features-content"];
+
+
+// // // console.log(document.querySelector("h4").innerHTML)
+
+
+// // // document.getElementsByClass('h4').innerHTML = siteContent['contact']['contact-h4'];
+// // // console.log(document.querySelector('h4').innerHTML)
 
 
 
