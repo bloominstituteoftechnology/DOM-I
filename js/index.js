151642f19f8c6ea224b97c2ba5706b3c67df0797
CMVNK <Kopecky12112@gmail.com>
2018-04-24 10:24:47 -0500

    variables and top half
    declare variables and start the top half of the web page.
    
    Create new element , a, price, and another new element, a, map
    
    Turned nav links green
    
    Inital commit

--- js/index.js
+++ js/index.js
@@ -38,5 +38,60 @@ const siteContent = {
 };
 
 // Example: Update the img src for the logo
-let logo = document.getElementById("logo-img");
-logo.setAttribute('src', siteContent["nav"]["img-src"])
+
+//var
+let menu = document.querySelectorAll('a');
+let nav = document.querySelector('nav')
+let newNav = document.createElement('a');
+let newNewNav = document.createElement('a');
+let navLinks = document.querySelectorAll('nav a')
+let ctaText = document.querySelector('.cta-text h1');
+let ctaBut = document.querySelector('.cta-text button');
+let ctaImg = document.querySelector('.cta img');
+let topContenth4 = document.querySelector('.text-content h4');
+let topContentP = document.querySelector('.text-content p');
+let topContenth4Second = document.querySelectorAll('.text-content h4')[1];
+let topContentPSecond = document.querySelectorAll('.text-content p')[1];;
+let topContentImg = document.querySelector('.middle-img');
+
+// nav
+
+menu[0].innerHTML = siteContent['nav']["nav-item-1"];
+menu[1].innerHTML = siteContent['nav']["nav-item-2"];
+menu[2].innerHTML = siteContent['nav']["nav-item-3"];
+menu[3].innerHTML = siteContent['nav']["nav-item-4"];
+menu[4].innerHTML = siteContent['nav']["nav-item-5"];
+menu[5].innerHTML = siteContent['nav']["nav-item-6"];
+
+// create new Nav items
+nav.appendChild(newNav)
+newNav.setAttribute('href', '#');
+newNav.innerHTML = 'Map '
+
+nav.appendChild(newNewNav)
+newNewNav.setAttribute('href', '#');
+newNewNav.innerHTML = 'Price'
+
+
+// style nav
+
+navLinks.forEach(function(item) {
+ item.style.color = 'green';
+});
+
+// cta
+
+
+ctaText.innerHTML = siteContent['cta']["h1"]; 
+ctaBut.innerHTML = siteContent['cta']["button"]; 
+ctaImg.setAttribute('src', siteContent['cta']["img-src"]);
+
+// main-content  top-content
+
+
+topContenth4.innerHTML = siteContent['main-content']["features-h4"]; 
+topContentP.innerHTML = siteContent['main-content']["features-content"]; 
+topContenth4Second.innerHTML = siteContent['main-content']["about-h4"]; 
+topContentPSecond.innerHTML = siteContent['main-content']['features-content'];
+
+topContentImg.setAttribute('src', siteContent['main-content']['middle-img-src']);
