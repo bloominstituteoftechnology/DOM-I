1b77783e067e0c552fbb46ce8069357b9ce92777
Niloc92 <prestonresult@github.com>
2018-04-24 17:35:05 -0500

    stretching

--- js/index.js
+++ js/index.js
@@ -137,3 +137,60 @@ document.querySelector('.bottom-content').getElementsByTagName('p')[2].addEventL
 //   event.stopImmediatePropagation();
 //   console.log("I am button");
 // });
+
+//tween time
+
+var logo = document.getElementById("logo-img");
+TweenMax.to(logo, 2, { width: "200px", height: "150px" });
+console.log('ok', logo)
+var game = new Phaser.Game(800, 600, Phaser.CANVAS, 'phaser-example', { preload: preload, create: create, update: update });
+
+var distance = 300;
+var speed = 6;
+var max = 500;
+
+var canvas;
+
+var xx = [];
+var yy = [];
+var zz = [];
+
+function preload() {
+
+  game.load.image('star', 'assets/demoscene/star2.png');
+
+}
+
+function create() {
+
+  canvas = game.add.bitmapData(800, 600);
+  canvas.addToWorld();
+
+  for (var i = 0; i < max; i++) {
+    xx[i] = Math.floor(Math.random() * 800) - 400;
+    yy[i] = Math.floor(Math.random() * 600) - 300;
+    zz[i] = Math.floor(Math.random() * 1700) - 100;
+  }
+
+}
+
+function update() {
+
+  canvas.clear();
+
+  for (var i = 0; i < max; i++) {
+    var perspective = distance / (distance - zz[i]);
+    var x = game.world.centerX + xx[i] * perspective;
+    var y = game.world.centerY + yy[i] * perspective;
+
+    zz[i] += speed;
+
+    if (zz[i] > 300) {
+      zz[i] -= 600;
+    }
+
+    //  Swap this for a standard drawImage call
+    canvas.draw('star', x, y);
+  }
+
+}
