60a3f297cdbb75ade49cbc5286d3fc68bdd83611
Andrew Kolumbic <andrewkolumbic@Andrews-Mac-mini.local>
2018-04-24 13:21:28 -0700

    completed assignment

--- js/index.js
+++ js/index.js
@@ -40,4 +40,75 @@ const siteContent = {
 let logo = document.getElementById("logo-img");
 logo.setAttribute('src', siteContent["nav"]["img-src"])
 
+let headerImg = document.getElementById("cta-img");
+headerImg.setAttribute("src", "img/header-img.png")
 
+let middleImg = document.getElementById("middle-img");
+middleImg.setAttribute("src", "img/mid-page-accent.jpg")
+
+//Navigation
+let services = document.getElementsByTagName("a")[0];
+services.innerHTML = "Services";
+let product = document.getElementsByTagName("a")[1];
+product.innerHTML = "Product";
+let vision = document.getElementsByTagName("a")[2];
+vision.innerHTML = "Vision";
+let features = document.getElementsByTagName("a")[3];
+features.innerHTML = "Features";
+let about = document.getElementsByTagName("a")[4];
+about.innerHTML = "About"
+let contact = document.getElementsByTagName("a")[5];
+contact.innerHTML = "Contact";
+
+//CTA
+let h1Message = document.getElementsByTagName("h1")[0];
+h1Message.innerHTML = "DOM Is Awesome";
+
+let button = document.getElementsByTagName("button")[0];
+button.innerHTML = "Get Started";
+
+//Main content 
+let featuresH4 = document.getElementsByTagName('h4')[0];
+featuresH4.innerHTML = "Features";
+let featureContent = document.getElementsByTagName('p')[0];
+featureContent.innerHTML = "Features content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis."
+let aboutH4 = document.getElementsByTagName('h4')[1];
+aboutH4.innerHTML = "About";
+let aboutContent = document.getElementsByTagName('p')[1];
+aboutContent.innerHTML = "About content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis."
+let servicesH4 = document.getElementsByTagName('h4')[2];
+servicesH4.innerHTML = "Services";
+let servicesContent = document.getElementsByTagName('p')[2];
+servicesContent.innerHTML = "Services content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis."
+let productH4 = document.getElementsByTagName('h4')[3];
+productH4.innerHTML = "Product"
+let productContent = document.getElementsByTagName('p')[3];
+productContent.innerHTML = "Product content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis."
+let visionH4 = document.getElementsByTagName('h4')[4];
+visionH4.innerHTML = "Vision";
+let visionContent = document.getElementsByTagName('p')[4];
+visionContent.innerHTML = "Vision content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis."
+
+//Contact
+
+let contactH4 = document.getElementsByTagName('h4')[5];
+contactH4.innerHTML = "Contact";
+let address = document.getElementsByTagName('p')[5];
+address.innerHTML = "123 Way 456 Street Somewhere, USA";
+let phone = document.getElementsByTagName('p')[6];
+phone.innerHTML = "1 (888) 888-8888";
+let email = document.getElementsByTagName('p')[7];
+email.innerHTML = "sales@greatidea.io";
+
+//footer
+let footer = document.getElementsByTagName('p')[8];
+footer.innerHTML = "Copyright Great Idea! 2018"
+
+//Changing color of nav links to green
+let navigation = document.querySelectorAll("a");
+let i;
+for (i = 0; i < navigation.length; i++) {
+  navigation[i].style.color = "green";
+}
+
+ 
