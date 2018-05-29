ecddceb80d6a4d1baf09836f8e16fc1e92ae964b
tommycam <tommycam67@gmail.com>
2018-05-29 17:07:18 -0500

    507pm cst

--- js/index.js
+++ js/index.js
@@ -37,6 +37,44 @@ const siteContent = {
   },
 };
 
-// Example: Update the img src for the logo
+//Dom Manipulation
+
+//nav
 let logo = document.getElementById("logo-img");
 logo.setAttribute('src', siteContent["nav"]["img-src"])
+
+let navCount = 1;
+document.querySelectorAll("nav a").forEach((el) => {
+  el.innerHTML = siteContent["nav"][`nav-item-${navCount}`];
+  navCount++;
+});
+
+//CTA
+document.querySelector(".cta h1").innerHTML = siteContent["cta"]["h1"];
+document.querySelector(".cta button").innerHTML = siteContent["cta"]["button"];
+
+document.getElementById("cta-img").setAttribute('src', siteContent["cta"]["img-src"]);
+
+//main content
+document.querySelectorAll(".text-content h4")[0].innerHTML = siteContent["main-content"]["features-h4"];
+document.querySelectorAll(".text-content h4")[1].innerHTML = siteContent["main-content"]["about-h4"];
+document.querySelectorAll(".text-content h4")[2].innerHTML = siteContent["main-content"]["services-h4"];
+document.querySelectorAll(".text-content h4")[3].innerHTML = siteContent["main-content"]["product-h4"];
+document.querySelectorAll(".text-content h4")[4].innerHTML = siteContent["main-content"]["vision-h4"];
+
+document.querySelectorAll(".text-content p")[0].innerHTML = siteContent["main-content"]["features-content"];
+document.querySelectorAll(".text-content p")[1].innerHTML = siteContent["main-content"]["about-content"];
+document.querySelectorAll(".text-content p")[2].innerHTML = siteContent["main-content"]["services-content"];
+document.querySelectorAll(".text-content p")[3].innerHTML = siteContent["main-content"]["product-content"];
+document.querySelectorAll(".text-content p")[4].innerHTML = siteContent["main-content"]["vision-content"];
+
+document.getElementById("middle-img").setAttribute('src', siteContent["main-content"]["middle-img-src"]);
+
+//contact
+document.querySelector(".contact h4").innerHTML = siteContent["contact"]["contact-h4"];
+document.querySelectorAll(".contact p")[0].innerHTML = siteContent["contact"]["address"];
+document.querySelectorAll(".contact p")[1].innerHTML = siteContent["contact"]["phone"];
+document.querySelectorAll(".contact p")[2].innerHTML = siteContent["contact"]["email"];
+
+//footer
+document.querySelector("footer p").innerHTML = siteContent["footer"]["copyright"];
