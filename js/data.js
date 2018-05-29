37d8774dd9f1bcad2db4d482c419b31ef903beb9
Luis <luis@Luiss-MacBook-Air.local>
2018-05-29 11:02:01 -0700

    inital commit

--- js/data.js
+++ js/data.js
@@ -1,11 +1,11 @@
-alert('we got in');
+alert('we got in')
 
 const siteContent = [
   {
-    "header": {
-      "key": "value"
+    header: {
+      key: 'value'
     }
   }
-];
+]
 
-export default siteContent;
+export default siteContent
