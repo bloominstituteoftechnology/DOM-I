5625a9a48d0844636c48a7a7c2446bfbdb3dc786
Erin Meredith <ekmeredith@att.net>
2018-05-29 19:24:09 -0400

    changed to use siteContent

--- js/index.js
+++ js/index.js
@@ -41,30 +41,18 @@ const siteContent = {
 
 
 let logo = document.getElementById("logo-img");
-logo.setAttribute("src", "img/logo.png");
-//logo.setAttribute("src", siteContent["nav"]["img-src"];
-//logo.setAttribute('src', '. /img/logo.png';
+logo.setAttribute("src", siteContent["nav"]["img-src"]);
 
 let navBar = document.querySelector("nav");
 navBar.style = "display: flex; justify-content: space-between;";
 
-//let nav1 = document.querySelector("nav a");
-//nav1.innerHTML = "Services";
-
 let nav1 = document.querySelectorAll("nav a");
-nav1[0].innerHTML = "Services";
-nav1[1].innerHTML = "Product";
-nav1[2].innerHTML = "Vision";
-nav1[3].innerHTML = "Features";
-nav1[4].innerHTML = "About";
-nav1[5].innerHTML = "Contact";
-
-
-//"cta": {
-  //  "h1": "DOM Is Awesome",
-    //"button": "Get Started",
-    //"img-src": "img/header-img.png"
-       
+nav1[0].innerHTML = siteContent["nav"]["nav-item-1"];
+nav1[1].innerHTML = siteContent["nav"]["nav-item-2"];
+nav1[2].innerHTML = siteContent["nav"]["nav-item-3"];
+nav1[3].innerHTML = siteContent["nav"]["nav-item-4"];
+nav1[4].innerHTML = siteContent["nav"]["nav-item-5"];
+nav1[5].innerHTML = siteContent["nav"]["nav-item-6"];
 
     
 let ctaText = document.querySelector(".cta-text");
@@ -72,41 +60,41 @@ ctaText.innerHTML = "DOM <br>Is <br>Awesome";
 ctaText.style = "font-family: Bangers; letter-spacing: 0px;";
 
 let ctaImg = document.getElementById("cta-img");
-ctaImg.setAttribute("src", "img/header-img.png");
+ctaImg.setAttribute("src", siteContent["cta"]["img-src"]);
 
 let middleImg = document.getElementById("middle-img");
-middleImg.setAttribute("src", "img/mid-page-accent.jpg");
+middleImg.setAttribute("src", siteContent["main-content"]["middle-img-src"]);
 
 let mainContent = document.querySelector(".main-content");
 mainContent.style = "display: flex; justify-content: space-evenly; flex-wrap: wrap; align-text: left;";
 
 let subHeads = document.querySelectorAll(".main-content  h4");
-subHeads[0].innerHTML = "Features";
-subHeads[1].innerHTML = "About";
-subHeads[2].innerHTML = "Services";
-subHeads[3].innerHTML = "Product";
-subHeads[4].innerHTML = "Vision";
+subHeads[0].innerHTML = siteContent["main-content"]["features-h4"];
+subHeads[1].innerHTML = siteContent["main-content"]["about-h4"];
+subHeads[2].innerHTML = siteContent["main-content"]["services-h4"];
+subHeads[3].innerHTML = siteContent["main-content"]["product-h4"];
+subHeads[4].innerHTML = siteContent["main-content"]["vision-h4"];
 
 let paragraphs = document.querySelectorAll(".main-content  p");
-paragraphs[0].innerHTML = "Features content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis."
-paragraphs[1].innerHTML = "About content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis."
-paragraphs[2].innerHTML = "Services content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis."
-paragraphs[3].innerHTML = "Product content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis."
-paragraphs[4].innerHTML = "Vision content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis."
+paragraphs[0].innerHTML = siteContent["main-content"]["features-content"];
+paragraphs[1].innerHTML = siteContent["main-content"]["about-content"];
+paragraphs[2].innerHTML = siteContent["main-content"]["services-content"];
+paragraphs[3].innerHTML = siteContent["main-content"]["product-content"];
+paragraphs[4].innerHTML = siteContent["main-content"]["vision-content"];
 
 let contactHead = document.querySelector(".contact  h4");
-contactHead.innerHTML = "Contact";
+contactHead.innerHTML = siteContent["contact"]["contact-h4"];
 
 let contactContent = document.querySelectorAll(".contact p");
-contactContent[0].innerHTML = "123 Way 456 Street <br>Somewhere, USA"
-contactContent[1].innerHTML = "1 (888) 888-8888"
-contactContent[2].innerHTML = "sales@greatidea.io"
+contactContent[0].innerHTML = siteContent["contact"]["address"];
+contactContent[1].innerHTML = siteContent["contact"]["phone"];
+contactContent[2].innerHTML = siteContent["contact"]["email"];
 contactContent.style = "margin-top: 5px;";
 
 let footer = document.querySelector("footer");
-footer.innerHTML = "Copyright Great Idea! 2018";
+footer.innerHTML = siteContent["footer"]["copyright"];;
 footre.style = "align-text: center; height: 50px;";
 
 
 let button = document.querySelector(".cta-text button");
-button.innerHTML = "Get Started";
+button.innerHTML = siteContent["cta"]["button"];
