327c94da6744157a55500a3529e8f89f2ed1ac3b
JacobLayton <jlayjlay3@gmail.com>
2018-05-29 12:46:31 -0700

    nav bar complete

--- js/index.js
+++ js/index.js
@@ -40,3 +40,12 @@ const siteContent = {
 // Example: Update the img src for the logo
 let logo = document.getElementById("logo-img");
 logo.setAttribute('src', siteContent["nav"]["img-src"])
+
+//--------------------NAV BAR-----------------------------------//
+let nav = document.getElementsByTagName("a");
+nav[0].innerHTML = siteContent.nav["nav-item-1"];
+nav[1].innerHTML = siteContent.nav["nav-item-2"];
+nav[2].innerHTML = siteContent.nav["nav-item-3"];
+nav[3].innerHTML = siteContent.nav["nav-item-4"];
+nav[4].innerHTML = siteContent.nav["nav-item-5"];
+nav[5].innerHTML = siteContent.nav["nav-item-6"];
