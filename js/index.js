fe69008a6ca4cdc8664bb3485426c627bef2eb48
Thomas Greenhalgh <thomasgreenhalgh@gmail.com>
2018-05-29 13:31:59 -0700

    Completes tasks 1 and 2

--- js/index.js
+++ js/index.js
@@ -1,42 +1,116 @@
 const siteContent = {
-  "nav": {
-    "nav-item-1": "Services",
-    "nav-item-2": "Product",
-    "nav-item-3": "Vision",
-    "nav-item-4": "Features",
-    "nav-item-5": "About",
-    "nav-item-6": "Contact",
-    "img-src": "img/logo.png"
+  nav: {
+    'nav-item-1': 'Services',
+    'nav-item-2': 'Product',
+    'nav-item-3': 'Vision',
+    'nav-item-4': 'Features',
+    'nav-item-5': 'About',
+    'nav-item-6': 'Contact',
+    'img-src': 'img/logo.png',
   },
-  "cta": {
-    "h1": "DOM Is Awesome",
-    "button": "Get Started",
-    "img-src": "img/header-img.png"
+  cta: {
+    h1: 'DOM Is Awesome',
+    button: 'Get Started',
+    'img-src': 'img/header-img.png',
   },
-  "main-content": {
-    "features-h4":"Features",
-    "features-content": "Features content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.",
-    "about-h4":"About",
-    "about-content": "About content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.",
-    "middle-img-src": "img/mid-page-accent.jpg",
-    "services-h4":"Services",
-    "services-content": "Services content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.",
-    "product-h4":"Product",
-    "product-content": "Product content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.",
-    "vision-h4":"Vision",
-    "vision-content": "Vision content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.",
+  'main-content': {
+    'features-h4': 'Features',
+    'features-content':
+      'Features content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.',
+    'about-h4': 'About',
+    'about-content':
+      'About content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.',
+    'middle-img-src': 'img/mid-page-accent.jpg',
+    'services-h4': 'Services',
+    'services-content':
+      'Services content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.',
+    'product-h4': 'Product',
+    'product-content':
+      'Product content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.',
+    'vision-h4': 'Vision',
+    'vision-content':
+      'Vision content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.',
   },
-  "contact": {
-    "contact-h4" : "Contact",
-    "address" : "123 Way 456 Street Somewhere, USA",
-    "phone" : "1 (888) 888-8888",
-    "email" : "sales@greatidea.io",
+  contact: {
+    'contact-h4': 'Contact',
+    address: '123 Way 456 Street Somewhere, USA',
+    phone: '1 (888) 888-8888',
+    email: 'sales@greatidea.io',
   },
-  "footer": {
-    "copyright" : "Copyright Great Idea! 2018"
+  footer: {
+    copyright: 'Copyright Great Idea! 2018',
   },
 };
 
-// Example: Update the img src for the logo
-let logo = document.getElementById("logo-img");
-logo.setAttribute('src', siteContent["nav"]["img-src"])
+// Logo
+{
+  let logo = document.getElementById('logo-img');
+  logo.setAttribute('src', siteContent.nav['img-src']);
+}
+// Nav bar
+{
+  let nav = document.querySelectorAll('nav a');
+  let navItem = 'nav-item-0';
+  for (let i = 1; i < 7; i++) {
+    navItem = navItem.slice(0, -1) + i;
+    nav[i - 1].innerHTML = siteContent['nav'][navItem];
+  }
+}
+// CTA section
+{
+  let ctaText = document.querySelector('.cta .cta-text h1');
+  ctaStr = '<br>';
+  ctaStrArr = siteContent.cta.h1.split(' ');
+  for (let i = 0; i < 3; i++) {
+    ctaStr += ctaStrArr[i] + '<br>';
+  }
+  ctaText.innerHTML = ctaStr;
+
+  let ctaButton = document.querySelector('.cta button');
+  ctaButton.innerHTML = 'Get Started';
+
+  let ctaImg = document.querySelector('#cta-img');
+  ctaImg.setAttribute('src', siteContent.cta['img-src']);
+}
+
+// Main content
+{
+  let mainContentH4s = document.querySelectorAll(
+    '.main-content .text-content h4',
+  );
+  mainContentH4s[0].innerHTML = siteContent['main-content']['features-h4'];
+  mainContentH4s[1].innerHTML = siteContent['main-content']['about-h4'];
+  mainContentH4s[2].innerHTML = siteContent['main-content']['services-h4'];
+  mainContentH4s[3].innerHTML = siteContent['main-content']['product-h4'];
+  mainContentH4s[4].innerHTML = siteContent['main-content']['vision-h4'];
+
+  let mainContentPs = document.querySelectorAll(
+    '.main-content .text-content p',
+  );
+  mainContentPs[0].innerHTML = siteContent['main-content']['features-content'];
+  mainContentPs[1].innerHTML = siteContent['main-content']['about-content'];
+  mainContentPs[2].innerHTML = siteContent['main-content']['services-content'];
+  mainContentPs[3].innerHTML = siteContent['main-content']['product-content'];
+  mainContentPs[4].innerHTML = siteContent['main-content']['vision-content'];
+
+  let middleImg = document.querySelector('#middle-img');
+  middleImg.setAttribute('src', siteContent['main-content']['middle-img-src']);
+}
+
+// Contact
+{
+  let contactH4 = document.querySelector('.contact h4');
+  contactH4.innerHTML = siteContent.contact['contact-h4'];
+
+  let contactPs = document.querySelectorAll('.contact p');
+  console.log('ps', contactPs);
+  contactPs[0].innerHTML = siteContent.contact.address;
+  contactPs[1].innerHTML = siteContent.contact.phone;
+  contactPs[2].innerHTML = siteContent.contact.email;
+}
+
+// Footer
+{
+  let footer = document.querySelector('footer p');
+  footer.innerHTML = siteContent.footer.copyright;
+}
