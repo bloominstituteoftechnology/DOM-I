ea508ef3072d83485277e4b80f81283c06b61616
Jasper <joshtietz11@gmail.com>
2018-05-29 15:33:04 -0500

    Joshua Tietz - Dom-I

--- js/index.js
+++ js/index.js
@@ -39,4 +39,48 @@ const siteContent = {
 
 // Example: Update the img src for the logo
 let logo = document.getElementById("logo-img");
-logo.setAttribute('src', siteContent["nav"]["img-src"])
+logo.setAttribute('src', siteContent["nav"]["img-src"]);
+
+let navBar = document.querySelectorAll("nav a");
+
+for (var i = 0; i < navBar.length; i++) {
+  navBar[i].innerHTML = siteContent["nav"] [`nav-item-${[1 + i]}`]
+}
+
+let headerImg = document.getElementById("cta-img");
+headerImg.setAttribute('src', siteContent["cta"]["img-src"]);
+
+let ctaText = document.querySelector(".cta h1");
+ctaText.innerHTML = siteContent["cta"] ["h1"];
+
+let ctaButton = document.querySelector(".cta button");
+ctaButton.innerHTML = siteContent["cta"] ["button"];
+
+let textHeading = document.querySelectorAll(".text-content h4");
+let textContent = document.querySelectorAll(".text-content p");
+
+  textHeading[0].innerHTML = siteContent["main-content"]["features-h4"];
+  textHeading[1].innerHTML = siteContent["main-content"]["about-h4"];
+  textHeading[2].innerHTML = siteContent["main-content"]["services-h4"];
+  textHeading[3].innerHTML = siteContent["main-content"]["product-h4"];
+  textHeading[4].innerHTML = siteContent["main-content"]["vision-h4"];
+
+  textContent[0].innerHTML = siteContent["main-content"]["features-content"];
+  textContent[1].innerHTML = siteContent["main-content"]["about-content"];
+  textContent[2].innerHTML = siteContent["main-content"]["services-content"];
+  textContent[3].innerHTML = siteContent["main-content"]["product-content"];
+  textContent[4].innerHTML = siteContent["main-content"]["vision-content"];
+
+  let midImg = document.getElementById("middle-img");
+  midImg.setAttribute('src', siteContent["main-content"]["middle-img-src"]);
+
+let contactHead = document.querySelector(".contact h4");
+let contactBody = document.querySelectorAll(".contact p");
+
+contactHead.innerHTML = siteContent["contact"] ["contact-h4"];
+contactBody[0].innerHTML = siteContent["contact"] ["address"];
+contactBody[1].innerHTML = siteContent["contact"] ["phone"];
+contactBody[2].innerHTML = siteContent["contact"] ["email"];
+
+let footer = document.querySelector("footer p");
+footer.innerHTML = siteContent["footer"] ["copyright"]
