48c3dab41d34b20d95adf196c06e096772d50ec8
Gabriel Duquette <gabrielduquette@gmail.com>
2018-05-29 16:55:28 -0400

    most of DOM-I done

--- js/index.js
+++ js/index.js
@@ -14,29 +14,87 @@ const siteContent = {
     "img-src": "img/header-img.png"
   },
   "main-content": {
-    "features-h4":"Features",
+    "features-h4": "Features",
     "features-content": "Features content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.",
-    "about-h4":"About",
+    "about-h4": "About",
     "about-content": "About content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.",
     "middle-img-src": "img/mid-page-accent.jpg",
-    "services-h4":"Services",
+    "services-h4": "Services",
     "services-content": "Services content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.",
-    "product-h4":"Product",
+    "product-h4": "Product",
     "product-content": "Product content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.",
-    "vision-h4":"Vision",
+    "vision-h4": "Vision",
     "vision-content": "Vision content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.",
   },
   "contact": {
     "contact-h4" : "Contact",
-    "address" : "123 Way 456 Street Somewhere, USA",
-    "phone" : "1 (888) 888-8888",
-    "email" : "sales@greatidea.io",
+    "address": "123 Way 456 Street Somewhere, USA",
+    "phone": "1 (888) 888-8888",
+    "email": "sales@greatidea.io",
   },
   "footer": {
-    "copyright" : "Copyright Great Idea! 2018"
+    "copyright": "Copyright Great Idea! 2018"
   },
 };
 
 // Example: Update the img src for the logo
 let logo = document.getElementById("logo-img");
 logo.setAttribute('src', siteContent["nav"]["img-src"])
+
+let nav = document.querySelector("header nav");
+
+let blog = document.createElement("a");
+let blogText = document.createTextNode("Blog");
+blog.appendChild(blogText);
+nav.appendChild(blog);
+
+let testimonials = document.createElement("a");
+let testimonialsText = document.createTextNode("Testimonials");
+testimonials.appendChild(testimonialsText);
+nav.prepend(testimonials);
+
+let navItems = document.querySelectorAll("header nav a");
+let navItem = 'nav-item-0';
+for (let i = 1; i < 7; i++) {
+  navItem = navItem.slice(0, -1) + i;
+  navItems[i - 1].innerHTML = siteContent['nav'][navItem];
+  navItems[i - 1].style.color = "green";
+}
+
+let ctaHeading = document.querySelector(".cta .cta-text h1");
+ctaHeading.innerHTML = siteContent["cta"]["h1"];
+
+let ctaButton = document.querySelector(".cta .cta-text button");
+ctaButton.innerHTML = siteContent["cta"]["button"];
+
+let ctaImg = document.getElementById("cta-img");
+ctaImg.setAttribute('src', siteContent["cta"]["img-src"]);
+
+let featuresHeading = document.querySelectorAll(".main-content h4");
+let featuresContent = document.querySelectorAll(".main-content p");
+
+featuresHeading[0].innerHTML = siteContent["main-content"]["features-h4"];
+featuresContent[0].innerHTML = siteContent["main-content"]["features-content"];
+featuresHeading[1].innerHTML = siteContent["main-content"]["about-h4"];
+featuresContent[1].innerHTML = siteContent["main-content"]["about-content"];
+
+let middleImg = document.getElementById("middle-img");
+middleImg.setAttribute('src', siteContent["main-content"]["middle-img-src"]);
+
+featuresHeading[2].innerHTML = siteContent["main-content"]["services-h4"];
+featuresContent[2].innerHTML = siteContent["main-content"]["services-content"];
+featuresHeading[3].innerHTML = siteContent["main-content"]["product-h4"];
+featuresContent[3].innerHTML = siteContent["main-content"]["product-content"];
+featuresHeading[4].innerHTML = siteContent["main-content"]["vision-h4"];
+featuresContent[4].innerHTML = siteContent["main-content"]["vision-content"];
+
+let contactHeading = document.querySelector(".contact h4");
+contactHeading.innerHTML = siteContent["contact"]["contact-h4"];
+
+let contactContent = document.querySelectorAll(".contact p");
+contactContent[0].innerHTML = siteContent["contact"]["address"];
+contactContent[1].innerHTML = siteContent["contact"]["phone"];
+contactContent[2].innerHTML = siteContent["contact"]["email"];
+
+let footer = document.querySelector("footer");
+footer.innerHTML = siteContent["footer"]["copyright"];
