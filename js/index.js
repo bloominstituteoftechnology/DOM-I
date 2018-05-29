c60b698b3044aabe7d8f48f6e2c8a124c640218a
Eric B <timberwolf.eric@gmail.com>
2018-05-29 18:42:22 -0500

    Playing with selectors in index.js

--- js/index.js
+++ js/index.js
@@ -37,6 +37,85 @@ const siteContent = {
   },
 };
 
-// Example: Update the img src for the logo
+
 let logo = document.getElementById("logo-img");
-logo.setAttribute('src', siteContent["nav"]["img-src"])
+logo.setAttribute('src', siteContent["nav"]["img-src"]);
+
+let nav = document.querySelectorAll("nav a");
+
+
+let navbar = document.querySelector("nav")
+let a1 = document.createElement("a")
+let a2 = document.createElement("a")
+navbar.appendChild(a1)
+navbar.prepend(a2)
+
+a1.innerHTML = "Portfolio"
+a2.innerHTML = "Prices"
+a1.style.color = "green"
+a2.style.color = "green"
+
+let nav1 = nav[0];
+let nav2 = nav[1];
+let nav3 = nav[2];
+let nav4 = nav[3];
+let nav5 = nav[4];
+let nav6 = nav[5];
+
+
+nav1.innerHTML = siteContent["nav"]["nav-item-1"]
+nav2.innerHTML = siteContent["nav"]["nav-item-2"]
+nav3.innerHTML = siteContent["nav"]["nav-item-3"]
+nav4.innerHTML = siteContent["nav"]["nav-item-4"]
+nav5.innerHTML = siteContent["nav"]["nav-item-5"]
+nav6.innerHTML = siteContent["nav"]["nav-item-6"]
+
+nav1.style.color = "green"
+nav2.style.color = "green"
+nav3.style.color = "green"
+nav4.style.color = "green"
+nav5.style.color = "green"
+nav6.style.color = "green"
+
+let awesomeH1 = document.querySelector(".cta-text h1");
+awesomeH1.innerHTML = siteContent["cta"]["h1"];
+
+let buttonH1 = document.querySelector(".cta-text button").textContent = siteContent["cta"]["button"]
+
+let pic2 = document.getElementById("cta-img").setAttribute('src', siteContent["cta"]["img-src"])
+
+
+let textConth4 = document.querySelectorAll(".top-content .text-content h4")
+
+textConth4[0].innerHTML = siteContent["main-content"]["features-h4"];
+textConth4[1].innerHTML = siteContent["main-content"]["about-h4"]
+
+let textContp = document.querySelectorAll(".top-content .text-content p");
+
+textContp[0].innerHTML = siteContent["main-content"]["features-content"]
+textContp[1].innerHTML = siteContent["main-content"]["about-content"]
+
+
+let pic3 = document.getElementById("middle-img").setAttribute('src', siteContent["main-content"]["middle-img-src"])
+
+let text2Conth4 = document.querySelectorAll(".bottom-content .text-content h4")
+let text2Contp = document.querySelectorAll(".bottom-content .text-content p")
+
+text2Conth4[0].innerHTML = siteContent["main-content"]["services-h4"]
+text2Conth4[1].innerHTML = siteContent["main-content"]["product-h4"]
+text2Conth4[2].innerHTML = siteContent["main-content"]["vision-h4"]
+
+text2Contp[0].innerHTML = siteContent["main-content"]["services-content"]
+text2Contp[1].innerHTML = siteContent["main-content"]["product-content"]
+text2Contp[2].innerHTML = siteContent["main-content"]["vision-content"]
+
+let contacth4 = document.querySelector(".contact h4").innerHTML = siteContent["contact"]["contact-h4"]
+
+let contactp = document.querySelectorAll(".contact p")
+
+contactp[0].innerHTML = siteContent["contact"]["address"]
+contactp[1].innerHTML = siteContent["contact"]["phone"]
+contactp[2].innerHTML = siteContent["contact"]["email"]
+
+document.querySelector("footer p").innerHTML = siteContent["footer"]["copyright"]
+
