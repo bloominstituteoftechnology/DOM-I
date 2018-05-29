f24d72945af861df4d1f4b3fed89a8f191eb7e10
Blake Fletcher <blakefletcher@Blakes-MacBook-Air.local>
2018-05-29 15:26:42 -0400

    Blake Fletcher - DOM-I
    First commit
    Added content to site with innnerHTML
    TBD: Line spacing for CTA text

--- js/index.js
+++ js/index.js
@@ -9,7 +9,7 @@ const siteContent = {
     "img-src": "img/logo.png"
   },
   "cta": {
-    "h1": "DOM Is Awesome",
+    "h1": "DOM<br> Is<br> Awesome", // why does <br> work in p but not h1?
     "button": "Get Started",
     "img-src": "img/header-img.png"
   },
@@ -40,3 +40,41 @@ const siteContent = {
 // Example: Update the img src for the logo
 let logo = document.getElementById("logo-img");
 logo.setAttribute('src', siteContent["nav"]["img-src"])
+
+let navBar = document.querySelectorAll("nav a");
+navBar[0].innerHTML="Services";
+navBar[1].innerHTML="Product";
+navBar[2].innerHTML="Vision";
+navBar[3].innerHTML="Features";
+navBar[4].innerHTML="About";
+navBar[5].innerHTML="Contact";
+
+document.querySelector("h1").innerHTML = "DOM Is Awesome";
+document.querySelector("button").innerHTML = "Get Started";
+
+document.getElementById("cta-img").src="img/header-img.png";
+
+let mainContent = document.querySelectorAll(".main-content h4");
+mainContent[0].innerHTML="Features";
+mainContent[1].innerHTML="About";
+mainContent[2].innerHTML="Services";
+mainContent[3].innerHTML="Product";
+mainContent[4].innerHTML="Vision";
+
+let mainContentP = document.querySelectorAll(".main-content p");
+mainContentP[0].innerHTML="Features content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.";
+mainContentP[1].innerHTML="About content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.";
+mainContentP[2].innerHTML="Services content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.";
+mainContentP[3].innerHTML="Product content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.";
+mainContentP[4].innerHTML="Vision content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.";
+
+document.getElementById("middle-img").src="img/mid-page-accent.jpg";
+
+document.querySelector(".contact h4").innerHTML="Contact";
+
+let contactP =document.querySelectorAll(".contact p");
+contactP[0].innerHTML="123 Way 456 Street<br>Somewhere, USA";
+contactP[1].innerHTML="1 (888) 888-8888";
+contactP[2].innerHTML="sales@greatidea.io";
+
+document.querySelector("footer").innerHTML="Copyright Great Idea! 2018";
