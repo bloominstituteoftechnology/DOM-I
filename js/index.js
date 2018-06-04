27216777374f719af20a02afd9e745de3d54dc98
meganfontenot <meganfontenot517@yahoo.com>
2018-06-04 12:44:07 -0500

    changed my code to be clearer after doing more research

--- js/index.js
+++ js/index.js
@@ -6,12 +6,10 @@ const siteContent = {
     "nav-item-4": "Features",
     "nav-item-5": "About",
     "nav-item-6": "Contact",
-    "new-nav-item-1": "",
-    "new-nav-item-2": "Last (new!)",
     "img-src": "img/logo.png"
   },
   "cta": {
-    "h1": "DOM <br> Is <br> Awesome",
+    "h1": "DOM Is Awesome",
     "button": "Get Started",
     "img-src": "img/header-img.png"
   },
@@ -30,7 +28,7 @@ const siteContent = {
   },
   "contact": {
     "contact-h4" : "Contact",
-    "address" : "123 Way 456 Street <br> Somewhere, USA",
+    "address" : "123 Way 456 Street Somewhere, USA",
     "phone" : "1 (888) 888-8888",
     "email" : "sales@greatidea.io",
   },
@@ -40,121 +38,43 @@ const siteContent = {
 };
 
 // Example: Update the img src for the logo
-let logoImg = document.getElementById("logo-img");
-logoImg.setAttribute('src', siteContent["nav"]["img-src"]);
+let logo = document.getElementById("logo-img");
+logo.setAttribute('src', siteContent["nav"]["img-src"]);
 
-document.querySelectorAll("a")[0].innerHTML = siteContent["nav"]["nav-item-1"];
-document.querySelectorAll("a")[1].innerHTML = siteContent["nav"]["nav-item-2"];
-document.querySelectorAll("a")[2].innerHTML = siteContent["nav"]["nav-item-3"];
-document.querySelectorAll("a")[3].innerHTML = siteContent["nav"]["nav-item-4"];
-document.querySelectorAll("a")[4].innerHTML = siteContent["nav"]["nav-item-5"];
-document.querySelectorAll("a")[5].innerHTML = siteContent["nav"]["nav-item-6"];
+let navBar = document.querySelectorAll("nav a");
+navBar[0].innerHTML="Services";
+navBar[1].innerHTML="Product";
+navBar[2].innerHTML="Vision";
+navBar[3].innerHTML="Features";
+navBar[4].innerHTML="About";
+navBar[5].innerHTML="Contact";
 
-document.querySelectorAll("a")[0].style.color = "green";
-document.querySelectorAll("a")[1].style.color = "green";
-document.querySelectorAll("a")[2].style.color = "green";
-document.querySelectorAll("a")[3].style.color = "green";
-document.querySelectorAll("a")[4].style.color = "green";
-document.querySelectorAll("a")[5].style.color = "green";
+document.querySelector("h1").innerHTML = "DOM Is Awesome";
+document.querySelector("button").innerHTML = "Get Started";
 
-let newNavItem = document.createElement('a');
-newNavItem.textContent = 'First (new!)';
-newNavItem.style.color = "green";
+document.getElementById("cta-img").src="img/header-img.png";
 
-let myNav = document.querySelector('nav');
-myNav.prepend(newNavItem);
+let mainContent = document.querySelectorAll(".main-content h4");
+mainContent[0].innerHTML="Features";
+mainContent[1].innerHTML="About";
+mainContent[2].innerHTML="Services";
+mainContent[3].innerHTML="Product";
+mainContent[4].innerHTML="Vision";
 
-let newNavItem2 = document.createElement(`a`);
-newNavItem2.textContent = 'Last (new!)';
-newNavItem2.style.color = "green";
+let mainContentP = document.querySelectorAll(".main-content p");
+mainContentP[0].innerHTML="Features content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.";
+mainContentP[1].innerHTML="About content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.";
+mainContentP[2].innerHTML="Services content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.";
+mainContentP[3].innerHTML="Product content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.";
+mainContentP[4].innerHTML="Vision content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.";
 
-let myNav2 = document.querySelector('nav');
-myNav2.appendChild(newNavItem2);
-//Memo: Refactor this later to be less repetitive, please!
+document.getElementById("middle-img").src="img/mid-page-accent.jpg";
 
+document.querySelector(".contact h4").innerHTML="Contact";
 
-//nav
+let contactP =document.querySelectorAll(".contact p");
+contactP[0].innerHTML="123 Way 456 Street Somewhere, USA";
+contactP[1].innerHTML="1 (888) 888-8888";
+contactP[2].innerHTML="sales@greatidea.io";
 
-document.querySelector(".cta-text h1").innerHTML = siteContent["cta"]["h1"];
-document.querySelector(".cta-text h1").style.textAlign = "center";
-
-document.querySelector(".cta-text button").innerHTML = siteContent["cta"]["button"];
-
-let ctaImg = document.getElementById("cta-img");
-ctaImg.setAttribute('src', siteContent["cta"]["img-src"]);
-//cta
-
-document.querySelectorAll(".top-content .text-content h4")[0].innerHTML = siteContent["main-content"]["features-h4"];
-document.querySelectorAll(".top-content .text-content p")[0].innerHTML = siteContent["main-content"]["features-content"];
-
-document.querySelectorAll(".top-content .text-content h4")[1].innerHTML = siteContent["main-content"]["about-h4"];
-document.querySelectorAll(".top-content .text-content p")[1].innerHTML = siteContent["main-content"]["about-content"];
-
-document.querySelectorAll(".bottom-content .text-content h4")[0].innerHTML = siteContent["main-content"]["services-h4"];
-document.querySelectorAll(".bottom-content .text-content p")[0].innerHTML = siteContent["main-content"]["services-content"];
-
-document.querySelectorAll(".bottom-content .text-content h4")[1].innerHTML = siteContent["main-content"]["product-h4"];
-document.querySelectorAll(".bottom-content .text-content p")[1].innerHTML = siteContent["main-content"]["product-content"];
-
-document.querySelectorAll(".bottom-content .text-content h4")[2].innerHTML = siteContent["main-content"]["vision-h4"];
-document.querySelectorAll(".bottom-content .text-content p")[2].innerHTML = siteContent["main-content"]["vision-content"];
-
-let middleImg = document.getElementById("middle-img");
-middleImg.setAttribute('src', siteContent["main-content"]["middle-img-src"]);
-//main-content
-
-document.querySelector(".contact h4").innerHTML = siteContent["contact"]["contact-h4"];
-document.querySelectorAll(".contact p")[0].innerHTML = siteContent["contact"]["address"];
-document.querySelectorAll(".contact p")[1].innerHTML = siteContent["contact"]["phone"];
-document.querySelectorAll(".contact p")[2].innerHTML = siteContent["contact"]["email"];
-//contact
-
-document.querySelector("footer p").innerHTML = siteContent["footer"]["copyright"];
-//footer
-
-/* ASSIGNMENT II STARTS HERE */
-
-let productHeader = document.querySelector(".top-content");
-
-productHeader.addEventListener(`mouseover`, (event) => {
-  productHeader.style.color = "purple";
-});
-
-productHeader.addEventListener(`mouseleave`, (event) => {
-  productHeader.style.color = "black";
-});
-
-document.querySelector(".cta-text button").addEventListener('click', (event) => {
-  window.alert('Are you sure that you are ready to get started?');
-});
-
-document.addEventListener('wheel', (event) => {
-  document.querySelector(".cta button").style.backgroundColor = "red";
-});
-
-document.addEventListener('keydown', (event) => {
-  let keystrokeDiv = document.createElement('div');
-  keystrokeDiv.textContent = 'You pressed a key on your keyboard!';
-  keystrokeDiv.style.textAlign = "center";
-  document.querySelector(".main-content").appendChild(keystrokeDiv);
-});
-
-document.addEventListener('auxclick', (event) => {
-  document.querySelector('#cta-img').style.backgroundColor = "green";
-});
-
-document.addEventListener('dblclick', (event) => {
-  document.querySelector('#logo-img').style.backgroundColor = "blue";
-});
-
-document.addEventListener('contextmenu', (event) => {
-  window.alert(`Hey, I see you bringing up that context menu on this page.`)
-});
-
-document.querySelector('#middle-img').addEventListener('drag', (event) => {
-  document.querySelector('.main-content').style.backgroundColor = "gray";
-})
-
-document.querySelector('#middle-img').addEventListener('dragend', (event) => {
-  document.querySelector('.main-content').style.backgroundColor = "white";
-})
+document.querySelector("footer").innerHTML="Copyright Great Idea! 2018";
