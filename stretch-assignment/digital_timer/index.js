c120ec91025c797e297436f05527535ac7366acf
Lauren <llwor94@gmail.com>
2018-07-09 13:47:54 -0700

    mstens goes up to 9 and then stops

--- stretch-assignment/digital_timer/index.js
+++ stretch-assignment/digital_timer/index.js
@@ -3,10 +3,22 @@ let msTens = document.getElementById("msTens");
 let msHundreds = document.getElementById("msHundreds");
 let ones = document.getElementById("secondOnes");
 let tens = document.getElementById("secondTens");
+let msTensCount = 0;
+let msHundredsCount = 0;
+tens.innerHTML = 0;
+ones.innerHTML = 0;
+msHundreds.innerHTML = 0;
+msTens.innerHTML = 0;
 
 function increment() {
-  if 
+  msTensCount = msTensCount + 1;
+  if (msTens.innerHTML <= 8) {
+    msTens.innerHTML = msTensCount;
+  }
+  // } else {
+  //   msTens.innerHTML = 0;
+  //   msHundreds.innerHTML 
+  // }
+  console.log(`time: ${time} msTens: ${msTens.innerHTML}`);
 }
-setInterval(function() {
-
-}, 10);
+setInterval(increment, 10);
