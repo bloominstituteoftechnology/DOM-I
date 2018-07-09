3920d62bef8adda70dbe4925e3bde48fa3a79d7e
Jurgen Kela <jurgenkela@Jurgens-MacBook-Pro.local>
2018-07-09 16:18:55 -0400

    updated and selected

--- js/index.js
+++ js/index.js
@@ -38,5 +38,26 @@ const siteContent = {
 };
 
 // Example: Update the img src for the logo
+
+//  =========  Top Nav ==========
 let logo = document.getElementById("logo-img");
 logo.setAttribute('src', siteContent["nav"]["img-src"])
+
+
+let navItem1 = document.getElementsByTagName("a");
+navItem1[0].innerHTML = siteContent["nav"]["nav-item-1"];
+navItem1[1].innerHTML = siteContent["nav"]["nav-item-2"];
+navItem1[2].innerHTML = siteContent["nav"]["nav-item-3"];
+navItem1[3].innerHTML = siteContent["nav"]["nav-item-4"];
+navItem1[4].innerHTML = siteContent["nav"]["nav-item-5"];
+navItem1[5].innerHTML = siteContent["nav"]["nav-item-6"];
+
+
+// =========== CTA ==========
+let ctaH1 = document.querySelector(".cta .cta-text h1");
+let ctaButton = document.querySelector(".cta .cta-text button");
+let ctaImg = document.getElementById("cta-img");
+
+ctaH1.innerHTML = siteContent["cta"]["h1"];
+ctaButton.innerHTML = siteContent["cta"]["button"];
+ctaImg.setAttribute("src", siteContent["cta"]["img-src"]);
