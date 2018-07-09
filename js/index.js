656a6ec3a50ed3dae838200247bacfe71c4dd11e
Sean <sean47900@gmail.com>
2018-07-09 15:41:00 -0500

    images

--- js/index.js
+++ js/index.js
@@ -40,3 +40,9 @@ const siteContent = {
 // Example: Update the img src for the logo
 let logo = document.getElementById("logo-img");
 logo.setAttribute('src', siteContent["nav"]["img-src"])
+
+let image = document.querySelectorAll("img");
+image[0].setAttribute('src', siteContent["nav"]["img-src"]);
+image[1].setAttribute('src', siteContent["cta"]["img-src"]);
+image[2].setAttribute('src', siteContent["main-content"]["middle-img-src"]);
+
