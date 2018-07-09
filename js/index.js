ae6439f616b91a866480fdf41edab029c6d547fd
Naaz Shaikh <Naaz@Naazs-MacBook-Pro.local>
2018-07-09 13:21:54 -0700

    init commit

--- js/index.js
+++ js/index.js
@@ -40,3 +40,7 @@ const siteContent = {
 // Example: Update the img src for the logo
 let logo = document.getElementById("logo-img");
 logo.setAttribute('src', siteContent["nav"]["img-src"])
+
+let navlink = document.querySelector("nav a");
+navLinks.innerHTML('')
+
