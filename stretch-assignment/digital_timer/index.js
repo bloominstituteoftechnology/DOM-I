b13f9cfffef58edf1ab8bff020525da3fd047805
Lauren <llwor94@gmail.com>
2018-07-09 13:17:29 -0700

    added selectors for stretch assignment

---
+++ stretch-assignment/digital_timer/index.js
@@ -0,0 +1,12 @@
+let digits = document.querySelectorAll(".digits .digit");
+let msTens = document.getElementById("msTens");
+let msHundreds = document.getElementById("msHundreds");
+let ones = document.getElementById("secondOnes");
+let tens = document.getElementById("secondTens");
+
+function increment() {
+  if 
+}
+setInterval(function() {
+
+}, 10);
