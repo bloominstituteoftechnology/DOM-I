eca747547cf97ff145cf18382eb41040e9a02502
Trevor <trevorfehrman@gmail.com>
2018-07-09 14:34:26 -0700

    DOM manipulation Loop refactor and stretch assignment styling

--- js/index.js
+++ js/index.js
@@ -1,60 +1,62 @@
 const siteContent = {
-  "nav": {
-    "nav-item-1": "Services",
-    "nav-item-2": "Product",
-    "nav-item-3": "Vision",
-    "nav-item-4": "Features",
-    "nav-item-5": "About",
-    "nav-item-6": "Contact",
-    "img-src": "img/logo.png"
-  },
+	nav: {
+		"nav-item-1": "Services",
+		"nav-item-2": "Product",
+		"nav-item-3": "Vision",
+		"nav-item-4": "Features",
+		"nav-item-5": "About",
+		"nav-item-6": "Contact",
+		"img-src": "img/logo.png"
+	},
 
-  "cta": {
-    "h1": "DOM Is Awesome",
-    "button": "Get Started",
-    "img-src": "img/header-img.png"
-  },
-  "main-content": {
-    "features-h4":"Features",
-    "features-content": "Features content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.",
-    "about-h4":"About",
-    "about-content": "About content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.",
-    "middle-img-src": "img/mid-page-accent.jpg",
-    "services-h4":"Services",
-    "services-content": "Services content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.",
-    "product-h4":"Product",
-    "product-content": "Product content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.",
-    "vision-h4":"Vision",
-    "vision-content": "Vision content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.",
-  },
-  "contact": {
-    "contact-h4" : "Contact",
-    "address" : "123 Way 456 Street Somewhere, USA",
-    "phone" : "1 (888) 888-8888",
-    "email" : "sales@greatidea.io",
-  },
-  "footer": {
-    "copyright" : "Copyright Great Idea! 2018"
-  },
+	cta: {
+		h1: "DOM Is Awesome",
+		button: "Get Started",
+		"img-src": "img/header-img.png"
+	},
+	"main-content": {
+		"features-h4": "Features",
+		"features-content":
+			"Features content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.",
+		"about-h4": "About",
+		"about-content":
+			"About content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.",
+		"middle-img-src": "img/mid-page-accent.jpg",
+		"services-h4": "Services",
+		"services-content":
+			"Services content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.",
+		"product-h4": "Product",
+		"product-content":
+			"Product content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.",
+		"vision-h4": "Vision",
+		"vision-content":
+			"Vision content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis."
+	},
+	contact: {
+		"contact-h4": "Contact",
+		address: "123 Way 456 Street Somewhere, USA",
+		phone: "1 (888) 888-8888",
+		email: "sales@greatidea.io"
+	},
+	footer: {
+		copyright: "Copyright Great Idea! 2018"
+	}
 };
 
 // Example: Update the img src for the logo
 let logo = document.getElementById("logo-img");
-logo.setAttribute('src', siteContent["nav"]["img-src"])
+logo.setAttribute("src", siteContent["nav"]["img-src"]);
 
 let ctaimg = document.getElementById("cta-img");
-ctaimg.setAttribute('src', siteContent["cta"]["img-src"])
-
-
-
+ctaimg.setAttribute("src", siteContent["cta"]["img-src"]);
 
 let navKeyArray = Object.keys(siteContent.nav);
 
-let nav = document.getElementsByTagName('nav')[0];
+let nav = document.getElementsByTagName("nav")[0];
 let anchorList = nav.children;
 
 for (i = 0; i < anchorList.length; i++) {
-  anchorList[i].textContent = siteContent.nav[navKeyArray[i]];
+	anchorList[i].textContent = siteContent.nav[navKeyArray[i]];
 }
 
 var a = document.createElement("a");
@@ -71,33 +73,26 @@ let ctaText = document.getElementsByTagName("h1")[0];
 ctaText.textContent = siteContent["cta"]["h1"];
 
 let buttonText = document.getElementsByTagName("button")[0];
-buttonText.textContent = siteContent["cta"]['button'];
+buttonText.textContent = siteContent["cta"]["button"];
 
-let contentHeaders = document.querySelectorAll('h4');
+let mainArray = ["features", "about", "services", "product", "vision"];
+document.querySelectorAll(".text-content").forEach((element, index) => {
+	let key = mainArray[index]; // features, about, services ...
+	let h4TextKey = key + "-h4"; // features-h4, about-h4k
+	let pTextKey = key + "-content";
+	element.querySelector("h4").textContent =
+		siteContent["main-content"][h4TextKey];
+	element.querySelector("p").textContent =
+		siteContent["main-content"][pTextKey];
+});
 
-contentHeaders[0].textContent = siteContent["main-content"]["features-h4"];
-contentHeaders[1].textContent = siteContent["main-content"]["about-h4"];
-contentHeaders[2].textContent = siteContent["main-content"]["services-h4"];
-contentHeaders[3].textContent = siteContent["main-content"]["product-h4"];
-contentHeaders[4].textContent = siteContent["main-content"]["vision-h4"];
-contentHeaders[5].textContent = siteContent["contact"]["contact-h4"];
-
-let contentText = document.querySelectorAll('p');
-
-contentText[0].textContent = siteContent["main-content"]["features-content"];
-contentText[1].textContent = siteContent["main-content"]["about-content"];
-contentText[2].textContent = siteContent["main-content"]["services-content"];
-contentText[3].textContent = siteContent["main-content"]["product-content"];
-contentText[4].textContent = siteContent["main-content"]["vision-content"];
-
-let middleImage = document.querySelector('#middle-img');
-middleImage.setAttribute('src', siteContent["main-content"]["middle-img-src"]);
+let middleImage = document.querySelector("#middle-img");
+middleImage.setAttribute("src", siteContent["main-content"]["middle-img-src"]);
 
 let contactP = document.querySelectorAll(".contact p");
 contactP[0].textContent = siteContent["contact"]["address"];
 contactP[1].textContent = siteContent["contact"]["phone"];
 contactP[2].textContent = siteContent["contact"]["email"];
-
 
 let footerP = document.querySelector("footer p");
 footerP.textContent = siteContent["footer"]["copyright"];
--- stretch-assignment/digital_timer/index.js
+++ stretch-assignment/digital_timer/index.js
@@ -1,57 +1,119 @@
-//**Pointing to Divs*/
+// //**Pointing to Divs*/
 
-let theTensPlace = document.querySelector("#secondTens");  
-let theOnesPlace = document.querySelector("#secondOnes");
-let tenths = document.querySelector("#msHundreds");
-let hundreths = document.querySelector("#msTens");
+// let theTensPlace = document.querySelector("#secondTens");
+// let theOnesPlace = document.querySelector("#secondOnes");
+// let tenths = document.querySelector("#msHundreds");
+// let hundreths = document.querySelector("#msTens");
 
-let startTime; //Need to declare globaly so local scopes have access.
+// let startTime; //Need to declare globaly so local scopes have access.
 
-//**Gets a duration measurement, converts that measurement to 4 sperate digits, puts them in the place they belong*/
+// //**Gets a duration measurement, converts that measurement to 4 sperate digits, puts them in the place they belong*/
 
-const timeCheck = function (maxTime) {
-    let endTime = new Date().getTime();
-    let duration = endTime - startTime;
-    if (duration > maxTime) {
-        clearInterval(timeLooper);
-        document.querySelector(".digits").style.color = "red";
-        document.getElementById("start-button").removeAttribute('disabled');
-        return;
-    }
-    theTensPlace.textContent = Math.floor(duration / 10000);
-    theOnesPlace.textContent = Math.floor(duration / 1000) % 10;
-    tenths.textContent = Math.floor(duration / 100) % 10;
-    hundreths.textContent = Math.floor(duration / 10) % 10;
+// const timeCheck = function (maxTime) {
+//     let endTime = new Date().getTime();
+//     let duration = endTime - startTime;
+//     if (duration > maxTime) {
+//         clearInterval(timeLooper);
+//         document.querySelector(".digits").style.color = "red";
+//         document.getElementById("start-button").removeAttribute('disabled');
+//         return;
+//     }
+//     theTensPlace.textContent = Math.floor(duration / 10000);
+//     theOnesPlace.textContent = Math.floor(duration / 1000) % 10;
+//     tenths.textContent = Math.floor(duration / 100) % 10;
+//     hundreths.textContent = Math.floor(duration / 10) % 10;
+// }
+
+// let timeLooper; //Need to declare globaly so local scopes have access.
+
+// //*Button Functions*/
+
+// //The start button function begins the measurement that timecheck() ends.
+// const startButtonFunction = function() {
+//     timeLooper = setInterval(timeCheck, 10, 3009); //TODO the `09` is because of CMOS off by one error.  There's probably a smarter way to do this.
+//     startTime = new Date().getTime();
+//     document.getElementById("start-button").setAttribute('disabled', 'disabled');
+//     document.querySelector(".digits").style.color = "black";
+// }
+
+// const resetButtonFunction = function() {
+//     theTensPlace.textContent = 0;
+//     theOnesPlace.textContent = 0;
+//     tenths.textContent = 0;
+//     hundreths.textContent = 0;
+//     document.querySelector(".digits").style.color = "black";
+// }
+
+// const stopButtonFunction = function() {
+//     clearInterval(timeLooper);
+//     document.getElementById("start-button").removeAttribute('disabled');
+// }
+
+// //*Event Listeners*/
+
+// document.getElementById("start-button").addEventListener("click", startButtonFunction);
+// document.getElementById("reset-button").addEventListener("click", resetButtonFunction);
+// document.getElementById("stop-button").addEventListener("click", stopButtonFunction);
+
+const secondsRest = document.getElementById("secondTens");
+const secondOnes = document.getElementById("secondOnes");
+const msHundreds = document.getElementById("msHundreds");
+const msTens = document.getElementById("msTens");
+const startBtn = document.getElementById("start-btn");
+const resetBtn = document.getElementById("reset-btn");
+const stopBtn = document.getElementById("stop-btn");
+
+const state = {
+	startTime: null,
+	paused: false,
+	pausedAt: 0,
+	intervalHandle: null,
+	elapsed: 0
+};
+
+function updateDisplay() {
+	const elapsed = state.elapsed;
+	msTens.textContent = Math.floor(elapsed / 10) % 10;
+	msHundreds.textContent = Math.floor(elapsed / 100) % 10;
+	secondOnes.textContent = Math.floor(elapsed / 1000) % 10;
+	secondsRest.textContent = Math.floor(elapsed / 10000);
 }
 
-let timeLooper; //Need to declare globaly so local scopes have access.
+function kickOffTimer(duration) {
+	if (state.intervalHandle) clearInterval(state.intervalHandle);
+	if (state.paused) {
+		state.paused = false;
+		duration = duration - state.elapsed;
+	}
+	state.startTime = Date.now();
+	state.intervalHandle = setInterval(() => {
+		const currentTime = Date.now();
+		const elapsed = currentTime - state.startTime;
 
-//*Button Functions*/
+		if (elapsed > duration) {
+			clearInterval(state.intervalHandle);
+			state.elapsed = 0;
+			state.pausedAt = 0;
+			startBtn.textContent = "Start";
+			return;
+		}
+		state.elapsed = elapsed + state.pausedAt;
 
-//The start button function begins the measurement that timecheck() ends.
-const startButtonFunction = function() {
-    timeLooper = setInterval(timeCheck, 10, 3009); //TODO the `09` is because of CMOS off by one error.  There's probably a smarter way to do this.
-    startTime = new Date().getTime();
-    document.getElementById("start-button").setAttribute('disabled', 'disabled');
-    document.querySelector(".digits").style.color = "black";
+		updateDisplay();
+	}, 10);
 }
 
-const resetButtonFunction = function() {
-    theTensPlace.textContent = 0;
-    theOnesPlace.textContent = 0;
-    tenths.textContent = 0;
-    hundreths.textContent = 0;
-    document.querySelector(".digits").style.color = "black";
-}
-
-const stopButtonFunction = function() {
-    clearInterval(timeLooper);
-    document.getElementById("start-button").removeAttribute('disabled');
-}
-
-//*Event Listeners*/
-
-document.getElementById("start-button").addEventListener("click", startButtonFunction);
-document.getElementById("reset-button").addEventListener("click", resetButtonFunction);
-document.getElementById("stop-button").addEventListener("click", stopButtonFunction);
-
+startBtn.addEventListener("click", () => kickOffTimer(10009));
+resetBtn.addEventListener("click", () => {
+	clearInterval(state.intervalHandle);
+	state.elapsed = 0;
+	state.pausedAt = 0;
+	startBtn.textContent = "Start";
+	updateDisplay();
+});
+stopBtn.addEventListener("click", () => {
+	clearInterval(state.intervalHandle);
+	state.paused = true;
+	startBtn.textContent = "Resume";
+	state.pausedAt = state.elapsed;
+});
