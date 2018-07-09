ce864a0674efa0cd05205af044e359c95aaa1ef1
Lauren <llwor94@gmail.com>
2018-07-09 14:06:59 -0700

    all numbers increase

--- stretch-assignment/digital_timer/index.js
+++ stretch-assignment/digital_timer/index.js
@@ -5,20 +5,36 @@ let ones = document.getElementById("secondOnes");
 let tens = document.getElementById("secondTens");
 let msTensCount = 0;
 let msHundredsCount = 0;
-tens.innerHTML = 0;
-ones.innerHTML = 0;
-msHundreds.innerHTML = 0;
-msTens.innerHTML = 0;
+let onesCount = 0;
+let tensCount = 0;
+// tens.innerHTML = 0;
+// ones.innerHTML = 0;
+// msHundreds.innerHTML = 0;
+// msTens.innerHTML = 0;
 
 function increment() {
-  msTensCount = msTensCount + 1;
-  if (msTens.innerHTML <= 8) {
-    msTens.innerHTML = msTensCount;
+  if (msTensCount <= 8) {
+    msTensCount += 1;
+  } else if (msHundredsCount <= 8) {
+    msTensCount = 0;
+    msHundredsCount += 1;
+  } else if (onesCount <= 8) {
+    msTensCount = 0;
+    msHundredsCount = 0;
+    onesCount += 1;
+  } else {
+    msTensCount = 0;
+    msHundredsCount = 0;
+    onesCount = 0;
+    tensCount = 1;
+    clearInterval;
+    // digits.style.color = "red";
   }
-  // } else {
-  //   msTens.innerHTML = 0;
-  //   msHundreds.innerHTML 
-  // }
-  console.log(`time: ${time} msTens: ${msTens.innerHTML}`);
+
+  msTens.innerHTML = msTensCount;
+  msHundreds.innerHTML = msHundredsCount;
+  ones.innerHTML = onesCount;
+  tens.innerHTML = tensCount;
+  console.log(`msTensCount: ${msTensCount} msTens: ${msTens.innerHTML}`);
 }
 setInterval(increment, 10);
