3992a8a1043a5a2fca824678943f2418743a7b6b
rodacoram <rodacoram8@gmail.com>
2018-07-09 17:25:42 -0600

    Rodrigo Acosta-Ramos
    Task 1 and 2

--- js/index.js
+++ js/index.js
@@ -39,4 +39,36 @@ const siteContent = {
 
 // Example: Update the img src for the logo
 let logo = document.getElementById("logo-img");
-logo.setAttribute('src', siteContent["nav"]["img-src"])
+logo.setAttribute('src', siteContent["nav"]["img-src"]);
+
+let navbar = document.querySelector("nav");
+  navbar.innerHTML = `<a>${siteContent["nav"]["nav-item-1"]}</a><a>${siteContent["nav"]["nav-item-2"]}</a><a>${siteContent["nav"]["nav-item-3"]}</a><a>${siteContent["nav"]["nav-item-4"]}</a><a>${siteContent["nav"]["nav-item-5"]}</a><a>${siteContent["nav"]["nav-item-6"]}</a>`;
+
+let heading = document.querySelector(".cta-text");
+heading.innerHTML = `<h1>${siteContent["cta"]["h1"]}</h1><button>${siteContent["cta"]["button"]}</button>`;
+
+let banner = document.getElementById("cta-img");
+banner.setAttribute('src', siteContent["cta"]["img-src"]);
+
+let headTags = document.querySelectorAll(".text-content h4");
+headTags[0].innerHTML = `<h4>${siteContent["main-content"]["features-h4"]}</h4>`;
+headTags[1].innerHTML = `<h4>${siteContent["main-content"]["about-h4"]}</h4>`;
+headTags[2].innerHTML = `<h4>${siteContent["main-content"]["services-h4"]}</h4>`;
+headTags[3].innerHTML = `<h4>${siteContent["main-content"]["product-h4"]}</h4>`;
+headTags[4].innerHTML = `<h4>${siteContent["main-content"]["vision-h4"]}</h4>`;
+
+let pTags = document.querySelectorAll(".text-content p");
+pTags[0].innerHTML = `<p>${siteContent["main-content"]["features-content"]}</p>`;
+pTags[1].innerHTML = `<p>${siteContent["main-content"]["about-content"]}</p>`;
+pTags[2].innerHTML = `<p>${siteContent["main-content"]["services-content"]}</p>`;
+pTags[3].innerHTML = `<p>${siteContent["main-content"]["product-content"]}</p>`;
+pTags[4].innerHTML = `<p>${siteContent["main-content"]["vision-content"]}</p>`;
+
+let midImg = document.getElementById("middle-img");
+midImg.setAttribute('src', siteContent["main-content"]["middle-img-src"]);
+
+let contactInfo = document.querySelector(".contact");
+contactInfo.innerHTML = `<h4>${siteContent["contact"]["contact-h4"]}</h4><p>${siteContent["contact"]["address"]}</p><p>${siteContent["contact"]["phone"]}</p><p>${siteContent["contact"]["email"]}</p>`;
+
+let footer = document.querySelector("footer");
+footer.innerHTML = `<p>${siteContent["footer"]["copyright"]}</p>`;
