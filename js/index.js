e4729fa9d59ed5fcbbfe70d57e5fcecfe59f378b
“Kennedykid1995” <34872885+Kennedykid1995@users.noreply.github.com>
2018-07-09 15:22:38 -0500

    Dom 1 images
    Images are in, now moving onto the navbar

--- js/index.js
+++ js/index.js
@@ -40,4 +40,9 @@ const siteContent = {
 // Example: Update the img src for the logo
 let logo = document.getElementById("logo-img");
 logo.setAttribute('src', siteContent["nav"]["img-src"]);
- 
+
+let image = document.getElementById("cta-img"); 
+image.setAttribute('src', siteContent["cta"]["img-src"]);
+
+let imageMiddle = document.getElementById("middle-img"); 
+imageMiddle.setAttribute('src', siteContent["main-content"]["middle-img-src"]);
