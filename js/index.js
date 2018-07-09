d6f55dcdbea8533526ba268a104605f2d7d60d24
LizBaker <lizbaker.pdx@gmail.com>
2018-07-09 14:35:23 -0700

    Did a whole lot of stuff without commits -manipulated dom for the nav, top content, middle content, and bottom -now working on the contact section

--- js/index.js
+++ js/index.js
@@ -39,4 +39,68 @@ const siteContent = {
 
 // Example: Update the img src for the logo
 let logo = document.getElementById("logo-img");
-logo.setAttribute('src', siteContent["nav"]["img-src"])
+logo.setAttribute('src', siteContent["nav"]["img-src"]);
+
+
+let nav = document.querySelectorAll('nav a');
+nav[0].innerHTML = (siteContent["nav"]["nav-item-1"]);
+
+let nav1 = document.querySelectorAll('nav a');
+nav1[1].innerHTML = (siteContent["nav"]["nav-item-2"]);
+
+let nav2 = document.querySelectorAll('nav a');
+nav2[2].innerHTML = (siteContent["nav"]["nav-item-3"]);
+
+let nav3 = document.querySelectorAll('nav a');
+nav3[3].innerHTML = (siteContent["nav"]["nav-item-4"]);
+
+let nav4 = document.querySelectorAll('nav a');
+nav4[4].innerHTML = (siteContent["nav"]["nav-item-5"]);
+
+let nav5 = document.querySelectorAll('nav a');
+nav5[5].innerHTML = (siteContent["nav"]["nav-item-6"]);
+
+
+let cta = document.getElementById("cta-img");
+cta.setAttribute('src', siteContent["cta"]["img-src"]);
+
+let ctaText = document.querySelector(".cta-text h1");
+ctaText.innerHTML = (siteContent["cta"]["h1"]);
+
+let ctaBtn = document.querySelector(".cta-text button");
+ctaBtn.innerHTML = (siteContent["cta"]["button"]);
+
+
+let middle = document.getElementById("middle-img");
+middle.setAttribute('src', siteContent["main-content"]["middle-img-src"]);
+
+let mainText01 = document.querySelectorAll('.top-content .text-content h4');
+mainText01[0].innerHTML = (siteContent["main-content"]["features-h4"]);
+
+let mainText1 = document.querySelectorAll('.top-content .text-content p');
+mainText1[0].innerHTML = (siteContent["main-content"]["features-content"]);
+
+let mainText02 = document.querySelectorAll('.top-content .text-content h4');
+mainText02[1].innerHTML = (siteContent["main-content"]["about-h4"]);
+
+let mainText2 = document.querySelectorAll('.top-content .text-content p');
+mainText2[1].innerHTML = (siteContent["main-content"]["about-content"]);
+
+
+let mainText03 = document.querySelectorAll('.bottom-content .text-content h4');
+mainText03[0].innerHTML = (siteContent["main-content"]["services-h4"]);
+
+let mainText3 = document.querySelectorAll('.bottom-content .text-content p');
+mainText3[0].innerHTML = (siteContent["main-content"]["services-content"]);
+
+let mainText04 = document.querySelectorAll('.bottom-content .text-content h4');
+mainText04[1].innerHTML = (siteContent["main-content"]["product-h4"]);
+
+let mainText4 = document.querySelectorAll('.bottom-content .text-content p');
+mainText4[1].innerHTML = (siteContent["main-content"]["product-content"]);
+
+let mainText05 = document.querySelectorAll('.bottom-content .text-content h4');
+mainText05[2].innerHTML = (siteContent["main-content"]["vision-h4"]);
+
+let mainText5 = document.querySelectorAll('.bottom-content .text-content p');
+mainText5[2].innerHTML = (siteContent["main-content"]["vision-content"]);
