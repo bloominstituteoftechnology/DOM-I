ce452123edaed233bbf812b2996695e7f7d69940
Joseph Stossmeister <joseph.stossmeister@gmail.com>
2018-07-09 16:09:50 -0400

    Main finished -appended a new contact p -created a new div, prepended an h4 and p to it -prepended new div to top-content

--- js/index.js
+++ js/index.js
@@ -9,7 +9,7 @@ const siteContent = {
     "img-src": "img/logo.png"
   },
   "cta": {
-    "h1": "DOM Is Awesome",
+    "h1": "DOM <br>Is <br>Awesome",
     "button": "Get Started",
     "img-src": "img/header-img.png"
   },
@@ -28,7 +28,7 @@ const siteContent = {
   },
   "contact": {
     "contact-h4" : "Contact",
-    "address" : "123 Way 456 Street Somewhere, USA",
+    "address" : "123 Way 456 Street<br> Somewhere, USA",
     "phone" : "1 (888) 888-8888",
     "email" : "sales@greatidea.io",
   },
@@ -44,11 +44,12 @@ logo.setAttribute('src', siteContent["nav"]["img-src"]);
 //my stuff
 //navs
 let navLinks = document.querySelectorAll('header nav a');
+navLinks.forEach(nav => nav.style.color = 'green');
 navLinks.forEach((nav, pos) => nav.innerText = siteContent.nav[`nav-item-${pos+1}`]);
 
 //cta
 let ctaBox = document.querySelector('.cta .cta-text');
-ctaBox.querySelector('h1').innerText = siteContent.cta.h1;
+ctaBox.querySelector('h1').innerHTML = siteContent.cta.h1;
 ctaBox.querySelector('button').innerText = siteContent.cta.button;
 
 let ctaImg = document.getElementById('cta-img');
@@ -61,6 +62,22 @@ topContentBoxes[0].querySelector('h4').innerText = siteContent['main-content']['
 topContentBoxes[0].querySelector('p').innerText = siteContent['main-content']['features-content'];
 topContentBoxes[1].querySelector('h4').innerText = siteContent['main-content']['about-h4'];
 topContentBoxes[1].querySelector('p').innerText = siteContent['main-content']['about-content'];
+
+//create a new div, give it the class .text-content, create a header and paragraph, set their text,
+//give new paragraph some padding, append header and paragrah to new div, prepend div to .top-content
+let newTextBox = document.createElement('div');
+newTextBox.className = 'text-content';
+let newTextBoxHeader = document.createElement('h4');
+let newTextBoxPara = document.createElement('p');
+newTextBoxHeader.innerText = 'Sample';
+newTextBoxPara.innerText = 'Sample content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.';
+newTextBoxPara.style.paddingRight = '20px';
+newTextBox.append(newTextBoxHeader);
+newTextBox.append(newTextBoxPara);
+document.querySelector('.main-content .top-content').prepend(newTextBox);
+
+//give the other text-content p's some padding as well.
+topContentBoxes.forEach(box => box.querySelector('p').style.paddingRight = '20px');
 
 
 let middleImg = document.getElementById('middle-img');
@@ -79,10 +96,14 @@ bottomContentBoxes[2].querySelector('p').innerText = siteContent['main-content']
 let contactEl = document.querySelector('.contact');
 contactEl.querySelector('h4').innerText = siteContent['contact']['contact-h4'];
 let contactParas = contactEl.querySelectorAll('p');
-contactParas[0].innerText = siteContent['contact']['address'];
+contactParas[0].innerHTML = siteContent['contact']['address'];
 contactParas[1].innerText = siteContent['contact']['phone'];
 contactParas[2].innerText = siteContent['contact']['email'];
 
 //footer
 let footerP = document.querySelector('footer p');
 footerP.innerText = siteContent.footer.copyright;
+
+let newFooterP = document.createElement('p');
+newFooterP.innerText = 'DOM is Great, Inc.';
+document.querySelector('footer').append(newFooterP);
