1293126462a4a798274aa67414033bb5e0cc7963
William <kwontaewoongah@gmail.com>
2018-07-09 16:14:26 -0700

    added disable to start button

--- stretch-assignment/digital_timer/index.js
+++ stretch-assignment/digital_timer/index.js
@@ -1,6 +1,6 @@
 totalMS=0;
 let interval;
-function incrementTimer(){
+incrementTimer=()=>{
     interval=setInterval(function() {
     if (totalMS<10000) {
     totalMS+=10;
@@ -17,6 +17,7 @@ function incrementTimer(){
     if (seconds==='10') {
         toggle=0;
         document.querySelector('.digits').style.color='red';
+        document.getElementById('startButton').disabled=false;
     }
     let totalTime=seconds+''+milliseconds;
     document.getElementById('secondTens').innerHTML=totalTime[0];
@@ -25,8 +26,9 @@ function incrementTimer(){
     document.getElementById('msTens').innerHTML=totalTime[3];} 
 },10);
 }
-function reset() {
+reset=()=>{
     clearInterval(interval);
+    document.getElementById('startButton').disabled=false;
     totalMS=0;
     document.getElementById('secondTens').innerHTML=0;
     document.getElementById('secondOnes').innerHTML=0;
@@ -38,12 +40,13 @@ let startBtnText=document.createTextNode('Start Me Up');
 startBtn.setAttribute('id','startButton');
 startBtn.appendChild(startBtnText);
 document.querySelector('.digits').appendChild(startBtn);
-document.getElementById('startButton').addEventListener('click',incrementTimer);
+document.getElementById('startButton').addEventListener('click',()=>{document.getElementById('startButton').disabled=true; incrementTimer();});
 let resetBtn=document.createElement('Button');
 let resetBtnText=document.createTextNode('Reset Me');
 resetBtn.setAttribute('id','resetButton');
 resetBtn.appendChild(resetBtnText);
 document.querySelector('.digits').appendChild(resetBtn);
-document.getElementById('resetButton').addEventListener('click',reset);
+document.getElementById('resetButton').addEventListener('click',()=>{document.getElementById('startButton').disabled=false;reset()});
 document.getElementById('startButton').style.backgroundColor='green';
 document.getElementById('resetButton').style.backgroundColor='red';
+document.body.style.fontFamily='Open Sans';
