3021ca37865469d2af2630d5db10eee722c21e95
Jun Hyuk Kim <junhyukkeem@gmail.com>
2018-07-09 13:41:45 -0700

    implement start/reset button that meets stretch requirements

--- stretch-assignment/digital_timer/index.js
+++ stretch-assignment/digital_timer/index.js
@@ -3,34 +3,51 @@ let ones = document.getElementById('secondOnes');
 let msH = document.getElementById('msHundreds');
 let msT = document.getElementById('msTens');
 let all = document.querySelectorAll('.digit');
-console.log(all);
 let time = 0;
 
-let count = setInterval(function(){
-	time += 1;
-	timeStr = `${0000 + time}`;
-	if (time < 10) {
-		msT.innerText = `${timeStr[0]}`;
-	}
-	if (time > 10 && time < 100) {
-		msH.innerText = `${timeStr[0]}`;
-		msT.innerText = `${timeStr[1]}`;
-	}
-	if (time > 100 && time < 1000) {
-		tens.innerText = `0`;
-		ones.innerText = `${timeStr[0]}`;
-		msH.innerText = `${timeStr[1]}`;
-		msT.innerText = `${timeStr[2]}`;
-	}
-	if (time >= 1000) {
-		clearInterval(count);
-		tens.innerText = `${timeStr[0]}`;
-		ones.innerText = `${timeStr[1]}`;
-		msH.innerText = `${timeStr[2]}`;
-		msT.innerText = `${timeStr[3]}`;
-		all.forEach((element)=>{
-			element.style.color = 'red';
-		})
-	}
-}, 10);
+let start = document.getElementById('start');
+let reset = document.getElementById('reset');
 
+
+start.addEventListener('click', () => {
+	let count = setInterval(function(){
+		time += 1;
+		timeStr = `${0000 + time}`;
+		start.setAttribute('disabled', 'disabled');
+		if (time < 10) {
+			msT.innerText = `${timeStr[0]}`;
+		}
+		if (time > 10 && time < 100) {
+			msH.innerText = `${timeStr[0]}`;
+			msT.innerText = `${timeStr[1]}`;
+		}
+		if (time > 100 && time < 1000) {
+			tens.innerText = `0`;
+			ones.innerText = `${timeStr[0]}`;
+			msH.innerText = `${timeStr[1]}`;
+			msT.innerText = `${timeStr[2]}`;
+		}
+		if (time >= 1000) {
+			clearInterval(count);
+			tens.innerText = `${timeStr[0]}`;
+			ones.innerText = `${timeStr[1]}`;
+			msH.innerText = `${timeStr[2]}`;
+			msT.innerText = `${timeStr[3]}`;
+			all.forEach((element)=>{
+				element.style.color = 'red';
+			})
+			start.removeAttribute('disabled');
+		}
+	}, 10);
+});
+
+reset.addEventListener('click', () => {
+	time = 0;
+	tens.innerText = `-`;
+	ones.innerText = `-`;
+	msH.innerText = `-`;
+	msT.innerText = `-`;
+	all.forEach((element)=>{
+		element.style.color = 'black';
+	})
+});
