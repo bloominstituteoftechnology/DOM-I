f89d72a4ce35e99e31a9433b88df6754c1c584f5
William <kwontaewoongah@gmail.com>
2018-07-09 15:57:42 -0700

    finished styling and implementing start and reset buttons

--- stretch-assignment/digital_timer/index.js
+++ stretch-assignment/digital_timer/index.js
@@ -1,7 +1,7 @@
 totalMS=0;
-
-setInterval(incrementTimer,10);
+let interval;
 function incrementTimer(){
+    interval=setInterval(function() {
     if (totalMS<10000) {
     totalMS+=10;
     let seconds=Math.floor(totalMS/1000);
@@ -15,12 +15,35 @@ function incrementTimer(){
         milliseconds='0'+milliseconds
     }
     if (seconds==='10') {
+        toggle=0;
         document.querySelector('.digits').style.color='red';
     }
     let totalTime=seconds+''+milliseconds;
     document.getElementById('secondTens').innerHTML=totalTime[0];
     document.getElementById('secondOnes').innerHTML=totalTime[1];
     document.getElementById('msHundreds').innerHTML=totalTime[2];
-    document.getElementById('msTens').innerHTML=totalTime[3];
+    document.getElementById('msTens').innerHTML=totalTime[3];} 
+},10);
 }
+function reset() {
+    clearInterval(interval);
+    totalMS=0;
+    document.getElementById('secondTens').innerHTML=0;
+    document.getElementById('secondOnes').innerHTML=0;
+    document.getElementById('msHundreds').innerHTML=0;
+    document.getElementById('msTens').innerHTML=0;
 }
+let startBtn=document.createElement('BUTTON');
+let startBtnText=document.createTextNode('Start Me Up');
+startBtn.setAttribute('id','startButton');
+startBtn.appendChild(startBtnText);
+document.querySelector('.digits').appendChild(startBtn);
+document.getElementById('startButton').addEventListener('click',incrementTimer);
+let resetBtn=document.createElement('Button');
+let resetBtnText=document.createTextNode('Reset Me');
+resetBtn.setAttribute('id','resetButton');
+resetBtn.appendChild(resetBtnText);
+document.querySelector('.digits').appendChild(resetBtn);
+document.getElementById('resetButton').addEventListener('click',reset);
+document.getElementById('startButton').style.backgroundColor='green';
+document.getElementById('resetButton').style.backgroundColor='red';
