735cccf6b4e999bb44e4d60e50fd60a6ff74773b
“Kennedykid1995” <34872885+Kennedykid1995@users.noreply.github.com>
2018-07-10 17:47:39 -0500

    Working on Drag and Drop
    Testing out some drag and drop stuff, using MDN to get some code and then working on mixing it into my code

--- js/index.js
+++ js/index.js
@@ -175,3 +175,57 @@ topHeader[1].addEventListener("dblclick", (event)=>{
  topHeader[1].addEventListener("dblclick", (event)=>{
       event.target.style.float = "right"; 
       })
+
+
+      var dragged;
+
+      /* events fired on the draggable target */
+      document.addEventListener("drag", function( event ) {
+    
+      }, false);
+    
+      document.addEventListener("dragstart", function( event ) {
+          // store a ref. on the dragged elem
+          dragged = event.target;
+          // make it half transparent
+          event.target.style.opacity = .5;
+      }, false);
+    
+      document.addEventListener("dragend", function( event ) {
+          // reset the transparency
+          event.target.style.opacity = "";
+      }, false);
+    
+      /* events fired on the drop targets */
+      document.addEventListener("dragover", function( event ) {
+          // prevent default to allow drop
+          event.preventDefault();
+      }, false);
+    
+      document.addEventListener("dragenter", function( event ) {
+          // highlight potential drop target when the draggable element enters it
+          if ( event.target.className == "dropzone" ) {
+              event.target.style.background = "purple";
+          }
+    
+      }, false);
+    
+      document.addEventListener("dragleave", function( event ) {
+          // reset background of potential drop target when the draggable element leaves it
+          if ( event.target.className == "dropzone" ) {
+              event.target.style.background = "";
+          }
+    
+      }, false);
+    
+      document.addEventListener("drop", function( event ) {
+          // prevent default action (open as link for some elements)
+          event.preventDefault();
+          // move dragged elem to the selected drop target
+          if ( event.target.className == "dropzone" ) {
+              event.target.style.background = "";
+              dragged.parentNode.removeChild( dragged );
+              event.target.appendChild( dragged );
+          }
+        
+      }, false);
