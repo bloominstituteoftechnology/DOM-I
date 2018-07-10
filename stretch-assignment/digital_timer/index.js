b0f1012de9507a249de8c15b319d6558cb0cf804
codejoncode <jonathanjholloway@gmail.com>
2018-07-09 19:14:05 -0500

    added a reset button to the timer

--- stretch-assignment/digital_timer/index.js
+++ stretch-assignment/digital_timer/index.js
@@ -29,6 +29,16 @@ function stopTimer () {
  clearInterval(intervalID); 
  
 }
+function resetTimer () {
+    so = 0;
+    mh = 0;
+    st = 0;
+    mt = 0;
+    msHundreds.innerHTML = mh;
+    msDigits.innerHTML = mt;
+    secondOnes.innerHTML = so; 
+    secondTens.innerHTML = st;
+}
 
 let msDigits = document.querySelector("#msTens"); 
 let msHundreds = document.querySelector("#msHundreds");
@@ -46,16 +56,20 @@ let digitsClass = document.querySelector(".digits");
 
 let start = document.createElement("button"); 
 let stop = document.createElement("button"); 
+let reset = document.createElement("button");
 
 let startText = document.createTextNode("Start");//Sets Place for text;
 let stopText = document.createTextNode("Stop");//Sets Place for text;
+let resetText = document.createTextNode("Reset");
 
 start.appendChild(startText);
 stop.appendChild(stopText); 
+reset.appendChild(resetText); 
 
 let body = document.querySelector("body");
 body.appendChild(start);
 body.appendChild(stop); 
+body.appendChild(reset); 
 
 let buttons = document.querySelectorAll("button");
 
@@ -68,4 +82,5 @@ function timerGo () {
 buttons[0].addEventListener("click", timerGo, false); 
  
 buttons[1].addEventListener("click", stopTimer, false); 
+buttons[2].addEventListener("click", resetTimer, false); 
 
