7cce2d303206ff3f1847b800bddda5aec863bc73
Tom Tarpey <decagondevelopment@gmail.com>
2018-08-13 22:55:56 +0100

    create digital timer function to encapsulate the logic

--- stretch-assignment/digital_timer/index.js
+++ stretch-assignment/digital_timer/index.js
@@ -6,3 +6,8 @@ const sO = document.getElementById("secondOnes");
 const mH = document.getElementById("msHundreds");
 const mT = document.getElementById("msTens");
 const mO = document.getElementById("msOnes");
+
+// digital timer function
+function digitalTimer() {
+
+}
