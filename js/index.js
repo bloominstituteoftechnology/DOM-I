7998f3d6482fcf432632637acfd4eb3ae9742c2c
Andrew <ghobrial.andrew@gmail.com>
2018-08-13 17:12:13 -0400

    contact  dom selectors complete

--- js/index.js
+++ js/index.js
@@ -90,20 +90,22 @@ bottom_content_text_content[1].innerHTML = siteContent['main-content']['product-
 bottom_content_text_content[2].innerHTML = siteContent['main-content']['vision-content'];
 //
 
+// Contact
+const contact_header = document.querySelector('.contact h4');
+contact_header.innerHTML = siteContent['contact']['contact-h4'];
+const contact_text = document.querySelectorAll('.contact p');
+contact_text[0].innerHTML = siteContent['contact']['address'];
+contact_text[1].innerHTML = siteContent['contact']['phone'];
+contact_text[2].innerHTML = siteContent['contact']['email'];
 
+// Footer
 
-
-
-// "main-content": {
-//   "features-h4":"Features",
-//   "features-content": "Features content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.",
-//   "about-h4":"About",
-//   "about-content": "About content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.",
-//   "middle-img-src": "img/mid-page-accent.jpg",
-//   "services-h4":"Services",
-//   "services-content": "Services content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.",
-//   "product-h4":"Product",
-//   "product-content": "Product content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.",
-//   "vision-h4":"Vision",
-//   "vision-content": "Vision content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.",
+// "contact": {
+//   "contact-h4" : "Contact",
+//   "address" : "123 Way 456 Street Somewhere, USA",
+//   "phone" : "1 (888) 888-8888",
+//   "email" : "sales@greatidea.io",
+// },
+// "footer": {
+//   "copyright" : "Copyright Great Idea! 2018"
 // },
