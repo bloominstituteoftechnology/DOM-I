0a683863f1c7a5b1d3376807557d06118e7b6917
Michael Agard <mdagard@gmail.com>
2018-08-13 16:12:07 -0400

    got nav bar to show

--- js/index.js
+++ js/index.js
@@ -42,4 +42,23 @@ const logo = document.getElementById("logo-img");
 logo.setAttribute('src', siteContent["nav"]["img-src"]);
 
 const cta = document.getElementById("logo-img");
-logo.setAttribute('src', siteContent["cta"]["img-src"])
+logo.setAttribute('src', siteContent["cta"]["img-src"]);
+
+const navItem1 = document.getElementsByTagName("a")[0];
+navItem1.textContent = siteContent.nav["nav-item-1"];
+
+const navItem2 = document.getElementsByTagName("a")[1];
+navItem2.textContent = siteContent.nav["nav-item-2"];
+
+const navItem3 = document.getElementsByTagName("a")[2];
+navItem3.textContent = siteContent.nav["nav-item-3"];
+
+const navItem4 = document.getElementsByTagName("a")[3];
+navItem4.textContent = siteContent.nav["nav-item-4"];
+
+const navItem5 = document.getElementsByTagName("a")[4];
+navItem5.textContent = siteContent.nav["nav-item-5"];
+
+const navItem6 = document.getElementsByTagName("a")[5];
+navItem6.textContent = siteContent.nav["nav-item-6"];
+
