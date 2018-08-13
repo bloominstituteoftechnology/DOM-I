51257620410be89e3339c0f599fe94b3174f1eb8
Timothy Hoang <timh1203@gmail.com>
2018-08-13 16:38:34 -0500

    Add main content top

--- js/index.js
+++ js/index.js
@@ -16,13 +16,18 @@ const siteContent = {
   "main-content": {
     "features-h4":"Features",
     "features-content": "Features content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.",
+
     "about-h4":"About",
     "about-content": "About content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.",
+
     "middle-img-src": "img/mid-page-accent.jpg",
+
     "services-h4":"Services",
     "services-content": "Services content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.",
+
     "product-h4":"Product",
     "product-content": "Product content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.",
+
     "vision-h4":"Vision",
     "vision-content": "Vision content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.",
   },
@@ -58,3 +63,10 @@ for (let i = 0; i < tagElements.length; i++) {
 // CTA
 document.querySelector('.cta-text').getElementsByTagName('h1')[0].innerText = siteContent["cta"]["h1"];
 document.querySelector('.cta-text').getElementsByTagName('button')[0].innerText = siteContent["cta"]["button"];
+
+// MAIN CONTENT: TOP
+document.querySelector('.main-content').getElementsByTagName('h4')[0].innerText = siteContent["main-content"]["features-h4"];
+document.querySelector('.main-content').getElementsByTagName('p')[0].innerText = siteContent["main-content"]["features-content"];
+
+document.querySelector('.main-content').getElementsByTagName('h4')[1].innerText = siteContent["main-content"]["about-h4"];
+document.querySelector('.main-content').getElementsByTagName('p')[1].innerText = siteContent["main-content"]["about-content"];
