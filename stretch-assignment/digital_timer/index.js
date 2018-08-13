b2e7f5eee0b4af3c4ebf2e31014453f08e462f4c
Tom Tarpey <decagondevelopment@gmail.com>
2018-08-13 21:35:34 +0100

    started the timer project

---
+++ stretch-assignment/digital_timer/index.js
@@ -0,0 +1,6 @@
+// captire the html elements
+const digits = document.querySelectorAll(".digit");
+const secondT = document.getElementById('secondTens');
+const secondO = document.getElementById('secondOnes');
+const msH = document.getElementById('msHundreds');
+const msT = document.getElementById('msTens');
