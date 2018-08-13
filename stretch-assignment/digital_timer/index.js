550cb5f407de6222ad2082ce2faeaa9ce6f282d7
Tom Tarpey <decagondevelopment@gmail.com>
2018-08-13 23:24:31 +0100

    created the main logic in the update function

--- stretch-assignment/digital_timer/index.js
+++ stretch-assignment/digital_timer/index.js
@@ -3,6 +3,7 @@
 const digits = document.querySelector(".digits");
 const sT = document.getElementById("secondTens");
 const sO = document.getElementById("secondOnes");
+const colon = document.getElementById("colon");
 const mH = document.getElementById("msHundreds");
 const mT = document.getElementById("msTens");
 const mO = document.getElementById("msOnes");
@@ -14,9 +15,13 @@ function digitalTimer() {
   function startUp() {
     let ms = 0;
     sT.innerHTML = "0";
+    
     sO.innerHTML = "0";
+    
     mH.innerHTML = "0";
+    
     mT.innerHTML = "0";
+
     
     // set a timer interval to this timer interval and add a call to update
     const timerInterval = this.setInterval(() => {
@@ -32,5 +37,35 @@ function digitalTimer() {
 
   // incrementer function takes in a string then turns it in to a number adds 1 to it turns it back in to a string and returns the output
   function inc(string) { return (Number(string) + 1).toString(); }
+
+  // update function
+  function update(ms) {
+    if (ms === 10000) { // final condition. if the timer reaches the goal then set the inner html numbers correctly
+      sT.innerHTML = "1";
+      sO.innerHTML = "0";
+      mH.innerHTML = "0";
+      mT.innerHTML = "0";
+      // turn the numbers and colon red
+      sT.style.color = "red";
+      sO.style.color = "red";
+      mH.style.color = "red";
+      mT.style.color = "red";
+      colon.style.color = "red";
+      // else if the remainder of ms / 1000 = 0 then encrement sO
+    } else if (ms % 1000 === 0) {
+      sO.innerHTML = inc(sO.innerHTML);
+      mH.innerHTML = "0";
+      mT.innerHTML = "0";
+
+      // else if the ramainder of ms / 100 = 0 then increment mH
+    } else if (ms % 100 === 0) {
+      mH.innerHTML = inc(mH.innerHTML);
+      mT.innerHTML = "0";
+      // otherwise increment mT
+    } else {
+      mT.innerHTML = inc(mT.innerHTML);
+    }
+  }
+  
 
 }
