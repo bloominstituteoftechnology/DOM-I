80a84819eee266886943a181d8d108c005d39121
Tom Tarpey <decagondevelopment@gmail.com>
2018-08-13 22:11:21 +0100

    if the count is less than 10 the tens usits and msH are all set to 0 and the msT are updated

--- stretch-assignment/digital_timer/index.js
+++ stretch-assignment/digital_timer/index.js
@@ -13,4 +13,12 @@ let msH = document.getElementById("msHundreds");
 let msT = document.getElementById("msTens");
 let ms = count.toString();
 
+// if the count is less than 10 then the tens units msh are all set to 0 and the ms are updated
+if (count < 10) {
+    secondT.innerHTML = "0";
+    secondO.innerHTML = "0";
+    msH.innerHTML = "0"
+    msT.innerHTML = ms;
+  }
+
 });
