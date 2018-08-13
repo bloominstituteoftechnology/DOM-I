e2e4e86cc64f1e5d6d575dcf15de9aa9ceb31c16
Vera Simon <ghjkl>
2018-08-13 14:48:13 -0700

    Content population
    Populated nav, cta, and main-content content

--- js/index.js
+++ js/index.js
@@ -38,5 +38,57 @@ const siteContent = {
 };
 
 // Example: Update the img src for the logo
-let logo = document.getElementById("logo-img");
+const logo = document.getElementById("logo-img");
 logo.setAttribute('src', siteContent["nav"]["img-src"])
+
+const navLinks = document.querySelectorAll('header > nav > a');
+navLinks.forEach((link, i) => link.innerText = siteContent.nav[`nav-item-${[i+1]}`]);
+
+// .cta
+const ctaTxt = document.querySelector('.cta-text > h1');
+ctaTxt.innerText = siteContent.cta.h1;
+
+const ctaButton = document.querySelector('.cta-text > button');
+ctaButton.innerText = siteContent.cta.button;
+
+const ctaImg = document.querySelector('#cta-img')
+ctaImg.src = siteContent.cta['img-src'];
+
+// .main-content
+// .main-content > .top-content
+const topMainHead1 = document.querySelectorAll('.top-content > div > h4')[0];
+topMainHead1.innerText = siteContent['main-content']['features-h4'];
+
+const topMainPara1 = document.querySelectorAll('.top-content > div > p')[0];
+topMainPara1.innerText = siteContent['main-content']['features-content'];
+
+const topMainHead2 = document.querySelectorAll('.top-content > div > h4')[1];
+topMainHead2.innerText = siteContent['main-content']['about-h4'];
+
+const topMainPara2 = document.querySelectorAll('.top-content > div > p')[1];
+topMainPara2.innerText = siteContent['main-content']['about-content'];
+
+// .main-content > .middle-img
+const midImg = document.querySelector('#middle-img');
+midImg.src = siteContent["main-content"]["middle-img-src"];
+
+// .main-content > .bottom-content
+const bottomMainHead1 = document.querySelectorAll('.bottom-content > div > h4')[0];
+bottomMainHead1.innerText = siteContent['main-content']['services-h4'];
+
+const bottomMainPara1 = document.querySelectorAll('.bottom-content > div > p')[0];
+bottomMainPara1.innerText = siteContent['main-content']['services-content'];
+
+const bottomMainHead2 = document.querySelectorAll('.bottom-content > div > h4')[1];
+bottomMainHead2.innerText = siteContent['main-content']['product-h4'];
+
+const bottomMainPara2 = document.querySelectorAll('.bottom-content > div > p')[1];
+bottomMainPara2.innerText = siteContent['main-content']['product-content'];
+
+const bottomMainHead3 = document.querySelectorAll('.bottom-content > div > h4')[2];
+bottomMainHead3.innerText = siteContent['main-content']['vision-h4'];
+
+const bottomMainPara3 = document.querySelectorAll('.bottom-content > div > p')[2];
+bottomMainPara3.innerText = siteContent['main-content']['vision-content'];
+
+// .contact
