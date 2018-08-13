16eba3d8d752e6da45f32069488d62a11adf8673
Sasha <taylork85@gmail.com>
2018-08-13 15:21:19 -0500

    Sasha Submission

--- js/index.js
+++ js/index.js
@@ -39,4 +39,81 @@ const siteContent = {
 
 // Example: Update the img src for the logo
 let logo = document.getElementById("logo-img");
-logo.setAttribute('src', siteContent["nav"]["img-src"])
+logo.setAttribute('src', siteContent["nav"]["img-src"]);
+
+let nav = document.querySelectorAll('nav a');
+nav[0].innerHTML = (siteContent["nav"]["nav-item-1"]);
+
+let nav1 = document.querySelectorAll('nav a');
+nav1[1].innerHTML = (siteContent["nav"]["nav-item-2"]);
+
+let nav2 = document.querySelectorAll('nav a');
+nav2[2].innerHTML = (siteContent["nav"]["nav-item-3"]);
+
+let nav3 = document.querySelectorAll('nav a');
+nav3[3].innerHTML = (siteContent["nav"]["nav-item-4"]);
+
+let nav4 = document.querySelectorAll('nav a');
+nav4[4].innerHTML = (siteContent["nav"]["nav-item-5"]);
+
+let nav5 = document.querySelectorAll('nav a');
+nav5[5].innerHTML = (siteContent["nav"]["nav-item-6"]);
+
+let cta = document.getElementById("cta-img");
+cta.setAttribute('src', siteContent["cta"]["img-src"]);
+
+let ctaText = document.querySelector(".cta-text h1");
+ctaText.innerHTML = (siteContent["cta"]["h1"]);
+
+let ctaBtn = document.querySelector(".cta-text button");
+ctaBtn.innerHTML = (siteContent["cta"]["button"]);
+
+let middle = document.getElementById("middle-img");
+middle.setAttribute('src', siteContent["main-content"]["middle-img-src"]);
+
+let mainContent1 = document.querySelectorAll('.top-content .text-content h4');
+mainContent1[0].innerHTML = (siteContent["main-content"]["features-h4"]);
+
+let mainText1 = document.querySelectorAll('.top-content .text-content p');
+mainText1[0].innerHTML = (siteContent["main-content"]["features-content"]);
+
+let mainContent2 = document.querySelectorAll('.top-content .text-content h4');
+mainContent2[1].innerHTML = (siteContent["main-content"]["about-h4"]);
+
+let mainText2 = document.querySelectorAll('.top-content .text-content p');
+mainText2[1].innerHTML = (siteContent["main-content"]["about-content"]);
+
+let mainContent3 = document.querySelectorAll('.bottom-content .text-content h4');
+mainContent3[0].innerHTML = (siteContent["main-content"]["services-h4"]);
+
+let mainText3 = document.querySelectorAll('.bottom-content .text-content p');
+mainText3[0].innerHTML = (siteContent["main-content"]["services-content"]);
+
+let mainContent4 = document.querySelectorAll('.bottom-content .text-content h4');
+mainContent4[1].innerHTML = (siteContent["main-content"]["product-h4"]);
+
+let mainText4 = document.querySelectorAll('.bottom-content .text-content p');
+mainText4[1].innerHTML = (siteContent["main-content"]["product-content"]);
+
+let mainContent5 = document.querySelectorAll('.bottom-content .text-content h4');
+mainContent5[2].innerHTML = (siteContent["main-content"]["vision-h4"]);
+
+let mainText5 = document.querySelectorAll('.bottom-content .text-content p');
+mainText5[2].innerHTML = (siteContent["main-content"]["vision-content"]);
+let contact = document.querySelector(".contact h4");
+
+contact.innerHTML = (siteContent["contact"]["contact-h4"])
+let contact0 = document.querySelectorAll(".contact p");
+
+contact0[0].innerHTML = (siteContent["contact"]["address"])
+let contact1 = document.querySelectorAll(".contact p");
+
+contact1[1].innerHTML = (siteContent["contact"]["phone"])
+let contact2 = document.querySelectorAll(".contact p");
+
+contact2[2].innerHTML = (siteContent["contact"]["email"])
+let footer = document.querySelector("footer p");
+
+footer.innerHTML = (siteContent["footer"]["copyright"]);
+let oldNav = document.querySelector("nav");
+
