a6e0f0347c26dbc0563c706895feb6075b103739
Brock Maraee Rohloff <00AgentB@gmail.com>
2018-08-13 16:40:55 -0600

    Finished all tasks and the stretch goals (not stretch project).

--- js/index.js
+++ js/index.js
@@ -37,6 +37,85 @@ const siteContent = {
   },
 };
 
-// Example: Update the img src for the logo
-let logo = document.getElementById("logo-img");
-logo.setAttribute('src', siteContent["nav"]["img-src"])
+
+// Navigation
+const logo = document.getElementById("logo-img");
+const navItem = document.querySelectorAll("nav a");
+const nav = document.querySelector("nav");
+let firstTag = document.createElement("a");
+let secondTag = document.createElement("a");
+
+logo.setAttribute('src', siteContent["nav"]["img-src"]);
+for(i = 0; i < navItem.length; i++) { 
+  navItem[i].innerText = siteContent["nav"][Object.keys(siteContent["nav"])[i]];
+  navItem[i].style.color = "green";
+}
+
+firstTag.href = "#";
+secondTag.href = "#";
+firstTag.innerText = "HELP"; 
+secondTag.innerText = "TRAPPED"; 
+
+nav.prepend(firstTag); //Color intentionally not green for effect.
+nav.appendChild(secondTag); //Color intentionally not green for effect.
+
+// cta Section
+const cta = siteContent["cta"];
+
+const ctaH1 = document.querySelector(".cta-text h1");
+const ctaButton = document.querySelector(".cta-text button");
+const ctaImage = document.querySelector("#cta-img"); 
+
+ctaH1.innerHTML = cta["h1"].split(" ").join("<br>");
+ctaButton.innerText = cta["button"]; 
+ctaImage.src = cta["img-src"]; 
+
+//Stretch
+let navBool = true;
+const helpMe = function() {
+  let color = "";
+  if(navBool){
+  color = "red";
+  }
+  navBool = !navBool;
+document.querySelector("nav a:first-of-type").style.color = color;
+document.querySelector("nav a:last-of-type").style.color = color;
+}
+
+ctaButton.onclick = helpMe;
+
+// Main Content
+const mainContent = siteContent["main-content"];
+
+const mainContentH4 = document.querySelectorAll(".main-content h4"); 
+const mainContentP = document.querySelectorAll(".main-content p"); 
+const mainContentImg = document.querySelector("#middle-img");
+
+mainH4Keys = Object.keys(mainContent).filter(key => key.includes("-h4"));
+mainPKeys = Object.keys(mainContent).filter(key => key.includes("-content"));
+
+for(i = 0; i < mainContentH4.length; i++) {
+  mainContentH4[i].innerText = mainContent[mainH4Keys[i]];
+}
+for(i = 0; i < mainContentP.length; i++) {
+  mainContentP[i].innerText = mainContent[mainPKeys[i]];
+}
+mainContentImg.src = mainContent["middle-img-src"];
+
+// Contact
+const contact = siteContent["contact"];
+
+const contactH4 = document.querySelector(".contact h4");
+const address = document.querySelector(".contact p:nth-of-type(1)");
+const phone = document.querySelector(".contact p:nth-of-type(2)");
+const email = document.querySelector(".contact p:nth-of-type(3)");
+const addressIndex = contact["address"].slice(0, contact["address"].lastIndexOf(" ")).lastIndexOf(" ");
+
+contactH4.innerText = contact["contact-h4"];
+address.innerHTML = contact["address"].slice(0, addressIndex) + "<br>" + contact["address"].slice(addressIndex);
+phone.innerText = contact["phone"];
+email.innerText = contact["email"];
+// Footer
+
+const copyright = document.querySelector("footer p");
+copyright.innerText = siteContent["footer"]["copyright"];
