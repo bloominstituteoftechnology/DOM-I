91a6eea2721121bb559ac1d0f38299ae7b6b39b7
Joseph Chretien-Golden <joechre@gmail.com>
2018-08-13 17:56:51 -0500

    test

--- js/index.js
+++ js/index.js
@@ -40,3 +40,16 @@ const siteContent = {
 // Example: Update the img src for the logo
 let logo = document.getElementById("logo-img");
 logo.setAttribute('src', siteContent["nav"]["img-src"])
+
+let circle = document.getElementById('cta-img');
+circle.setAttribute('src', siteContent["cta"]["img-src"])
+
+let midImg = document.getElementById('middle-img');
+midImg.setAttribute('src', siteContent["main-content"]["middle-img-src"])
+
+
+
+
+
+
+
