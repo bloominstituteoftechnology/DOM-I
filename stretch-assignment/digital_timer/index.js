4c372460c20f0ce8a5292a986885f445b9f1c803
Tom Tarpey <decagondevelopment@gmail.com>
2018-08-13 22:54:55 +0100

    added the element selectors

--- stretch-assignment/digital_timer/index.js
+++ stretch-assignment/digital_timer/index.js
@@ -1,1 +1,8 @@
-// starting fresh i am getting too tired need to have a walk then come back to this problem fresh
+
+// setup the elements
+const digits = document.querySelector(".digits");
+const sT = document.getElementById("secondTens");
+const sO = document.getElementById("secondOnes");
+const mH = document.getElementById("msHundreds");
+const mT = document.getElementById("msTens");
+const mO = document.getElementById("msOnes");
