812595e2c8e2f65b2999808046b4db73ca1bbf13
Tom Tarpey <decagondevelopment@gmail.com>
2018-08-13 21:46:18 +0100

    setup the callbacks for the timer interval functions

--- stretch-assignment/digital_timer/index.js
+++ stretch-assignment/digital_timer/index.js
@@ -4,3 +4,50 @@ const secondT = document.getElementById('secondTens');
 const secondO = document.getElementById('secondOnes');
 const msH = document.getElementById('msHundreds');
 const msT = document.getElementById('msTens');
+
+// set the counters to 0
+let counter = 0
+let counterMillis = 0;
+let counterMillisH = 0;
+
+
+// Digit mover setups for callbacks on the timer interval 
+
+//setup a digit mover for the seconds
+function digitsSeconds() {
+    if(counter<9) {
+        counter++
+        secondO.innerHTML = counter;
+    }
+    else {
+        secondO.innerHTML = "0";
+    }
+};
+
+// setup digits mover for tens
+function digitsTen() {
+    secondT.innerHTML = "1";
+};
+
+// setup digit mover for hundreds of ms
+function timerMsHunreds() {
+    if(counterMillisH < 9) {
+        counterMillisH++;
+        msH.innerHTML = counterMillisH;
+    } else {
+        counterMillisH = 0;
+    }
+};
+
+// setup digit mover for tens of ms
+function digitsMsTens() {
+    if(counterMillis < 90) {
+        counterMillis = counterMillis + 10;
+        msT.innerHTML = counterMillis;
+    } else {
+        counterMs = 0;
+    }
+};
+
+
+
