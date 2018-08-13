cb853c2d7b956997e4bde1b37a7781daeb075134
Kevin Sooter <kevin.m.sooter@gmail.com>
2018-08-13 16:18:16 -0500

    Completed Task 1-4

--- js/index.js
+++ js/index.js
@@ -43,9 +43,9 @@ const siteContent = {
 };
 
 // Task 2
-const navBar = document.querySelector('nav');
-const navContent = document.querySelectorAll('nav a');
 const logo = document.getElementById('logo-img');
+const navBar = document.querySelector('nav');
+const navItems = document.querySelectorAll('nav a');
 const ctaHead = document.querySelector('.cta-text h1');
 const ctaButton = document.querySelector('.cta-text button');
 const ctaImg = document.getElementById('cta-img');
@@ -54,3 +54,62 @@ const mainContentBody = document.querySelectorAll('.main-content p');
 const middleImg = document.getElementById('middle-img');
 const contactHead = document.querySelector('.contact h4');
 const contactBody = document.querySelectorAll('.contact p');
+const footer = document.querySelector('footer');
+
+// Task 4 - Added Nav Items
+const portfolio = document.createElement('a');
+const portfolioText = document.createTextNode('Portfolio');
+const home = document.createElement('a');
+const homeText = document.createTextNode('Home');
+
+// Header
+
+// Great Idea! Logo
+logo.src = siteContent['nav']['img-src'];
+
+// NavBar
+for (let i = 0; i < navItems.length; i++) {
+  navItems[i].innerHTML = siteContent['nav']['nav-item-' + (i + 1)];
+  navItems[i].style.color = 'green';
+}
+
+home.appendChild(homeText);
+home.style.cssText = 'color: green; cursor: pointer;';
+navBar.prepend(home);
+
+portfolio.appendChild(portfolioText);
+portfolio.style.cssText = 'color: green; cursor: pointer;';
+navBar.appendChild(portfolio);
+
+// CTA
+
+ctaHead.innerHTML = siteContent['cta']['h1'];
+ctaButton.innerHTML = siteContent['cta']['button'];
+ctaImg.src = siteContent['cta']['img-src'];
+
+// Main Content
+
+mainContentHead[0].innerHTML = siteContent['main-content']['features-h4'];
+mainContentBody[0].innerHTML = siteContent['main-content']['features-content'];
+mainContentHead[1].innerHTML = siteContent['main-content']['about-h4'];
+mainContentBody[1].innerHTML = siteContent['main-content']['about-content'];
+
+middleImg.src = siteContent['main-content']['middle-img-src'];
+
+mainContentHead[2].innerHTML = siteContent['main-content']['services-h4'];
+mainContentBody[2].innerHTML = siteContent['main-content']['services-content'];
+mainContentHead[3].innerHTML = siteContent['main-content']['product-h4'];
+mainContentBody[3].innerHTML = siteContent['main-content']['product-content'];
+mainContentHead[4].innerHTML = siteContent['main-content']['vision-h4'];
+mainContentBody[4].innerHTML = siteContent['main-content']['vision-content'];
+
+// Contact
+
+contactHead.innerHTML = siteContent['contact']['contact-h4'];
+contactBody[0].innerHTML = siteContent['contact']['address'];
+contactBody[1].innerHTML = siteContent['contact']['phone'];
+contactBody[2].innerHTML = siteContent['contact']['email'];
+
+// Footer
+
+footer.innerHTML = siteContent['footer']['copyright'];
