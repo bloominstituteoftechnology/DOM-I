effe7d28f980c0092585eb2bd85b6c802193667b
iAmAdamReid <adam@adamreid.me>
2018-08-14 00:08:54 -0700

    Added Stop and Reset Buttons with Styling

--- stretch-assignment/digital_timer/index.js
+++ stretch-assignment/digital_timer/index.js
@@ -5,29 +5,41 @@ let msHundreds = document.getElementById('msHundreds');
 let secondOnes = document.getElementById('secondOnes');
 let secondTens = document.getElementById('secondTens');
 
+document.body.style.cssText = 'display: flex; flex-flow: column wrap;'
+document.querySelector('.digits').style.cssText = 'margin: 10px; width: 100%; display: flex; justify-content: center;';
+
 // Assign a counter for each digit
 let msTensCount = 0;
 let msHundredsCount = 0;
 let secondOnesCount = 0;
 let secondTensCount = 0;
 
+// On page load, set isPaused and isFinished to true
+let isPaused = true;
+let isFinished = true;
 
-// Create a button for the clock
-let button = document.createElement('button');
-let buttonText = document.createTextNode('Start Clock');
-button.appendChild(buttonText);
-document.body.appendChild(button);
+// Create a reset button for the clock
+let start = document.createElement('button');
+let startText = document.createTextNode('Start Clock');
+start.appendChild(startText);
+start.classList.add('start-btn');
+document.body.appendChild(start);
+start.style.cssText = 'background: green; color: white;'
+
+// Create a reset button
+let reset = document.createElement('button');
+let resetText = document.createTextNode('Reset Clock');
+reset.appendChild(resetText);
+reset.classList.add('reset-btn');
+document.body.appendChild(reset);
+reset.style.cssText = 'background: blue; color: white;';
 
 // Style the button
-let clockBtn = document.createElement('style');
-clockBtn.innerHTML = "button {border: 2px solid black; display: block; position: absolute; margin-top: 50px; cursor: pointer;}";
-document.body.appendChild(clockBtn);
-button.classList.add('clockBtn');
+let buttonStyle = document.createElement('style');
+buttonStyle.innerHTML = "button {border: 2px solid black; cursor: pointer; width: 100px; height: 50px; margin: 10px; outline: none; border-radius: 10px;}";
+document.body.appendChild(buttonStyle);
 
-
-
-
-// Create a function to change digit colors
+// Create a function to change digit colors at 10s
 let digits = document.querySelector('div.digits').children;
 function digitColor() {
     for (let i = 0; i < digits.length; i++) {
@@ -35,9 +47,50 @@ function digitColor() {
     }
 }
 
+document.querySelector('.reset-btn').addEventListener('click', function(){
+    isPaused = true;
+    setTimeout(function(){
+        toZero();
+    }, 20);
+    isFinished = false;
+    start.innerText = 'Start Clock';
+});
+
+// Function that resets the timer
+function toZero(){
+    msTens.innerText = 0;
+    msHundreds.innerText = 0;
+    secondOnes.innerText = 0;
+    secondTens.innerText = 0;
+    msTensCount = 0;
+    msHundredsCount = 0;
+    secondOnesCount = 0;
+    secondTensCount = 0;
+    for (let i = 0; i < digits.length; i++) {
+        digits[i].classList.remove('redDigit');
+    }
+}
+// Assign a click listener for the button
+document.querySelector('.start-btn').addEventListener('click', function(){
+    if(isPaused == true){
+        start.innerText = 'Stop Clock';
+        startClock();
+        isPaused = false;
+        isFinished = false;
+        start.style.background = 'red';
+    } else if (isPaused == false){
+        start.innerText = 'Start Clock';
+        isPaused = true;
+        start.style.background = 'green';
+    }
+});
 
 // Create the function that starts the clock
 function startClock() {
+    if(isFinished == true){
+        toZero();
+        isFinished = false;
+    }
     let timer = setInterval(function () {
         msTensCount += 1;
         msTens.innerText = msTensCount;
@@ -65,17 +118,13 @@ function startClock() {
         if (secondTensCount == 1) {
             clearInterval(timer);
             digitColor();
+            isFinished = true;
+            isPaused = true;
+            start.innerText = 'Reset & Start';
+            start.style.background = 'green';
         }
-
+        if (isPaused == true){
+            clearInterval(timer);
+        }
     }, 10);
 }
-
-// Assign a click listener for the button
-document.querySelector('button').addEventListener('click', function(){
-    startClock();
-});
-
-// TO-DO
-// Add pause functionality to clock button
-// Add a reset button
-// Add auto-reset if time is at 10 seconds
