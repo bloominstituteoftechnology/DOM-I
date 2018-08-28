13c2df21fb863cde4e3ad9929d1bf39882065a76
alasalle <angela.lasalle91@gmail.com>
2018-08-28 16:41:35 -0400

    completed Stretch Assignment: Timer updated index.js updated README.md

--- stretch-assignment/README.md
+++ stretch-assignment/README.md
@@ -17,14 +17,14 @@ Topics:
 
 ## Beginning Project
  * You'll be working in the `digital_timer` directory
- * Add your script tag into you index.html
+ * [x] Add your script tag into you index.html
  * You can open your `index.html` file in your browser to see your work, but you may also work in codepen.io first if you'd like
  * All your work will be done in `index.js`
  * Make sure to read both the `index.html` and `styles.css` files so that you know what you're working with
  * Organization of your code in `index.js` will be up to you. There will be suggestions provided later in the day
 
 ## Project Description
- You will be making a timer that:
+ * [x] You will be making a timer that:
   * Counts up to 10 seconds
   * Increments every 10 ms
   * Has digits change to red when it gets to 10 seconds
@@ -34,8 +34,8 @@ Topics:
 
  ## Stretch On The Stretch!
   If you compelete the timer with time to spare:
-  * Self study and research DOM `events` and `event listeners`
-  * SG1: Implement a start button. The digital timer should not start until the button is pressed.
-  * SG2: Once you have a start button working, configure it so that when a user presses the start button it is disabled and not enabled until the timer finishes.
-  * SG3: Once you have finished SG2, add new new button called `reset` that resets the timer to 0:00:00 and then pressing the start button again will start the timer from 0.
-  * SG4: Finally, if you have completed all the stretch goals, spend some time styling your timer and buttons.
+  * [x] Self study and research DOM `events` and `event listeners`
+  * [x] SG1: Implement a start button. The digital timer should not start until the button is pressed.
+  * [x] SG2: Once you have a start button working, configure it so that when a user presses the start button it is disabled and not enabled until the timer finishes.
+  * [x] SG3: Once you have finished SG2, add new new button called `reset` that resets the timer to 0:00:00 and then pressing the start button again will start the timer from 0.
+  * [x] SG4: Finally, if you have completed all the stretch goals, spend some time styling your timer and buttons.
---
+++ stretch-assignment/digital_timer/index.js
@@ -0,0 +1,88 @@
+const secondTens = document.getElementById("secondTens");
+const secondOnes = document.getElementById("secondOnes");
+const msHundreds = document.getElementById("msHundreds");
+const msTens = document.getElementById("msTens");
+
+function startTimer() {
+    
+    const msTensInterval = window.setInterval(function() {
+        if (this.msTens.innerText === "9") {
+            this.msTens.innerText = "0";
+        } else {
+            this.msTens.innerText = (Number(this.msTens.innerText) || 0) + 1;
+        }
+    }, 10);
+
+    const msHundredsInterval = window.setInterval(function(){
+        if (this.msHundreds.innerText === "9") {
+            this.msHundreds.innerText = "0";
+        } else {
+            this.msHundreds.innerText = (Number(this.msHundreds.innerText) || 0) + 1;
+        }
+    }, 100);
+
+    const secondOnesInterval = window.setInterval(function() {
+        if (this.secondOnes.innerText === "9") {
+            this.secondOnes.innerText = "0";
+        } else {
+            this.secondOnes.innerText = (Number(this.secondOnes.innerText) || 0) + 1;
+        }
+    }, 1000);
+
+    resetButton.disabled = true;
+
+    window.setTimeout(function() {
+        clearInterval(msTensInterval);
+        clearInterval(msHundredsInterval);
+        clearInterval(secondOnesInterval);
+        this.secondTens.innerText = 1;
+        for (let digit of document.getElementsByClassName("digit")) {
+            digit.style.color = "red";
+        }
+        resetButton.disabled = false;
+    }, 10000);
+}
+
+const body = document.getElementsByTagName("body")[0];
+
+const buttonsDiv = document.createElement("div");
+buttonsDiv.style.marginTop = "20px"
+
+body.appendChild(buttonsDiv);
+
+const startButton = document.createElement("button");
+startButton.innerText = "Start";
+startButton.addEventListener("click", function() {
+    startButton.disabled = true;
+    return startTimer();
+});
+
+buttonsDiv.appendChild(startButton);
+
+const resetButton = document.createElement("button");
+resetButton.innerText = "Reset";
+resetButton.addEventListener("click", function() {
+    msTens.innerText = 0;
+    msHundreds.innerText = 0;
+    secondOnes.innerText = 0;
+    secondTens.innerText = 0;
+    for (let digit of document.getElementsByClassName("digit")) {
+        digit.style.color = "black";
+    }
+
+    startButton.disabled = false;
+    resetButton.disabled = true;
+    return resetTimer();
+});
+
+buttonsDiv.appendChild(resetButton);
+
+startButton.style.padding = "10px 20px";
+resetButton.style.padding = "10px 20px";
+startButton.style.border = "2px solid #1A5276";
+resetButton.style.border = "2px solid #1A5276";
+startButton.style.backgroundColor = "#2ECC71";
+resetButton.style.backgroundColor = "#2ECC71";
+resetButton.style.marginLeft = "10px";
+body.style.display = "flex";
+body.style.flexDirection = "column";
