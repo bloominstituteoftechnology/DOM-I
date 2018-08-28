0668382261844466456a36fbe8f2bdb487b73e2c
Bruce Cabanayan <bcabanayan@Unknown.domain>
2018-08-28 11:55:40 -0700

    Replaced all inner text and attributes with object properties from siteContent object

--- js/index.js
+++ js/index.js
@@ -42,84 +42,82 @@ let logo = document.getElementById("logo-img");
 logo.setAttribute('src', siteContent["nav"]["img-src"])
 
 let nav1 = document.querySelectorAll('div header nav a')[0];
-nav1.innerHTML = 'Services';
+nav1.innerHTML = siteContent['nav']['nav-item-1'];
 nav1.style.color = 'green';
 
 let nav2 = document.querySelectorAll('div header nav a')[1];
-nav2.innerHTML = 'Product';
+nav2.innerHTML = siteContent['nav']['nav-item-2'];
 nav2.style.color = 'green';
 
 let nav3 = document.querySelectorAll('div header nav a')[2];
-nav3.innerHTML = 'Vision';
+nav3.innerHTML = siteContent['nav']['nav-item-3'];
 nav3.style.color = 'green';
 
 let nav4 = document.querySelectorAll('div header nav a')[3];
-nav4.innerHTML = 'Features';
+nav4.innerHTML = siteContent['nav']['nav-item-4'];
 nav4.style.color = 'green';
 
 let nav5 = document.querySelectorAll('div header nav a')[4];
-nav5.innerHTML = 'About';
+nav5.innerHTML = siteContent['nav']['nav-item-5'];
 nav5.style.color = 'green';
 
 let nav6 = document.querySelectorAll('div header nav a')[5];
-nav6.innerHTML = 'Contact';
+nav6.innerHTML = siteContent['nav']['nav-item-6'];
 nav6.style.color = 'green';
 
-let cta = document.querySelector('.cta');
-
 let ctaH1 = document.querySelector('.cta .cta-text h1');
-ctaH1.innerHTML = 'DOM<br>Is<br>Awesome';
+ctaH1.innerHTML = siteContent['cta']['h1'].replace(/ /g, '<br>');
 
 let button = document.querySelector('.cta .cta-text button');
-button.innerHTML = 'Get Started';
+button.innerHTML = siteContent['cta']['button'];
 
 let ctaImage = document.querySelector('.cta img');
-ctaImage.setAttribute('src', 'img/header-img.png');
+ctaImage.setAttribute('src', siteContent['cta']['img-src']);
 
 let features = document.querySelectorAll('.main-content .top-content h4')[0];
-features.innerHTML = 'Features';
+features.innerHTML = siteContent['main-content']['features-h4'];
 
 let featuresText = document.querySelectorAll('.main-content .top-content p')[0];
-featuresText.innerHTML = 'Features content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.';
+featuresText.innerHTML = siteContent['main-content']['features-content'];
 
 let about = document.querySelectorAll('.main-content .top-content h4')[1];
-about.innerHTML = 'About';
+about.innerHTML = siteContent['main-content']['about-h4'];
 
 let aboutText = document.querySelectorAll('.main-content .top-content p')[1];
-aboutText.innerHTML = 'About content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.';
+aboutText.innerHTML = siteContent['main-content']['about-content'];
 
 let middleImg = document.querySelector('.middle-img');
-middleImg.setAttribute('src', 'img/mid-page-accent.jpg');
+middleImg.setAttribute('src', siteContent['main-content']['middle-img-src']);
 
 let services = document.querySelectorAll('.main-content .bottom-content h4')[0];
-services.innerHTML = 'Services';
+services.innerHTML = siteContent['main-content']['services-h4'];
 
 let servicesText = document.querySelectorAll('.main-content .bottom-content p')[0];
-servicesText.innerHTML = 'Services content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.';
+servicesText.innerHTML = siteContent['main-content']['services-content'];
 
 let product = document.querySelectorAll('.main-content .bottom-content h4')[1];
-product.innerHTML = 'Product';
+product.innerHTML = siteContent['main-content']['product-h4'];
 
 let productText = document.querySelectorAll('.main-content .bottom-content p')[1];
-productText.innerHTML = 'Product content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.';
+productText.innerHTML = siteContent['main-content']['product-content'];
 
 let vision = document.querySelectorAll('.main-content .bottom-content h4')[2];
-vision.innerHTML = 'Vision';
+vision.innerHTML = siteContent['main-content']['vision-h4'];
 
 let visionText = document.querySelectorAll('.main-content .bottom-content p')[2];
-visionText.innerHTML = 'Vision content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.';
+visionText.innerHTML = siteContent['main-content']['vision-content'];
 
 let contact = document.querySelector('.contact h4');
-contact.innerHTML = 'Contact';
+contact.innerHTML = siteContent['contact']['contact-h4'];
 
 let address = document.querySelectorAll('.contact p')[0];
-address.innerHTML = '123 Way 456 Street<br>Somewhere, USA'
+address.innerHTML = siteContent['contact']['address'].replace('Street ','Street<br>');
 
 let phone = document.querySelectorAll('.contact p')[1];
-phone.innerHTML = '1 (888) 888-8888';
+phone.innerHTML = siteContent['contact']['phone'];
 
 let email = document.querySelectorAll('.contact p')[2];
-email.innerHTML = 'sales@greatidea.io';
+email.innerHTML = siteContent['contact']['email'];
 
 let footer = document.querySelector('footer p');
-footer.innerHTML = 'Copyright Great Idea! 2018';
+footer.innerHTML = siteContent['footer']['copyright'];
