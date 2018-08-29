f9092a7181a6ce838665e075f6db96956952f4ea
Alex Martin <atonymartin20@yahoo.com>
2018-08-28 21:34:22 -0500

    Nav bar and cta section completed.

--- js/index.js
+++ js/index.js
@@ -41,5 +41,64 @@ const siteContent = {
 const logo = document.getElementById("logo-img");
 logo.setAttribute('src', siteContent["nav"]["img-src"])
 
-const navItem1 = document.getElementsByTagName('a')[0];
-navItem1.textContent = siteContent.nav['nav-item-1'];
+//beginning of nav
+const navItem1 = document.querySelectorAll('a')[0];
+navItem1.innerHTML = siteContent.nav['nav-item-1'];
+
+const navItem2 = document.querySelectorAll('a')[1];
+navItem2.innerHTML = siteContent.nav['nav-item-2'];
+
+const navItem3 = document.querySelectorAll('a')[2];
+navItem3.innerHTML = siteContent.nav['nav-item-3'];
+
+const navItem4 = document.querySelectorAll('a')[3];
+navItem4.innerHTML = siteContent.nav['nav-item-4'];
+
+const navItem5 = document.querySelectorAll('a')[4];
+navItem5.innerHTML = siteContent.nav['nav-item-5'];
+
+const navItem6 = document.querySelectorAll('a')[5];
+navItem6.innerHTML = siteContent.nav['nav-item-6'];
+
+//end of nav
+// beginning of cta
+const h1 = document.querySelector('h1');
+h1.innerHTML = siteContent.cta["h1"];
+
+const button = document.querySelector('button');
+button.innerHTML = siteContent.cta["button"];
+
+
+const ctaImg = document.getElementById("cta-img");
+ctaImg.setAttribute('src', siteContent["cta"]["img-src"])
+
+//end of cta
+// beginning of main content
+const featuresh4 = document.querySelectorAll('h4')[0];
+featuresh4.innerHTML = siteContent.top-content["features-h4"];
+
+const featuresContent = document.querySelectorAll('p')[0];
+featuresContent.innerHTML = siteContent.main-content["features-content"];
+// end of main content
+// "main-content": {
+//   "features-h4":"Features",
+//   "features-content": "Features content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.",
+//   "about-h4":"About",
+//   "about-content": "About content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.",
+//   "middle-img-src": "img/mid-page-accent.jpg",
+//   "services-h4":"Services",
+//   "services-content": "Services content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.",
+//   "product-h4":"Product",
+//   "product-content": "Product content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.",
+//   "vision-h4":"Vision",
+//   "vision-content": "Vision content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.",
+// },
+// "contact": {
+//   "contact-h4" : "Contact",
+//   "address" : "123 Way 456 Street Somewhere, USA",
+//   "phone" : "1 (888) 888-8888",
+//   "email" : "sales@greatidea.io",
+// },
+// "footer": {
+//   "copyright" : "Copyright Great Idea! 2018"
+// },
