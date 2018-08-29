1ba075108136241b0865ad44af1734a80613b560
BrandonMoll <brandonmoll85@gmail.com>
2018-08-28 21:19:50 -0500

    forgot to do commits =/, project completed

--- js/index.js
+++ js/index.js
@@ -40,3 +40,72 @@ const siteContent = {
 // Example: Update the img src for the logo
 let logo = document.getElementById("logo-img");
 logo.setAttribute('src', siteContent["nav"]["img-src"])
+
+let headerLinks = document.querySelectorAll('a');
+headerLinks[0].innerHTML = siteContent['nav']['nav-item-1'];
+headerLinks[1].innerHTML = siteContent['nav']['nav-item-2'];
+headerLinks[2].innerHTML = siteContent['nav']['nav-item-3'];
+headerLinks[3].innerHTML = siteContent['nav']['nav-item-4'];
+headerLinks[4].innerHTML = siteContent['nav']['nav-item-5'];
+headerLinks[5].innerHTML = siteContent['nav']['nav-item-6'];
+
+let DOMisAwesome = document.querySelector('.cta-text h1');
+DOMisAwesome.innerHTML = siteContent['cta']['h1'];
+
+let button = document.querySelector('.cta-text button');
+button.innerHTML = siteContent['cta']['button'];
+
+let codeSnippet = document.getElementById('cta-img');
+codeSnippet.setAttribute('src', siteContent['cta']['img-src']);
+
+let featuresAboutH4 = document.querySelectorAll('.top-content .text-content h4');
+featuresAboutH4[0].innerHTML = siteContent['main-content']['features-h4'];
+featuresAboutH4[1].innerHTML = siteContent['main-content']['about-h4'];
+
+let featuresAboutContent = document.querySelectorAll('.top-content .text-content p');
+featuresAboutContent[0].innerHTML = siteContent['main-content']['features-content'];
+featuresAboutContent[1].innerHTML = siteContent['main-content']['about-content'];
+
+let middleImg = document.getElementById('middle-img');
+middleImg.setAttribute('src', siteContent['main-content']['middle-img-src']);
+
+let servicesProductVisionH4 = document.querySelectorAll('.bottom-content .text-content h4');
+servicesProductVisionH4[0].innerHTML = siteContent['main-content']['services-h4'];
+servicesProductVisionH4[1].innerHTML = siteContent['main-content']['product-h4'];
+servicesProductVisionH4[2].innerHTML = siteContent['main-content']['vision-h4'];
+
+let servicesProductVisionContent = document.querySelectorAll('.bottom-content .text-content p');
+servicesProductVisionContent[0].innerHTML = siteContent['main-content']['services-content'];
+servicesProductVisionContent[1].innerHTML = siteContent['main-content']['product-content'];
+servicesProductVisionContent[2].innerHTML = siteContent['main-content']['vision-content'];
+
+let contactH4 = document.querySelector('.contact h4');
+contactH4.innerHTML = siteContent['contact']['contact-h4'];
+
+let contactContent = document.querySelectorAll('.contact p');
+contactContent[0].innerHTML = siteContent['contact']['address'];
+contactContent[1].innerHTML = siteContent['contact']['phone'];
+contactContent[2].innerHTML = siteContent['contact']['email'];
+
+let footer = document.querySelector('footer p');
+footer.innerHTML = siteContent['footer']['copyright'];
+
+let makeNavGreen = document.querySelectorAll('a');
+makeNavGreen[0].setAttribute('style', 'color: green;')
+makeNavGreen[1].setAttribute('style', 'color: green;')
+makeNavGreen[2].setAttribute('style', 'color: green;')
+makeNavGreen[3].setAttribute('style', 'color: green;')
+makeNavGreen[4].setAttribute('style', 'color: green;')
+makeNavGreen[5].setAttribute('style', 'color: green;')
+
+let nav = document.querySelector('nav');
+let newLink = document.createElement('a');
+let pizza = document.createElement('a');
+
+nav.appendChild(newLink).innerHTML = 'New Link';
+newLink.setAttribute('href', '#');
+newLink.setAttribute('style', 'color: green;');
+
+nav.appendChild(pizza).innerHTML = 'Pizza';
+pizza.setAttribute('href', '#');
+pizza.setAttribute('style', 'color: green;');
