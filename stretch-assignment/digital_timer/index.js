2e513efc08494f98d072e2186a4cb5c50b7b30c8
alasalle <angela.lasalle91@gmail.com>
2018-08-28 23:50:40 -0400

    fixed timer issue

--- stretch-assignment/digital_timer/index.js
+++ stretch-assignment/digital_timer/index.js
@@ -7,15 +7,15 @@ function startTimer() {
     
     const msTensInterval = window.setInterval(function() {
         if (this.msTens.innerText === "9") {
-            this.msTens.innerText = "0";
+            this.msTens.innerText = 0;
         } else {
             this.msTens.innerText = (Number(this.msTens.innerText) || 0) + 1;
         }
-    }, 10);
+    }, 20);
 
     const msHundredsInterval = window.setInterval(function(){
         if (this.msHundreds.innerText === "9") {
-            this.msHundreds.innerText = "0";
+            this.msHundreds.innerText = 0;
         } else {
             this.msHundreds.innerText = (Number(this.msHundreds.innerText) || 0) + 1;
         }
@@ -23,7 +23,7 @@ function startTimer() {
 
     const secondOnesInterval = window.setInterval(function() {
         if (this.secondOnes.innerText === "9") {
-            this.secondOnes.innerText = "0";
+            this.secondOnes.innerText = 0;
         } else {
             this.secondOnes.innerText = (Number(this.secondOnes.innerText) || 0) + 1;
         }
