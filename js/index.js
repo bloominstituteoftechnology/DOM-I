c7edc9bbc147f18145ca116a6ed209033b88b4f1
Paul DeYonghe <p.deyonghe@gmail.com>
2018-08-28 21:32:03 -0500

    not much done here

--- js/index.js
+++ js/index.js
@@ -40,3 +40,6 @@ const siteContent = {
 // Example: Update the img src for the logo
 let logo = document.getElementById("logo-img");
 logo.setAttribute('src', siteContent["nav"]["img-src"])
+
+let header1 = document.getElementsByClassName("cta-text");
+header1.setAttribute('src', siteContent["cta"]["h1"]);
