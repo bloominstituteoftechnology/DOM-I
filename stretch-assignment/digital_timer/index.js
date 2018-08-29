d792907ad0cb2f2234e6f1c43d787b5557324e5c
alasalle <angela.lasalle91@gmail.com>
2018-08-29 00:45:00 -0400

    removed css redDigit class through timer so the digit color will reset

--- stretch-assignment/digital_timer/index.js
+++ stretch-assignment/digital_timer/index.js
@@ -4,58 +4,57 @@ const msHundreds = document.getElementById("msHundreds");
 const msTens = document.getElementById("msTens");
 
 function startTimer() {
-    
-    const msTensInterval = window.setInterval(function() {
-        if (this.msTens.innerText === "9") {
-            this.msTens.innerText = 0;
-        } else {
-            this.msTens.innerText = (Number(this.msTens.innerText) || 0) + 1;
-        }
-    }, 20);
+  const msTensInterval = window.setInterval(function() {
+    if (this.msTens.innerText === "9") {
+      this.msTens.innerText = 0;
+    } else {
+      this.msTens.innerText = (Number(this.msTens.innerText) || 0) + 1;
+    }
+  }, 20);
 
-    const msHundredsInterval = window.setInterval(function(){
-        if (this.msHundreds.innerText === "9") {
-            this.msHundreds.innerText = 0;
-        } else {
-            this.msHundreds.innerText = (Number(this.msHundreds.innerText) || 0) + 1;
-        }
-    }, 100);
+  const msHundredsInterval = window.setInterval(function() {
+    if (this.msHundreds.innerText === "9") {
+      this.msHundreds.innerText = 0;
+    } else {
+      this.msHundreds.innerText = (Number(this.msHundreds.innerText) || 0) + 1;
+    }
+  }, 100);
 
-    const secondOnesInterval = window.setInterval(function() {
-        if (this.secondOnes.innerText === "9") {
-            this.secondOnes.innerText = 0;
-        } else {
-            this.secondOnes.innerText = (Number(this.secondOnes.innerText) || 0) + 1;
-        }
-    }, 1000);
+  const secondOnesInterval = window.setInterval(function() {
+    if (this.secondOnes.innerText === "9") {
+      this.secondOnes.innerText = 0;
+    } else {
+      this.secondOnes.innerText = (Number(this.secondOnes.innerText) || 0) + 1;
+    }
+  }, 1000);
 
-    resetButton.disabled = true;
+  resetButton.disabled = true;
 
-    window.setTimeout(function() {
-        clearInterval(msTensInterval);
-        clearInterval(msHundredsInterval);
-        clearInterval(secondOnesInterval);
-        this.secondTens.innerText = 1;
-        let makeDigitRed = document.querySelectorAll(".digit");
-        for (let i = 0; i < makeDigitRed.length; i++) {
-          makeDigitRed[i].classList.add("redDigit");
-        }
-        resetButton.disabled = false;
-    }, 10000);
+  window.setTimeout(function() {
+    clearInterval(msTensInterval);
+    clearInterval(msHundredsInterval);
+    clearInterval(secondOnesInterval);
+    this.secondTens.innerText = 1;
+    let makeDigitRed = document.querySelectorAll(".digit");
+    for (let i = 0; i < makeDigitRed.length; i++) {
+      makeDigitRed[i].classList.add("redDigit");
+    }
+    resetButton.disabled = false;
+  }, 10000);
 }
 
 const body = document.getElementsByTagName("body")[0];
 
 const buttonsDiv = document.createElement("div");
-buttonsDiv.style.marginTop = "20px"
+buttonsDiv.style.marginTop = "20px";
 
 body.appendChild(buttonsDiv);
 
 const startButton = document.createElement("button");
 startButton.innerText = "Start";
 startButton.addEventListener("click", function() {
-    startButton.disabled = true;
-    return startTimer();
+  startButton.disabled = true;
+  return startTimer();
 });
 
 buttonsDiv.appendChild(startButton);
@@ -63,17 +62,17 @@ buttonsDiv.appendChild(startButton);
 const resetButton = document.createElement("button");
 resetButton.innerText = "Reset";
 resetButton.addEventListener("click", function() {
-    msTens.innerText = 0;
-    msHundreds.innerText = 0;
-    secondOnes.innerText = 0;
-    secondTens.innerText = 0;
-    for (let digit of document.getElementsByClassName("digit")) {
-        digit.style.color = "black";
-    }
-
-    startButton.disabled = false;
-    resetButton.disabled = true;
-    return resetTimer();
+  msTens.innerText = 0;
+  msHundreds.innerText = 0;
+  secondOnes.innerText = 0;
+  secondTens.innerText = 0;
+  let resetDigitColor = document.querySelectorAll(".digit");
+  for (let i = 0; i < resetDigitColor.length; i++) {
+    resetDigitColor[i].classList.remove("redDigit");
+  }
+  startButton.disabled = false;
+  resetButton.disabled = true;
+  return resetTimer();
 });
 
 buttonsDiv.appendChild(resetButton);
