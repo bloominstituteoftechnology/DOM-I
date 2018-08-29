6938e6dd588359277399747227f8f12ceafdfdef
Alex Ostrander <alexanderostrander23@gmail.com>
2018-08-28 23:02:21 -0400

    Getting the hang of the dom. Half way finished with the project

--- js/index.js
+++ js/index.js
@@ -9,7 +9,7 @@ const siteContent = {
     "img-src": "img/logo.png"
   },
   "cta": {
-    "h1": "DOM Is Awesome",
+    "h1": "DOM<br> Is<br> Awesome",
     "button": "Get Started",
     "img-src": "img/header-img.png"
   },
@@ -49,3 +49,23 @@ navArr[2].innerHTML = siteContent['nav']['nav-item-3'];
 navArr[3].innerHTML = siteContent['nav']['nav-item-4'];
 navArr[4].innerHTML = siteContent['nav']['nav-item-5'];
 navArr[5].innerHTML = siteContent['nav']['nav-item-6'];
+
+const ctaText = document.querySelector(".cta-text h1")
+ctaText.innerHTML = siteContent['cta']['h1'];
+
+const ctaImg = document.getElementById("cta-img");
+ctaImg.setAttribute('src', siteContent['cta']['img-src']);
+
+const ctaButton = document.querySelector('.cta-text button');
+ctaButton.innerHTML = siteContent['cta']['button'];
+
+const topHeader = document.querySelectorAll('.main-content .top-content .text-content h4');
+topHeader[0].innerHTML = siteContent['main-content']['features-h4'];
+topHeader[1].innerHTML = siteContent['main-content']['about-h4'];
+
+const topPara = document.querySelectorAll('.main-content .top-content .text-content p');
+topPara[0].innerHTML = siteContent['main-content']['features-content'];
+topPara[1].innerHTML = siteContent['main-content']['about-content'];
+
+const midImg = document.getElementById('middle-img');
+midImg.setAttribute('src', siteContent['main-content']['middle-img-src'])
