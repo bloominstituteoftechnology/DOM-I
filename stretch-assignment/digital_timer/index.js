0020f881553d5201690ebe48346d0a3332d9c866
Rick Fleming <elfeming111@yahoo.com>
2018-08-28 19:24:33 -0700

    Added start and reset buttons to index.js and styled buttons in style.css

--- stretch-assignment/digital_timer/index.js
+++ stretch-assignment/digital_timer/index.js
@@ -2,7 +2,7 @@
 const digits = document.getElementsByClassName('digits')[0];
 
 // Update timer function - 10s = 10000ms
-const startTimer = function(){
+const createTimer = function(){
     const startTime = Date.now();
     let isLastUpdate = false;
     return function(){
@@ -28,9 +28,51 @@ const startTimer = function(){
             for(let i = 0; i < digits.children.length; i++){
                 digits.children[i].classList.add('redDigit');
             }
+            startButton.disabled = false;
         }
     };
 }
 
-const updateTimer = startTimer();
-setInterval(updateTimer, 10);
+let interval;
+function startTimer(){
+    const updateTimer = createTimer();
+    interval = setInterval(updateTimer, 10);
+    startButton.disabled = true;
+    for(let i = 0; i < digits.children.length; i++){
+        digits.children[i].classList.remove('redDigit');
+    }
+}
+
+function resetTimer(){
+    clearInterval(interval);
+    digits.children[0].innerHTML = '0';
+    digits.children[1].innerHTML = '0';
+    digits.children[3].innerHTML = '0';
+    digits.children[4].innerHTML = '0';
+    startButton.disabled = false;
+    for(let i = 0; i < digits.children.length; i++){
+        digits.children[i].classList.remove('redDigit');
+    }
+}
+
+
+// Add button
+const body = document.getElementsByTagName('body')[0];
+const buttons = document.createElement('div');
+buttons.className = 'buttons'
+body.appendChild(buttons);
+
+const startButton = document.createElement('button');
+const resetButton = document.createElement('button');
+
+startButton.className = 'button';
+resetButton.className = 'button';
+
+startButton.innerHTML = 'Start';
+resetButton.innerHTML = 'Reset';
+
+startButton.onclick = startTimer;
+resetButton.onclick = resetTimer;
+
+buttons.appendChild(startButton);
+buttons.appendChild(resetButton);
