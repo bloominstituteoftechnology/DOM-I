d8027a64664fa49d87343100517439bac6419093
FreeKeyBoy <jjhutton81@gmail.com>
2018-08-29 17:31:10 -0700

    Finished assignment on file index.js. Meant to add more commits, but forgot.

--- js/index.js
+++ js/index.js
@@ -38,5 +38,82 @@ const siteContent = {
 };
 
 // Example: Update the img src for the logo
+// Setting img src
 let logo = document.getElementById("logo-img");
 logo.setAttribute('src', siteContent["nav"]["img-src"])
+
+let midImg = document.getElementById("cta-img");
+midImg.setAttribute('src', siteContent["cta"]["img-src"])
+
+let codeImg = document.getElementById("middle-img");
+codeImg.setAttribute('src', siteContent["main-content"]["middle-img-src"])
+
+/* Header Content */
+let nav = document.getElementsByTagName('nav')[0];
+let ctaTxt = document.getElementsByClassName('cta-text')[0];
+
+// Prepended tag
+let prepATag = document.createElement("a");
+prepATag.innerText = "Home";
+prepATag.setAttribute('href', '#');
+prepATag.style.color = "Green";
+
+// Appended tag
+let appATag = document.createElement("a");
+appATag.innerText = "Stuff";
+appATag.setAttribute('href', '#');
+appATag.style.color = "Green";
+
+nav.aTags = nav.getElementsByTagName('a');
+
+for (let i = 0; i < nav.aTags.length; i++) {
+  nav.aTags[i].innerText = siteContent["nav"]["nav-item-"+(i+1)];
+  nav.aTags[i].style.color = "Green";
+}
+
+nav.prepend(prepATag);
+nav.appendChild(appATag);
+ctaTxt.getElementsByTagName('h1')[0].innerText = siteContent["cta"]["h1"];
+ctaTxt.getElementsByTagName('button')[0].innerText = siteContent["cta"]["button"];
+
+
+/* Body Content */
+let mainContent = document.getElementsByClassName("main-content")[0];
+mainContent.h4 = mainContent.getElementsByTagName("h4");
+mainContent.p = mainContent.getElementsByTagName("p");
+
+
+/* Setting H4 text */
+mainContent.h4[0].innerText = siteContent["main-content"]["features-h4"];
+mainContent.h4[1].innerText = siteContent["main-content"]["about-h4"];
+mainContent.h4[2].innerText = siteContent["main-content"]["services-h4"];
+mainContent.h4[3].innerText = siteContent["main-content"]["product-h4"];
+mainContent.h4[4].innerText = siteContent["main-content"]["vision-h4"];
+
+/* Setting P text */
+mainContent.p[0].innerText = siteContent["main-content"]["features-content"];
+mainContent.p[1].innerText = siteContent["main-content"]["about-content"];
+mainContent.p[2].innerText = siteContent["main-content"]["services-content"];
+mainContent.p[3].innerText = siteContent["main-content"]["product-content"];
+mainContent.p[4].innerText = siteContent["main-content"]["vision-content"];
+
+/* End body content */
+
+/* Contact info */
+let contactInfo = document.getElementsByClassName("contact")[0];
+contactInfo.h4 = contactInfo.getElementsByTagName("h4");
+contactInfo.p = contactInfo.getElementsByTagName("p");
+
+contactInfo.h4[0].innerText = siteContent["contact"]["contact-h4"];
+contactInfo.p[0].innerText = siteContent["contact"]["address"];
+contactInfo.p[1].innerText = siteContent["contact"]["phone"];
+contactInfo.p[2].innerText = siteContent["contact"]["email"];
+/* End contact info */
+
+/* Footer */
+let footer = document.getElementsByTagName("footer")[0];
+footer.p = footer.getElementsByTagName("p")[0];
+footer.p.innerText = siteContent["footer"]["copyright"];
+/* End footer */
+
+
