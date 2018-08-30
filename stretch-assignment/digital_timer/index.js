5b5d1b85cc5cc3a2e97e4fbbb80a401dd7ec0f65
Abigayle Franklin <abifanklin@gmail.com>
2018-08-29 20:06:08 -0500

    digital counter partially working

--- stretch-assignment/digital_timer/index.js
+++ stretch-assignment/digital_timer/index.js
@@ -2,25 +2,38 @@
 window.onload = function(){
     document.getElementById("msTens").innerHTML = '0';
     let tms = setInterval(function(){
+        if (document.getElementById("msTens").innerHTML < 9) {
         document.getElementById("msTens").textContent++;
+        } else {
+        document.getElementById("msTens").innerHTML = '0';}
     }, 10);
-    setTimeout(function(){clearInterval(tms); }, 90);
+    
+    
 
     document.getElementById("msHundreds").innerHTML = '0';
     let hms = setInterval(function(){
+        if (document.getElementById("msHundreds").innerHTML < 9) {
         document.getElementById("msHundreds").textContent++;
+        } else {
+        document.getElementById("msHundreds").innerHTML = '0';}
     }, 100);
-    setTimeout(function(){clearInterval(hms)}, 900);
+    
 
     document.getElementById("secondOnes").innerHTML = '0';
     let os = setInterval(function(){
+        if (document.getElementById("secondOnes").innerHTML < 10) {
         document.getElementById("secondOnes").textContent++;
+        } else {
+        document.getElementById("secondOnes").innerHTML = '0';}
     }, 1000);
-    setTimeout(function(){clearInterval(os)}, 10000);
+    
 
-    document.getElementById("secondTens").innerHTML = '0';
-    let ts = setInterval(function(){
-        document.getElementById("secondTens").textContent++;
-    }, 10000);
-    setTimeout(function(){clearInterval(ts)}, 100000);
+    // document.getElementById("secondTens").innerHTML = '0';
+    // let ts = setInterval(function(){
+    //     if (document.getElementById("secondTens").innerHTML <= 1) {
+    //     document.getElementById("secondTens").textContent++;
+    //     } else {
+    //     document.getElementById("secondTens").innerHTML = '0';}
+    // }, 10);
+    
 }
