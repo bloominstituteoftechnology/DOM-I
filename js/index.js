bbd9cc99833860a7acf3ba814a20939d95f39921
Denise Escobar <descobar1490@gmail.com>
2018-08-30 20:45:03 -0500

    was able to add missing contact paragraphs

--- js/index.js
+++ js/index.js
@@ -14,26 +14,26 @@ const siteContent = {
     "img-src": "img/header-img.png"
   },
   "main-content": {
-    "features-h4":"Features",
+    "features-h4": "Features",
     "features-content": "Features content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.",
-    "about-h4":"About",
+    "about-h4": "About",
     "about-content": "About content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.",
     "middle-img-src": "img/mid-page-accent.jpg",
-    "services-h4":"Services",
+    "services-h4": "Services",
     "services-content": "Services content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.",
-    "product-h4":"Product",
+    "product-h4": "Product",
     "product-content": "Product content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.",
-    "vision-h4":"Vision",
+    "vision-h4": "Vision",
     "vision-content": "Vision content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.",
   },
   "contact": {
-    "contact-h4" : "Contact",
-    "address" : "123 Way 456 Street Somewhere, USA",
-    "phone" : "1 (888) 888-8888",
-    "email" : "sales@greatidea.io",
+    "contact-h4": "Contact",
+    "address": "123 Way 456 Street Somewhere, USA",
+    "phone": "1 (888) 888-8888",
+    "email": "sales@greatidea.io",
   },
   "footer": {
-    "copyright" : "Copyright Great Idea! 2018"
+    "copyright": "Copyright Great Idea! 2018"
   },
 };
 
@@ -42,19 +42,13 @@ let logo = document.getElementById("logo-img");
 logo.setAttribute('src', siteContent["nav"]["img-src"]);
 
 let nav = document.getElementsByTagName("a");
-nav[0].style.color = "green";
-nav[1].style.color = "green";
-nav[2].style.color = "green";
-nav[3].style.color = "green";
-nav[4].style.color = "green";
-nav[5].style.color = "green";
-nav[0].innerHTML = siteContent["nav"]["nav-item-1"];
-nav[1].innerHTML = siteContent["nav"]["nav-item-2"];
-nav[2].innerHTML = siteContent["nav"]["nav-item-3"];
-nav[3].innerHTML = siteContent["nav"]["nav-item-4"];
-nav[4].innerHTML = siteContent["nav"]["nav-item-5"];
-nav[5].innerHTML = siteContent["nav"]["nav-item-6"];
+for (i = 0; i < nav.length - 1; i++) {
+  nav[i].style.color = "green";
+}
 
+for (i = 0; i < nav.length - 1; i++) {
+  nav[i].innerHTML = siteContent["nav"]["nav-item-1"];
+}
 
 let ctaImg = document.getElementById("cta-img");
 ctaImg.setAttribute("src", siteContent["cta"]["img-src"]);
@@ -66,6 +60,7 @@ let ctabutn = document.querySelector("button");
 ctabutn.innerHTML = siteContent["cta"]["button"];
 
 let allH4 = document.querySelectorAll("h4");
+allH4[i].innerHTML = siteContent["main-content"]["features-h4"];
 allH4[0].innerHTML = siteContent["main-content"]["features-h4"];
 allH4[1].innerHTML = siteContent["main-content"]["about-h4"];
 allH4[2].innerHTML = siteContent["main-content"]["services-h4"];
@@ -84,8 +79,14 @@ maincontentp[4].innerHTML = siteContent["main-content"]["vision-content"];
 let middleimg = document.getElementById("middle-img");
 middleimg.setAttribute("src", siteContent["main-content"]["middle-img-src"]);
 
-//let contPara = document.querySelectorAll(".contact");
-//contPara[1].innerHTML = siteContent["contact"]["address"];
+let contPara = document.querySelectorAll(".contact p");
+contPara[0].innerHTML = siteContent["contact"]["address"];
+contPara[1].innerHTML = siteContent["contact"]["phone"];
+contPara[2].innerHTML = siteContent["contact"]["email"];
 
 let footerPara = document.getElementsByTagName('p');
 footerPara[footerPara.length - 1].innerHTML = siteContent["footer"]["copyright"];
+
+let newNav = document.createElement("a");
+newNav.innerHTML = "Blog";
+nav.appendChild(newNav);
