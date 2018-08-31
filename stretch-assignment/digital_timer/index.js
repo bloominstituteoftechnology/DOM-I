45b3b939e4223135757bbefa72e81dc063e57d13
Bruce Cabanayan <bcabanayan@Bruces-MacBook-Pro.local>
2018-08-30 20:50:37 -0700

    Added timer to stretch project

--- stretch-assignment/digital_timer/index.js
+++ stretch-assignment/digital_timer/index.js
@@ -1,18 +1,43 @@
-
-// document.getElementById('msTens').innerHTML = 0
+let secondTens = document.querySelector('#secondTens');
+let secondOnes = document.querySelector('#secondOnes');
+let msHundreds = document.querySelector('#msHundreds');
+let msTens = document.querySelector('#msTens');
 
 let counter = 0;
-let incrementTimer = function () {
+
+let intervalTimer = function () {
+    setInterval(countUp, 10);
+} 
+
+function countUp() {
     counter++;
+    if (counter < 10) {
+        msTens.innerHTML = (counter.toString())[0];
+    }
+    else if (counter < 100) {
+        msTens.innerHTML = (counter.toString())[1];
+        msHundreds.innerHTML = (counter.toString())[0];
+    }
+    else if (counter < 1000) {
+        msTens.innerHTML = (counter.toString())[2];
+        msHundreds.innerHTML = (counter.toString())[1];
+        secondOnes.innerHTML = (counter.toString())[0];
+    } 
+    else if (counter < 10000) {
+        msTens.innerHTML = (counter.toString())[3];
+        msHundreds.innerHTML = (counter.toString())[2];
+        secondOnes.innerHTML = (counter.toString())[1];
+        secondTens.innerHTML = (counter.toString())[0];
+    }
+    else {
+        clearInterval(intervalTimer);
+        msTens.innerHTML = '0';
+        msHundreds.innerHTML = '0';
+        secondOnes.innerHTML = '0';
+        secondTens.innerHTML = '1';  
+    }
 }
-let timerString = 'test';
 
-let timerTest = document.querySelector('p');
-// timerTest.innerHTML = 'test';
-let timerFunction = window.setInterval(100, incrementTimer);
+let startButton = document.querySelector('#start');
 
-
-
-
-
-
+startButton.addEventListener('click', intervalTimer);
