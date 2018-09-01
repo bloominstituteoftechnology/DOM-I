2bf75d5d4b45f624b62222ea849b27aac57ef27f
Oliver Abreu <oliverabreu@Olivers-MacBook.local>
2018-09-01 18:19:52 -0400

    Completed: Task 2: Create selectors to point your data into elements

--- js/index.js
+++ js/index.js
@@ -9,7 +9,7 @@ const siteContent = {
     "img-src": "img/logo.png"
   },
   "cta": {
-    "h1": "DOM Is Awesome",
+    "h1": "DOM <br> Is <br>  Awesome",
     "button": "Get Started",
     "img-src": "img/header-img.png"
   },
@@ -37,6 +37,69 @@ const siteContent = {
   },
 };
 
-// Example: Update the img src for the logo
-let logo = document.getElementById("logo-img");
-logo.setAttribute('src', siteContent["nav"]["img-src"])
+// *** TASK 2: CREATE SELECTORS TO POINT YOUR DATA ("SiteContent") INTO ELEMENTS *** //
+
+// <-- (RE)CREATING THE LOGO SELECTOR--> //
+const logo = document.getElementById("logo-img");
+logo.setAttribute("src", siteContent["nav"]["img-src"]);
+
+// <-- CREATING THE HEADER NAVIGATION SELECTOR --> //
+const headerNav = document.querySelectorAll("nav a");
+headerNav[0].innerHTML = siteContent["nav"]["nav-item-1"];
+headerNav[1].innerHTML = siteContent["nav"]["nav-item-2"];
+headerNav[2].innerHTML = siteContent["nav"]["nav-item-3"];
+headerNav[3].innerHTML = siteContent["nav"]["nav-item-4"];
+headerNav[4].innerHTML = siteContent["nav"]["nav-item-5"];
+headerNav[5].innerHTML = siteContent["nav"]["nav-item-6"];
+
+// <-- CREATING THE HEADER CTA H1 SELECTOR --> //
+const headerCTA = document.querySelector(".cta h1");
+headerCTA.innerHTML = siteContent["cta"]["h1"];
+
+// <-- CREATING THE HEADER CTA BUTTON SELECTOR --> //
+const ctaButton = document.querySelector(".cta button");
+ctaButton.innerHTML = siteContent["cta"]["button"];
+
+// <-- CREATING THE HEADER CTA IMAGE SELECTOR --> //
+const ctaIMG = document.getElementById("cta-img");
+ctaIMG.setAttribute("src", siteContent["cta"]["img-src"]);
+
+// <-- CREATING THE TOP CONTENT HEADING SELECTOR --> //
+const topHEADER = document.querySelectorAll(".main-content .text-content h4")
+topHEADER[0].innerHTML = siteContent["main-content"]["features-h4"];
+topHEADER[1].innerHTML = siteContent["main-content"]["about-h4"];
+
+// <-- CREATING THE TOP CONTENT PARAGRAPH SELECTOR --> //
+const topPARAGRAPH = document.querySelectorAll(".main-content .top-content .text-content p")
+topPARAGRAPH[0].innerHTML = siteContent["main-content"]["features-content"];
+topPARAGRAPH[1].innerHTML = siteContent["main-content"]["about-content"];
+
+// <-- CREATING THE MIDDLE IMAGE SELECTOR --> //
+const middleIMG = document.getElementById("middle-img");
+middleIMG.setAttribute("src", siteContent["main-content"]["middle-img-src"]);
+
+// <-- CREATING THE BOTTOM CONTENT HEADING SELECTOR --> //
+const bottomHEADER = document.querySelectorAll(".main-content .bottom-content .text-content h4");
+bottomHEADER[0].innerHTML = siteContent["main-content"]["services-h4"];
+bottomHEADER[1].innerHTML = siteContent["main-content"]["product-h4"];
+bottomHEADER[2].innerHTML = siteContent["main-content"]["vision-h4"];
+
+// <-- CREATING THE BOTTOM CONTENT HEADING SELECTOR --> //
+const bottomPARAGRAPH = document.querySelectorAll(".main-content .bottom-content .text-content p")
+bottomPARAGRAPH[0].innerHTML = siteContent["main-content"]["services-content"];
+bottomPARAGRAPH[1].innerHTML = siteContent["main-content"]["product-content"];
+bottomPARAGRAPH[2].innerHTML = siteContent["main-content"]["vision-content"];
+
+// <-- CREATING THE CONTACT HEADING SELECTOR --> //
+const contactHEADING = document.querySelector(".contact h4");
+contactHEADING.innerHTML = siteContent["contact"]["contact-h4"];
+
+// <-- CREATING THE CONTACT PARAGRAPH SELECTOR --> //
+const contactPARAGRAPH = document.querySelectorAll(".contact p");
+contactPARAGRAPH[0].innerHTML = siteContent["contact"]["address"];
+contactPARAGRAPH[1].innerHTML = siteContent["contact"]["phone"];
+contactPARAGRAPH[2].innerHTML = siteContent["contact"]["email"];
+
+// <-- CREATING THE COPYRIGHT SELECTOR --> //
+const copyright = document.querySelector("footer p");
+copyright.innerHTML = siteContent["footer"]["copyright"];
