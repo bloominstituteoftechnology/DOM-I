585da8870bbd7e2dfb7a7129f9242406f7a80d75
Elvis Ibarra <ielvisd@gmail.com>
2018-09-16 16:20:22 -0700

    Finished header, cta & main-content

--- js/index.js
+++ js/index.js
@@ -14,29 +14,96 @@ const siteContent = {
     "img-src": "img/header-img.png"
   },
   "main-content": {
-    "features-h4":"Features",
+    "features-h4": "Features",
     "features-content": "Features content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.",
-    "about-h4":"About",
+    "about-h4": "About",
     "about-content": "About content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.",
     "middle-img-src": "img/mid-page-accent.jpg",
-    "services-h4":"Services",
+    "services-h4": "Services",
     "services-content": "Services content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.",
-    "product-h4":"Product",
+    "product-h4": "Product",
     "product-content": "Product content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.",
-    "vision-h4":"Vision",
+    "vision-h4": "Vision",
     "vision-content": "Vision content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.",
   },
   "contact": {
-    "contact-h4" : "Contact",
-    "address" : "123 Way 456 Street Somewhere, USA",
-    "phone" : "1 (888) 888-8888",
-    "email" : "sales@greatidea.io",
+    "contact-h4": "Contact",
+    "address": "123 Way 456 Street Somewhere, USA",
+    "phone": "1 (888) 888-8888",
+    "email": "sales@greatidea.io",
   },
   "footer": {
-    "copyright" : "Copyright Great Idea! 2018"
+    "copyright": "Copyright Great Idea! 2018"
   },
 };
 
-// Example: Update the img src for the logo
+//Example: Update the img src for the logo
 let logo = document.getElementById("logo-img");
 logo.setAttribute('src', siteContent["nav"]["img-src"])
+
+// Update the nav links
+let navitem1 = document.getElementsByTagName("a")[0];
+navitem1.innerHTML = siteContent["nav"]["nav-item-1"]
+
+let navitem2 = document.getElementsByTagName("a")[1];
+navitem2.innerHTML = siteContent["nav"]["nav-item-2"]
+
+let navitem3 = document.getElementsByTagName("a")[2];
+navitem3.innerHTML = siteContent["nav"]["nav-item-3"]
+
+let navitem4 = document.getElementsByTagName("a")[3];
+navitem4.innerHTML = siteContent["nav"]["nav-item-4"]
+
+let navitem5 = document.getElementsByTagName("a")[4];
+navitem5.innerHTML = siteContent["nav"]["nav-item-5"]
+
+let navitem6 = document.getElementsByTagName("a")[5];
+navitem6.innerHTML = siteContent["nav"]["nav-item-6"]
+
+//CTA
+let ctatitle = document.getElementsByTagName("h1")[0];
+ctatitle.innerHTML =  siteContent["cta"]["h1"];
+
+let buttontext = document.getElementsByTagName("button")[0];
+buttontext.innerHTML =  siteContent["cta"]["button"];
+
+let headerimage = document.getElementById("cta-img");
+headerimage.setAttribute('src', siteContent["cta"]["img-src"]);
+
+//Main-Content
+let featurestitle= document.getElementsByTagName("h4")[0];
+featurestitle.innerHTML =  siteContent["main-content"]["features-h4"];
+
+let abouttitle= document.getElementsByTagName("h4")[1];
+abouttitle.innerHTML =  siteContent["main-content"]["about-h4"];
+
+let servicestitle= document.getElementsByTagName("h4")[2];
+servicestitle.innerHTML =  siteContent["main-content"]["services-h4"];
+
+let producttitle= document.getElementsByTagName("h4")[3];
+producttitle.innerHTML =  siteContent["main-content"]["product-h4"];
+
+let visiontitle= document.getElementsByTagName("h4")[4];
+visiontitle.innerHTML =  siteContent["main-content"]["vision-h4"];
+
+let featurescontent= document.getElementsByTagName("p")[0];
+featurescontent.innerHTML =  siteContent["main-content"]["features-content"];
+
+let aboutcontent= document.getElementsByTagName("p")[1];
+aboutcontent.innerHTML =  siteContent["main-content"]["about-content"];
+
+let middleimage = document.getElementById("middle-img");
+middleimage.setAttribute('src', siteContent["main-content"]["middle-img-src"]);
+
+let servicescontent= document.getElementsByTagName("p")[2];
+servicescontent.innerHTML =  siteContent["main-content"]["services-content"];
+
+let productcontent= document.getElementsByTagName("p")[3];
+productcontent.innerHTML =  siteContent["main-content"]["product-content"];
+
+let visioncontent= document.getElementsByTagName("p")[4];
+visioncontent.innerHTML =  siteContent["main-content"]["product-content"];
+
+//Contact
+
+
