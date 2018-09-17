50b9c0c8fbdb95e3cb64c81703870674732d2b55
Trevor Lewis <tenzingtitan@gmail.com>
2018-09-17 14:07:45 -0700

    Mockup complete, working on task #4

--- js/index.js
+++ js/index.js
@@ -76,6 +76,7 @@ h4[1].innerHTML = siteContent['main-content']['about-h4'];
 h4[2].innerHTML = siteContent['main-content']['services-h4'];
 h4[3].innerHTML = siteContent['main-content']['product-h4'];
 h4[4].innerHTML = siteContent['main-content']['vision-h4'];
+h4[5].innerHTML = siteContent['contact']['contact-h4'];
 
 let p = document.querySelectorAll("p");
 p[0].innerHTML = siteContent['main-content']['features-content'];
@@ -83,6 +84,10 @@ p[1].innerHTML = siteContent['main-content']['about-content'];
 p[2].innerHTML = siteContent['main-content']['services-content'];
 p[3].innerHTML = siteContent['main-content']['product-content'];
 p[4].innerHTML = siteContent['main-content']['vision-content'];
+p[5].innerHTML = siteContent['contact']['address'];
+p[6].innerHTML = siteContent['contact']['phone'];
+p[7].innerHTML = siteContent['contact']['email'];
+p[8].innerHTML = siteContent['footer']['copyright'];
 
-// Contact
+
 
