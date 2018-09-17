fda4763736ac0f345daf6d844c50322afd9cf6ef
Wanda <doubleuevans@gmail.com>
2018-09-17 14:16:35 -0500

    initial commit/pull

--- js/index.js
+++ js/index.js
@@ -40,3 +40,11 @@ const siteContent = {
 // Example: Update the img src for the logo
 let logo = document.getElementById("logo-img");
 logo.setAttribute('src', siteContent["nav"]["img-src"])
+
+//nav links
+const navItems = document.querySelectorAll('a');
+for(let i = 0; i < navItems.length; i++) {
+  navItems[0].innerHTML = nav.nav-item-1
+}
+//
+
