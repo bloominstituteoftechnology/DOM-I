9350f618d658fcfda78f728b2c440988be50c51a
Brandon Strellis <bstrellis@gmail.com>
2018-09-17 16:13:11 -0400

    tasks 1,2,3 complete

--- js/index.js
+++ js/index.js
@@ -40,3 +40,46 @@ const siteContent = {
 // Example: Update the img src for the logo
 let logo = document.getElementById("logo-img");
 logo.setAttribute('src', siteContent["nav"]["img-src"])
+
+// Update nav items with text
+let navAs = document.querySelectorAll('nav a');
+console.log(navAs);
+navAs.style.color = 'green';
+navAs[0].innerText = siteContent.nav["nav-item-1"];
+navAs[1].innerText = siteContent.nav["nav-item-2"];
+navAs[2].innerText = siteContent.nav["nav-item-3"];
+navAs[3].innerText = siteContent.nav["nav-item-4"];
+navAs[4].innerText = siteContent.nav["nav-item-5"];
+navAs[5].innerText = siteContent.nav["nav-item-6"];
+
+// Update CTA
+document.querySelector('.cta-text h1').innerHTML = siteContent.cta.h1;
+document.querySelector('.cta-text button').innerHTML = siteContent.cta.button;
+document.querySelector('#cta-img').src = siteContent.cta["img-src"];
+
+// Update main content
+let mainContentH4s = document.querySelectorAll('.main-content h4');
+mainContentH4s[0].innerText = siteContent["main-content"]["features-h4"];
+mainContentH4s[1].innerText = siteContent["main-content"]["about-h4"];
+mainContentH4s[2].innerText = siteContent["main-content"]["services-h4"];
+mainContentH4s[3].innerText = siteContent["main-content"]["product-h4"];
+mainContentH4s[4].innerText = siteContent["main-content"]["vision-h4"];
+
+let mainContentPs = document.querySelectorAll('.main-content p');
+mainContentPs[0].innerText = siteContent["main-content"]["features-content"];
+mainContentPs[1].innerText = siteContent["main-content"]["about-content"];
+mainContentPs[2].innerText = siteContent["main-content"]["services-content"];
+mainContentPs[3].innerText = siteContent["main-content"]["product-content"];
+mainContentPs[4].innerText = siteContent["main-content"]["vision-content"];
+
+document.querySelector('.middle-img').src = siteContent["main-content"]["middle-img-src"];
+
+// Update contact
+document.querySelector('.contact h4').innerText = siteContent.contact["contact-h4"];
+let contactPs = document.querySelectorAll('.contact p');
+contactPs[0].innerText = siteContent.contact.address;
+contactPs[1].innerText = siteContent.contact.phone;
+contactPs[2].innerText = siteContent.contact.email;
+
+// Update footer
+document.querySelector('footer p').innerText = siteContent.footer.copyright;
