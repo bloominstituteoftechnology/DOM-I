8849ad1654efdef7dc265987340b412cd9b0b3b1
kyranmccann <kyran.mccann@gmail.com>
2018-09-17 14:52:48 -0700

    changing to .innerText

--- stretch-assignment/digital_timer/index.js
+++ stretch-assignment/digital_timer/index.js
@@ -39,10 +39,10 @@ function timer() {
 
         }
 
-        document.getElementById('msTens').innerHTML = mTens;
-        document.getElementById('msHundreds').innerHTML = mHundreds;
-        document.getElementById('secondOnes').innerHTML = sOnes;
-        document.getElementById('secondTens').innerHTML = sTens;
+        document.getElementById('msTens').innerText = mTens;
+        document.getElementById('msHundreds').innerText = mHundreds;
+        document.getElementById('secondOnes').innerText = sOnes;
+        document.getElementById('secondTens').innerText = sTens;
        },   10);
 }
 
@@ -52,10 +52,10 @@ mHundreds = 0;
 sOnes = 0;
 sTens = 0;
 
-document.getElementById('msTens').innerHTML = mTens;
-document.getElementById('msHundreds').innerHTML = mHundreds;
-document.getElementById('secondOnes').innerHTML = sOnes;
-document.getElementById('secondTens').innerHTML = sTens;
+document.getElementById('msTens').innerText = mTens;
+document.getElementById('msHundreds').innerText = mHundreds;
+document.getElementById('secondOnes').innerText = sOnes;
+document.getElementById('secondTens').innerText = sTens;
 let blackDigits = document.getElementsByClassName('digit');
 
 for (let i = 0; i < blackDigits.length; i++) {
