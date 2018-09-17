2b82f607d59e2741977bece6bc2da0083d8d3049
Jasmine Galang <jsmn.glng@gmail.com>
2018-09-17 12:31:04 -0700

    created selectors and update html with JSON data

--- js/index.js
+++ js/index.js
@@ -38,5 +38,53 @@ const siteContent = {
 };
 
 // Example: Update the img src for the logo
-let logo = document.getElementById("logo-img");
-logo.setAttribute('src', siteContent["nav"]["img-src"])
+let logo = document.getElementById('logo-img');
+logo.setAttribute('src', siteContent['nav']['img-src']);
+
+let navItems = document.querySelectorAll('header nav a');
+navItems.forEach((item, i) => item.innerText = siteContent['nav'][`nav-item-${i+1}`]);
+
+let ctaText = document.querySelector('.cta-text h1');
+let ctaBtn = document.querySelector('.cta-text button');
+let ctaImg = document.getElementById('cta-img');
+ctaText.innerText = siteContent['cta']['h1'];
+ctaBtn.innerText = siteContent['cta']['button'];
+ctaImg.src = siteContent['cta']['img-src'];
+
+let topContent = document.querySelector('.main-content .top-content');
+let featuresH4 = topContent.children[0].children[0];
+let featuresContent = topContent.children[0].children[1];
+featuresH4.innerText = siteContent['main-content']['features-h4'];
+featuresContent.innerText = siteContent['main-content']['features-content'];
+
+let middleImg = document.getElementById('middle-img');
+middleImg.src = siteContent['main-content']['middle-img-src'];
+
+let bottomContent = document.querySelector('.main-content .bottom-content');
+let servicesH4 = bottomContent.children[0].children[0];
+let servicesContent = bottomContent.children[0].children[1];
+servicesH4.innerText = siteContent['main-content']['services-h4'];
+servicesContent.innerText = siteContent['main-content']['services-content'];
+
+let productH4 = bottomContent.children[1].children[0];
+let productContent = bottomContent.children[1].children[1];
+productH4.innerText = siteContent['main-content']['product-h4'];
+productContent.innerText = siteContent['main-content']['product-content'];
+
+let visionH4 = bottomContent.children[2].children[0];
+let visionContent = bottomContent.children[2].children[1];
+visionH4.innerText = siteContent['main-content']['vision-h4'];
+visionContent.innerText = siteContent['main-content']['vision-content'];
+
+let contact = document.querySelector('.contact');
+let contactH4 = contact.children[0];
+let contactAddress = contact.children[1];
+let contactNumber = contact.children[2];
+let contactEmail = contact.children[3];
+contactH4.innerText = siteContent['contact']['contact-h4'];
+contactAddress.innerText = siteContent['contact']['address'];
+contactNumber.innerText = siteContent['contact']['phone'];
+contactEmail.innerText = siteContent['contact']['email'];
+
+let footerText = document.querySelector('footer p');
+footerText.innerText = siteContent['footer']['copyright'];
