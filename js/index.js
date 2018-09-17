4b06958c82fca66ff1200f2b8b281db8a538a9b9
Brian G <brian.gregorius@gmail.com>
2018-09-17 16:01:54 -0400

    completed nav and main content

--- js/index.js
+++ js/index.js
@@ -39,4 +39,47 @@ const siteContent = {
 
 // Example: Update the img src for the logo
 let logo = document.getElementById("logo-img");
-logo.setAttribute('src', siteContent["nav"]["img-src"])
+logo.setAttribute('src', siteContent["nav"]["img-src"]);
+console.log(logo);
+
+
+let navItems = document.querySelectorAll('nav a');
+
+navItems[0].innerText = siteContent['nav']['nav-item-1'];
+navItems[1].innerText = siteContent['nav']['nav-item-2'];
+navItems[2].innerText = siteContent['nav']['nav-item-3'];
+navItems[3].innerText = siteContent['nav']['nav-item-4'];
+navItems[4].innerText = siteContent['nav']['nav-item-5'];
+navItems[5].innerText = siteContent['nav']['nav-item-6'];
+
+const introText = document.querySelector('h1');
+introText.innerText = siteContent['cta']['h1'];
+
+const buttonText = document.querySelector('button');
+buttonText.innerText = siteContent['cta']['button'];
+
+const introPic = document.getElementById('cta-img');
+introPic.setAttribute('src', siteContent['cta']['img-src']);
+
+const mainTextH4 = document.querySelectorAll('.top-content h4');
+const mainP = document.querySelectorAll('.top-content p');
+
+mainTextH4[0].innerText = siteContent['main-content']['features-h4'];
+mainP[0].innerText = siteContent['main-content']['features-content'];
+mainTextH4[1].innerText = siteContent['main-content']['about-h4'];
+mainP[1].innerText = siteContent['main-content']['about-content'];
+
+const mainPic = document.getElementById('middle-img');
+mainPic.setAttribute('src', siteContent['main-content']['middle-img-src']);
+
+const bottomInfo = document.querySelectorAll('.bottom-content h4');
+const bottomParag = document.querySelectorAll('.bottom-content p');
+
+bottomInfo[0].innerText = siteContent['main-content']['services-h4'];
+bottomParag[0].innerText = siteContent['main-content']['services-content'];
+bottomInfo[1].innerText = siteContent['main-content']['product-h4'];
+bottomParag[1].innerText = siteContent['main-content']['product-content'];
+bottomInfo[2].innerText= siteContent['main-content']['vision-h4'];
+bottomParag[2].innerText = siteContent['main-content']['vision-content'];
+
+
