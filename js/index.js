de3d9b3ff69ef4476b208c168a399479ef782ded
Yusuf Nafey <yusufnafey@yahoo.com>
2018-09-17 14:39:00 -0700

    everything complete except: footer, color of 2 new nav items

--- js/index.js
+++ js/index.js
@@ -41,13 +41,35 @@ const siteContent = {
 let logo = document.getElementById("logo-img");
 logo.src = siteContent.nav["img-src"]
 
-let navItems = document.querySelectorAll('a')
+let navItems = document.querySelectorAll('header nav a')
 navItems[0].innerText = siteContent.nav["nav-item-1"]
 navItems[1].innerText = siteContent.nav["nav-item-2"]
 navItems[2].innerText = siteContent.nav["nav-item-3"]
 navItems[3].innerText = siteContent.nav["nav-item-4"]
 navItems[4].innerText = siteContent.nav["nav-item-5"]
 navItems[5].innerText = siteContent.nav["nav-item-6"]
+
+let resume = document.createElement('a')
+let portfolio = document.createElement('a')
+
+let mainNav = document.querySelector('header nav')
+
+mainNav.prepend(resume);
+mainNav.appendChild(portfolio);
+
+resume.innerText = 'Resume'
+portfolio.innerText = 'Portfolio'
+resume.href = '#'
+portfolio.href = '#'
+
+navItems[0].style.color = 'green'
+navItems[1].style.color = 'green'
+navItems[2].style.color = 'green'
+navItems[3].style.color = 'green'
+navItems[4].style.color = 'green'
+navItems[5].style.color = 'green'
+
+
 
 let ctaText = document.querySelector('.cta .cta-text h1')
 ctaText.innerText = siteContent.cta["h1"]
@@ -59,18 +81,26 @@ let ctaImage = document.querySelector('#cta-img')
 ctaImage.src = siteContent.cta["img-src"]
 
 
+
+
+
 let topContentHeaders = document.querySelectorAll('.top-content .text-content h4')
 topContentHeaders[0].innerText = siteContent["main-content"]["features-h4"]
 topContentHeaders[1].innerText = siteContent["main-content"]["about-h4"]
-
 
 let topContentText = document.querySelectorAll('.top-content .text-content p')
 topContentText[0].innerText = siteContent["main-content"]["features-content"]
 topContentText[1].innerText = siteContent["main-content"]["features-content"]
 
 
+
+
+
 let middleImage = document.querySelector('.middle-img')
 middleImage.src = siteContent["main-content"]["middle-img-src"]
+
+
+
 
 
 let bottomContentHeaders = document.querySelectorAll('.bottom-content .text-content h4')
@@ -78,11 +108,13 @@ bottomContentHeaders[0].innerText = siteContent["main-content"]["services-h4"]
 bottomContentHeaders[1].innerText = siteContent["main-content"]["product-h4"]
 bottomContentHeaders[2].innerText = siteContent["main-content"]["vision-h4"]
 
-
 let bottomContentText = document.querySelectorAll('.bottom-content .text-content p')
 bottomContentText[0].innerText = siteContent["main-content"]["services-content"]
 bottomContentText[1].innerText = siteContent["main-content"]["product-content"]
 bottomContentText[2].innerText = siteContent["main-content"]["vision-content"]
+
+
+
 
 
 // let contactHeader = document.querySelectorAll('.content h4')
@@ -95,9 +127,16 @@ contactText[1].innerText = siteContent.contact.phone
 contactText[2].innerText = siteContent.contact.email
 
 
+
+
+
 let footerText = document.querySelector('.footer p')
 footer.innerText = siteContent.footer.copyright
 
-
 // let footerText = document.querySelector('.footer p')
 // footerText.innerText = siteContent.footer.copyright
+
+
+
+
+// ======================TASK 4======================
