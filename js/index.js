88f9740afd9f367dcea737b3885b53e185728453
Phil Hart <phil@Phils-iMac.local>
2018-09-17 16:37:12 -0600

    Almost finished

--- js/index.js
+++ js/index.js
@@ -1,6 +1,6 @@
 const siteContent = {
   "nav": {
-    "nav-item-1": "Services",1
+    "nav-item-1": "Services",
     "nav-item-2": "Product",
     "nav-item-3": "Vision",
     "nav-item-4": "Features",
@@ -14,29 +14,71 @@ const siteContent = {
     "img-src": "img/header-img.png"
   },
   "main-content": {
-    "features-h4":"Features",
+    "features-h4": "Features",
     "features-content": "Features content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.",
-    "about-h4":"About",
+    "about-h4": "About",
     "about-content": "About content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.",
     "middle-img-src": "img/mid-page-accent.jpg",
-    "services-h4":"Services",
+    "services-h4": "Services",
     "services-content": "Services content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.",
-    "product-h4":"Product",
+    "product-h4": "Product",
     "product-content": "Product content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.",
-    "vision-h4":"Vision",
+    "vision-h4": "Vision",
     "vision-content": "Vision content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.",
   },
   "contact": {
-    "contact-h4" : "Contact",
-    "address" : "123 Way 456 Street Somewhere, USA",
-    "phone" : "1 (888) 888-8888",
-    "email" : "sales@greatidea.io",
+    "contact-h4": "Contact",
+    "address": "123 Way 456 Street Somewhere, USA",
+    "phone": "1 (888) 888-8888",
+    "email": "sales@greatidea.io",
   },
   "footer": {
-    "copyright" : "Copyright Great Idea! 2018"
+    "copyright": "Copyright Great Idea! 2018"
   },
 };
 
 // Example: Update the img src for the logo
 let logo = document.getElementById("logo-img");
 logo.setAttribute('src', siteContent["nav"]["img-src"])
+
+const nav = document.querySelectorAll('a');
+
+nav[0].innerText = 'Services';
+nav[1].innerText = 'Product';
+nav[2].innerText = 'Vision';
+nav[3].innerText = 'Features';
+nav[4].innerText = 'About';
+nav[5].innerText = 'Contact';
+
+const topText = document.querySelector('h1');
+topText.innerText = 'Dom Is Awesome';
+
+let code = document.getElementById("cta-img");
+code.setAttribute('src', siteContent["cta"]["img-src"]);
+
+const topButton = document.querySelector('button')
+topButton.innerText = 'Get Started';
+
+let code2 = document.getElementById("middle-img");
+code2.setAttribute('src', siteContent["main-content"]["img-src"]);
+
+const mainContent = document.querySelectorAll('h4')
+mainContent[0].innerText = 'Features';
+mainContent[1].innerText = 'About';
+mainContent[2].innerText = 'Services';
+mainContent[3].innerText = 'Product';
+mainContent[4].innerText = 'Vision';
+mainContent[5].innerText = 'Contact';
+
+const mainText = document.querySelectorAll('p')
+mainText[0].innerText = 'Features content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.';
+mainText[1].innerText = 'About content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.';
+mainText[2].innerText = 'Services content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.';
+mainText[3].innerText = 'Product content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.';
+mainText[4].innerText = 'Vision content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.';
+mainText[5].innerText = '123 Way 456 Street Somewhere, USA';
+mainText[6].innerText = '1 (888) 888-8888';
+mainText[7].innerText = 'sales@greatidea.io';
+mainText[8].innerText = 'Copyright Great Idea! 2018';
+
+
