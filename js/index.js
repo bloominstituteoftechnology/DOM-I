3986d87deb5db77c89aec1c41f553e2c359ce508
unknown <quan.nguyen199712@gmail.com>
2018-09-17 18:05:25 -0400

    Finished setting up the page

--- js/index.js
+++ js/index.js
@@ -39,4 +39,46 @@ const siteContent = {
 
 // Example: Update the img src for the logo
 let logo = document.getElementById("logo-img");
-logo.setAttribute('src', siteContent["nav"]["img-src"])
+logo.src="img/logo.png";
+logo.alt="Logo image";
+let nav= document.getElementsByTagName("a");
+nav[0].innerText="Services";
+nav[1].innerText="Product";
+nav[2].innerText="Vision";
+nav[3].innerText="Features";
+nav[4].innerText="About";
+nav[5].innerText="Contact";
+let ctaH1= document.querySelector(".cta-text h1");
+ctaH1.innerHTML="DOM <br> is<br> Awesome";
+let ctaImg= document.getElementById("cta-img");
+ctaImg.src="img/header-img.png";
+ctaImg.alt="Image of a code snipplet";
+let ctaButton= document.querySelector(".cta-text button");
+ctaButton.innerText="Get Started";
+let mainTopH4 = document.querySelectorAll(".top-content h4");
+mainTopH4[0].innerText="Features";
+mainTopH4[1].innerText="About";
+let mainTopP= document.querySelectorAll(".top-content p");
+mainTopP[0].innerText= "Features content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.";
+mainTopP[1].innerText="About content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.";
+let middleImg= document.getElementById("middle-img");
+middleImg.src="img/mid-page-accent.jpg";
+middleImg.alt="Image of code snippets across the screen";
+let mainBotH4 = document.querySelectorAll(".bottom-content .text-content h4");
+mainBotH4[0].innerText="Services";
+mainBotH4[1].innerText="Product";
+mainBotH4[2].innerText="Vision";
+let mainBotP = document.querySelectorAll(".bottom-content .text-content p");
+mainBotP[0].innerText="Services content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.";
+mainBotP[1].innerText="Product content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.";
+mainBotP[2].innerText="Vision content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.";
+let contactH4= document.querySelector(".contact h4");
+contactH4.innerText="Contact";
+let contactP=document.querySelectorAll(".contact p");
+contactP[0].innerHTML="123 Way 456 Street<br>Somewhere, USA";
+contactP[1].innerText="1 (888) 888-8888";
+contactP[2].innerText="sales@greatidea.io";
+let footerP= document.querySelector("footer p");
+footerP.innerText="Copyright Great Idea! 2018";
+
+
