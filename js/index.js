2136753df66ce396305492f6a898a5563f404deb
Corey Gumbs <cgumbs@Coreys-MacBook-Pro-3.local>
2018-09-17 14:28:22 -0400

    [init] initial fileset up

--- js/index.js
+++ js/index.js
@@ -39,4 +39,7 @@ const siteContent = {
 
 // Example: Update the img src for the logo
 let logo = document.getElementById("logo-img");
-logo.setAttribute('src', siteContent["nav"]["img-src"])
+logo.setAttribute('src', siteContent["nav"]["img-src"]);
+
+let navigation = document.querySelector('.container header nav');
+console.log(navigation);
