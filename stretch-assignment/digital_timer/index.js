0adaf8000bbc917388f85d4c99830bc6fca469d7
Andre Myrick <amyrick.820@gmail.com>
2018-09-17 19:13:16 -0400

    updated timer, not finished

--- stretch-assignment/digital_timer/index.js
+++ stretch-assignment/digital_timer/index.js
@@ -2,11 +2,10 @@ let msTens = document.querySelector("#msTens");
 let msHundreds = document.querySelector("#msHundreds");
 let secondOnes = document.querySelector("#secondOnes");
 let secondTens = document.querySelector("#secondTens");
-msTens.innerText = 0;
-msHundreds.innerText = 0;
-secondOnes.innerText = 0;
-secondTens.innerText = 0;
-
+msTens.innerText = Math.trunc(0);
+msHundreds.innerText = Math.trunc(0);
+secondOnes.innerText = Math.trunc(0);
+secondTens.innerText = Math.trunc(0);
 
 let incMSTens = window.setInterval(incrementMSTens, 10);
 let incMSHundreds = window.setInterval(incrementMSHundreds, 100);
@@ -15,10 +14,16 @@ let incSecondTens = window.setInterval(incrementSecondTens, 10000);
 
 function incrementMSTens() {
     msTens.innerText++;
+    if (msTens.innerText == 9) {
+        msTens.innerText = 0;
+    }
 }
 
 function incrementMSHundreds() {
     msHundreds.innerText++;
+    if (msHundreds.innerText == 9) {
+        msHundreds.innerText = 0;
+    }
 }
 
 function incrementSecondOnes() {
