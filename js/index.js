ebb64e51ace87ed1c6fb211aeba8a18f7cf4f4e1
jarheadm249 <36177264+jarheadm249@users.noreply.github.com>
2018-09-17 18:44:39 -0400

    Steve Alverson

--- js/index.js
+++ js/index.js
@@ -37,6 +37,78 @@ const siteContent = {
   },
 };
 
+
+
 // Example: Update the img src for the logo
 let logo = document.getElementById("logo-img");
-logo.setAttribute('src', siteContent["nav"]["img-src"])
+logo.setAttribute("src", siteContent["nav"]["img-src"]);
+
+
+const mainHeader = document.querySelector('header');
+mainHeader.style.border = 'solid black 1px';
+mainHeader.style.backgroundColor = 'LightGrey';
+
+const mainBody = document.querySelector('html');
+mainBody.style.backgroundColor = 'LightSteelBlue';
+
+const navBar = document.querySelectorAll('a');
+navBar[0].innerText = siteContent.nav['nav-item-1'];
+navBar[1].innerText = siteContent.nav['nav-item-2'];
+navBar[2].innerText = siteContent.nav['nav-item-3'];
+navBar[3].innerText = siteContent.nav['nav-item-4'];
+navBar[4].innerText = siteContent.nav['nav-item-5'];
+navBar[5].innerText = siteContent.nav['nav-item-6'];
+
+for (let i=0; i < navBar.length; i++) {
+  navBar[i].style.color = 'green';
+}
+
+const newItem = document.createElement('a');
+const navBar1 = document.querySelector('nav');
+newItem.innerText = 'Hello';
+navBar1.prepend(newItem);
+newItem.style.color = 'green';
+
+const newItem1 = document.createElement('a');
+const navBar2 = document.querySelector('nav');
+newItem1.innerText = 'GoodBye';
+navBar2.append(newItem1);
+newItem1.style.color = 'green';
+
+
+
+const ctaText = document.getElementsByTagName('h1');
+ctaText[0].innerHTML = "Dom <br> is <br> Awesome";
+
+const button = document.getElementsByTagName('button');
+button[0].innerText = 'Get Started';
+
+const ctaImage = document.getElementById('cta-img');
+ctaImage.src = 'img/header-img.png';
+
+const titleH4 = document.getElementsByTagName('h4');
+titleH4[0].innerText = siteContent['main-content'] ['features-h4'];
+titleH4[1].innerText = siteContent['main-content']["about-h4"];
+titleH4[2].innerText = siteContent['main-content']["services-h4"];
+titleH4[3].innerText = siteContent['main-content']["product-h4"];
+titleH4[4].innerText = siteContent['main-content']["vision-h4"];
+
+
+
+const paraText =document.getElementsByTagName('p');
+
+paraText[0].innerText = "Features content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.";
+paraText[1].innerText = "About content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.";
+paraText[2].innerText = "Services content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.";
+paraText[3].innerText = "Product content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.";
+paraText[4].innerText = "Vision content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.";
+
+paraText[5].innerHTML = "123 Way 456 Street <br> Somewhere, USA"
+paraText[6].innerText = "1 (888) 888-8888"
+paraText[7].innerText = "sales@greatidea.io"
+
+paraText[8].innerText = "Copyright Great Idea! 2018"
+
+const midImage = document.querySelector('.middle-img');
+midImage.src = "img/mid-page-accent.jpg";
+
