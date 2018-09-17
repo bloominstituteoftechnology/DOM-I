4a59e65275c8222b5790c997bc2c2f9390317bf0
kyranmccann <kyran.mccann@gmail.com>
2018-09-17 12:58:54 -0700

    stretch project: digital timer

---
+++ stretch-assignment/digital_timer/index.js
@@ -0,0 +1,64 @@
+document.getElementById('start-timer').addEventListener("click", timer, {once: true});
+document.getElementById('reset-timer').addEventListener("click", reset, {once:true});
+
+
+let mTens = 0;
+let mHundreds = 0;
+let sOnes = 0;
+let sTens = 0;
+function timer() {
+
+    let timer = setInterval(function(){
+
+        mTens += 1;
+
+        if (mTens > 9){
+            mHundreds += 1;
+            mTens = 0;
+        };
+
+        if (mHundreds > 9){
+            sOnes += 1;
+            mHundreds = 0;
+        };
+
+        if (sOnes > 9) {
+            sTens += 1;
+            sOnes = 0;
+        };
+
+
+        if (sTens === 1){
+            mTens = 0;
+            document.getElementById('reset-timer').addEventListener("click", reset, {once: true});
+                clearInterval(timer);
+                let redDigits = Array.from(document.getElementsByClassName('digit'));
+                for (let i = 0; i < redDigits.length; i++) {
+                    redDigits[i].classList.add('redDigit');
+                };
+
+        }
+
+        document.getElementById('msTens').innerHTML = mTens;
+        document.getElementById('msHundreds').innerHTML = mHundreds;
+        document.getElementById('secondOnes').innerHTML = sOnes;
+        document.getElementById('secondTens').innerHTML = sTens;
+       },   10);
+}
+
+function reset(){
+mTens = 0;
+mHundreds = 0;
+sOnes = 0;
+sTens = 0;
+
+document.getElementById('msTens').innerHTML = mTens;
+document.getElementById('msHundreds').innerHTML = mHundreds;
+document.getElementById('secondOnes').innerHTML = sOnes;
+document.getElementById('secondTens').innerHTML = sTens;
+let blackDigits = Array.from(document.getElementsByClassName('digit'));
+for (let i = 0; i < blackDigits.length; i++) {
+    blackDigits[i].classList.remove('redDigit');
+};
+document.getElementById('start-timer').addEventListener("click", timer, {once: true});
+}
