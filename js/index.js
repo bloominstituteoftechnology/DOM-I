578237a39f96b3b1150f46a75d584be0b0cbfc38
Sarah Tennis <sarahtennis@Sarahs-Air.fios-router.home>
2018-09-17 18:11:49 -0400

    completed DOM I project and stretch

--- js/index.js
+++ js/index.js
@@ -37,6 +37,101 @@ const siteContent = {
   },
 };
 
+//------------------------------------------------------------------------
+
 // Example: Update the img src for the logo
 let logo = document.getElementById("logo-img");
 logo.setAttribute('src', siteContent["nav"]["img-src"])
+
+// link text (links will hold the anchor elements before the append/prepend)
+const linkText = ['Services', 'Product', 'Vision', 'Features', 'About', 'Contact'];
+let links = document.querySelectorAll('a');
+links.forEach((link, i) => link.innerText = linkText[i]);
+
+// cta content
+const ctaHeader = document.querySelector('.cta .cta-text h1');
+ctaHeader.innerHTML = 'DOM<br> Is<br> Awesome';
+
+const ctaButton = document.querySelector('.cta .cta-text button');
+ctaButton.innerText = 'Get Started';
+
+const ctaImg = document.getElementById('cta-img');
+ctaImg.src = 'img/header-img.png';
+
+// h4 text
+const h4Text = ['Features', 'About', 'Services', 'Product', 'Vision', 'Contact'];
+const h4Elements = document.querySelectorAll('h4');
+h4Elements.forEach((element, i) => element.innerText = h4Text[i]);
+
+// main content p text
+const mainContentPText = [
+  'Features content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.',
+  'About content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.',
+  'Services content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.',
+  'Product content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.',
+  'Vision content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.'
+];
+const mainContentP =  document.querySelectorAll('.main-content p');
+mainContentP.forEach((para, i) => para.innerText = mainContentPText[i]);
+
+//middle image
+const middleImg = document.getElementById('middle-img');
+middleImg.src = 'img/mid-page-accent.jpg';
+
+//contact text
+const contactText = [
+  '123 Way 456 Street<br>Somewhere, USA',
+  '1 (888) 888-8888',
+  'sales@greatidea.io'
+];
+const contactP = document.querySelectorAll('.contact p');
+contactP.forEach((para, i) => para.innerHTML = contactText[i]);
+
+//footer text
+const footerElement = document.querySelector('footer p');
+footerElement.innerText = 'Copyright Great Idea! 2018';
+
+//new nav elements
+const newLink1 = document.createElement('a');
+newLink1.innerText = 'Blog';
+newLink1.href = '#';
+
+const newLink2 = document.createElement('a'); 
+newLink2.innerText = 'Welcome';
+newLink2.href = '#';
+
+const navElement = document.getElementsByTagName('nav');
+navElement[0].prepend(newLink2);
+navElement[0].append(newLink1);
+links = document.querySelectorAll('a'); // reassign links to include append/prepend
+links.forEach((link) => link.style.color = 'green');
+
+
+//stretch changes
+const htmlPage = document.getElementsByTagName('html');
+htmlPage[0].style.cursor = 'grab';
+
+let counter = setInterval(cursorFunction, 500);
+
+function cursorFunction() {
+  if(htmlPage[0].style.cursor === 'grab'){
+    htmlPage[0].style.cursor = 'grabbing';
+  } else {
+    htmlPage[0].style.cursor = 'grab';
+  }
+}
+
+ctaButton.addEventListener('click', buttonClick);
+
+function buttonClick() {
+  if(htmlPage[0].style.filter === 'invert(0%)'){
+    htmlPage[0].style.filter = 'invert(100%)';
+    htmlPage[0].style.background = 'black';
+  } else {
+    htmlPage[0].style.filter = 'invert(0%)';
+    htmlPage[0].style.background = 'none';
+  }
+}
+
+
+
