faf6cfb1c07a989236f0c1946146111715bc7166
txiong000 <tkx422@gmail.com>
2018-09-17 15:09:55 -0500

    Toua's DOM-I project

--- js/index.js
+++ js/index.js
@@ -40,3 +40,43 @@ const siteContent = {
 // Example: Update the img src for the logo
 let logo = document.getElementById("logo-img");
 logo.setAttribute('src', siteContent["nav"]["img-src"])
+
+let ctaImg = document.getElementById("cta-img");
+ctaImg.setAttribute('src', siteContent["cta"]["img-src"])
+
+let midLogo = document.getElementById("middle-img");
+midLogo.setAttribute('src', siteContent["main-content"]["middle-img-src"]);
+
+let nav = document.getElementsByTagName("a");
+nav[0].innerHTML =siteContent["nav"]["nav-item-1"];
+nav[1].innerHTML = siteContent["nav"]["nav-item-2"];
+nav[2].innerHTML = siteContent["nav"]["nav-item-3"];
+nav[3].innerHTML =siteContent["nav"]["nav-item-4"];
+nav[4].innerHTML = siteContent["nav"]["nav-item-5"];
+nav[5].innerHTML = siteContent["nav"]["nav-item-6"];
+
+let cta = document.querySelector("h1");
+cta.innerHTML = siteContent["cta"]["h1"];
+
+const mainButton = document.querySelector("Button");
+mainButton.innerHTML = siteContent["cta"]["button"];
+
+let mainContent = document.getElementsByTagName("h4");
+mainContent[0].innerHTML = siteContent["main-content"]["features-h4"];
+mainContent[1].innerHTML = siteContent["main-content"]["about-h4"];
+mainContent[2].innerHTML = siteContent["main-content"]["services-h4"];
+mainContent[3].innerHTML = siteContent["main-content"]["product-h4"];
+mainContent[4].innerHTML = siteContent["main-content"]["vision-h4"];
+mainContent[5].innerHTML = siteContent["contact"]["contact-h4"];
+
+let paragraph = document.getElementsByTagName("p");
+paragraph[0].innerHTML = siteContent["main-content"]["features-content"];
+paragraph[1].innerHTML = siteContent["main-content"]["about-content"];
+paragraph[2].innerHTML = siteContent["main-content"]["services-content"];
+paragraph[3].innerHTML = siteContent["main-content"]["product-content"];
+paragraph[4].innerHTML = siteContent["main-content"]["vision-content"];
+paragraph[5].innerHTML = siteContent["contact"]["address"];
+paragraph[6].innerHTML = siteContent["contact"]["phone"];
+paragraph[7].innerHTML = siteContent["contact"]["email"];
+paragraph[8].innerHTML = siteContent["footer"]["copyright"];
+
