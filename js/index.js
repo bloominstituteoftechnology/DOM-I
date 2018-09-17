62398004decf3803edddf6f562a7672e0c7c90b3
Yusuf Nafey <yusufnafey@yahoo.com>
2018-09-17 14:06:27 -0700

    most of project complete. PS: sorry, completely forgot about committing :P

--- js/index.js
+++ js/index.js
@@ -28,7 +28,7 @@ const siteContent = {
   },
   "contact": {
     "contact-h4" : "Contact",
-    "address" : "123 Way 456 Street Somewhere, USA",
+    "address" : "123 Way 456 Street \nSomewhere, USA",
     "phone" : "1 (888) 888-8888",
     "email" : "sales@greatidea.io",
   },
@@ -39,4 +39,65 @@ const siteContent = {
 
 // Example: Update the img src for the logo
 let logo = document.getElementById("logo-img");
-logo.setAttribute('src', siteContent["nav"]["img-src"])
+logo.src = siteContent.nav["img-src"]
+
+let navItems = document.querySelectorAll('a')
+navItems[0].innerText = siteContent.nav["nav-item-1"]
+navItems[1].innerText = siteContent.nav["nav-item-2"]
+navItems[2].innerText = siteContent.nav["nav-item-3"]
+navItems[3].innerText = siteContent.nav["nav-item-4"]
+navItems[4].innerText = siteContent.nav["nav-item-5"]
+navItems[5].innerText = siteContent.nav["nav-item-6"]
+
+let ctaText = document.querySelector('.cta .cta-text h1')
+ctaText.innerText = siteContent.cta["h1"]
+
+let ctaButton = document.querySelector('.cta .cta-text button')
+ctaButton.innerText = siteContent.cta.button
+
+let ctaImage = document.querySelector('#cta-img')
+ctaImage.src = siteContent.cta["img-src"]
+
+
+let topContentHeaders = document.querySelectorAll('.top-content .text-content h4')
+topContentHeaders[0].innerText = siteContent["main-content"]["features-h4"]
+topContentHeaders[1].innerText = siteContent["main-content"]["about-h4"]
+
+
+let topContentText = document.querySelectorAll('.top-content .text-content p')
+topContentText[0].innerText = siteContent["main-content"]["features-content"]
+topContentText[1].innerText = siteContent["main-content"]["features-content"]
+
+
+let middleImage = document.querySelector('.middle-img')
+middleImage.src = siteContent["main-content"]["middle-img-src"]
+
+
+let bottomContentHeaders = document.querySelectorAll('.bottom-content .text-content h4')
+bottomContentHeaders[0].innerText = siteContent["main-content"]["services-h4"]
+bottomContentHeaders[1].innerText = siteContent["main-content"]["product-h4"]
+bottomContentHeaders[2].innerText = siteContent["main-content"]["vision-h4"]
+
+
+let bottomContentText = document.querySelectorAll('.bottom-content .text-content p')
+bottomContentText[0].innerText = siteContent["main-content"]["services-content"]
+bottomContentText[1].innerText = siteContent["main-content"]["product-content"]
+bottomContentText[2].innerText = siteContent["main-content"]["vision-content"]
+
+
+// let contactHeader = document.querySelectorAll('.content h4')
+let contactHeader = document.querySelector('.contact h4')
+contactHeader.innerText = siteContent.contact["contact-h4"]
+
+let contactText = document.querySelectorAll('.contact p')
+contactText[0].innerText = siteContent.contact.address
+contactText[1].innerText = siteContent.contact.phone
+contactText[2].innerText = siteContent.contact.email
+
+
+let footerText = document.querySelector('.footer p')
+footer.innerText = siteContent.footer.copyright
+
+
+// let footerText = document.querySelector('.footer p')
+// footerText.innerText = siteContent.footer.copyright
