f2ac237fd0b900e86bae34d66bb7a484abbfec80
Ryan Boris <neodynia@icloud.com>
2018-09-17 14:27:19 -0400

    Timer tasks delineated

---
+++ stretch-assignment/digital_timer/index.js
@@ -0,0 +1,21 @@
+/**
+ * Timer Functionality:
+ */
+
+
+/**
+ * TODO: Timer counts to 10.
+ */
+
+
+/**
+ * TODO: Increments every 10ms
+ */
+
+/**
+ * TODO: Digits become red when it reaches 10s
+ */
+
+/**
+ * TODO: Stops at 10s
+ */
