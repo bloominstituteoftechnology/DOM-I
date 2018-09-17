64617b423cef25a53ed10c52587be43432dac307
Hunter315 <hunter.mckay.smith@gmail.com>
2018-09-17 14:33:07 -0600

    line breaks

--- js/index.js
+++ js/index.js
@@ -40,3 +40,78 @@ const siteContent = {
 // Example: Update the img src for the logo
 let logo = document.getElementById("logo-img");
 logo.setAttribute('src', siteContent["nav"]["img-src"])
+
+// create A tags
+const firstA = document.createElement('a');
+const lastA = document.createElement('a');
+
+
+//nav
+const nav = document.querySelector('nav');
+nav.prepend(firstA);
+nav.appendChild(lastA);
+
+const a = document.querySelectorAll('a');
+a[0].innerHTML = siteContent["nav"]["nav-item-0"]
+a[1].innerHTML = siteContent["nav"]["nav-item-1"]
+a[2].innerHTML = siteContent["nav"]["nav-item-2"]
+a[3].innerHTML = siteContent["nav"]["nav-item-3"]
+a[4].innerHTML = siteContent["nav"]["nav-item-4"]
+a[5].innerHTML = siteContent["nav"]["nav-item-5"]
+a[6].innerHTML = siteContent["nav"]["nav-item-6"]
+a[7].innerHTML = siteContent["nav"]["nav-item-7"]
+//anchor tags color style
+a[0].style.color = 'green';
+a[1].style.color = 'green';
+a[2].style.color = 'green';
+a[3].style.color = 'green';
+a[4].style.color = 'green';
+a[5].style.color = 'green';
+a[6].style.color = 'green';
+a[7].style.color = 'green';
+
+//h1
+const h1 = document.querySelector('.cta-text h1');
+h1.innerHTML = siteContent["cta"]["h1"].split(' ').join('<br>');
+
+//cta button
+const button = document.querySelector('.cta-text button');
+button.innerHTML = siteContent["cta"]["button"]
+
+//cta image
+const ctaimg = document.getElementById('cta-img');
+ctaimg.setAttribute('src', siteContent["cta"]["img-src"])
+
+//top content p tags
+const topcontentp = document.querySelectorAll('.top-content p');
+topcontentp[0].innerHTML = siteContent["main-content"]["features-content"]
+topcontentp[1].innerHTML = siteContent["main-content"]["about-content"]
+
+//middle image
+const middleimg = document.getElementById('middle-img');
+middleimg.setAttribute('src', siteContent["main-content"]["middle-img-src"])
+
+//bottom content p tags
+const bottomcontentp = document.querySelectorAll('.bottom-content p');
+bottomcontentp[0].innerHTML = siteContent["main-content"]["services-content"]
+bottomcontentp[1].innerHTML = siteContent["main-content"]["product-content"]
+bottomcontentp[2].innerHTML = siteContent["main-content"]["vision-content"]
+
+//h4 tags
+const h4 = document.querySelectorAll('h4');
+h4[0].innerHTML = siteContent["main-content"]["features-h4"]
+h4[1].innerHTML = siteContent["main-content"]["about-h4"]
+h4[2].innerHTML = siteContent["main-content"]["services-h4"]
+h4[3].innerHTML = siteContent["main-content"]["product-h4"]
+h4[4].innerHTML = siteContent["main-content"]["vision-h4"]
+h4[5].innerHTML = siteContent["contact"]["contact-h4"]
+
+//contact p tags
+const contactp = document.querySelectorAll('.contact p');
+contactp[0].innerHTML = siteContent["contact"]["address"]
+contactp[1].innerHTML = siteContent["contact"]["phone"]
+contactp[2].innerHTML = siteContent["contact"]["email"]
+
+//footer p tag
+const footerp = document.querySelector('footer p');
+footerp.innerHTML = siteContent["footer"]["copyright"]
