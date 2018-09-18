b7076d378683cd3ad86e9b87122761c6ea22c28c
Jake Thomas <jmaxt12@gmail.com>
2018-09-17 18:26:05 -0600

    Complete

--- js/index.js
+++ js/index.js
@@ -40,3 +40,62 @@ const siteContent = {
 // Example: Update the img src for the logo
 let logo = document.getElementById("logo-img");
 logo.setAttribute('src', siteContent["nav"]["img-src"])
+
+const nav = document.querySelectorAll('a');
+
+nav[0].innerText = 'Services';
+nav[1].innerText = 'Product';
+nav[2].innerText = 'Vision';
+nav[3].innerText = 'Features';
+nav[4].innerText = 'About';
+nav[5].innerText = 'Contact';
+
+let ctaImg = document.getElementById("cta-img");
+ctaImg.setAttribute('src', siteContent["cta"]["img-src"])
+
+let midImg = document.getElementById("middle-img");
+midImg.setAttribute('src', siteContent["main-content"]["middle-img-src"])
+
+const mainHeader = document.querySelector('h1');
+
+mainHeader.innerText = 'DOM Is Awesome';
+
+const button = document.querySelector('button');
+
+button.innerText = 'Get Started';
+
+const headerFour = document.querySelectorAll('h4');
+
+headerFour[0].innerText = 'Features';
+headerFour[1].innerText = 'About';
+headerFour[2].innerText = 'Services';
+headerFour[3].innerText = 'Product';
+headerFour[4].innerText = 'Vision';
+headerFour[5].innerText = 'Contact';
+
+const p = document.querySelectorAll('p');
+
+p[0].innerText = 'Features content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.';
+p[1].innerText = 'About content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.';
+p[2].innerText = 'Services content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.';
+p[3].innerText = 'Product content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.';
+p[4].innerText = 'Vision content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.';
+p[5].innerText = '123 Way 456 Street Somewhere, USA';
+p[6].innerText = '1 (888) 888-8888';
+p[7].innerText = 'sales@greatidea.io';
+
+nav[0].style.color = 'green';
+nav[1].style.color = 'green';
+nav[2].style.color = 'green';
+nav[3].style.color = 'green';
+nav[4].style.color = 'green';
+nav[5].style.color = 'green';
+
+selectedNavLinks = document.getElementsByTagName("nav")[0];
+let createNewNode = (name) => {
+  let newNode = document.createElement("a");
+  newNode.innerHTML = name;
+  return newNode;
+}
+selectedNavLinks.prepend(createNewNode("Extra Item 1"));
+selectedNavLinks.append(createNewNode("Extra Item 2"));
