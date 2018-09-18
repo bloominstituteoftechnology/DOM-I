a40630f76a4359d3fb59e2feb58770ee70372d15
Gabriel Samaniego <selfdev.gs@gmail.com>
2018-09-17 19:43:42 -0700

    Digital Timer complete

--- stretch-assignment/digital_timer/index.js
+++ stretch-assignment/digital_timer/index.js
@@ -1,33 +1,20 @@
+// ==================== DIGITAL TIMER =====================
+
 // You will be making a timer that:
 // * Counts up to 10 seconds
 // * Increments every 10 ms
 // * Has digits change to red when it gets to 10 seconds
 // * Should not count past 10 seconds
-let digit = document.querySelector(".digits");
+let digits = document.querySelector(".digits");
 let msTens = document.querySelector("#msTens");
 let msHundreds = document.querySelector("#msHundreds");
 let secondOnes = document.querySelector("#secondOnes");
 let secondTens = document.querySelector("#secondTens");
-let startTime = 0;
 let msOnesCounter = 0;
 let msHundredsCounter = 0;
 let secondOnesCounter = 0;
 let secondTensCounter = 0;
-let timer = setInterval(addTime, 10);
-let refresh = function() {
-  msOnesCounter = 0;
-};
-
-// init();
-
-//  buttons
-let startBtn = document.querySelector("#start");
-startBtn.addEventListener("click", () => (startTime = 10));
-// start
-
-// pause button (temporary)
-let stopBtn = document.querySelector("#stop");
-stopBtn.addEventListener("click", () => clearInterval(timer));
+let timer;
 
 msTens.textContent = msOnesCounter;
 msHundreds.textContent = msHundredsCounter;
@@ -35,29 +22,28 @@ secondOnes.textContent = secondOnesCounter;
 secondTens.textContent = secondTensCounter;
 
 function init() {
+  digits.style.color = "white";
   msOnesCounter = 0;
   msHundredsCounter = 0;
   secondOnesCounter = 0;
   secondTensCounter = 0;
+  msTens.textContent = 0;
+  msHundreds.textContent = 0;
+  secondOnes.textContent = 0;
+  secondTens.textContent = 0;
+  body.style.color = "white";
 }
 
 function addTime() {
-  if (msOnesCounter < 9) {
-    ++msOnesCounter;
-  } else if (msOnesCounter > 8) {
-    msHundredsCounter++;
-    // console.log(msHundredsCounter);
-    refresh();
-    addSecond();
+  if (msOnesCounter <= 10) {
+    msOnesCounter++;
   }
 
-  msTens.innerText = msOnesCounter;
-  msHundreds.innerText = msHundredsCounter;
-  secondOnes.textContent = secondOnesCounter;
-  secondTens.textContent = secondTensCounter;
-}
+  if (msOnesCounter > 9) {
+    msOnesCounter = 0;
+    msHundredsCounter++;
+  }
 
-function addSecond() {
   if (msHundredsCounter > 9) {
     msHundredsCounter = 0;
     secondOnesCounter++;
@@ -71,6 +57,57 @@ function addSecond() {
 
   if (secondTensCounter > 0) {
     clearInterval(timer);
-    digit.classList.add("redDigit");
+    digits.classList.add("redDigit");
   }
+
+  msTens.innerText = msOnesCounter;
+  msHundreds.innerText = msHundredsCounter;
+  secondOnes.textContent = secondOnesCounter;
+  secondTens.textContent = secondTensCounter;
 }
+
+// ==================== BUTTONS ===================== //
+let buttonClass = document.querySelectorAll("button");
+buttonClass.forEach(function(button) {
+  button.style.border = "none";
+  button.style.color = "white";
+  button.style.fontSize = "35px";
+  button.style.fontFamily = "Helvetica";
+  button.style.minWidth = "150px";
+  button.style.padding = "10px 20px";
+  button.style.cursor = "pointer";
+});
+
+// START
+let startBtn = document.querySelector("#start");
+startBtn.addEventListener("click", () => (timer = setInterval(addTime, 10)));
+startBtn.style.backgroundColor = "green";
+
+// STOP
+let stopBtn = document.querySelector("#stop");
+stopBtn.addEventListener("click", () => clearInterval(timer));
+stopBtn.style.backgroundColor = "red";
+
+// RESET
+let resetBtn = document.querySelector("#reset");
+resetBtn.addEventListener("click", () => init());
+resetBtn.style.backgroundColor = "orange";
+
+// ==================== STYLING ===================== //
+
+let body = document.querySelector("body");
+body.style.backgroundColor = "#333";
+body.style.color = "white";
+body.style.flexDirection = "column";
+
+let head = document.querySelector("head");
+let linkTag = document.createElement("link");
+let googleFont = head.appendChild(linkTag);
+linkTag.href = "https://fonts.googleapis.com/css?family=VT323";
+linkTag.rel = "stylesheet";
+
+body.style.fontFamily = "'VT323', monospace";
+
+let digit = document.querySelectorAll(".digit");
+digit.forEach(digit => (digit.style.fontSize = "200px"));
+digits.style.marginTop = "200px";
