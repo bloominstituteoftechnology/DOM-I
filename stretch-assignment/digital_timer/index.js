5c2c123676e5ae6ed1ba56ee04bf041261ad5db1
Adam <ajmboosh@gmail.com>
2018-09-17 23:09:17 -0500

    stretch timer complete!

--- stretch-assignment/digital_timer/index.js
+++ stretch-assignment/digital_timer/index.js
@@ -2,26 +2,33 @@ const sTen = document.querySelector("#secondTens");
 const sOne = document.querySelector("#secondOnes");
 const msHun = document.querySelector("#msHundreds");
 const msTen = document.querySelector("#msTens");
-const allDigits = document.querySelectorAll("#digit");
+const allDigits = document.querySelector("#digits");
+sTen.style.display = "none";
+msTen.style.display = "none";
+var totalSeconds = 0;
+setInterval(setTime, 10);
 
-function counter() {
-  let num = 0;
-  function goUp() {
-    num++;
-    return num;
-  }
-  return goUp;
-}
-let newCounter = counter();
-
-var sOneCount = setInterval(sOneTimer, 1000);
-function sOneTimer() {
-  sTen.innerHTML = 0;
-  var sOneTime = newCounter();
-  sOne.innerHTML = sOneTime;
-  if (sOneTime === 10) {
-    clearTimeout(sOneCount);
-    sTen.style.display = "none";
+function setTime() {
+  ++totalSeconds;
+  msHun.innerHTML = increment(totalSeconds % 60);
+  sOne.innerHTML = increment(parseInt(totalSeconds / 60));
+  if (sOne.innerHTML >= 10) {
+    clearTimeout(setTime);
+    msHun.innerHTML = 0;
+    msTen.style.display = "initial";
+    msTen.innerHTML = 0;
+    sOne.innerHTML = 10;
+    msHun.style.color = "red";
+    msTen.style.color = "red";
     sOne.style.color = "red";
+  }
+}
+
+function increment(val) {
+  var valString = val + "";
+  if (valString.length < 2) {
+    return "0" + valString;
+  } else {
+    return valString;
   }
 }
