f3ef3da58c2d6ce81e0e84972fa5f1a8b1b507eb
nickkruger <ngkruger@gmail.com>
2018-09-24 13:15:24 -0600

    Updated Navigation Bar

--- js/index.js
+++ js/index.js
@@ -40,3 +40,18 @@ const siteContent = {
 // Example: Update the img src for the logo
 let logo = document.getElementById("logo-img");
 logo.setAttribute('src', siteContent["nav"]["img-src"])
+
+//Navigation Bar
+
+let navItems = document.querySelectorAll('header nav a');
+navItems[0].innerText = siteContent['nav']['nav-item-1'];
+navItems[1].innerText = siteContent['nav']['nav-item-2'];
+navItems[2].innerText = siteContent['nav']['nav-item-3'];
+navItems[3].innerText = siteContent['nav']['nav-item-4'];
+navItems[4].innerText = siteContent['nav']['nav-item-5'];
+navItems[5].innerText = siteContent['nav']['nav-item-6'];
+
+//cta Section
+
+let cta = document.querySelector('h1');
+cta.innerHTML = siteContent['cta']['h1'];
