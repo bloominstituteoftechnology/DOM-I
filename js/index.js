158c77955432e02d33b29d7a6e4f6ed2a026ca25
Orlando Castillo <omaddoc@gmail.com>
2018-09-26 04:42:48 -0400

    finished project

--- js/index.js
+++ js/index.js
@@ -38,5 +38,83 @@ const siteContent = {
 };
 
 // Example: Update the img src for the logo
+
+// Filling the images
 let logo = document.getElementById("logo-img");
-logo.setAttribute('src', siteContent["nav"]["img-src"])
+logo.setAttribute('src', siteContent["nav"]["img-src"]);
+
+let toAct = document.getElementById('cta-img');
+toAct.setAttribute('src', siteContent['cta']['img-src']);
+
+let banner = document.getElementById('middle-img');
+banner.setAttribute('src', siteContent['main-content']['middle-img-src']);
+
+
+// Getting the nav to appear
+let navvy = document.querySelectorAll('nav a');
+navvy.forEach((link, i) => {link.innerHTML = siteContent['nav'][`nav-item-${i+1}`]}); //borrowed, but attempted shorter ways and this was the shortest
+
+// getting cta to appear
+let popCta = document.getElementsByClassName('cta')[0];
+popCta.getElementsByTagName('h1')[0].innerHTML = siteContent.cta.h1;
+popCta.getElementsByTagName('button')[0].innerHTML = siteContent.cta.button;
+
+// get left side of main content
+let mContent = document.getElementsByClassName('text-content')[0];
+mContent.getElementsByTagName('h4')[0].innerHTML = siteContent['main-content']['features-h4'];
+mContent.getElementsByTagName('p')[0].innerHTML = siteContent['main-content']['features-content'];
+
+// get right side of main content
+let mContent2 = document.getElementsByClassName('text-content')[1];
+mContent2.getElementsByTagName('h4')[0].innerHTML = siteContent['main-content']['about-h4'];
+mContent2.getElementsByTagName('p')[0].innerHTML = siteContent['main-content']['about-content'];
+
+// get bottom content
+
+// services
+let mBContent = document.getElementsByClassName('text-content')[2];
+mBContent.getElementsByTagName('h4')[0].innerHTML = siteContent['main-content']['services-h4'];
+mBContent.getElementsByTagName('p')[0].innerHTML = siteContent['main-content']['services-content'];
+
+// product
+let mBContent2 = document.getElementsByClassName('text-content')[3];
+mBContent2.getElementsByTagName('h4')[0].innerHTML = siteContent['main-content']['product-h4'];
+mBContent2.getElementsByTagName('p')[0].innerHTML = siteContent['main-content']['product-content'];
+
+// vision
+let mBContent3 = document.getElementsByClassName('text-content')[4];
+mBContent3.getElementsByTagName('h4')[0].innerHTML = siteContent['main-content']['vision-h4'];
+mBContent3.getElementsByTagName('p')[0].innerHTML = siteContent['main-content']['vision-content'];
+
+// "contact": {
+//   "contact-h4" : "Contact",
+//   "address" : "123 Way 456 Street Somewhere, USA",
+//   "phone" : "1 (888) 888-8888",
+//   "email" : "sales@greatidea.io",
+// },
+// "footer": {
+//   "copyright" : "Copyright Great Idea! 2018"
+// },
+
+// contact
+let conInfo = document.getElementsByClassName('contact')[0];
+conInfo.getElementsByTagName('h4')[0].innerHTML = siteContent['contact']['contact-h4'];
+conInfo.getElementsByTagName('p')[0].innerHTML = siteContent['contact']['address'];
+conInfo.getElementsByTagName('p')[1].innerHTML = siteContent['contact']['phone'];
+conInfo.getElementsByTagName('p')[2].innerHTML = siteContent['contact']['email'];
+
+let foot = document.getElementsByTagName('footer')[0];
+foot.getElementsByTagName('p')[0].innerHTML = siteContent['footer']['copyright'];
+
+
+
+// Filling in Images
+
+// let headImg = document.getElementById('logo-img');
+// headImg.src = 'img/logo.png';
+
+// let logoImg = document.getElementById('cta-img');
+// logoImg.src = 'img/header-img.png';
+
+// let bannerImg = document.getElementById('middle-img');
+// bannerImg.src = 'img/mid-page-accent.jpg';
