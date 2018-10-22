bf649bf5f2d3877947b69127badbeb0c4c5323bc
Holden B <holden.bucher.92@gmail.com>
2018-10-22 10:16:59 -0500

    changed HTML using js. also used prepend() and appendChild() to play with nav-bar.

--- js/index.js
+++ js/index.js
@@ -40,3 +40,67 @@ const siteContent = {
 // Example: Update the img src for the logo
 let logo = document.getElementById("logo-img");
 logo.setAttribute('src', siteContent["nav"]["img-src"])
+
+let navA = document.querySelectorAll('a');
+navA[0].textContent = 'Services';
+navA[1].textContent = 'Product';
+navA[2].textContent = 'Vision';
+navA[3].textContent = 'Features';
+navA[4].textContent = 'About';
+navA[5].textContent = 'Contact';
+
+const h1 = document.querySelector('h1');
+h1.innerText = "DOM \r\n";
+h1.innerText += " Is \r\n";
+h1.innerText += " Awesome";
+const btn = document.querySelector('button');
+btn.textContent = 'Get Started';
+const ctaImage = document.getElementById('cta-img');
+ctaImage.src = siteContent['cta']['img-src'];
+
+const textCon = document.getElementsByClassName('text-content');
+textCon[0].querySelector('h4').textContent = siteContent['main-content']['features-h4'];
+textCon[0].querySelector('p').textContent = siteContent['main-content']['features-content'];
+textCon[1].querySelector('h4').textContent = siteContent['main-content']['about-h4'];
+textCon[1].querySelector('p').textContent = siteContent['main-content']['features-content'];
+textCon[2].querySelector('h4').textContent = siteContent['main-content']['services-h4'];
+textCon[2].querySelector('p').textContent = siteContent['main-content']['services-content'];
+textCon[3].querySelector('h4').textContent = siteContent['main-content']['product-h4'];
+textCon[3].querySelector('p').textContent = siteContent['main-content']['product-content'];
+textCon[4].querySelector('h4').textContent = siteContent['main-content']['vision-h4'];
+textCon[4].querySelector('p').textContent = siteContent['main-content']['vision-content'];
+
+const mid = document.getElementById('middle-img');
+mid.src = siteContent['main-content']['middle-img-src'];
+
+const contact = document.getElementsByClassName('contact');
+contact[0].querySelector('h4').textContent = siteContent['contact']['contact-h4'];
+contact[0].querySelectorAll('p')[0].innerText = '123 Way 456 Street \r\n';
+contact[0].querySelectorAll('p')[0].innerText += 'Somewhere, USA';
+contact[0].querySelectorAll('p')[1].textContent = siteContent['contact']['phone'];
+contact[0].querySelectorAll('p')[2].textContent = siteContent['contact']['email'];
+
+const footy = document.querySelector('footer').querySelector('p');
+footy.textContent = siteContent['footer']['copyright'];
+
+
+
+const womp = document.createElement('a');
+womp.textContent = 'prepended';
+womp.style.color = 'orange';
+womp.style.cursor = 'pointer';
+const brap = document.createElement('a');
+brap.textContent = 'appended';
+brap.style.color = 'orange';
+brap.style.cursor = 'pointer';
+document.querySelector('nav').prepend(womp);
+document.querySelector('nav').appendChild(brap);
+
+
+
+//green nav text
+
+for (let j = 0; j < navA.length; j++) {
+  navA[j].style.color = 'green';
+}
+
