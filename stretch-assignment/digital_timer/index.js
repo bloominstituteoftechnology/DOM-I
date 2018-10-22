23a66f24a32d6273fcf99f232baeff5891f1774c
Dennis Orbison <orbison.dennis@gmail.com>
2018-10-22 15:22:16 -0700

    timer counts up, working on changing color to red

--- stretch-assignment/digital_timer/index.js
+++ stretch-assignment/digital_timer/index.js
@@ -4,6 +4,49 @@
 let secondTens = document.querySelector('#secondTens');
 let secondOnes = document.querySelector('#secondOnes');
 let msHundrends = document.querySelector('#msHundreds');
-let MsTens = document.querySelector('#msTens');
+let msTens = document.querySelector('#msTens');
+
+let secTen = 0;
+let secOne = 0;
+let msT = 0;
+let msH = 0;
+
+function timer() {
+    if (msT <= 0) {
+        let timerOff = setInterval(() => {
+            msT++
+            if (msT >= 10) {
+                msH++;
+                msT = 0;
+
+            }
+            if (msH >= 10) {
+                secOne++;
+                msH = 0;
+
+            }
+            if (secOne >= 10) {
+                secTen++;
+                secOne = 0;
+
+            }
+            if (secTen >= 10) {
+                msTens.style.color = 'red';
+                msHundrends.style.color = 'red';
+                secondTens.style.color = 'red';
+                secondOnes.style.color = 'red';
+            }
+            // if (secTen >= 10) clearInterval(timerOff)
+
+            msTens.textContent = msT;
+            msHundrends.textContent = msH;
+            secondTens.textContent = secTen;
+            secondOnes.textContent = secOne;
+
+        }, 10)
+    }
+}
+
+timer();
 
 //console.log(secondTens);
