2ab8cad4297c21ef86e855b6654657dd326bb342
Dennis Orbison <orbison.dennis@gmail.com>
2018-10-22 14:29:14 -0700

    selecting elements with dom

--- stretch-assignment/digital_timer/index.js
+++ stretch-assignment/digital_timer/index.js
@@ -1,2 +1,9 @@
 // document.querySelector('#secondTens').textContent = '5';
 // document.querySelector('#secondOnes').innerContent = '5';
+
+let secondTens = document.querySelector('#secondTens');
+let secondOnes = document.querySelector('#secondOnes');
+let msHundrends = document.querySelector('#msHundreds');
+let MsTens = document.querySelector('#msTens');
+
+//console.log(secondTens);
