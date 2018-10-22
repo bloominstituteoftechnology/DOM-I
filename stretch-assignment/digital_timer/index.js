5b05294c6f531343dd6e49267a5170376099b38e
Trai Lynne <trailynne@outlook.com>
2018-10-22 15:31:09 -0600

    TImer: Made timer selector and set it to turn red in interval function

--- stretch-assignment/digital_timer/index.js
+++ stretch-assignment/digital_timer/index.js
@@ -1,3 +1,12 @@
+let timer = document.getElementsByClassName('digit');
+
+timer = Array.from(timer);
+
 window.setInterval(function() {
     console.log('testing');
+    timer.forEach(item => {
+        item.style.color = 'red';
+    })
 }, 10000);
+
+console.log(timer)
