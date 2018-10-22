94d68f5d5f8891ae9942b35a44f8108de39cf176
cbtsao47 <cbtsao47@gmail.com>
2018-10-22 16:54:17 -0500

    tasks 1~3 completed

--- js/index.js
+++ js/index.js
@@ -40,3 +40,68 @@ const siteContent = {
 // Example: Update the img src for the logo
 let logo = document.getElementById("logo-img");
 logo.setAttribute('src', siteContent["nav"]["img-src"])
+
+// nav bar links
+const services = document.querySelector('a:nth-of-type(1)');
+const product = document.querySelector('a:nth-of-type(2)');
+const vision = document.querySelector('a:nth-of-type(3)');
+const features = document.querySelector('a:nth-of-type(4)');
+const about = document.querySelector('a:nth-of-type(5)');
+const contact = document.querySelector('a:nth-of-type(6)');
+
+services.textContent = siteContent.nav['nav-item-1'];
+product.textContent = siteContent.nav["nav-item-2"];
+vision.textContent = siteContent.nav["nav-item-3"];
+features.textContent = siteContent.nav["nav-item-4"];
+about.textContent = siteContent.nav["nav-item-5"];
+contact.textContent = siteContent.nav["nav-item-6"]
+
+// cta 
+const ctaText = document.querySelector('.cta-text h1');
+const ctaImg = document.querySelector('#cta-img');
+const lineBreak = document.createElement('br');
+const btn = document.querySelector('button');
+
+ctaText.textContent = siteContent.cta.h1;
+ctaText.appendChild(lineBreak);
+ctaImg.src = siteContent.cta["img-src"];
+btn.textContent = siteContent.cta.button;
+
+// top content
+const topContent = document.querySelector('.top-content');
+
+topContent.querySelector('.text-content h4').textContent = siteContent["main-content"]["features-h4"];
+topContent.querySelector('.text-content p').textContent = siteContent["main-content"]["features-content"];
+topContent.querySelector('.text-content:nth-of-type(2) h4').textContent = siteContent["main-content"]["about-h4"];
+topContent.querySelector('.text-content:nth-of-type(2) p').textContent = siteContent["main-content"]["about-content"];
+
+// middle img
+
+const midImg = document.querySelector('#middle-img');
+midImg.src = 'img/mid-page-accent.jpg';
+
+// bottom content
+
+const bottomContent = document.querySelector('.bottom-content');
+
+bottomContent.querySelector('.text-content h4').textContent = siteContent["main-content"]["services-h4"];
+bottomContent.querySelector('.text-content p').textContent = siteContent["main-content"]["services-content"];
+
+bottomContent.querySelector('.text-content:nth-of-type(2) h4').textContent = siteContent["main-content"]["product-h4"];
+bottomContent.querySelector('.text-content:nth-of-type(2) p').textContent = siteContent["main-content"]["product-content"];
+
+bottomContent.querySelector('.text-content:nth-of-type(3) h4').textContent = siteContent["main-content"]["vision-h4"];
+bottomContent.querySelector('.text-content:nth-of-type(3) p').textContent = siteContent["main-content"]["vision-content"];
+
+// contact
+
+const contactInfo = document.querySelector('.contact');
+contactInfo.querySelector('h4').textContent = siteContent.contact["contact-h4"];
+contactInfo.querySelector('p').textContent = siteContent.contact.address;
+contactInfo.querySelector('p:nth-of-type(2)').textContent = siteContent.contact.phone;
+contactInfo.querySelector('p:nth-of-type(3)').textContent = siteContent.contact.email;
+
+// footer
+
+const footer = document.querySelector('footer');
+footer.querySelector('p').textContent = siteContent.footer.copyright;
