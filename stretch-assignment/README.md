113689d9c64b98a26ce16eb3b120db026b546317
David Freitag <freitagdavid@outlook.com>
2018-10-23 15:14:57 -0400

    finished listed stretch goals maybe slightly differently though.

--- stretch-assignment/README.md
+++ stretch-assignment/README.md
@@ -41,8 +41,9 @@ You will also need to look up and use `window.setInterval`. For this method, and
 
 If you compelete the timer with time to spare:
 
--   Self study and research DOM `events` and `event listeners`
--   SG1: Implement a start button. The digital timer should not start until the button is pressed.
+Self study and research DOM `events` and `event listeners`
+SG1: Implement a start button. The digital timer should not start until the button is pressed.
+
 -   SG2: Once you have a start button working, configure it so that when a user presses the start button it is disabled and not enabled until the timer finishes.
 -   SG3: Once you have finished SG2, add new new button called `reset` that resets the timer to 0:00:00 and then pressing the start button again will start the timer from 0.
 -   SG4: Finally, if you have completed all the stretch goals, spend some time styling your timer and buttons.
--- stretch-assignment/digital_timer/index.js
+++ stretch-assignment/digital_timer/index.js
@@ -1,23 +1,67 @@
-document.addEventListener("DOMContentLoaded", function() {
-    let startTime = new Date();
-    let clock = document.querySelectorAll(".digits *:not(#colon)");
-    let wholeClock = document.querySelector(".digits");
-    let updateClockInt = setInterval(() => {
-        updateClock(startTime, clock);
-    }, 10);
-
-    function updateClock(startTime, clock) {
+class Clock {
+    constructor(wholeClock) {
+        this.clock = document.querySelectorAll('.digits *:not(#colon)');
+        this.wholeClock = wholeClock;
+        this.start = document.querySelector('.start');
+    }
+    init() {
+        let _this = this;
+        this.startTime = new Date();
+        console.log(this);
+        this.start.disabled = true;
+        this.clockInt = setInterval(function() {
+            _this.updateClock();
+        }, 10);
+    }
+    updateClock() {
         let currentTime = new Date();
-        let currentDifference = Math.floor((currentTime - startTime) / 10)
+        let currentDifference = Math.floor((currentTime - this.startTime) / 10)
             .toString()
-            .padStart(4, "0")
-            .split("");
-        for (i = 0; i < clock.length; i++) {
-            clock[i].textContent = currentDifference[i];
+            .padStart(4, '0')
+            .split('');
+        for (let i = 0; i < this.clock.length; i++) {
+            this.clock[i].textContent = currentDifference[i];
         }
-        if (currentDifference[0] == "1") {
-            clearInterval(updateClockInt);
-            wholeClock.style.color = "red";
+        if (currentDifference[0] == '1') {
+            this.stop();
+            this.wholeClock.classList.add('redDigit');
         }
     }
+    stop() {
+        clearInterval(this.clockInt);
+        this.start.disabled = false;
+    }
+}
+
+// document.addEventListener('DOMContentLoaded', function() {
+//     start.addEventListener('click', event => {
+//         startTime = new Date();
+//         setInterval(() => {
+//             updateClock(startTime, clock);
+//         }, 10);
+//     });
+
+//     function updateClock(startTime, clock) {
+//         let currentTime = new Date();
+//         let currentDifference = Math.floor((currentTime - startTime) / 10)
+//             .toString()
+//             .padStart(4, '0')
+//             .split('');
+//         for (i = 0; i < clock.length; i++) {
+//             clock[i].textContent = currentDifference[i];
+//         }
+//         if (currentDifference[0] == '1') {
+//             clearInterval(working.updateClockInt);
+//             wholeClock.classList.add('redDigit');
+//         }
+//     }
+// });
+
+document.addEventListener('DOMContentLoaded', () => {
+    const wholeClock = document.querySelector('.digits');
+    const mainClock = new Clock(wholeClock);
+    const start = document.querySelector('.start');
+    const stop = document.querySelector('.stop');
+    start.addEventListener('click', init => mainClock.init());
+    stop.addEventListener('click', stop => mainClock.stop());
 });
