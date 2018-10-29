6b78f3071faeff1a0e39c68f8711885a3b0f6351
Joseph Eastman <josephryan96@gmail.com>
2018-10-29 11:19:12 -0700

    index.js updated nav

--- js/index.js
+++ js/index.js
@@ -39,4 +39,15 @@ const siteContent = {
 
 // Example: Update the img src for the logo
 let logo = document.getElementById("logo-img");
-logo.setAttribute('src', siteContent["nav"]["img-src"])
+logo.setAttribute('src', siteContent["nav"]["img-src"]);
+
+//nav
+let nav = document.querySelectorAll('nav a');
+
+nav[0].textContent = siteContent.nav['nav-item-1'];
+nav[1].textContent = siteContent.nav['nav-item-2'];
+nav[2].textContent = siteContent.nav['nav-item-3'];
+nav[3].textContent = siteContent.nav['nav-item-4'];
+nav[4].textContent = siteContent.nav['nav-item-5'];
+nav[5].textContent = siteContent.nav['nav-item-6'];
+
