6888432a90a0b0af9006881d2e2f8d3bd2fd8189
chrisgins <christiansgina@gmail.com>
2018-10-29 13:18:06 -0600

    add Contact Information

--- js/index.js
+++ js/index.js
@@ -15,16 +15,16 @@ const siteContent = {
   },
   "main-content": {
     "features-h4":"Features",
-    "features-content": "Features content __elementmentum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus sc__elementrisque quis.",
+    "features-content": "Features content ___elementmentmentum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus sc___elementmentrisque quis.",
     "about-h4":"About",
-    "about-content": "About content __elementmentum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus sc__elementrisque quis.",
+    "about-content": "About content ___elementmentmentum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus sc___elementmentrisque quis.",
     "middle-img-src": "img/mid-page-accent.jpg",
     "services-h4":"Services",
-    "services-content": "Services content __elementmentum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus sc__elementrisque quis.",
+    "services-content": "Services content ___elementmentmentum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus sc___elementmentrisque quis.",
     "product-h4":"Product",
-    "product-content": "Product content __elementmentum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus sc__elementrisque quis.",
+    "product-content": "Product content ___elementmentmentum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus sc___elementmentrisque quis.",
     "vision-h4":"Vision",
-    "vision-content": "Vision content __elementmentum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus sc__elementrisque quis.",
+    "vision-content": "Vision content ___elementmentmentum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus sc___elementmentrisque quis.",
   },
   "contact": {
     "contact-h4" : "Contact",
@@ -38,48 +38,59 @@ const siteContent = {
 };
 
 // Example: Update the img src for the logo
-const logo = document.get__elementmentById("logo-img");
+const logo = document.get___elementmentmentById("logo-img");
 logo.setAttribute('src', siteContent["nav"]["img-src"])
 
 // =====Navigation=====
 
-const navS__elementctor = document.queryS__elementctor("nav");
-navS__elementctor.appendChild(document.create__elementment("a"));
-navS__elementctor.appendChild(document.create__elementment("a"));
+const navS___elementmentctor = document.queryS___elementmentctor("nav");
+navS___elementmentctor.appendChild(document.create___elementmentment("a"));
+navS___elementmentctor.appendChild(document.create___elementmentment("a"));
 
-const navLinks = document.queryS__elementctorAll("nav a");
+const navLinks = document.queryS___elementmentctorAll("nav a");
 const navKeys = Object.keys(siteContent["nav"]);
-navLinks.forEach(function(___elementment,index){
-  ___elementment.textContent = siteContent["nav"][navKeys[index]];
-  ___elementment.style.color = "green";
+navLinks.forEach(function(____elementmentment,index){
+  ____elementmentment.textContent = siteContent["nav"][navKeys[index]];
+  ____elementmentment.style.color = "green";
 })
 
 /// ====CTA====
 
-const ctaImg = document.get__elementmentById("cta-img");
+const ctaImg = document.get___elementmentmentById("cta-img");
 ctaImg.setAttribute('src', siteContent["cta"]["img-src"])
 
 
 // ====DOM-ish stuff====
 
-const header1 = document.queryS__elementctor(".cta-text");
-header1.prepend(document.create__elementment("h1"));
-header1.prepend(document.create__elementment("h1"));
-header1 = document.queryS__elementctorAll(".cta-text h1");
+const header1 = document.queryS___elementmentctor(".cta-text");
+header1.prepend(document.create___elementmentment("h1"));
+header1.prepend(document.create___elementmentment("h1"));
+header1 = document.queryS___elementmentctorAll(".cta-text h1");
 
-header1.forEach(function(___elementment,index){
-  ___elementment.textContent = siteContent["cta"]["h1"].split(" ")[index];
+header1.forEach(function(____elementmentment,index){
+  ____elementmentment.textContent = siteContent["cta"]["h1"].split(" ")[index];
 })
 
-const getButton = document.queryS__elementctor(".cta-text button");
+const getButton = document.queryS___elementmentctor(".cta-text button");
 getButton.textContent = siteContent["cta"]["button"];
 
 // ====Main Content====
 
-const topContent = document.queryS__elementctorAll(".main-content .text-content h4, .main-content .text-content p");
+const topContent = document.queryS___elementmentctorAll(".main-content .text-content h4, .main-content .text-content p");
 const mainKeys = Object.keys(siteContent["main-content"]);
 mainKeys.splice(4,1);
 
-topContent.forEach(function(__element,index){
-    __element.textContent = siteContent["main-content"][mainKeys[index]];
+topContent.forEach(function(___elementment,index){
+    ___elementment.textContent = siteContent["main-content"][mainKeys[index]];
+})
+
+const middleImg = document.get_elementmentById("middle-img");
+middleImg.setAttribute('src', siteContent["main-content"]["middle-img-src"]);
+
+// ====Contact Information====
+
+const contactText = document.queryS_elementctorAll(".contact *");
+const contactKeys = Object.keys(siteContent["contact"]);
+contactText.forEach(function(_element,index){
+  _element.textContent = siteContent["contact"][contactKeys[index]];
 })
