b3ccf0c5376199d3f13ea349285f02a536d3c66f
chrisgins <christiansgina@gmail.com>
2018-10-29 15:00:04 -0600

    Stretch: Create Const variable for getElementById

---
+++ stretch-assignment/digital_timer/index.js
@@ -0,0 +1,8 @@
+
+// ====Create Const variable for getElementById====
+
+
+const msTens = document.getElementById("msTens");
+const msHundreds = document.getElementById("msHundreds");
+const secondOnes = document.getElementById("secondOnes");
+const secondTens = document.getElementById("secondTens");
