183716395294448e0ca636e4470773b5547cfc8d
edreeseg <edward.reeseg@gmail.com>
2018-10-29 22:06:34 -0400

    DOM structure complete

--- js/index.js
+++ js/index.js
@@ -40,3 +40,47 @@ const siteContent = {
 // Example: Update the img src for the logo
 let logo = document.getElementById("logo-img");
 logo.setAttribute('src', siteContent["nav"]["img-src"])
+
+let navElements = document.querySelectorAll('nav a');
+for (let i = 0; i < navElements.length; i++){
+  let child = Object.values(siteContent.nav)[i];
+  navElements[i].textContent = child;
+}
+
+let heading = document.querySelector('.cta-text h1');
+heading.textContent = siteContent.cta.h1;
+
+let button = document.querySelector('.cta-text button');
+button.textContent = siteContent.cta.button;
+
+let ctaIMG = document.getElementById('cta-img');
+ctaIMG.src = siteContent.cta["img-src"];
+
+let mainHeadings = document.querySelectorAll('.text-content h4');
+mainHeadings[0].textContent = siteContent['main-content']['features-h4'];
+mainHeadings[1].textContent = siteContent['main-content']['about-h4'];
+mainHeadings[2].textContent = siteContent['main-content']['services-h4'];
+mainHeadings[3].textContent = siteContent['main-content']['product-h4'];
+mainHeadings[4].textContent = siteContent['main-content']['vision-h4'];
+
+let featuresContent = document.querySelector('.top-content .text-content p');
+featuresContent.textContent = siteContent['main-content']['features-content'];
+
+let aboutContent = document.querySelectorAll('.top-content .text-content p')[1];
+aboutContent.textContent = siteContent['main-content']['about-content'];
+
+let middleIMG = document.getElementById('middle-img');
+middleIMG.src = siteContent['main-content']['middle-img-src'];
+
+let bottomContent = document.querySelectorAll('.bottom-content .text-content p');
+bottomContent[0].textContent = siteContent['main-content']['services-content'];
+bottomContent[1].textContent = siteContent['main-content']['product-content'];
+bottomContent[2].textContent = siteContent['main-content']['vision-content'];
+
+let contactSection = document.querySelector('.contact').children;
+for (let i = 0; i < contactSection.length; i++){
+  contactSection[i].textContent = Object.values(siteContent.contact)[i];
+}
+
+let footer = document.querySelector('footer');
+footer.children[0].textContent = siteContent.footer.copyright;
