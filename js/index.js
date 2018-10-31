d7fd6ce752ca3ec6c97ea882814b5109a4d46229
Trip1701 <bj2336@email.vccs.edu>
2018-10-30 21:03:51 -0400

    Navigation Items added to Nav Bar

--- js/index.js
+++ js/index.js
@@ -40,3 +40,10 @@ const siteContent = {
 // Example: Update the img src for the logo
 let logo = document.getElementById("logo-img");
 logo.setAttribute('src', siteContent["nav"]["img-src"])
+
+let navigation = document.querySelectorAll('a');
+
+for(let i = 0; i < navigation.length; i++) {
+  navigation[i].innerText = siteContent["nav"][`nav-item-${i+1}`];
+}
+console.log(navigation);
