f0a2a69f1fc1890d3d5e0d220ae9ce9725843233
echopecho <pbsmithtbn@gmail.com>
2018-10-31 18:10:20 -0500

    Add reset button
    I completed the SG3 task to add a functioning reset button.  The functionality for this button was much less complicated than the start button so it did not require much time.  It did require me to learn the :not() CSS selector to be able grab all the items in .digit class besides the one with the ID #colon.

--- stretch-assignment/digital_timer/index.js
+++ stretch-assignment/digital_timer/index.js
@@ -1,27 +1,13 @@
 let clock = document.querySelectorAll('.digit');
+let digits = document.querySelectorAll('.digit:not(#colon)');
 let tensPlace = document.querySelector('#secondTens');
 let onesPlace = document.querySelector('#secondOnes');
 let tenthsPlace = document.querySelector('#msTens');
 let hundredthsPlace = document.querySelector('#msHundreds');
 let startBtn = document.querySelector('.start');
+let resetBtn = document.querySelector('.reset');
 
 var counter = 0;
-
-// var tenSecondTimer = setInterval(function() {
-//   tensPlace.textContent = Math.floor(counter / 1000);
-//   var oneSecond = counter % 1000;
-//   onesPlace.textContent = Math.floor(oneSecond / 100);
-//   var tenthSecond = oneSecond % 100;
-//   tenthsPlace.textContent = Math.floor(tenthSecond / 10);
-//   var hundredthSecond = tenthSecond % 10;
-//   hundredthsPlace.textContent = Math.floor(hundredthSecond / 1);
-//   counter++;
-
-//   if(counter > 1000) {
-//     clearInterval(tenSecondTimer);
-//     clock.forEach(digit => digit.style.color = 'red');
-//   }
-// }, 10);
 
 function timer() {
   tensPlace.textContent = Math.floor(counter / 1000);
@@ -48,3 +34,9 @@ startBtn.addEventListener('click', function() {
     }
   }, 10)
 });
+
+resetBtn.addEventListener('click', function() {
+  counter = 0;
+  digits.forEach(digit => digit.textContent = '-');
+  clock.forEach(digit => digit.style.color = 'black');
+})
