27269d3fd06dca4b3498171d932b5f6b5b394c91
Arpita Sinha <sinha.arpita@gmail.com>
2018-10-31 11:00:08 -0700

    MVP FOR DOM-I
    Final commit of MVP DOM-I

--- README.md
+++ README.md
@@ -1,11 +1,15 @@
 # DOM I - JavaScript Content Management System
 
 ## Project Description
-You are going to be emulating a content managment system by controlling the content in the JavaScript file instead of the HTML file. This project is an excercise pointed at selecting elements and then updating them without touching the HTML file using the DOM.
+You are going to be emulating a content managment system by controlling the content in the JavaScript file
+instead of the HTML file. This project is an excercise pointed at selecting elements and then updating them
+ without touching the HTML file using the DOM.
 
-You have been provided a [JSON object](js/index.js) with all the necessary data to accomplish this task.  Use dot or bracket notation to traverse the data provided.
+You have been provided a [JSON object](js/index.js) with all the necessary data to accomplish this task.
+  Use dot or bracket notation to traverse the data provided.
 
-**ONE RULE: You cannot update the HTML or CSS files directly.  You must use JavaScript and the DOM only for your tasks today.**
+**ONE RULE: You cannot update the HTML or CSS files directly.
+  You must use JavaScript and the DOM only for your tasks today.**
 
 ## Task 1: Set Up The Project With Git
 
@@ -24,12 +28,16 @@ You have been provided a [JSON object](js/index.js) with all the necessary data 
 
 ## Task 4: Add new content
 * [ ] Change the color of the navigation text to be green.
-* [ ] Utilize `.appendChild()` and `.prepend()` to add two new items to the navigation system. You can call them whatever you want.
+* [ ] Utilize `.appendChild()` and `.prepend()` to add two new items to the navigation system.
+ You can call them whatever you want.
 * [ ] Check your work by looking at the [original html](original.html) in the browser
 
 ## Stretch Goals
-* [ ] Update styles throughout the page as you see fit.  Study what happens when you updated the DOM using style in JavaScript.  
-* [ ] Study tomorrow's lesson on events and try to integrate a button that can update content on the site with a click of a button.  You could build a similar data object with new values to help you test the click event.
+* [ ] Update styles throughout the page as you see fit.  Study what happens when you updated the DOM
+using style in JavaScript.
+* [ ] Study tomorrow's lesson on events and try to integrate a button that can update content
+on the site with a click of a button.  You could build a similar data object with new values to help
+you test the click event.
 
 ## Stretch Project: Digital Timer
 This project is heavier on logic but employs some DOM manipulation to achieve it's goals.  Go check it out here: [stretch assignment](stretch-assignment) and see how far you can get. 
--- js/index.js
+++ js/index.js
@@ -38,5 +38,84 @@ const siteContent = {
 };
 
 // Example: Update the img src for the logo
+
+
+
+let navItems=document.querySelectorAll(".container header a");
+navItems[0].textContent=siteContent['nav']["nav-item-1"];
+navItems[1].textContent=siteContent['nav']["nav-item-2"];
+navItems[2].textContent=siteContent['nav']["nav-item-3"];
+navItems[3].textContent=siteContent['nav']["nav-item-4"];
+navItems[4].textContent=siteContent['nav']["nav-item-5"];
+navItems[5].textContent=siteContent['nav']["nav-item-6"];
+
+let newElement =document.createElement("a");
+newElement.href="#";
+newElement.textContent="Search";
+let nav =document.querySelector(".container header nav");
+nav.append(newElement );
+
+
+
 let logo = document.getElementById("logo-img");
-logo.setAttribute('src', siteContent["nav"]["img-src"])
+logo.src= siteContent["nav"]["img-src"];
+logo.alt="Great Idea! Company logo.";
+
+
+
+let imageHeader=document.getElementById("cta-img");
+imageHeader.setAttribute('src',siteContent["cta"]["img-src"]);
+imageHeader.alt="Image of a code snippet.";
+
+let middleImage= document.getElementById("middle-img");
+middleImage.setAttribute('src',siteContent["main-content"]["middle-img-src"]);
+middleImage.alt="Image of code snippets across the screen";
+
+let headerLine= document.querySelector(".cta .cta-text h1");
+
+headerLine.textContent=siteContent["cta"]["h1"];
+//headerLine.style.fontWeight="bold";
+headerLine.style.width = "175px";
+headerLine.style.marginRight = "60px";
+
+
+let btn=document.querySelector(".cta .cta-text button");
+
+btn.textContent=siteContent["cta"]["button"];
+
+let upperH4=document.querySelectorAll(".main-content .top-content .text-content h4");
+//console.log(upper[0]);
+upperH4[0].textContent =siteContent['main-content']["features-h4"];
+upperH4[1].textContent=siteContent['main-content']["about-h4"];
+let upperP=document.querySelectorAll(".main-content .top-content .text-content p");
+upperP[0].textContent =siteContent['main-content']["features-content"];
+upperP[1].textContent =siteContent['main-content']["about-content"];
+
+
+
+
+let lowerH4=document.querySelectorAll(".main-content .bottom-content .text-content h4");
+//console.log(upper[0]);
+lowerH4[0].textContent =siteContent['main-content']["services-h4"];
+lowerH4[1].textContent=siteContent['main-content']["product-h4"];
+lowerH4[2].textContent=siteContent['main-content']["vision-h4"];
+
+let lowerP=document.querySelectorAll(".main-content .bottom-content .text-content p");
+lowerP[0].textContent =siteContent['main-content']["services-content"];
+lowerP[1].textContent =siteContent['main-content']["product-content"];
+lowerP[2].textContent =siteContent['main-content']["vision-content"];
+
+
+
+let contactDetails = document.querySelector(".contact h4");
+contactDetails.textContent =siteContent["contact"]["contact-h4"];
+
+let contactDetailsParaList = document.querySelectorAll(".contact p");
+
+contactDetailsParaList[0].textContent = siteContent['contact']["address"];
+contactDetailsParaList[1].textContent = siteContent['contact']["phone"];
+contactDetailsParaList[2].textContent = siteContent['contact']["email"];
+
+let foot =document.querySelector("footer p")
+foot.textContent =siteContent["footer"]["copyright"];
+
