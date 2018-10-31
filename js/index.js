b7e37b5b333a31e3d42bdcc0c3874d4d1cb5d4da
jrizza88 <jetorres88@gmail.com>
2018-10-31 00:20:52 -0400

    began project and completed nav and anchor tag section

--- js/index.js
+++ js/index.js
@@ -10,6 +10,7 @@ const siteContent = {
   },
   "cta": {
     "h1": "DOM Is Awesome",
+    "h2": "DOM Is Awesome",
     "button": "Get Started",
     "img-src": "img/header-img.png"
   },
@@ -40,3 +41,41 @@ const siteContent = {
 // Example: Update the img src for the logo
 let logo = document.getElementById("logo-img");
 logo.setAttribute('src', siteContent["nav"]["img-src"])
+
+
+// 1. Create selectors to point your data into elements
+//Create selectors by using any of the DOM element's methods
+//Note that IDs have been used on all images. Use the IDs to update src path content
+
+// navigation and anchor elements from the header
+const navItems = document.querySelectorAll('nav a')
+console.log(navItems);
+navItems[0].textContent = siteContent["nav"]["nav-item-1"];
+navItems[1].textContent = siteContent["nav"]["nav-item-2"];
+navItems[2].textContent = siteContent["nav"]["nav-item-3"];
+navItems[3].textContent = siteContent["nav"]["nav-item-4"];
+navItems[4].textContent = siteContent["nav"]["nav-item-5"];
+navItems[5].textContent = siteContent["nav"]["nav-item-6"];
+
+
+// section class cta    "h1": "DOM Is Awesome",
+  //  "button": "Get Started",
+  //  "img-src": "img/header-img.png"
+const ctaH1 = document.getElementsByTagName('h1');
+ctaH1[0].textContent = siteContent.cta.h1;
+// ctaH1.setAttribute('cta', siteContent['cta']['h1'])
+console.log(ctaH1)
+
+const ctaH2 = document.querySelector('h2');
+ctaH2.textContent = siteContent.cta.h2;
+console.log(ctaH2);
+// image set 
+const ctaImg = document.getElementById('cta-img').src = siteContent['cta']["img-src"];
+
+
+// 2. Update the HTML with the JSON data
+// Remember, NO direct updating of the HTML source is allowed.
+//  Using your selectors, update the content to match the example file.
+//  Remember to update the src attributes on images
+
+// 3. Add new content
