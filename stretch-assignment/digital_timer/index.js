f95914793884aeda1540f239215751246f5b69b5
echopecho <pbsmithtbn@gmail.com>
2018-10-31 16:39:55 -0500

    Complete SG1 and SG2
    I spent a good deal of time trying to figure out how to use the function with a EventListener and have it stop when I wanted it to.  That was the major hurdle.  Now that I am passed that, the other stretch tasks shouldn't be too bad,

--- stretch-assignment/digital_timer/index.js
+++ stretch-assignment/digital_timer/index.js
@@ -1,12 +1,29 @@
+let clock = document.querySelectorAll('.digit');
 let tensPlace = document.querySelector('#secondTens');
 let onesPlace = document.querySelector('#secondOnes');
 let tenthsPlace = document.querySelector('#msTens');
 let hundredthsPlace = document.querySelector('#msHundreds');
-
-
+let startBtn = document.querySelector('.start');
 
 var counter = 0;
-var tenSecondTimer = setInterval(function() {
+
+// var tenSecondTimer = setInterval(function() {
+//   tensPlace.textContent = Math.floor(counter / 1000);
+//   var oneSecond = counter % 1000;
+//   onesPlace.textContent = Math.floor(oneSecond / 100);
+//   var tenthSecond = oneSecond % 100;
+//   tenthsPlace.textContent = Math.floor(tenthSecond / 10);
+//   var hundredthSecond = tenthSecond % 10;
+//   hundredthsPlace.textContent = Math.floor(hundredthSecond / 1);
+//   counter++;
+
+//   if(counter > 1000) {
+//     clearInterval(tenSecondTimer);
+//     clock.forEach(digit => digit.style.color = 'red');
+//   }
+// }, 10);
+
+function timer() {
   tensPlace.textContent = Math.floor(counter / 1000);
   var oneSecond = counter % 1000;
   onesPlace.textContent = Math.floor(oneSecond / 100);
@@ -15,9 +32,19 @@ var tenSecondTimer = setInterval(function() {
   var hundredthSecond = tenthSecond % 10;
   hundredthsPlace.textContent = Math.floor(hundredthSecond / 1);
   counter++;
+}
 
-  if(counter > 1000) {
-    clearInterval(tenSecondTimer)
-  }
-}, 10);
+startBtn.addEventListener('click', function() {
 
+  let tenSecondTimer = setInterval(function() {
+    timer()
+    
+    startBtn.disabled = true;
+
+    if(counter > 1000) {
+      clearInterval(tenSecondTimer)
+      clock.forEach(digit => digit.style.color = 'red');
+      startBtn.disabled = false;
+    }
+  }, 10)
+});
