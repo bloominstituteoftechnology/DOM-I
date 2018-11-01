cd129c2acf8a176f3862109a33e5c120e6069f06
leila <nb.leila10@gmail.com>
2018-10-31 21:53:54 -0500

    Stretch Project - Make a timer
    Counts up to 10 seconds
    Increments every 10 ms
    Should not count past 10 seconds

---
+++ stretch-assignment/digital_timer/index.js
@@ -0,0 +1,52 @@
+// Initializing the counters
+
+let msTensCounter = 0;
+let msHundredsCounter = 0;
+let secondOnesCounter = 0;
+let secondTensCounter = 0;
+
+// Initializing the timer to 00:00
+
+const msTens = document.querySelector('#msTens');
+msTens.textContent = msTensCounter;
+const msHundreds = document.querySelector('#msHundreds');
+msHundreds.textContent = msHundredsCounter;
+const secondOnes = document.querySelector('#secondOnes');
+secondOnes.textContent = secondOnesCounter;
+const secondTens = document.querySelector('#secondTens');
+secondTens.textContent = secondTensCounter;
+
+//Start the timer for every 10 milliseconds
+const timer = setInterval(setTimer, 10);
+
+function setTimer() {
+  msTens.textContent = msTensCounter + 1;
+  msTensCounter++;
+  if(msTensCounter === 9) {
+    msTensCounter = 0;
+    msTens.textContent = msTensCounter;
+    msHundreds.textContent = msHundredsCounter + 1;
+    msHundredsCounter ++;
+    if(msHundredsCounter === 9) {
+      msTensCounter = 0;
+      msTens.textContent = msTensCounter
+      msHundredsCounter = 0;
+      msHundreds.textContent = msHundredsCounter
+      secondOnes.textContent = secondOnesCounter + 1;
+      secondOnesCounter ++;
+      if(secondOnesCounter === 10) {
+        secondOnesCounter = 0;
+        msTens.textContent = msTensCounter;
+        msHundreds.textContent = msHundredsCounter;
+        secondOnes.textContent = secondOnesCounter;
+        secondTens.textContent = secondTensCounter + 1;
+
+        stopTimer();
+      }
+    }
+  }
+}
+
+function stopTimer() {
+    clearInterval(timer);
+}
