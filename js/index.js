e2be7c027f2336771d1a1c8d9edc0fa96ed4b293
Ashley Owens <ash4741@gmail.com>
2018-10-31 22:25:32 -0400

    Finish MVP
    Need a little guidance on how to make the header Dom is Amazing to be one word per line. Learned that getElementBy methods are extremely restrictive and querySelect is way better.

--- js/index.js
+++ js/index.js
@@ -40,3 +40,74 @@ const siteContent = {
 // Example: Update the img src for the logo
 let logo = document.getElementById("logo-img");
 logo.setAttribute('src', siteContent["nav"]["img-src"])
+
+// Navigation
+let navLink = document.querySelectorAll('a')
+navLink[0].textContent = siteContent['nav']["nav-item-1"]
+navLink[1].textContent = siteContent['nav']["nav-item-2"]
+navLink[2].textContent = siteContent['nav']["nav-item-3"]
+navLink[3].textContent = siteContent['nav']["nav-item-4"]
+navLink[4].textContent = siteContent['nav']["nav-item-5"]
+navLink[5].textContent = siteContent['nav']["nav-item-6"]
+
+// Turn Links Green
+for (let i = 0; i < navLink.length; i++) {
+  let navColor = navLink[i]
+  navColor.style.color = 'green'
+}
+
+// Add Child Element to Nav
+let newChild = document.createElement("a")
+newChild.href = '#'
+newChild.textContent = 'Blog'
+
+let navBar = document.querySelector("nav")
+navBar.appendChild(newChild);
+
+// Add Child Element to Nav
+let anotherNewChild = document.createElement("a")
+anotherNewChild.href = '#'
+anotherNewChild.textContent = 'My CV'
+
+navBar.prepend(anotherNewChild);
+
+// CTA Content
+let ctaH1 = document.querySelector('section.cta .cta-text h1');
+ctaH1.textContent = siteContent['cta']['h1']
+
+let ctaButton = document.querySelector('section.cta .cta-text button');
+ctaButton.textContent = siteContent['cta']['button']
+
+let ctaImage = document.querySelector('section.cta img');
+ctaImage.setAttribute('src', siteContent['cta']['img-src'])
+
+// Main Content Area
+let topContent = document.querySelector('.top-content');
+
+topContent.children[0].querySelector('h4').textContent = siteContent['main-content']['features-h4']
+topContent.children[0].querySelector('p').textContent = siteContent['main-content']['features-content']
+topContent.children[1].querySelector('h4').textContent = siteContent['main-content']['about-h4']
+topContent.children[1].querySelector('p').textContent = siteContent['main-content']['about-content']
+
+let mainImage = document.querySelector('.middle-img');
+mainImage.setAttribute('src', siteContent['main-content']['middle-img-src'])
+
+let bottomContent = document.querySelector('.bottom-content');
+
+bottomContent.children[0].querySelector('h4').textContent = siteContent['main-content']['services-h4']
+bottomContent.children[0].querySelector('p').textContent = siteContent['main-content']['services-content']
+bottomContent.children[1].querySelector('h4').textContent = siteContent['main-content']['product-h4']
+bottomContent.children[1].querySelector('p').textContent = siteContent['main-content']['product-content']
+bottomContent.children[2].querySelector('h4').textContent = siteContent['main-content']['vision-h4']
+bottomContent.children[2].querySelector('p').textContent = siteContent['main-content']['vision-content']
+
+// Contact Area
+let contactArea = document.querySelector('.contact');
+
+contactArea.children[0].textContent = siteContent['contact']['contact-h4']
+contactArea.children[1].textContent = siteContent['contact']['address']
+contactArea.children[2].textContent = siteContent['contact']['phone']
+contactArea.children[3].textContent = siteContent['contact']['email']
+
+// Footer
+let footerContent = document.querySelector('footer p').textContent = siteContent['footer']['copyright']
