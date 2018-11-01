0f5c7812751cc63afc8dafc51e0bed0d3242f439
Taylor Belk <belktaylor12@gmail.com>
2018-10-31 22:08:05 -0500

    added all of main-content to index.js

--- js/index.js
+++ js/index.js
@@ -78,3 +78,101 @@ const ctaImage = document.getElementById('cta-img');
 
 ctaImage.setAttribute('src', siteContent["cta"]["img-src"]);
 
+
+//------------------------------------------------------main-content
+
+// "main-content": {
+//   "features-h4":"Features",   //section class main-content; div class top-content;
+//   "features-content": "Features content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.",
+//   "about-h4":"About",
+//   "about-content": "About content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.",
+//   "middle-img-src": "img/mid-page-accent.jpg",
+//   "services-h4":"Services",
+//   "services-content": "Services content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.",
+//   "product-h4":"Product",
+//   "product-content": "Product content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.",
+//   "vision-h4":"Vision",
+//   "vision-content": "Vision content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.",
+// },
+
+//-------------Features-h4
+
+const featTitle = document.querySelector('div.text-content > h4');
+
+featTitle.textContent = siteContent["main-content"]["features-h4"];
+
+//-------------features-content
+
+const featContent = document.querySelector('div.text-content > p');
+
+featContent.textContent = siteContent["main-content"]["features-content"];
+
+//-------------about-h4
+
+
+const aboutTitle = document.querySelector('div.text-content:nth-child(2) > h4');
+
+aboutTitle.textContent = siteContent["main-content"]["about-h4"];
+
+//-------------about-content
+
+const aboutCont = document.querySelector('div.text-content:nth-child(2) > p');
+
+aboutCont.textContent = siteContent["main-content"]["about-content"];
+
+
+//-------------middle-img-src
+
+const midImage = document.getElementById('middle-img');
+
+midImage.setAttribute('src', siteContent["main-content"]["middle-img-src"]);
+
+//------------services-h4
+
+const servTitle = document.querySelector('div.bottom-content > div.text-content > h4');
+
+servTitle.textContent = siteContent["main-content"]["services-h4"];
+
+//------------services-content
+
+const servCont = document.querySelector('div.bottom-content > div.text-content > p');
+
+servCont.textContent = siteContent["main-content"]["services-content"];
+
+//------------product-h4
+
+const prodTitle = document.querySelector('div.bottom-content > div.text-content:nth-child(2) > h4');
+
+prodTitle.textContent = siteContent["main-content"]["product-h4"];
+
+//------------product-content
+
+const prodCont = document.querySelector('div.bottom-content > div.text-content:nth-child(2) > p');
+
+prodCont.textContent = siteContent["main-content"]["product-content"];
+
+//------------vision-h4
+
+const visionTitle = document.querySelector('div.bottom-content > div.text-content:nth-child(3) > h4');
+
+visionTitle.textContent = siteContent["main-content"]["product-h4"];
+
+//------------vision-content
+
+const visionCont = document.querySelector('div.bottom-content > div.text-content:nth-child(3) > p');
+
+visionCont.textContent = siteContent["main-content"]["vision-content"];
+
+
+
+
+//-------------------------------------------------------------------CONTACT
+
+// "contact": {
+//   "contact-h4" : "Contact",
+//   "address" : "123 Way 456 Street Somewhere, USA",
+//   "phone" : "1 (888) 888-8888",
+//   "email" : "sales@greatidea.io",
+// },
+
+
