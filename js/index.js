8c6b9a55b0c3f0c28fd2eedaa6006edc1c73aea9
karlie0 <kamabrapa88@gmail.com>
2018-11-01 00:31:54 -0400

    Finished updating HTML with JSON data

--- js/index.js
+++ js/index.js
@@ -37,6 +37,66 @@ const siteContent = {
   },
 };
 
-// Example: Update the img src for the logo on html
+// header
+//  header  nav
+let aTags = document.querySelectorAll('a');
+aTags[0].textContent = siteContent["nav"]["nav-item-1"];
+aTags[1].textContent = siteContent["nav"]["nav-item-2"];
+aTags[2].textContent = siteContent["nav"]["nav-item-3"];  
+aTags[3].textContent = siteContent["nav"]["nav-item-4"];
+aTags[4].textContent = siteContent["nav"]["nav-item-5"];
+aTags[5].textContent = siteContent["nav"]["nav-item-6"];
+
+// header img
 let logo = document.getElementById("logo-img");
 logo.setAttribute('src', siteContent["nav"]["img-src"])
+
+// section .cta  .cta-text h1
+const ctaH1 = document.querySelector('.cta-text h1');
+siteContent.cta["h1"] = 'DOM <br> Is <br> Awesome'
+ctaH1.innerHTML = siteContent.cta["h1"];
+
+//  section .cta .cta-text button
+const button = document.querySelector('.cta-text button')
+button.textContent = siteContent["cta"]["button"];
+
+//  section .cta img .cta-img
+const ctaImg = document.querySelector('#cta-img');
+ctaImg.src = siteContent["cta"]["img-src"];
+
+//  section .main-content .top-content  .text-content h4
+const h4TopContent = document.querySelectorAll(".top-content  .text-content h4");
+h4TopContent[0].textContent = siteContent["main-content"]["features-h4"];
+h4TopContent[1].textContent = siteContent["main-content"]["about-h4"];
+
+//  section .main-content .top-content  .text-content p
+const pTopContent = document.querySelectorAll(".top-content  .text-content p");
+pTopContent[0].textContent = siteContent["main-content"]["features-content"];
+pTopContent[1].textContent = siteContent["main-content"]["about-content"];
+
+//  section .main-content img .middle-img
+const middleImg = document.querySelector('#middle-img');
+middleImg.src = siteContent["main-content"]["middle-img-src"];
+
+//  section .main-content .bottom-content  .text-content h4
+const h4BottomContent = document.querySelectorAll(".bottom-content .text-content h4");
+h4BottomContent[0].textContent = siteContent["main-content"]["services-h4"];
+h4BottomContent[1].textContent = siteContent["main-content"]["product-h4"];
+h4BottomContent[2].textContent = siteContent["main-content"]["vision-h4"];
+
+//  section .main-content .bottom-content  .text-content p
+const pBottomContent = document.querySelectorAll(".bottom-content  .text-content p");
+pBottomContent[0].textContent = siteContent["main-content"]["services-content"];
+pBottomContent[1].textContent = siteContent["main-content"]["product-content"];
+pBottomContent[2].textContent = siteContent["main-content"]["vision-content"];
+
+//  section .contact  h4
+const contactH4 = document.querySelector('.contact h4')
+contactH4.textContent = siteContent["contact"]["contact-h4"];
+
+//  section .contact p
+const contactP = document.querySelectorAll('.contact p');
+contactP[0].textContent = siteContent["contact"]["address"];
+contactP[1].textContent = siteContent["contact"]["phone"];
+contactP[2].textContent = siteContent["contact"]["email"];
+
