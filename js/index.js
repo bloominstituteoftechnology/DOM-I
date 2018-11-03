114aea1670ce9e87db1b8feefbbce7a532927f59
Melvin Merlos <melvinamerlos@gmail.com>
2018-11-03 11:48:06 -0400

    update 1: all tasks completed

--- js/index.js
+++ js/index.js
@@ -39,4 +39,60 @@ const siteContent = {
 
 // Example: Update the img src for the logo
 let logo = document.getElementById("logo-img");
-logo.setAttribute('src', siteContent["nav"]["img-src"])
+logo.setAttribute('src', siteContent["nav"]["img-src"]);
+
+// ## Task 2: Create selectors to point your data into elements
+// * [ ] Create selectors by using any of the DOM element's methods
+// * [ ] Note that IDs have been used on all images.  Use the IDs to update src path content
+
+// nav bar --------------------------
+const navItems = document.querySelectorAll('a');
+navItems[0].textContent = 'Services';
+navItems[1].textContent = 'Product';
+navItems[2].textContent = 'Vision';
+navItems[3].textContent = 'Features';
+navItems[4].textContent = 'About';
+navItems[5].textContent = 'Contact';
+
+// cta section --------------------------
+//DOM is Awesome
+const h1 = document.querySelectorAll('h1');
+h1[0].textContent = 'DOM ' + 'Is ' + 'Awesome';
+
+// code snippet
+let cta = document.getElementById("cta-img");
+cta.setAttribute('src', siteContent["cta"]["img-src"]);
+
+// Get started button
+const button = document.getElementsByTagName('button');
+button[0].textContent = 'Get Started';
+
+// main content --------------------------
+const h4 = document.querySelectorAll('h4');
+h4[0].textContent = 'Features';
+h4[1].textContent = 'About';
+h4[2].textContent = 'Services';
+h4[3].textContent = 'Product';
+h4[4].textContent = 'Vision';
+
+// middle image
+let middleImg = document.getElementById("middle-img");
+middleImg.setAttribute('src', siteContent["main-content"]["middle-img-src"]);
+
+// paragraph
+const paragraph = document.querySelectorAll('p');
+paragraph[0].textContent = 'Features content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.';
+paragraph[1].textContent = 'About content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.';
+paragraph[2].textContent = 'Services content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.';
+paragraph[3].textContent = 'Product content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.';
+paragraph[4].textContent = 'Vision content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.';
+
+// contact section --------------------------
+h4[5].textContent = 'Conatct';
+paragraph[5].textContent = '123 Way 456 Street, Somewhere, USA';
+paragraph[6].textContent = '1 (888) 888-8888';
+paragraph[7].textContent = 'sales@greatidea.io';
+
+// footer section --------------------------
+
+paragraph[8].textContent = 'Copyright Great Idea! 2018';
