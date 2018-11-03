4b8d71fe27077ed7d5e2daceb53f9d2f8794c740
derekjones-io <42226382+derekjones-io@users.noreply.github.com>
2018-11-03 01:49:13 -0400

    Change text color of navigation to green

--- js/index.js
+++ js/index.js
@@ -39,6 +39,9 @@ const siteContent = {
 
 // Navigation
 const nav = document.querySelectorAll("nav a");
+
+nav.forEach(element => element.style.color = "green");
+
 nav[0].textContent = siteContent.nav["nav-item-1"];
 nav[1].textContent = siteContent.nav["nav-item-2"];
 nav[2].textContent = siteContent.nav["nav-item-3"];
