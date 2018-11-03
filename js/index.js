a4732f9f2886af22e727a88b64492ec0ca9d1197
derekjones-io <42226382+derekjones-io@users.noreply.github.com>
2018-11-03 01:30:33 -0400

    Finish contact section of great-idea

--- js/index.js
+++ js/index.js
@@ -80,4 +80,11 @@ sectionContent[3].textContent = siteContent["main-content"]["product-content"];
 sectionContent[4].textContent = siteContent["main-content"]["vision-content"];
 
 
+// Contact
+const contactHeader = document.querySelector(".contact h4");
+contactHeader.textContent = siteContent.contact["contact-h4"];
 
+const contactContent = document.querySelectorAll(".contact p");
+contactContent[0].textContent = siteContent.contact["address"];
+contactContent[1].textContent = siteContent.contact["phone"];
+contactContent[2].textContent = siteContent.contact["email"];
