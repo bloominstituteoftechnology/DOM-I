a7c5ebb1280ca4c9188d2bb849475ce6babc4b67
ltann <leeroyleeroy18@gmail.com>
2018-11-05 19:09:57 -0800

    finished task 1-4

--- js/index.js
+++ js/index.js
@@ -40,3 +40,79 @@ const siteContent = {
 // Example: Update the img src for the logo
 let logo = document.getElementById("logo-img");
 logo.setAttribute('src', siteContent["nav"]["img-src"])
+
+//nav
+let aTags = document.querySelectorAll('a')
+
+aTags[0].setAttribute('class', '.nav-item-1')
+aTags[0].innerText = "Service"
+aTags[1].setAttribute('class', '.nav-item-2')
+aTags[1].innerText = "Product"
+aTags[2].setAttribute('class', '.nav-item-3')
+aTags[2].innerText = "Vision"
+aTags[3].setAttribute('class', '.nav-item-4')
+aTags[3].innerText = "Features"
+aTags[4].setAttribute('class', '.nav-item-5')
+aTags[4].innerText = "About"
+aTags[5].setAttribute('class', '.nav-item-6')
+aTags[5].innerText = "Product"
+
+aTags.forEach(link => link.style.color ='green')
+
+//add two nav items
+const newATag1 = document.createElement('a')
+const mainNav = document.querySelector('nav')
+newATag1.innerText = 'Reviews'
+newATag1.href = '#'
+mainNav.prepend(newATag1)
+newATag1.style.color = 'green'
+
+const newATag2 = document.createElement('a')
+const mainNav2 = document.querySelector('nav')
+newATag2.innerText = 'Info'
+newATag2.href = '#'
+mainNav2.append(newATag2)
+newATag2.style.color = 'green'
+
+//main header section
+let mainHeading = document.querySelector('h1')
+mainHeading.textContent = siteContent['cta']['h1']
+
+let mainButton = document.querySelector('button')
+mainButton.innerText = "Get Started"
+
+let logo2 = document.querySelector("#cta-img")
+logo2.setAttribute('src', siteContent["cta"]["img-src"])
+
+//main-content
+let mainContent = document.querySelectorAll('.text-content')
+
+mainContent[0].querySelector('h4').textContent = siteContent['main-content']['features-h4']
+mainContent[0].querySelector('p').textContent = siteContent['main-content']['features-content']
+mainContent[1].querySelector('h4').textContent = siteContent['main-content']['about-h4']
+mainContent[1].querySelector('p').textContent = siteContent['main-content']['about-content']
+mainContent[2].querySelector('h4').textContent = siteContent['main-content']['services-h4']
+mainContent[2].querySelector('p').textContent = siteContent['main-content']['services-content']
+mainContent[3].querySelector('h4').textContent = siteContent['main-content']['product-h4']
+mainContent[3].querySelector('p').textContent = siteContent['main-content']['product-content']
+mainContent[4].querySelector('h4').textContent = siteContent['main-content']['vision-h4']
+mainContent[4].querySelector('p').textContent = siteContent['main-content']['vision-content']
+
+let logo3= document.querySelector("#middle-img")
+logo3.setAttribute('src', siteContent["main-content"]["middle-img-src"])
+
+
+//contact 
+
+let contactInfo = document.querySelector('.contact')
+
+contactInfo.getElementsByTagName('h4')[0].textContent = siteContent["contact"]["contact-h4"]
+contactInfo.getElementsByTagName('p')[0].textContent = siteContent["contact"]["address"]
+contactInfo.getElementsByTagName('p')[1].textContent = siteContent["contact"]["phone"]
+contactInfo.getElementsByTagName('p')[2].textContent = siteContent["contact"]["email"]
+
+//footer
+
+let footer = document.querySelector('footer')
+
+footer.innerText = "Copyright Great Idea! 2018"
