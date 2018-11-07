8aaf496ee1c6bb830a2192961e678557d99249f8
ltann <leeroyleeroy18@gmail.com>
2018-11-06 23:21:44 -0800

    stretch- linked index.js. set timer to 0

---
+++ stretch-assignment/digital_timer/index.js
@@ -0,0 +1,10 @@
+// set timer to 0
+let secondTens = document.querySelector('#secondTens')
+secondTens.textContent = '0'
+let secondOnes = document.querySelector('#secondOnes')
+secondOnes.textContent = '0'
+let msHundreds = document.querySelector('#msHundreds')
+msHundreds.textContent = '0'
+let msTens = document.querySelector('#msTens')
+msTens.textContent = '0'
+
