ace8a6004b39423fd261f41635d01b8143d63578
Ej <ejaona@gmail.com>
2018-11-29 15:59:59 -0600

    Completed task 2 and 3. Created selectors to point data into elements in index.js, updated all img src attributes

--- js/index.js
+++ js/index.js
@@ -1,5 +1,5 @@
 const siteContent = {
-  "nav": {
+  nav: {
     "nav-item-1": "Services",
     "nav-item-2": "Product",
     "nav-item-3": "Vision",
@@ -8,35 +8,137 @@ const siteContent = {
     "nav-item-6": "Contact",
     "img-src": "img/logo.png"
   },
-  "cta": {
-    "h1": "DOM Is Awesome",
-    "button": "Get Started",
+  cta: {
+    h1: "DOM Is Awesome",
+    button: "Get Started",
     "img-src": "img/header-img.png"
   },
   "main-content": {
-    "features-h4":"Features",
-    "features-content": "Features content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.",
-    "about-h4":"About",
-    "about-content": "About content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.",
+    "features-h4": "Features",
+    "features-content":
+      "Features content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.",
+    "about-h4": "About",
+    "about-content":
+      "About content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.",
     "middle-img-src": "img/mid-page-accent.jpg",
-    "services-h4":"Services",
-    "services-content": "Services content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.",
-    "product-h4":"Product",
-    "product-content": "Product content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.",
-    "vision-h4":"Vision",
-    "vision-content": "Vision content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.",
+    "services-h4": "Services",
+    "services-content":
+      "Services content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.",
+    "product-h4": "Product",
+    "product-content":
+      "Product content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.",
+    "vision-h4": "Vision",
+    "vision-content":
+      "Vision content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis."
   },
-  "contact": {
-    "contact-h4" : "Contact",
-    "address" : "123 Way 456 Street Somewhere, USA",
-    "phone" : "1 (888) 888-8888",
-    "email" : "sales@greatidea.io",
+  contact: {
+    "contact-h4": "Contact",
+    address: "123 Way 456 Street Somewhere, USA",
+    phone: "1 (888) 888-8888",
+    email: "sales@greatidea.io"
   },
-  "footer": {
-    "copyright" : "Copyright Great Idea! 2018"
-  },
+  footer: {
+    copyright: "Copyright Great Idea! 2018"
+  }
 };
 
 // Example: Update the img src for the logo
 let logo = document.getElementById("logo-img");
-logo.setAttribute('src', siteContent["nav"]["img-src"])
+logo.setAttribute("src", siteContent["nav"]["img-src"]);
+// ====================== Nav ===========================
+const navigation = document.querySelector("nav");
+const services = navigation.firstElementChild;
+const product = services.nextElementSibling;
+const vision = product.nextElementSibling;
+const features = vision.nextElementSibling;
+const about = features.nextElementSibling;
+const contact = about.nextElementSibling;
+
+services.textContent = "Services";
+product.textContent = "Product";
+vision.textContent = "Vision";
+features.textContent = "Features";
+about.textContent = "About";
+contact.textContent = "Contact";
+
+//=========================== Call To Action ==============
+const ctaText = document.querySelector(".cta").firstElementChild
+  .firstElementChild;
+
+const ctaImg = document.querySelector("#cta-img");
+
+ctaText.textContent = "DOM Is Awesome";
+const ctaBtn = ctaText.nextElementSibling;
+ctaBtn.textContent = "Get Started";
+ctaImg.src = "img/header-img.png";
+
+//========================== Main Content =================
+
+const mainContent = document.querySelector(".main-content");
+const topContent = document.querySelector(".top-content");
+const mainTextDiv = document.querySelectorAll(".text-content");
+const middleImg = document.querySelector("#middle-img");
+const secondTopDiv = topContent.nextElementSibling;
+
+Array.from(mainTextDiv);
+mainTextDiv[0].classList.add("first-text-content");
+mainTextDiv[1].classList.add("second-text-content");
+mainTextDiv[2].classList.add("third-text-content");
+mainTextDiv[3].classList.add("fourth-text-content");
+mainTextDiv[4].classList.add("fith-text-content");
+
+const mainH4 = document.querySelector(".first-text-content").firstElementChild;
+const secondH4 = document.querySelector(".second-text-content")
+  .firstElementChild;
+const thirdH4 = document.querySelector(".third-text-content").firstElementChild;
+const fourthH4 = document.querySelector(".fourth-text-content")
+  .firstElementChild;
+const fithH4 = document.querySelector(".fith-text-content").firstElementChild;
+
+const mainPtag = mainH4.nextElementSibling;
+const secondPtag = secondH4.nextElementSibling;
+const thirdPtag = thirdH4.nextElementSibling;
+const fourthPtag = fourthH4.nextElementSibling;
+const fithPtag = fithH4.nextElementSibling;
+
+mainH4.textContent = "Features";
+mainPtag.textContent =
+  "Features content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.";
+
+secondH4.textContent = "About";
+secondPtag.textContent =
+  "About content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.";
+
+fourthH4.textContent = "Product";
+fourthPtag.textContent =
+  "Product content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.";
+
+thirdH4.textContent = "Services";
+thirdPtag.textContent =
+  "Services content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.";
+
+fithH4.textContent = "Vision";
+fithPtag.textContent =
+  "Vision content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.";
+
+middleImg.src = "img/mid-page-accent.jpg";
+
+//===================================== Contact =======================================
+
+const contactSection = document.querySelector(".contact");
+const contactH4 = contactSection.firstElementChild;
+const contactP1 = contactH4.nextElementSibling;
+const contactP2 = contactP1.nextElementSibling;
+const contactP3 = contactP2.nextElementSibling;
+
+contactH4.textContent = "Contact";
+contactP1.textContent = "123 Way 456 Street Somewhere, USA";
+contactP2.textContent = "1 (888) 888-8888";
+contactP3.textContent = "sales@greatidea.io";
+
+//===================================== Footer =========================================
+
+const footer = document.querySelector("footer");
+const footerPtag = footer.firstElementChild;
+
+footerPtag.textContent = "Copyright Great Idea! 2018";
