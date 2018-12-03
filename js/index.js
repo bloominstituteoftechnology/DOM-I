ded000700e5cf5a889d161f5060a1c80d7fc7b57
stellarluminant <stellarluminant@gmail.com>
2018-12-03 15:30:19 -0800

    Add json content
    index.js now contains a rather poorly hard-coded program for inserting the JSON data into the DOM.

--- js/index.js
+++ js/index.js
@@ -39,4 +39,56 @@ const siteContent = {
 
 // Example: Update the img src for the logo
 let logo = document.getElementById("logo-img");
-logo.setAttribute('src', siteContent["nav"]["img-src"])
+logo.setAttribute('src', siteContent["nav"]["img-src"]);
+
+let navLinks = document.querySelectorAll("nav a");
+navLinks[0].textContent = siteContent["nav"]["nav-item-1"];
+navLinks[1].textContent = siteContent["nav"]["nav-item-2"];
+navLinks[2].textContent = siteContent["nav"]["nav-item-3"];
+navLinks[3].textContent = siteContent["nav"]["nav-item-4"];
+navLinks[4].textContent = siteContent["nav"]["nav-item-5"];
+navLinks[5].textContent = siteContent["nav"]["nav-item-6"];
+
+let ctaH1 = document.querySelector(".cta h1");
+ctaH1.textContent = siteContent["cta"]["h1"];
+
+let ctaButton = document.querySelector(".cta button");
+ctaButton.textContent = siteContent["cta"]["button"];
+
+let ctaImg = document.querySelector("#cta-img");
+ctaImg.src = siteContent["cta"]["img-src"];
+
+let topContent = document.querySelectorAll(".main-content .top-content .text-content");
+
+topContent[0].querySelector("h4").textContent = siteContent["main-content"]["features-h4"];
+topContent[0].querySelector("p").textContent = siteContent["main-content"]["features-content"];
+
+topContent[1].querySelector("h4").textContent = siteContent["main-content"]["about-h4"];
+topContent[1].querySelector("p").textContent = siteContent["main-content"]["about-content"];
+
+let middleImg = document.querySelector("#middle-img");
+middleImg.src = siteContent["main-content"]["middle-img-src"];
+
+let bottomContent = document.querySelectorAll(".main-content .bottom-content .text-content");
+
+bottomContent[0].querySelector("h4").textContent = siteContent["main-content"]["services-h4"];
+bottomContent[0].querySelector("p").textContent = siteContent["main-content"]["services-content"];
+
+bottomContent[1].querySelector("h4").textContent = siteContent["main-content"]["product-h4"];
+bottomContent[1].querySelector("p").textContent = siteContent["main-content"]["product-content"];
+
+bottomContent[2].querySelector("h4").textContent = siteContent["main-content"]["vision-h4"];
+bottomContent[2].querySelector("p").textContent = siteContent["main-content"]["vision-content"];
+
+let contact = document.querySelector(".contact");
+contact.querySelector("h4").textContent = siteContent["contact"]["contact-h4"];
+
+let contactTexts = document.querySelectorAll(".contact p");
+contactTexts[0].textContent = siteContent["contact"]["address"];
+contactTexts[1].textContent = siteContent["contact"]["phone"];
+contactTexts[2].textContent = siteContent["contact"]["email"];
+
+let footerCopyright = document.querySelector("footer p");
+footerCopyright.textContent = siteContent["footer"]["copyright"];
+
+
