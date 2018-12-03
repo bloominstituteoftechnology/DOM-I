8d3e7760da6748500960b14120841acf117a0ce0
umbur <work1415@gmail.com>
2018-12-03 15:15:13 -0700

    Task 2 completed

--- js/index.js
+++ js/index.js
@@ -42,5 +42,54 @@ let logo = document.getElementById("logo-img");
 logo.setAttribute('src', siteContent["nav"]["img-src"])
 
 // navigation
-const nav = document.querySelectorAll('a');
-nav.setAttribute('src', siteContent["nav"]["a"])
+const navigation = document.querySelectorAll('a');
+navigation[0].textContent = siteContent.nav["nav-item-1"];
+navigation[1].textContent = siteContent.nav["nav-item-2"];
+navigation[2].textContent = siteContent.nav["nav-item-3"];
+navigation[3].textContent = siteContent.nav["nav-item-4"];
+navigation[4].textContent = siteContent.nav["nav-item-5"];
+navigation[5].textContent = siteContent.nav["nav-item-6"];
+
+// image header
+let himage = document.getElementById("cta-img");
+himage.src = siteContent['cta']['img-src'];
+
+// cta h1
+let ctih1 = document.querySelector('.cta .cta-text h1');
+ctih1.textContent = siteContent['cta']['h1'];
+
+// cta button
+let ctaButton = document.querySelector('.cta .cta-text button');
+ctaButton.textContent = siteContent["cta"]["button"];
+
+// main content
+const mTitles = document.querySelectorAll('h4');
+mTitles[0].textContent = siteContent["main-content"]["features-h4"];
+mTitles[1].textContent = siteContent["main-content"]["about-h4"];
+mTitles[2].textContent = siteContent["main-content"]["services-h4"];
+mTitles[3].textContent = siteContent["main-content"]["product-h4"];
+mTitles[4].textContent = siteContent["main-content"]["vision-h4"];
+
+const mContent = document.querySelectorAll('p');
+mContent[0].textContent = siteContent["main-content"]["features-content"];
+mContent[1].textContent = siteContent["main-content"]["about-content"];
+mContent[2].textContent = siteContent["main-content"]["services-content"];
+mContent[3].textContent = siteContent["main-content"]["product-content"];
+mContent[4].textContent = siteContent["main-content"]["vision-content"];
+
+// image between contents
+const mImage = document.getElementById("middle-img");
+mImage.setAttribute('src', siteContent["main-content"]["middle-img-src"]);
+
+//Contact info
+const cTitle = document.querySelector('.contact h4');
+cTitle.textContent = siteContent.contact["contact-h4"];
+const cPar = document.querySelectorAll('.contact p');
+cPar[0].textContent = siteContent.contact["address"];
+cPar[1].textContent = siteContent.contact["phone"];
+cPar[2].textContent = siteContent.contact["email"];
+
+
+// Footer
+const footer = document.querySelector('footer p');
+footer.textContent = siteContent.footer["copyright"]; 
