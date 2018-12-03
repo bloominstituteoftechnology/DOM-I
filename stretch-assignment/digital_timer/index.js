ce543c42ff50b1ee3e215c22ec49e9b5319d1998
ltrii <lelandtr@gmail.com>
2018-12-03 14:58:50 -0800

    Some digital timer work

---
+++ stretch-assignment/digital_timer/index.js
@@ -0,0 +1,8 @@
+let mstens = document.querySelector('#msTens');
+let mshund = document.querySelector('#msHundreds');
+let secones = document.querySelector('#secondOnes');
+let sectens = document.querySelector('#secondTens');
+
+for(let mt = 0; mt < 10; mt++) {
+    
+}
