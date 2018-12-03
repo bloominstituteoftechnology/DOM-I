7a9e987345015bb70515b41fc0f68cd960ab6206
Moonkaman <nickbatmanglidj@gmail.com>
2018-12-03 13:05:39 -0700

    Add start button to timer

--- stretch-assignment/digital_timer/index.js
+++ stretch-assignment/digital_timer/index.js
@@ -1,24 +1,40 @@
 const digits = document.querySelectorAll('.digit');
+const startBtn = document.querySelector('.start-btn');
+let going = false;
 
-digits[0].textContent = digits[1].textContent = digits[3].textContent = digits[4].textContent = 0;
-
-window.setInterval( _ => {
-  if(digits[0].textContent != 1){
-    if(digits[4].textContent < 9) {
-      digits[4].textContent++;
-    } else {
-      digits[3].textContent++;
-      digits[4].textContent = 0;
-    }
-
-    if(digits[3].textContent == 9) {
-      digits[1].textContent++;
-      digits[3].textContent = 0;
-    }
-
-    if(digits[1].textContent == 10) {
-      digits[0].textContent++;
-      digits[1].textContent = 0;
-    }
+startBtn.addEventListener('click', _ => {
+  if(going === false){
+    digits[0].textContent = digits[1].textContent = digits[3].textContent = digits[4].textContent = 0;
+    timer();
   }
-}, 10)
+});
+
+function timer(){
+  going = true;
+  startBtn.classList.remove('start-btn');
+  startBtn.classList.add('start-btn-lock');
+  window.setInterval( _ => {
+    if(digits[0].textContent != 1) {
+      if(digits[4].textContent < 9) {
+        digits[4].textContent++;
+      } else {
+        digits[3].textContent++;
+        digits[4].textContent = 0;
+      }
+
+      if(digits[3].textContent == 9) {
+        digits[1].textContent++;
+        digits[3].textContent = 0;
+      }
+
+      if(digits[1].textContent == 10) {
+        digits[0].textContent++;
+        digits[1].textContent = 0;
+      }
+    } else {
+      going = false;
+      startBtn.classList.add('start-btn');
+      startBtn.classList.remove('start-btn-lock');
+    }
+  }, 10);
+}
