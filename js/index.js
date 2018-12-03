e4ae3bf55500bc894fb7774de43c5d7276dd2c53
rmejia-tajc <rmejia.tajc@gmail.com>
2018-12-03 14:36:08 -0700

    update nav links

--- js/index.js
+++ js/index.js
@@ -39,10 +39,21 @@ const siteContent = {
 
 // Example: Update the img src for the logo
 let logo = document.getElementById("logo-img");
-logo.setAttribute('src', siteContent["nav"]["img-src"])
+logo.setAttribute('src', siteContent["nav"]["img-src"]);
 
 const cta = document.getElementById("cta-img");
 cta.src = siteContent.cta["img-src"];
 
 const middle = document.getElementById("middle-img");
 middle.setAttribute('src', siteContent["main-content"]["middle-img-src"]);
+
+const nav = document.querySelectorAll('a');
+nav[0].textContent = siteContent.nav["nav-item-1"];
+nav[1].textContent = siteContent.nav["nav-item-2"];
+nav[2].textContent = siteContent.nav["nav-item-3"];
+nav[3].textContent = siteContent.nav["nav-item-4"];
+nav[4].textContent = siteContent.nav["nav-item-5"];
+nav[5].textContent = siteContent.nav["nav-item-6"];
+
+
+
