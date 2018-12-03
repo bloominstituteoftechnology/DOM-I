af85eb99047c94cafe60183bbe6b4424e03a7635
Hamza Elkhoudiri <elkhoudh@gmail.com>
2018-12-03 13:25:53 -0500

    Added Comments

--- js/index.js
+++ js/index.js
@@ -46,6 +46,22 @@ const siteContent = {
 let logo = document.getElementById("logo-img");
 logo.setAttribute("src", siteContent["nav"]["img-src"]);
 
+// Header Title Text
+let h1 = document.getElementsByTagName("h1");
+h1[0].textContent = siteContent.cta.h1;
+
+// Call to Action Button
+let button = document.getElementsByTagName("button");
+button[0].textContent = siteContent.cta.button;
+
+// Paragraph Titles
+let h4 = document.getElementsByTagName("h4");
+h4[0].textContent = siteContent["main-content"]["features-h4"];
+h4[1].textContent = siteContent["main-content"]["about-h4"];
+h4[2].textContent = siteContent["main-content"]["services-h4"];
+h4[3].textContent = siteContent["main-content"]["product-h4"];
+h4[4].textContent = siteContent["main-content"]["vision-h4"];
+
 // Code snippet image
 let snippet = document.getElementById("cta-img");
 snippet.setAttribute("src", siteContent["cta"]["img-src"]);
@@ -59,6 +75,7 @@ headerLink1[3].textContent = siteContent.nav["nav-item-4"];
 headerLink1[4].textContent = siteContent.nav["nav-item-5"];
 headerLink1[5].textContent = siteContent.nav["nav-item-6"];
 
+// Paragraph Text
 let featuresPTags = document.querySelectorAll("p");
 let contentArray = [
   siteContent["main-content"]["features-content"],
@@ -70,20 +87,12 @@ let contentArray = [
 for (let i = 0; i < featuresPTags.length; i++) {
   featuresPTags[i].textContent = contentArray[i];
 }
+
+// Middle Image
 let middleImage = document.querySelector("#middle-img");
 middleImage.setAttribute("src", siteContent["main-content"]["middle-img-src"]);
 
-let h1 = document.getElementsByTagName("h1");
-h1[0].textContent = siteContent.cta.h1;
-let button = document.getElementsByTagName("button");
-button[0].textContent = siteContent.cta.button;
-let h4 = document.getElementsByTagName("h4");
-h4[0].textContent = siteContent["main-content"]["features-h4"];
-h4[1].textContent = siteContent["main-content"]["about-h4"];
-h4[2].textContent = siteContent["main-content"]["services-h4"];
-h4[3].textContent = siteContent["main-content"]["product-h4"];
-h4[4].textContent = siteContent["main-content"]["vision-h4"];
-
+// Contact Section
 let contact = document.querySelector(".contact").querySelectorAll("p");
 let contacth4 = document.querySelector(".contact").querySelectorAll("h4");
 contacth4[0].textContent = siteContent.contact["contact-h4"];
@@ -91,5 +100,6 @@ contact[0].textContent = siteContent.contact.address;
 contact[1].textContent = siteContent.contact.phone;
 contact[2].textContent = siteContent.contact.email;
 
+// Footer Section
 let footerText = document.querySelector("footer").querySelector("p");
 footerText.innerText = siteContent.footer.copyright;
