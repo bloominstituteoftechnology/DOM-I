69e1bcd84708b9fcb1208b43e7c895b7d9f5e545
Daniel Weinman <namniewd@gmail.com>
2018-12-03 15:01:42 -0500

    initial commit updated DOM using object from index.js

--- js/index.js
+++ js/index.js
@@ -40,3 +40,60 @@ const siteContent = {
 // Example: Update the img src for the logo
 let logo = document.getElementById("logo-img");
 logo.setAttribute('src', siteContent["nav"]["img-src"])
+
+// nav DOM manipulation
+const navItems = document.querySelectorAll('a');
+
+navItems[0].textContent = siteContent.nav["nav-item-1"];
+navItems[1].textContent = siteContent.nav["nav-item-2"];
+navItems[2].textContent = siteContent.nav["nav-item-3"];
+navItems[3].textContent = siteContent.nav["nav-item-4"];
+navItems[4].textContent = siteContent.nav["nav-item-5"];
+navItems[5].textContent = siteContent.nav["nav-item-6"];
+
+// CTA DOM manipulation
+const mainHeaderText = document.querySelector('h1');
+const button = document.querySelector('button');
+const headerImage = document.querySelector('#cta-img');
+
+mainHeaderText.textContent = `${siteContent.cta.h1.slice(0,3)} \r\n`;
+mainHeaderText.textContent += `${siteContent.cta.h1.slice(4,6)} \r\n`;
+mainHeaderText.textContent += `${siteContent.cta.h1.slice(7)}`;
+button.textContent = siteContent.cta.button;
+headerImage.src = siteContent.cta["img-src"];
+
+// Main Content DOM Manipulation
+const topH4 = document.querySelectorAll('.main-content .top-content .text-content h4');
+const topContent = document.querySelectorAll('.main-content .top-content .text-content p');
+const middleImage = document.querySelector('#middle-img');
+const bottomH4 = document.querySelectorAll('.main-content .bottom-content .text-content h4');
+const bottomContent = document.querySelectorAll('.main-content .bottom-content .text-content p');
+
+topH4[0].textContent =  siteContent["main-content"]["features-h4"];
+topH4[1].textContent =  siteContent["main-content"]["about-h4"];
+topContent[0].textContent =  siteContent["main-content"]["features-content"];
+topContent[1].textContent =  siteContent["main-content"]["about-content"];
+middleImage.src = siteContent["main-content"]["middle-img-src"];
+bottomH4[0].textContent = siteContent["main-content"]["services-h4"];
+bottomH4[1].textContent = siteContent["main-content"]["product-h4"];
+bottomH4[2].textContent = siteContent["main-content"]["vision-h4"];
+bottomContent[0].textContent =  siteContent["main-content"]["services-content"];
+bottomContent[1].textContent =  siteContent["main-content"]["product-content"];
+bottomContent[2].textContent =  siteContent["main-content"]["vision-content"];
+
+// Contact DOM manipulation
+const contactH4 = document.querySelector('.contact h4');
+const contactParagraphs = document.querySelectorAll('.contact p');
+contactH4.textContent = siteContent.contact["contact-h4"];
+contactParagraphs[0].textContent = siteContent.contact.address;
+contactParagraphs[1].textContent = siteContent.contact.phone;
+contactParagraphs[2].textContent = siteContent.contact.email;
+
+// Footer DOM manipulation
+const footer = document.querySelector('footer p');
+footer.textContent = siteContent.footer.copyright;
+
+
+
+
+
