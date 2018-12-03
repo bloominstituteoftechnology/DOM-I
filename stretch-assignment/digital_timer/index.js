d96047a2ae9bd22218accd3869f6c42a682e1d08
Moonkaman <nickbatmanglidj@gmail.com>
2018-12-03 14:03:53 -0700

    Finished timer stretch + extra lap functionality

--- stretch-assignment/digital_timer/index.js
+++ stretch-assignment/digital_timer/index.js
@@ -1,11 +1,22 @@
 const digits = document.querySelectorAll('.digit');
 const startBtn = document.querySelector('.start-btn');
+const currLap = document.querySelector('.current .the-lap');
+const currtime = document.querySelector('.current .time');
+
 let going = false;
+let currentLap = 0;
+let reset = false;
 
 startBtn.addEventListener('click', _ => {
+  if(reset) location.reload();
+
   if(going === false){
     digits[0].textContent = digits[1].textContent = digits[3].textContent = digits[4].textContent = 0;
+    startBtn.textContent = "Lap"
     timer();
+  }else {
+    currentLap = lap();
+    makeLap(`Lap ${currentLap}`, `${digits[0].textContent}${digits[1].textContent}:${digits[3].textContent}${digits[4].textContent}`);
   }
 });
 
@@ -14,6 +25,8 @@ function timer(){
   startBtn.classList.remove('start-btn');
   startBtn.classList.add('start-btn-lock');
   window.setInterval( _ => {
+    currLap.textContent = `Lap ${currentLap+1}`;
+    currtime.textContent = `${digits[0].textContent}${digits[1].textContent}:${digits[3].textContent}${digits[4].textContent}`;
     if(digits[0].textContent != 1) {
       if(digits[4].textContent < 9) {
         digits[4].textContent++;
@@ -33,8 +46,32 @@ function timer(){
       }
     } else {
       going = false;
+      reset = true;
       startBtn.classList.add('start-btn');
       startBtn.classList.remove('start-btn-lock');
+      startBtn.textContent = 'Reset & Start Again';
     }
   }, 10);
+
+}
+
+const lap = (_ => {
+  let counter = 1;
+  return _ => counter++; return counter;
+})();
+
+function makeLap(theLap, theTime){
+  const lapCont = document.createElement('div');
+  const lapNum = document.createElement('p');
+  const lapTime = document.createElement('p');
+
+  lapCont.classList.add('lap');
+  lapNum.classList.add('the-lap');
+  lapTime.classList.add('time');
+
+  lapNum.textContent = theLap;
+  lapTime.textContent = theTime;
+
+  lapCont.append(lapNum, lapTime);
+  document.querySelector('.past-laps').prepend(lapCont);
 }
