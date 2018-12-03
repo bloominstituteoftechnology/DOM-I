fca9b52321fd090c569e706398d105fbdc7f1724
croy <roy.christie95@gmail.com>
2018-12-03 17:12:31 -0500

    Finished page, I'm sorry I forgot to commit, refactoring for DRY code now

--- js/index.js
+++ js/index.js
@@ -40,7 +40,6 @@ const siteContent = {
 // Navigation Content
 
 const navItems = document.querySelectorAll("a");
-
 for (let i = 0; i < navItems.length; i++) {
   navItems[i].textContent = siteContent.nav[`nav-item-${i + 1}`];
 }
@@ -49,10 +48,42 @@ let logo = document.getElementById("logo-img");
 logo.setAttribute('src', siteContent["nav"]["img-src"])
 
 // CTA Content
-const ctaContent = document.querySelector('.cta');
+const ctaText = document.querySelector('.cta-text h1');
+ctaText.textContent = siteContent["cta"]["h1"]
+const ctaButton = document.querySelector('.cta-text button');
+ctaButton.textContent = siteContent["cta"]["button"]
 
-// let ctaImg = document.getElementById("cta-img");
-// logo.setAttribute('src', siteContent['cta']['img-src'])
+let ctaImg = document.getElementById("cta-img");
+ctaImg.setAttribute('src', siteContent["cta"]["img-src"])
 
-// let middleImg = document.getElementById("middle-img");
-// logo.setAttribute('src', siteContent['main-content']['img-src'])
+// Main Content
+mainContentH4 = Array.from(document.querySelectorAll(".main-content .text-content h4"));
+mainContentH4[0].textContent = siteContent["main-content"]["features-h4"];
+mainContentH4[1].textContent = siteContent["main-content"]["about-h4"];
+mainContentH4[2].textContent = siteContent["main-content"]["services-h4"];
+mainContentH4[3].textContent = siteContent["main-content"]["product-h4"];
+mainContentH4[4].textContent = siteContent["main-content"]["vision-h4"];
+
+mainContentText = Array.from(document.querySelectorAll(".main-content .text-content p"));
+mainContentText[0].textContent = siteContent["main-content"]["features-content"];
+mainContentText[1].textContent = siteContent["main-content"]["about-content"];
+mainContentText[2].textContent = siteContent["main-content"]["services-content"];
+mainContentText[3].textContent = siteContent["main-content"]["product-content"];
+mainContentText[4].textContent = siteContent["main-content"]["vision-content"];
+
+let middleImg = document.getElementById("middle-img");
+middleImg.setAttribute('src', siteContent['main-content']['middle-img-src'])
+
+
+// Contact
+const contactHeader = document.querySelector(".contact h4");
+contactHeader.textContent = siteContent["contact"]["contact-h4"]
+
+const contactText = Array.from(document.querySelectorAll(".contact p"));
+contactText[0].textContent = siteContent["contact"]["address"];
+contactText[1].textContent = siteContent["contact"]["phone"];
+contactText[2].textContent = siteContent["contact"]["email"];
+
+// Footer
+const footerText = document.querySelector("footer");
+footerText.textContent = siteContent["footer"]["copyright"];
