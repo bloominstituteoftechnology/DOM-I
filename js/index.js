c709a3b0a86d41342c562902529489a3e6eebe4d
umbur <work1415@gmail.com>
2018-12-03 13:47:01 -0700

    initial commit

--- js/index.js
+++ js/index.js
@@ -40,3 +40,7 @@ const siteContent = {
 // Example: Update the img src for the logo
 let logo = document.getElementById("logo-img");
 logo.setAttribute('src', siteContent["nav"]["img-src"])
+
+// navigation
+const nav = document.querySelectorAll('a');
+nav.setAttribute('src', siteContent["nav"]["a"])
