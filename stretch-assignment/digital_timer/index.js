e3754708189299d2ba72e946ab373743fb80f210
kjohna <43828763+kjohna@users.noreply.github.com>
2018-12-04 08:14:11 -0700

    first draft of stretch-assignment

---
+++ stretch-assignment/digital_timer/index.js
@@ -0,0 +1,28 @@
+
+function setup () {
+  const digits = document.querySelectorAll(".digits");
+  console.log(digits[0].childNodes);
+  let count = 0;
+  const stop = 100 * 10; // ms/s * s -- set stop time
+
+  const delay = window.setInterval(() => {
+    digits[0].childNodes.forEach(element => {
+      if (element.id === "msTens") {
+        element.textContent = count % 10;
+      } else if (element.id === "msHundreds") {
+        element.textContent = Math.floor(count / 10) % 10;
+      } else if (element.id === "secondOnes") {
+        element.textContent = Math.floor(count / 100) % 10;
+      } else if (element.id === "secondTens") {
+        element.textContent = Math.floor(count / 1000) % 10;
+      }
+    });
+    if (count < 1000) {
+      count ++;
+    } else {
+      window.clearInterval(delay);
+    }
+  }, 10); // execute every 10 ms
+}
+setup();
+
