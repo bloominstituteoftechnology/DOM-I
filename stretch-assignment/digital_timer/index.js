f11de1a42b1f143400fc70653cecd6af25e8fe46
kjohna <43828763+kjohna@users.noreply.github.com>
2018-12-04 08:55:18 -0700

    add start/stop buttons

--- stretch-assignment/digital_timer/index.js
+++ stretch-assignment/digital_timer/index.js
@@ -1,39 +1,27 @@
 
 function startTimer () {
-  const digits = document.querySelectorAll(".digits");
-  console.log(digits[0]);
   let count = 0;
   const stop = 100 * 10; // ms/s * s -- set stop time
-
   const delay = window.setInterval(() => {
     const msTens = count % 10;
     const msHundreds = Math.floor(count / 10) % 10;
     const secondOnes = Math.floor(count / 100) % 10;
     const secondTens = Math.floor(count / 1000) % 10;
     setDigits (msTens, msHundreds, secondOnes, secondTens);
-    // digits[0].childNodes.forEach(element => {
-    //   if (element.id === "msTens") {
-    //     element.textContent = count % 10;
-    //   } else if (element.id === "msHundreds") {
-    //     element.textContent = Math.floor(count / 10) % 10;
-    //   } else if (element.id === "secondOnes") {
-    //     element.textContent = Math.floor(count / 100) % 10;
-    //   } else if (element.id === "secondTens") {
-    //     element.textContent = Math.floor(count / 1000) % 10;
-    //   }
-    // });
     if (count < stop) {
       count ++;
     } else {
+      const digits = document.querySelectorAll(".digits");
       window.clearInterval(delay);
-      digits[0].className = "redDigit";
+      digits[0].childNodes.forEach(child => {
+        child.className = "digit redDigit";
+      });
     }
   }, 10); // execute every 10 ms
 }
-startTimer();
 
 function resetTimer () {
-
+  setDigits ("-", "-", "-", "-");
 }
 
 function setDigits (msTens, msHundreds, secondOnes, secondTens) {
@@ -51,3 +39,8 @@ function setDigits (msTens, msHundreds, secondOnes, secondTens) {
   });
 }
 
+const startButton = document.querySelector("#startButton");
+console.log(startButton);
+startButton.addEventListener("click", startTimer);
+const resetButton = document.querySelector("#resetButton");
+resetButton.addEventListener("click", resetTimer);
