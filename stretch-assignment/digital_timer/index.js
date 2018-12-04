0b3be6f600d9a9c520bf20b72650020e28cd9e59
Nathan Thomas <nwthomas@me.com>
2018-12-03 16:04:40 -0800

    Create timer feature starting at 0

--- stretch-assignment/digital_timer/index.js
+++ stretch-assignment/digital_timer/index.js
@@ -1,25 +1,76 @@
 const main = document.querySelector("main");
 const header = document.createElement("h1");
-header.textContent = "The Final Countdown";
+header.textContent = "The Final Countup";
 main.prepend(header);
 const button = document.querySelector("input");
-const msTens = document.querySelector("#msTens");
-const msHundreds = document.querySelector("#msHundreds");
-const secOnes = document.querySelector("#secondOnes");
-const secTens = document.querySelector("#secondTens");
-const semicolon = document.querySelector("#colon");
-
+let msTens = document.querySelector("#msTens");
+let msHundreds = document.querySelector("#msHundreds");
+let secOnes = document.querySelector("#secondOnes");
+let secTens = document.querySelector("#secondTens");
+let semicolon = document.querySelector("#colon");
 let timerGoing = false;
+let timer;
 
-function start() {}
+function updateSecTens() {
+  secTens.textContent = parseInt(secTens.textContent) + 1;
+  timer = clearInterval(timer);
+  timerGoing = false;
+}
 
-function pause() {}
+function updateSecOnes() {
+  if (secOnes.textContent === "9") {
+    secOnes.textContent = 0;
+    updateSecTens();
+  } else {
+    secOnes.textContent = parseInt(secOnes.textContent) + 1;
+  }
+}
+
+function updateMSHundreds() {
+  if (msHundreds.textContent === "9") {
+    msHundreds.textContent = 0;
+    updateSecOnes();
+  } else {
+    msHundreds.textContent = parseInt(msHundreds.textContent) + 1;
+  }
+}
+
+function updateMSTens() {
+  if (msTens.textContent === "9") {
+    msTens.textContent = 0;
+    updateMSHundreds();
+  } else {
+    msTens.textContent = parseInt(msTens.textContent) + 1;
+  }
+}
+
+function start() {
+  if (timerGoing === false) {
+    timerGoing = true;
+    timer = setInterval(updateMSTens, 10);
+    button.textContent = "Pause";
+  } else {
+    timer = clearInterval(timer);
+    button.textContent = "Start";
+    timerGoing = false;
+  }
+}
+
+function restart() {
+  timerGoing = false;
+  timer = 0;
+  secTens = 1;
+  secOnes = 0;
+  msTens = 0;
+  msHundreds = 0;
+}
 
 button.addEventListener("click", () => {
   if (timerGoing === false) {
     start();
   } else if (timerGoing === true) {
-    pause();
+    restart();
+    console.log("!");
   } else {
     alert("Something went wrong.");
   }
