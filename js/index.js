597f155ca4ef74ab9770e26bae226d4617cea9c1
Samsoucy <newf930@outlook.com>
2018-12-03 19:55:49 -0500

    task four

--- js/index.js
+++ js/index.js
@@ -38,38 +38,49 @@ const siteContent = {
 };
 
 // Example: Update the img src for the logo
-let logo = document.getElementById("logo-img");
+const logo = document.getElementById("logo-img");
 logo.setAttribute('src', siteContent["nav"]["img-src"])
 
-let headerImage = document.getElementById("cta-img");
+const headerImage = document.getElementById("cta-img");
 headerImage.src = "img/header-img.png";
 headerImage.alt = "Header image";
 
 
-let mainContent = document.getElementById("middle-img");
+const mainContent = document.getElementById("middle-img");
 mainContent.src = "img/mid-page-accent.jpg";
 mainContent.alt = "Middle image";
 
 
 
+
 const nav = document.getElementsByTagName("a");
 console.log(nav);
-nav[0].textContent = siteContent.nav["nav-item-1"];
-nav[1].textContent = siteContent.nav["nav-item-2"];
-nav[2].textContent = siteContent.nav["nav-item-3"];
-nav[3].textContent = siteContent.nav["nav-item-4"];
-nav[4].textContent = siteContent.nav["nav-item-5"];
-nav[5].textContent = siteContent.nav["nav-item-6"];
+nav[0].textContent = siteContent["nav"]["nav-item-1"];
+nav[0].style.color = "green";
+nav[1].textContent = siteContent["nav"]["nav-item-2"];
+nav[1].style.color = "green";
+nav[2].textContent = siteContent["nav"]["nav-item-3"];
+nav[2].style.color = "green";
+nav[3].textContent = siteContent["nav"]["nav-item-4"];
+nav[3].style.color = "green";
+nav[4].textContent = siteContent["nav"]["nav-item-5"];
+nav[4].style.color = "green";
+nav[5].textContent = siteContent["nav"]["nav-item-6"];
+nav[5].style.color = "green";
+
+
 
 
 const ctaHeader = document.getElementsByTagName("h1");
 ctaHeader.style = "white-space: pre";
-ctaHeader[0].textContent ="DOM \r\n";
-ctaHeader[0].textContent +="is \r\n";
-ctaHeader[0].textContent += "Awesome";
+ctaHeader[0].innerText = "DOM \r\n";
+ctaHeader[0].innerText += "Is \r\n";
+ctaHeader[0].innerText += "Awesome";
 
 const ctaButton = document.getElementsByTagName("button");
 ctaButton[0].textContent = "Get Started";
+
+
 
 const htags = document.getElementsByTagName("h4");
 
@@ -79,4 +90,19 @@ htags[2].innerText = "Services";
 htags[3].innerText = "Product";
 htags[4].innerText = "Vision";
 htags[5].innerText = "Contact";
+
 const pTags = document.getElementsByTagName("p");
+
+pTags[0].innerText = "Features content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.";
+pTags[1].innerText = "About content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.";
+pTags[2].innerText = "Services content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.";
+pTags[3].innerText = "Product content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.";
+pTags[4].innerText = "Vision content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.";
+pTags[5].innerText = "123 Way 456 Street\r\n"
+pTags[5].innerText += "Somewhere, USA";
+pTags[6].innerText = "1 (888) 888-8888";
+pTags[7].innerText = "sales@greatidea.io"; 
+
+const footer = document.getElementsByTagName("footer");
+footer[0].innerText = "Copyright Great Idea! 2018"
+
