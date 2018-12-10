07ee0c766e4cf4f3523435e0bf5ddcb17e8e8bb3
JamesB <jblazer1@yahoo.com>
2018-12-10 14:06:40 -0800

    Add main-content

--- js/index.js
+++ js/index.js
@@ -59,3 +59,33 @@ button.textContent = siteContent.cta.button;
 
 const codeSnip = document.getElementById('middle-img');
 codeSnip.src = siteContent['main-content']['middle-img-src'];
+
+const features = document.querySelectorAll('h4')[0];
+features.textContent = siteContent['main-content']['features-h4'];
+
+const about = document.querySelectorAll('h4')[1];
+about.textContent = siteContent['main-content']['about-h4'];
+
+const services = document.querySelectorAll('h4')[2];
+services.textContent = siteContent['main-content']['services-h4'];
+
+const product = document.querySelectorAll('h4')[3];
+product.textContent = siteContent['main-content']['product-h4'];
+
+const vision = document.querySelectorAll('h4')[4];
+vision.textContent = siteContent['main-content']['vision-h4'];
+
+const featuresPara = document.querySelectorAll('p')[0];
+featuresPara.textContent = siteContent['main-content']['features-content'];
+
+const aboutPara = document.querySelectorAll('p')[1];
+aboutPara.textContent = siteContent['main-content']['about-content'];
+
+const servicesPara = document.querySelectorAll('p')[2];
+servicesPara.textContent = siteContent['main-content']['services-content'];
+
+const productPara = document.querySelectorAll('p')[3];
+productPara.textContent = siteContent['main-content']['product-content'];
+
+const visionPara = document.querySelectorAll('p')[4];
+visionPara.textContent = siteContent['main-content']['vision-content'];
