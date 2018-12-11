6e4b05b4c188eb443c40de7f2c4e86f1c21ca718
Preprocessing - I <karenly1961@gmail.com>
2018-12-10 19:55:30 -0500

    @ VivaCode , Need to check entrires

--- js/index.js
+++ js/index.js
@@ -38,10 +38,10 @@ const siteContent = {
 };
 
 // Example: Update the img src for the logo
-// let logo = document.getElementById("logo-img");
-// logo.setAttribute('src', siteContent["nav"]["img-src"]);
+let logo = document.getElementById("logo-img");
+logo.setAttribute('src', siteContent["nav"]["img-src"]);
 
-
+// nav
 
 const nav = document.querySelectorAll('a');
 nav[0].textContent = siteContent['nav']['nav-item-1'];
@@ -52,6 +52,8 @@ nav[4].textContent = siteContent['nav']['nav-item-5'];
 nav[5].textContent = siteContent['nav']['nav-item-6'];
 
 //------------------------------------------------------
+
+//cta
 
 const ctaText = document.querySelector('h1');
 h1.textContent = siteContent['cta']['h1'];
@@ -64,13 +66,7 @@ const ctaImage = getElementById('cta-img');
 ctaImage.setAttribute('src', siteContent['cta']['img-src']);
 //--------------------------------------------------------
 
-// "main-content": {
-
-//   "services-content": "Services content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.",
-//   "product-h4":"Product",
-//   "product-content": "Product content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.",
-//   "vision-h4":"Vision",
-//   "vision-content": "Vision 
+//main-content
 
 const mainH4 = document.querySelector('main-content');
 mainH4.textContent = siteContent['main-content']['features-h4'];
@@ -91,15 +87,41 @@ const servicesContent = document.querySelector('main-content');
 servicesContent.textContent = siteContent['main-content']['services-content'];
 
 const productH4 = document.querySelector('main-content');
-productH4.tetContent = siteContent['main-content']['product-h4'];
+productH4.textContent = siteContent['main-content']['product-h4'];
 
 const productContent = document.querySelector('main-content');
 productContent.textContent = siteContent['main-content']['product-content'];
 
 const visionH4 = document.querySelector('main-content');
+visionH4.textContent = siteContent['main-content']['vision-h4'];
 
+const visionContent = document.querySelector('main-content');
+visionContent = siteContent['main-content']['vision-content'];
+//------------------------------------------------------
 
+//contact
 
+// "contact": {
+//   "contact-h4" : "Contact",
+//   "address" : "123 Way 456 Street Somewhere, USA",
+//   "phone" : "1 (888) 888-8888",
+//   "email" : "sales@greatidea.io",
+// },
+
+const contactH4 = document.querySelector('contact');
+contactH4.textContent = siteContent['contact']['contact-h4'];
+
+const address = document.querySelector('contact');
+address.textContent = siteContent['contact']['address'];
+
+const phone = document.querySelector('contact');
+phone.textContent = siteContent['contact']['phone'];
+
+const email = document.querySelector('contact');
+email.textContent = siteContent['contact']['email'];
+//---------------------------------------------------
+
+//footer
 
 
 const footer = document.querySelector('copyright');
