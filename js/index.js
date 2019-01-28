6f4bfcf7823b16b5de185e8029954c5c81ef5cd7
RyanNFreeman <ryan.norman.freeman@gmail.com>
2019-01-28 15:05:24 -0700

    added images to DOM

--- js/index.js
+++ js/index.js
@@ -41,5 +41,22 @@ const siteContent = {
 let logo = document.getElementById("logo-img");
 logo.setAttribute('src', siteContent["nav"]["img-src"])
 
-let nav = document.getElementById("container nav");
-nav.setAttribute('src', siteContent["nav"])
+let nav = document.getElementsByTagName("a")
+
+nav[0].textContent = siteContent.nav["nav-item-1"]
+nav[1].textContent = siteContent.nav["nav-item-2"]
+nav[2].textContent = siteContent.nav["nav-item-3"]
+nav[3].textContent = siteContent.nav["nav-item-4"]
+nav[4].textContent = siteContent.nav["nav-item-5"]
+nav[5].textContent = siteContent.nav["nav-item-6"]
+
+let h1 = document.querySelector("h1").textContent = siteContent.cta.h1
+
+let mainImg = document.getElementById("cta-img");
+mainImg.setAttribute('src', siteContent["cta"]["img-src"])
+
+let mainBtn = document.getElementsByTagName("button")
+mainBtn.textContent = siteContent.cta.button
+
+const scrollImg = document.getElementById("middle-img").setAttribute('src', siteContent["main-content"]["middle-img-src"])
+
