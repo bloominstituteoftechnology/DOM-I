7be82be26276b86df2ebf5f5a836b7e7c9127883
Kristea Gamel <krgamel@gmail.com>
2019-01-30 10:42:52 -0600

    Pictures
    linked all three pictures using setAttribute.
    
    Signed-off-by: Kristea Gamel <krgamel@gmail.com>

--- js/index.js
+++ js/index.js
@@ -39,4 +39,12 @@ const siteContent = {
 
 // Example: Update the img src for the logo
 let logo = document.getElementById("logo-img");
-logo.setAttribute('src', siteContent["nav"]["img-src"])
+logo.setAttribute('src', siteContent["nav"]["img-src"]);
+
+let headerImg = document.getElementById("cta-img");
+headerImg.setAttribute('src', siteContent["cta"]["img-src"]);
+
+let middleImg = document.getElementById("middle-img");
+middleImg.setAttribute('src', siteContent ["main-content"]["middle-img-src"]);
+
+// let nav = document.getElementsByTagName('a');
