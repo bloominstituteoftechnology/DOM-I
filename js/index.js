4b3aba4ed1bcb000f5340087e9cc1c13140cec01
Snigdha Regmi <sregmi@NORD.LOCAL>
2019-01-30 21:59:54 -0700

    selector done

--- js/index.js
+++ js/index.js
@@ -9,7 +9,7 @@ const siteContent = {
     "img-src": "img/logo.png"
   },
   "cta": {
-    "h1": "DOM </br>Is </br>Awesome",
+    "h1": "DOM Is Awesome",
     "button": "Get Started",
     "img-src": "img/header-img.png"
   },
@@ -28,7 +28,7 @@ const siteContent = {
   },
   "contact": {
     "contact-h4" : "Contact",
-    "address" : "123 Way 456 Street </br> Somewhere, USA",
+    "address" : "123 Way 456 Street  Somewhere, USA",
     "phone" : "1 (888) 888-8888",
     "email" : "sales@greatidea.io",
   },
