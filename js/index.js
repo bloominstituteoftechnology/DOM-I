34c9d401e9064ab5371e8315a09b8f058f66a9e5
ken1286 <kindle1286@gmail.com>
2019-04-08 23:14:27 -0700

    Text changed to green

--- js/index.js
+++ js/index.js
@@ -37,15 +37,37 @@ const siteContent = {
   },
 };
 
+
+
 // Example: Update the img src for the logo
 let logo = document.getElementById("logo-img");
 logo.setAttribute('src', siteContent["nav"]["img-src"]);
+const navigation = document.querySelector("nav");
+
+
+const appendLink = document.createElement("a");
+const prependLink = document.createElement("a");
+
+appendLink.href = "#";
+prependLink.href = "#";
+prependLink.textContent = "FirstLink";
+appendLink.textContent = "LastLink";
+prependLink.style.color = 'green';
+appendLink.style.color = 'green';
 
 const navElements = document.querySelectorAll("a");
+navElements.forEach( element => element.style.color = 'green');
+console.log(navElements);
+navigation.prepend(prependLink);
+navigation.append(appendLink);
+
+navElements.forEach( (element, index) => element.textContent = 
+Object.values(siteContent.nav)[index]);
+
 // Iterate through siteContent.nav and add text content
-for(let i = 0; i < navElements.length; i++) {
-  navElements[i].textContent = Object.values(siteContent.nav)[i];
-}
+// for(let i = 0; i < navElements.length; i++) {
+//   navElements[i].textContent = Object.values(siteContent.nav)[i];
+// }
 // navElements[0].textContent = siteContent.nav["nav-item-1"];
 
 const h1Tag = document.getElementsByTagName("h1");
@@ -79,13 +101,18 @@ paragraphContent[8].textContent = siteContent["footer"]["copyright"];
 const centerImage = document.querySelector('#middle-img');
 centerImage.setAttribute('src', siteContent["main-content"]["middle-img-src"]);
 
-const appendLink = document.createElement("a");
-const prependLink = document.createElement("a");
 
-const navigation = document.querySelector("nav");
-appendLink.href = "#";
-prependLink.href = "#";
-prependLink.textContent = "Linkadoodledoo";
-appendLink.textContent = "LinkasaurusRex";
-navigation.prepend(prependLink);
-navigation.append(appendLink);
+
+
+// const appendLink = document.createElement("a");
+// const prependLink = document.createElement("a");
+
+// const navigation = document.querySelector("nav");
+// appendLink.href = "#";
+// prependLink.href = "#";
+// prependLink.textContent = "Linkadoodledoo";
+// appendLink.textContent = "LinkasaurusRex";
+// navigation.prepend(prependLink);
+// navigation.append(appendLink);
+
+// navigation.children.forEach( element => element.style.color = 'green');
