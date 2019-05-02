3582090dbd3897bc91ed3cef0fe3bd67546a7cdc
Jacob VanWormer <sharpiejacob@gmail.com>
2019-05-01 23:55:50 -0700

    Tried changing Nav color

--- js/index.js
+++ js/index.js
@@ -42,27 +42,32 @@ let logo = document.getElementById("logo-img");
 logo.setAttribute('src', siteContent["nav"]["img-src"])
 
 
-const cta = document.getElementById('cta-img');
+let cta = document.getElementById('cta-img');
 cta.setAttribute('src', siteContent["cta"]["img-src"])
 
-const middleimg = document.getElementById('middle-img');
+let middleimg = document.getElementById('middle-img');
 middleimg.setAttribute('src', siteContent["main-content"]["middle-img-src"])
 
 // NAV
-const services = document.getElementsByTagName('a')[0];
+let services = document.getElementsByTagName('a')[0];
 services.innerHTML = 'Services';
 
-const product = document.getElementsByTagName('a')[1];
+
+let product = document.getElementsByTagName('a')[1];
 product.innerHTML = 'Product';
 
-const vision = document.getElementsByTagName('a')[2];
+let vision = document.getElementsByTagName('a')[2];
 vision.innerHTML = 'Vision';
 
-const about = document.getElementsByTagName('a')[3];
+let about = document.getElementsByTagName('a')[3];
 about.innerHTML = 'About';
 
-const contact = document.getElementsByTagName('a')[4];
+let contact = document.getElementsByTagName('a')[4];
 contact.innerHTML = 'Contact';
+
+
+let nav_color = document.getElementsByClassName('header-na a')
+nav_color.style.color = "green";
 
 // HEADER
 
@@ -72,11 +77,6 @@ h1.innerText = siteContent['cta']['h1'];
 let button = document.getElementsByTagName("button")[0];
 button.innerText = siteContent['cta']['button'];
 
-let headerImg = document.getElementById("cta-img");
-headerImg.setAttribute("src", siteContent["cta"]["img-src"]);
-
-let middleImg = document.getElementById("middle-img");
-middleImg.setAttribute("src", "img/mid-page-accent.jpg")
 
 // main-content
 
@@ -92,28 +92,28 @@ aboutH4.innerText = siteContent['main-content']['about-h4'];
 let aboutContent1 = document.getElementsByTagName('p')[1];
 aboutContent1.innerText = siteContent['main-content']['about-content'];
 
-let services_H4 = document.getElementsByTagName('h4')[2];
-services_H4.innerText = siteContent['main-content']['services-h4'];
+let servicesH4 = document.getElementsByTagName('h4')[2];
+servicesH4.innerText = siteContent['main-content']['services-h4'];
 
 let servicesContent = document.getElementsByTagName('p')[2];
 servicesContent.innerText = siteContent['main-content']['services-content'];
 
-let product_H4 = document.getElementsByTagName('h4')[3];
-product_H4.innerText = siteContent['main-content']['product-h4'];
+let productH4 = document.getElementsByTagName('h4')[3];
+productH4.innerText = siteContent['main-content']['product-h4'];
 
 let productContent = document.getElementsByTagName('p')[3];
 productContent.innerText = siteContent['main-content']['product-content'];
 
-let vision_H4 = document.getElementsByTagName('h4')[4];
-vision_H4.innerText = siteContent['main-content']['vision-h4'];
+let visionH4 = document.getElementsByTagName('h4')[4];
+visionH4.innerText = siteContent['main-content']['vision-h4'];
 
 let visionContent = document.getElementsByTagName('p')[4];
 visionContent.innerText = siteContent['main-content']['vision-content'];
 
 // Contact
 
-let contact_H4 = document.getElementsByTagName('h4')[5];
-contact_H4.innerText = siteContent['contact']['contact-h4'];
+let contactH4 = document.getElementsByTagName('h4')[5];
+contactH4.innerText = siteContent['contact']['contact-h4'];
 
 let address = document.getElementsByTagName('p')[5];
 address.innerText = siteContent['contact']['address'];
