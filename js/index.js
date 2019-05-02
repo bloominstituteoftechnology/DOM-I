356ef9cc3ae937f0a0e6e0073602527bc1ee2722
Jacob VanWormer <sharpiejacob@gmail.com>
2019-05-01 23:26:50 -0700

    added all selectors

--- js/index.js
+++ js/index.js
@@ -48,9 +48,84 @@ cta.setAttribute('src', siteContent["cta"]["img-src"])
 const middleimg = document.getElementById('middle-img');
 middleimg.setAttribute('src', siteContent["main-content"]["middle-img-src"])
 
+// NAV
 const services = document.getElementsByTagName('a')[0];
 services.innerHTML = 'Services';
 
+const product = document.getElementsByTagName('a')[1];
+product.innerHTML = 'Product';
 
+const vision = document.getElementsByTagName('a')[2];
+vision.innerHTML = 'Vision';
+
+const about = document.getElementsByTagName('a')[3];
+about.innerHTML = 'About';
+
+const contact = document.getElementsByTagName('a')[4];
+contact.innerHTML = 'Contact';
+
+// HEADER
+
+let h1 = document.getElementsByTagName('h1')[0];
+h1.innerText = siteContent['cta']['h1'];
+
+let button = document.getElementsByTagName("button")[0];
+button.innerText = siteContent['cta']['button'];
+
+let headerImg = document.getElementById("cta-img");
+headerImg.setAttribute("src", siteContent["cta"]["img-src"]);
+
+let middleImg = document.getElementById("middle-img");
+middleImg.setAttribute("src", "img/mid-page-accent.jpg")
+
+// main-content
+
+let featuresH4 = document.getElementsByTagName('h4')[0];
+featuresH4.innerText = siteContent['main-content']['features-h4'];
+
+let featuresContent = document.getElementsByTagName('p')[0];
+featuresContent.innerText = siteContent['main-content']['features-content'];
+
+let aboutH4 = document.getElementsByTagName('h4')[1];
+aboutH4.innerText = siteContent['main-content']['about-h4'];
+
+let aboutContent1 = document.getElementsByTagName('p')[1];
+aboutContent1.innerText = siteContent['main-content']['about-content'];
+
+let services_H4 = document.getElementsByTagName('h4')[2];
+services_H4.innerText = siteContent['main-content']['services-h4'];
+
+let servicesContent = document.getElementsByTagName('p')[2];
+servicesContent.innerText = siteContent['main-content']['services-content'];
+
+let product_H4 = document.getElementsByTagName('h4')[3];
+product_H4.innerText = siteContent['main-content']['product-h4'];
+
+let productContent = document.getElementsByTagName('p')[3];
+productContent.innerText = siteContent['main-content']['product-content'];
+
+let vision_H4 = document.getElementsByTagName('h4')[4];
+vision_H4.innerText = siteContent['main-content']['vision-h4'];
+
+let visionContent = document.getElementsByTagName('p')[4];
+visionContent.innerText = siteContent['main-content']['vision-content'];
+
+// Contact
+
+let contact_H4 = document.getElementsByTagName('h4')[5];
+contact_H4.innerText = siteContent['contact']['contact-h4'];
+
+let address = document.getElementsByTagName('p')[5];
+address.innerText = siteContent['contact']['address'];
+
+let phone = document.getElementsByTagName('p')[6];
+phone.innerText = siteContent['contact']['phone'];
+
+let email = document.getElementsByTagName('p')[7];
+email.innerText = siteContent['contact']['email'];
+
+//footer
+let footer = document.getElementsByTagName('p')[8];
+footer.innerText = siteContent['footer']['copyright'];
 
 
