dd2937d152f09b146abd9942debb710235fd79a5
Zaur <42130952+ZaurInvictus@users.noreply.github.com>
2019-05-01 22:13:45 -0500

    cta upd

--- js/index.js
+++ js/index.js
@@ -14,27 +14,27 @@ const siteContent = {
     "img-src": "img/header-img.png"
   },
   "main-content": {
-    "features-h4":"Features",
+    "features-h4": "Features",
     "features-content": "Features content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.",
-    "about-h4":"About",
+    "about-h4": "About",
     "about-content": "About content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.",
     "middle-img-src": "img/mid-page-accent.jpg",
 
-    "services-h4":"Services",
+    "services-h4": "Services",
     "services-content": "Services content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.",
-    "product-h4":"Product",
+    "product-h4": "Product",
     "product-content": "Product content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.",
-    "vision-h4":"Vision",
+    "vision-h4": "Vision",
     "vision-content": "Vision content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.",
   },
   "contact": {
-    "contact-h4" : "Contact",
-    "address" : "123 Way 456 Street Somewhere, USA",
-    "phone" : "1 (888) 888-8888",
-    "email" : "sales@greatidea.io",
+    "contact-h4": "Contact",
+    "address": "123 Way 456 Street Somewhere, USA",
+    "phone": "1 (888) 888-8888",
+    "email": "sales@greatidea.io",
   },
   "footer": {
-    "copyright" : "Copyright Great Idea! 2018"
+    "copyright": "Copyright Great Idea! 2018"
   },
 };
 
@@ -56,7 +56,7 @@ const anchorTags = document.querySelectorAll('a');
 // anchorTags[5].textContent = siteContent['nav']['nav-item-6'];
 
 const titles = Object.values(siteContent.nav);
-console.log(titles);
+
 anchorTags.forEach((e, i) => {
   e.textContent = titles[i];
   e.style.color = 'green'; //NAV COLORS GREEN
@@ -67,17 +67,20 @@ anchorTags.forEach((e, i) => {
 //   e.style.color = 'green';
 // })
 
-
 //CTA
 const ctaHeader = document.querySelector('.cta h1');
 const ctaButton = document.querySelector('.cta button');
 const ctaImg = document.querySelector('#cta-img');
 
-ctaHeader.textContent = siteContent.cta.h1;
 ctaButton.textContent = siteContent.cta.button;
-ctaImg.setAttribute( 'src', siteContent['cta']['img-src']);
+ctaImg.setAttribute('src', siteContent['cta']['img-src']);
 
+let h1Arr = siteContent['cta']['h1'].split(' ');
+h1Arr.splice(1, 0, `<br>`);
+h1Arr.splice(3, 0, `<br>`);
+let newH1Str = h1Arr.join('');
 
+ctaHeader.innerHTML = newH1Str;
 
 //////////////////////////
 //FUNCTIONALITY
@@ -112,7 +115,7 @@ ctaButton.addEventListener('click', () => {
 // });
 
 logo.addEventListener('click', () => {
-    console.log(nav.innerHTML = '');
+  console.log(nav.innerHTML = '');
 });
 
 //TOGGLE CLASS
@@ -165,6 +168,3 @@ contactP[2].textContent = siteContent.contact.email;
 //FOOTER
 const footer = document.querySelector('footer p');
 footer.textContent = siteContent.footer.copyright;
-
-
-
