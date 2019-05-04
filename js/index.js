1c5ebfb736db8e0f59952edcc2db5261070deda1
Ronald Cho <ronaldcho@gmail.com>
2019-05-03 17:01:37 -0700

    dom js

--- js/index.js
+++ js/index.js
@@ -40,3 +40,122 @@ const siteContent = {
 // Example: Update the img src for the logo
 let logo = document.getElementById("logo-img");
 logo.setAttribute('src', siteContent["nav"]["img-src"])
+
+
+// nav items
+const nav = document.querySelector("nav");
+const navItems = document.querySelectorAll("nav a");
+const navValues = Object.values(siteContent["nav"]);
+
+for (let i = 0; i < 6; i++) {
+  navItems[i].textContent = navValues[i];
+}
+
+// Change nav color to green
+navItems.forEach(item => (item.style.color = "seagreen"));
+
+// Creating Two more nav items
+const newNav1 = document.createElement("a");
+newNav1.textContent = "Home";
+newNav1.style.color = "seagreen";
+newNav1.style.cursor = 'pointer';
+nav.prepend(newNav1);
+
+const newNav2 = document.createElement("a");
+newNav2.textContent = "Random";
+newNav2.style.color = "seagreen";
+newNav2.style.cursor = 'pointer';
+nav.appendChild(newNav2);
+
+// CTA items
+const cta = document.querySelector(".cta");
+const ctaLogo = document.getElementById("cta-img");
+const ctaH1 = document.querySelector(".cta-text h1");
+const ctaButton = document.querySelector(".cta-text button");
+
+let h1Arr = siteContent['cta']['h1'].split(' ');
+h1Arr.splice(1, 0, `<br>`);
+h1Arr.splice(3, 0, `<br>`);
+let newH1 = h1Arr.join('');
+
+ctaH1.innerHTML = newH1;
+ctaButton.textContent = siteContent["cta"]["button"];
+ctaLogo.setAttribute("src", siteContent["cta"]["img-src"]);
+
+// top content
+const topContentHeadings = document.querySelectorAll(
+  ".top-content .text-content h4"
+);
+const topContentParas = document.querySelectorAll(
+  ".top-content .text-content p"
+);
+
+topContentHeadings[0].textContent = siteContent["main-content"]["features-h4"];
+topContentParas[0].textContent = siteContent["main-content"]["features-content"];
+topContentHeadings[1].textContent = siteContent["main-content"]["about-h4"];
+topContentParas[1].textContent = siteContent["main-content"]["about-content"];
+
+// Center img
+const centerImg = document.getElementById("middle-img");
+centerImg.setAttribute("src", siteContent["main-content"]["middle-img-src"]);
+
+// Bottom Content
+const botContentHeadings = document.querySelectorAll(
+  ".bottom-content .text-content h4"
+);
+const botContentParas = document.querySelectorAll(
+  ".bottom-content .text-content p"
+);
+
+botContentHeadings[0].textContent = siteContent["main-content"]["services-h4"];
+botContentParas[0].textContent = siteContent["main-content"]["services-content"];
+botContentHeadings[1].textContent = siteContent["main-content"]["product-h4"];
+botContentParas[1].textContent = siteContent["main-content"]["product-content"];
+botContentHeadings[2].textContent = siteContent["main-content"]["vision-h4"];
+botContentParas[2].textContent = siteContent["main-content"]["vision-content"];
+
+// Contact
+const contactHeader = document.querySelector(".contact h4");
+const contactParas = document.querySelectorAll(".contact p");
+
+let contactArr = siteContent['contact']['address'].split(' ');
+contactArr.splice(4, 0, `<br>`);
+let newAddress = contactArr.join('');
+
+contactHeader.textContent = siteContent["contact"]["contact-h4"];
+
+contactParas[0].innerHTML = newAddress;
+contactParas[1].textContent = siteContent['contact']['phone'];
+contactParas[2].textContent = siteContent['contact']['email'];
+
+// Footer
+const footerP = document.querySelector("footer p");
+
+footerP.textContent = siteContent["footer"]["copyright"];
+
+// STRETCH
+// Adding Styles through JS to Make nav fixed
+const header = document.querySelector("header");
+header.style.position = "fixed";
+header.style.top = "0";
+header.style.zIndex = "1";
+header.style.backgroundColor = "#fff";
+
+cta.style.marginTop = "100px";
+
+// Button event listener
+ctaButton.addEventListener("click", e => {
+  // if (ctaH1.textContent === siteContent['cta']['h1']) {
+  //   ctaH1.textContent = `Don't Click Me!`;
+  // } else {
+  //   ctaH1.textContent = siteContent['cta']['h1'];
+  // }
+
+  if (cta.style.flexDirection === "row" && ctaH1.innerHTML === newH1) {
+    cta.style.flexDirection = "row-reverse";
+    ctaH1.innerHTML = `Don't <br> Click <br> Me`;
+  } else {
+    cta.style.flexDirection = "row";
+    ctaH1.innerHTML = newH1;
+  }
+});
