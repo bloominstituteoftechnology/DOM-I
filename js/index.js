cdabc08eda4f051e071a90c4c5478c1c5e87751f
Darren Richardson <antoniounivers@gmail.com>
2019-06-17 15:57:12 -0400

    mvp met

--- js/index.js
+++ js/index.js
@@ -40,3 +40,94 @@ const siteContent = {
 // Example: Update the img src for the logo
 let logo = document.getElementById("logo-img");
 logo.setAttribute('src', siteContent["nav"]["img-src"])
+
+let anchor = siteContent["nav"]
+let navbar = document.querySelectorAll('a')
+
+
+
+
+  navbar.forEach(function(item, i){
+    item.textContent = anchor[`nav-item-${i+1}`]
+   
+  });
+
+  
+//  ======================= Header ==================
+
+  let ctaText = document.getElementsByClassName("cta-text")[0];
+  ctaText.getElementsByTagName("h1")[0].innerHTML = siteContent["cta"]["h1"];
+  ctaText.getElementsByTagName("button")[0].innerHTML = siteContent['cta']['button']
+
+  // cta imag
+   let ctaimg = document.getElementById("cta-img")
+   ctaimg.setAttribute('src',siteContent['cta']['img-src'])
+
+  //  ================= Main Content ==================
+  let firstContent = document.getElementsByClassName('text-content')[0]
+  firstContent.getElementsByTagName('h4')[0].innerHTML = siteContent['main-content']['features-h4']
+  firstContent.getElementsByTagName('p')[0].innerHTML = siteContent['main-content']["features-content"]
+
+  let secondContent = document.getElementsByClassName('text-content')[1]
+ 
+  secondContent.getElementsByTagName('h4')[0].innerHTML = siteContent['main-content']['about-h4']
+  secondContent.getElementsByTagName('p')[0].innerHTML = siteContent['main-content']["features-content"]
+
+  let nextLogo = document.getElementById('middle-img')
+  nextLogo.setAttribute('src',siteContent['main-content']['middle-img-src'])
+
+  let thirdContent = document.getElementsByClassName('text-content')[2]
+  
+  thirdContent.getElementsByTagName('h4')[0].innerHTML = siteContent['main-content']['services-h4']
+  thirdContent.getElementsByTagName('p')[0].innerHTML = siteContent['main-content']["services-content"]
+
+
+  let forthContent = document.getElementsByClassName('text-content')[3]
+  
+  forthContent.getElementsByTagName('h4')[0].innerHTML = siteContent['main-content']['product-h4']
+  forthContent.getElementsByTagName('p')[0].innerHTML = siteContent['main-content']["product-content"]
+
+  let fifthContent = document.getElementsByClassName('text-content')[4]
+  
+  fifthContent.getElementsByTagName('h4')[0].innerHTML = siteContent['main-content']['vision-h4']
+  fifthContent.getElementsByTagName('p')[0].innerHTML = siteContent['main-content']["vision-content"]
+// ======================= Contact Info ========================
+  const contactH4 = document.querySelectorAll("h4")[5];
+  contactH4.innerText = siteContent["contact"]["contact-h4"];
+  // Address
+  const address = document.querySelectorAll("p")[5];
+  address.innerText = siteContent["contact"]["address"];
+  // Phone
+  const phone = document.querySelectorAll("p")[6];
+  phone.innerText = siteContent["contact"]["phone"];
+  // E-mail
+  const email = document.querySelectorAll("p")[7];
+  email.innerText = siteContent["contact"]["email"];
+// =========================== Footer ==========================
+const copyright = document.getElementsByTagName("p")[8];
+copyright.innerText = siteContent['footer']['copyright']
+
+// ======================= adding elements to nav & changing color ==================
+const navigation = document.querySelector('nav')
+const newanchor = document.createElement('a')
+newanchor.innerText= "Hello"
+
+const newanchor1 = document.createElement('a')
+newanchor1.innerText= "GoodBye"
+
+console.log(navigation.prepend(newanchor))
+
+console.log(navigation.appendChild(newanchor1))
+navbar.forEach(function (item){
+  item.style.color = "green"
+  
+ 
+})
+
+
+
+// newAnchor.innerText = "Hello"
+
+ 
+// const newAnchor2 = document.createElement('a')
+
