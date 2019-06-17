72ec9099f30902d77abfacdd540c31d154954b47
Faye Skeen <fskeen@gmail.com>
2019-06-17 16:23:00 -0700

    Hit MVP.

--- js/index.js
+++ js/index.js
@@ -37,6 +37,117 @@ const siteContent = {
   },
 };
 
+
+
 // Example: Update the img src for the logo
 let logo = document.getElementById("logo-img");
 logo.setAttribute('src', siteContent["nav"]["img-src"])
+
+// Nav items
+let navitems = document.querySelectorAll('nav > a');
+navitems[0].textContent = siteContent["nav"]["nav-item-1"];
+navitems[1].textContent = siteContent["nav"]["nav-item-2"];
+navitems[2].textContent = siteContent["nav"]["nav-item-3"];
+navitems[3].textContent = siteContent["nav"]["nav-item-4"];
+navitems[4].textContent = siteContent["nav"]["nav-item-5"];
+navitems[5].textContent = siteContent["nav"]["nav-item-6"];
+
+
+// appending and prepending links 
+
+let nav = document.querySelector("nav");
+
+let appendLink = document.createElement('a');
+appendLink.textContent = "Appended Item";
+appendLink.style.color = "green"
+nav.appendChild(appendLink);
+
+let prependLink = document.createElement('a');
+prependLink.textContent = "Prepended Item";
+prependLink.style.color = "green"
+nav.prepend(prependLink);
+
+navitems.forEach(function (a) {
+  a.style.color = "green";
+})
+
+// CTA section
+
+let ctaImage = document.getElementById("cta-img");
+ctaImage.setAttribute('src', siteContent["cta"]["img-src"]);
+
+let ctaH1 = document.querySelector(".cta h1");
+ctaH1.textContent = siteContent["cta"]["h1"];
+
+let ctaBtn = document.querySelector(".cta button");
+ctaBtn.textContent = siteContent["cta"]["button"];
+
+// an extra button trying to enable/disable an animation. It didn't work!
+
+// let nopeButton = document.createElement("button")
+// nopeButton.textContent = "Never mind";
+// nopeButton.style.color = "grey"
+
+// let cta = document.querySelector(".cta");
+// cta.append(nopeButton);
+
+
+// middle image
+
+let middleImage = document.getElementById("middle-img");
+middleImage.setAttribute('src', siteContent["main-content"]["middle-img-src"]);
+
+
+// text content experiment
+
+let textContentP = document.querySelectorAll(".text-content > p");
+textContentP[0].textContent = siteContent["main-content"]["features-content"];
+textContentP[1].textContent = siteContent["main-content"]["about-content"];
+textContentP[2].textContent = siteContent["main-content"]["services-content"];
+textContentP[3].textContent = siteContent["main-content"]["product-content"];
+textContentP[4].textContent = siteContent["main-content"]["vision-content"];
+
+let textContentH4 = document.querySelectorAll(".text-content > h4");
+textContentH4[0].textContent = siteContent["main-content"]["features-h4"];
+textContentH4[1].textContent = siteContent["main-content"]["about-h4"];
+textContentH4[2].textContent = siteContent["main-content"]["services-h4"];
+textContentH4[3].textContent = siteContent["main-content"]["product-h4"];
+textContentH4[4].textContent = siteContent["main-content"]["vision-h4"];
+
+// contact & footer
+
+let contactH4 = document.querySelector(".contact > h4");
+contactH4.textContent = siteContent["contact"]["contact-h4"];
+
+let contactP = document.querySelectorAll(".contact > p");
+contactP[0].textContent = siteContent["contact"]["address"];
+contactP[1].textContent = siteContent["contact"]["phone"];
+contactP[2].textContent = siteContent["contact"]["email"];
+
+// footer
+
+let footer = document.querySelectorAll("footer > p");
+footer.textContent = siteContent["footer"]["copyright"];
+
+// Trying to get the coding image rotating by pressing the "Get Started" button. It's not working, but I don't know enough about JS to know why. I found this structure in a Stack Overflow post.
+
+// let timer;
+// let turn = 0;
+
+// function turnOn() {
+//   timer = setInterval(turnImg, 200);
+//   let x = document.querySelector(".cta button");
+//   x.disabled = true;
+// }
+
+// function turnOff() {
+//   clearInterval(timer);
+//   let x = document.querySelector(".cta button");
+//   x.disabled = false;
+// }
+
+// function turnImg() {
+//   let x = document.getElementById("cta-img");
+//   turn += 60;
+//   x.style.transform = "rotate("+ (turn % 360) +"deg)"
+// }
