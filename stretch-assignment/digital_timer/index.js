395f01d51c86bd1e282bc5d8745fa5ceae3c85ac
Shelby Diamond <Shelbydiamond518@gmail.com>
2019-07-08 16:08:01 -0700

    updated to get counter on site

--- stretch-assignment/digital_timer/index.js
+++ stretch-assignment/digital_timer/index.js
@@ -1,9 +1,22 @@
+const secondTens = document.getElementById("secondTens");
+const secondOnes = document.getElementById("secondOnes");
+const msHundreds = document.getElementById("msHundreds");
+const msTens = document.getElementById("msTens");
+
 let timer = window.setInterval(callback, 1000);
+let timers = window.setInterval(callbacks, 10);
 let counter = 0;
+let counters = 0;
 
 function callback() {
   if (counter < 10) counter++;
+  document.getElementById("secondOnes").innerHTML = counter;
   console.log(counter);
 }
 
-callback();
+function callbacks() {
+  counters++;
+  document.getElementById("msHundreds").innerHTML = counters;
+  document.getElementById("msTens").innerHTML = counters;
+  console.log(counters);
+}
