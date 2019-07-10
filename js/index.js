1b866bebd013d9bde9ce472640b5e5683be0eb66
Jade E Lopez <38263897+jadeli1720@users.noreply.github.com>
2019-07-09 22:53:54 -0600

    Started stretch goal: timer

--- js/index.js
+++ js/index.js
@@ -1,5 +1,5 @@
 const siteContent = {
-  "nav": {
+  nav: {
     "nav-item-1": "Services",
     "nav-item-2": "Product",
     "nav-item-3": "Vision",
@@ -8,120 +8,121 @@ const siteContent = {
     "nav-item-6": "Contact",
     "img-src": "img/logo.png"
   },
-  "cta": {
-    "h1": "DOM Is Awesome",
-    "button": "Get Started",
+  cta: {
+    h1: "DOM Is Awesome",
+    button: "Get Started",
     "img-src": "img/header-img.png"
   },
   "main-content": {
-    "features-h4":"Features",
-    "features-content": "Features content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.",
-    "about-h4":"About",
-    "about-content": "About content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.",
+    "features-h4": "Features",
+    "features-content":
+      "Features content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.",
+    "about-h4": "About",
+    "about-content":
+      "About content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.",
     "middle-img-src": "img/mid-page-accent.jpg",
-    "services-h4":"Services",
-    "services-content": "Services content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.",
-    "product-h4":"Product",
-    "product-content": "Product content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.",
-    "vision-h4":"Vision",
-    "vision-content": "Vision content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.",
+    "services-h4": "Services",
+    "services-content":
+      "Services content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.",
+    "product-h4": "Product",
+    "product-content":
+      "Product content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.",
+    "vision-h4": "Vision",
+    "vision-content":
+      "Vision content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis."
   },
-  "contact": {
-    "contact-h4" : "Contact",
-    "address" : "123 Way 456 Street Somewhere, USA",
-    "phone" : "1 (888) 888-8888",
-    "email" : "sales@greatidea.io",
+  contact: {
+    "contact-h4": "Contact",
+    address: "123 Way 456 Street Somewhere, USA",
+    phone: "1 (888) 888-8888",
+    email: "sales@greatidea.io"
   },
-  "footer": {
-    "copyright" : "Copyright Great Idea! 2018"
-  },
+  footer: {
+    copyright: "Copyright Great Idea! 2018"
+  }
 };
 
 // Example: Update the img src for the logo
 
 // Navigation
-const header = document.querySelector('header')
-header.style.position = 'fixed'
-header.style.top = '0'
-header.style.background = '#fff'
-header.style.paddingBottom = '20px'
+const header = document.querySelector("header");
+header.style.position = "fixed";
+header.style.top = "0";
+header.style.background = "#fff";
+header.style.paddingBottom = "20px";
 
 let logo = document.getElementById("logo-img");
-logo.setAttribute('src', siteContent["nav"]["img-src"])
+logo.setAttribute("src", siteContent["nav"]["img-src"]);
 
-const navLinks = document.querySelectorAll("nav a")
-navLinks[0].textContent = siteContent['nav']['nav-item-1'];
-navLinks[1].textContent = siteContent['nav']['nav-item-2'];
-navLinks[2].textContent = siteContent['nav']['nav-item-3'];
-navLinks[3].textContent = siteContent['nav']['nav-item-4'];
-navLinks[4].textContent = siteContent['nav']['nav-item-5'];
-navLinks[5].textContent = siteContent['nav']['nav-item-6'];
+const navLinks = document.querySelectorAll("nav a");
+navLinks[0].textContent = siteContent["nav"]["nav-item-1"];
+navLinks[1].textContent = siteContent["nav"]["nav-item-2"];
+navLinks[2].textContent = siteContent["nav"]["nav-item-3"];
+navLinks[3].textContent = siteContent["nav"]["nav-item-4"];
+navLinks[4].textContent = siteContent["nav"]["nav-item-5"];
+navLinks[5].textContent = siteContent["nav"]["nav-item-6"];
 
-navLinks.forEach(color => color.style.color = 'green')
+navLinks.forEach(color => (color.style.color = "green"));
 
-const allNavs = document.querySelector('nav')
+const allNavs = document.querySelector("nav");
 
-const blog = document.createElement('a')
-blog.textContent = "Blog"
+const blog = document.createElement("a");
+blog.textContent = "Blog";
 
-const home = document.createElement('a')
-home.textContent = "Home"
+const home = document.createElement("a");
+home.textContent = "Home";
 
-allNavs.appendChild(blog)
-allNavs.prepend(home)
+allNavs.appendChild(blog);
+allNavs.prepend(home);
 
-blog.style.color = 'green'
-blog.style.cursor = 'pointer'
-home.style.color = 'green'
-home.style.cursor = 'pointer'
-
+blog.style.color = "green";
+blog.style.cursor = "pointer";
+home.style.color = "green";
+home.style.cursor = "pointer";
 
 // CTA
-let cta = document.querySelector('.cta')
-cta.style.marginTop = '112px'
+let cta = document.querySelector(".cta");
+cta.style.marginTop = "112px";
 
 let ctaImage = document.getElementById("cta-img");
-ctaImage.setAttribute('src', siteContent["cta"]["img-src"])
+ctaImage.setAttribute("src", siteContent["cta"]["img-src"]);
 
-let ctaTitle = document.querySelector('.cta-text > h1')
-ctaTitle.textContent = siteContent['cta']['h1']
-ctaTitle.innerHTML = 'DOM <br> IS <br> AWESOME'
+let ctaTitle = document.querySelector(".cta-text > h1");
+ctaTitle.textContent = siteContent["cta"]["h1"];
+ctaTitle.innerHTML = "DOM <br> IS <br> AWESOME";
 
-let ctaButton = document.querySelector('.cta-text > button')
-ctaButton.textContent = siteContent['cta']['button']
-
+let ctaButton = document.querySelector(".cta-text > button");
+ctaButton.textContent = siteContent["cta"]["button"];
 
 // Middle Content
-let mainTitle = document.querySelectorAll('.text-content h4')
-mainTitle[0].textContent = siteContent['main-content']['features-h4']
-mainTitle[1].textContent = siteContent['main-content']['about-h4']
-mainTitle[2].textContent = siteContent['main-content']['services-h4']
-mainTitle[3].textContent = siteContent['main-content']['product-h4']
-mainTitle[4].textContent = siteContent['main-content']['vision-h4']
+let mainTitle = document.querySelectorAll(".text-content h4");
+mainTitle[0].textContent = siteContent["main-content"]["features-h4"];
+mainTitle[1].textContent = siteContent["main-content"]["about-h4"];
+mainTitle[2].textContent = siteContent["main-content"]["services-h4"];
+mainTitle[3].textContent = siteContent["main-content"]["product-h4"];
+mainTitle[4].textContent = siteContent["main-content"]["vision-h4"];
 
-let para = document.querySelectorAll('.text-content  p')
-para[0].textContent = siteContent['main-content']['features-content']
-para[1].textContent = siteContent['main-content']['about-content']
-para[2].textContent = siteContent['main-content']['services-content']
-para[3].textContent = siteContent['main-content']['product-content']
-para[4].textContent = siteContent['main-content']['vision-content']
-
+let para = document.querySelectorAll(".text-content  p");
+para[0].textContent = siteContent["main-content"]["features-content"];
+para[1].textContent = siteContent["main-content"]["about-content"];
+para[2].textContent = siteContent["main-content"]["services-content"];
+para[3].textContent = siteContent["main-content"]["product-content"];
+para[4].textContent = siteContent["main-content"]["vision-content"];
 
 let midImage = document.getElementById("middle-img");
-midImage.setAttribute('src', siteContent["main-content"]['middle-img-src'])
-
+midImage.setAttribute("src", siteContent["main-content"]["middle-img-src"]);
 
 // Contact
-let contactTitle = document.querySelector('.contact h4')
-contactTitle.textContent = siteContent['contact']['contact-h4']
+let contactTitle = document.querySelector(".contact h4");
+contactTitle.textContent = siteContent["contact"]["contact-h4"];
 
-let contactPara = document.querySelectorAll('.contact p')
-contactPara[0].textContent = siteContent['contact']['address']
-contactPara[1].textContent = siteContent['contact']['phone']
-contactPara[2].textContent = siteContent['contact']['email']
+let contactPara = document.querySelectorAll(".contact p");
+contactPara[0].textContent = siteContent["contact"]["address"];
+contactPara[1].textContent = siteContent["contact"]["phone"];
+contactPara[2].textContent = siteContent["contact"]["email"];
 
-contactPara[0].innerHTML = '123 Way 456 Street <br> Somewhere, USA'
+contactPara[0].innerHTML = "123 Way 456 Street <br> Somewhere, USA";
 
-// footer 
-let footer = document.querySelector('footer')
-footer.textContent = siteContent['footer']['copyright']
+// footer
+let footer = document.querySelector("footer");
+footer.textContent = siteContent["footer"]["copyright"];
--- stretch-assignment/digital_timer/index.js
+++ stretch-assignment/digital_timer/index.js
@@ -2,23 +2,61 @@
 
 //Use .setInterval() => runs callback after a certain (or for a fixed)amount of time, and then keeps running it after that certain amount of time.
 
-
 /*
 //Steps: 
-1.) Give .setInterval a callback that will do something every     10ms
+1.) Give .setInterval a callback that will do something every 10ms
+
 2.) Set timer value to 0
+
 3.) Increment the timer by 10ms
-4.) Tie the incrementing behavior to start button > add onclick     event listener
+
+4.) Tie the incrementing behavior to start button > add onclick event listener
     a) grab the button and save to a variable called button
-    b)
+        const button = document.querySelector('button');
+    b) add event listener looks like this:
+        button.addEventListener('click', event => {
+            event.preventDefault();
+        })
+    c) .setInterval() inside click event
+
+5.) Now we need to stop the timer using a feature of .setInterval()
+    a) Returns a value that we can later reference: a clear interval that we     can use to stop it from running 
+        const = interval
+    b) Use if statement to check the value of our timer
+    c) this is the implicit use of closures
+
+6) Now we must get the numbers to display in the HTML file properly by:
+    a) grabbing all the values
+    b) setting them to proper values > use math method
+        1) grab 10's place: 
+            * timer/ 10 : shifts the decimal over to the left by 1 i.e(12340 becomes 1234.0)
+            * then get the  % 10 (modulo: performs a division by 10 & gives the remainder...the value in the ones place)
+        2) set msTens textContent to the above
+        3) Use Math.floor() on msHundreds so that the value after the decimal from the hundreds of the hundreds place is the value of the 10s place rounds it down.
+        4) repeat process for  secondOnes and secondTens
+7) Need timer to restart when the start button is clicked
 
 */
 
-const button = document.querySelector('button');
+const button = document.querySelector('button'),
+    secondTens = document.querySelector('#secondTens'),
+    secondOnes = document.querySelector('#secondOnes'),
+    msHundreds = document.querySelector('#msHundreds'),
+    msTens = document.querySelector('#msTens')
 
 let timer = 0;
-setInterval(() => {
-    timer += 10;
-    console.log(timer)//so we can see the timer incrementing this will keep running though in console.
-}, 10);
 
+button.addEventListener("click", event => {
+    event.preventDefault();
+    const interval = setInterval(() => {
+        timer += 10;
+        // console.log(timer); //so we can see the timer incrementing this will keep running though in console.
+        msTens.textContent = (timer / 10) % 10;
+        msHundreds.textContent = Math.floor(timer / 100) % 10;
+        secondOnes.textContent = Math.floor(timer / 1000) % 10;
+        secondTens.textContent = Math.floor(timer / 10000) % 10;
+        if (timer >= 3000) {
+            clearInterval(interval);
+        }
+    }, 10);
+});
