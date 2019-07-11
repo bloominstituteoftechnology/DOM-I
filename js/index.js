392127f1f22574ff259bd91a0fb2fb4b61bed826
Th5tch3r <amber.phan27@gmail.com>
2019-07-10 22:11:29 -0400

    finish mvp

--- js/index.js
+++ js/index.js
@@ -9,7 +9,7 @@ const siteContent = {
     "img-src": "img/logo.png"
   },
   "cta": {
-    "h1": "DOM Is Awesome",
+    "h1": "DOM <br> Is <br> Awesome",
     "button": "Get Started",
     "img-src": "img/header-img.png"
   },
@@ -39,17 +39,23 @@ const siteContent = {
 
 // Example: Update the img src for the logo
 //img src update
+const logo = document.querySelector('#logo-img');
+logo.src = siteContent["nav"]["img-src"];
 
-// let logo = document.getElementById("logo-img");
-// logo.src = siteContent["nav"]["img-src"];
+const ctaImg = document.querySelector('#cta-img');
+ctaImg.src = siteContent["cta"]["img-src"];
 
-// const ctaImg = document.getElementById("cta-img");
-// ctaImg.src =  "img/header-img.png";
-
-// const midImg = document.getElementById("middle-img");
-// midImg.src = "img/mid-page-accent.jpg"
+const middleImg = document.querySelector('#middle-img');
+middleImg.src = siteContent["main-content"]["middle-img-src"];
 
 //nav 
+const nav = document.querySelectorAll('header a');
+nav[0].innerHTML = siteContent["nav"]["nav-item-1"];
+nav[1].innerHTML = siteContent["nav"]["nav-item-2"];
+nav[2].innerHTML = siteContent["nav"]["nav-item-3"];
+nav[3].innerHTML = siteContent["nav"]["nav-item-4"];
+nav[4].innerHTML = siteContent["nav"]["nav-item-5"];
+nav[5].innerHTML = siteContent["nav"]["nav-item-6"];
 // const pageNav = document.querySelectorAll('a');
 // pageNav[0].textContent = siteContent.nav["nav-item-1"];
 // pageNav[1].textContent = siteContent.nav["nav-item-2"];
@@ -59,6 +65,13 @@ const siteContent = {
 // pageNav[5].textContent = siteContent.nav["nav-item-6"];
 
 //make nav green
+nav[0].style.color = "green";
+nav[1].style.color = "green";
+nav[2].style.color = "green";
+nav[3].style.color = "green";
+nav[4].style.color = "green";
+nav[5].style.color = "green";
+
 // pageNav[0].style.color = "green"; 
 // pageNav[1].style.color = "green";
 // pageNav[2].style.color = "green";
@@ -67,53 +80,60 @@ const siteContent = {
 // pageNav[5].style.color = "green";
 
 //h1
+const h1 = document.querySelector('.cta-text h1');
+h1.innerHTML = siteContent["cta"]["h1"];
+
 // const ctaText = document.querySelector('.cta-text h1');
 // ctaText.textContent = siteContent.cta["h1"];
 // siteContent.cta["h1"] = "Dom<br> Is<br> Awesome!";
 // ctaText.innerHTML = siteContent.cta["h1"];
 
 //button
+const button = document.querySelector(".cta-text button");
+button.innerHTML = siteContent["cta"]["button"];
 // const ctaButton = document.querySelector("button");
 // ctaButton.textContent = siteContent.cta["button"];
 
 //Text h4
-// const textH4 = document.querySelectorAll(".text-content h4");
-// textH4[0].textContent = siteContent["main-content"]["features-h4"];
-// textH4[1].textContent = siteContent["main-content"]["about-h4"];
-// textH4[2].textContent = siteContent["main-content"]["services-h4"];
-// textH4[3].textContent = siteContent["main-content"]["product-h4"];
-// textH4[4].textContent = siteContent["main-content"]["vision-h4"];
+const textH4 = document.querySelectorAll(".text-content h4");
+textH4[0].textContent = siteContent["main-content"]["features-h4"];
+textH4[1].textContent = siteContent["main-content"]["about-h4"];
+textH4[2].textContent = siteContent["main-content"]["services-h4"];
+textH4[3].textContent = siteContent["main-content"]["product-h4"];
+textH4[4].textContent = siteContent["main-content"]["vision-h4"];
 
 //Text para
-// const para = document.querySelectorAll(".text-content p");
-// para[0].textContent = siteContent["main-content"]["features-content"];
-// para[1].textContent = siteContent["main-content"]["about-content"];
-// para[2].textContent = siteContent["main-content"]["services-content"];
-// para[3].textContent = siteContent["main-content"]["product-content"];
-// para[4].textContent = siteContent["main-content"]["vision-content"];
+const para = document.querySelectorAll(".text-content p");
+para[0].textContent = siteContent["main-content"]["features-content"];
+para[1].textContent = siteContent["main-content"]["about-content"];
+para[2].textContent = siteContent["main-content"]["services-content"];
+para[3].textContent = siteContent["main-content"]["product-content"];
+para[4].textContent = siteContent["main-content"]["vision-content"];
 
 //Contact 
-// const contactHeader = document.querySelectorAll(".contact h4");
-// contactHeader[0].textContent = siteContent["contact"]["contact-h4"];
+const contactHeader = document.querySelector(".contact h4");
+contactHeader.innerHTML = siteContent["contact"]["contact-h4"];
 
-// const contactPara = document.querySelectorAll(".contact p");
-// contactPara[0].textContent = siteContent["contact"]["address"];
-// contactPara[1].textContent = siteContent["contact"]["phone"];
-// contactPara[2].textContent = siteContent["contact"]["email"];
+const contactPara = document.querySelectorAll(".contact p");
+contactPara[0].textContent = siteContent["contact"]["address"];
+contactPara[1].textContent = siteContent["contact"]["phone"];
+contactPara[2].textContent = siteContent["contact"]["email"];
 
 //Footer
-// const footer = document.querySelector("footer p");
-// footer.textContent = siteContent["footer"]["copyright"];
+const footer = document.querySelector("footer p");
+footer.textContent = siteContent["footer"]["copyright"];
 
 //Add 2 new nav
-// const index = document.createElement("a");
-// index.href = "#";
-// index.textContent = "Home";
-// const firstNav = document.querySelector("header nav");
-// firstNav.prepend(index); 
+const home = document.createElement("a");
+home.href = "#";
+home.innerHTML = "Home";
+const firstNav = document.querySelector("header nav");
+firstNav.prepend(home); 
+home.style.color = "green";
 
-// const signUp = document.createElement("a");
-// signUp.href = "#";
-// signUp.textContent = "Sign Up";
-// const lastNav = document.querySelector("header nav");
-// lastNav.append(signUp);
+const signUp = document.createElement("a");
+signUp.href = "#";
+signUp.innerHTML = "Sign Up";
+const lastNav = document.querySelector("header nav");
+lastNav.append(signUp);
+signUp.style.color = "green";
