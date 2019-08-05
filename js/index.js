56e08ae9d16bdf3f06891ca82a9ddb8b79009d86
Kyle Guerrero <kyleguerrero85@gmail.com>
2019-08-05 16:25:42 -0500

    Working on footer

--- js/index.js
+++ js/index.js
@@ -40,3 +40,75 @@ const siteContent = {
 // Example: Update the img src for the logo
 let logo = document.getElementById("logo-img");
 logo.setAttribute('src', siteContent["nav"]["img-src"])
+
+// Setting The Navigation
+const navItemOne = document.querySelector('nav a:nth-child(1)');
+const navItemTwo = document.querySelector('nav a:nth-child(2)');
+const navItemThree = document.querySelector('nav a:nth-child(3)');
+const navItemFour = document.querySelector('nav a:nth-child(4)');
+const navItemFive = document.querySelector('nav a:nth-child(5)');
+const navItemSix = document.querySelector('nav a:nth-child(6)');
+
+navItemOne.textContent = siteContent.nav["nav-item-1"];
+navItemTwo.textContent = siteContent.nav["nav-item-2"];
+navItemThree.textContent = siteContent.nav["nav-item-3"];
+navItemFour.textContent = siteContent.nav["nav-item-4"];
+navItemFive.textContent = siteContent.nav["nav-item-5"];
+navItemSix.textContent = siteContent.nav["nav-item-6"];
+
+// CTA header text
+const ctaHeader = document.querySelector('.cta-text');
+const ctaArray = siteContent.cta.h1.split(" ");
+document.querySelector('.cta-text h1').remove();
+ctaHeader.insertAdjacentHTML("afterbegin", `<h1>${ctaArray[0]} <br>${ctaArray[1]}<br> ${ctaArray[2]}</h1>`);
+
+// ctaArray.forEach(word => {
+// 	ctaHeader.insertAdjacentHTML('afterbegin', `<h1>${word}<br></h1>`);
+// })
+
+
+
+// CTA Button
+const ctaButton = document.querySelector('.cta-text button');
+ctaButton.textContent = siteContent.cta.button;
+
+// CTA Image
+const ctaImage = document.querySelector('#cta-img')
+ctaImage.src = siteContent.cta["img-src"];
+
+// Feature & About Content
+const featuresHeader = document.querySelector('.top-content .text-content:nth-child(1) h4');
+const featuresText = document.querySelector('.top-content .text-content:nth-child(1) p');
+
+const aboutHeader = document.querySelector('.top-content .text-content:nth-child(2) h4');
+const aboutText = document.querySelector('.top-content .text-content:nth-child(2) p');
+
+featuresHeader.textContent = siteContent["main-content"]["features-h4"];
+featuresText.textContent = siteContent["main-content"]["features-content"];
+
+aboutHeader.textContent = siteContent["main-content"]["about-h4"];
+aboutText.textContent = siteContent["main-content"]["about-content"];
+
+// Middle Image
+const middleImage = document.querySelector('#middle-img');
+middleImage.src = siteContent["main-content"]["middle-img-src"];
+
+// Services Product & Vision
+const serviceHeader = document.querySelector(".bottom-content .text-content:nth-child(1) h4");
+const serviceText = document.querySelector(".bottom-content .text-content:nth-child(1) p");
+
+const productHeader = document.querySelector(".bottom-content .text-content:nth-child(2) h4");
+const productText = document.querySelector(".bottom-content .text-content:nth-child(2) p");
+
+const visionHeader = document.querySelector(".bottom-content .text-content:nth-child(3) h4");
+const visionText = document.querySelector(".bottom-content .text-content:nth-child(3) p");
+
+serviceHeader.textContent = siteContent["main-content"]["services-h4"];
+serviceText.textContent = siteContent["main-content"]["services-content"];
+
+productHeader.textContent = siteContent["main-content"]["product-h4"];
+productText.textContent = siteContent["main-content"]["product-content"];
+
+visionHeader.textContent = siteContent["main-content"]["vision-h4"];
+visionText.textContent = siteContent["main-content"]["vision-content"];
+
