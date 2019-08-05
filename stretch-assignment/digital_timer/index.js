5396020b9ebdc72e3deec91a5a50039a7c232465
noahfranco <noahfra@gmail.com>
2019-08-05 11:05:19 -0400

    testing this now

--- stretch-assignment/digital_timer/index.js
+++ stretch-assignment/digital_timer/index.js
@@ -6,50 +6,16 @@ const msTens = document.getElementById("msTens");
 let totalSeconds = 0; 
 window.setInterval(time, 1000); 
 
-function time() {
-    totalSeconds++ 
-
-    if(totalSeconds > 1000) {
-        secondsTens.innerHTML = 1
-        secondOnes.innerHTML = 0
-        msHundreds.innerHTML = 0
-        msTens.innerHTML = 0
-
-        return false;
-
-    } else {
-        let number = totalSeconds.toString().split(" ")
-        if(number.length === 3) {
-            msTens.innerHTML = number[2]
-            msHundreds.innerHTML = number[1]
-            secondOnes.innerHTML = number[0]
-        }
-
-        if(number.length === 2) {
-            msTens.innerHTML = number[1]
-            msHundreds.innerHTML = number[0] 
-        }
-
-        if(number.length === 1) {
-            msTens.innerHTML = number[0]
-        }
-        return true; 
-    } 
+class timer {
+    constructor() {
+        this.timer = 0; 
+        this.timer = document.getElementsByClassName("digit"); 
+        this.secondTens = document.getElementById("secondTens"); 
+        this.secondOnes = document.getElementById("secondOnes");
+        this.msHundreds = document.getElementById("msHundreds"); 
+        this.msTens = document.getElementById("msTens"); 
+    }
+    
 }
-
-
-
-
-// function 
-
-// const callBack = window.setInterval(function() {
-//     time() ? console.log(totalSeconds) : clearInterval(callBack);
-//   }, 10)
-
-
-
-
-
-
 
 
