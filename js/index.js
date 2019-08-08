6b877f90c9bcac74d852611e71c574e9b97fcc45
Andrew Nottoli <nottolivc@gmail.com>
2019-08-07 19:38:07 -0700

    updated HTML for heading BR, looped through footer array and added .forEach methods to defined querySelectors

--- js/index.js
+++ js/index.js
@@ -37,51 +37,132 @@ const siteContent = {
   },
 };
 
+// // Example: Update the img src for the logo
+// let logo = document.getElementById("logo-img");
+// logo.setAttribute('src', siteContent["nav"]["img-src"]);
+// const body = document.querySelector.body;
+// //document.body.textContent = siteContent[“cta”][“h1"];
+// const navBar = document.querySelectorAll('a');
+// console.log(navBar);
+// navBar[0].textContent = "Services";
+// navBar[1].textContent = "Product";
+// navBar[2].textContent = "Vision";
+// navBar[3].textContent = "Features";
+// navBar[4].textContent = "About";
+// navBar[5].textContent = "Contact";
+// let cta = document.getElementById("cta-img");
+// cta.setAttribute('src', siteContent["cta"]["img-src"]);
+// console.log(cta);
+
+// let middleImg = document.getElementById("middle-img");
+// middleImg.setAttribute('src', siteContent["main-content"]["middle-img-src"]);
+// console.log(middleImg);
+
+// let ctaText = document.getElementsByClassName("cta");
+// console.log(ctaText);
+// //ctaText.setAttribute('src', siteContent["cta"]["h1"]);
+
+// let mainContent = document.querySelectorAll(['.text-content']);
+// mainContent[0].textContent = siteContent['main-content']['features-h4'];
+// mainContent[1].textContent = siteContent['main-content']['about-h4'];
+// mainContent[2].textContent = siteContent['main-content']['services-h4'];
+// mainContent[3].textContent = siteContent['main-content']['product-h4'];
+// mainContent[4].textContent = siteContent['main-content']['vision-h4'];
+
+
 // Example: Update the img src for the logo
-let logo = document.getElementById("logo-img");
-logo.setAttribute('src', siteContent["nav"]["img-src"]);
-const body = document.querySelector.body;
-//document.body.textContent = siteContent[“cta”][“h1"];
-const navBar = document.querySelectorAll('a');
-console.log(navBar);
-navBar[0].textContent = "Services";
-navBar[1].textContent = "Product";
-navBar[2].textContent = "Vision";
-navBar[3].textContent = "Features";
-navBar[4].textContent = "About";
-navBar[5].textContent = "Contact";
-let cta = document.getElementById("cta-img");
-cta.setAttribute('src', siteContent["cta"]["img-src"]);
-console.log(cta);
+let logo = document.getElementById('logo-img');
+logo.setAttribute('src', siteContent['nav']['img-src']);
 
-let middleImg = document.getElementById("middle-img");
-middleImg.setAttribute('src', siteContent["main-content"]["middle-img-src"]);
-console.log(middleImg);
+/* Nav */
+let newItem = document.createElement('a');
+let newItem2 = document.createElement('a');
+let navItem = document.querySelectorAll('nav a');
+let nav = document.querySelector('nav');
 
-let ctaText = document.getElementsByClassName("cta");
-console.log(ctaText);
-//ctaText.setAttribute('src', siteContent["cta"]["h1"]);
+newItem.textContent = 'Blog';
+newItem2.textContent = 'Home';
 
-let mainContent = document.querySelectorAll(['.text-content']);
-mainContent[0].textContent = siteContent['main-content']['features-h4'];
-mainContent[1].textContent = siteContent['main-content']['about-h4'];
-mainContent[2].textContent = siteContent['main-content']['services-h4'];
-mainContent[3].textContent = siteContent['main-content']['product-h4'];
-mainContent[4].textContent = siteContent['main-content']['vision-h4'];
+navItem.forEach(
+  (item, i) =>
+    (item.textContent = siteContent.nav['nav-item-' + (i + 1)]) &&
+    (item.style.color = 'lightgrey')
+);
+
+nav.appendChild(newItem);
+nav.prepend(newItem2);
+
+newItem.style.color = 'grey';
+newItem2.style.color = 'grey';
+
+/* CTA Section */
+
+// Query Selectors
+let ctaHeading = document.querySelector('.cta-text h1');
+let ctaBtn = document.querySelector('.cta-text button');
+let ctaImg = document.querySelector('#cta-img');
+let ctaHeadingArr = siteContent.cta.h1.split(' ');
+
+// Set textContent of cta elements
+ctaHeading.innerHTML = ctaHeadingArr.join('<br>');
+ctaBtn.textContent = siteContent.cta.button;
+
+// Set src attribute for cta image
+ctaImg.src = siteContent.cta['img-src'];
+
+// Main 
+
+// Store main-content object to create an array
+let mainContent = siteContent['main-content'];
+
+// Query Selectors
+let mainContentHeadings = document.querySelectorAll('.text-content h4');
+let mainContentText = document.querySelectorAll('.text-content p');
+let mainContentImg = document.querySelector('#middle-img');
+
+// Store Object Values in Array for loop
+let mainContentArr = Object.entries(mainContent);
+let mainContentHeadingArr = [];
+let mainContentTextArr = [];
+
+// Use forEach and .includes to create heading and content arrays
+mainContentArr.forEach(item => {
+  if (item[0].includes('h4')) {
+    mainContentHeadingArr.push(item[1]);
+  } else if (item[0].includes('content')) {
+    mainContentTextArr.push(item[1]);
+  }
+});
+
+// Loop through nodes and set textContent to their respective array values
+for (let i = 0; i < mainContentHeadings.length; i++) {
+  mainContentHeadings[i].textContent = mainContentHeadingArr[i];
+  mainContentText[i].textContent = mainContentTextArr[i];
+}
+
+// src middle-img
+mainContentImg.setAttribute(
+  'src',
+  siteContent['main-content']['middle-img-src']
+);
 
 
-let mainP = document.querySelectorAll('.text-content p');
-mainP[0].textContent = siteContent['main-content']['features-content'];
-mainP[1].textContent = siteContent['main-content']['about-content'];
-mainP[2].textContent = siteContent['main-content']['services-content'];
-mainP[3].textContent = siteContent['main-content']['product-content'];
-mainP[4].textContent = siteContent['main-content']['vision-content'];
+// Contact
+let contactHeading = document.querySelector('.contact h4');
+let contactParagraphs = document.querySelectorAll('.contact p');
+let contact = siteContent.contact;
 
-let contact = document.querySelectorAll('.contact');
-contact[0].textContent = siteContent['contact']['contact-h4'];
-contact[1].textContent = siteContent['contact']['address'];
-contact[2].textContent = siteContent['contact']['phone'];
-contact[3].textContent = siteContent['contact']['email'];
+// Contact Array for looping through paragraphs
+let contactArr = [contact.address, contact.phone, contact.email];
 
-let footer = document.querySelector('footer');
-footer.texContent = siteContent['footer']
+// Set h4 to contact-h4
+contactHeading.textContent = siteContent.contact['contact-h4'];
+
+// Set p tags to contact address, phone and email
+for (let i = 0; i < contactParagraphs.length; i++) {
+  contactParagraphs[i].textContent = contactArr[i];
+}
+
+/* Footer */
+let copyright = document.querySelector('footer p');
+copyright.textContent = ('copyright 2019');
