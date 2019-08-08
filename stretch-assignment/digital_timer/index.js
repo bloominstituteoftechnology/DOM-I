451b5c7531d43cd3b3d542a667f34fd9adbe1851
bobbidigi <bobbidigi34@gmail.com>
2019-08-08 14:18:30 -0400

    finished timer

--- stretch-assignment/digital_timer/index.js
+++ stretch-assignment/digital_timer/index.js
@@ -1,13 +1,49 @@
+let secondTens = document.getElementById('secondTens');
+let secondOnes = document.getElementById('secondOnes');
+let msTens = document.getElementById('msTens');
+let msHundreds = document.getElementById('msHundreds');
+let msT = 0;
+let msH = 0;
 let seconds = 0;
+let secondT = 0;
+
+timer();
 
 function timer() {
     t = setTimeout(add, 10);
-    // console.log(t);
 }
-timer();
 
-function add(){
-    seconds++;
-    console.log(seconds);
+function add(){    
+    if(msT < 9){
+        msT++;
+    }
+    else{
+        msT = 0;
+        if(msH < 9){
+            msH++;
+        }else{
+            msH = 0;
+            if(seconds < 9){
+                seconds++;
+            }else{
+                seconds = 0;
+                if(secondT < 9){
+                   secondT++;
+                   if(secondT === 1){
+                       msTens.textContent = msT;
+                       msHundreds.textContent = msH;
+                       secondOnes.textContent = seconds;
+                       secondTens.textContent = secondT;
+                       return;
+                   }
+                }
+            }
+        }
+    }
+
+    msTens.textContent = msT;
+    msHundreds.textContent = msH;
+    secondOnes.textContent = seconds;
+    secondTens.textContent = secondT;
     timer();
 }
