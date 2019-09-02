290a5679e3ae939fa86f7693e2bddf5d419b7db2
John Shoff <fearthedev@pop-os.localdomain>
2019-09-02 05:26:37 -0400

    Initial Commit

--- js/index.js
+++ js/index.js
@@ -39,4 +39,45 @@ const siteContent = {
 
 // Example: Update the img src for the logo
 let logo = document.getElementById("logo-img");
-logo.setAttribute('src', siteContent["nav"]["img-src"])
+logo.setAttribute('src', siteContent["nav"]["img-src"]);
+
+
+//Navigation
+
+
+// Call to Action
+let ctaHeadding = document.querySelector('.cta-text h1');
+let ctaButton = document.querySelector('.cta-text button');
+let ctaImage = document.querySelector('#cta-img');
+
+ctaHeadding.textContent = siteContent.cta.h1;
+ctaButton.textContent = siteContent.cta.button;
+ctaImage.src = siteContent.cta["img-src"];
+
+
+// Top content
+let topContentFirstH4 = document.querySelector('.top-content:first-child h4');
+let topContentLastH4 = document.querySelector('.top-content:nth-child(2) h4');
+
+let topContentFirstP = document.querySelector('.top-content:first-child p');
+let topContentLastP = document.querySelector('.top-content:nth-child(2) p');
+
+topContentFirstH4.textContent = siteContent["main-content"]["features-h4"];
+topContentLastH4.textContent = siteContent["main-content"]["about-h4"];
+
+topContentFirstP.textContent = siteContent["main-content"]["features-content"];
+topContentLastP.textContent = siteContent["main-content"]["about-content"];
+
+// Middle image
+let middleImage = document.querySelector('#middle-img');
+middleImage.src = siteContent["main-content"]["middle-img-src"];
+
+
+// Contact
+let contactSectionH4 = document.querySelector('.contact h4');
+
+contactSectionH4.textContent = siteContent.contact["contact-h4"];
+
+// Footer
+let footerParagraph = document.querySelector('footer p');
+footerParagraph.textContent = siteContent.footer.copyright;
