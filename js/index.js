d56dd28f4d98dbf5ee14b44b2fdb32f685c581ba
Manju KC <kcmanjuthapa@gmail.com>
2019-09-03 16:06:56 -0400

    changed some code

--- js/index.js
+++ js/index.js
@@ -1,5 +1,5 @@
 const siteContent = {
-  "nav": {
+  nav: {
     "nav-item-1": "Services",
     "nav-item-2": "Product",
     "nav-item-3": "Vision",
@@ -8,67 +8,76 @@ const siteContent = {
     "nav-item-6": "Contact",
     "img-src": "img/logo.png"
   },
-  "cta": {
-    "h1": "DOM Is Awesome",
-    "button": "Get Started",
+  cta: {
+    h1: "DOM Is Awesome",
+    button: "Get Started",
     "img-src": "img/header-img.png"
   },
   "main-content": {
-    "features-h4":"Features",
-    "features-content": "Features content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.",
-    "about-h4":"About",
-    "about-content": "About content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.",
+    "features-h4": "Features",
+    "features-content":
+      "Features content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.",
+    "about-h4": "About",
+    "about-content":
+      "About content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.",
     "middle-img-src": "img/mid-page-accent.jpg",
-    "services-h4":"Services",
-    "services-content": "Services content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.",
-    "product-h4":"Product",
-    "product-content": "Product content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.",
-    "vision-h4":"Vision",
-    "vision-content": "Vision content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.",
+    "services-h4": "Services",
+    "services-content":
+      "Services content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.",
+    "product-h4": "Product",
+    "product-content":
+      "Product content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.",
+    "vision-h4": "Vision",
+    "vision-content":
+      "Vision content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis."
   },
-  "contact": {
-    "contact-h4" : "Contact",
-    "address" : "123 Way 456 Street Somewhere, USA",
-    "phone" : "1 (888) 888-8888",
-    "email" : "sales@greatidea.io",
+  contact: {
+    "contact-h4": "Contact",
+    address: "123 Way 456 Street Somewhere, USA",
+    phone: "1 (888) 888-8888",
+    email: "sales@greatidea.io"
   },
-  "footer": {
-    "copyright" : "Copyright Great Idea! 2018"
-  },
+  footer: {
+    copyright: "Copyright Great Idea! 2018"
+  }
 };
 
 // Example: Update the img src for the logo
 let logo = document.getElementById("logo-img");
-logo.setAttribute('src', siteContent["nav"]["img-src"])
+logo.setAttribute("src", siteContent["nav"]["img-src"]);
 
-
-//code start
+//code start nav bar
 
 const headerNav = document.querySelectorAll("a");
-let headerNav2 = Array.from(headerNav)
+let headerNav2 = Array.from(headerNav);
 
-headerNav2.map((item,index) =>{
-  item.textContent = Object.values(siteContent.nav)[index]
-})
-
-
-
-const  headerNavigation = document.querySelectorAll("nav a");
-headerNavigation.forEach((item) => {
-   item.style.color = 'green'; 
+headerNav2.map((item, index) => {
+  item.textContent = Object.values(siteContent.nav)[index];
 });
 
-const navigation = document.querySelector('nav')
-const home = document.createElement('a')
-home.textContent = 'Home';
-const shop = document.createElement('a')
-shop.textContent = 'Show Now'
-navigation.prepend(home)
-navigation.appendChild(shop)
+const headerNavigation = document.querySelectorAll("nav a");
+headerNavigation.forEach(item => {
+  item.style.color = "green";
+});
 
-const headerone = document.querySelector("h1").textContent = siteContent.cta["h1"];
-const btn = document.querySelector("button").textContent = siteContent.cta["button"];
-const ctaImg = document.querySelector("#cta-img").src = siteContent.cta["img-src"];
+const navigation = document.querySelector("nav");
+const home = document.createElement("a");
+home.textContent = "Home";
+const shop = document.createElement("a");
+shop.textContent = "Show Now";
+navigation.prepend(home);
+navigation.appendChild(shop);
+
+// First Content
+
+const headerone = (document.querySelector("h1").textContent =
+  siteContent.cta["h1"]);
+const btn = (document.querySelector("button").textContent =
+  siteContent.cta["button"]);
+const ctaImg = (document.querySelector("#cta-img").src =
+  siteContent.cta["img-src"]);
+
+//Second Content
 
 const headingH4 = document.querySelectorAll("h4");
 const paragraphs = document.querySelectorAll("p");
@@ -85,17 +94,19 @@ paragraphs[4].textContent = siteContent["main-content"]["vision-content"];
 const mainSectionImg = (document.getElementById("middle-img").src =
   siteContent["main-content"]["middle-img-src"]);
 
-  headingH4[5].textContent = siteContent.contact["contact-h4"];
-  paragraphs[5].textContent = siteContent.contact.address;
-  paragraphs[6].textContent = siteContent.contact.phone;
-  paragraphs[7].textContent = siteContent.contact.email;
+//Cotact
+headingH4[5].textContent = siteContent.contact["contact-h4"];
+paragraphs[5].textContent = siteContent.contact.address;
+paragraphs[6].textContent = siteContent.contact.phone;
+paragraphs[7].textContent = siteContent.contact.email;
 
-  const footer = document.querySelector("footer p");
-  footer.textContent = siteContent .footer["copyright"];
+//Footer
 
-  
+const footer = document.querySelector("footer p");
+footer.textContent = siteContent.footer["copyright"];
 
-  const button = document.querySelector('button');
+//Button
+const button = document.querySelector("button");
 button.textContent = siteContent.cta.button;
 button.style[`background-color`] = "dodgerblue";
 
