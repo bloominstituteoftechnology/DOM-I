ea0ed6d08eb13ab06b858990a8a89c151f96667a
Rodney-Race <rodneyrace@gmail.com>
2019-09-06 20:58:41 -0400

    update

--- js/index.js
+++ js/index.js
@@ -41,38 +41,82 @@ const siteContent = {
 let logo = document.getElementById("logo-img");
 logo.setAttribute('src', siteContent["nav"]["img-src"])
 
-let navItems = document.querySelectorAll("a");
-navItems[0].textContent = siteContent["nav"]["nav-item-1"];
-navItems[1].textContent = siteContent["nav"]["nav-item-2"];
-navItems[2].textContent = siteContent["nav"]["nav-item-3"];
-navItems[3].textContent = siteContent["nav"]["nav-item-4"];
-navItems[4].textContent = siteContent["nav"]["nav-item-5"];
-navItems[5].textContent = siteContent["nav"]["nav-item-6"];
+// Images
+let ctaImg = document.getElementById('cta-img');
+ctaImg.setAttribute('src', siteContent["cta"]["img-src"]);
+let middleImg = document.getElementById('middle-img');
+middleImg.setAttribute('src', siteContent["main-content"]["middle-img-src"]);
 
-let ctaText = document.querySelector('h1');
-ctaText.textContent = siteContent['cta']['h1']
-let ctaButton = document.querySelector('button');
-ctaButton.textContent = siteContent['cta']['button']
-let ctaImg = document.getElementById("cta-img");
-ctaImg.setAttribute('src', siteContent['cta']['img-src'])
+// Header navigation
+let navigation = document.querySelectorAll('nav a');
+const navigationArr = Array.from(navigation);
+navigationArr[0].textContent = siteContent["nav"]["nav-item-1"];
+navigationArr[1].textContent = siteContent["nav"]["nav-item-2"];
+navigationArr[2].textContent = siteContent["nav"]["nav-item-3"];
+navigationArr[3].textContent = siteContent["nav"]["nav-item-4"];
+navigationArr[4].textContent = siteContent["nav"]["nav-item-5"];
+navigationArr[5].textContent = siteContent["nav"]["nav-item-6"];
 
-let featuresTitle = document.querySelector('.top-content h4');
-featuresTitle.textContent = siteContent['main-content']['features-h4']
-let featuresText = document.querySelector('.text-content p ');
-featuresText.textContent = siteContent['main-content']['features-content']
-
-
-let middle = document.getElementById('middle-img');
-middle.setAttribute('src', siteContent['main-content']['middle-img-src']);
-
-let serviceTitle = document.querySelector('.bottom-content h4');
-serviceTitle.textContent = siteContent['main-content']['service-h4']
-let serviceText = document.querySelector('.text-content p ');
-serviceText.textContent = siteContent['main-content']['service-content']
+// Header
+let headerContent = document.querySelectorAll('div .cta-text h1');
+const headerContentArr = Array.from(headerContent);
+headerContent[0].textContent = siteContent["cta"]['h1'];
+let ctaButton = document.querySelectorAll('div .cta-text button');
+const ctaButtonArr = Array.from(ctaButton);
+ctaButton[0].textContent = siteContent["cta"]['button'];
 
 
-let footerText = document.querySelector('footer');
-footerText.textContent = siteContent['footer']['copyright'];
+// Main content top
+let mainContentHeader = document.querySelectorAll('div .text-content h4');
+const mainContentHeaderArr = Array.from(mainContentHeader);
+mainContentHeaderArr[0].textContent = siteContent['main-content']['features-h4'];
+mainContentHeaderArr[1].textContent = siteContent['main-content']['about-h4'];
+mainContentHeaderArr[2].textContent = siteContent['main-content']['services-h4'];
+mainContentHeaderArr[3].textContent = siteContent['main-content']['product-h4'];
+mainContentHeaderArr[4].textContent = siteContent['main-content']['vision-h4'];
 
-let contact = document.querySelector('contact h4');
-contact.textContent = siteContent['contact']['contact-h4'];
+//Main content bottom
+let mainContentParagraph = document.querySelectorAll('div .text-content p');
+const mainContentParagraphrArr = Array.from(mainContentParagraph);
+mainContentParagraphrArr[0].textContent = siteContent['main-content']['features-content'];
+mainContentParagraphrArr[1].textContent = siteContent['main-content']['about-content'];
+mainContentParagraphrArr[2].textContent = siteContent['main-content']['services-content'];
+mainContentParagraphrArr[3].textContent = siteContent['main-content']['product-content'];
+mainContentParagraphrArr[4].textContent = siteContent['main-content']['vision-content'];
+
+// Contact section 
+let contactHeader = document.querySelectorAll('.contact h4');
+const contactHeaderArr = Array.from(contactHeader);
+contactHeaderArr[0].textContent = siteContent['contact']['contact-h4'];
+let contactParagraph = document.querySelectorAll('.contact p');
+const contactParagraphArr = Array.from(contactParagraph);
+contactParagraphArr[0].textContent = siteContent['contact']['address'];
+contactParagraphArr[1].textContent = siteContent['contact']['phone'];
+contactParagraphArr[2].textContent = siteContent['contact']['email'];
+
+
+// Footer 
+let footer = document.querySelector('footer p');
+footer.textContent = siteContent['footer']['copyright'];
+
+// change color text of the navigation 
+let navColor = document.querySelectorAll('nav a');
+navColor.forEach( (element) => {
+  element.style.color = 'green';
+});
+
+// add navigation items
+const navElements = document.querySelector('nav');
+let newNavItem = document.createElement('a');
+newNavItem.textContent = "Login";
+navElements.appendChild(newNavItem);
+
+const navElements2 = document.querySelector('nav');
+let newNavItem2 = document.createElement('a');
+newNavItem2.textContent = "Home";
+navElements.prepend(newNavItem2);
+
+navColor = document.querySelectorAll('nav a');
+navColor.forEach( (element) => {
+  element.style.color = 'green';
+});
--- js/middle.js
+++
@@ -1,2 +0,0 @@
-let middle = document.getElementById('main-content-img');
-middle.setAttribute('src', siteContent['main-content']['middle-img-src']);
