7b1de4c7da07e249f6c728ac7c8fd9737471115c
Kris <kristian.claire.101@gmail.com>
2019-09-30 13:16:12 -0700

    nav bar completed

--- js/index.js
+++ js/index.js
@@ -39,4 +39,37 @@ const siteContent = {
 
 // Example: Update the img src for the logo
 let logo = document.getElementById("logo-img");
-logo.setAttribute('src', siteContent["nav"]["img-src"])
+logo.setAttribute('src', siteContent["nav"]["img-src"]);
+
+/* This is the navigation bar code */
+let navBar = document.querySelectorAll('nav a'); //selects all of the nav bar a tags.
+navBar.forEach((element, i) => {
+element.textContent = siteContent.nav[`nav-item-${i+1}`];
+element.style.color = 'green'; //color change
+});
+
+// appendChild() or .prepend()
+let navigationBar = document.querySelector('nav'); //Parent Element
+let newNavItem1 = document.createElement('a');  //New Nav Item
+newNavItem1.textContent = 'New Item 1'; //Text on New Nav Item
+navigationBar.appendChild(newNavItem1); //Added to end of Nav Bar
+
+let newNavItem2 = document.createElement('a') //New Nav Item
+newNavItem2.textContent = 'New Item 2'; //Text on New Nav Item
+navigationBar.prepend(newNavItem2); //Added to beginning of Nav Bar
+
+/* .cta section code */
+let ctaH1 = document.querySelector('.cta-text h1');
+ctaH1.textContent = siteContent['cta']['h1'];
+//This is to try to match the style
+ctaH1.style.width = '50%';
+ctaH1.style.fontSize = '80px';
+ctaH1.style.textAlign = 'center';
+
+let ctaButton = document.querySelector('.cta-text button');
+ctaButton.textContent = siteContent['cta']['button'];
+ctaButton.style.marginLeft = '15%';
+
+let ctaIMG = document.querySelector('#cta-img');
+ctaIMG.setAttribute('src', siteContent['cta']['img-src']);
+
