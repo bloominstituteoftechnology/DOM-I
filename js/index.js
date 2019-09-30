d8e67654e6b4ccc6388c779be99bf789fc0e642f
Alberto Galvan <astrometrics@Unimatrix-Zero.local>
2019-09-30 16:43:16 -0500

    Contact done

--- js/index.js
+++ js/index.js
@@ -68,6 +68,7 @@ h4[2].textContent = siteContent["main-content"]["services-h4"];
 h4[3].textContent = siteContent["main-content"]["product-h4"];
 h4[4].textContent = siteContent["main-content"]["vision-h4"];
 
+
 let p = document.querySelectorAll("p");
 p[0].textContent = siteContent["main-content"]["features-content"];
 p[1].textContent = siteContent["main-content"]["about-content"];
@@ -75,7 +76,11 @@ p[2].textContent = siteContent["main-content"]["services-content"];
 p[3].textContent = siteContent["main-content"]["product-content"];
 p[4].textContent = siteContent["main-content"]["vision-content"];
 
-
+///contact
+h4[5].textContent = siteContent["contact"]["contact-h4"];
+p[5].textContent = siteContent["contact"]["address"];
+p[6].textContent = siteContent["contact"]["phone"];
+p[7].textContent = siteContent["contact"]["email"];
 
 
 
