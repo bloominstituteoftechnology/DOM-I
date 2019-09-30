2d369cd3da2cbf932f6b528c80de5a5e20122cb1
Sara De La Cruz <saradelacruz@Saras-MacBook-Air.local>
2019-09-30 16:06:40 -0500

    images, header text, main text added

--- js/index.js
+++ js/index.js
@@ -1,5 +1,5 @@
 const siteContent = {
-  "nav": {
+  nav: {
     "nav-item-1": "Services",
     "nav-item-2": "Product",
     "nav-item-3": "Vision",
@@ -8,35 +8,86 @@ const siteContent = {
     "nav-item-6": "Contact",
     "img-src": "img/logo.png"
   },
-  "cta": {
-    "h1": "DOM Is Awesome",
-    "button": "Get Started",
+  cta: {
+    h1: "DOM Is Awesome",
+    button: "Get Started",
     "img-src": "img/header-img.png"
   },
   "main-content": {
-    "features-h4":"Features",
-    "features-content": "Features content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.",
-    "about-h4":"About",
-    "about-content": "About content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.",
+    "features-h4": "Features",
+    "features-content":
+      "Features content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.",
+    "about-h4": "About",
+    "about-content":
+      "About content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.",
     "middle-img-src": "img/mid-page-accent.jpg",
-    "services-h4":"Services",
-    "services-content": "Services content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.",
-    "product-h4":"Product",
-    "product-content": "Product content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.",
-    "vision-h4":"Vision",
-    "vision-content": "Vision content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.",
+    "services-h4": "Services",
+    "services-content":
+      "Services content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.",
+    "product-h4": "Product",
+    "product-content":
+      "Product content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.",
+    "vision-h4": "Vision",
+    "vision-content":
+      "Vision content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis."
   },
-  "contact": {
-    "contact-h4" : "Contact",
-    "address" : "123 Way 456 Street Somewhere, USA",
-    "phone" : "1 (888) 888-8888",
-    "email" : "sales@greatidea.io",
+  contact: {
+    "contact-h4": "Contact",
+    address: "123 Way 456 Street Somewhere, USA",
+    phone: "1 (888) 888-8888",
+    email: "sales@greatidea.io"
   },
-  "footer": {
-    "copyright" : "Copyright Great Idea! 2018"
-  },
+  footer: {
+    copyright: "Copyright Great Idea! 2018"
+  }
 };
 
 // Example: Update the img src for the logo
+
+//All images
 let logo = document.getElementById("logo-img");
-logo.setAttribute('src', siteContent["nav"]["img-src"])
+logo.setAttribute("src", siteContent["nav"]["img-src"]);
+
+let headerLogo = document.getElementById("cta-img");
+headerLogo.src = siteContent["cta"]["img-src"];
+
+let middleLogo = document.getElementById("middle-img");
+middleLogo.src = siteContent["main-content"]["middle-img-src"];
+
+//Top Nav
+
+let topNav = document.querySelectorAll("a");
+
+topNav[0].textContent = siteContent.nav["nav-item-1"];
+topNav[1].textContent = siteContent.nav["nav-item-2"];
+topNav[2].textContent = siteContent.nav["nav-item-3"];
+topNav[3].textContent = siteContent.nav["nav-item-4"];
+topNav[4].textContent = siteContent.nav["nav-item-5"];
+topNav[5].textContent = siteContent.nav["nav-item-6"];
+
+//Header Text
+
+let mainHeader = document.querySelector("h1");
+mainHeader.textContent = siteContent.cta["h1"];
+
+let btn = document.querySelector("button");
+btn.textContent = siteContent.cta["button"];
+
+//Middle Text
+
+let midTextTitle = document.querySelectorAll("h4");
+midTextTitle[0].textContent = siteContent["main-content"]["features-h4"];
+midTextTitle[1].textContent = siteContent["main-content"]["about-h4"];
+midTextTitle[2].textContent = siteContent["main-content"]["services-h4"];
+midTextTitle[3].textContent = siteContent["main-content"]["product-h4"];
+midTextTitle[4].textContent = siteContent["main-content"]["vision-h4"];
+
+let midTextContent = document.querySelectorAll("p");
+
+midTextContent[0].textContent = siteContent["main-content"]["features-content"];
+midTextContent[1].textContent = siteContent["main-content"]["about-content"];
+midTextContent[2].textContent = siteContent["main-content"]["services-content"];
+midTextContent[3].textContent = siteContent["main-content"]["product-content"];
+midTextContent[4].textContent = siteContent["main-content"]["vision-content"];
+
+//footer
