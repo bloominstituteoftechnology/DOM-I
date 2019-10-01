c17914beea68eb9e8e5b6b303f4f0ad8661f2f73
Norma Bunton <te_nj25@hotmail.com>
2019-09-30 21:45:40 -0400

    the Dom

--- README.md
+++ README.md
@@ -11,16 +11,16 @@ You have been provided a [JSON object](js/index.js) with all the necessary data 
 
 **Follow these steps to set up and work on your project:**
 
-* [ ] Create a forked copy of this project.
-* [ ] Add your project manager as collaborator on Github.
-* [ ] Clone your OWN version of the repository (Not Lambda's by mistake!).
-* [ ] Create a new branch: git checkout -b `<firstName-lastName>`.
-* [ ] Implement the project on your newly created `<firstName-lastName>` branch, committing changes regularly.
-* [ ] Push commits: git push origin `<firstName-lastName>`.
+* [x ] Create a forked copy of this project.
+* [x ] Add your project manager as collaborator on Github.
+* [ x] Clone your OWN version of the repository (Not Lambda's by mistake!).
+* [x ] Create a new branch: git checkout -b `<firstName-lastName>`.
+* [x ] Implement the project on your newly created `<firstName-lastName>` branch, committing changes regularly.
+* [x ] Push commits: git push origin `<firstName-lastName>`.
 
 **Follow these steps for completing your project.**
 
-* [ ] Submit a Pull-Request to merge <firstName-lastName> Branch into master (student's  Repo). **Please don't merge your own pull request**
+* [ x] Submit a Pull-Request to merge <firstName-lastName> Branch into master (student's  Repo). **Please don't merge your own pull request**
 * [ ] Add your project manager as a reviewer on the pull-request
 * [ ] Your project manager will count the project as complete by merging the branch back into master.
 
@@ -34,9 +34,9 @@ You have been provided a [JSON object](js/index.js) with all the necessary data 
 * [ ] Remember to update the src attributes on images
 
 ## Task 3: Add new content
-* [ ] Change the color of the navigation text to be green.
-* [ ] Utilize `.appendChild()` and `.prepend()` to add two new items to the navigation system. You can call them whatever you want.
-* [ ] Check your work by looking at the [original html](original.html) in the browser
+* [ x] Change the color of the navigation text to be green.
+* [ x] Utilize `.appendChild()` and `.prepend()` to add two new items to the navigation system. You can call them whatever you want.
+* [x ] Check your work by looking at the [original html](original.html) in the browser
 
 ## Stretch Goals
 * [ ] Update styles throughout the page as you see fit.  Study what happens when you updated the DOM using style in JavaScript.  
--- js/index.js
+++ js/index.js
@@ -40,3 +40,86 @@ const siteContent = {
 // Example: Update the img src for the logo
 let logo = document.getElementById("logo-img");
 logo.setAttribute('src', siteContent["nav"]["img-src"])
+
+let nav = document.getElementsByTagName("nav")[0];
+
+
+
+
+nav.getElementsByTagName('a')[0].textContent=siteContent['nav']['nav-item-1']
+
+nav.getElementsByTagName('a')[1].textContent=siteContent['nav']
+['nav-item-2']
+
+nav.getElementsByTagName('a')[2].textContent=siteContent['nav']
+['nav-item-3']
+
+nav.getElementsByTagName('a')[3].textContent=siteContent['nav']
+['nav-item-4']
+
+nav.getElementsByTagName('a')[4].textContent=siteContent['nav']
+['nav-item-5']
+
+nav.getElementsByTagName('a')[5].textContent=siteContent['nav']
+['nav-item-6']
+
+var home = document.createElement('a')
+home.setAttribute('href', '#')
+home.textContent='Home'
+nav.prepend(home)
+
+var blog = document.createElement('a')
+blog.setAttribute('href', '#')
+blog.textContent='Blog'
+nav.appendChild(blog)
+
+nav.querySelectorAll('a').forEach(link => link.setAttribute('style','color: green'));
+
+let cta=document.querySelector('.cta');
+cta.getElementsByTagName('h1')[0].textContent=siteContent['cta']['h1']
+
+cta.getElementsByTagName('button')[0].textContent=siteContent['cta']['button']
+
+document.getElementById('cta-img').setAttribute('src', siteContent['cta']['img-src'])
+
+let mainContent=document.querySelector('.main-content');
+mainContent.getElementsByTagName('h4')[0].textContent=siteContent['main-content']['features-h4']
+
+mainContent.getElementsByTagName('p')[0].textContent=siteContent['main-content']['features-content']
+
+mainContent.getElementsByTagName('h4')[1].textContent=siteContent['main-content']['about-h4']
+
+mainContent.getElementsByTagName('p')[1].textContent=siteContent['main-content']['about-content']
+
+mainContent.getElementsByClassName('middle-img')[0].setAttribute('src', siteContent['main-content']['middle-img-src'])
+
+mainContent.getElementsByTagName('h4')[2].textContent=siteContent['main-content']['services-h4']
+
+mainContent.getElementsByTagName('p')[2].textContent=siteContent['main-content']['services-content']
+
+mainContent.getElementsByTagName('h4')[3].textContent=siteContent['main-content']['product-h4']
+
+mainContent.getElementsByTagName('p')[3].textContent=siteContent['main-content']['product-content']
+
+mainContent.getElementsByTagName('h4')[4].textContent=siteContent['main-content']['vision-h4']
+
+mainContent.getElementsByTagName('p')[4].textContent=siteContent['main-content']['vision-content']
+
+let contact=document.querySelector('.contact')
+contact.getElementsByTagName('h4')[0]
+.textContent=siteContent['contact']['contact-h4']
+
+contact.getElementsByTagName('p')[0]
+.textContent=siteContent['contact']['address']
+
+contact.getElementsByTagName('p')[1]
+.textContent=siteContent['contact']['phone']
+
+contact.getElementsByTagName('p')[2]
+.textContent=siteContent['contact']['email']
+
+let footer=document.querySelector('footer')
+footer.querySelector('p')
+.textContent=siteContent['footer']['copyright']
+
+
