faedbf2207b900f2d0ddfd05ec8f965a9c067ec1
daltonwalkerdw <53591219+daltonwalkerdw@users.noreply.github.com>
2019-10-02 20:12:24 -0400

    finished, working on stretch

--- js/index.js
+++ js/index.js
@@ -40,3 +40,90 @@ const siteContent = {
 // Example: Update the img src for the logo
 let logo = document.getElementById("logo-img");
 logo.setAttribute('src', siteContent["nav"]["img-src"])
+
+const nav = document.querySelector('nav')
+const aTag = document.querySelector('nav').querySelectorAll('a');
+
+aTag.forEach(element => {
+   element.style.color = 'green'
+    });
+
+ var node = document.createElement("a");
+ var textNode = document.createTextNode("Invert");
+ node.appendChild(textNode);
+ document.querySelector('nav').appendChild(node).id = "invert";
+  var node2 = document.createElement("a");
+  var textNode2 = document.createTextNode("Classic");
+  node2.appendChild(textNode2);
+ 
+  document.querySelector('nav').prepend(node2);
+  let invert = document.getElementById('invert')
+// let revert = document.getElementById("revert").style.display = "none"
+
+  invert.addEventListener('click', ()=>{
+     document.querySelector('*').style.color = "white"
+     document.querySelector("*").style.backgroundColor = "black"
+     
+     
+  })
+  
+//   revert.addEventListener('click', ()=>{
+//     document.querySelector('*').style.color = "black"
+//     document.querySelector("*").style.backgroundColor = "white"
+//  })
+ 
+aTag[0].textContent = siteContent["nav"]["nav-item-1"];
+aTag[1].textContent = siteContent["nav"]["nav-item-2"];
+aTag[2].textContent = siteContent["nav"]["nav-item-3"];
+aTag[3].textContent = siteContent["nav"]["nav-item-4"];
+aTag[4].textContent = siteContent["nav"]["nav-item-5"];
+aTag[5].textContent = siteContent["nav"]["nav-item-6"];
+
+
+
+ 
+
+ 
+
+// end nav
+
+
+
+
+// cta
+let headerImage = document.getElementById("cta-img");
+headerImage.setAttribute('src', siteContent["cta"]["img-src"])
+
+let button = document.querySelector("button")
+button.textContent = "Get Started"
+
+let ctaH1 = document.querySelector(".cta-text h1")
+ctaH1.textContent = "DOM Is Awesome"
+// end cta
+
+//main-content
+let featuresH4 = document.querySelector(".main-content").querySelectorAll('h4');
+let featureCont = document.querySelector(".main-content").querySelectorAll('p');
+featuresH4[0].textContent = siteContent["main-content"]["features-h4"];
+featureCont[0].textContent = siteContent['main-content']['features-content'];
+featuresH4[1].textContent = siteContent["main-content"]["about-h4"];
+featureCont[1].textContent = siteContent['main-content']['about-content'];
+const middleImg = document.getElementById('middle-img').setAttribute('src', siteContent['main-content']['middle-img-src'])
+featuresH4[2].textContent = siteContent["main-content"]["services-h4"];
+featureCont[2].textContent = siteContent['main-content']['services-content'];
+featuresH4[3].textContent = siteContent["main-content"]["product-h4"];
+featureCont[3].textContent = siteContent['main-content']['product-content'];
+featuresH4[4].textContent = siteContent["main-content"]["vision-h4"];
+featureCont[4].textContent = siteContent['main-content']['vision-content'];
+// end main-content
+
+// contact
+let contactH4 = document.querySelector('.contact h4').textContent = siteContent['contact']['contact-h4']
+let contactPTags = document.querySelector('.contact').querySelectorAll('p');
+contactPTags[0].textContent = siteContent['contact']['address'];
+contactPTags[1].textContent = siteContent['contact']['phone'];
+contactPTags[2].textContent = siteContent['contact']['email'];
+// end contact
+
+// footer
+let footer = document.querySelector("footer p").textContent = siteContent['footer']['copyright']
