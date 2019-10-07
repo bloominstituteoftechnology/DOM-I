aaf5988fe944d194f558369002d03d13ebcdfae8
Maria Richards <53357020+aTechNewbie38@users.noreply.github.com>
2019-10-07 16:37:14 -0400

    Completed Dom-I assignment

--- js/index.js
+++ js/index.js
@@ -40,3 +40,60 @@ const siteContent = {
 // Example: Update the img src for the logo
 let logo = document.getElementById("logo-img");
 logo.setAttribute('src', siteContent["nav"]["img-src"])
+
+//Nav Section 
+let nav = document.querySelectorAll("a");
+nav[0].innerHTML = siteContent.nav["nav-item-1"];
+nav[1].innerHTML = siteContent.nav["nav-item-2"];
+nav[2].innerHTML = siteContent.nav["nav-item-3"];
+nav[3].innerHTML = siteContent.nav["nav-item-4"];
+nav[4].innerHTML = siteContent.nav["nav-item-5"];
+nav[5].innerHTML = siteContent.nav["nav-item-6"];
+
+//Nav Color 
+for(let i=0; i < nav.length; i++){
+  nav[i].style.color = "green";
+}
+
+//CTA Context
+let ctaText = document.querySelector(".cta-text > h1");
+ctaText.innerHTML = siteContent.cta.h1;
+
+let ctaButton = document.querySelector(".cta-text > button");
+ctaButton.innerHTML = siteContent.cta.button;
+
+//CTA Image
+let cta = document.getElementById("cta-img");
+cta.setAttribute('src', siteContent["cta"]["img-src"])
+
+//Main Content
+let mainContent = document.querySelectorAll(".main-content h4");
+mainContent[0].innerHTML = "Features";
+mainContent[1].innerHTML = "About";
+mainContent[2].innerHTML = "Services";
+mainContent[3].innerHTML = "Product";
+mainContent[4].innerHTML = "Vision";
+
+//Middle Image
+let middleImage = document.getElementById("middle-img");
+middleImage.setAttribute("src", siteContent["main-content"]["middle-img-src"]);
+
+let mainContentP = document.querySelectorAll(".main-content p");
+mainContentP[0].innerHTML = "Features content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.";
+mainContentP[1].innerHTML = "About content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.";
+mainContentP[2].innerHTML = "Services content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.";
+mainContentP[3].innerHTML = "Product content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.";
+mainContentP[4].innerHTML = "Vision content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.";
+
+//Bottom Content 
+let bottomContent = document.querySelector(".contact h4").innerHTML = "contact";
+
+let contactP = document.querySelectorAll(".contact p");
+contactP[0].innerHTML = "123 Wau 456 Street Somewhere, USA";
+contactP[1].innerHTML = "1 (888) 888-8888";
+contactP[2].innerHTML = "sales@greatidea.io";
+
+document.querySelector("footer").innerHTML = "Copyright Great Idea! 2018";
+
+
+
