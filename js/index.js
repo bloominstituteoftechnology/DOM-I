6c0072488455cf9d806d8f2481714950c8e008b1
Derek Jones <derek@derekjones.io>
2019-10-21 03:08:32 -0400

    Add middle content DOM selectors

--- js/index.js
+++ js/index.js
@@ -72,3 +72,30 @@ let ctaImg = document.querySelector('#cta-img');
 ctaHeader.innerHTML = siteContent['cta']['h1'].replace(/\s/g, '<br>');
 ctaButton.textContent = siteContent['cta']['button'];
 ctaImg.src = siteContent['cta']['img-src'];
+
+/***********************************************
+                  Middle Content
+***********************************************/
+
+// Add section headers
+let sectionHeader = document.querySelectorAll('.main-content h4');
+
+sectionHeader[0].textContent = siteContent['main-content']['features-h4'];
+sectionHeader[1].textContent = siteContent['main-content']['about-h4'];
+sectionHeader[2].textContent = siteContent['main-content']['services-h4'];
+sectionHeader[3].textContent = siteContent['main-content']['product-h4'];
+sectionHeader[4].textContent = siteContent['main-content']['vision-h4'];
+
+// Add middle banner image
+let middleImage = document.querySelector('#middle-img');
+
+middleImage.src = siteContent['main-content']['middle-img-src'];
+
+// Add section content
+let sectionContent = document.querySelectorAll('.main-content p');
+
+sectionContent[0].textContent = siteContent['main-content']['features-content'];
+sectionContent[1].textContent = siteContent['main-content']['about-content'];
+sectionContent[2].textContent = siteContent['main-content']['services-content'];
+sectionContent[3].textContent = siteContent['main-content']['product-content'];
+sectionContent[4].textContent = siteContent['main-content']['vision-content'];
