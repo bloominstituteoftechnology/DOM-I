4727db211438feb9988992160b9a53f3a6d2edde
camfunk <cameronfunk92@gmail.com>
2019-10-28 18:22:49 -0400

    finished tasks two and three

--- js/index.js
+++ js/index.js
@@ -45,13 +45,30 @@ const siteContent = {
   }
 };
 
-let nav = document.querySelectorAll("a");
+let nav = document.querySelectorAll("nav > a");
 nav[0].textContent = siteContent["nav"]["nav-item-1"];
 nav[1].textContent = siteContent["nav"]["nav-item-2"];
 nav[2].textContent = siteContent["nav"]["nav-item-3"];
 nav[3].textContent = siteContent["nav"]["nav-item-4"];
 nav[4].textContent = siteContent["nav"]["nav-item-5"];
 nav[5].textContent = siteContent["nav"]["nav-item-6"];
+
+//green
+let green = document.querySelectorAll("a");
+green.forEach(function(a) {
+  a.style.color = "green";
+});
+
+//append and prepend
+let newNav = document.querySelector("nav");
+
+let appendNav = document.createElement("a");
+appendNav.textContent = "Cyborg Barry";
+newNav.appendChild(appendNav);
+
+let prependNav = document.createElement("a");
+prependNav.textContent = "Basic Barry";
+newNav.prepend(prependNav);
 
 let logo = document.getElementById("logo-img");
 logo.setAttribute("src", siteContent["nav"]["img-src"]);
@@ -62,11 +79,36 @@ ctaImg.setAttribute("src", siteContent["cta"]["img-src"]);
 let ctaButton = document.querySelector(".cta .cta-text button");
 ctaButton.textContent = siteContent["cta"]["button"];
 
-//let middleImg = document.querySelector(".middle-img");
-//middleImg.setAttribute("src", siteContent [])
+let mainContentH4 = document.querySelectorAll(".main-content h4");
+mainContentH4[0].textContent = siteContent["main-content"]["features-h4"];
+mainContentH4[1].textContent = siteContent["main-content"]["about-h4"];
+mainContentH4[2].textContent = siteContent["main-content"]["services-h4"];
+mainContentH4[3].textContent = siteContent["main-content"]["product-h4"];
+mainContentH4[4].textContent = siteContent["main-content"]["vision-h4"];
+
+let middleImg = document.querySelector(".middle-img");
+middleImg.setAttribute("src", siteContent["main-content"]["middle-img-src"]);
+
+let mainContentP = document.querySelectorAll(".main-content p");
+mainContentP[0].textContent = siteContent["main-content"]["features-content"];
+mainContentP[1].textContent = siteContent["main-content"]["about-content"];
+mainContentP[2].textContent = siteContent["main-content"]["services-content"];
+mainContentP[3].textContent = siteContent["main-content"]["product-content"];
+mainContentP[4].textContent = siteContent["main-content"]["vision-content"];
 
 document.querySelector("h1").textContent = "DOM is Awesome";
 cta.innerHTML = "DOM<br> Is<br> Awesome";
+
+let contactH4 = document.querySelector(".contact h4");
+contactH4.textContent = siteContent["contact"]["contact-h4"];
+
+let contactP = document.querySelectorAll(".contact p");
+contactP[0].textContent = siteContent["contact"]["address"];
+contactP[1].textContent = siteContent["contact"]["phone"];
+contactP[2].textContent = siteContent["contact"]["email"];
+
+let footer = document.querySelector("footer p");
+footer.textContent = siteContent["footer"]["copyright"];
 
 // Example: Update the img src for the logo
 //let logo = document.getElementById("logo-img");
