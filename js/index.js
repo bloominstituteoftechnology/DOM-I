ca415efbee9096aa4a3f766cbdabc17d8b5acd39
Leisha <banksleisha@yahoo.com>
2019-10-30 23:59:49 -0400

    complete DOM I

--- js/index.js
+++ js/index.js
@@ -1,3 +1,5 @@
+//LB--edit in Atom
+
 const siteContent = {
   "nav": {
     "nav-item-1": "Services",
@@ -39,4 +41,56 @@ const siteContent = {
 
 // Example: Update the img src for the logo
 let logo = document.getElementById("logo-img");
-logo.setAttribute('src', siteContent["nav"]["img-src"])
+logo.setAttribute('src', siteContent["nav"]["img-src"]);
+
+//start code
+
+let headerImg = document.getElementById("cta-img");
+headerImg.setAttribute('src', siteContent["cta"]["img-src"]);
+
+let middleImg = document.getElementById("middle-img");
+middleImg.setAttribute('src', siteContent["main-content"]["middle-img-src"]);
+
+let h1 = document.querySelector("h1");
+h1.textContent = siteContent["cta"]["h1"];
+
+let button = document.querySelector("button");
+button.textContent = siteContent["cta"]["button"];
+
+let anchors = document.querySelectorAll("a");
+for(let i = 0; i < anchors.length; i++){
+  anchors[i].textContent = siteContent["nav"]["nav-item-"+(i+1)];
+  anchors[i].style.color = "green";
+}
+
+const aChildAppend = document.createElement("a");
+aChildAppend.href = "#";
+aChildAppend.textContent = "Child Append";
+aChildAppend.style.color = "green";
+const nav = document.querySelector("nav");
+nav.appendChild(aChildAppend);
+
+const aPrepend = document.createElement("a");
+aPrepend.href = "#";
+aPrepend.textContent = "Prepend";
+aPrepend.style.color = "green";
+nav.prepend(aPrepend);
+
+let h4 = document.querySelectorAll("h4");
+h4[0].textContent = siteContent["main-content"]["features-h4"];
+h4[1].textContent = siteContent["main-content"]["about-h4"];
+h4[2].textContent = siteContent["main-content"]["services-h4"];
+h4[3].textContent = siteContent["main-content"]["product-h4"];
+h4[4].textContent = siteContent["main-content"]["vision-h4"];
+h4[5].textContent = siteContent["contact"]["contact-h4"];
+
+let p = document.querySelectorAll("p");
+p[0].textContent = siteContent["main-content"]["features-content"];
+p[1].textContent = siteContent["main-content"]["about-content"];
+p[2].textContent = siteContent["main-content"]["services-content"];
+p[3].textContent = siteContent["main-content"]["product-content"];
+p[4].textContent = siteContent["main-content"]["vision-content"];
+p[5].textContent = siteContent["contact"]["address"];
+p[6].textContent = siteContent["contact"]["phone"];
+p[7].textContent = siteContent["contact"]["email"];
+p[8].textContent = siteContent["footer"]["copyright"];
