4d95369c7793c5e40145bc8b1e234874a5927212
reggieTheCoder <33520187+reggieTheCoder@users.noreply.github.com>
2019-10-30 22:29:18 -0400

    navigation bar complete

--- js/index.js
+++ js/index.js
@@ -40,3 +40,13 @@ const siteContent = {
 // Example: Update the img src for the logo
 let logo = document.getElementById("logo-img");
 logo.setAttribute('src', siteContent["nav"]["img-src"])
+
+
+// update the navigation links
+let navigationLinks = document.querySelectorAll("nav a");
+navigationLinks.forEach((link, i) => {
+  link.textContent = siteContent.nav[`nav-item-${i+1}`];
+})
+
+
+
