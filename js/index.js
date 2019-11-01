03a077710e152a266f7d8d2b94a890edd401c3cd
Jessica <Jessica@users-iMac.local>
2019-10-31 23:33:22 -0400

    DOM-I

--- js/index.js
+++ js/index.js
@@ -38,5 +38,109 @@ const siteContent = {
 };
 
 // Example: Update the img src for the logo
-let logo = document.getElementById("logo-img");
-logo.setAttribute('src', siteContent["nav"]["img-src"])
+// let logo = document.getElementById("logo-img");
+// logo.setAttribute('src', siteContent["nav"]["img-src"])
+
+
+// VARIABLES
+let index = 0; 
+
+// NAVIGATION 
+
+const navBar = document.querySelectorAll("nav"); 
+const navLinks = document.querySelectorAll("a"); 
+const logo = document.querySelector("#logo-img");
+
+for(index; index < navLinks.length; index++){
+  navLinks[index].textContent = siteContent.nav[`nav-item-${index + 1}`];
+}
+
+navLinks[0].style.color = "green"
+navLinks[1].style.color = "green"
+navLinks[2].style.color = "green"
+navLinks[3].style.color = "green"
+navLinks[4].style.color = "green"
+navLinks[5].style.color = "green"
+
+
+logo.src = "../img/logo.png";
+
+
+//APPENDING  
+const Forum = document.createElement('a');
+Forum.href = "#";
+Forum.textContent = "Forum";
+
+const mainNavBar = document.querySelector('nav');
+mainNavBar.append(Forum);
+Forum.style.color = "green";
+
+//PREPENDING
+const Login = document.createElement('a');
+Login.href = "#"; 
+Login.textContent = "Login";
+
+const mainNavBar2 = document.querySelector('nav');
+mainNavBar2.prepend(Login);
+Login.style.color = "green";
+
+
+// BANNER TEXT
+
+const bannerText = document.querySelector(".cta-text h1"); 
+bannerText.textContent = siteContent.cta["h1"]; 
+
+const bannerButton = document.querySelector("button");
+bannerButton.textContent = siteContent.cta["button"]; 
+
+const bannerImg = document.querySelector("#cta-img");
+bannerImg.src = "../img/header-img.png";
+ 
+
+// BODY TEXT 
+
+const bodyH4 = document.querySelectorAll(".main-content h4");
+const bodyParagraphs = document.querySelectorAll(".main-content p");
+
+//top content
+bodyH4[0].textContent = siteContent[`main-content`]["features-h4"]; 
+bodyParagraphs[0].textContent = siteContent[`main-content`]["features-content"]; 
+
+bodyH4[1].textContent = siteContent[`main-content`]["about-h4"]; 
+bodyParagraphs[1].textContent = siteContent[`main-content`]["about-content"];
+
+//bottom content
+bodyH4[2].textContent = siteContent[`main-content`]["services-h4"];
+bodyParagraphs[2].textContent = siteContent[`main-content`]["services-content"];
+
+bodyH4[3].textContent = siteContent[`main-content`]["product-h4"]; 
+bodyParagraphs[3].textContent = siteContent[`main-content`]["product-content"];
+
+bodyH4[4].textContent = siteContent[`main-content`]["vision-h4"]; 
+bodyParagraphs[4].textContent = siteContent[`main-content`]["vision-content"];
+
+//mid image
+let bodyImg = document.getElementById("middle-img"); 
+bodyImg.setAttribute('src', siteContent["main-content"]["middle-img-src"]); 
+
+
+// FOOTER 
+
+const contactBlock = document.querySelectorAll(".contact");
+
+const contactHeader = document.querySelector(".contact h4");
+contactHeader.textContent = siteContent[`contact`] ["contact-h4"];
+
+const contactPTags = document.querySelectorAll(".contact p");
+contactPTags[0].textContent = siteContent[`contact`] ["address"];
+contactPTags[0].style.width = "9rem";
+contactPTags[1].textContent = siteContent[`contact`] ["phone"];
+contactPTags[2].textContent = siteContent[`contact`] ["email"];
+
+
+const footerBlock = document.querySelector("footer p");
+footerBlock.textContent = siteContent[`footer`] ["copyright"];
+
+
+
+
