f59ca4bf8708c065e8a76b302f01235c7aa38904
Anita Garris <aniigarris@gmail.com>
2019-11-01 22:24:54 -0600

    Completed the project

--- js/index.js
+++ js/index.js
@@ -40,3 +40,45 @@ const siteContent = {
 // Example: Update the img src for the logo
 let logo = document.getElementById("logo-img");
 logo.setAttribute('src', siteContent["nav"]["img-src"])
+
+let aTags = document.querySelectorAll('a');
+aTags[0].textContent = siteContent["nav"]["nav-item-1"];
+aTags[1].textContent = siteContent["nav"]["nav-item-2"];
+aTags[2].textContent = siteContent["nav"]["nav-item-3"];
+aTags[3].textContent = siteContent["nav"]["nav-item-4"];
+aTags[4].textContent = siteContent["nav"]["nav-item-5"];
+aTags[5].textContent = siteContent["nav"]["nav-item-6"];
+
+let ctaImg = document.getElementById('cta-img');
+ctaImg.setAttribute('src', siteContent["cta"]["img-src"])
+
+let ctah1 = document.querySelector('h1');
+ctah1.textContent = siteContent["cta"]["h1"];
+
+let ctaButton = document.querySelector('button');
+ctaButton.textContent = siteContent["cta"]["button"];
+
+let headings = document.querySelectorAll('h4');
+
+headings[0].textContent = siteContent['main-content']['features-h4'];  
+headings[1].textContent = siteContent['main-content']['about-h4'];
+headings[2].textContent = siteContent['main-content']['services-h4'];
+headings[3].textContent = siteContent['main-content']['product-h4'];
+headings[4].textContent = siteContent['main-content']['vision-h4'];
+headings[5].textContent = siteContent['contact']['contact-h4'];
+
+let midPic = document.getElementById('middle-img');
+midPic.setAttribute('src', siteContent['main-content']['middle-img-src'])
+
+let content = document.querySelectorAll('p');
+
+content[0].textContent = siteContent['main-content']['features-content'];  
+content[1].textContent = siteContent['main-content']['about-content'];
+content[2].textContent = siteContent['main-content']['services-content'];
+content[3].textContent = siteContent['main-content']['product-content'];
+content[4].textContent = siteContent['main-content']['vision-content'];
+content[5].textContent = siteContent['contact']['address'];
+content[6].textContent = siteContent['contact']['phone'];
+content[7].textContent = siteContent['contact']['email'];
+content[8].textContent = siteContent['footer']['copyright'];
+
