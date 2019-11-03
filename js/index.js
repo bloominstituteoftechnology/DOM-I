3dbff3430c57d9acb37d85f1e68a2cda74886a24
Alison Ludick <alison.ludick@gmail.com>
2019-11-03 15:40:46 +0200

    made page look like original.html

--- js/index.js
+++ js/index.js
@@ -39,27 +39,32 @@ const siteContent = {
 
 // Example: Update the img src for the logo (Do likewise for the rest of the images)
 let logo = document.getElementById("logo-img");
-logo.setAttribute('src', siteContent["nav"]["img-src"])
+logo.setAttribute('src', siteContent["nav"]["img-src"]); 
 
-// Add new link in navigation element, at the end, called Services
+// set header image
+const logo2 = document.getElementById("cta-img"); 
+console.log(logo2); 
+logo2.src = "img/header-img.png"; 
+
+// Add new link in navigation element, at the end, called Services [and then comment it out]
 const headerElement = document.querySelector('header');
 headerElement;
 const newLink = document.createElement('a');
 newLink;
-newLink.textContent = "Services";
-newLink;
-headerElement.appendChild(newLink);
+// newLink.textContent = "Services";
+// newLink;
+// headerElement.appendChild(newLink);
 
 // Give the Services link a green color
 newLink.style.color = "green";
 newLink;
 
 // Add new link to navigation, to the front, called Product
-const firstLink = document.createElement('a');
-firstLink;
-firstLink.textContent = "Product";
-firstLink;
-headerElement.prepend(firstLink);
+// const firstLink = document.createElement('a');
+// firstLink;
+// firstLink.textContent = "Product";
+// firstLink;
+// headerElement.prepend(firstLink);
 
 // Update all links in navigation bar using querySelectorAll
 // const linkElements = document.querySelectorAll('nav a');
@@ -90,7 +95,10 @@ console.log(navValues);
 [...navLinks].forEach((link, index) => {
     link.textContent = navValues[index];
     link.setAttribute('style', 'color: green');
+    // navBar.removeChild(navBar.lastChild); //trying to remove logo link
+    
 })
+
 
 // Bonus' suggestion: try to use a different loop for the contact (and refer to the previous loop we used for the navs)
 let contactSection = document.querySelector('section.contact'); // storing the Contact info in the Section tag in a variable called contactSection
@@ -99,7 +107,32 @@ let headerContact = contactSection.querySelector('h4'); // draw out just the Con
 headerContact.textContent = siteContent.contact['contact-h4']; // set the HTML value of the Contact header (H4) to the JavaScript key value pair for the Contact object within siteContent 
 console.log(headerContact);
 
-// the Paragraphs
+// The Upper Body Paragraphs, Headings and Para Content
+let upperBodyHeadings = document.querySelectorAll('h4'); 
+console.log(upperBodyHeadings); 
+upperBodyHeadings[0].textContent = "Features"; 
+upperBodyHeadings[1].textContent = "About";
+
+let middleBodyParas = document.querySelectorAll('p'); 
+console.log(middleBodyParas); 
+middleBodyParas[0].textContent = "Features content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis."; 
+middleBodyParas[1].textContent = "About content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis."; 
+
+// Middle Image
+const logo3 = document.getElementById("middle-img"); 
+console.log(logo3); 
+logo3.src = "img/mid-page-accent.jpg"; 
+
+//The Lower Body Headings and Paragraphs
+upperBodyHeadings[2].textContent = "Services"; 
+upperBodyHeadings[3].textContent = "Product";
+upperBodyHeadings[4].textContent = "Vision";
+
+middleBodyParas[2].textContent = "Services content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis."; 
+middleBodyParas[3].textContent = "Product content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis."; 
+middleBodyParas[4].textContent = "Vision content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis."; 
+
+// the Contact Paragraph
 let contactParas = contactSection.querySelectorAll('p'); // draw out just the paragraphs in the Contact section
 console.log(contactParas);
 let ObjParas = siteContent.contact; // taking the Contact object with siteContent and storing in a variable called ObjParas
@@ -109,3 +142,18 @@ console.log(paraValues);
 [...contactParas].forEach((p, index) => { // call the forEach method over the contactParas array?
     p.textContent = paraValues[index + 1];
 })
+
+// the Button
+const getStartedButton = document.querySelector('button');
+console.log(getStartedButton); 
+getStartedButton.textContent = "Get Started"; // add the "Get Started" label to the button
+
+// Populate Features paragraph, heading and content
+const featPara = document.querySelector('h1'); 
+console.log(featPara);
+featPara.textContent = "Dom Is Awesome"; 
+
+// the Footer
+const footerCopy = document.querySelector('footer'); 
+console.log(footerCopy);
+footerCopy.textContent = "Copyright Great Idea! 2018";  
