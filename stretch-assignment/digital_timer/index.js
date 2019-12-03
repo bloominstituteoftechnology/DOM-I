1a91b4367c2860fe615fc281bc135a3514c3a3a0
ljh-c <57099181+ljh-c@users.noreply.github.com>
2019-12-02 23:50:32 -0800

    timer starts on button click

--- stretch-assignment/digital_timer/index.js
+++ stretch-assignment/digital_timer/index.js
@@ -7,18 +7,41 @@ let totalMs = 0;
 
 let timerDigits = document.querySelectorAll('.digit');
 
-const timer = setInterval(() => {
-    totalMs+= 10;
-    msTensLabel.textContent = (totalMs / 10) % 10;
-    msHundredsLabel.textContent = Math.floor((totalMs / 100) % 10);
-    secondsLabel.textContent = Math.floor((totalMs / 1000) % 10);
-    secondTensLabel.textContent = Math.floor(totalMs / 10000);
+function countUp() {
+    const timer = setInterval(() => {
+        totalMs+= 10;
+        msTensLabel.textContent = (totalMs / 10) % 10;
+        msHundredsLabel.textContent = Math.floor((totalMs / 100) % 10);
+        secondsLabel.textContent = Math.floor((totalMs / 1000) % 10);
+        secondTensLabel.textContent = Math.floor(totalMs / 10000);
+    
+        if (totalMs === 10000) {
+            timerDigits.forEach(digit => digit.style.color = 'red');
+            clearInterval(timer);
+        }
+    }, 10);
+}
 
-    if (totalMs === 10000) {
-        timerDigits.forEach(digit => digit.style.color = 'red');
-        clearInterval(timer);
-    }
-}, 10);
+// const timer = setInterval(() => {
+//     totalMs+= 10;
+//     msTensLabel.textContent = (totalMs / 10) % 10;
+//     msHundredsLabel.textContent = Math.floor((totalMs / 100) % 10);
+//     secondsLabel.textContent = Math.floor((totalMs / 1000) % 10);
+//     secondTensLabel.textContent = Math.floor(totalMs / 10000);
+
+//     if (totalMs === 10000) {
+//         timerDigits.forEach(digit => digit.style.color = 'red');
+//         clearInterval(timer);
+//     }
+// }, 10);
+
+let btn = document.createElement('button');
+btn.textContent = 'start';
+btn.setAttribute("onclick", "countUp()");
+
+document.body.appendChild(btn);
+
+console.log(btn);
 
 // let timer = function() {
 //     totalMs+= 10;
