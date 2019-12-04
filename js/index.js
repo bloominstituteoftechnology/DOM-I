dcf8382207fd70a3b09cb46383e2e89712c9fbe2
Allan V. Pimble, Sr <allan.pimble@gmail.com>
2019-12-04 18:36:23 -0500

    MVP completed

--- js/index.js
+++ js/index.js
@@ -46,3 +46,67 @@ logo.setAttribute('src', siteContent["nav"]["img-src"])
 
 const domIsAwesome = document.querySelector('.cta-text > h1');
 domIsAwesome.innerHTML = `DOM <br> Is <br> Awesome`
+
+
+// Header
+const theNav    = document.querySelectorAll('nav a')
+const ctaH1     = document.querySelector('.cta-text h1');
+const ctaButton = document.querySelector('.cta-text button');
+const ctaImg    = document.querySelector('#cta-img')
+
+// Top Content
+const topContentH4 = Array.from(document.querySelectorAll('.top-content .text-content h4'));
+const topContentP  = Array.from(document.querySelectorAll('.top-content .text-content p'));
+
+// Bottom Content
+const bottomContentH4 = Array.from(document.querySelectorAll('.bottom-content .text-content h4'));
+const bottomContentP  = Array.from(document.querySelectorAll('.bottom-content .text-content p'));
+
+// Middle Image
+const middleImg = document.querySelector('#middle-img')
+
+// Contact
+const contactH4 = document.querySelectorAll('.contact h4')
+const contactP = document.querySelectorAll('.contact p')
+
+// Footer
+const footer = document.querySelector('footer');
+
+// header nav
+theNav[0].textContent = siteContent['nav']['nav-item-1'];
+theNav[1].textContent = siteContent['nav']['nav-item-2'];
+theNav[2].textContent = siteContent['nav']['nav-item-3'];
+theNav[3].textContent = siteContent['nav']['nav-item-4'];
+theNav[4].textContent = siteContent['nav']['nav-item-5'];
+theNav[5].textContent = siteContent['nav']['nav-item-6'];
+
+// class="cta"
+ctaH1.textContent = siteContent['cta']['h1'];
+ctaButton.textContent = siteContent['cta']['button'];
+ctaImg.src = siteContent['cta']['img-src']
+
+// class="top-content"
+topContentH4[0].textContent = siteContent['main-content']['features-h4']
+topContentP[0].textContent  = siteContent['main-content']['features-content']
+topContentH4[1].textContent = siteContent['main-content']['about-h4']
+topContentP[1].textContent  = siteContent['main-content']['about-content']
+
+// middle-img-src"
+middleImg.src = siteContent['main-content']['middle-img-src']
+
+// class="bottom-content"
+bottomContentH4[0].textContent = siteContent['main-content']['services-h4']
+bottomContentP[0].textContent  = siteContent['main-content']['services-content']
+bottomContentH4[1].textContent = siteContent['main-content']['product-h4']
+bottomContentP[1].textContent  = siteContent['main-content']['product-content']
+bottomContentH4[2].textContent = siteContent['main-content']['vision-h4']
+bottomContentP[2].textContent  = siteContent['main-content']['vision-content']
+
+//  class="contact"
+contactH4[0].textContent = siteContent['contact']['contact-h4']
+contactP[0].textContent  = siteContent['contact']['address']
+contactP[1].textContent  = siteContent['contact']['phone']
+contactP[2].textContent  = siteContent['contact']['email']
+
+// footer
+footer.textContent = siteContent['footer']['copyright']
