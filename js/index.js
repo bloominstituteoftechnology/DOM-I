3129aeefec091ae86715799a681d13009a02f041
Leo <leo.petkovic83@gmail.com>
2019-12-06 00:46:57 +0100

    finish

--- js/index.js
+++ js/index.js
@@ -40,3 +40,48 @@ const siteContent = {
 // Example: Update the img src for the logo
 let logo = document.getElementById("logo-img");
 logo.setAttribute('src', siteContent["nav"]["img-src"])
+
+let navBar = document.querySelectorAll("nav a");
+navBar[0].text = siteContent.nav["nav-item-1"]
+navBar[1].text = siteContent.nav["nav-item-2"]
+navBar[2].text = siteContent.nav["nav-item-3"]
+navBar[3].text = siteContent.nav["nav-item-4"]
+navBar[4].text = siteContent.nav["nav-item-5"]
+navBar[5].text = siteContent.nav["nav-item-6"]
+
+
+
+let ctaImg = document.getElementById("cta-img");
+ctaImg.setAttribute('src', siteContent["cta"]["img-src"]);
+
+let domIsAwesome = document.querySelector("h1");
+domIsAwesome.textContent = siteContent["cta"]["h1"];
+
+let button = document.querySelector("button");
+button.textContent = siteContent["cta"]["button"];
+
+let contentHead = document.querySelectorAll(".text-content h4");
+contentHead[0].textContent = siteContent["main-content"]["features-h4"];
+contentHead[1].textContent = siteContent["main-content"]["about-h4"];
+contentHead[2].textContent = siteContent["main-content"]["services-h4"];
+contentHead[3].textContent = siteContent["main-content"]["product-h4"];
+contentHead[4].textContent = siteContent["main-content"]["vision-h4"];
+
+
+let paragraphs = document.querySelectorAll(".text-content p");
+paragraphs[0].textContent = siteContent["main-content"]["features-content"];
+paragraphs[1].textContent = siteContent["main-content"]["about-content"];
+paragraphs[2].textContent = siteContent["main-content"]["services-content"];
+paragraphs[3].textContent = siteContent["main-content"]["product-content"];
+paragraphs[4].textContent = siteContent["main-content"]["vision-content"];
+
+let midImg = document.getElementById("middle-img");
+midImg.setAttribute('src', siteContent["main-content"]["middle-img-src"]);
+
+let contactHead = document.querySelector(".contact h4");
+contactHead.textContent = siteContent["contact"]["contact-h4"];
+
+let contact = document.querySelectorAll(".contact p");
+contact[0].textContent = siteContent["contact"]["address"];
+contact[1].textContent = siteContent["contact"]["phone"];
+contact[2].textContent = siteContent["contact"]["email"];
