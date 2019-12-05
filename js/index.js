51f27482fb403b78de8b4c33b1b5fa75d730b461
Ahmad Wali Sayadi <sayadi@Ahmads-MacBook-Pro.local>
2019-12-05 13:08:54 -0800

    project- completed

--- js/index.js
+++ js/index.js
@@ -41,6 +41,8 @@ const siteContent = {
 const logoImg = document.getElementById("logo-img");
 logoImg.src = siteContent.nav["img-src"];
 
+//  navigation section 
+
 const navItems = document.querySelectorAll('nav a');
 navItems[0].text = siteContent.nav["nav-item-1"];
 navItems[1].text = siteContent.nav["nav-item-2"];
@@ -48,6 +50,24 @@ navItems[2].text = siteContent.nav["nav-item-3"];
 navItems[3].text = siteContent.nav["nav-item-4"];
 navItems[4].text = siteContent.nav["nav-item-5"];
 navItems[5].text = siteContent.nav["nav-item-6"];
+
+navItems.forEach(element =>{
+  element.style.color ="#15ad3d";
+})
+
+const newNav = document.querySelector('nav');
+let homeNav = document.createElement('a');
+homeNav.innerHTML = "Home";
+newNav.prepend(homeNav);
+homeNav.style.color ="#15ad3d";
+ 
+let logNav = document.createElement('a');
+logNav.innerHTML = "Log";
+newNav.append(logNav);
+logNav.style.color ="#15ad3d";
+
+
+
 
 
 const ctaText = document.querySelector(".cta h1");
@@ -63,4 +83,36 @@ ctaImg.src = siteContent.cta["img-src"];
 
 
 const h4Tag = document.querySelectorAll('.text-content h4');
-h4Tag.innerText = siteContent.main-content["features-h4"];
+h4Tag[0].innerText = siteContent["main-content"]["features-h4"];
+h4Tag[1].innerText = siteContent["main-content"]["about-h4"];
+h4Tag[2].innerText = siteContent["main-content"]["services-h4"];
+h4Tag[3].innerText = siteContent["main-content"]["product-h4"];
+h4Tag[4].innerText = siteContent["main-content"]["vision-h4"];
+
+
+const pTag = document.querySelectorAll('.text-content p');
+pTag[0].innerHTML = siteContent["main-content"]["features-content"];
+pTag[1].innerHTML = siteContent["main-content"]["about-content"];
+pTag[2].innerHTML = siteContent["main-content"]["services-content"];
+pTag[3].innerHTML = siteContent["main-content"]["product-content"];
+pTag[4].innerHTML = siteContent["main-content"]["vision-content"];
+
+const middleImg = document.querySelector('.middle-img');
+middleImg.src = siteContent["main-content"]["middle-img-src"];
+
+
+const contH4 = document.querySelector('.contact h4');
+contH4.innerHTML = siteContent["contact"]["contact-h4" ];
+
+
+
+const contP = document.querySelectorAll('.contact p');
+contP[0].innerHTML = siteContent["contact"]["address"]
+contP[1].innerHTML = siteContent["contact"]["phone"]
+contP[2].innerHTML = siteContent["contact"]["email"]
+
+const buttomLine = document.querySelector('footer p');
+buttomLine.innerHTML = siteContent["footer"]["copyright" ]
+
+
+
