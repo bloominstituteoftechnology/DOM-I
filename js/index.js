f5697c9a8b1fb5e9533b2ab10a86a71d739e5e23
michelle-hey <miscmiche@gmail.com>
2019-12-04 20:51:31 -0500

    Inserted text for nav links

--- js/index.js
+++ js/index.js
@@ -8,11 +8,13 @@ const siteContent = {
     "nav-item-6": "Contact",
     "img-src": "img/logo.png"
   },
+
   "cta": {
     "h1": "DOM Is Awesome",
     "button": "Get Started",
     "img-src": "img/header-img.png"
   },
+
   "main-content": {
     "features-h4":"Features",
     "features-content": "Features content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.",
@@ -26,17 +28,52 @@ const siteContent = {
     "vision-h4":"Vision",
     "vision-content": "Vision content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.",
   },
+
   "contact": {
     "contact-h4" : "Contact",
     "address" : "123 Way 456 Street Somewhere, USA",
     "phone" : "1 (888) 888-8888",
     "email" : "sales@greatidea.io",
   },
+
   "footer": {
     "copyright" : "Copyright Great Idea! 2018"
   },
 };
 
+// For HEADER nav
+let logo = document.getElementById("logo-img");
+logo.setAttribute('src', siteContent["nav"]["img-src"])
+
+let links = document.getElementsByTagName("a");
+
+links[0].innerHTML = siteContent["nav"]["nav-item-1"]
+
+
+links[1].innerHTML = siteContent["nav"]["nav-item-2"]
+
+links[2].innerHTML = siteContent["nav"]["nav-item-3"]
+
+links[3].innerHTML = siteContent["nav"]["nav-item-4"]
+
+links[4].innerHTML = siteContent["nav"]["nav-item-5"]
+
+links[5].innerHTML = siteContent["nav"]["nav-item-6"]
+
+
 // Example: Update the img src for the logo
 let logo = document.getElementById("logo-img");
 logo.setAttribute('src', siteContent["nav"]["img-src"])
+
+
+// Section class cta text and button
+
+const button = 
+
+// do I use cta the class or do I need to make a new name for a new variable
+
+// in CTA section class
+// insert text in h1 that DOM Is Awesome and edit the button
+
+// Do we need to use siteContent to access anything in the HTML?
+
