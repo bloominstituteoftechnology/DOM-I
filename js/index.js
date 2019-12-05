74717c84f3834f1ad1d803b5b36acf12b790b980
williamschwindt <33535511+williamschwindt@users.noreply.github.com>
2019-12-04 17:59:15 -0800

    project complete

--- js/index.js
+++ js/index.js
@@ -40,3 +40,50 @@ const siteContent = {
 // Example: Update the img src for the logo
 let logo = document.getElementById("logo-img");
 logo.setAttribute('src', siteContent["nav"]["img-src"])
+
+
+let navBtns = document.querySelectorAll('a');
+navBtns[0].textContent = siteContent.nav["nav-item-1"];
+navBtns[1].textContent = siteContent.nav["nav-item-2"];
+navBtns[2].textContent = siteContent.nav["nav-item-3"];
+navBtns[3].textContent = siteContent.nav["nav-item-4"];
+navBtns[4].textContent = siteContent.nav["nav-item-5"];
+navBtns[5].textContent = siteContent.nav["nav-item-6"];
+
+document.querySelector('h1').textContent = siteContent.cta['h1'];
+document.querySelector('button').textContent = siteContent.cta['button'];
+document.querySelector("#cta-img").src = siteContent.cta['img-src'];
+
+document.querySelectorAll('.top-content .text-content h4')[0].textContent = siteContent['main-content']['features-h4'];
+document.querySelectorAll('.top-content .text-content p')[0].textContent = siteContent['main-content']['features-content'];
+document.querySelectorAll('.top-content .text-content h4')[1].textContent = siteContent['main-content']['about-h4'];
+document.querySelectorAll('.top-content .text-content p')[1].textContent = siteContent['main-content']['about-content'];
+
+document.querySelector('.middle-img').src = siteContent["main-content"]["middle-img-src"];
+
+document.querySelectorAll('.bottom-content .text-content h4')[0].textContent = siteContent['main-content']['services-h4'];
+document.querySelectorAll('.bottom-content .text-content p')[0].textContent = siteContent['main-content']['services-content'];
+document.querySelectorAll('.bottom-content .text-content h4')[1].textContent = siteContent['main-content']['product-h4'];
+document.querySelectorAll('.bottom-content .text-content p')[1].textContent = siteContent['main-content']['product-content'];
+document.querySelectorAll('.bottom-content .text-content h4')[2].textContent = siteContent['main-content']['vision-h4'];
+document.querySelectorAll('.bottom-content .text-content p')[2].textContent = siteContent['main-content']['vision-content'];
+
+
+document.querySelector('.contact h4').textContent = siteContent.contact['contact-h4'];
+
+document.querySelectorAll('.contact p')[0].textContent = siteContent.contact['address'];
+document.querySelectorAll('.contact p')[1].textContent = siteContent.contact['phone'];
+document.querySelectorAll('.contact p')[2].textContent = siteContent.contact['email'];
+
+
+document.querySelector('footer p').textContent = siteContent.footer.copyright;
+
+
+let newAnchorFirst = document.createElement('a');
+newAnchorFirst.textContent = 'Team';
+let newAnchorLast = document.createElement('a');
+newAnchorLast.textContent = 'Sign In';
+document.querySelector('nav').appendChild(newAnchorFirst);
+document.querySelector('nav').prepend(newAnchorLast);
+
+document.querySelectorAll('nav a').forEach(el => el.style.color = 'green');
