9a0f12e987ed94167db7315f3c56ca29a628d58c
Tyler Alsop <tylerjalsop@gmail.com>
2019-12-04 19:57:48 -0700

    Fixed syntax by adding semi-colons

--- js/index.js
+++ js/index.js
@@ -43,61 +43,61 @@ logo.setAttribute('src', siteContent["nav"]["img-src"])
 
 //Elements by Class
 
-const containerCl = document.getElementsByClassName('container')
+const containerCl = document.getElementsByClassName('container');
 
-const logoCl = document.getElementsByClassName('logo')
+const logoCl = document.getElementsByClassName('logo');
 
-const ctaCl = document.getElementsByClassName('cta')
+const ctaCl = document.getElementsByClassName('cta');
 
-const ctatextCl = document.getElementsByClassName('cta-text')
+const ctatextCl = document.getElementsByClassName('cta-text');
 
-const maincontentCl = document.getElementsByClassName('main-content')
+const maincontentCl = document.getElementsByClassName('main-content');
 
-const topcontentCl = document.getElementsByClassName('top-content')
+const topcontentCl = document.getElementsByClassName('top-content');
 
-const textcontentCl = document.getElementsByClassName('text-content')
+const textcontentCl = document.getElementsByClassName('text-content');
 
-const middleimgCl = document.getElementsByClassName('middle-img')
+const middleimgCl = document.getElementsByClassName('middle-img');
 
-const bottomcontentCl = document.getElementsByClassName('bottom-content')
+const bottomcontentCl = document.getElementsByClassName('bottom-content');
 
-const contactCl = document.getElementsByClassName('contact')
+const contactCl = document.getElementsByClassName('contact');
 
 //Elements by ID
 
-const logoimgId = document.getElementById('logo-img')
+const logoimgId = document.getElementById('logo-img');
 
-const ctaimgId = document.getElementById('cta-img')
+const ctaimgId = document.getElementById('cta-img');
 
-const middleimgId = document.getElementById('middle-img')
+const middleimgId = document.getElementById('middle-img');
 
 //Query Selectors - Elements
 
-const header = document.querySelector('header')
+const header = document.querySelector('header');
 
-const nav = document.querySelector('nav')
+const nav = document.querySelector('nav');
 
-const a = document.querySelector('a')
+const a = document.querySelector('a');
 
-const section = document.querySelector('section')
+const section = document.querySelector('section');
 
-const div = document.querySelector('div')
+const div = document.querySelector('div');
 
 //Query Selectors - cta
 
-const ctaH1 = document.querySelector('.cta-text h1')
+const ctaH1 = document.querySelector('.cta-text h1');
 
-const ctaButton = document.querySelector('.cta-text button')
+const ctaButton = document.querySelector('.cta-text button');
 
-const ctaImg = document.querySelector('.cta img')
+const ctaImg = document.querySelector('.cta img');
 
 //Query Selectors - main-content
 
-const mainContent = document.querySelectorAll('.main-content')
+const mainContent = document.querySelectorAll('.main-content');
 
-const mainContentH4s = document.querySelectorAll('.main-content h4')
+const mainContentH4s = document.querySelectorAll('.main-content h4');
 
-const mainContentPs = document.querySelectorAll('.main-content p')
+const mainContentPs = document.querySelectorAll('.main-content p');
 
 const mainMiddleImg = document.getElementById("middle-img");
 
@@ -112,48 +112,47 @@ const footerP = document.querySelector('footer p');
 //////////////////////ADDING SITE CONTENT///////////////////////////
 
 //Nav Bar
-nav.children[0].textContent = siteContent['nav']['nav-item-1']
-nav.children[1].textContent = siteContent['nav']['nav-item-2']
-nav.children[2].textContent = siteContent['nav']['nav-item-3']
-nav.children[3].textContent = siteContent['nav']['nav-item-4']
-nav.children[4].textContent = siteContent['nav']['nav-item-5']
-nav.children[5].textContent = siteContent['nav']['nav-item-6']
+nav.children[0].textContent = siteContent['nav']['nav-item-1'];
+nav.children[1].textContent = siteContent['nav']['nav-item-2'];
+nav.children[2].textContent = siteContent['nav']['nav-item-3'];
+nav.children[3].textContent = siteContent['nav']['nav-item-4'];
+nav.children[4].textContent = siteContent['nav']['nav-item-5'];
+nav.children[5].textContent = siteContent['nav']['nav-item-6'];
 
 //cta
 
-ctaH1.textContent = siteContent['cta']['h1']
-ctaH1.style.width = '42%'
+ctaH1.textContent = siteContent['cta']['h1'];
+ctaH1.style.width = '42%';
 
-
-ctaButton.textContent = siteContent['cta']['button']
+ctaButton.textContent = siteContent['cta']['button'];
 ctaButton.style.alignItem = 'center';
 
-ctaImg.setAttribute('src', siteContent["cta"]["img-src"])
+ctaImg.setAttribute('src', siteContent["cta"]["img-src"]);
 
 //main-content
 
-mainContentH4s[0].textContent = siteContent['main-content']["features-h4"]
-mainContentH4s[1].textContent = siteContent['main-content']["about-h4"]
-mainContentH4s[2].textContent = siteContent['main-content']["services-h4"]
-mainContentH4s[3].textContent = siteContent['main-content']["product-h4"]
-mainContentH4s[4].textContent = siteContent['main-content']["vision-h4"]
+mainContentH4s[0].textContent = siteContent['main-content']["features-h4"];
+mainContentH4s[1].textContent = siteContent['main-content']["about-h4"];
+mainContentH4s[2].textContent = siteContent['main-content']["services-h4"];
+mainContentH4s[3].textContent = siteContent['main-content']["product-h4"];
+mainContentH4s[4].textContent = siteContent['main-content']["vision-h4"];
 
-mainContentPs[0].textContent = siteContent['main-content']["features-content"]
-mainContentPs[1].textContent = siteContent['main-content']["about-content"]
-mainContentPs[2].textContent = siteContent['main-content']["services-content"]
-mainContentPs[3].textContent = siteContent['main-content']["product-content"]
-mainContentPs[4].textContent = siteContent['main-content']["vision-content"]
+mainContentPs[0].textContent = siteContent['main-content']["features-content"];
+mainContentPs[1].textContent = siteContent['main-content']["about-content"];
+mainContentPs[2].textContent = siteContent['main-content']["services-content"];
+mainContentPs[3].textContent = siteContent['main-content']["product-content"];
+mainContentPs[4].textContent = siteContent['main-content']["vision-content"];
 
-mainMiddleImg.setAttribute('src', siteContent["main-content"]["middle-img-src"])
+mainMiddleImg.setAttribute('src', siteContent["main-content"]["middle-img-src"]);
 
 
 //contact
 
-contact.children[0].textContent = siteContent['contact']['contact-h4']
-contact.children[1].textContent = siteContent['contact']['address']
-contact.children[2].textContent = siteContent['contact']['phone']
-contact.children[3].textContent = siteContent['contact']['email']
+contact.children[0].textContent = siteContent['contact']['contact-h4'];
+contact.children[1].textContent = siteContent['contact']['address'];
+contact.children[2].textContent = siteContent['contact']['phone'];
+contact.children[3].textContent = siteContent['contact']['email'];
 
 //footer
 
-footerP.textContent = siteContent['footer']['copyright']
+footerP.textContent = siteContent['footer']['copyright'];
