09b9d451500ebf12e18a33375a5a4a6389b7c8fd
Allan V. Pimble, Sr <allan.pimble@gmail.com>
2019-12-10 12:52:44 -0500

    Update to index.js
    I corrected the timer so that it runs correctly: 
    It does not run past 10 seconds
    The display turns red at 10 seconds
    The Reset button resets the counter and returns the display color to black

--- stretch-assignment/digital_timer/index.js
+++ stretch-assignment/digital_timer/index.js
@@ -1,24 +1,47 @@
-const one    = document.getElementById("secondTens")
-const two    = document.getElementById("secondOnes")
-const three  = document.getElementById("msHundreds")
-const four   = document.getElementById("msTens")
-const digits = document.querySelectorAll(".digit")
-const body = document.body;
+const one    = document.getElementById("secondTens");
+const two    = document.getElementById("secondOnes");
+const three  = document.getElementById("msHundreds");
+const four   = document.getElementById("msTens");
+const digits = document.querySelectorAll(".digit");
+const body   = document.body;
 
-let onePlus = 0;
-let twoPlus = 0;
-let threePlus = 0;
-let fourPlus = 0;
+// The values for the timer elements
+let onePlus = 0,
+   twoPlus = 0,
+   threePlus = 0,
+   fourPlus = 0;
+
+// Holds the value for setInterval
+let startId = 0;
+
+// Format the timer
+digits[0].style.marginTop = "20px";
+body.style.flexDirection = "column";
+one.innerText = 0;
+two.innerText = 0;
+three.innerText = 0;
+four.innerText = 0;
+
+// Add the Start and Reset buttons and include some styling
+const startButton = createButton("Start", "green");
+const resetButton = createButton("Reset", "blue");
+const btnGroup = document.createElement("div");
+
+btnGroup.style.width = "50%";
+btnGroup.style.margin = "0 auto";
+btnGroup.appendChild(startButton);
+btnGroup.appendChild(resetButton);
+body.appendChild(btnGroup);
+
+// Add event listeners to the buttons
+startButton.addEventListener("click", start);
+resetButton.addEventListener("click", reset);
 
 function reset() {
-   initialize();
-   digits.forEach(digit => digit.style.color = 'black');
-   clearInterval(start);
-   startButton.disabled = false;
-
-}
-
-function initialize() {
+   // Return the timer to its original color
+   digits.forEach(digit => digit.classList.remove("redDigit"));
+   
+   // Reset the numbers and the variables that represent them
    one.innerText = 0;
    two.innerText = 0;
    three.innerText = 0;
@@ -26,49 +49,40 @@ function initialize() {
    onePlus = 0;
    twoPlus = 0;
    threePlus = 0;
-   fourPlus = 0;   
+   fourPlus = 0;
 }
 
-initialize()
-
-digits[0].style.marginTop = "20px"
-body.style.flexDirection = 'column'
-
-
-// Add a button to stop the counter
-function appendButton(caption, color) {
-   const button = document.createElement('button')
+function createButton(caption, color) {
+   const button = document.createElement("button");
    button.innerText = caption;
-   button.style.padding = '10px'
-   button.style.margin = '10px'
-   button.style.fontSize = '26px'
-   button.style.width = '45%';
+   button.style.padding = "10px";
+   button.style.margin = "10px";
+   button.style.fontSize = "26px";
+   button.style.width = "45%";
    button.style.color = color;
    button.style.fontWeight = 900;
    return button;
 }
 
-const startButton = appendButton('Start', 'green')
-const resetButton = appendButton('Reset', 'blue')
-const btnGroup = document.createElement('div')
-
-   btnGroup.style.width = '50%'
-   btnGroup.style.margin = '0 auto'
-   btnGroup.appendChild(startButton)
-   btnGroup.appendChild(resetButton)
-   body.appendChild(btnGroup)
-
-// Create a timer
-function start() {
-   window.setInterval(callback, 10)
-   startButton.disabled = true;
+// Allow the timer to count up to 9:99, then hard code the final result
+function finale() {
+   digits.forEach(digit => digit.classList.add("redDigit"));
+   one.innerText = 1;
+   two.innerText = 0;
+   three.innerText = 0;
+   four.innerText = 0;
 }
 
-function callback() {
+// The call to this function increments the last digit of the counter
+// After the last digit reaches nine, the next call resets it to zero,
+//    and carries the 1 to the next digit to its left, and cascades this
+//    this action to the left
+function runTimer() {
    if (fourPlus >= 9) {
       ++threePlus;
       fourPlus = -1;
-   } ++fourPlus;
+   }
+   ++fourPlus;
 
    if (threePlus > 9) {
       ++twoPlus;
@@ -76,27 +90,20 @@ function callback() {
    }
 
    if (twoPlus > 9) {
+      startButton.disabled = false;
+      clearInterval(startId); // Stop the timer
       finale();
       return;
    }
-   
+
    one.innerText = onePlus;
    two.innerText = twoPlus;
    three.innerText = threePlus;
    four.innerText = fourPlus;
 }
 
-startButton.addEventListener('click', start)
-resetButton.addEventListener('click', reset)
-
-function myStopFunction() {
-  clearInterval(start);
-}
-
-function finale() {
-   digits.forEach(digit => digit.style.color = 'red');
-   one.innerText = 1;
-   two.innerText = 0;
-   three.innerText = 0;
-   four.innerText = 0;
+// Initialize the timer
+function start() {
+   startId = window.setInterval(runTimer, 10);
+   startButton.disabled = true;
 }
