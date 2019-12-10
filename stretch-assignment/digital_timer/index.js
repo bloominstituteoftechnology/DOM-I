bf8ef50d6484766731efddf138b63ed6370e8c91
avpimblesr <allan.pimble@gmail.com>
2019-12-09 19:04:48 -0500

    added some to the stretch

--- stretch-assignment/digital_timer/index.js
+++ stretch-assignment/digital_timer/index.js
@@ -1,47 +1,102 @@
-// Variables for the timer
-const secondTens = document.getElementById("secondTens")
-const secondOnes = document.getElementById("secondOnes")
-// const colon      = document.getElementById("colon")
-const msHundreds = document.getElementById("msHundreds")
-const msTens     = document.getElementById("msTens")
-const digits     = document.querySelector('.digits')
+const one    = document.getElementById("secondTens")
+const two    = document.getElementById("secondOnes")
+const three  = document.getElementById("msHundreds")
+const four   = document.getElementById("msTens")
+const digits = document.querySelectorAll(".digit")
+const body = document.body;
 
-// Variables for the button and its container
-const body   = document.body;
-const div    = document.createElement('div')
-const button = document.createElement('button')
-const br     = document.createElement('br')
-const jsMain = document.getElementById('jsMain')
+let onePlus = 0;
+let twoPlus = 0;
+let threePlus = 0;
+let fourPlus = 0;
 
-// Timer actions
-secondTens.innerText = 0;
-secondOnes.innerText = 0;
-msHundreds.innerText = 0;
-msTens.innerText = 0;
+function reset() {
+   initialize();
+   digits.forEach(digit => digit.style.color = 'black');
+   clearInterval(start);
+   startButton.disabled = false;
 
-let tenths = 0, hundredths = 0;
-let tens = 0, hundreds = 0;
-
-const stopWatch = setInterval(timer, 10);
-
-function timer() {
-  // hundredths should count to nine, then pass 1 to tenths
-  (tens < 9) ? ++tens : ++hundreds;
-  msHundreds.innerText = tens;
 }
 
-clearInterval(stopWatch)
-timer();
+function initialize() {
+   one.innerText = 0;
+   two.innerText = 0;
+   three.innerText = 0;
+   four.innerText = 0;
+   onePlus = 0;
+   twoPlus = 0;
+   threePlus = 0;
+   fourPlus = 0;   
+}
 
-// When timer reaches 10 seconds, the digits turn red
-digits.style.color = "red"
+initialize()
 
-// // Button actions
-// button.innerText = "START"
+digits[0].style.marginTop = "20px"
+body.style.flexDirection = 'column'
 
-// // Add a div containing a button below .digits
-// div.appendChild(button)
-// body.appendChild(div)
 
-// // Insert the button in the body above the <script>
-// jsMain.insertAdjacentElement('beforebegin', div)
+// Add a button to stop the counter
+function appendButton(caption, color) {
+   const button = document.createElement('button')
+   button.innerText = caption;
+   button.style.padding = '10px'
+   button.style.margin = '10px'
+   button.style.fontSize = '26px'
+   button.style.width = '45%';
+   button.style.color = color;
+   button.style.fontWeight = 900;
+   return button;
+}
+
+const startButton = appendButton('Start', 'green')
+const resetButton = appendButton('Reset', 'blue')
+const btnGroup = document.createElement('div')
+
+   btnGroup.style.width = '50%'
+   btnGroup.style.margin = '0 auto'
+   btnGroup.appendChild(startButton)
+   btnGroup.appendChild(resetButton)
+   body.appendChild(btnGroup)
+
+// Create a timer
+function start() {
+   window.setInterval(callback, 10)
+   startButton.disabled = true;
+}
+
+function callback() {
+   if (fourPlus >= 9) {
+      ++threePlus;
+      fourPlus = -1;
+   } ++fourPlus;
+
+   if (threePlus > 9) {
+      ++twoPlus;
+      threePlus = 0;
+   }
+
+   if (twoPlus > 9) {
+      finale();
+      return;
+   }
+   
+   one.innerText = onePlus;
+   two.innerText = twoPlus;
+   three.innerText = threePlus;
+   four.innerText = fourPlus;
+}
+
+startButton.addEventListener('click', start)
+resetButton.addEventListener('click', reset)
+
+function myStopFunction() {
+  clearInterval(start);
+}
+
+function finale() {
+   digits.forEach(digit => digit.style.color = 'red');
+   one.innerText = 1;
+   two.innerText = 0;
+   three.innerText = 0;
+   four.innerText = 0;
+}
