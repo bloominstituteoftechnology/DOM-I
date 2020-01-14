8205a71b07e33054303e7d1d61bbd06e9a410e4d
Anthony <chitowncoder@gmail.com>
2020-01-14 15:19:24 -0600

    I have called the titles and will next work on calling the text

--- js/index.js
+++ js/index.js
@@ -86,3 +86,15 @@ button.textContent = siteContent.cta.button;
 button.style.background = "dodgerblue";
 button.style.color = "black";
 
+
+//* THIS IS WHERE I WILL CALL THE TITLES *//
+
+let h4 = document.querySelectorAll("h4");
+h4[0].textContent = siteContent["main-content"]["features-h4"];
+h4[1].textContent = siteContent["main-content"]["about-h4"];
+h4[2].textContent = siteContent["main-content"]["services-h4"];
+h4[3].textContent = siteContent["main-content"]["product-h4"];
+h4[4].textContent = siteContent["main-content"]["vision-h4"];
+h4[5].textContent = siteContent["main-content"]["contact-h4"];
+
+
