74983abe4996394355e0dd204d1b28d4b0b36dcb
argunderson <amanda.r.gunderson@gmail.com>
2020-01-15 20:42:44 -0800

    updated js

--- js/index.js
+++ js/index.js
@@ -40,4 +40,18 @@ const siteContent = {
 // Example: Update the img src for the logo
 let logo = document.getElementById("logo-img");
 logo.setAttribute('src', siteContent["nav"]["img-src"])
-check
+
+const cta = document.querySelector('h1');
+cta.textContent = "DOM IS AWESOME";
+
+const ctaImg = document.getElementById("cta-img");
+Image.src = "logo.png";
+
+
+
+const NavA = document.querySelectorAll("a");
+const Newarr = Array.from(NavA);
+NavA.forEach((a, i) => a.textContent = siteContent.nav[`nav-item-${i + 1}`]);
+
+
+
