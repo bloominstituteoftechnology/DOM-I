91cbdb041efa0270349a739af92d85dbec688d73
Hester Knight <hesterknight@hesters-mbp.mynet>
2020-01-19 18:52:13 +0000

    reattempting timer

--- stretch-assignment/digital_timer/index.js
+++ stretch-assignment/digital_timer/index.js
@@ -1,12 +1,41 @@
 
+
+    //selecting elements 
+    const digitsContainer = document.querySelector('.digits');
+
+    const tens = document.getElementById('secondTens');
+
+    const ones = document.getElementById('secondOnes');
+
+    const colon = document.getElementById('colon');
+
+    const hundreds = document.getElementById('msHundreds');
+
+    const mstens = document.getElementById('msTens');
+
+
+//text content: so digits start at 
+
+tens.textContent = "0";
+ones.textContent = "0";
+hundreds.textContent = "0";
+mstens.textContent = "0";
+
+
+
+
+
+
+
+
 //setting each number on digital timer to 0
 
-let timeSecondTens = 0;
-let timeSecondOnes = 0;
-let timemsHundreds = 0;
-let timemsTens = 0;
+let countUp;
 
-
+// let timeSecondTens = 0;
+// let timeSecondOnes = 0;
+// let timemsHundreds = 0;
+// let timemsTens = 0;
 
 
 //WINDOW 
@@ -14,28 +43,21 @@ let timemsTens = 0;
 
 let windowInterval =  window.setInterval(time, 10); 
 
-//using window's cb function to execute function
+// using window's cb function to execute function
 
 function time() {
-    if(timemsHundreds === 10) {
-        timemsHundreds += 10;
+  
+var rightnow = new Date().getTime();
+
+if (countUp > 0 ) {
+    var miliseconds = rightnow.getMilliseconds();
+     miliseconds +=1;
+}
+
 }
 
 
 
-
-
-const mainelement = document.querySelector('div');
-
-const tens = document.getElementById('secondTens');
-
-const ones = document.getElementById('secondOnes');
-
-const colon = document.getElementById('colon');
-
-const hundreds = document.getElementById('msHundreds');
-
-const mstens = document.getElementById('msTens');
 
 
 
