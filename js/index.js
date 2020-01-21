f7b443fedc541b4ffded67e4f3f862d2d36c8d5f
Amber Robinson <ballewaer@gmail.com>
2020-01-21 15:42:25 -0800

    addded middle img, h4, and p

--- js/index.js
+++ js/index.js
@@ -73,4 +73,22 @@ imgHeader.setAttribute("src", siteContent.cta["img-src"]);
 console.log(getStartedButton);
 
 
+// Image Middle (cta)
 
+
+let mainContentImg = document.getElementById("middle-img")
+mainContentImg.src = siteContent["main-content"]["middle-img-src"];
+
+let h4 = document.querySelectorAll("h4");
+h4[0].textContent = siteContent["main-content"]["features-h4"];
+h4[1].textContent = siteContent["main-content"]["about-h4"];
+h4[2].textContent = siteContent["main-content"]["services-h4"];
+h4[3].textContent = siteContent["main-content"]["product-h4"];
+h4[4].textContent = siteContent["main-content"]["vision-h4"];
+
+let p = document.querySelectorAll("p");
+p[0].textContent = siteContent["main-content"]["features-content"];
+p[1].textContent = siteContent["main-content"]["about-content"];
+p[2].textContent = siteContent["main-content"]["services-content"];
+p[3].textContent = siteContent["main-content"]["product-content"];
+p[4].textContent = siteContent["main-content"]["vision-content"];
