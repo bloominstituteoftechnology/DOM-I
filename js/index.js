5b3719c978ff3a600cab6dcee9283befda754882
Amber Robinson <ballewaer@gmail.com>
2020-01-21 15:24:06 -0800

    Completed Task 1 and 2, button, and cta image

--- js/index.js
+++ js/index.js
@@ -40,3 +40,37 @@ const siteContent = {
 // Example: Update the img src for the logo
 let logo = document.getElementById("logo-img");
 logo.setAttribute('src', siteContent["nav"]["img-src"])
+
+
+// Task 1 and 2; create selectors, and elements
+
+const navBarItems = document.querySelectorAll("a");
+
+navBarItems[0].textContent = siteContent.nav[`nav-item-1`];
+navBarItems[1].textContent = siteContent.nav[`nav-item-2`];
+navBarItems[2].textContent = siteContent.nav[`nav-item-3`];
+navBarItems[3].textContent = siteContent.nav[`nav-item-4`];
+navBarItems[4].textContent = siteContent.nav[`nav-item-5`];
+navBarItems[5].textContent = siteContent.nav[`nav-item-6`];
+
+//h1 text
+
+const h1 = document.querySelector("h1");
+
+h1.textContent = siteContent.cta.h1;
+
+//Button - Get Started
+
+const getStartedButton = document/querySelector("button");
+
+getStartedButton.textContent = siteContent.cta.button;
+
+//Image (cta)
+
+let imgHeader = document.querySelector("#cta-img");
+
+imgHeader.setAttribute("src", siteContent.cta["img-src"]);
+console.log(getStartedButton);
+
+
+
