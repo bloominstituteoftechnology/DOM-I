1284d284b6f058c4db77e9328cd404974cdae239
vanesa-tamu <vahe5495@tamu.edu>
2020-02-10 15:35:45 -0600

    added nav and images

--- js/index.js
+++ js/index.js
@@ -40,3 +40,24 @@ const siteContent = {
 // Example: Update the img src for the logo
 let logo = document.getElementById("logo-img");
 logo.setAttribute('src', siteContent["nav"]["img-src"])
+
+
+const codesnippet = document.getElementById('middle-img');
+codesnippet.setAttribute('src', siteContent["main-content"]["middle-img-src"])
+
+const circleImg = document.getElementById('cta-img');
+circleImg.setAttribute('src', siteContent.cta["img-src"])
+
+const nav = document.querySelectorAll('a'); //returns a nodelist 
+console.log(nav)
+
+nav[0].textContent = siteContent["nav"]["nav-item-1"];
+nav[1].textContent = siteContent["nav"]["nav-item-2"];
+nav[2].textContent = siteContent["nav"]["nav-item-3"];
+nav[3].textContent = siteContent["nav"]["nav-item-4"];
+nav[4].textContent = siteContent["nav"]["nav-item-5"];
+nav[5].textContent = siteContent["nav"]["nav-item-6"];
+
+nav.forEach((green) => {
+  green.style.color = "green"
+})
