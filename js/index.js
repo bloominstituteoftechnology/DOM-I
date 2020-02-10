1344b0f0feeb017bec302011d27dbbcc18e30475
Pete1701 <pete.petrongonas@pirron.one>
2020-02-11 00:45:45 +0100

    MVP

--- js/index.js
+++ js/index.js
@@ -39,4 +39,67 @@ const siteContent = {
 
 // Example: Update the img src for the logo
 let logo = document.getElementById("logo-img");
-logo.setAttribute('src', siteContent["nav"]["img-src"])
+logo.setAttribute('src', 'img/logo.png')
+
+let navMenu = document.querySelectorAll('a');
+let navM = Array.from(navMenu);
+navM[0].textContent = 'Services'
+navM[1].textContent = 'Product'
+navM[2].textContent = 'Vision'
+navM[3].textContent = 'Features'
+navM[4].textContent = 'About'
+navM[5].textContent = 'Contact'
+
+let navigator = document.querySelector('nav');
+let add1 = document.createElement('a');
+add1.textContent = 'Home'
+add1.style.color = 'green'
+let add2 = document.createElement('a');
+add2.textContent = 'Help'
+add2.style.color = 'green'
+navigator.prepend(add1)
+navigator.appendChild(add2)
+
+navM.forEach(i => {
+  i.style.color = 'green';
+});
+
+let ctaContent = document.querySelector('.cta h1');
+ctaContent.innerHTML = 'DOM<br>Is<br>Awesome';
+
+let ctaButton = document.querySelector('.cta button');
+ctaButton.textContent = "Get Started";
+
+let ctaImg = document.getElementById('cta-img');
+ctaImg.setAttribute('src', 'img/header-img.png')
+
+let mainTitle = document.querySelectorAll('.text-content h4');
+let mainT = Array.from(mainTitle);
+mainT[0].textContent = 'Features'
+mainT[1].textContent = 'About'
+mainT[2].textContent = 'Services'
+mainT[3].textContent = 'Product'
+mainT[4].textContent = 'Vision'
+
+let mainContent = document.querySelectorAll('.text-content p');
+let mainCont = Array.from(mainContent);
+mainCont[0].textContent = 'Features content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.'
+mainCont[1].textContent = 'About content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.'
+mainCont[2].textContent = 'Services content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.'
+mainCont[3].textContent = 'Product content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.'
+mainCont[4].textContent = 'Vision content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.'
+
+let midImg = document.getElementById("middle-img");
+midImg.setAttribute('src', 'img/mid-page-accent.jpg')
+
+let contactTitle = document.querySelector('.contact h4');
+contactTitle.textContent = 'Contact'
+
+let contactContent = document.querySelectorAll('.contact p');
+let contactCont = Array.from(contactContent);
+contactCont[0].innerHTML = "123 Way 456 Street<br>Somewhere, USA";
+contactCont[1].textContent = "1 (888) 888-8888";
+contactCont[2].textContent = "sales@greatidea.io";
+
+let footerCont = document.querySelector('footer p');
+footerCont.textContent = 'Copyright Great Idea! 2018';
