2d16905bf46cc36d9277f78863babdc31e9e761b
AhmedBarre10 <barreahmed600@gmail.com>
2020-02-10 20:10:07 -0500

    DOM Project

--- js/index.js
+++ js/index.js
@@ -43,37 +43,44 @@ logo.setAttribute('src', siteContent["nav"]["img-src"])
 const change1 = document.querySelector('#logo-img').src = 'file:///C:/Users/16143/desktop/Ahmed/DOM-I/img/logo.png';
 const change2 = document.querySelector('#cta-img').src = 'file:///C:/Users/16143/desktop/Ahmed/DOM-I/img/header-img.png';
 const change3 = document.querySelector('#middle-img').src = 'file:///C:/Users/16143/desktop/Ahmed/DOM-I/img/mid-page-accent.jpg';
+const navSetup = document.querySelectorAll('nav a');
+navSetup.forEach(( currentValue, index) => {
+currentValue.textContent = siteContent.nav[`nav-item-${index + 1}`];
+});
 
-const Services = document.querySelectorAll('nav a ')[0].innerHTML = ' Services';
-const Product = document.querySelectorAll('nav a ')[1].innerHTML = ' Product';
-const Vision = document.querySelectorAll('nav a')[2].innerHTML = ' Vision';
-const Features = document.querySelectorAll('nav a')[3].innerHTML = ' Features';
-const About = document.querySelectorAll('nav a')[4].innerHTML = ' About';
-const Contact = document.querySelectorAll('nav a')[5].innerHTML = ' Contact';
 
 
 const h1 = document.querySelector('.cta-text h1').innerHTML ="DOM<br>Is<br>Awesome"
 const button = document.querySelector('.cta-text button').innerHTML =" Get Started "
-let Featuresh4 = document.querySelectorAll('.top-content .text-content h4 ')[0].innerHTML +='Features';
-let Featuresp = document.querySelectorAll('.top-content .text-content p  ')[0].innerHTML +='Features content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.';
-let Abouth4 = document.querySelectorAll('.top-content .text-content h4 ')[1].innerHTML +='About';
-let Aboutp = document.querySelectorAll('.top-content .text-content p  ')[1].innerHTML +='About content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.';
 
+let topContentH4 = document.querySelectorAll('.main-content .top-content H4');
+topContentH4[0].textContent = siteContent['main-content']['features-h4']
+topContentH4[1].textContent = siteContent['main-content']['about-h4']
 
-let Servicesh4 = document.querySelectorAll('.bottom-content .text-content h4 ')[0].innerHTML +='Services';
-let Servicesp = document.querySelectorAll('.bottom-content .text-content p  ')[0].innerHTML +='Services content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.';
+let topContentP = document.querySelectorAll('.main-content .top-content p');
+topContentP[0].textContent = siteContent['main-content']['features-content']
+topContentP[1].textContent = siteContent['main-content']['about-content']
 
-let Producth4 = document.querySelectorAll('.bottom-content .text-content h4 ')[1].innerHTML +='Product';
-let Productp = document.querySelectorAll('.bottom-content .text-content p  ')[1].innerHTML +='Product content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.';
+let bottomContentH4 = document.querySelectorAll('.main-content .bottom-content h4');
+bottomContentH4[0].textContent = siteContent['main-content']['services-h4']
+bottomContentH4[1].textContent = siteContent['main-content']['product-h4']
+bottomContentH4[2].textContent = siteContent['main-content']['vision-h4']
 
-let Visionh4 = document.querySelectorAll('.bottom-content .text-content h4 ')[2].innerHTML +='Vision';
-let Visionp = document.querySelectorAll('.bottom-content .text-content p  ')[2].innerHTML +='Vision content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.';
+let bottomContentP = document.querySelectorAll('.main-content .bottom-content p');
+bottomContentP[0].textContent = siteContent['main-content']['services-content']
+bottomContentP[1].textContent = siteContent['main-content']['product-content']
+bottomContentP[2].textContent = siteContent['main-content']['vision-content']
 
-let Contacth4 = document.querySelectorAll('.contact h4')[0].innerHTML +='Contact';
-let ContactAddress = document.querySelectorAll('.contact p ')[0].innerHTML +='123 Way 456 Street<br> Somewhere, USA';
-let ContactPhone = document.querySelectorAll('.contact p ')[1].innerHTML +='1 (888) 888-8888';
-let ContactEmail = document.querySelectorAll('.contact p ')[2].innerHTML +='sales@greatidea.io';
-let Footerp = document.querySelectorAll('footer p')[0].innerHTML +='Copyright Great Idea! 2018';
+const contactHeader = document.querySelectorAll('.contact h4');
+contactHeader[0].innerText = siteContent.contact['contact-h4'];
+
+const contactContent = document.querySelectorAll('.contact p');
+contactContent[0].textContent = siteContent.contact.address
+contactContent[1].textContent = siteContent.contact.phone
+contactContent[2].textContent = siteContent.contact.email
+
+const footer = document.querySelector('footer');
+footer.textContent = siteContent.footer.copyright;
 
 
 document.querySelector("body").addEventListener("click", function(){
@@ -93,23 +100,21 @@ document.querySelector("nav").addEventListener("mouseover", function(){
 document.querySelector(".main-content").addEventListener("mouseover", function(){
   this.style.backgroundColor = '#FF4F58FF';
 });
- document.querySelectorAll('nav a')[0].style.color = "green";
- document.querySelectorAll('nav a')[1].style.color = "green";
- document.querySelectorAll('nav a')[2].style.color = "green";
- document.querySelectorAll('nav a')[3].style.color = "green";
- document.querySelectorAll('nav a')[4].style.color = "green";
- document.querySelectorAll('nav a')[5].style.color = "green";
+let green = document.querySelectorAll("header nav a");
+green.forEach(aGreen => {
+  aGreen.style.color = "green";
+});
 
  // Item Creation Step 1: create a new element
 // Item Creation Step 1: create a new element
 const addEnd= document.createElement("a");
 // Item Creation Step 2: add some content to the newly created div
-addEns.textContent = "Append";
+addEnd.textContent = "Append";
 addEnd.style.color = "green"
 // Item Creation Step 3: prepend or append the element to an existing DOM node on the page
-const navEnd= document.querySelector('nav');
+const navEnd = document.querySelector('nav');
 
-secondaryContent.append(addEnd);
+navEnd.append(addEnd);
 
 
 
@@ -118,6 +123,5 @@ const addBegining = document.createElement("a");
 addBegining.textContent = "Prepend";
 addBegining.style.color = "green"
 // Item Creation Step 3: prepend or append the element to an existing DOM node on the page
-const navBegining = document.querySelector('nav');
-
-pre.prepend(addBegining);
+const navStart = document.querySelector('nav');
+navStart.prepend(addBegining);
