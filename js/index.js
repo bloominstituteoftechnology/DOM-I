f8ba8b0ac25b359660b560f4e1ae285eaa1e7ed4
Alex <alexcouts18@gmail.com>
2020-02-13 01:39:37 -0500

    completed two of three JS challenges in assignment

--- js/index.js
+++ js/index.js
@@ -40,3 +40,70 @@ const siteContent = {
 // Example: Update the img src for the logo
 let logo = document.getElementById("logo-img");
 logo.setAttribute('src', siteContent["nav"]["img-src"])
+
+
+
+
+// ## Task 1: Create selectors to point your data into elements
+// * [ ] Create selectors by using any of the DOM element's methods
+// * [ ] Note that IDs have been used on all images.  Use the IDs to update src path content
+
+let links = document.querySelectorAll('a');
+
+// links[0].textContent = siteContent["nav"]["nav-item-1"];
+// links[1].textContent = siteContent["nav"]["nav-item-2"];
+// links[2].textContent = siteContent["nav"]["nav-item-3"];
+// links[3].textContent = siteContent["nav"]["nav-item-4"];
+// links[4].textContent = siteContent["nav"]["nav-item-5"];
+// links[5].textContent = siteContent["nav"]["nav-item-6"];
+
+
+links.forEach((element, index) => {
+  element.textContent = siteContent["nav"][`nav-item-${index + 1}`]
+})
+
+
+
+// ## Task 2: Update the HTML with the JSON data
+// * [ ] Remember, NO direct updating of the HTML source is allowed.
+// * [ ] Using your selectors, update the content to match the example file.
+// * [ ] Remember to update the src attributes on images
+
+let subheaders = document.querySelectorAll('h4');
+// subheadersArray = Array.from(subheaders);
+subheaders.item(0).textContent = siteContent["main-content"]["features-h4"];
+subheaders.item(1).textContent = siteContent["main-content"]["about-h4"];
+subheaders.item(2).textContent = siteContent["main-content"]["services-h4"];
+subheaders.item(3).textContent = siteContent["main-content"]["product-h4"];
+subheaders.item(4).textContent = siteContent["main-content"]["vision-h4"];
+
+
+
+
+
+
+// ## Task 3: Add new content
+// * [ ] Change the color of the navigation text to be green.
+// * [ ] Utilize `.appendChild()` and `.prepend()` to add two new items to the navigation system. You can call them whatever you want.
+// * [ ] Check your work by looking at the [original html](original.html) in the browser
+
+links.forEach( (element) => {
+ element.style.color = "green";
+})
+
+
+
+
+// ## Stretch Goals
+// * [ ] Update styles throughout the page as you see fit.  Study what happens when you updated the DOM using style in JavaScript.  
+// * [ ] Study tomorrow's lesson on events and try to integrate a button that can update content on the site with a click of a button.  You could build a similar data object with new values to help you test the click event.
+
+
+
+
+
+
+
+
+// ## Stretch Project: Digital Timer
+// This project is heavier on logic but employs some DOM manipulation to achieve it's goals.  Go check it out here: [stretch assignment](stretch-assignment) and see how far you can get. 
