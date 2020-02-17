44501b9c6d8b54eff88ba0a7ed726f3de511a1e7
Kim Ly Buck <kim.3993@gmail.com>
2020-02-16 22:08:40 -0600

    added comments, clean up code

--- stretch-assignment/digital_timer/index.js
+++ stretch-assignment/digital_timer/index.js
@@ -1,23 +1,23 @@
+//calculate each number for timer
 const getTimeParts = elapseTime => {
     const secondTens = Math.floor(elapseTime/10000);
     elapseTime -= secondTens * 10000;
-
     const second = Math.floor(elapseTime / 1000);
     elapseTime -= second * 1000;
-
     const msHundreds = Math.floor(elapseTime / 100);
     elapseTime -= msHundreds * 100;
-
     const msTens = Math.floor(elapseTime /10);
-
     return [secondTens,second,msHundreds,msTens];
 };
+
+//get elements from DOM
 const secondTensEl = document.getElementById('secondTens');
 const secondOnesEl = document.getElementById('secondOnes');
 const msHundsEl = document.getElementById('msHundreds');
 const msTensEl = document.getElementById('msTens');
-
 let timePassed = 0;
+
+//update timer every 10ms
 const interval = setInterval(()=>{
     timePassed += 10;
     const [secondTens,second,msHundreds,msTens] = getTimeParts(timePassed)
