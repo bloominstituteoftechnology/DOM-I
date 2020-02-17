6ef8f11ef0aeb7d32850732c11d73d909a485f6b
Lindsey Henager <lindseyhenager@lindseys-air.attlocal.net>
2020-02-16 17:56:33 -0800

    added contact info and footer

--- js/index.js
+++ js/index.js
@@ -57,3 +57,39 @@ welcomeImage.setAttribute('src', siteContent["cta"]["img-src"])
 
 let ctaButton = document.querySelector('button');
 ctaButton.textContent = siteContent["cta"]["button"];
+
+
+let mainHeadings = document.querySelectorAll('h4');
+console.log(mainHeadings);
+
+mainHeadings[0].textContent = siteContent['main-content']['features-h4'];
+mainHeadings[1].textContent = siteContent['main-content']['about-h4'];
+mainHeadings[2].textContent = siteContent['main-content']['services-h4'];
+mainHeadings[3].textContent = siteContent['main-content']['product-h4'];
+mainHeadings[4].textContent = siteContent['main-content']['vision-h4'];
+
+let featuresParagraphs = document.querySelectorAll('p');
+
+featuresParagraphs[0].textContent = siteContent['main-content']['features-content'];
+featuresParagraphs[1].textContent = siteContent['main-content']['about-content'];
+featuresParagraphs[2].textContent = siteContent['main-content']['services-content'];
+featuresParagraphs[3].textContent = siteContent['main-content']['product-content'];
+featuresParagraphs[4].textContent = siteContent['main-content']['vision-content'];
+
+
+let middleImage = document.getElementById('middle-img');
+middleImage.setAttribute('src', siteContent['main-content']['middle-img-src']);
+
+
+let contactHeading = document.querySelectorAll('.contact h4');
+console.log(contactHeading);
+contactHeading[0].textContent = siteContent['contact']['contact-h4'];
+
+let contactInfo = document.querySelectorAll('.contact p');
+
+contactInfo[0].textContent = siteContent['contact']['address'];
+contactInfo[1].textContent = siteContent['contact']['phone'];
+contactInfo[2].textContent = siteContent['contact']['email'];
+
+let footerInfo = document.querySelectorAll('footer p');
+footerInfo[0].textContent = siteContent['footer']['copyright'];
