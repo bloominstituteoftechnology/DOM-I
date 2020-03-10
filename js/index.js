12ce94d1d506b1357d391a7bbc8216bbbddc4a75
Leonard Ayala <leonardayala@Leonards-MBP.hsd1.ct.comcast.net>
2020-03-10 05:37:35 -0400

    finished, no stretch

--- js/index.js
+++ js/index.js
@@ -41,5 +41,78 @@ const siteContent = {
 let logo = document.getElementById("logo-img");
 logo.setAttribute('src', siteContent["nav"]["img-src"])
 
+// Navigation
+let Navigationnn = document.querySelectorAll('a')
 
-tesrting testing tesrting 123
+Navigationnn[0].setAttribute('class', '.nav-item-1')
+Navigationnn[0].innerText = "Service"
+Navigationnn[1].setAttribute('class', '.nav-item-2')
+Navigationnn[1].innerText = "Product"
+Navigationnn[2].setAttribute('class', '.nav-item-3')
+Navigationnn[2].innerText = "Vision"
+Navigationnn[3].setAttribute('class', '.nav-item-4')
+Navigationnn[3].innerText = "Features"
+Navigationnn[4].setAttribute('class', '.nav-item-5')
+Navigationnn[4].innerText = "About"
+Navigationnn[5].setAttribute('class', '.nav-item-6')
+Navigationnn[5].innerText = "Product"
+
+Navigationnn.forEach(link => link.style.color ='purple')
+
+//add two nav items
+const newNavigationnn = document.createElement('a')
+const mainNav = document.querySelector('nav')
+newNavigationnn.innerText = 'Reviews'
+newNavigationnn.href = '#'
+mainNav.prepend(newNavigationnn)
+newNavigationnn.style.color = 'purple'
+
+const newNavigationnn2 = document.createElement('a')
+const mainNav2 = document.querySelector('nav')
+newNavigationnn2.innerText = 'Info'
+newNavigationnn2.href = '#'
+mainNav2.append(newNavigationnn2)
+newNavigationnn2.style.color = 'green'
+
+//main header section
+let mainHeading = document.querySelector('h1')
+mainHeading.textContent = siteContent['cta']['h1']
+
+let mainButton = document.querySelector('button')
+mainButton.innerText = "Get Started"
+
+let logo2 = document.querySelector("#cta-img")
+logo2.setAttribute('src', siteContent["cta"]["img-src"])
+
+//main-content
+let mainContent = document.querySelectorAll('.text-content')
+
+mainContent[0].querySelector('h4').textContent = siteContent['main-content']['features-h4']
+mainContent[0].querySelector('p').textContent = siteContent['main-content']['features-content']
+mainContent[1].querySelector('h4').textContent = siteContent['main-content']['about-h4']
+mainContent[1].querySelector('p').textContent = siteContent['main-content']['about-content']
+mainContent[2].querySelector('h4').textContent = siteContent['main-content']['services-h4']
+mainContent[2].querySelector('p').textContent = siteContent['main-content']['services-content']
+mainContent[3].querySelector('h4').textContent = siteContent['main-content']['product-h4']
+mainContent[3].querySelector('p').textContent = siteContent['main-content']['product-content']
+mainContent[4].querySelector('h4').textContent = siteContent['main-content']['vision-h4']
+mainContent[4].querySelector('p').textContent = siteContent['main-content']['vision-content']
+
+let logo3= document.querySelector("#middle-img")
+logo3.setAttribute('src', siteContent["main-content"]["middle-img-src"])
+
+
+//contact 
+
+let contactInfo = document.querySelector('.contact')
+
+contactInfo.getElementsByTagName('h4')[0].textContent = siteContent["contact"]["contact-h4"]
+contactInfo.getElementsByTagName('p')[0].textContent = siteContent["contact"]["address"]
+contactInfo.getElementsByTagName('p')[1].textContent = siteContent["contact"]["phone"]
+contactInfo.getElementsByTagName('p')[2].textContent = siteContent["contact"]["email"]
+
+//footer
+
+let footer = document.querySelector('footer')
+
+footer.innerText = "Copyright Great Idea! 2018"
