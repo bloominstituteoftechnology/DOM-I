9dd36e2dfe729ebae0a84aabd0c9cf8c1d48046f
James Brennan <creative44@Jamess-MacBook-Pro.local>
2020-03-09 20:23:59 -0400

    fix

--- js/index.js
+++ js/index.js
@@ -13,6 +13,10 @@ const siteContent = {
     "button": "Get Started",
     "img-src": "img/header-img.png"
   },
+
+
+
+  
   "main-content": {
     "features-h4":"Features",
     "features-content": "Features content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.",
@@ -38,5 +42,96 @@ const siteContent = {
 };
 
 // Example: Update the img src for the logo
+
+var list = document.getElementsByTagName("cta h1");
+list.innerHTML = "Milk";
+
+
 let logo = document.getElementById("logo-img");
-logo.setAttribute('src', siteContent["nav"]["img-src"])
+logo.setAttribute('src', siteContent["nav"]["img-src"]);
+
+document.querySelector("nav a:nth-of-type(1)").textContent= siteContent["nav"]["nav-item-1"];
+document.querySelector("nav a:nth-of-type(2)").textContent= siteContent["nav"]["nav-item-2"];
+document.querySelector("nav a:nth-of-type(3)").textContent= siteContent["nav"]["nav-item-3"];
+document.querySelector("nav a:nth-of-type(4)").textContent= siteContent["nav"]["nav-item-4"];
+document.querySelector("nav a:nth-of-type(5)").textContent= siteContent["nav"]["nav-item-5"];
+document.querySelector("nav a:nth-of-type(6)").textContent= siteContent["nav"]["nav-item-6"];
+
+
+// new nav items
+
+
+
+
+
+
+
+const newNavItem = document.createElement('a');
+
+newNavItem.textContent = "Link"
+
+
+const addOne = document.querySelector('nav')
+addOne.append(newNavItem)
+
+const newNavItem2 = document.createElement('a');
+
+newNavItem2.textContent = "Link 2"
+
+addOne.prepend(newNavItem2) 
+
+//
+
+document.querySelector
+document.querySelector("#cta-img").setAttribute("src", siteContent["cta"]["img-src"])
+
+document.querySelector ("h1").textContent = siteContent["cta"]["h1"];
+
+document.querySelector ("button").textContent = siteContent["cta"]["button"];
+
+document.querySelector('.middle-img').setAttribute('src', siteContent['main-content']['middle-img-src']);
+
+// 1st row
+
+document.querySelector(".top-content .text-content h4").textContent=siteContent["main-content"]["features-h4"]
+
+document.querySelector(".top-content .text-content p").textContent=siteContent["main-content"]["features-content"]
+
+document.querySelector(".top-content .text-content:nth-of-type(2) h4").textContent=siteContent["main-content"]["about-h4"]
+
+document.querySelector(".top-content .text-content:nth-of-type(2) p").textContent=siteContent["main-content"]["about-content"]
+
+// 2nd row
+
+document.querySelector(".bottom-content .text-content h4").textContent=siteContent["main-content"]["services-h4"]
+
+document.querySelector(".bottom-content .text-content p").textContent=siteContent["main-content"]["services-content"]
+
+document.querySelector(".bottom-content .text-content:nth-of-type(2) h4").textContent=siteContent["main-content"]["product-h4"]
+
+document.querySelector(".bottom-content .text-content:nth-of-type(2) p").textContent=siteContent["main-content"]["product-content"]
+
+document.querySelector(".bottom-content .text-content:nth-of-type(3) h4").textContent=siteContent["main-content"]["vision-h4"]
+
+document.querySelector(".bottom-content .text-content:nth-of-type(3) p").textContent=siteContent["main-content"]["vision-content"]
+
+//footer
+
+document.querySelector(".contact h4").textContent=siteContent["contact"]["contact-h4"];
+
+document.querySelector(".contact p:nth-of-type(1)").textContent=siteContent["contact"]["address"];
+document.querySelector(".contact p:nth-of-type(2)").textContent=siteContent["contact"]["phone"];
+document.querySelector(".contact p:nth-of-type(3)").textContent=siteContent["contact"]["email"];
+
+document.querySelector("footer p").textContent=siteContent["footer"]["copyright"];
+
+
+//nav color
+
+const link = document.querySelectorAll('a');
+link.forEach((item) => {
+  item.style.color = 'green';
+});
+
+
+//
