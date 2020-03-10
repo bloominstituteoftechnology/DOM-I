a3191191d4bf276073723e1cb675e3ff395581c3
fadygouda <fadygouda123@gmail.com>
2020-03-09 23:42:05 -0700

    fixed img tags

--- js/index.js
+++ js/index.js
@@ -39,4 +39,13 @@ const siteContent = {
 
 // Example: Update the img src for the logo
 let logo = document.getElementById("logo-img");
-logo.setAttribute('src', siteContent["nav"]["img-src"])
+logo.src = "img/logo.png"
+
+let ctaImg = document.getElementById("cta-img");
+ctaImg.src = "img/header-img.png"
+
+let middleImg = document.getElementById("middle-img");
+middleImg.src = 'img/mid-page-accent.jpg'
+
+let headline = document.querySelector("h1");
+headline.textContent = "DOM IS AWESOME"
