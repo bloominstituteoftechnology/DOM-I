b81595052b2c71ef1aa7a4008b2a026169d0a1f9
sam-h-hall <sam_h_hall@icloud.com>
2020-03-11 22:19:33 -0500

    finished project

--- js/index.js
+++ js/index.js
@@ -40,3 +40,95 @@ const siteContent = {
 // Example: Update the img src for the logo
 let logo = document.getElementById("logo-img");
 logo.setAttribute('src', siteContent["nav"]["img-src"])
+
+//could you use a switch for this? Or for loop to do this quicker?
+const navSelector = document.querySelectorAll("a");
+
+//one way to do it...
+// navChange[0].textContent = (siteContent["nav"]["nav-item-1"]);
+// navChange[1].textContent = (siteContent["nav"]["nav-item-2"]);
+// navChange[2].textContent = (siteContent["nav"]["nav-item-3"]);
+// navChange[3].textContent = (siteContent["nav"]["nav-item-4"]);
+// navChange[4].textContent = (siteContent["nav"]["nav-item-5"]);
+// navChange[5].textContent = (siteContent["nav"]["nav-item-6"]);
+
+//another... --> make this function reusable elsewhere? add selector arg, obj, propName
+//try using a counter or for loop and .toString
+let navChangeFunc = (index, propNum) => navSelector[index].textContent = (siteContent["nav"]["nav-item-" + propNum]);
+navChangeFunc(0, "1");
+navChangeFunc(1, "2");
+navChangeFunc(2, "3");
+navChangeFunc(3, "4");
+navChangeFunc(4, "5");
+navChangeFunc(5, "6");
+
+
+//bad form to do this as one line?
+//for 'h1', need to add <br>s
+const title = document.querySelector("h1").textContent = (siteContent["cta"]["h1"]);
+// document.querySelector("h1").textContent = ("Dom "+ <br>+ "Is "+ <br> + "Awesome");
+
+const button = document.querySelector("button").textContent = (siteContent["cta"]["button"]);
+
+const mainImage = document.getElementById("cta-img").setAttribute("src", siteContent["cta"]["img-src"]);
+
+//add h4 titles
+const h4Selector = document.querySelectorAll("h4");
+let h4Adder = (index, prop) => h4Selector[index].textContent = (siteContent["main-content"][prop + "-h4"]);
+
+h4Adder(0, "features");
+h4Adder(1, "about");
+h4Adder(2, "services");
+h4Adder(3, "product");
+h4Adder(4, "vision");
+
+h4Selector[5].textContent = (siteContent["contact"]["contact-h4"]);
+
+//add paragraphs
+const pSelector = document.querySelectorAll("p");
+
+let paragraphAdder = (index, prop) => pSelector[index].textContent = (siteContent["main-content"][prop + "-content"]);
+
+paragraphAdder(0, "features");
+paragraphAdder(1, "about");
+paragraphAdder(2, "services");
+paragraphAdder(3, "product");
+paragraphAdder(4, "vision");
+
+pSelector[5].textContent = (siteContent["contact"]["address"]);
+pSelector[6].textContent = (siteContent["contact"]["phone"]);
+pSelector[7].textContent = (siteContent["contact"]["email"]);
+pSelector[8].textContent = (siteContent["footer"]["copyright"]);
+
+
+//add middle image
+const middleImg = document.getElementById("middle-img").setAttribute("src", siteContent["main-content"]["middle-img-src"]);
+
+
+//adding Log In button to nav
+const newLogIn = document.createElement('a');
+newLogIn.textContent = "Log In";
+newLogIn.style.color = "green";
+newLogIn.href = "#";
+
+document.querySelector('nav').append(newLogIn);
+
+//adding Archive button to nav
+const newPortfolio = document.createElement('a');
+newPortfolio.textContent = "Portfolio";
+newPortfolio.style.color = "green";
+newPortfolio.href = "#";
+
+document.querySelector('nav').prepend(newPortfolio);
+
+
+//make nav green
+//could expand this to shorten paragraphAdder, etc.
+function makeGreen(selector) {
+  for (let i = 0; i < selector.length; i++) {
+    selector[i].style.color = "green";
+  }
+}
+
+makeGreen(navSelector);
+
