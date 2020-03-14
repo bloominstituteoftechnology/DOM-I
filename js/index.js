19fc2fdf145133ea739944d4fe0ffb19190bb743
lex-marie790 <lexi.marie790@gmail.com>
2020-03-13 17:55:43 -0700

    ID Selectors created, Content Updated

--- js/index.js
+++ js/index.js
@@ -40,3 +40,80 @@ const siteContent = {
 // Example: Update the img src for the logo
 let logo = document.getElementById("logo-img");
 logo.setAttribute('src', siteContent["nav"]["img-src"])
+
+/**********************************************
+                  NAV BAR LINKS
+ **********************************************/
+
+document.querySelectorAll('a')[0].textContent = "Services";
+document.querySelectorAll('a')[1].textContent = "Product";
+document.querySelectorAll('a')[2].textContent = "Vision";
+document.querySelectorAll('a')[3].textContent = "Features";
+document.querySelectorAll('a')[4].textContent = "About";
+document.querySelectorAll('a')[5].textContent = "Contact";
+
+
+/**********************************************
+                    CTA
+ **********************************************/
+
+ document.querySelector('h1').textContent = "DOM Is Awesome"
+ document.querySelector('button').textContent = "Get Started"
+ document.getElementById("cta-img").src = "img/header-img.png";
+
+
+ /**********************************************
+                    Features
+ **********************************************/
+
+document.getElementsByTagName('h4')[0].textContent = "Features";
+document.getElementsByTagName('p')[0].textContent = "Features content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis."
+
+/**********************************************
+                    About
+ **********************************************/
+
+document.getElementsByTagName('h4')[1].textContent = "About";
+document.getElementsByTagName('p')[1].textContent = "About content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.";
+
+ /**********************************************
+                    Middle Image
+ **********************************************/
+
+document.getElementById("middle-img").src = "img/mid-page-accent.jpg";
+
+ /**********************************************
+                    Services
+ **********************************************/
+
+document.getElementsByTagName('h4')[2].textContent = "Services";
+document.getElementsByTagName('p')[2].textContent = "Services content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.";
+
+ /**********************************************
+                    Product
+ **********************************************/
+
+document.getElementsByTagName('h4')[3].textContent = "Product";
+document.getElementsByTagName('p')[3].textContent = "Product content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.";
+
+ /**********************************************
+                    Vision
+ **********************************************/
+
+document.getElementsByTagName('h4')[4].textContent = "Vision";
+document.getElementsByTagName('p')[4].textContent = "Vision content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.";
+
+ /**********************************************
+                    Contact
+ **********************************************/
+
+document.getElementsByTagName('h4')[5].textContent = "Contact";
+document.getElementsByTagName('p')[5].textContent = "123 Way 456 Street Somewhere, USA";
+document.getElementsByTagName('p')[6].textContent = "1 (888) 888-8888";
+document.getElementsByTagName('p')[7].textContent = "sales@greatidea.io";
+
+ /**********************************************
+                    Footer
+ **********************************************/
+
+ document.getElementsByTagName('p')[8].textContent = "Copyright Great Idea 2018"
