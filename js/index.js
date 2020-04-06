c9abfd1eeb47c87697f59eea860d01c98755ee89
JM <manzur-1990@hotmail.com>
2020-04-06 18:07:07 -0500

    HTML content is displaying, need to work on styles

--- js/index.js
+++ js/index.js
@@ -40,3 +40,139 @@ const siteContent = {
 // Example: Update the img src for the logo
 let logo = document.getElementById("logo-img");
 logo.setAttribute('src', siteContent["nav"]["img-src"])
+
+
+// Nav -------------------------
+
+
+const navLinks = document.querySelectorAll('a');
+
+navLinks[0].textContent = siteContent['nav']['nav-item-1'];
+navLinks[1].textContent = siteContent['nav']['nav-item-2'];
+navLinks[2].textContent = siteContent['nav']['nav-item-3'];
+navLinks[3].textContent = siteContent['nav']['nav-item-4'];
+navLinks[4].textContent = siteContent['nav']['nav-item-5'];
+navLinks[5].textContent = siteContent['nav']['nav-item-6'];
+
+
+
+navLinks[0].style.color = "green";
+navLinks[1].style.color = "green";
+navLinks[2].style.color = "green";
+navLinks[3].style.color = "green";
+navLinks[4].style.color = "green";
+navLinks[5].style.color = "green";
+
+
+const navBar = document.querySelector('nav');
+const nav1 = document.createElement('a');
+const nav2 = document.createElement('a');
+
+nav1.href = '#';
+nav1.textContent = 'Awesome';
+nav1.style.color = 'green';
+
+
+nav2. href = '#';
+nav2.textContent = 'Ness';
+nav2.style.color = 'green';
+
+navBar.prepend(nav1);
+navBar.append(nav2);
+
+
+// CTA ------------------------------
+
+const ctaContent = document.querySelector('h1')
+
+ctaContent.textContent = "DOM Is Awesome"
+
+
+const ctaButton = document.querySelector('button')
+ctaButton.textContent = "Get Started"
+
+document.getElementById("cta-img").src="img/header-img.png";
+
+
+
+// Main Content ----------------------------
+
+// Features
+const featureH4 = document.getElementsByTagName('h4')[0]
+featureH4.textContent = "Features";
+
+const featuresP = document.getElementsByTagName('p')[0]
+featuresP.textContent = "Features content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis."
+// Features Ends
+
+
+// About
+const aboutH4 = document.getElementsByTagName('h4')[1]
+aboutH4.textContent = "Features";
+
+const aboutP = document.getElementsByTagName('p')[1]
+aboutP.textContent = "Features content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis."
+// About Ends
+
+
+// Mid Image
+document.getElementById("middle-img").src = "img/mid-page-accent.jpg"
+// Mid Image Ends
+
+
+// Services
+const servicesH4 = document.getElementsByTagName('h4')[2]
+servicesH4.textContent = "Features";
+
+const servicesP = document.getElementsByTagName('p')[2]
+servicesP.textContent = "Features content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis."
+// Services Ends
+
+
+// Product
+const productH4 = document.getElementsByTagName('h4')[3]
+productH4.textContent = "Features";
+
+const productP = document.getElementsByTagName('p')[3]
+productP.textContent = "Features content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis."
+// Product Ends
+
+
+// Vision
+const visionH4 = document.getElementsByTagName('h4')[4]
+visionH4.textContent = "Vision";
+
+const visionP = document.getElementsByTagName('p')[4]
+visionP.textContent = "Vision content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis."
+// Vision Ends
+
+
+// Bottom Content --------------------------
+
+// Contact
+const contact = document.getElementsByTagName('h4')[5]
+
+contact.textContent = "Contact"
+const street = document.getElementsByTagName('p')[5]
+street.textContent = "123 Way Up North Street"
+street.fontSize = '1px'
+
+const somewhere = document.getElementsByTagName ('p')[6]
+somewhere.textContent = "Somwhere, USA"
+
+const numberPhone = document.getElementsByTagName ('p')[7]
+numberPhone.textContent = "1 (888) 888-8888"
+
+const email = document.getElementsByTagName ('p')[7]
+email.textContent = "sales@greatidea.io"
+
+// Contact Ends
+
+
+// Footer ---------------------------
+
+const footer = document.getElementsByTagName('p')[8]
+footer.textContent = "Copyright Great Idea 2018"
+
+
+
