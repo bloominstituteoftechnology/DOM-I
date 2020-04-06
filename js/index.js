22fca5eff76b3018f991d4f4cbf2b5c360c38e71
Shane Kaestner <shane.kaestner@yahoo.com>
2020-04-06 12:58:10 -0700

    adjusted the json for the h1 adding breakpoints so that it would be in a collumn instead of a line.

--- js/index.js
+++ js/index.js
@@ -9,7 +9,7 @@ const siteContent = {
     "img-src": "img/logo.png"
   },
   "cta": {
-    "h1": "DOM Is Awesome",
+    "h1": "DOM<br/>Is<br/>Awesome",
     "button": "Get Started",
     "img-src": "img/header-img.png"
   },
@@ -45,7 +45,7 @@ logo.setAttribute('src', siteContent["nav"]["img-src"]);
 let ctaTextEl = document.querySelector('h1');
  //console.log(ctaTextEl);
 // //ctaTextEl.textContent = "";
-ctaTextEl.textContent = siteContent["cta"]["h1"];
+ctaTextEl.innerHTML = siteContent["cta"]["h1"];
 
 let ctaButton = document.querySelector("button");
 ctaButton.textContent = siteContent["cta"]["button"];
