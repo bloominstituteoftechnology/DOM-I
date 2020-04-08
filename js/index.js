3504102bb437d1ae9b55deaf7edd45a795a3326f
tbbcoach <58452640+tbbcoach@users.noreply.github.com>
2020-04-07 22:01:28 -0400

    started coding.

--- js/index.js
+++ js/index.js
@@ -40,3 +40,10 @@ const siteContent = {
 // Example: Update the img src for the logo
 let logo = document.getElementById("logo-img");
 logo.setAttribute('src', siteContent["nav"]["img-src"])
+
+let nav1 = document.querySelector("nav-item-1");
+nav1.setAttribute('a', siteContent["nav"]['nav-item-1'])
+
+
+
+
