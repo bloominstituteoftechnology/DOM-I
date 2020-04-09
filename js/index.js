41c8137f71b1ee4b2c0a095a1d00716ac5df4c43
tbbcoach <58452640+tbbcoach@users.noreply.github.com>
2020-04-08 21:31:27 -0400

    more additions to DOM to match sample fire.

--- js/index.js
+++ js/index.js
@@ -41,6 +41,23 @@ const siteContent = {
 let logo = document.getElementById("logo-img");
 logo.setAttribute('src', siteContent["nav"]["img-src"])
 
+//let nav = document.querySelector("a");
+//nav.textContent = "Services"
+
+let navItems = document.querySelectorAll("a");
+navItems[0].textContent = siteContent["nav"]["nav-item-1"];
+navItems[0].style.color = "green";
+navItems[1].textContent = siteContent["nav"]["nav-item-2"];
+navItems[1].style.color = "green";
+navItems[2].textContent = siteContent["nav"]["nav-item-3"];
+navItems[2].style.color = "green";
+navItems[3].textContent = siteContent["nav"]["nav-item-4"];
+navItems[3].style.color = "green";
+navItems[4].textContent = siteContent["nav"]["nav-item-5"];
+navItems[4].style.color = "green";
+navItems[5].textContent = siteContent["nav"]["nav-item-6"];
+navItems[5].style.color = "green";
+
 let snippet = document.getElementById("cta-img");
 snippet.setAttribute('src', siteContent["cta"]["img-src"])
 
@@ -53,20 +70,28 @@ button.textContent = "Get Started";
 let mainHeadLine = document.querySelector(".cta-text h1");
 mainHeadLine.textContent = "DOM is Awesome!";
 
-let topContent = document.querySelector(".top-content .text-content p");
-topContent.textContent = "Features content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis."
+let topContent = document.querySelectorAll(".top-content .text-content p");
+topContent[0].textContent = "Features content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis."
+topContent[1].textContent = "About content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis."
 
-let topSubHead = document.querySelector(".top-content .text-content h4");
-topSubHead.textContent = "Features";
+let topSubHead = document.querySelectorAll(".top-content .text-content h4");
+topSubHead[0].textContent = "Features";
+topSubHead[1].textContent = "About";
 
-let bottomSubHead1 = document.querySelector(".bottom-content .text-content h4");
-bottomSubHead1.textContent = "Services";
 
-let bottomContent = document.querySelector(".bottom-content .text-content p");
-bottomContent.textContent = "Services content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis."
+let bottomSubHead = document.querySelectorAll(".bottom-content .text-content h4");
+bottomSubHead[0].textContent = "Services";
+bottomSubHead[1].textContent = "Product";
+bottomSubHead[2].textContent = "Vision";
 
-let contact = document.getElementsByClassName("contact");
-contact.textContent =  "123 Way 456 Street Somewhere, USA";
+let bottomContent = document.querySelectorAll(".bottom-content .text-content p");
+bottomContent[0].textContent = "Services content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis."
+bottomContent[1].textContent = "Product content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis."
+bottomContent[2].textContent = siteContent["main-content"]['vision-content'];
+
+
+let contact = document.getElementsByClassName("contact p");
+contact.textContent =  "123 Way 456 Street Somewhere, USA"; 
 
 let contactHeading = document.querySelector(".contact h4");
 contactHeading.textContent = "Contact";
