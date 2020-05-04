698bcf3e189da0bc9d1f0e417895c79b52413a73
KSClopton <kyle.clopton@outlook.com>
2020-05-04 19:34:03 -0400

    finished all tasks. Need to do stretch goals

--- README.md
+++ README.md
@@ -28,6 +28,9 @@ You have been provided a [JSON object](js/index.js) with all the necessary data 
 ## Task 1: Create selectors to point your data into elements
 
 * [ ] Create selectors by using any of the DOM element's methods
+document.getElementsByTagName('div')
+document.getElementsById()
+
 * [ ] Note that IDs have been used on all images.  Use the IDs to update src path content
 
 ## Task 2: Update the HTML with the JSON data
--- js/index.js
+++ js/index.js
@@ -40,3 +40,95 @@ const siteContent = {
 // Example: Update the img src for the logo
 let logo = document.getElementById("logo-img");
 logo.setAttribute('src', siteContent["nav"]["img-src"])
+
+// ## Task 1: Create selectors to point your data into elements
+// * [ ] Create selectors by using any of the DOM element's methods
+// * [ ] Note that IDs have been used on all images.  Use the IDs to update src path content
+
+let heroImg = document.getElementById('cta-img');
+heroImg.setAttribute('src' , siteContent['cta']['img-src']);
+
+let middleImg = document.getElementById('middle-img');
+middleImg.setAttribute('src', siteContent['main-content']['middle-img-src']);
+
+
+
+// ## Task 2: Update the HTML with the JSON data
+
+// * [ ] Remember, NO direct updating of the HTML source is allowed.
+// * [ ] Using your selectors, update the content to match the example file.
+// * [ ] Remember to update the src attributes on images
+
+let navItems = document.querySelectorAll('nav a');
+navItems[0].textContent = siteContent['nav']['nav-item-1'];
+navItems[1].textContent = siteContent['nav']['nav-item-2'];
+navItems[2].textContent = siteContent['nav']['nav-item-3'];
+navItems[3].textContent = siteContent['nav']['nav-item-4'];
+navItems[4].textContent = siteContent['nav']['nav-item-5'];
+navItems[5].textContent = siteContent['nav']['nav-item-6'];
+
+
+let buttonText = document.querySelector('button');
+buttonText.textContent = siteContent['cta']['button'];
+
+let mainHeader = document.querySelector('h1');
+mainHeader.textContent = siteContent['cta']['h1'];
+
+
+let headers = document.querySelectorAll('h4');
+headers[0].textContent = siteContent['main-content']['features-h4'];
+headers[1].textContent = siteContent['main-content']['about-h4'];
+headers[2].textContent = siteContent['main-content']['services-h4'];
+headers[3].textContent = siteContent['main-content']['product-h4'];
+headers[4].textContent = siteContent['main-content']['vision-h4'];
+headers[5].textContent = siteContent['contact']['contact-h4'];
+
+let content = document.querySelectorAll('p');
+
+content[0].textContent = siteContent['main-content']['features-content'];
+content[1].textContent = siteContent['main-content']['about-content'];
+content[2].textContent = siteContent['main-content']['services-content'];
+content[3].textContent = siteContent['main-content']['product-content'];
+content[4].textContent = siteContent['main-content']['vision-content'];
+content[5].textContent = siteContent['contact']['address'];
+content[6].textContent = siteContent['contact']['phone'];
+content[7].textContent = siteContent['contact']['email'];
+content[8].textContent = siteContent['footer']['copyright'];
+
+
+// ## Task 3: Add new content
+
+// * [ ] Change the color of the navigation text to be green.
+// * [ ] Utilize `.appendChild()` and `.prepend()` to add two new items to the navigation system. You can call them whatever you want.
+// * [ ] Check your work by looking at th1e [original html](original.html) in the browser
+
+navItems.forEach(function(item) {
+  item.style.color = 'green';
+});
+
+const myAppend = {
+href: "#",
+textContent: "End"
+}
+
+const myPrepend = {
+  href: "#",
+  textContent: "Beginning"
+  }
+
+const newAnchorBegin = document.createElement('a');
+const newAnchorEnd = document.createElement('a');
+
+newAnchorBegin.href = myPrepend.href;
+newAnchorEnd.href = myAppend.href;
+newAnchorBegin.textContent = myPrepend.textContent;
+newAnchorEnd.textContent = myAppend.textContent;
+
+document.querySelector('nav').prepend(newAnchorBegin);
+document.querySelector('nav').appendChild(newAnchorEnd);
+
+
+// ## Stretch Goals
+
+// * [ ] Update styles throughout the page as you see fit.  Study what happens when you updated the DOM using style in JavaScript.  
+// * [ ] Study tomorrow's lesson on events and try to integrate a button that can update content on the site with a click of a button.  You could build a similar data object with new values to help you test the click event.
