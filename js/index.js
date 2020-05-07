32ce61b04e458321776ee4a868a907024df6d1c4
dustin <dustnclaytiner@gmail.com>
2020-05-06 22:33:38 -0500

    almost done

--- js/index.js
+++ js/index.js
@@ -38,5 +38,47 @@ const siteContent = {
 };
 
 // Example: Update the img src for the logo
+//selectors
+
+//nav
 let logo = document.getElementById("logo-img");
 logo.setAttribute('src', siteContent["nav"]["img-src"])
+let allAs = document.querySelectorAll('a');
+let navy = document.querySelector('nav')
+allAs[0].textContent = siteContent['nav']['nav-item-1'];
+allAs[1].textContent = siteContent['nav']['nav-item-2'];
+allAs[2].textContent = siteContent['nav']['nav-item-3'];
+allAs[3].textContent = siteContent['nav']['nav-item-4'];
+allAs[4].textContent = siteContent['nav']['nav-item-5'];
+allAs[5].textContent = siteContent['nav']['nav-item-6'];
+// navy.appendChild('Stats');
+// navy.prepend('Careers');
+document.createElement
+allAs.forEach(element => element.style.color = "green")
+
+//cta
+let awesome = document.querySelector('h1').textContent = "Dom is Awesome!";
+let button = document.querySelector('button').textContent = "Get Started";
+let img2 = document.getElementById('cta-img').setAttribute('src', siteContent["cta"]["img-src"]);
+//main-content
+let h4s = document.querySelectorAll('h4');
+let P = document.querySelectorAll("p");
+h4s[0].textContent = siteContent['main-content']['features-h4'];
+P[0].textContent = siteContent['main-content']['features-content'];
+h4s[1].textContent = siteContent['main-content']['about-h4'];
+P[1].textContent = siteContent['main-content']['about-content'];
+let img3 = document.getElementById('middle-img').setAttribute('src', siteContent["main-content"]["middle-img-src"]);
+h4s[2].textContent = siteContent['main-content']['services-h4'];
+P[2].textContent = siteContent['main-content']['services-content'];
+h4s[3].textContent = siteContent['main-content']['product-h4'];
+P[3].textContent = siteContent['main-content']['product-content'];
+h4s[4].textContent = siteContent['main-content']['vision-h4'];
+P[4].textContent = siteContent['main-content']['vision-content'];
+
+//contact
+h4s[5].textContent = siteContent['contact']['contact-h4'];
+P[5].textContent = siteContent['contact']['address'];
+P[6].textContent = siteContent['contact']['phone'];
+P[7].textContent = siteContent['contact']['email'];
+//footer
+P[8].textContent = siteContent['footer']['copyright']
