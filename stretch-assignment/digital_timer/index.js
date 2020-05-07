c9505df0a1f474977c84274d2d3341ac36299c33
JenVest2020 <jenvest2019@gmail.com>
2020-05-07 18:09:47 -0500

    giving up on stretch for the day...back at later

--- stretch-assignment/digital_timer/index.js
+++ stretch-assignment/digital_timer/index.js
@@ -1,1 +1,8 @@
-let numbers = document.querySelectorAll('.digits');
+let numbers = document.getElementsByClassName('digit');
+
+const interval = setInterval(myTimer, 1000);
+function myTimer() {
+    let d = new Date();
+    let t = d.toLocaleTimeString();
+    document.getElementsByClassName('digit').innerHTML = t;
+};
