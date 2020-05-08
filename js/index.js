e9d20671af42d2c4ed1c27c6419d063ba4ce28e6
Tylar Washington-Beechum <tylay47@gmail.com>
2020-05-08 18:20:16 -0400

    5/8/2020

--- js/index.js
+++ js/index.js
@@ -9,7 +9,7 @@ const siteContent = {
     "img-src": "img/logo.png"
   },
   "cta": {
-    "h1": "DOM Is Awesome",
+    "h1": "DOM\n Is\n Awesome",
     "button": "Get Started",
     "img-src": "img/header-img.png"
   },
@@ -40,3 +40,42 @@ const siteContent = {
 // Example: Update the img src for the logo
 let logo = document.getElementById("logo-img");
 logo.setAttribute('src', siteContent["nav"]["img-src"])
+
+ const text = document.querySelectorAll('.text-content, h4, p');
+ const bottomimg = document.querySelector('.middle-img');
+ const navs = document.querySelectorAll('a');
+ const topimg = document.querySelector('#cta-img');
+ const texttop = document.querySelectorAll('.cta-text,h1,button');
+ const contact = document.querySelectorAll('.contact,h4,p');
+
+navs[0].textContent = 'Service';
+navs[1].textContent = 'Product';
+navs[2].textContent = 'Vision';
+navs[3].textContent = 'Features';
+navs[4].textContent = 'About';
+navs[5].textContent = 'Contact';
+topimg.src = "img/header-img.png";
+texttop[1].textContent = 'DOM\n Is\n Awesome';
+texttop[2].textContent = 'Get Started';
+ text[1].textContent = 'Features';
+ text[2].textContent = "Features content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.";
+ text[2].style.width = '450px';
+ text[2].style.paddingRight = '8%';
+ text[4].textContent = "About";
+ text[5].textContent = "About content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.";
+bottomimg.src = 'img/mid-page-accent.jpg';
+text[7].textContent = 'Services';
+text[8].textContent = 'Services content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.';
+text[8].style.width = '300px';
+text[10].textContent = 'Product';
+text[11].textContent ='Product content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.';
+text[11].style.width = '300px';
+text[13].textContent = 'Vision';
+text[14].textContent = 'Vision content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.'
+contact[11].textContent = 'Contact';
+contact[12].textContent = '123 Way 456 Street Somewhere, USA';
+contact[13].textContent = '1 (888) 888-8888';
+contact[14].textContent = 'sales@greatidea.io';
+contact[15].textContent = 'Copyright Great Idea! 2018';
+
+
