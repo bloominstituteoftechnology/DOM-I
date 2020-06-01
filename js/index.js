a04f5e4c66dcd2df3eca35ded48a71bc2bb1b338
Isaac M Gorman <59988845+imgorman@users.noreply.github.com>
2020-06-01 11:05:39 -0500

    done

--- js/index.js
+++ js/index.js
@@ -40,3 +40,113 @@ const siteContent = {
 // Example: Update the img src for the logo
 let logo = document.getElementById("logo-img");
 logo.setAttribute('src', siteContent["nav"]["img-src"])
+
+
+// Header --------------------------------------------------------------------------------
+// Adding an Announcement Banner ---- 
+const grabBody = document.querySelector('body');
+const bannerAnnouncement = document.createElement('div');
+bannerAnnouncement.style.height = "100px";
+
+bannerAnnouncement.setAttribute("class", "banner");
+bannerAnnouncement.style.backgroundColor = "lightblue";
+bannerAnnouncement.style.display = "flex";
+bannerAnnouncement.style.justifyContent = "center";
+bannerAnnouncement.style.alignItems = "center";
+
+grabBody.prepend(bannerAnnouncement);
+
+
+
+const grabBanner = document.querySelector('.banner');
+const announcement = document.createElement("h1");
+
+announcement.textContent = "🔥Buy Now!!🔥"
+announcement.style.fontSize = "2rem";
+announcement.style.color = "black";
+
+grabBanner.prepend(announcement);
+
+// Adding an Announcement Banner ---- 
+
+
+// - Grab elements on the DOM 
+const navItems = document.querySelectorAll('nav a');
+navItems[0].setAttribute("class", siteContent.nav["nav-item-1"]);
+navItems[1].setAttribute("class", siteContent.nav["nav-item-2"]);
+navItems[2].setAttribute("class", siteContent.nav["nav-item-3"]);
+navItems[3].setAttribute("class", siteContent.nav["nav-item-4"]);
+navItems[4].setAttribute("class", siteContent.nav["nav-item-5"]);
+navItems[5].setAttribute("class", siteContent.nav["nav-item-6"]);
+ 
+
+// - Assigning jSON content
+navItems[0].textContent = siteContent.nav["nav-item-1"];
+navItems[0].style.color = "green";
+
+navItems[1].textContent = siteContent.nav["nav-item-2"];
+navItems[1].style.color = "green";
+
+navItems[2].textContent = siteContent.nav["nav-item-3"];
+navItems[2].style.color = "green";
+
+navItems[3].textContent = siteContent.nav["nav-item-4"];
+navItems[3].style.color = "green";
+
+navItems[4].textContent = siteContent.nav["nav-item-5"];
+navItems[4].style.color = "green";
+
+navItems[5].textContent = siteContent.nav["nav-item-6"];
+navItems[5].style.color = "green";
+// Header --------------------------------------------------------------------------------
+
+
+// cta --------------------------------------------------------------------------------
+
+const ctaText = document.querySelector('.cta h1');
+ctaText.textContent  = siteContent.cta.h1;
+
+const ctaButt = document.querySelector('.cta button');
+ctaButt.textContent = siteContent.cta.button
+
+const ctaImg = document.querySelector('.cta img');
+ctaImg.setAttribute('src', siteContent.cta["img-src"]);
+
+
+// cta --------------------------------------------------------------------------------
+
+
+// main-content --------------------------------------------------------------------------------
+
+const topTextContent = document.querySelectorAll('.text-content h4');
+topTextContent[0].textContent = siteContent["main-content"]["features-h4"];
+topTextContent[1].textContent = siteContent["main-content"]["about-h4"];
+topTextContent[2].textContent = siteContent["main-content"]["services-h4"];
+topTextContent[3].textContent = siteContent["main-content"]["product-h4"];
+topTextContent[4].textContent = siteContent["main-content"]["vision-h4"];
+
+const topParaTextContent = document.querySelectorAll('.text-content p');
+topParaTextContent[0].textContent = siteContent["main-content"][ "features-content"];
+topParaTextContent[1].textContent = siteContent["main-content"][ "about-content"];
+topParaTextContent[2].textContent = siteContent["main-content"][ "services-content"];
+topParaTextContent[3].textContent = siteContent["main-content"][ "product-content"];
+topParaTextContent[4].textContent = siteContent["main-content"][ "vision-content"];
+
+const getImg = document.querySelector(".middle-img");
+getImg.src = siteContent["main-content"]["middle-img-src"]
+
+// contact --------------------------------------------------------------------------------
+const contactH4 = document.querySelector('.contact h4');
+const contactp = document.querySelectorAll('.contact p');
+
+contactH4.textContent = siteContent.contact["contact-h4"];
+contactp[0].textContent = siteContent.contact.address
+contactp[1].textContent = siteContent.contact.phone
+contactp[2].textContent = siteContent.contact.email
+// contact --------------------------------------------------------------------------------
+
+// footer --------------------------------------------------------------------------------
+const daFooter = document.querySelector('footer p');
+daFooter.textContent = siteContent.footer.copyright
+
+// footer --------------------------------------------------------------------------------
