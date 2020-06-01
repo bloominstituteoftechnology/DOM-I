fabd182d4749f2dd39b8026b7c9530ad94b5c30b
unknown <admin@social-collab.org>
2020-05-31 23:07:28 -0400

    Attempted Digital Timer Stretch

---
+++ stretch-assignment/digital_timer/index.js
@@ -0,0 +1,41 @@
+// const getOne = window.setInterval(msTens,10);
+// const getTwo = window.setInterval(msHundreds,100);
+// const getThree = window.setInterval(secondOnes,1000);
+// const getFour = window.setInterval(secondTens,10000);
+
+// const mstens = getElementById("msTens");
+// const mshundreds = getElementById("msHundreds");
+// const secondones = getElementById("secondOnes");
+// const secondtens = getElementById("secondTens");
+
+// function msTens(){
+//     mstens.textContent = getOne;
+// }
+
+// function msHundreds() {
+//     mshundreds.textContent = getTwo;
+// }
+
+// function secondOnes() {
+//     secondones.textContent = getThree;
+// }
+
+// function secondTens() {
+//     secondtens.textContent = getFour;
+// }
+
+// if(getFour >= 100000) {
+//     clearInterval(getFour);
+// }
+
+// if(getThree >= 10000) {
+//     clearInterval(getThree);
+// }
+
+// if(getTwo >= 1000) {
+//     clearInterval(getTwo);
+// }
+
+// if(getOne >= 100) {
+//     clearInterval(getOne);
+// }
