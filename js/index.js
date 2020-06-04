db87ec401f59cfd5e1b8df6eb1f813bd04f4099d
Jr08151 <Jr08151@users.noreply.github.com>
2020-06-04 19:17:35 -0400

    updated Navs and text color

--- js/index.js
+++ js/index.js
@@ -40,3 +40,18 @@ const siteContent = {
 // Example: Update the img src for the logo
 let logo = document.getElementById("logo-img");
 logo.setAttribute('src', siteContent["nav"]["img-src"])
+
+
+//Update the navigation links
+const navItems= document.querySelectorAll("nav a");
+navItems[0].textContent = siteContent.nav["nav-item-1"];
+navItems[1].textContent = siteContent.nav["nav-item-2"];
+navItems[2].textContent = siteContent.nav["nav-item-3"];
+navItems[3].textContent = siteContent.nav["nav-item-4"];
+navItems[4].textContent = siteContent.nav["nav-item-5"];
+navItems[5].textContent = siteContent.nav["nav-item-6"];
+
+//Update color of navigation text to Green
+navItems.forEach( function (currentValue) {
+  currentValue.style.color = "green";
+});
