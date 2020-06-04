c479c21e6ef3d42a4041dfdb7c6e0ba314a0158e
noahbibb21 <noahbibb90@gmail.com>
2020-06-04 17:01:06 -0400

    done

--- js/index.js
+++ js/index.js
@@ -56,5 +56,62 @@ navItems[2].style.color = 'green'
 navItems[3].style.color = 'green'
 navItems[4].style.color = 'green'
 
+const navTop = document.querySelector('nav');
+const firstNav = document.createElement ('a');
+const lastNav = document.createElement ('a');
 
+firstNav.href = '#';
+firstNav.textContent = 'First';
+firstNav.style.color = 'green';
 
+lastNav.href = '#';
+lastNav.textContent = 'Last';
+lastNav.style.color = 'green';
+
+navTop.prepend(firstNav);
+navTop.append(lastNav);
+
+const header1 = document.querySelector('h1');
+header1.textContent = siteContent['cta']['h1'];
+header1.innerHTML = 'Dom Is Awesome';
+
+const button1 = document.querySelector('button');
+button1.textContent = siteContent['cta']['button'];
+button1.innerHTML = 'Get Started';
+
+const pic1 = document.getElementById('cta-img');
+pic1.setAttribute('src', siteContent ['cta']['img-src'])
+
+const mainHead = document.querySelectorAll('.main-content .text-content h4');
+const mainContent = document.querySelectorAll('.main-content .text-content p');
+const middleImg = document.querySelector('#middle-img');
+
+mainHead[0].textContent = siteContent['main-content']['features-h4'];
+mainContent[0].textContent = siteContent['main-content']['features-content'];
+
+mainHead[1].textContent = siteContent['main-content']['about-h4'];
+mainContent[1].textContent = siteContent['main-content']['about-content'];
+
+middleImg.src = siteContent['main-content']['middle-img-src'];
+
+mainHead[2].textContent = siteContent['main-content']['services-h4'];
+mainContent[2].textContent = siteContent['main-content']['services-content'];
+
+mainHead[3].textContent = siteContent['main-content']['product-h4'];
+mainContent[3].textContent = siteContent['main-content']['product-content'];
+
+mainHead[4].textContent = siteContent['main-content']['vision-h4'];
+mainContent[4].textContent = siteContent['main-content']['vision-content'];
+
+const contactH4 = document.querySelector('.contact h4');
+const contactContent = document.querySelectorAll('.contact p');
+
+contactH4.textContent = siteContent['contact']['contact-h4'];
+
+contactContent[0].textContent = siteContent['contact']['address'];
+contactContent[1].textContent = siteContent['contact']['phone'];
+contactContent[2].textContent = siteContent['contact']['email'];
+
+ 
+const footerContent = document.querySelector('footer p');
+footerContent.textContent = siteContent['footer']['copyright'];
