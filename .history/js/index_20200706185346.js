5da9b660ed2971f35df5a0ff5f6ce3079bd847bc
Chris <ctcoleman510@gmail.com>
2020-07-06 18:56:21 -0400

    Finished DOM Manipulation Page

--- .history/js/index_20200706185346.js
+++ .history/js/index_20200706185346.js
@@ -1,3 +1,28 @@
+const siteContent = {
+
+  "main-content": {
+    "features-h4": "Features",
+    "features-content": "Features content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.",
+    "about-h4": "About",
+    "about-content": "About content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.",
+    "middle-img-src": "img/mid-page-accent.jpg",
+    "services-h4": "Services",
+    "services-content": "Services content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.",
+    "product-h4": "Product",
+    "product-content": "Product content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.",
+    "vision-h4": "Vision",
+    "vision-content": "Vision content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.",
+  },
+  "contact": {
+    "contact-h4": "Contact",
+    "address": "123 Way 456 Street Somewhere, USA",
+    "phone": "1 (888) 888-8888",
+    "email": "sales@greatidea.io",
+  },
+  "footer": {
+    "copyright": "Copyright Great Idea! 2018"
+  },
+};
 
 // NavBar
 const navigationBar = document.querySelector('nav')
@@ -42,15 +67,11 @@ bottomContentTextAreas[0].textContent = 'Services content elementum magna eros, 
 bottomContentHeadings[1].textContent = "Product"
 bottomContentTextAreas[1].textContent = 'Product content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.'
 
-bottomContentHeadings[2].textContent = 'Vision'
-bottomContentTextAreas[2].textContent = 'Vision content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.'
+bottomContentHeadings[2] = 'Vision'
+bottomContentTextAreas[2] = 'Vision content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.'
 
 const contactSection = document.querySelector('.contact')
 const contactHeading = contactSection.querySelector('h4')
 const contactTextAreas = contactSection.querySelectorAll('p')
 
-contactHeading.textContent = 'Contact'
-
-contactTextAreas[0].textContent = '123 Way 456 Street Somewhere, USA'
-contactTextAreas[1].textContent = '1 (888) 888-8888 '
-contactTextAreas[2].textContent = 'sales@greatidea.io'
+contactHeading.textContent = 
--- .history/js/index_20200706185428.js
+++ .history/js/index_20200706185428.js
@@ -1,3 +1,28 @@
+const siteContent = {
+
+  "main-content": {
+    "features-h4": "Features",
+    "features-content": "Features content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.",
+    "about-h4": "About",
+    "about-content": "About content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.",
+    "middle-img-src": "img/mid-page-accent.jpg",
+    "services-h4": "Services",
+    "services-content": "Services content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.",
+    "product-h4": "Product",
+    "product-content": "Product content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.",
+    "vision-h4": "Vision",
+    "vision-content": "Vision content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.",
+  },
+  "contact": {
+    "contact-h4": "Contact",
+    "address": "123 Way 456 Street Somewhere, USA",
+    "phone": "1 (888) 888-8888",
+    "email": "sales@greatidea.io",
+  },
+  "footer": {
+    "copyright": "Copyright Great Idea! 2018"
+  },
+};
 
 // NavBar
 const navigationBar = document.querySelector('nav')
@@ -51,6 +76,4 @@ const contactTextAreas = contactSection.querySelectorAll('p')
 
 contactHeading.textContent = 'Contact'
 
-contactTextAreas[0].textContent = '123 Way 456 Street Somewhere, USA'
-contactTextAreas[1].textContent = '1 (888) 888-8888 '
-contactTextAreas[2].textContent = 'sales@greatidea.io'
+contactTextAreas[0].textContent = ''
