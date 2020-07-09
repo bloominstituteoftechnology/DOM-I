29011b8c75b07842c98d80f62055889a3989a1d9
Ella Mae Freshwater <emfresh2o@Ellas-MacBook-Pro.local>
2020-07-08 23:30:22 -0400

    first commit... not done yet!

--- js/index.js
+++ js/index.js
@@ -40,3 +40,52 @@ const siteContent = {
 // Example: Update the img src for the logo
 let logo = document.getElementById("logo-img");
 logo.setAttribute('src', siteContent["nav"]["img-src"])
+
+//Nav Items
+let navItems = document.querySelectorAll("a");
+for (let i=0; i<navItems.length; i++){
+  navItems[i].textContent = siteContent.nav[`nav-item-${i+1}`];
+}
+
+// Change the color of Nav
+for(let i=0; i<navItems.length; i++){
+  navItems[i].style.color = 'green';
+}
+
+// Added two new items to Nav
+const nav = document.querySelector('nav');
+const newItems = ['Gallery', 'Blog'];
+for (let i=0; i<newItems.length; i++){
+  const aTag = document.createElement('a');
+  aTag.textContent = newItems[i];
+  nav.appendChild(aTag);
+}
+
+// CTA Section
+const ctaH1 = document.querySelector('.cta .cta-text h1');
+ctaH1.textContent = siteContent['cta']['h1'];
+
+const ctaButton = document.querySelector('.cta .cta-text button');
+ctaButton.textContent = siteContent['cta']['button'];
+
+const ctaImage = document.querySelector('#cta-img');
+ctaImage.src = siteContent['cta']['img-src'];
+
+// Main Section //
+// Content H4
+const keyTitlesH4 = ['features-h4', 'about-h4', 'services-h4', 'product-h4', 'vision-h4'];
+
+// Content P
+const keyTitlesP = ['features-content', 'about-content', 'services-content', 'product-content', 'vision-content'];
+const mainContentP = document.querySelectorAll('.main-content .text-content p');
+for (let i=0; i<keyTitlesP.length; i++){
+  mainContentP[i].textContent = siteContent['main-content'][keyTitlesP[i]];
+}
+
+// Content Image
+const middleImg = document.querySelector('#middle-img');
+middleImg.src = siteContent['main-content']['middle-img-src'];
+
+
+
+
