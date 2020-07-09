cf8bb15281476acf249b74595a5d46ceb928b7a7
Jeanellle96 <jeanellepazos@gmail.com>
2020-07-08 20:32:23 -0700

    Finished just need to add the content

--- js/index.js
+++ js/index.js
@@ -40,3 +40,61 @@ const siteContent = {
 // Example: Update the img src for the logo
 let logo = document.getElementById("logo-img");
 logo.setAttribute('src', siteContent["nav"]["img-src"])
+
+const nav = document.querySelectorAll('a')
+nav[0].textContent = siteContent['nav']['nav-item-1']
+nav[1].textContent = siteContent['nav']['nav-item-2']
+nav[2].textContent = siteContent['nav']['nav-item-3']
+nav[3].textContent = siteContent['nav']['nav-item-4']
+nav[4].textContent = siteContent['nav']['nav-item-5']
+nav[5].textContent = siteContent['nav']['nav-item-6']
+nav.forEach((greenColor) => {
+  return greenColor.style.color = 'green'
+})
+
+const h1 = document.querySelector('h1')
+//console.log(h1)
+h1.textContent = siteContent.cta.h1
+
+const button = document.querySelector('button')
+button.textContent = siteContent.cta.button
+
+const mainImg = document.querySelector('#cta-img')
+mainImg.src = siteContent.cta["img-src"]
+
+const titleContent = document.querySelectorAll('.text-content h4')
+titleContent[0].textContent = 'Feature'
+titleContent[1].textContent = 'About'
+titleContent[2].textContent = 'Services'
+titleContent[3].textContent = 'Product'
+titleContent[4].textContent = 'Vision'
+
+const pContent = document.querySelectorAll('p')
+pContent[0].textContent = siteContent["main-content"]["features-content"]
+pContent[1].textContent = siteContent["main-content"]["about-content"]
+pContent[2].textContent = siteContent["main-content"]["services-content"]
+pContent[3].textContent = siteContent["main-content"]["products-content"]
+pContent[4].textContent = siteContent["main-content"]["vision-content"]
+
+const midImg = document.querySelector('.middle-img')
+midImg.src = siteContent["main-content"]["middle-img-src"]
+
+const footerMain = document.querySelector('.contact h4')
+footerMain.textContent = 'Contact'
+
+const footerInfo = document.querySelectorAll('.contact p')
+footerInfo[0].textContent = "123 Way 456 Street Somewhere, USA"
+footerInfo[1].textContent = "footerInfo[0].textContent"
+footerInfo[2].textContent = "sales@greatidea.io"
+
+const footerCopyRight = document.querySelector('footer p')
+footerCopyRight.textContent = "Copyright Great Idea! 2018"
+
+// new content
+
+//const newContent = document.createElement('a')
+//newContent.textContent = 'Material' 
+
+//const newContent = document.createElement('a')
+//newContent.textContent = 'Prices' 
+
