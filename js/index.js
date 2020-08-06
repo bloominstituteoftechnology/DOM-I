0ec78c194004b4a3427789ca069811919c617d01
Alan Paul Mir <alanpaulphotos@gmail.com>
2020-08-06 18:40:31 -0400

    Returned all images to the index.html page

--- js/index.js
+++ js/index.js
@@ -39,4 +39,13 @@ const siteContent = {
 
 // Example: Update the img src for the logo
 let logo = document.getElementById("logo-img");
-logo.setAttribute('src', siteContent["nav"]["img-src"])
+logo.setAttribute('src', siteContent["nav"]["img-src"]);
+
+let circleLogo = document.getElementById('cta-img');
+circleLogo.setAttribute('src', siteContent['cta']['img-src']);
+
+let middleImage = document.getElementById("middle-img");
+middleImage.setAttribute('src', siteContent['main-content']['middle-img-src']);
+
+let mainText = document.querySelector('.cta-text h1');
+console.log(mainText);
