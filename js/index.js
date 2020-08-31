42eaebf44935c43c036312097e7c98be4289998f
Tom Sherman <tompsherman@gmail.com>
2020-08-31 17:14:38 -0400

    DOM matching original

--- js/index.js
+++ js/index.js
@@ -38,12 +38,52 @@ const siteContent = {
 };
 
 // Example: Update the img src for the logo
-let logo = document.getElementById("logo-img");
+const logo = document.getElementById("logo-img");
 logo.setAttribute('src', siteContent["nav"]["img-src"])
 
-let contact = document.querySelector('.contact')
-let contactH4 = contact.nextElementSibling
+const nav = document.querySelectorAll("nav a")
+nav[0].textContent = "Services"
+nav[1].textContent = "Product"
+nav[2].textContent = "Vision"
+nav[3].textContent = "Features"
+nav[4].textContent = "About"
+nav[5].textContent = "Contact"
+
+const headline = document.querySelector(".cta-text h1")
+headline.innerText = "DOM is Awesome"
+
+const button = document.querySelector(".cta-text button")
+button.textContent = "Get Started"
+
+const mainHeadlines = document.querySelectorAll(".main-content h4")
 
 
-let footer = document.querySelector('footer')
+mainHeadlines[0].textContent = "Features"
+mainHeadlines[1].textContent = "About"
+mainHeadlines[2].textContent = "Services"
+mainHeadlines[3].textContent = "Product"
+mainHeadlines[4].textContent = "Vision"
+
+const mainPs = document.querySelectorAll(".main-content p")
+console.log(mainPs)
+mainPs[0].textContent = "Features content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis."
+mainPs[1].textContent = "About content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis."
+mainPs[2].textContent = "Services content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis."
+mainPs[3].textContent = "Product content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis."
+mainPs[4].textContent = "Vision content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis."
+
+const middleImage = document.querySelector(".middle-img")
+middleImage.setAttribute("src", "img/mid-page-accent.jpg")
+
+const contactH4 = document.querySelector('.contact h4')
+
+contactH4.textContent = "Contact"
+
+const contactP = document.querySelectorAll('.contact p')
+
+contactP[0].textContent = "123 Way 456 Street Anywhere, USA"
+contactP[1].textContent = "1 (888) 888-8888"
+contactP[2].textContent = "sales@greatidea.io"
+
+const footer = document.querySelector('footer')
 footer.textContent = 'Copyright Great Idea! 2020'
