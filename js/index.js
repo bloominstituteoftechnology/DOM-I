f29ab7e4fb3e1c054fe3b4c7e74bb24c49273249
Francis Bonacic <francisbonacic@icloud.com>
2020-08-31 14:48:24 -0700

    finished, harder than I thought to revisit this after 4 weeks

--- js/index.js
+++ js/index.js
@@ -40,3 +40,99 @@ const siteContent = {
 // Example: Update the img src for the logo
 let logo = document.getElementById("logo-img");
 logo.setAttribute('src', siteContent["nav"]["img-src"])
+
+
+
+
+// NAV LINKS // 
+
+const navLinks = document.querySelectorAll('header nav a');
+
+navLinks[0].textContent = 'Services';
+navLinks[1].textContent = 'Product';
+navLinks[2].textContent = 'Vision';
+navLinks[3].textContent = 'Features';
+navLinks[4].textContent = 'About';
+navLinks[5].textContent = 'Contact';
+
+//Nav Link Color change//
+
+navLinks[0].style.color="green"
+navLinks[1].style.color="green"
+navLinks[2].style.color="green"
+navLinks[3].style.color="green"
+navLinks[4].style.color="green"
+navLinks[5].style.color="green"
+
+//add new links to nav 
+
+// newNavLink = document.querySelector('a')
+// newNavLink.textContent="More"
+// navLinks.appendChild(newNavLink)
+
+const newNavLink = document.createElement('a');
+newNavLink.textContent = 'More';
+newNavLink.href = '#';
+const nav = document.querySelector('nav');
+nav.appendChild(newNavLink);
+
+const firstNavLink = document.createElement('a');
+firstNavLink.textContent = 'Sign-In';
+firstNavLink.href = '#';
+nav.prepend(firstNavLink);
+
+newNavLink.style.color="green"
+firstNavLink.style.color="green"
+
+
+// CTA SECTION //
+
+const cta = document.querySelector('h1');
+cta.textContent = 'DOM IS AWESOME';
+
+const button = document.querySelector('button');
+button.textContent = 'Get Started'
+
+const img = document.getElementById('cta-img');
+img.setAttribute('src', siteContent['cta']['img-src'])
+
+
+// MAIN CONTENT // 
+
+const mainTextTop = document.querySelectorAll('h4');
+const mainParTop = document.querySelectorAll('p');
+
+
+mainTextTop[0].textContent = 'Features';
+mainParTop[0].textContent = 'Features content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.';
+
+mainTextTop[1].textContent = 'About';
+mainParTop[1].textContent = 'About content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.'
+
+const middleImg = document.getElementById('middle-img');
+middleImg.setAttribute('src', siteContent["main-content"]["middle-img-src"]);
+
+mainTextTop[2].textContent = 'Services';
+mainParTop[2].textContent ='Services content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.'
+
+mainTextTop[3].textContent = 'Product';
+mainParTop[3].textContent = 'Product content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.'
+
+mainTextTop[4].textContent = 'Vision';
+mainParTop[4].textContent = 'Vision content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.'
+
+// CONTACT // 
+
+const title = document.querySelector('.contact h4')
+title.textContent = "Contact"
+
+const contactInfo = document.querySelectorAll('.contact p')
+contactInfo[0].textContent = "123 Way 456 Street Somewhere, USA"
+contactInfo[1].textContent = "1 (888) 888-8888"
+contactInfo[2].textContent = "sales@greatidea.io"
+
+
+// FOOTER //
+
+const footer = document.querySelector('footer p');
+footer.textContent = "Copyright Great Idea! 2018"
