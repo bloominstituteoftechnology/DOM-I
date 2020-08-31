3941635c1e6e6f7319da4ead3d652b85b6050fe7
Tzong-Lian-Tsay <tzong-lian-tsay@lambdastudents.com>
2020-08-31 17:55:40 -0400

    MVP complete

--- js/index.js
+++ js/index.js
@@ -40,3 +40,86 @@ const siteContent = {
 // Example: Update the img src for the logo
 let logo = document.getElementById("logo-img");
 logo.setAttribute('src', siteContent["nav"]["img-src"])
+
+
+
+// Implement Nav Links
+const allNavLinks = document.links;
+// console.log(siteContent.nav["nav-item-1"])
+
+for (let i=1; i < 6; i++){
+  // console.log(siteContent.nav[`nav-item-${i}`]);
+  let navFix = siteContent.nav[`nav-item-${i}`];
+  // console.log(allNavLinks[i])
+  allNavLinks[i].innerText = navFix;
+}
+
+
+// Implement Call to Action (cta)
+const ctaText = document.querySelector('h1');
+ctaText.textContent = siteContent.cta.h1; //'DOM IS AWESOME'
+
+const ctaButton = document.querySelector('button');
+ctaButton.textContent = siteContent.cta.button; //'Get Started'
+
+const ctaImg = document.getElementById("cta-img");
+ctaImg.setAttribute('src', siteContent["cta"]["img-src"]);
+
+// Implement Main Content
+
+const mainContent_texts = document.querySelectorAll('.main-content h4, .main-content p');
+// const mainContent_h4_p = document.querySelectorAll('.text-content > h4, .text-content > p'); -> This selects the same tags
+// console.log(mainContent_texts);
+arrayMCTexts = Array.from(mainContent_texts);
+// console.log(arrayMCTexts);
+// console.log(siteContent['main-content']['features-h4'])
+
+arrayMCTexts[0].textContent = siteContent["main-content"]['features-h4'];
+arrayMCTexts[1].textContent = siteContent["main-content"]['features-content'];
+arrayMCTexts[2].textContent = siteContent["main-content"]['about-h4'];
+arrayMCTexts[3].textContent = siteContent["main-content"]['about-content'];
+arrayMCTexts[4].textContent = siteContent["main-content"]['services-h4'];
+arrayMCTexts[5].textContent = siteContent["main-content"]['services-content'];
+arrayMCTexts[6].textContent = siteContent["main-content"]['product-h4'];
+arrayMCTexts[7].textContent = siteContent["main-content"]['product-content'];
+arrayMCTexts[8].textContent = siteContent["main-content"]['vision-h4'];
+arrayMCTexts[9].textContent = siteContent["main-content"]['vision-content'];
+
+//Random code that didn't work -->
+
+// arrayMC_h4_p = Array.from(mainContent_h4_p);
+// console.log(arrayMC_h4_p);
+
+// This makes the dangerous assumption that object keys are in order
+// const mainContentFix = arrayMC_h4_p.map(item => {
+//   for (let i=0; i<arrayMC_h4_p.length; i ++){
+//     console.log(siteContent['main-content']);
+//     return item = siteContent['main-content'][Object.keys(item)][i]
+//   }
+// });
+  // console.log(siteContent["main-content"][`features-h4`]);
+  // let h4Fix = siteContent["main-content"][i];
+  // mc_h4[i].innerText = h4Fix;
+
+const mainContentImg = document.querySelector('.middle-img');
+mainContentImg.setAttribute('src', siteContent["main-content"]["middle-img-src"]);
+
+
+// Implement Contact
+const contacth4 = document.querySelector('.contact h4');
+contacth4.textContent = siteContent['contact']['contact-h4'];
+console.log(contacth4.textContent);
+
+//const contactP = document.querySelectorAll('.contact p');
+const contactP1 = document.querySelector('.contact p');
+contactP1.textContent = siteContent['contact']['address'];
+
+const contactP2 = contactP1.nextElementSibling;
+contactP2.textContent = siteContent['contact']['phone'];
+
+const contactP3 = contactP2.nextElementSibling;
+contactP3.textContent = siteContent['contact']['email'];
+
+// Implement Footer
+const footerP = document.querySelector('footer'); // only one element in the footer
+footerP.textContent = siteContent['footer']['copyright'];
