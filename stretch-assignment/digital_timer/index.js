89f08a30f1a9b1a140af8cb992ab02dac8a04d8f
Mark Hillin <mark.hillin@gmail.com>
2020-09-02 21:51:46 -0500

    Advanced Stretch

--- stretch-assignment/digital_timer/index.js
+++ stretch-assignment/digital_timer/index.js
@@ -1,7 +1,25 @@
+// creating shortcut for console.log
 const msTens = document.getElementById("msTens")
-var seconds = 0
+const msHundreds = document.getElementById("msHundreds")
+const secOnes = document.getElementById("secondOnes");
+const secTens = document.getElementById("secondTens");
+
+let secondsTens = 0
+let secondsOnes = 0
+let secondsHundreds = 0
+
+secOnes.textContent = secondsOnes;
+secTens.textContent = secondsTens;
 
 setInterval(() => {
-    seconds = seconds + 1
-    msTens.textContent = seconds
+  if(secondsTens <= 1) {
+
+    if( secondsOnes === 9 ) {
+      secOnes.textContent = 0
+    } else {
+      secondsOnes = secondsOnes + 1
+    secOnes.textContent = secondsOnes
+    }
+    
+  }
 }, 1000);
