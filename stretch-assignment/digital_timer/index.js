7cf0c0793aad93dba41f537ed261f359b778b544
Mark Hillin <mark.hillin@gmail.com>
2020-09-02 22:33:06 -0500

    Advanced Stretch 50%

--- stretch-assignment/digital_timer/index.js
+++ stretch-assignment/digital_timer/index.js
@@ -4,22 +4,45 @@ const msHundreds = document.getElementById("msHundreds")
 const secOnes = document.getElementById("secondOnes");
 const secTens = document.getElementById("secondTens");
 
-let secondsTens = 0
-let secondsOnes = 0
-let secondsHundreds = 0
+let secondsTens = 0;
+let secondsOnes = 0;
+let milliSHundreds = 0;
+let milliSTens = 0;
 
 secOnes.textContent = secondsOnes;
 secTens.textContent = secondsTens;
+msTens.textContent = milliSTens;
+msHundreds.textContent = milliSHundreds;
 
 setInterval(() => {
-  if(secondsTens <= 1) {
+  if (secondsTens === 1) {
+    return;
+  }
 
-    if( secondsOnes === 9 ) {
-      secOnes.textContent = 0
-    } else {
-      secondsOnes = secondsOnes + 1
-    secOnes.textContent = secondsOnes
-    }
+  milliSTens += 1;
+  msTens.textContent = milliSTens;
+}, 10);
+
+
+setInterval(() => {
+  if (secondsTens === 1) {
+    return;
+  }
+  
+  if( secondsOnes === 9) {
+    secOnes.style.color = "red";
+    secTens.style.color = "red";
+
+    secondsTens += 1;
+    secondsOnes = 0;
+    secTens.textContent = secondsTens;
+    secOnes.textContent = secondsOnes;
+
     
+    const colon1 = document.getElementById("colon")
+    colon1.style.color = "red";
+  } else {
+    secondsOnes = secondsOnes + 1
+    secOnes.textContent = secondsOnes
   }
 }, 1000);
