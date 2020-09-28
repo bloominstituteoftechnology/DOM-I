d0fb9222fd1920186209f916eb69c7cf5c984f15
Olivia Toolson <oliviatoolson@Olivias-MacBook-Pro.local>
2020-09-28 13:25:30 -0600

    completed navigation bar, cta, top content, and middle image

--- js/index.js
+++ js/index.js
@@ -40,3 +40,59 @@ const siteContent = {
 // Example: Update the img src for the logo
 let logo = document.getElementById("logo-img");
 logo.setAttribute('src', siteContent["nav"]["img-src"])
+
+// nav
+const navigationBar = document.querySelectorAll('a');
+navigationBar[0].textContent = "Services"
+navigationBar[1].textContent = "Product"
+navigationBar[2].textContent = "Vision"
+navigationBar[3].textContent = "Features"
+navigationBar[4].textContent = "About"
+navigationBar[5].textContent = "Contact"
+
+// cta
+const h1 = document.querySelector('h1')
+h1.textContent = "DOM Is Awesome"
+
+const button = document.querySelector('button')
+button.textContent = "Get Started"
+
+const titleSectionImg = document.querySelector('#cta-img')
+titleSectionImg.setAttribute('src', siteContent["cta"]["img-src"])
+
+// main content
+// features and about text boxes
+const mainContent = document.querySelector('.main-content')
+const topContent = document.querySelector('.top-content')
+const bottomContent = document.querySelector('.bottom-content')
+
+const textBox = topContent.querySelector('.text-content')
+const textBoxOne = textBox.cloneNode(true)
+topContent.appendChild(textBoxOne)
+
+const cardTitleFeatures = textBox.querySelector('h4')
+const cardContentFeatures = textBox.querySelector('p')
+cardTitleFeatures.textContent = "Features"
+cardContentFeatures.textContent = "Features content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis."
+
+const cardTitleAbout = textBoxOne.querySelector('h4')
+const cardContentAbout = textBoxOne.querySelector('p')
+cardTitleAbout.textContent = "About"
+cardContentAbout.textContent = "About content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis."
+
+// middle image
+const middleImg = document.querySelector('#middle-img')
+middleImg.setAttribute('src', siteContent["main-content"]["middle-img-src"])
+
+// services, product, and vision text boxes
+const textBoxTwo = bottomContent.querySelector('.bottom-content')
+const textBoxThree = textBoxTwo.cloneNode(true)
+const textboxFour = textBoxTwo.cloneNode(true)
+bottomContent.appendChild(textBoxTwo)
+bottomContent.appendChild(textBoxThree)
+bottomContent.appendChild(textBoxFour)
+
+
+// contact
+
+// footer
