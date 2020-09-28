6d20e65817910cea7b40a5a11c7a1a39df349c7e
will Fletcher <wfletch7@gmail.com>
2020-09-28 17:45:00 -0400

    finished matching the original to index

--- js/index.js
+++ js/index.js
@@ -57,6 +57,7 @@ navigator.children[3].textContent = siteContent["nav"]["nav-item-4"]
 navigator.children[4].textContent = siteContent["nav"]["nav-item-5"]
 navigator.children[5].textContent = siteContent["nav"]["nav-item-6"]
 
+
 let h1 = document.querySelector("h1")
 h1.textContent = siteContent["cta"]["h1"]
 
@@ -67,11 +68,63 @@ let circlePic = document.querySelector("#cta-img")
 circlePic.setAttribute('src', siteContent["cta"]["img-src"])
 
 
-const featureHeader = document.querySelector(".top-content")
+const featureHeader = document.querySelector(".text-content:nth-of-type(1) h4")
 featureHeader.textContent = siteContent["main-content"]["features-h4"]
-
-const featureP = document.querySelector(".top-content")
+const featureP = document.querySelector(".text-content:nth-of-type(1) p")
 featureP.textContent = siteContent["main-content"]["features-content"]
 
-// const aboutHeader = document.querySelector("h4:nth-of-type(2)")
-// aboutHeader.textContent = siteContent["main-content"]["about-h4"]
+const aboutHeader = document.querySelector(".text-content:nth-of-type(2) h4")
+aboutHeader.textContent = siteContent["main-content"]["about-h4"]
+
+const aboutP = document.querySelector(".text-content:nth-of-type(2) p")
+aboutP.textContent = siteContent["main-content"]["about-content"]
+
+let hackerPic = document.querySelector("#middle-img")
+hackerPic.setAttribute('src', siteContent["main-content"]["middle-img-src"])
+
+const servicesH = document.querySelector(".bottom-content .text-content:nth-of-type(1) h4")
+servicesH.textContent = siteContent['main-content']['services-h4']
+const servicesP = document.querySelector(".bottom-content .text-content:nth-of-type(1) p")
+servicesP.textContent = siteContent['main-content']['services-content']
+const productH = document.querySelector(".bottom-content .text-content:nth-of-type(2) h4")
+productH.textContent = siteContent['main-content']['product-h4']
+const productP = document.querySelector(".bottom-content .text-content:nth-of-type(2) p")
+productP.textContent = siteContent['main-content']['product-content']
+const visionH = document.querySelector(".bottom-content .text-content:nth-of-type(3) h4")
+visionH.textContent = siteContent['main-content']['vision-h4']
+const visionP = document.querySelector(".bottom-content .text-content:nth-of-type(3) p")
+visionP.textContent = siteContent['main-content']['vision-content']
+
+
+const contactH = document.querySelector('.contact h4')
+contactH.textContent = siteContent['contact']['contact-h4']
+const contactA = document.querySelector('.contact p:nth-of-type(1)')
+contactA.textContent = siteContent['contact']['address']
+const contactP = document.querySelector('.contact p:nth-of-type(2)')
+contactP.textContent = siteContent['contact']['phone']
+const contactE = document.querySelector('.contact p:nth-of-type(3)')
+contactE.textContent = siteContent['contact']['email']
+
+const footer = document.querySelector('footer')
+footer.textContent = siteContent["footer"]['copyright']
+
+const newLink = document.createElement("a")
+newLink.textContent = "eggstra"
+newLink.id = "BlogLink"
+newLink.href = '#'
+
+document.querySelector("nav").appendChild(newLink)
+
+const newestLink = document.createElement("a")
+newestLink.textContent = "blogged"
+newLink.href = "#"
+document.querySelector("nav").appendChild(newestLink)
+
+newLink.style.color = "green"
+newestLink.style.color = "green"
+navigator.children[0].style.color = "green"
+navigator.children[1].style.color = "green"
+navigator.children[2].style.color = "green"
+navigator.children[3].style.color = "green"
+navigator.children[4].style.color = "green"
+navigator.children[5].style.color = "green"
