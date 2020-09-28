3611464b5414e6a5bfc77e2448d35947d2469639
Benaiah Varner <benaiahvarner@Benaiahs-MBP.home>
2020-09-28 18:54:56 -0400

    done

--- js/index.js
+++ js/index.js
@@ -40,3 +40,59 @@ const siteContent = {
 // Example: Update the img src for the logo
 let logo = document.getElementById("logo-img");
 logo.setAttribute('src', siteContent["nav"]["img-src"])
+// NAV
+let nav = document.querySelector('nav')
+nav.children[0].textContent  = 'Services'
+nav.children[1].textContent  = 'Product'
+nav.children[2].textContent  = 'Vision'
+nav.children[3].textContent  = 'Features'
+nav.children[4].textContent  = 'About'
+nav.children[5].textContent  = 'Contact'
+
+// CTA
+let h1 = document.querySelector('h1')
+h1.textContent = "DOM Is Awesome"
+
+let button = document.querySelector('button')
+button.textContent = "Get Started"
+
+let snippet = document.getElementById("cta-img");
+snippet.setAttribute('src', siteContent["cta"]["img-src"])
+// MAIN CONTENT
+
+
+
+let middleImg = document.getElementById("middle-img");
+middleImg.setAttribute('src', siteContent["main-content"]["middle-img-src"])
+const textContent = document.querySelectorAll('.text-content')
+
+const firstText = textContent[0]
+console.log(firstText)
+firstText.children[0].textContent = "Features"
+firstText.children[1].textContent = "Features content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis."
+
+const secondText = textContent[1]
+secondText.children[0].textContent = "About"
+secondText.children[1].textContent = "About content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis."
+
+const thirdText = textContent[2]
+thirdText.children[0].textContent = "Services"
+thirdText.children[1].textContent = "Services content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis."
+
+const fourthText = textContent[3]
+fourthText.children[0].textContent= "Product"
+fourthText.children[1].textContent= "Product content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis."
+
+const fifthText = textContent[4]
+fifthText.children[0].textContent= "Vision"
+fifthText.children[1].textContent= "Vision content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis."
+//CONTACT
+
+const contact = document.querySelector('.contact')
+contact.children[0].textContent = "Contact"
+contact.children[1].textContent = "123 Way 456 Street Somewhere, USA"
+contact.children[2].textContent = "1 (888) 888-8888"
+contact.children[3].textContent = "sales@greatidea.io"
+
+const footer = document.querySelector('footer')
+footer.firstChild.textContent = "Copyright Great Idea! 2018"
