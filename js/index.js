db5eb89a09eb9dbbc8797b6dc748fe71b23648ed
Sarah Mucho <64818980+SJMucho@users.noreply.github.com>
2020-09-29 22:01:05 -0400

    code snippet

--- js/index.js
+++ js/index.js
@@ -40,3 +40,6 @@ const siteContent = {
 // Example: Update the img src for the logo
 let logo = document.getElementById("logo-img");
 logo.setAttribute('src', siteContent["nav"]["img-src"])
+
+const codeSnippet = document.getElementById('cta-img')
+codeSnippet.src = "img/header-img.png"
