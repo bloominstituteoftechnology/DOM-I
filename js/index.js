fe1dd45b349dd63031638c467bc799e81bf82bd7
Rex-1031 <64785141+Rex-1031@users.noreply.github.com>
2020-09-30 21:16:51 -0400

    some mvp complete

--- js/index.js
+++ js/index.js
@@ -40,3 +40,14 @@ const siteContent = {
 // Example: Update the img src for the logo
 let logo = document.getElementById("logo-img");
 logo.setAttribute('src', siteContent["nav"]["img-src"])
+
+
+let headerImg = document.getElementById("cta-img");
+headerImg.setAttribute('src', siteContent['cta']['img-src'])
+
+
+let middleImg = document.getElementById("middle-img");
+middleImg.setAttribute('src', siteContent['main-content']['middle-img-src'])
+
+
+
