d844e3e12261f5622bd3f6b1365727d0497e7887
Shawn <shawn.harrington2776@gmail.com>
2020-10-26 15:22:52 -0700

    finished assignment

--- js/index.js
+++ js/index.js
@@ -40,3 +40,44 @@ const siteContent = {
 // Example: Update the img src for the logo
 let logo = document.getElementById("logo-img");
 logo.setAttribute('src', siteContent["nav"]["img-src"])
+
+// Updates the text content for the Nav Links
+let navItem = document.querySelectorAll("nav a");
+let navItemContent = ["Services", "Product", "Vision", "Features", "About", "Contact"]
+
+navItem.forEach(function(item, index){
+  item.textContent = navItemContent[index];
+});
+
+// Updates the text content for the H1
+let newH1 = document.querySelector("h1");
+newH1.innerText = `DOM \nIS\n AWESOME`;
+
+// Updates the text content for the button
+let newButtonText = document.querySelector(".cta-text button");
+newButtonText.textContent = "Get Started";
+
+// Updates the text content for the section headers
+let newH4s = document.querySelectorAll("h4");
+let newH4Content = ["Features", "About", "Services", "Product", "Vision", "Contact"]
+
+newH4s.forEach(function(item, index){
+  item.textContent = newH4Content[index];
+});
+
+// Updates the text content for the section paragraphs
+let newParagraphs = document.querySelectorAll("p");
+let newPTextContent = ["Features content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.","About content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.","Services content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.","Product content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.","Vision content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.","123 Way\n 456 Street\n Somewhere, USA","1 (888) 888-8888","sales@greatidea.io","Copyright Great Idea! 2018"]
+
+newParagraphs.forEach(function(item, index){
+  item.innerText = newPTextContent[index];
+});
+
+let newCTAImage = document.querySelector("#cta-img");
+newCTAImage.setAttribute('src', siteContent["cta"]["img-src"])
+
+
+let ctaMiddleImage = document.querySelector("#middle-img");
+console.log(ctaMiddleImage)
+
+ctaMiddleImage.src = "img/mid-page-accent.jpg"
