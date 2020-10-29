562b407f3f2d5dc40aa849a56c260be4cfca9fee
mostafawanas88 <mostafawanas@hotmail.com>
2020-10-29 16:10:05 +0400

    first commit

--- js/index.js
+++ js/index.js
@@ -40,3 +40,65 @@ const siteContent = {
 // Example: Update the img src for the logo
 let logo = document.getElementById("logo-img");
 logo.setAttribute('src', siteContent["nav"]["img-src"])
+
+//Setting out all the NAV names to be in line with original.html
+let navItems = document.querySelectorAll('nav a');
+let arrNavItems = Array.from(navItems);
+arrNavItems[0].textContent = siteContent["nav"]["nav-item-1"];
+arrNavItems[1].textContent = siteContent["nav"]["nav-item-2"];
+arrNavItems[2].textContent = siteContent["nav"]["nav-item-3"];
+arrNavItems[3].textContent = siteContent["nav"]["nav-item-4"];
+arrNavItems[4].textContent = siteContent["nav"]["nav-item-5"];
+arrNavItems[5].textContent = siteContent["nav"]["nav-item-6"];
+// is there a way to do this with map 
+// let updatedArrItems = arrNavItems.map()
+
+
+//Section CTA: setting up first section (h1 text,button and image)
+
+  //title / H1
+  let title = document.querySelector('h1');
+  title.textContent = siteContent["cta"]["h1"];
+  let lineBreak = document.createElement('br');
+  title.insertAdjacentElement('beforeend',lineBreak);
+
+  // Title Button
+  let button = document.querySelector('button');
+  button.textContent = siteContent["cta"]["button"];
+
+  // Title image
+  let ctaImage = document.getElementById('cta-img');
+  ctaImage.setAttribute('src',siteContent["cta"]["img-src"]);
+
+  //Section Main-Content: setting up all the H4's
+  let topContentTitles1 = document.getElementsByTagName('h4');
+  let arrH4Items = Array.from(topContentTitles1);
+  arrH4Items[0].textContent = siteContent["main-content"]["features-h4"];
+  arrH4Items[1].textContent = siteContent["main-content"]["about-h4"];
+  arrH4Items[2].textContent = siteContent["main-content"]["services-h4"];
+  arrH4Items[3].textContent = siteContent["main-content"]["product-h4"];
+  arrH4Items[4].textContent = siteContent["main-content"]["vision-h4"];
+  arrH4Items[5].textContent = siteContent["contact"]["contact-h4"];
+
+  // Section Main Content: Setting up all the P's
+  let mainContentPs = document.getElementsByTagName('p');
+  let arrPItems = Array.from(mainContentPs);
+  arrPItems[0].textContent = siteContent["main-content"]["features-content"];
+  arrPItems[1].textContent = siteContent["main-content"]["about-content"];
+  arrPItems[2].textContent = siteContent["main-content"]["services-content"];
+  arrPItems[3].textContent = siteContent["main-content"]["product-content"];
+  arrPItems[4].textContent = siteContent["main-content"]["vision-content"];
+  arrPItems[5].textContent = siteContent["contact"]["address"];
+  arrPItems[6].textContent = siteContent["contact"]["phone"];
+  arrPItems[7].textContent = siteContent["contact"]["email"];
+  arrPItems[8].textContent = siteContent["footer"]["copyright"];
+
+  // Section Main Content: Image
+  let mainContentImg = document.getElementById('middle-img');
+  mainContentImg.setAttribute('src',siteContent["main-content"]['middle-img-src']);
+
+
+
+
+
+
