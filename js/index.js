d86a54aca854f247e7c952772e57eda5a12603cb
Shenica Graham <gshenica@gmail.com>
2020-10-28 22:29:31 -0500

    Almost complete MVP

--- js/index.js
+++ js/index.js
@@ -37,6 +37,113 @@ const siteContent = {
   },
 };
 
-// Example: Update the img src for the logo
-let logo = document.getElementById("logo-img");
-logo.setAttribute('src', siteContent["nav"]["img-src"])
+// Images
+
+    // Example: Update the img src for the logo
+    let logo = document.getElementById("logo-img");
+    logo.setAttribute('src', siteContent["nav"]["img-src"])
+
+
+    // Update the img src for the cta image
+    let ctaImg = document.getElementById("cta-img");
+    ctaImg.setAttribute('src', siteContent["cta"]["img-src"]);
+
+    // Update the img src for the middle image
+    let middleImg = document.getElementById("middle-img");
+    middleImg.setAttribute('src', siteContent["main-content"]["middle-img-src"]);
+
+// Navigation
+
+    // Update main nav bar
+    let navMenu = document.querySelectorAll("a");
+    navMenu[0].textContent = siteContent["nav"]["nav-item-1"];
+    navMenu[1].textContent = siteContent["nav"]["nav-item-2"];
+    navMenu[2].textContent = siteContent["nav"]["nav-item-3"];
+    navMenu[3].textContent = siteContent["nav"]["nav-item-4"];
+    navMenu[4].textContent = siteContent["nav"]["nav-item-5"];
+    navMenu[5].textContent = siteContent["nav"]["nav-item-6"];
+
+// Update the h1 heading
+const h1 = document.querySelector("h1");
+h1.textContent = "DOM Is Awesome";
+
+// Update the button text
+const button = document.querySelector("button");
+button.textContent = "Get Started";
+
+// Main Content Section
+
+    // Top Content Section
+
+        // Grab the .top-content h4 tags
+        let mainTopTextH4 = document.body.querySelectorAll(".main-content .top-content .text-content h4");
+
+            // Update the first .top-content h4 heading
+            mainTopTextH4[0].textContent = siteContent['main-content']["features-h4"];
+
+            // Update the second .top-content h4 heading
+            mainTopTextH4[1].textContent = siteContent['main-content']["about-h4"]
+
+        // Grab the .top-content p tags
+        let mainTopTextP = document.body.querySelectorAll(".main-content .top-content .text-content p");
+
+            // Update the first .top-content paragraph
+            mainTopTextP[0].textContent = siteContent['main-content']["features-content"];
+
+            // Update the second .top-content paragraph
+            mainTopTextP[1].textContent = siteContent['main-content']['about-content'];
+
+    // Bottom Content Section
+
+        // Grab the .bottom-content h4 tags
+        let mainBottomTextH4 = document.body.querySelectorAll(".main-content .bottom-content .text-content h4");
+
+            // Update the first .bottom-content h4 heading
+            mainBottomTextH4[0].textContent = siteContent['main-content']["services-h4"];
+
+            // Update the second .bottom-content h4 heading
+            mainBottomTextH4[1].textContent = siteContent['main-content']["product-h4"]
+
+            // Update the third .bottom-content h4 heading
+            mainBottomTextH4[2].textContent = siteContent['main-content']["vision-h4"]
+
+        // Grab the .bottom-content p tags
+        let mainBottomTextP = document.body.querySelectorAll(".main-content .bottom-content .text-content p");
+
+            // Update the first .bottom-content paragraph
+            mainBottomTextP[0].textContent = siteContent['main-content']["services-content"];
+
+            // Update the second .bottom-content paragraph
+            mainBottomTextP[1].textContent = siteContent['main-content']['product-content'];    
+
+            // Update the third .bottom-content paragraph
+                mainBottomTextP[2].textContent = siteContent['main-content']['vision-content'];    
+
+    // Contact Section
+
+        // Grab the contact h4 heading
+        let contactH4 = document.body.querySelector(".contact h4");
+
+            // Update the .contact h4 heading
+            contactH4.textContent = siteContent["contact"]["contact-h4"];
+
+        // Grab the .contact paragraph
+        let contactP = document.body.querySelectorAll(".contact p");
+
+            // Update the .contact address
+            contactP[0].textContent = siteContent['contact']["address"];
+
+            // Update the .contact phone number
+            contactP[1].textContent = siteContent['contact']["phone"];
+
+            // Update the .contact email
+            contactP[2].textContent = siteContent['contact']["email"];
+
+    // Footer
+
+        // Grab the footer paragraph
+        let footer = document.body.querySelector("footer p");
+
+            // Update the footer paragrahp copyright
+            footer.textContent = siteContent["footer"]["copyright"]
+        
