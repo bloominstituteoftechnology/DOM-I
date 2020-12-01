43b5a85f211b96d118390f18cf8b27faeea3e1f1
Andrew Sohrabi <andrewsohrabi@Andrews-MacBook-Pro.local>
2020-11-30 19:57:43 -0800

    changed innerText of nav anchor tags

--- js/index.js
+++ js/index.js
@@ -41,7 +41,42 @@ const siteContent = {
 let logo = document.getElementById("logo-img");
 logo.setAttribute('src', siteContent["nav"]["img-src"]);
 
-const servicesLink = document.createElement("a");
-servicesLink.textContent = "Services";
-servicesLink.href = "#";
-document.querySelector("nav").appendChild(servicesLink);
+// Question: if the anchor tags already exist, is what I am doing below redundant?
+// const servicesLink = document.createElement("a");
+// servicesLink.textContent = "Services";
+// servicesLink.href = "#";
+// document.querySelector("nav").appendChild(servicesLink);
+
+// const productLink = servicesLink.nextElementSibling;
+// productLink.textContent = "Product";
+
+// const visionLink = productLink.nextElementSibling;
+// visionLink.textContent = "Vision";
+
+// const featuresLink = visionLink.nextElementSibling;
+// featuresLink.textContent = 'Features'
+
+// const aboutLink = featuresLink.nextElementSibling;
+// aboutLink.textContent = "About"
+
+// const contactLink = aboutLink.nextElementSibling;
+// contactLink.textContent='Contact'
+
+// const servicesLink = document.createElement("a");
+// servicesLink.textContent = "Services";
+// servicesLink.href = "#";
+// document.querySelector("nav").appendChild(servicesLink);
+
+// const productLink = document.createElement("a");
+// productLink.textContent = "Product";
+// productLink.href = "#";
+// document.querySelector("nav").appendChild(productLink);
+
+
+const nav = document.querySelectorAll('nav a');
+nav[0].innerText = siteContent['nav']['nav-item-1'];
+nav[1].innerText = siteContent['nav']['nav-item-2'];
+nav[2].innerText = siteContent['nav']['nav-item-3'];
+nav[3].innerText = siteContent['nav']['nav-item-4'];
+nav[4].innerText = siteContent['nav']['nav-item-5'];
+nav[5].innerText = siteContent['nav']['nav-item-6'];
