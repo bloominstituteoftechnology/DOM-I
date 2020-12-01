a0a4649ce13d3643fb1cc66b38f73808be8dbfdb
Andrew Sohrabi <andrewsohrabi@Andrews-MacBook-Pro.local>
2020-11-30 17:34:26 -0800

    for the PR

--- js/index.js
+++ js/index.js
@@ -39,4 +39,9 @@ const siteContent = {
 
 // Example: Update the img src for the logo
 let logo = document.getElementById("logo-img");
-logo.setAttribute('src', siteContent["nav"]["img-src"])
+logo.setAttribute('src', siteContent["nav"]["img-src"]);
+
+const servicesLink = document.createElement("a");
+servicesLink.textContent = "Services";
+servicesLink.href = "#";
+document.querySelector("nav").appendChild(servicesLink);
