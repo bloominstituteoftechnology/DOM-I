436d0a6ddfc2f76f6a3ff9bd49222f15d551c65d
Ivory Johnson-Wheeler <ivy@Ivorys-MacBook-Air.local>
2020-12-02 20:31:47 -0500

    forgot to do my commits while working will never do that again!

--- js/index.js
+++ js/index.js
@@ -40,3 +40,55 @@ const siteContent = {
 // Example: Update the img src for the logo
 let logo = document.getElementById("logo-img");
 logo.setAttribute('src', siteContent["nav"]["img-src"])
+
+
+let navText = document.querySelectorAll("nav a");
+navText[0].innerHTML = siteContent["nav"]["nav-item-1"];
+navText[1].innerHTML = siteContent["nav"]["nav-item-2"];
+navText[2].innerHTML = siteContent["nav"]["nav-item-3"];
+navText[3].innerHTML = siteContent["nav"]["nav-item-4"];
+navText[4].innerHTML = siteContent["nav"]["nav-item-5"];
+navText[5].innerHTML = siteContent["nav"]["nav-item-6"];
+navText.forEach((item, index) => {
+  item.style.color = "green";
+});
+let ctaText = document.querySelector(".cta-text h1");
+ctaText.innerHTML = siteContent["cta"]["h1"];
+let ctaButton = document.querySelector(".cta-text button");
+ctaButton.innerHTML = siteContent["cta"]["button"];
+let headerImg = document.getElementById("cta-img");
+headerImg.setAttribute("src", siteContent["cta"]["img-src"]);
+let textContent = document.querySelectorAll(".text-content");
+textContent[0].getElementsByTagName("h4")[0].innerHTML =
+  siteContent["main-content"]["features-h4"];
+textContent[0].getElementsByTagName("p")[0].innerHTML =
+  siteContent["main-content"]["features-content"];
+textContent[1].getElementsByTagName("h4")[0].innerHTML =
+  siteContent["main-content"]["about-h4"];
+textContent[1].getElementsByTagName("p")[0].innerHTML =
+  siteContent["main-content"]["about-content"];
+textContent[2].getElementsByTagName("h4")[0].innerHTML =
+  siteContent["main-content"]["services-h4"];
+textContent[2].getElementsByTagName("p")[0].innerHTML =
+  siteContent["main-content"]["services-content"];
+textContent[3].getElementsByTagName("h4")[0].innerHTML =
+  siteContent["main-content"]["product-h4"];
+textContent[3].getElementsByTagName("p")[0].innerHTML =
+  siteContent["main-content"]["product-content"];
+textContent[4].getElementsByTagName("h4")[0].innerHTML =
+  siteContent["main-content"]["vision-h4"];
+textContent[4].getElementsByTagName("p")[0].innerHTML =
+  siteContent["main-content"]["vision-content"];
+let midPageAccent = document.getElementById("middle-img");
+midPageAccent.src = siteContent["main-content"]["middle-img-src"];
+let contact = document.querySelectorAll(".contact");
+contact[0].getElementsByTagName("h4")[0].innerHTML =
+  siteContent["contact"]["contact-h4"];
+contact[0].getElementsByTagName("p")[0].innerHTML =
+  siteContent["contact"]["address"];
+contact[0].getElementsByTagName("p")[1].innerHTML =
+  siteContent["contact"]["phone"];
+contact[0].getElementsByTagName("p")[2].innerHTML =
+  siteContent["contact"]["email"];
+let footer = document.querySelector("footer p");
+footer.innerHTML = siteContent["footer"]["copyright"];
