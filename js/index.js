1ce253dba9117b8def7e50014722a95e678aceb6
Molly Brown <mollybrown0516@gmail.com>
2020-12-03 16:05:07 -0500

    Completed project, minus append and preprend task

--- js/index.js
+++ js/index.js
@@ -1,9 +1,4 @@
 
-
-
-
-
-
 const siteContent = {
   "nav": {
     "nav-item-1": "Services",
@@ -47,63 +42,62 @@ const siteContent = {
 let logo = document.getElementById('logo-img');
 logo.setAttribute('src', siteContent['nav']['img-src'])
 
+//NAVIGATION
 let navAttributes = document.querySelectorAll('nav a');
-//console.log('navAttributes', navAttributes);
+console.log('navAttributes', navAttributes);
 navAttributes[0].textContent = 'Services';
-//console.log(navAttributes[0]);
+console.log(navAttributes[0]);
 navAttributes[1].textContent = 'Product';
 navAttributes[2].textContent = 'Vision';
 navAttributes[3].textContent = 'Features';
 navAttributes[4].textContent = 'About';
 navAttributes[5].textContent = 'Contact';
 
+//H1 HEADING - DOM is Awesome & CIRCLE IMAGE
+//H1
 let firstHeading = document.querySelector('h1');
 console.log(firstHeading);
 
 firstHeading.textContent = "DOM Is Awesome";
-
+//START BUTTON
+const startButton = firstHeading.nextElementSibling;
+console.log(startButton);
+startButton.textContent = "Get Started";
+//CIRCLE IMAGE
 let circleImage = document.getElementById('cta-img');
-//console.log(circleImage);
+console.log(circleImage);
 circleImage.src = "img/header-img.png";
 
+//MIDDLE HORIZONTAL IMAGE
 let midImage = document.querySelector('.middle-img');
 console.log(midImage);
 midImage.src = "img/mid-page-accent.jpg";
 
 
-const startButton = firstHeading.nextElementSibling;
-console.log(startButton);
-startButton.textContent = "Get Started";
+//TEXT-CONTENT 
 
-//Top Content
+//TOP CONTENT - middle of page
 const topContentContainerDiv = document.querySelectorAll('.top-content .text-content');
 const firstTextContent = topContentContainerDiv[0];
 const secondTextContent = topContentContainerDiv[1];
-// First
+// First - features
 const firstTextContentTitleElement = firstTextContent.querySelector("h4");
 const firstTextContentParagraphElement = firstTextContent.querySelector("p");
 firstTextContentTitleElement.textContent = "Features";
 firstTextContentParagraphElement.textContent = "Features content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.";
-// Second
-//const firstTextContentTitleElement = secondTextContent.querySelector("h4");
-//const firstTextContentParagraphElement = secondTextContent.querySelector("p");
-//firstTextContentTitleElement.textContent = "Features";
-//firstTextContentParagraphElement.textContent = "Features content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.";
-
+// Second -about
 const secondTextContentTitleElement = secondTextContent.querySelector('h4');
 const secondTextContentParagraphElement = secondTextContent.querySelector('p');
 secondTextContentTitleElement.textContent = "About";
 secondTextContentParagraphElement.textContent = "About content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.";
 
-// BOTTOM CONTENT
-
+// BOTTOM CONTENT - middle of page
 const bottomContentContainerDiv = document.querySelectorAll('.bottom-content .text-content');
 const thirdTextContent = bottomContentContainerDiv[0];
 const fourthTextContent = bottomContentContainerDiv[1];
 const fifthTextContent = bottomContentContainerDiv[2];
 console.log(bottomContentContainerDiv);
 //First - Services
-
 const thirdTextContentTitleElement = thirdTextContent.querySelector('h4');
 thirdTextContentTitleElement.textContent = "Services";
 const thirdTextContentParagraphElement = thirdTextContent.querySelector('p');
@@ -119,11 +113,42 @@ fifthTextContentTitleElement.textContent = "Vision";
 const fifthTextContentParagraphElement = fifthTextContent.querySelector('p');
 fifthTextContentParagraphElement.textContent = "Vision content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, ininterdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metusscelerisque quis.";
 
+//CONTACT INFO - bottom of page
+
+const contactFooterInformation = document.querySelector('.contact h4');
+contactFooterInformation.textContent = "Contact"; 
+
+const contactParagraphElements = document.querySelectorAll('.contact p');
+console.log(contactParagraphElements);
+const paragraphElementOne = contactParagraphElements[0];
+const paragraphElementTwo = contactParagraphElements[1];
+const paragraphElementThree = contactParagraphElements[2];
+
+paragraphElementOne.textContent = "123 Way 456 Street Somewhere, USA";
+paragraphElementTwo.textContent = "1 (888) 888-8888";
+paragraphElementThree.textContent = "sales@greatidea.io";
+
+//FOOTER - copyright info
+
+const footerCopyRight = document.querySelector('footer p');
+console.log(footerCopyRight);
+footerCopyRight.textContent = "Copyright Great Idea! 2018";
+
+//ADD NEW CONTENT MVP
+//CHANGE NAVIGATION COLOR
+navAttributes[0].style.color = 'green';
+navAttributes[1].style.color = "green";
+navAttributes[2].style.color = 'green';
+navAttributes[3].style.color = 'green';
+navAttributes[4].style.color = 'green';
+navAttributes[5].style.color = 'green';
+
+//APPEND CHILD/ PREPREND CHILD
 
 /////////////////////////////////////TO DO TODAY//////////////////////////////////////////////////////////////////////////////////////////
-/////*[x] Need to complete this second text-content above, then do the same for the bottom three, complete the footer contact
-/////* Need to organize and clean up my code with comments 
-/////*Need to complete the Add New Content section of the README
+/////*[x] Need to complete this second text-content above, [x]then do the same for the bottom three, [x]complete the footer contact
+/////*[x] Need to organize and clean up my code with comments 
+/////*[x]Need to complete the Add New Content section of the README
 
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
