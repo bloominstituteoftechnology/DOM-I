db4df63de6442ded0df3a1852057b7331bb50956
Vital Boisset <vital.boisset@gmail.com>
2020-12-03 16:05:20 -0500

    MVP Reached

--- js/index.js
+++ js/index.js
@@ -46,60 +46,85 @@ const siteContent = {
 let logo = document.getElementById("logo-img");
 logo.setAttribute("src", siteContent["nav"]["img-src"]);
 
+// ---------------------- My Code -------------------
+
+// ########## HEADER ##########
+
 // ---- Navigation Bar ----
 let anchors = document.querySelectorAll('nav a');
-for(var i=0; i<anchors.length; i++) {
-  anchors[i].innerHTML = siteContent["nav"][`nav-item-${i + 1}`]
+for(var i=0; i<anchors.length; i++) { // loop through list of anchors
+  anchors[i].innerHTML = siteContent["nav"][`nav-item-${i + 1}`] // update each nav link
 };
 
+// add 2 nav links
+const navLink1 = document.createElement('a');
+const navLink2 = document.createElement('a');
 
-// ---- CTA -----------
-let h1 = document.querySelector(".cta-text h1");
-h1.innerHTML = siteContent["cta"]["h1"];
+// name the nav links
+navLink1.textContent = 'Home';
+navLink2.textContent = 'Blog';
 
-let button = document.querySelector(".cta-text button");
-button.innerHTML = siteContent["cta"]["button"];
+// add nav links to navbar
+document.querySelector('nav').prepend(navLink1);
+document.querySelector('nav').appendChild(navLink2);
 
-let ctaImage = document.getElementById("cta-img");
-ctaImage.setAttribute("src", siteContent["cta"]["img-src"]);
-
-// ---- Main Content (Top) ------
-let h4Features = document.querySelector(".main-content .top-content h4");
-h4Features.innerHTML = siteContent["main-content"]["features-h4"];
-
-let featuresContent = document.querySelector(".main-content .top-content p");
-featuresContent.innerHTML = siteContent["main-content"]["features-content"];
-
-let aboutHeader = document.querySelector(".main-content .top-content h4");
-aboutHeader.innerHTML = siteContent["main-content"]["about-h4"];
-
-let aboutContent = document.querySelector(".main-content .top-content p");
-aboutContent.innerHTML = siteContent["main-content"]["about-content"];
+// make all nav links green
+const navStyle = document.querySelectorAll('a');
+const navStyleArray = Array.from(navStyle);
+for(let i = 0; i < navStyleArray.length; i++){
+  navStyleArray[i].style.color = 'green';
+}
 
 
+// ########## HERO ##########
 
-// ---- Contact ---------
+let h1 = document.querySelector(".cta-text h1"); // Select h1
+h1.innerHTML = siteContent["cta"]["h1"]; // Update it
 
+let button = document.querySelector(".cta-text button"); // Select button
+button.innerHTML = siteContent["cta"]["button"]; // Update it
 
-// ---- Footer -------
+let ctaImage = document.getElementById("cta-img"); // Select cta image
+ctaImage.setAttribute("src", siteContent["cta"]["img-src"]); // Update it
 
+// ########## MAIN ##########
 
-// --------------------------------
+const mainHeader = document.querySelectorAll(".text-content h4"); // Select main headers
+const mainContent = document.querySelectorAll(".text-content p"); // Select main content
+const mainImg = document.getElementById("middle-img"); // Select main image
 
+// ---- Features ----
+mainHeader[0].textContent = siteContent["main-content"]["features-h4"]; // Update blurb header
+mainContent[0].textContent = siteContent["main-content"]["features-content"]; // Update blurb content
 
-//Top Bar
-//-- Nav Bar
-//-- Logo
+// ---- About ----
+mainHeader[1].textContent = siteContent["main-content"]["about-h4"];
+mainContent[1].textContent = siteContent["main-content"]["about-content"];
 
-//Header (CTA)
-//-- Header Txt
-//-- Button
+// ---- Middle Image ----
+mainImg.src = siteContent["main-content"]["middle-img-src"];
 
-//Main Content
-//-- Top Content
-//-- Middle Content
-//-- Bottom Content
+// ---- Services ----
+mainHeader[2].textContent = siteContent["main-content"]["services-h4"];
+mainContent[2].textContent = siteContent["main-content"]["services-content"];
 
-//Contact
+// ---- Product ----
+mainHeader[3].textContent = siteContent["main-content"]["product-h4"];
+mainContent[3].textContent = siteContent["main-content"]["product-content"];
 
-//Footer
+// ---- Vision ----
+mainHeader[4].textContent = siteContent["main-content"]["vision-h4"];
+mainContent[4].textContent = siteContent["main-content"]["vision-content"];
+
+// ########## FOOTER ##########
+const contactHeader = document.querySelector(".contact h4"); // Select contact headers
+const contactContent = document.querySelectorAll(".contact p"); // Select contact content
+const copyrightContent = document.querySelector("footer"); // Select copyright content
+
+contactHeader.textContent = siteContent["contact"]["contact-h4"]; // update contact header
+
+contactContent[0].textContent = siteContent["contact"]["address"]; // update contact content
+contactContent[1].textContent = siteContent["contact"]["phone"]; 
+contactContent[2].textContent = siteContent["contact"]["email"];
+
+copyrightContent.textContent = siteContent["footer"]["copyright"]; // update copyright content
