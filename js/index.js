77d5b28ba9c5e29819ea1159c75459851025c812
samuel singletary <codysingletary92@gmail.com>
2020-12-02 22:46:40 -0500

    finished

--- js/index.js
+++ js/index.js
@@ -45,9 +45,75 @@ logo.setAttribute('src', siteContent["nav"]["img-src"])
 
 let navBar = document.querySelectorAll('nav a');
 
-console.log(siteContent.nav["nav-item-1"])
-
 for (i = 0; i < navBar.length; i++){
   navBar[i].textContent = siteContent.nav[`nav-item-${i}`]
-  console.log(navBar[i])
+  navBar[i].style.color = "green" 
 }
+
+let newNav1 = document.createElement("a");
+newNav1.setAttribute("href", '#')
+newNav1.style.color = "green"
+newNav1.textContent = "NEWNAV1"
+
+let newNav2 = document.createElement("a");
+newNav2.setAttribute("href", '#')
+newNav2.style.color = "green"
+newNav2.textContent = "NEWNAV2"
+
+let nav = document.querySelector('nav');
+nav.appendChild(newNav1)
+nav.prepend(newNav2)
+
+lineBreak = document.createElement("br");
+let h1 = document.querySelector('h1')
+h1.innerHTML = "DOM <br> IS <br> AWESOME"
+
+let ctaImg = document.querySelector('#cta-img');
+ctaImg.src = siteContent["cta"]["img-src"]
+let ctaButton = h1.nextElementSibling
+ctaButton.textContent = "Get Started"
+
+let topContent = document.querySelector('.top-content');
+let features = topContent.firstElementChild
+let featuresh4 = features.firstElementChild
+featuresh4.textContent = siteContent["main-content"]["features-h4"]
+let featuresP = featuresh4.nextElementSibling;
+featuresP.textContent = siteContent["main-content"]["features-content"]
+let about = features.nextElementSibling;
+let abouth4 = about.firstElementChild;
+abouth4.textContent = siteContent["main-content"]["about-h4"];
+let aboutP = abouth4.nextElementSibling;
+aboutP.textContent = siteContent["main-content"]["about-content"]
+
+let middleImg = document.querySelector('#middle-img')
+middleImg.src = siteContent["main-content"]["middle-img-src"]
+
+let bottomContent = document.querySelector('.bottom-content');
+let services = bottomContent.firstElementChild;
+let servicesh4 = services.firstElementChild;
+servicesh4.textContent = siteContent["main-content"]["services-h4"]
+let servicesP = servicesh4.nextElementSibling;
+servicesP.textContent = siteContent["main-content"]["services-content"]
+let product = services.nextElementSibling;
+let producth4 = product.firstElementChild;
+producth4.textContent = siteContent["main-content"]["product-h4"]
+let productP = producth4.nextElementSibling;
+productP.textContent = siteContent["main-content"]["product-content"]
+let vision = product.nextElementSibling;
+let visionh4 = vision.firstElementChild;
+visionh4.textContent = siteContent["main-content"]["vision-h4"]
+let visionP = visionh4.nextElementSibling;
+visionP.textContent = siteContent["main-content"]["vision-content"]
+
+let contact = document.querySelector('.contact');
+let contacth4 = contact.firstElementChild
+contacth4.textContent = siteContent["contact"]["contact-h4"]
+let contactAddress = contacth4.nextElementSibling;
+contactAddress.textContent = siteContent["contact"]["address"]
+let contactPhone = contactAddress.nextElementSibling;
+contactPhone.textContent = siteContent["contact"]["phone"]
+let contactEmail = contactPhone.nextElementSibling;
+contactEmail.textContent = siteContent["contact"]["email"]
+
+let footer = document.querySelector('footer p')
+footer.textContent = siteContent["footer"]["copyright"]
