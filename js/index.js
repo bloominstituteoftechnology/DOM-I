853107d499a34c58d34cb645740a453aafe8382f
Jonathan Garcia <70542639+CoderJonathan@users.noreply.github.com>
2021-01-12 00:42:51 -0800

    MVP

--- js/index.js
+++ js/index.js
@@ -40,3 +40,58 @@ const siteContent = {
 // Example: Update the img src for the logo
 let logo = document.getElementById("logo-img");
 logo.setAttribute('src', siteContent["nav"]["img-src"])
+const nav = document.querySelector("nav");
+const navBar = document.querySelectorAll("a");
+const allImgs = document.querySelectorAll("img");
+
+const allH1 = document.querySelector("h1");
+const button = document.querySelector("button");
+
+const allh4 = document.querySelectorAll("h4");
+const allPara = document.querySelectorAll("p");
+
+allh4.forEach((h) => {
+	h.style.color = "green";
+});
+
+navBar[0].textContent = "Services";
+navBar[1].textContent = "Product";
+navBar[2].textContent = "Vision";
+navBar[3].textContent = "Features";
+navBar[4].textContent = "About";
+navBar[5].textContent = "Contact";
+
+navBar.forEach((l) => {
+	l.style.color = "green";
+});
+
+allImgs[1].setAttribute("src", siteContent["cta"]["img-src"]);
+allImgs[2].setAttribute("src", siteContent["main-content"]["middle-img-src"]);
+
+allH1.innerHTML = "Dom <br>Is <br>Awesome!";
+button.textContent = siteContent["cta"]["button"];
+button.addEventListener("click", (event) => {
+	button.style.backgroundColor = "red";
+});
+
+allh4[0].textContent = siteContent["main-content"]["features-h4"];
+allPara[0].textContent = siteContent["main-content"]["features-content"];
+
+allh4[1].textContent = siteContent["main-content"]["about-h4"];
+allPara[1].textContent = siteContent["main-content"]["about-content"];
+
+allh4[2].textContent = siteContent["main-content"]["services-h4"];
+allPara[2].textContent = siteContent["main-content"]["services-content"];
+
+allh4[3].textContent = siteContent["main-content"]["features-h4"];
+allPara[3].textContent = siteContent["main-content"]["features-content"];
+
+allh4[4].textContent = siteContent["main-content"]["vision-h4"];
+allPara[4].textContent = siteContent["main-content"]["vision-content"];
+
+allh4[5].textContent = siteContent["contact"]["contact-h4"];
+allPara[5].textContent = siteContent["contact"]["address"];
+allPara[6].textContent = siteContent["contact"]["phone"];
+allPara[7].textContent = siteContent["contact"]["email"];
+
+allPara[8].textContent = siteContent["footer"]["copyright"];
