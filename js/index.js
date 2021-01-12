83f23fabdf98bb9545e30d77251d24a530bb5289
BradleyHubbs <75264076+BradleyHubbs@users.noreply.github.com>
2021-01-11 19:32:33 -0500

    Finished Project

--- js/index.js
+++ js/index.js
@@ -40,3 +40,56 @@ const siteContent = {
 // Example: Update the img src for the logo
 let logo = document.getElementById("logo-img");
 logo.setAttribute('src', siteContent["nav"]["img-src"])
+
+const headerNav = document.querySelectorAll('a');
+headerNav[0].textContent = 'Services';
+headerNav[1].textContent = 'Product';
+headerNav[2].textContent = 'Vision';
+headerNav[3].textContent = 'Features';
+headerNav[4].textContent = 'About';
+headerNav[5].textContent = 'Contact';
+
+headerNav.forEach( link => link.style.color = "green");
+
+
+const firstSection = document.querySelector('.cta-text');
+
+const largeText = firstSection.querySelector('h1');
+
+const firstButton = firstSection.querySelector('button');
+
+largeText.textContent = "DOM IS AWESOME";
+
+firstButton.textContent = 'Get Started';
+
+const firstImg = document.querySelector("#cta-img");
+
+firstImg.src = "img/header-img.png";
+
+const midImg = document.querySelector("#middle-img");
+
+midImg.src = "img/mid-page-accent.jpg";
+
+const mainH4 = document.querySelectorAll('h4')
+mainH4[0].textContent = siteContent["main-content"]["features-h4"]
+
+const mainH41 = document.querySelectorAll('h4')
+mainH4[1].textContent = siteContent["main-content"]["about-h4"]
+
+const mainH42 = document.querySelectorAll('h4')
+mainH4[2].textContent = siteContent["main-content"]["services-h4"]
+
+const mainH43 = document.querySelectorAll('h4')
+mainH4[3].textContent = siteContent["main-content"]["product-h4"]
+
+const mainH44 = document.querySelectorAll('h4')
+mainH4[4].textContent = siteContent["main-content"]["vision-h4"]
+
+const mainstuff = document.querySelector('.main-content');
+const mainParas = mainstuff.querySelectorAll("p");
+
+mainParas[0].textContent = "Features content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis."
+mainParas[1].textContent = "About content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis."
+mainParas[2].textContent = "Services content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis."
+mainParas[3].textContent = "Product content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis."
+mainParas[4].textContent = "Vision content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis."
