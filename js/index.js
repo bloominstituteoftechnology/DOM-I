ebbd615ffcc6e88abfba9f1b4e6a45a65dd078d2
andrewroti <andrewroti@gmail.com>
2021-01-14 01:38:02 -0500

    fixing to reference the object and not html

--- js/index.js
+++ js/index.js
@@ -41,18 +41,20 @@ const siteContent = {
 let logo = document.getElementById("logo-img");
 logo.setAttribute('src', siteContent["nav"]["img-src"])
 let ctaImg = document.getElementById("cta-img");
-ctaImg.src = "img/header-img.png";
+ctaImg.src = siteContent["cta"]["img-src"];
 let midImg = document.getElementById("middle-img");
-midImg.src = "img/mid-page-accent.jpg";
+midImg.src = siteContent["main-content"]["middle-img-src"];
 
 
 let navTxt = document.querySelectorAll("a");
-navTxt[0].textContent = "Services";
-navTxt[1].textContent = "Product";
-navTxt[2].textContent = "Vision";
-navTxt[3].textContent = "Features";
-navTxt[4].textContent = "About";
-navTxt[5].textContent = "Contact";
+navTxt[0].textContent = siteContent["nav"]["nav-item-1"];
+navTxt[1].textContent = siteContent["nav"]["nav-item-2"];;
+navTxt[2].textContent = siteContent["nav"]["nav-item-3"];;
+navTxt[3].textContent = siteContent["nav"]["nav-item-4"];;
+navTxt[4].textContent = siteContent["nav"]["nav-item-5"];;
+navTxt[5].textContent = siteContent["nav"]["nav-item-6"];;
+
+navTxt.forEach(element => element.style.color = "green");
 
 let buttonTxt = document.querySelector("button");
 buttonTxt.textContent = "Get Started";
@@ -61,20 +63,20 @@ let titleTxt = document.querySelector("h1");
 titleTxt.textContent = "DOM Is Awesome";
 
 let headerTxt = document.querySelectorAll("h4");
-headerTxt[0].textContent = "Features";
-headerTxt[1].textContent = "About";
-headerTxt[2].textContent = "Services";
-headerTxt[3].textContent = "Product";
-headerTxt[4].textContent = "Vision";
-headerTxt[5].textContent = "Contact";
+headerTxt[0].textContent = siteContent["main-content"]["features-h4"];
+headerTxt[1].textContent = siteContent["main-content"]["about-h4"];
+headerTxt[2].textContent = siteContent["main-content"]["services-h4"];
+headerTxt[3].textContent = siteContent["main-content"]["product-h4"];
+headerTxt[4].textContent = siteContent["main-content"]["vision-h4"];
+headerTxt[5].textContent = siteContent["contact"]["contact-h4"];
 
 let pTxt = document.querySelectorAll("p");
-pTxt[0].textContent = "Features content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio,in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metusscelerisque quis";
-pTxt[1].textContent = "About content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, ininterdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.";
-pTxt[2].textContent = "Services content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio,in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.";
-pTxt[3].textContent = "Product content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.";
-pTxt[4].textContent = "Vision content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.";
-pTxt[5].textContent = "123 Way 456 Street Somewhere, USA";
-pTxt[6].textContent = "1 (888) 888-8888";
-pTxt[7].textContent = "sales@greatidea.io";
-pTxt[8].textContent = "Copyright Great Idea! 2018";
+pTxt[0].textContent = siteContent["main-content"]["features-content"]; 
+pTxt[1].textContent = siteContent["main-content"]["about-content"];
+pTxt[2].textContent = siteContent["main-content"]["services-content"];
+pTxt[3].textContent = siteContent["main-content"]["product-content"];
+pTxt[4].textContent = siteContent["main-content"]["vision-content"];
+pTxt[5].textContent = siteContent["contact"]["address"];
+pTxt[6].textContent = siteContent["contact"]["phone"];
+pTxt[7].textContent = siteContent["contact"]["email"];
+pTxt[8].textContent = siteContent["footer"]["copyright"];
