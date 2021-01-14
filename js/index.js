d98c5cc50b745c88af72334b77a8da185f4552dc
unknown <allenmk20073@yahoo.com>
2021-01-13 23:19:35 -0500

    added navbar and images with header

--- js/index.js
+++ js/index.js
@@ -40,3 +40,28 @@ const siteContent = {
 // Example: Update the img src for the logo
 let logo = document.getElementById("logo-img");
 logo.setAttribute('src', siteContent["nav"]["img-src"])
+
+// NAV BAR
+let navItems = document.querySelectorAll('a');
+
+navItems[0].textContent = siteContent['nav']['nav-item-1'];
+navItems[1].textContent = siteContent['nav']['nav-item-2'];
+navItems[2].textContent = siteContent['nav']['nav-item-3'];
+navItems[3].textContent = siteContent['nav']['nav-item-4'];
+navItems[4].textContent = siteContent['nav']['nav-item-5'];
+navItems[5].textContent = siteContent['nav']['nav-item-6'];
+
+//CTA SECTION
+
+let header1 = document.getElementsByTagName('h1');
+header1[0].textContent = siteContent['cta']['h1'];
+
+//Button
+
+let button = document.getElementsByTagName('button');
+button[0].textContent = siteContent['cta']['button'];
+
+//Image
+
+let image1 = document.getElementById('cta-img');
+image1.setAttribute('src', siteContent['cta']['img-src']);
