7344ef42accf8e6d1915b06c078d296aa2a9edf9
bukit3point0 <bukit3point0@gmail.com>
2021-02-06 23:39:18 -0500

    Done for the night. Need to add append/prepend to hit MVP

--- js/index.js
+++ js/index.js
@@ -40,3 +40,57 @@ const siteContent = {
 // Example: Update the img src for the logo
 let logo = document.getElementById("logo-img");
 logo.setAttribute('src', siteContent["nav"]["img-src"])
+
+// Nav Bar
+const nav = document.querySelectorAll("nav a");
+nav[0].innerText = siteContent.nav["nav-item-1"];
+nav[1].innerText = siteContent.nav["nav-item-2"];
+nav[2].innerText = siteContent.nav["nav-item-3"];
+nav[3].innerText = siteContent.nav["nav-item-4"];
+nav[4].innerText = siteContent.nav["nav-item-5"];
+nav[5].innerText = siteContent.nav["nav-item-6"];
+
+// Add Nav Items
+
+const header = document.querySelector("header");
+
+
+// Nav Color
+for(let i=0; i<nav.length; i++) {
+  nav[i].style.color = "green";
+}
+
+// CTA section
+document.querySelector("h1").innerHTML = siteContent.cta["h1"];
+document.querySelector("button").innerHTML = siteContent.cta["button"];
+document.querySelector("#cta-img").setAttribute("src", siteContent["cta"]["img-src"])
+
+// Main Content
+const mainHeaders = document.querySelectorAll(".text-content h4");
+const mainContent = document.querySelectorAll(".text-content p");
+
+mainHeaders[0].innerText = siteContent["main-content"]["features-h4"];
+mainHeaders[1].innerText = siteContent["main-content"]["about-h4"];
+mainHeaders[2].innerText = siteContent["main-content"]["services-h4"];
+mainHeaders[3].innerText = siteContent["main-content"]["product-h4"];
+mainHeaders[4].innerText = siteContent["main-content"]["vision-h4"];
+
+mainContent[0].innerText = siteContent["main-content"]["features-content"];
+mainContent[1].innerText = siteContent["main-content"]["about-content"];
+mainContent[2].innerText = siteContent["main-content"]["services-content"];
+mainContent[3].innerText = siteContent["main-content"]["product-content"];
+mainContent[4].innerText = siteContent["main-content"]["vision-content"];
+
+document.querySelector("#middle-img").setAttribute("src", siteContent["main-content"]["middle-img-src"]);
+
+// Bottom Content
+
+const contactP = document.querySelectorAll(".contact p");
+
+document.querySelector(".contact h4").innerText = siteContent["contact"]["contact-h4"];
+contactP[0].innerHTML = siteContent["contact"]["address"];
+contactP[1].innerText = siteContent["contact"]["phone"];
+contactP[2].innerText = siteContent["contact"]["email"];
+
+// Footer
+document.querySelector("footer p").innerText = siteContent.footer["copyright"];
