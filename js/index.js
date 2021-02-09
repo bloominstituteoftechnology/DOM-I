3e47de839066783c39d9644f30ec797d98f5cc1c
Tarah <tarah.agbokhana@gmail.com>
2021-02-09 01:02:13 -0500

    2nd Commit

--- js/index.js
+++ js/index.js
@@ -39,17 +39,60 @@ const siteContent = {
 
 
 let navItems = document.querySelectorAll("nav a");
-for (let i = 0; i < navItems.length; i++) {
-  navItems[i].textContent = siteContent['nav'][`nav-item-${i+1}`];
-}
+// for (let i = 0; i < navItems.length; i++) {
+//   navItems[i].textContent = siteContent['nav'][`nav-item-${i+1}`];
+// }
+navItems.forEach((item, i) => {
+ item.textContent = siteContent['nav'][Object.keys(siteContent['nav'])[i]];
+});
 
 // Example: Update the img src for the logo
 let logo = document.getElementById("logo-img");
 logo.setAttribute('src', siteContent["nav"]["img-src"]);
 
-let ctaa = document.querySelectorAll(".cta div.cta-text");
-ct
-// let ctaItem1 = document.querySelector(".cta-text > h1");
-// ctaItem1.textContent = siteContent['cta']["h1"];
-// let ctaItem2 = document.querySelector(".cta-text > button");
-// ctaItem2.textContent = siteContent['cta']["button"];
+let ctaItem1 = document.querySelector(".cta-text > h1");
+ctaItem1.textContent = siteContent['cta']["h1"];
+let ctaItem2 = document.querySelector(".cta-text > button");
+ctaItem2.textContent = siteContent['cta']["button"];
+
+let ctaImg = document.getElementById("cta-img");
+ctaImg.setAttribute('src', siteContent["cta"]["img-src"]);
+
+let middleImg = document.getElementById("middle-img");
+middleImg.setAttribute('src', "img/mid-page-accent.jpg");
+
+//Create Array of SiteContent
+const siteContentArray = Object.entries(siteContent["main-content"]);
+
+let mainContent_h4 = [];
+let mainContent_p = [];
+
+//Loop through siteContentArray to find <h4> and <p> keys; return new arrays
+siteContentArray.forEach((item) => {
+  item[0].includes('h4') ? mainContent_h4.push(item[1]) 
+  : item[0].includes('content') ? mainContent_p.push(item[1]) 
+  : null;
+});
+// console.log(mainContent_h4);
+// console.log(mainContent_p);
+
+
+//Create variables to hold new <h4> and <p> content for .main-content section
+let h4Items = document.querySelectorAll(".main-content h4");
+let pItems = document.querySelectorAll(".main-content p");
+
+console.log(h4Items);
+console.log(pItems);
+
+//Loop through arrays to set new content.
+
+h4Items.forEach((item, i) => {
+  item.textContent = mainContent_h4[i];
+});
+
+
+pItems.forEach((item, i) => {
+  item.textContent = mainContent_p[i];
+});
+
+
