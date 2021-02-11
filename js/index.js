fb81da90fb7c20b750c1daf37fa732b31d5bea84
Tee Chandy <73080544+tpchandy@users.noreply.github.com>
2021-02-10 22:57:47 -0500

    created selectors and added contents to header and top section of webpage.

--- js/index.js
+++ js/index.js
@@ -40,3 +40,21 @@ const siteContent = {
 // Example: Update the img src for the logo
 let logo = document.getElementById("logo-img");
 logo.setAttribute('src', siteContent["nav"]["img-src"])
+
+  const navigation = document.querySelectorAll('a');//query for the navigation menu items.
+  console.log(navigation); // checking to ensure that all'a' tags have been picked.
+  navigation[0].textContent = ('Services');
+  navigation[1].textContent = ('Product');
+  navigation[2].textContent = ('Vision');
+  navigation[3].textContent = ('Features');
+  navigation[4].textContent = ('About');
+  navigation[5].textContent = ('Contact');
+
+  const heading = document.querySelector('h1'); // fetching the main heading and assigning textContent as in original HTML
+  heading.textContent = 'DOM is Awesome';
+
+  const mainImage = document.querySelector('#cta-img'); // queried the main image and set its attribute to the img provided in file.
+  mainImage.setAttribute('src','img/header-img.png');
+
+  const button = document.querySelector('div.cta-text button'); // query to add text to button in this section.
+  button.textContent = 'Get Started';
