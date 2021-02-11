947d44d7c8a014724722da1d85bc03bf8517ddbd
Mason Muhlestein <shadowmas12@gmail.com>
2021-02-10 17:34:52 -0700

    All done!

--- js/index.js
+++ js/index.js
@@ -40,3 +40,45 @@ const siteContent = {
 // Example: Update the img src for the logo
 let logo = document.getElementById("logo-img");
 logo.setAttribute('src', siteContent["nav"]["img-src"])
+
+let circleLogo = document.getElementById("cta-img")
+circleLogo.setAttribute('src', siteContent["cta"]["img-src"])
+
+let barLogo = document.getElementById("middle-img")
+barLogo.setAttribute('src', siteContent["main-content"]["middle-img-src"])
+
+let getStarted = "Get Started"; 
+document.querySelector("button").textContent = getStarted
+
+let domAwesome = "DOM IS AWESOME"
+document.querySelector("h1").textContent = domAwesome
+
+document.querySelector('nav').children[0].textContent = (siteContent["nav"]["nav-item-1"])
+document.querySelector('nav').children[1].textContent = (siteContent["nav"]["nav-item-2"])
+document.querySelector('nav').children[2].textContent = (siteContent["nav"]["nav-item-3"])
+document.querySelector('nav').children[3].textContent = (siteContent["nav"]["nav-item-4"])
+document.querySelector('nav').children[4].textContent = (siteContent["nav"]["nav-item-5"])
+document.querySelector('nav').children[5].textContent = (siteContent["nav"]["nav-item-6"])
+
+//let features = document.querySelector("h4");
+
+document.querySelector(".top-content").children[0].children[0].textContent = (siteContent["main-content"]["features-h4"])
+document.querySelector(".top-content").children[1].children[0].textContent = (siteContent["main-content"]["about-h4"])
+document.querySelector(".bottom-content").children[0].children[0].textContent = (siteContent["main-content"]["services-h4"])
+document.querySelector(".bottom-content").children[1].children[0].textContent = (siteContent["main-content"]["product-h4"])
+document.querySelector(".bottom-content").children[2].children[0].textContent = (siteContent["main-content"]["vision-h4"])
+
+document.querySelector(".top-content").children[0].children[1].textContent = (siteContent["main-content"]["features-content"])
+document.querySelector(".top-content").children[1].children[1].textContent = (siteContent["main-content"]["about-content"])
+document.querySelector(".bottom-content").children[0].children[1].textContent = (siteContent["main-content"]["services-content"])
+document.querySelector(".bottom-content").children[1].children[1].textContent = (siteContent["main-content"]["product-content"])
+document.querySelector(".bottom-content").children[2].children[1].textContent = (siteContent["main-content"]["vision-content"])
+
+document.querySelector(".contact").children[0].textContent = (siteContent["contact"]["contact-h4"])
+document.querySelector(".contact").children[1].textContent = (siteContent["contact"]["address"])
+document.querySelector(".contact").children[2].textContent = (siteContent["contact"]["phone"])
+document.querySelector(".contact").children[3].textContent = (siteContent["contact"]["email"])
+
+document.querySelector("footer").children[0].textContent = (siteContent["footer"]["copyright"])
+//let features = document.getElementsByClassName("top-content").children[0].children[0];
+//document.getElementsByName("top-content"[0][0]).textContent = features
