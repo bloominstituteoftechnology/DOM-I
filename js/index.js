ce422b9d154df5c21cab0a0053ddb472a79aa478
Tee Chandy <73080544+tpchandy@users.noreply.github.com>
2021-02-10 23:53:17 -0500

    completed adding remaining content to the page.

--- js/index.js
+++ js/index.js
@@ -57,4 +57,32 @@ logo.setAttribute('src', siteContent["nav"]["img-src"])
   mainImage.setAttribute('src','img/header-img.png');
 
   const button = document.querySelector('div.cta-text button'); // query to add text to button in this section.
+
   button.textContent = 'Get Started';
+
+  const h4 = document.querySelectorAll('h4');// query for all the sub-headings
+  console.log(h4); // to verify that we have a list of 6 items.
+  h4[0].textContent = 'Features';
+  h4[1].textContent = 'About';
+  h4[2].textContent = 'Services';
+  h4[3].textContent = 'Products';
+  h4[4].textContent = 'Vision';
+  h4[5].textContent = 'Contact';
+
+  const p = document.querySelectorAll('p');// query to select all p tags to add content as provided in this json file.
+
+  p[0].textContent = 'Features content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.'
+  p[1].textContent = "About content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis."
+  p[2].textContent = "Services content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis."
+  p[3].textContent = "Product content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis."
+  p[4].textContent = "Vision content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis."
+  p[5].textContent = "123 Way 456 Street Somewhere, USA"
+  p[6].textContent = "1 (888) 888-8888"
+  p[7].textContent = "sales@greatidea.io"
+  p[8].textContent = "Copyright Great Idea! 2018"
+
+  const image2 = document.querySelector('#middle-img'); // selector for the image at the middle of the page
+  image2.setAttribute('src', 'img/mid-page-accent.jpg') // sourced from the provided img file.
+
+  
+
