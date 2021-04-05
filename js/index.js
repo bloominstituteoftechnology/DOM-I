7ce93bfc3cfa524a3780375e4aa70e8f3d6b75e7
MichaelHabermas <michaelghabermas@gmail.com>
2021-04-05 11:51:51 -0700

    mvp no stretch

--- js/index.js
+++ js/index.js
@@ -48,6 +48,14 @@ let logo = document.getElementById('logo-img');
 logo.setAttribute('src', siteContent['nav']['img-src']);
 logo.classList.add('.logo');
 
+let nav = document.querySelector('nav');
+
+let faq = document.createElement('a');
+nav.appendChild(faq);
+
+let media = document.createElement('a');
+nav.prepend(media);
+
 let navLinks = document.querySelectorAll('nav a');
 navLinks[0].textContent = siteContent['nav']['nav-item-1'];
 navLinks[1].textContent = siteContent['nav']['nav-item-2'];
@@ -55,6 +63,8 @@ navLinks[2].textContent = siteContent['nav']['nav-item-3'];
 navLinks[3].textContent = siteContent['nav']['nav-item-4'];
 navLinks[4].textContent = siteContent['nav']['nav-item-5'];
 navLinks[5].textContent = siteContent['nav']['nav-item-6'];
+navLinks[6].textContent = 'Media';
+navLinks[7].textContent = 'FAQ';
 
 // MAIN IMAGE
 let cta = document.querySelector('.cta');
