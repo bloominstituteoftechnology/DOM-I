60bc55fbf0f3adc71f31e56b1f6b5bd952ca2c38
thekid510 <76237067+thekid510@users.noreply.github.com>
2021-04-05 15:31:03 -0700

    1st commit

--- js/index.js
+++ js/index.js
@@ -40,3 +40,7 @@ const siteContent = {
 // Example: Update the img src for the logo
 let logo = document.getElementById("logo-img");
 logo.setAttribute('src', siteContent["nav"]["img-src"])
+
+const header = document.querySelector('header');
+
+let links = document.querySelectorAll("nav a");
