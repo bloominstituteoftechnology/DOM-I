88fe4db7ec890d77d2a5f07da7b497192941ba36
drslaughterrex <drslaughterrex@gmail.com>
2021-04-05 16:24:54 -0500

    finished nav bar and cta

--- js/index.js
+++ js/index.js
@@ -1,42 +1,63 @@
 const siteContent = {
-  "nav": {
-    "nav-item-1": "Services",
-    "nav-item-2": "Product",
-    "nav-item-3": "Vision",
-    "nav-item-4": "Features",
-    "nav-item-5": "About",
-    "nav-item-6": "Contact",
-    "img-src": "img/logo.png"
-  },
-  "cta": {
-    "h1": "DOM Is Awesome",
-    "button": "Get Started",
-    "img-src": "img/header-img.png"
-  },
-  "main-content": {
-    "features-h4":"Features",
-    "features-content": "Features content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.",
-    "about-h4":"About",
-    "about-content": "About content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.",
-    "middle-img-src": "img/mid-page-accent.jpg",
-    "services-h4":"Services",
-    "services-content": "Services content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.",
-    "product-h4":"Product",
-    "product-content": "Product content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.",
-    "vision-h4":"Vision",
-    "vision-content": "Vision content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.",
-  },
-  "contact": {
-    "contact-h4" : "Contact",
-    "address" : "123 Way 456 Street Somewhere, USA",
-    "phone" : "1 (888) 888-8888",
-    "email" : "sales@greatidea.io",
-  },
-  "footer": {
-    "copyright" : "Copyright Great Idea! 2018"
-  },
+	nav: {
+		"nav-item-1": "Services",
+		"nav-item-2": "Product",
+		"nav-item-3": "Vision",
+		"nav-item-4": "Features",
+		"nav-item-5": "About",
+		"nav-item-6": "Contact",
+		"img-src": "img/logo.png",
+	},
+	cta: {
+		h1: "DOM Is Awesome",
+		button: "Get Started",
+		"img-src": "img/header-img.png",
+	},
+	"main-content": {
+		"features-h4": "Features",
+		"features-content":
+			"Features content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.",
+		"about-h4": "About",
+		"about-content":
+			"About content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.",
+		"middle-img-src": "img/mid-page-accent.jpg",
+		"services-h4": "Services",
+		"services-content":
+			"Services content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.",
+		"product-h4": "Product",
+		"product-content":
+			"Product content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.",
+		"vision-h4": "Vision",
+		"vision-content":
+			"Vision content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.",
+	},
+	contact: {
+		"contact-h4": "Contact",
+		address: "123 Way 456 Street Somewhere, USA",
+		phone: "1 (888) 888-8888",
+		email: "sales@greatidea.io",
+	},
+	footer: {
+		copyright: "Copyright Great Idea! 2018",
+	},
 };
 
 // Example: Update the img src for the logo
 let logo = document.getElementById("logo-img");
-logo.setAttribute('src', siteContent["nav"]["img-src"])
+logo.setAttribute("src", siteContent["nav"]["img-src"]);
+
+const navLink = document.querySelectorAll("nav > a");
+navLink[0].textContent = siteContent['nav']['nav-item-1'];
+navLink[1].textContent = siteContent['nav']['nav-item-2'];
+navLink[2].textContent = siteContent['nav']['nav-item-3'];
+navLink[3].textContent = siteContent['nav']['nav-item-4'];
+navLink[4].textContent = siteContent['nav']['nav-item-5'];
+navLink[5].textContent = siteContent['nav']['nav-item-6'];
+
+const ctaImg = document.getElementById('cta-img');
+ctaImg.setAttribute("src", siteContent["cta"]["img-src"]);
+
+const ctaContent = document.querySelector('h1')
+ctaContent.textContent = siteContent['cta']['h1']
+const ctaButton = document.querySelector('button')
+ctaButton.textContent = siteContent['cta']['button']
