52b4f8754c56f040f89f97c00452b0bc4beed550
miranda-burt <mriandadburt@gmail.com>
2021-04-05 20:02:53 -0700

    halfway there

--- js/index.js
+++ js/index.js
@@ -42,8 +42,31 @@ const siteContent = {
 let logo = document.getElementById("logo-img");
 logo.setAttribute('src', siteContent["nav"]["img-src"])
 
-const navBar =  document.querySelectorAll (' nav  > a ') 
+const navBar =  document.querySelectorAll (' nav  > a ');
 
-Console.log(navBar)
+// console.log(navBar)
 
-navBar [0] .textContent = siteContent.nav ['nav-item-1']
+navBar [0] .textContent = siteContent.nav ["nav-item-1"];
+navBar [1] .textContent = siteContent.nav ["nav-item-2"];
+navBar [2] .textContent = siteContent.nav ["nav-item-3"];
+navBar [3] .textContent = siteContent.nav ["nav-item-4"];
+navBar [4] .textContent = siteContent.nav ["nav-item-5"];
+navBar [5] .textContent = siteContent.nav ["nav-item-6"];
+
+
+
+const circleImage = document.querySelector ('cta cta-img');
+circleImage.src = 'img/header-img.png'
+
+const title = document.querySelector ('h1');
+title.textContent = "dom is awesome";
+
+const button = document.querySelector ('button');
+button.textContent = "Get Started"
+
+//*********************** page break ************************* */
+
+
+
+
+
