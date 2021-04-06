ae7f02142884ad851facfec9068d1762d986e0c4
michaelquintdev <michaelquintdev@gmail.com>
2021-04-05 20:43:03 -0500

    Done with nav

--- js/index.js
+++ js/index.js
@@ -40,3 +40,43 @@ const siteContent = {
 // Example: Update the img src for the logo
 let logo = document.getElementById("logo-img");
 logo.setAttribute('src', siteContent["nav"]["img-src"])
+
+
+
+// nav
+let navItem = document.querySelectorAll("nav a");
+navItem[0].textContent = siteContent["nav"]["nav-item-1"];
+navItem[1].textContent = siteContent["nav"]["nav-item-2"];
+navItem[2].textContent = siteContent["nav"]["nav-item-3"];
+navItem[3].textContent = siteContent["nav"]["nav-item-4"];
+navItem[4].textContent = siteContent["nav"]["nav-item-5"];
+navItem[5].textContent = siteContent["nav"]["nav-item-6"];
+
+
+
+
+// let nav1 = document.querySelector("a:nth-of-type(1)")
+// nav1.textContent = "Services";
+// let nav2 = document.querySelector("a:nth-of-type(2)")
+// nav2.textContent = "Product";
+// let nav3 = document.querySelector("a:nth-of-type(3)")
+// nav3.textContent = "Vision";
+// let nav4 = document.querySelector("a:nth-of-type(4)")
+// nav4.textContent = "Features";
+// let nav5 = document.querySelector("a:nth-of-type(5)")
+// nav5.textContent = "About";
+// let nav6 = document.querySelector("a:nth-of-type(6)")
+// nav6.textContent = "Contact";
+
+// // Heading section
+// let img1 = document.querySelector("#cta-img");
+// img1.setAttribute("src", "img/header-img.png");
+// let domTitle = document.querySelector(".cta-text h1");
+// domTitle.textContent = "DOM IS AWESOME";
+// let newButton = document.querySelector(".cta-text button");
+// newButton.textContent = "Get Started";
+
+// // paragraphs
+
+// let secondPara = document.querySelector(".text-content:nth-of-type(2)");
+// secondPara.textContent = "Features content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.";
