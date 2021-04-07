3177883705c829f66bc52050ab01ed325c4e80d4
Mallory <mallorydoescoding@gmail.com>
2021-04-07 11:39:53 -0500

    All selectors have been created and are organized in the JS file

--- js/index.js
+++ js/index.js
@@ -40,3 +40,67 @@ const siteContent = {
 // Example: Update the img src for the logo
 let logo = document.getElementById("logo-img");
 logo.setAttribute('src', siteContent["nav"]["img-src"])
+
+
+
+//Selectors//
+
+//navBar and anchors//
+const navBarAnchors = document.getElementByTagName('nav a')
+const anchor2 = navBarAnchors.nextElementSibling
+const anchor3 = anchor2.nextElementSibling
+const anchor4 = anchor3.nextElementSibling
+const anchor5 = anchor4.nextElementSibling
+const anchor6 = anchor5.nextElementSibling
+
+//h1 text and button//
+const h1Container = document.getElementsByClassName('.cta-text')
+const h1Text = document.getElementsByTagName('h1')
+const h1Button = document.getElementByTagName('button')
+
+//mainImg//
+const mainImg = document.getElementById('cta-img')
+
+//topContent//
+
+//**main container**//
+const topContent = document.getElementsByClassName('.top-content')
+
+//**1st para in top content**/
+const paraHeading1 = topContent[0].children[0]
+const para1 = topContent[0].children[1]
+
+//**2nd para in top content**//
+const paraHeading2 = topContent[1].children[0]
+const para2 = topContent[1].children[1]
+
+//middleImg//
+const middleImg = document.getElementById('middle-img')
+
+//bottomContent//
+
+//**Main container**//
+const bottomContent = document.getElementsByClassName('.bottom-content')
+
+//**1st para in bottom content**//
+const bottomParaHeading1 = bottomContent[0].children[0]
+const bottomPara1 = bottomContent[0].children[1]
+
+//**2nd para in bottom content**//
+const bottomParaHeading2 = bottomContent[1].children[0]
+const bottomPara2 = bottomContent[1].children[1]
+
+//**3rd para in bottom content**//
+const bottomParaHeading3 = bottomContent[2].children[0]
+const bottomPara3 = bottomContent[2].children[1]
+
+//contact//
+const contactMain = document.getElementsByClassName('.contact')
+
+const contactHead = contactMain.getElementByTagName('h4')
+const contactPara1 = contactMain[1]
+const contactPara2 = contactMain[2]
+const contactPara3 = contactMain[3]
+
+//Footer//
+const Foot = document.getElementsByTagName('footer p')
