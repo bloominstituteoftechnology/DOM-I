1301f44fceee531e5960c39d990c0321ee0bdf34
Mallory <mallorydoescoding@gmail.com>
2021-04-08 11:24:45 -0500

    I fixed all of my selectors and made all the updates. All that is left is the style updates.

--- js/index.js
+++ js/index.js
@@ -46,61 +46,88 @@ logo.setAttribute('src', siteContent["nav"]["img-src"])
 //Selectors//
 
 //navBar and anchors//
-const navBarAnchors = document.getElementByTagName('nav a')
-const anchor2 = navBarAnchors.nextElementSibling
-const anchor3 = anchor2.nextElementSibling
-const anchor4 = anchor3.nextElementSibling
-const anchor5 = anchor4.nextElementSibling
-const anchor6 = anchor5.nextElementSibling
+const preNavBarAnchors = document.querySelector('.container');
+const navBarAnchors = preNavBarAnchors.firstElementChild.firstElementChild.firstElementChild;
+const anchor1 = navBarAnchors;
+anchor1.textContent = 'Services';
+const anchor2 = navBarAnchors.nextElementSibling;
+anchor2.textContent = 'Product';
+const anchor3 = anchor2.nextElementSibling;
+anchor3.textContent = 'Vision';
+const anchor4 = anchor3.nextElementSibling;
+anchor4.textContent = 'Features';
+const anchor5 = anchor4.nextElementSibling;
+anchor5.textContent = 'About';
+const anchor6 = anchor5.nextElementSibling;
+anchor6.textContent = 'Contact';
 
 //h1 text and button//
-const h1Container = document.getElementsByClassName('.cta-text')
-const h1Text = document.getElementsByTagName('h1')
-const h1Button = document.getElementByTagName('button')
+const h1Container = document.querySelector('.cta-text');
+const h1Text = h1Container.firstElementChild;
+h1Text.textContent = "Dom is Awesome";
+const h1Button = h1Container.lastElementChild;
+h1Button.textContent = "Get Started";
 
 //mainImg//
-const mainImg = document.getElementById('cta-img')
-
+let mainImg = document.getElementById("cta-img").src = 'img/header-img.png'
+console.log(mainImg)
 //topContent//
 
 //**main container**//
-const topContent = document.getElementsByClassName('.top-content')
+const topContent = document.querySelector('.top-content')
 
 //**1st para in top content**/
-const paraHeading1 = topContent[0].children[0]
-const para1 = topContent[0].children[1]
+const paraHeading1 = topContent.firstElementChild.firstElementChild
+paraHeading1.textContent = "Features"
+const para1 = topContent.firstElementChild.lastElementChild
+para1.textContent = "Features content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis."
 
 //**2nd para in top content**//
-const paraHeading2 = topContent[1].children[0]
-const para2 = topContent[1].children[1]
+const paraHeading2 = topContent.lastElementChild.firstElementChild
+paraHeading2.textContent = "About"
+const para2 = topContent.lastElementChild.lastElementChild
+para2.textContent = "About content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis."
 
 //middleImg//
-const middleImg = document.getElementById('middle-img')
+const middleImg = document.getElementById("middle-img").src = 'img/mid-page-accent.jpg'
 
 //bottomContent//
 
 //**Main container**//
-const bottomContent = document.getElementsByClassName('.bottom-content')
-
+const bottomContent = document.querySelector('.bottom-content')
 //**1st para in bottom content**//
-const bottomParaHeading1 = bottomContent[0].children[0]
-const bottomPara1 = bottomContent[0].children[1]
+const bottomParaHeading1 = bottomContent.firstElementChild.firstElementChild
+bottomParaHeading1.textContent = "Services"
+const bottomPara1 = bottomContent.firstElementChild.lastElementChild
+bottomPara1.textContent = "Services content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis."
+
 
 //**2nd para in bottom content**//
-const bottomParaHeading2 = bottomContent[1].children[0]
-const bottomPara2 = bottomContent[1].children[1]
+const bottomParaHeading2Pre = bottomContent.firstElementChild
+const bottomParaHeading2 = bottomParaHeading2Pre.nextElementSibling.firstElementChild
+bottomParaHeading2.textContent = "Product"
+const bottomPara2 = bottomParaHeading2Pre.nextElementSibling.lastElementChild
+bottomPara2.textContent = "Product content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis."
+
 
 //**3rd para in bottom content**//
-const bottomParaHeading3 = bottomContent[2].children[0]
-const bottomPara3 = bottomContent[2].children[1]
+const bottomParaHeading3 = bottomContent.lastElementChild.firstElementChild
+bottomParaHeading3.textContent = "Vision"
+const bottomPara3 = bottomContent.lastElementChild.lastElementChild
+bottomPara3.textContent = "Vision content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis."
 
 //contact//
-const contactMain = document.getElementsByClassName('.contact')
-
-const contactHead = contactMain.getElementByTagName('h4')
-const contactPara1 = contactMain[1]
-const contactPara2 = contactMain[2]
-const contactPara3 = contactMain[3]
+const contactMain = document.querySelector('.contact')
+const contactHead = contactMain.querySelector('h4')
+contactHead.textContent = "Contact"
+const contactPara1 = contactHead.nextElementSibling
+// contactPara1.textContent =  '123 way 456 Street \n Somewhere USA'
+const contactPara2 = contactPara1.nextElementSibling
+contactPara2.textContent = "1 (888) 888-8888"
+const contactPara3 = contactPara2.nextElementSibling
+contactPara3.textContent = "sales@greatidea.io"
 
 //Footer//
-const Foot = document.getElementsByTagName('footer p')
+const preFoot = document.querySelector('footer')
+const foot = preFoot.firstElementChild
+foot.textContent = "Copyright Great Idea! 2018"
