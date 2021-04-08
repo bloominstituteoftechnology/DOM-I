0d35301e3bff9efc70086530b5ae2a502e2289b2
Chris Lau <chrislyc1991@gmail.com>
2021-04-07 20:28:28 -0600

    completed basic tasks

--- js/index.js
+++ js/index.js
@@ -40,3 +40,43 @@ const siteContent = {
 // Example: Update the img src for the logo
 let logo = document.getElementById("logo-img");
 logo.setAttribute('src', siteContent["nav"]["img-src"])
+
+// adding images
+document.querySelector('#logo-img').src = siteContent.nav["img-src"];
+document.querySelector('#cta-img').src = siteContent.cta["img-src"];
+document.querySelector('#middle-img').src = siteContent["main-content"]["middle-img-src"];
+// updating nav
+let nav_keys = Object.keys(siteContent.nav);
+document.querySelectorAll('nav a').forEach((value,number)=>value.textContent = siteContent.nav[nav_keys[number]]);
+// h1
+document.querySelector('h1').textContent = siteContent.cta['h1'];
+// cta button
+document.querySelector('.cta-text button').textContent = siteContent.cta['button'];
+// main content
+// --h4
+let h4_text = Object.keys(siteContent["main-content"]).filter((value)=>value.includes('h4'));
+document.querySelectorAll('.text-content h4').forEach((value,i)=>value.textContent = siteContent["main-content"][h4_text[i]]);
+// --p
+let p_text = Object.keys(siteContent['main-content']).filter((value)=>value.includes('content'));
+document.querySelectorAll('.text-content p').forEach((value,i)=>value.textContent = siteContent["main-content"][p_text[i]]);
+// --img
+// let imgs = Object.keys(siteContent["main-content"]).filter((value)=>value.includes('img'));
+// document.querySelectorAll('.main-content img').forEach((value,i)=>value.src = siteContent['main-content'][imgs[i]]);
+
+// contact
+let contact_text = Object.keys(siteContent['contact']);
+document.querySelectorAll('.contact *').forEach((value,i)=>value.textContent = siteContent['contact'][contact_text[i]]);
+
+// footer
+document.querySelector('footer p').textContent = siteContent['footer'].copyright;
+
+// Adding new content
+document.querySelectorAll('header nav a').forEach((value)=>value.style.color = 'green');
+let new_element = document.createElement('a');
+new_element.textContent = 'Last Link';
+document.querySelector('header nav').appendChild(new_element);
+new_element = document.createElement('a');
+new_element.textContent = 'First Link';
+document.querySelector('header nav').prepend(new_element);
+
+
