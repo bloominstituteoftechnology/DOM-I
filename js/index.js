652b7cfb891518970a3b4bfe6c4dc82f7f6b79df
Tasnim Rahman <tasnimrahman@Tasnims-MacBook-Air.local>
2021-04-08 01:13:07 -0400

    meets requirements

--- js/index.js
+++ js/index.js
@@ -40,3 +40,86 @@ const siteContent = {
 // Example: Update the img src for the logo
 let logo = document.getElementById("logo-img");
 logo.setAttribute('src', siteContent["nav"]["img-src"])
+
+const anchorTag = document.querySelectorAll('nav a');
+console.log(anchorTag)
+anchorTag[0].textContent = siteContent["nav"]["nav-item-1"]
+anchorTag[1].textContent = siteContent["nav"]["nav-item-2"]
+anchorTag[2].textContent = siteContent["nav"]["nav-item-3"]
+anchorTag[3].textContent = siteContent["nav"]["nav-item-4"]
+anchorTag[4].textContent = siteContent["nav"]["nav-item-5"]
+anchorTag[5].textContent = siteContent["nav"]["nav-item-6"]
+
+
+const heading = document.querySelector('h1')
+heading.textContent = siteContent["cta"]["h1"]
+
+const getStarted = document.querySelector('button')
+getStarted.textContent = siteContent["cta"]["button"]
+
+const image = document.querySelector('img')
+image.setAttribute('src',siteContent["cta"]["img-src"])
+
+const topHeading = document.querySelectorAll('.top-content h4')
+console.log(topHeading)
+topHeading[0].textContent = siteContent['main-content']['features-h4']
+topHeading[1].textContent = siteContent['main-content']['about-h4']
+
+const topParagraph = document.querySelectorAll('.top-content p')
+topParagraph[0].textContent = siteContent['main-content']['features-content']
+topParagraph[1].textContent = siteContent['main-content']['about-content']
+
+const mainImage = document.querySelector('.main-content img')
+mainImage.setAttribute('src',siteContent['main-content']['middle-img-src'])
+
+const bottomHeading = document.querySelectorAll('.bottom-content h4')
+
+bottomHeading[0].textContent = siteContent["main-content"]["services-h4"]
+bottomHeading[1].textContent = siteContent["main-content"]["product-h4"]
+bottomHeading[2].textContent = siteContent["main-content"]["vision-h4"]
+
+const bottomParagraph = document.querySelectorAll('.bottom-content p')
+
+bottomParagraph[0].textContent = siteContent ["main-content"] ["services-content"]
+bottomParagraph[1].textContent = siteContent ["main-content"] ["product-content"]
+bottomParagraph[2].textContent = siteContent ["main-content"] ["vision-content"]
+
+const contact = document.querySelector('.contact h4')
+
+contact.textContent = siteContent["contact"]["contact-h4"]
+
+const contactInfo = document.querySelectorAll('.contact p')
+
+contactInfo[0].textContent = siteContent["contact"]["address"]
+contactInfo[1].textContent = siteContent["contact"]["phone"]
+contactInfo[2].textContent = siteContent["contact"]["email"]
+
+const copy = document.querySelector('footer p')
+copy.textContent = siteContent["footer"]["copyright"]
+
+const changeColor = document.querySelectorAll('header nav a')
+console.log(changeColor)
+changeColor[0].style.color = 'green'
+changeColor[1].style.color = 'green'
+changeColor[2].style.color = 'green'
+changeColor[3].style.color = 'green'
+changeColor[4].style.color = 'green'
+changeColor[5].style.color = 'green'
+
+
+const custom = document.createElement('a')
+const content = document.createElement('a')
+custom.innerHTML = "Custom"
+content.innerHTML = "Content"
+
+custom.style.color = 'green'
+content.style.color = 'green'
+
+const navigation = document.querySelector('nav')
+navigation.appendChild(content)
+navigation.appendChild(custom)
+
+
+
+
+
