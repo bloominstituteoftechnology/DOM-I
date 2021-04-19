42531be056f0fedd927477f1a289dda701a725a4
king-handy <a.king.life@pm.me>
2021-04-18 21:04:57 -0400

    header img

--- js/index.js
+++ js/index.js
@@ -40,3 +40,9 @@ const siteContent = {
 // Example: Update the img src for the logo
 let logo = document.getElementById("logo-img");
 logo.setAttribute('src', siteContent["nav"]["img-src"])
+
+
+let headerImage = document.getElementById('header-img');
+headerImage.setAttribute('src', siteContent['nav']['img/logo.png'])
+
+
