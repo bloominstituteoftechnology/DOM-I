a7b1210ca32f43ebe459cc112b7058930c75776e
Taormina7575 <vinnie.taormina@gmail.com>
2021-05-03 14:17:47 -0400

    final

--- js/index.js
+++ js/index.js
@@ -9,7 +9,7 @@ const siteContent = {
     "img-src": "img/logo.png"
   },
   "cta": {
-    "h1": "DOM Is Awesome",
+    "h1": `DOM Is Awesome`,
     "button": "Get Started",
     "img-src": "img/header-img.png"
   },
@@ -40,3 +40,75 @@ const siteContent = {
 // Example: Update the img src for the logo
 let logo = document.getElementById("logo-img");
 logo.setAttribute('src', siteContent["nav"]["img-src"])
+
+let navItems = document.querySelectorAll('nav a');
+let nav1 = navItems[0].textContent = (siteContent["nav"]["nav-item-1"])
+let nav2 = navItems[1].textContent = (siteContent["nav"]["nav-item-2"])
+let nav3 = navItems[2].textContent = (siteContent["nav"]["nav-item-3"])
+let nav4 = navItems[3].textContent = (siteContent["nav"]["nav-item-4"])
+let nav5 = navItems[4].textContent = (siteContent["nav"]["nav-item-5"])
+let nav6 = navItems[5].textContent = (siteContent["nav"]["nav-item-6"])
+
+let bigHead = document.querySelector('h1').textContent = (siteContent['cta']['h1'])
+
+
+
+let button = document.querySelector('button').textContent = (siteContent["cta"]['button'])
+
+let buttonImg = document.querySelector('#cta-img').src = (siteContent['cta']['img-src'])
+
+let topSection = document.querySelectorAll('.top-content .text-content')
+
+let top1 = topSection[0].children;
+let top2 = topSection[1].children;
+
+let featureHead = top1[0].textContent = (siteContent['main-content']['features-h4'])
+let featureDesc = top1[1].textContent = (siteContent['main-content']['features-content'])
+
+let aboutHead = top2[0].textContent = (siteContent['main-content']['about-h4'])
+let aboutDesc = top2[1].textContent = (siteContent['main-content']['about-content'])
+
+let middlePic = document.querySelector('#middle-img').src = (siteContent['main-content']['middle-img-src'])
+
+let bottomSection = document.querySelectorAll('.bottom-content .text-content')
+
+let bottom1 = bottomSection[0].children
+let bottom2 = bottomSection[1].children
+let bottom3 = bottomSection[2].children
+
+let servicesHead = bottom1[0].textContent = (siteContent['main-content']['services-h4'])
+let servicesDesc = bottom1[1].textContent = (siteContent['main-content']['services-content'])
+
+let productHead = bottom2[0].textContent = (siteContent['main-content']['product-h4'])
+let productDesc = bottom2[1].textContent = (siteContent['main-content']['product-content'])
+
+let visionHead = bottom3[0].textContent = (siteContent['main-content']['vision-h4'])
+let visionDesc = bottom3[1].textContent = (siteContent['main-content']['vision-content'])
+
+let contactHead = document.querySelector('.contact h4').textContent = (siteContent['contact']['contact-h4'])
+
+let contactParas = document.querySelectorAll('.contact p')
+let contactAddress = contactParas[0].textContent = (siteContent['contact']['address'])
+let contactNumb = contactParas[1].textContent = (siteContent['contact']['phone'])
+let contactMail = contactParas[2].textContent = (siteContent['contact']['email'])
+
+let footer = document.querySelector('footer p').textContent = (siteContent['footer']['copyright'])
+
+let nav = document.querySelector('nav');
+function addNav(linkName) {
+  let newLink = document.createElement('a')
+  newLink.textContent = (linkName)
+  nav.appendChild(newLink)
+}
+
+addNav('Hello')
+addNav('New')
+
+
+navItems.forEach(item => {
+  item.style.color = "green"
+})
+
+
+
+
