a0b09c4c276dde0a03d3ea469f380e1d597ecdc4
Daniel <dek.kneale@gmail.com>
2021-06-02 21:19:30 -0700

    finished all tasks

--- js/index.js
+++ js/index.js
@@ -40,3 +40,61 @@ const siteContent = {
 // Example: Update the img src for the logo
 let logo = document.getElementById("logo-img");
 logo.setAttribute('src', siteContent["nav"]["img-src"])
+
+const anchorTags = document.querySelectorAll('nav a')
+anchorTags[0].innerHTML = "Services"
+anchorTags[1].innerHTML = "Product"
+anchorTags[2].innerHTML = "Vision"
+anchorTags[3].innerHTML = "Features"
+anchorTags[4].innerHTML = "About"
+anchorTags[5].innerHTML = "Contact"
+
+const anchors = document.querySelector('a')
+anchors.setAttribute("style", "color:green;")
+
+// navagation.prepend(newA).prepend(newBtext)
+
+
+
+// anchorTags.style.color = "green"
+// document.querySelectorAll('nav a').style.color ="green";
+// document.getElementsByName("a").style.color = "green";
+// anchorTags.style="color:green"
+
+const heading = document.querySelector("h1")
+heading.innerHTML = "DOM Is Awesome"
+const button = document.querySelector("button") 
+button.innerHTML = "Get Started"
+let cta = document.getElementById("cta-img");
+cta.setAttribute('src', siteContent["cta"]["img-src"])
+
+const h4 = document.getElementsByTagName('h4')
+h4[0].innerHTML = "Features"
+h4[1].innerHTML = "About"
+h4[2].innerHTML = "Services"
+h4[3].innerHTML = "Product"
+h4[4].innerHTML = "Vision"
+h4[5].innerHTML = "Contact"
+
+const info = document.getElementsByTagName("p")
+info[0].innerHTML = "Features content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis."
+info[1].innerHTML = "About content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis."
+info[2].innerHTML = "Services content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis."
+info[3].innerHTML = "Product content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis."
+info[4].innerHTML = "Vision content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis."
+info[5].innerHTML = "123 Way 456 Street Somewhere, USA"
+info[6].innerHTML = "1 (888) 888-8888"
+info[7].innerHTML = "sales@greatidea.io"
+info[8].innerHTML = "Copyright Great Idea! 2018"
+
+let middleImg = document.getElementById("middle-img");
+middleImg.setAttribute('src', siteContent["main-content"]["middle-img-src"])
+
+
+const navagation = document.querySelector('nav')
+let newA = document.createElement('a')
+let newAtext = document.createTextNode('Examples')
+let newBtext = document.createTextNode('Explore -->')
+navagation.appendChild(newA).appendChild(newAtext)
+navagation.prepend(newBtext)
+
