1fe5b5ab150e02d3969a5675e298390818c3da85
marciemaclean <macleanphoenix@gmail.com>
2021-07-05 17:26:25 -0400

    main content added, contact info added

--- js/index.js
+++ js/index.js
@@ -61,3 +61,30 @@ ctabutton.textContent ='Get Started'
 let ctaimg = document.getElementById("cta-img");
 ctaimg.setAttribute('src', 'img/header-img.png')
 
+let mdimg = document.getElementById("middle-img")
+mdimg.setAttribute('src', "img/mid-page-accent.jpg")
+
+let mainContText = document.querySelectorAll('.text-content h4')
+
+mainContText[0].textContent = "Features"
+mainContText[1].textContent = "About"
+mainContText[2].textContent = "Services"
+mainContText[3].textContent = "Product"
+mainContText[4].textContent = "Vision"
+
+let mainContBlurb = document.querySelectorAll('.text-content p')
+
+mainContBlurb[0].textContent = "Features content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis."
+mainContBlurb[1].textContent = "About content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis."
+mainContBlurb[2].textContent = "Services content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis."
+mainContBlurb[3].textContent = "Product content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis."
+mainContBlurb[4].textContent = "Vision content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis."
+
+let contactHead = document.querySelector('.contact h4')
+contactHead.textContent = "Contact"
+
+let contactInfo = document.querySelectorAll('.contact p')
+contactInfo[0].textContent = "123 Way 456 Street Somewhere, USA"
+contactInfo[1].textContent = "1 (888) 888-8888"
+contactInfo[2].textContent = "sales@greatidea.io"
+
