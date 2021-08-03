3a8195a79ba40a0beca77e032ccd8c9766bee4fd
Matthew Stack <matthewstack@Matthews-MacBook-Pro-4.local>
2021-08-02 20:25:18 -0400

    finished project

--- js/index.js
+++ js/index.js
@@ -40,3 +40,67 @@ const siteContent = {
 // Example: Update the img src for the logo
 let logo = document.getElementById("logo-img");
 logo.setAttribute('src', siteContent["nav"]["img-src"])
+
+
+const topNav = document.querySelectorAll('nav a');
+topNav[0].textContent = siteContent['nav']['nav-item-1']
+topNav[1].textContent = siteContent['nav']['nav-item-2']
+topNav[2].textContent = siteContent['nav']['nav-item-3']
+topNav[3].textContent = siteContent['nav']['nav-item-4']
+topNav[4].textContent = siteContent['nav']['nav-item-5']
+topNav[5].textContent = siteContent['nav']['nav-item-6']
+
+
+topNav[0].style.color = 'green';
+topNav[1].style.color = 'green';
+topNav[2].style.color = 'green';
+topNav[3].style.color = 'green';
+topNav[4].style.color = 'green';
+topNav[5].style.color = 'green';
+
+
+
+const firstEl = document.createElement('p')
+firstEl.textContent = 'Ex 1';
+const secondEl = document.createElement('p')
+secondEl.textContent = 'Ex 2';
+
+const nav = document.querySelector('nav')
+nav.appendChild(firstEl)
+nav.prepend(secondEl)
+nav.style.color = 'green';
+
+document.querySelector('#cta-img').src = 'img/header-img.png'
+
+document.querySelector('div.cta-text h1').innerHTML = 'DOM <br> IS <br> AWESOME'
+document.querySelector('div.cta-text button').innerText = 'Get Started'
+
+document.querySelector('#middle-img').src = 'img/mid-page-accent.jpg'
+
+
+const hFour = document.querySelectorAll('.main-content h4');
+hFour[0].textContent = siteContent['main-content']['features-h4'];
+hFour[1].textContent = siteContent['main-content']['about-h4'];
+hFour[2].textContent = siteContent['main-content']['services-h4'];
+hFour[3].textContent = siteContent['main-content']['product-h4'];
+hFour[4].textContent = siteContent['main-content']['vision-h4'];
+
+const paragraphText = document.querySelectorAll('.main-content p');
+paragraphText[0].textContent = siteContent['main-content']['features-content'];
+paragraphText[1].textContent = siteContent['main-content']['about-content'];
+paragraphText[2].textContent = siteContent['main-content']['services-content'];
+paragraphText[3].textContent = siteContent['main-content']['product-content'];
+paragraphText[4].textContent = siteContent['main-content']['vision-content'];
+
+
+const contactH4 = document.querySelector('.contact h4');
+contactH4.textContent = siteContent['contact']['contact-h4'];
+
+const contactP = document.querySelectorAll('.contact p');
+contactP[0].innerHTML = '123 Way 456 Street <br> Somewhere, USA'
+contactP[1].textContent = siteContent['contact']['phone'];
+contactP[2].textContent = siteContent['contact']['email'];
+
+const copyrightF = document.querySelector('footer p');
+copyrightF.textContent = siteContent['footer']['copyright'];
+
