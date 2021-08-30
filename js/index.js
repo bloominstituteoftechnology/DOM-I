a5213a1f4ddcad3aec13af2405618d0d6ae5be09
peterdavidconley <85258866+peterdavidconley@users.noreply.github.com>
2021-08-30 08:46:12 -0400

    setting up project

--- js/index.js
+++ js/index.js
@@ -37,6 +37,8 @@ const siteContent = {
   },
 };
 
+// Creating comment for initial push
+
 // Example: Update the img src for the logo
 let logo = document.getElementById("logo-img");
 logo.setAttribute('src', siteContent["nav"]["img-src"])
