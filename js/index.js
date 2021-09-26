51fedb6d8dd4c55db0d1037e956ec271e5c817ad
Angelique Abacajan <ma_utsey@yahoo.com>
2021-09-25 22:47:39 -0400

    MVP

--- js/index.js
+++ js/index.js
@@ -50,3 +50,45 @@ navBar[2].innerHTML = 'Vision';
 navBar[3].innerHTML = 'Features';
 navBar[4].innerHTML = 'About';
 navBar[5].innerHTML = 'Contact';
+
+// CTA
+
+// h1
+const ctaHeaderOne = document.querySelector('h1');
+ctaHeaderOne.textContent = "DOM IS AWESOME";
+ctaHeaderOne.innerHTML = "DOM<br>IS<br>AWESOME";
+
+// button
+const ctaButton = document.querySelector('.cta-text button');
+ctaButton.textContent = "Get Started";
+
+// middle-img
+const middleImg = document.querySelector('.middle-img');
+middleImg.setAttribute('src', siteContent["main-content"]["middle-img-src"]);
+
+// h4 - all h4
+const headerFour = document.querySelectorAll('h4');
+headerFour[0].textContent = "Services";
+headerFour[1].textContent = "Product";
+headerFour[2].textContent = "Vision";
+headerFour[3].textContent = "Features";
+headerFour[4].textContent = "About";
+headerFour[5].textContent = "Contact";
+
+
+
+// p - all paragraphs
+const paragraphs = document.querySelectorAll('p');
+paragraphs[0].textContent = "Services content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.";
+paragraphs[1].textContent = "Product content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.";
+paragraphs[2].textContent = "Vision content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.";
+paragraphs[3].textContent = "Features content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.";
+paragraphs[4].textContent = "About content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.";
+
+// Contact 
+const contact = document.querySelectorAll('.contact p');
+
+// contact paragraphs
+contact[0].textContent = "123 Way 456 Street Somewhere, USA";
+contact[1].textContent = "1 (888) 888-8888";
+contact[2].textContent = "sales@greatidea.io";
