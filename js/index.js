ed9947f1757df3af5db67fa8814c461958691081
Lia Moua <moua0061@yahoo.com>
2021-09-27 19:04:15 -0400

    completed message, picture, main contents and middle img

--- js/index.js
+++ js/index.js
@@ -42,12 +42,12 @@ let logo = document.getElementById("logo-img");
 logo.setAttribute('src', siteContent["nav"]["img-src"])
 
 //Update the nav
-const services = document.querySelector('.nav-item-1');
-const product = document.querySelector('.nav-item-2');
-const vision = document.querySelector('.nav-item-3');
-const features = document.querySelector('.nav-item-4');
-const about = document.querySelector('.nav-item-5');
-const contact = document.querySelector('.nav-item-6');
+const services = document.querySelector('nav a:nth-child(1)');
+const product = document.querySelector('nav a:nth-child(2)');
+const vision = document.querySelector('nav a:nth-child(3)');
+const features = document.querySelector('nav a:nth-child(4)');
+const about = document.querySelector('nav a:nth-child(5)');
+const contact = document.querySelector('nav a:nth-child(6)');
 
 services.textContent = siteContent['nav']['nav-item-1'];
 product.textContent = siteContent['nav']['nav-item-2'];
@@ -55,3 +55,65 @@ vision.textContent = siteContent['nav']['nav-item-3'];
 features.textContent = siteContent['nav']['nav-item-4'];
 about.textContent = siteContent['nav']['nav-item-5'];
 contact.textContent = siteContent['nav']['nav-item-6'];
+
+//update on DOM is awesome text
+const domIsAwesomeText = document.querySelector('.cta-text h1');
+domIsAwesomeText.textContent = siteContent['cta']['h1']
+
+//button under DOM is awesome
+const button = document.querySelector('.cta-text button');
+button.textContent = siteContent['cta']['button'];
+
+//img next to DOM is awesome
+const image = document.querySelector('#cta-img');
+image.setAttribute('src', siteContent['cta']['img-src']);
+
+//main content - features title
+const featuresTitle = document.querySelector('.text-content h4:nth-child(1)');
+featuresTitle.textContent = siteContent['main-content']['features-h4'];
+
+//main content - features content
+const featuresContent = document.querySelector('.text-content p');
+featuresContent.textContent = siteContent['main-content']['features-content'];
+
+//main content - about title
+const aboutTitle = document.querySelector('.text-content:nth-child(2) h4');
+aboutTitle.textContent = siteContent['main-content']['about-h4'];
+
+//main content - about content
+const aboutContent = document.querySelector('.text-content:nth-child(2) p');
+aboutContent.textContent = siteContent['main-content']['about-content'];
+
+//middle img
+const middleImg = document.querySelector('#middle-img');
+middleImg.setAttribute('src', siteContent['main-content']['middle-img-src']);
+
+//bottom content - services title
+const servicesTitle = document.querySelector('.text-content:nth-child(3) h4');
+servicesTitle.textContent = siteContent['main-content']['services-h4'];
+
+//bottom content - services content
+const servicesContent = document.querySelector('.text-content:nth-child(3) p');
+servicesContent.textContent = siteContent['main-content']['services-content'];
+
+//bottom content - product title
+const productTitle = document.querySelector('.text-content:nth-child(4) h4');
+productTitle.textContent = siteContent['main-content']['product-h4'];
+
+//bottom content - product content
+const productContent = document.querySelector('.text-content:nth-child(4) p');
+productContent.textContent = siteContent['main-content']['product-content'];
+
+//bottom content - vision title
+const visionTitle = document.querySelector('.text-content:nth-child(5) h4');
+visionTitle.textContent = siteContent['main-content']['vision-h4'];
+
+//bottom content - vision content
+const visionContent = document.querySelector('.text-content:nth-child(5) p');
+visionContent.textContent = siteContent['main-content']['vision-content'];
+
+//contact 
+const contact = document.querySelector('.contact h4');
+contact.textContent = siteContent['contact']['contact-h4'];
+
+//footer
