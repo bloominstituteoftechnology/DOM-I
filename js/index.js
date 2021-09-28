c49538a832806616929a518c2f4701945a19da28
Hughes <hughespham2002the2nd@yahoo.com>
2021-09-27 20:39:09 -0500

    Done

--- js/index.js
+++ js/index.js
@@ -1,15 +1,15 @@
 const siteContent = {
   "nav": {
-   const :nav1 = document.querySelector('.nav-item-1').textContent = "Services",
-   const :nav2 = document.querySelector('.nav-item-2').textContent = "Product",
-   const :nav3 = document.querySelector('.nav-item-3').textContent = "Vision",
-   const :nav4 = document.querySelector('.nav-item-4').textContent = "Features",
-   const :nav5 = document.querySelector('.nav-item-5').textContent = "About",
-   const :nav6 = document.querySelector('.nav-item-6').textContent = "Contact",
+    "nav-item-1": "Services",
+    "nav-item-2": "Product",
+    "nav-item-3": "Vision",
+    "nav-item-4": "Features",
+    "nav-item-5": "About",
+    "nav-item-6": "Contact",
     "img-src": "img/logo.png"
   },
   "cta": {
-    const :mainHeader = document.querySelector('h1').textContent = "DOM Is Awesome",
+    "h1": "DOM Is Awesome",
     "button": "Get Started",
     "img-src": "img/header-img.png"
   },
@@ -36,9 +36,49 @@ const siteContent = {
     "copyright" : "Copyright Great Idea! 2018"
   },
 };
-
-document.querySelector('h1').textContent = "DOM<br> Is<br> Awesome";
-
 // Example: Update the img src for the logo
 let logo = document.getElementById("logo-img");
 logo.setAttribute('src', siteContent["nav"]["img-src"])
+
+let navMenu = document.getElementsByTagName("a");
+navMenu[0].textContent = 'Services';
+navMenu[1].textContent = 'Product';
+navMenu[2].textContent = 'Vision';
+navMenu[3].textContent = 'Features';
+navMenu[4].textContent = 'About';
+navMenu[5].textContent = 'Contact';
+
+const Header = document.querySelector('h1');
+Header.innerHTML = "DOM<br>Is<br>Awesome";
+
+const butt = document.querySelector('button');
+butt.textContent = "Get Started";
+
+const logoImg = document.querySelector('#cta-img');
+logoImg.setAttribute('src', siteContent['cta']['img-src']);
+
+const midImg = document.querySelector('.middle-img');
+midImg.setAttribute('src', siteContent["main-content"]["middle-img-src"]);
+
+const subHeader = document.querySelectorAll('h4');
+subHeader[0].textContent = "Features";
+subHeader[1].textContent = "About";
+subHeader[2].textContent = "Services";
+subHeader[3].textContent = "Product";
+subHeader[4].textContent = "Vision";
+subHeader[5].textContent = "Contact";
+
+const text = document.querySelectorAll('p');
+text[0].textContent ="Features content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.";
+text[1].textContent ="About content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.";
+text[2].textContent ="Services content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.";
+text[3].textContent ="Product content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.";
+text[4].textContent ="Vision content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.";
+
+const contact = document.querySelectorAll('.contact p');
+contact[0].innerHTML = "123 Way 456 Street<br>Somewhere, USA";
+contact[1].textContent = "1 (888) 888-8888";
+contact[2].textContent = "sales@greatidea.io"; 
+
+const footer = document.querySelector('footer p');
+footer.textContent = "Copyright Great Idea! 2018"; 
