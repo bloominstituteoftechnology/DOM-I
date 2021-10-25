e9b988ff6ce908676887fe5e19bddfc30186f4a9
quavo <qua.thesage19@gmail.com>
2021-10-25 14:59:22 -0500

    Finished anchor tag selectors

--- js/index.js
+++ js/index.js
@@ -40,3 +40,35 @@ const siteContent = {
 // Example: Update the img src for the logo
 let logo = document.getElementById("logo-img");
 logo.setAttribute('src', siteContent["nav"]["img-src"])
+// Nav Bar Selectors
+const navItem1 = document.querySelector('a:nth-of-type(1)');
+const navItem2 = document.querySelector('a:nth-of-type(2)');
+const navItem3 = document.querySelector('a:nth-of-type(3)');
+const navItem4 = document.querySelector('a:nth-of-type(4)');
+const navItem5 = document.querySelector('a:nth-of-type(5)');
+const navItem6 = document.querySelector('a:nth-of-type(6)');
+navItem1.textContent = siteContent['nav']['nav-item-1'];
+navItem2.textContent = siteContent['nav']['nav-item-2'];
+navItem3.textContent = siteContent['nav']['nav-item-3'];
+navItem4.textContent = siteContent['nav']['nav-item-4'];
+navItem5.textContent = siteContent['nav']['nav-item-5'];
+navItem6.textContent = siteContent['nav']['nav-item-6'];
+
+
+const ctaItems = document.querySelector('h1');
+ctaItems.textContent = siteContent['cta']['h1'];
+
+const ctabutton = document.querySelector('button');
+ctabutton.textContent = siteContent['cta']['button'];
+const ctaImage = document.getElementById('cta-img');
+ctaImage.setAttribute('src', siteContent['cta']['img-src']);
+
+
+
+const mainContent = document.querySelector('.main-content');
+const contactItem = document.querySelector('.contact');
+const footItem = document.querySelector('footer');
+
+
+
+
