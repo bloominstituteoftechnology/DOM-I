41d3209aa4efe7ecb0a31a2e0cd5fc579c2dc177
Jayson Byrd <jayson_byrd_95@outlook.com>
2021-10-25 14:22:06 -0400

    Half Way Point

--- js/index.js
+++ js/index.js
@@ -38,5 +38,38 @@ const siteContent = {
 };
 
 // Example: Update the img src for the logo
-let logo = document.getElementById("logo-img");
+const logo = document.getElementById("logo-img");
 logo.setAttribute('src', siteContent["nav"]["img-src"])
+
+
+navLink1 = document.querySelector('nav a');
+navLink2 = navLink1.nextSibling;
+navLink3 = navLink2.nextSibling;
+navLink4 = navLink3.nextSibling;
+navLink5 = navLink4.nextSibling;
+navLink6 = navLink5.nextSibling;
+
+navLink1.textContent = siteContent['nav']['nav-item-1'];
+navLink2.textContent = siteContent['nav']['nav-item-2'];
+navLink3.textContent = siteContent['nav']['nav-item-3'];
+navLink4.textContent = siteContent['nav']['nav-item-4'];
+navLink5.textContent = siteContent['nav']['nav-item-5'];
+navLink6.textContent = siteContent['nav']['nav-item-6'];
+
+const ctaText = document.querySelector('.cta-text h1');
+const ctaButton = document.querySelector('.cta-text button');
+const ctaImg = document.querySelector('#cta-img');
+ctaText.textContent = siteContent.cta.h1;
+ctaButton.textContent = siteContent.cta.button;
+ctaImg.src = 'img/header-img.png';
+
+
+const featuresH4 = document.querySelector('.top-content .text-content h4');
+const featuresP = document.querySelector('.top-content .text-content p');
+featuresH4.textContent = siteContent['main-content']['features-h4'];
+featuresP.textContent = siteContent['main-content']['features-content'];
+
+const aboutH4 = document.querySelector('.text-content:nth-of-type(2) h4');
+const aboutP = document.querySelector('.text-content:nth-of-type(2) p');
+aboutH4.textContent = siteContent['main-content']['about-h4'];
+aboutP.textContent = siteContent['main-content']['about-content'];
