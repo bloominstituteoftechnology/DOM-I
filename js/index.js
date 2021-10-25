f25a9470d06f0ea694ee2f2be1fd7c5b2ce15784
lgallo95 <lgallo2005@gmail.com>
2021-10-25 15:51:38 -0700

    Complete

--- js/index.js
+++ js/index.js
@@ -91,9 +91,48 @@ h4_3.textContent = siteContent['main-content']['services-h4'];
 const h4_4 = document.querySelectorAll(".main-content .bottom-content .text-content h4")[1];
 h4_4.textContent = siteContent['main-content']['product-h4'];
 
-const p_3 = document.querySelectorAll(".main-content .bottom-content .text-content h4")[0];
+const h4_5 = document.querySelectorAll(".main-content .bottom-content .text-content h4")[2];
+h4_5.textContent = siteContent['main-content']['vision-h4'];
+
+const p_3 = document.querySelectorAll(".main-content .bottom-content .text-content p")[0];
 p_3.textContent = siteContent['main-content']['services-content'];
 
-const p_4 = document.querySelectorAll(".main-content .bottom-content .text-content h4")[1];
+const p_4 = document.querySelectorAll(".main-content .bottom-content .text-content p")[1];
 p_4.textContent = siteContent['main-content']['product-content'];
+
+const p_5 = document.querySelectorAll(".main-content .bottom-content .text-content p")[2];
+p_5.textContent = siteContent['main-content']['vision-content'];
+
+
+const h4C = document.querySelector(".contact h4");
+h4C.textContent = siteContent['contact']['contact-h4'];
+
+const address = document.querySelectorAll(".contact p")[0];
+address.innerHTML = '123 Way 456 Street<br>Somewhere, USA';
+
+const number = document.querySelectorAll(".contact p")[1];
+number.textContent = siteContent['contact']['phone'];
+
+const eMail = document.querySelectorAll(".contact p")[2];
+eMail.textContent = siteContent['contact']['email'];
+
+const foot = document.querySelector('footer p');
+foot.textContent = siteContent['footer']['copyright'];
+
+
+const txtcolor = document.querySelectorAll('a');
+txtcolor.forEach(i => i.style.color = 'green');
+
+const add1 = document.createElement('a');
+add1.textContent = 'cool';
+add1.href = '#';
+document.querySelector('nav').appendChild(add1);
+
+const add2 = document.createElement('a');
+add2.textContent = 'name';
+add2.href = '#';
+document.querySelector('nav').prepend(add2);
+
+
+
 
