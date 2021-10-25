941d81d5d7f01ce5639d90dba067960ace392f81
EstrellaDionis <EstrellaDionis@gmail.com>
2021-10-25 16:28:30 -0400

    added container:nth 1

--- js/index.js
+++ js/index.js
@@ -40,3 +40,56 @@ const siteContent = {
 // Example: Update the img src for the logo
 let logo = document.getElementById("logo-img");
 logo.setAttribute('src', siteContent["nav"]["img-src"])
+
+
+//updating HTML with the JSON Data
+
+const services = document.querySelector('.container:nth-of-type(1)')
+
+// <a href="#">Services</a>
+// <a href="#">Product</a>
+// <a href="#">Vision</a>
+// <a href="#">Features</a>
+// <a href="#">About</a>
+// <a href="#">Contact</a>
+// </nav>
+
+
+let navColor = document.getElementsByClassName('container');
+navColor.style.color = 'green';
+
+
+
+
+
+// #### Create selectors to point your data into elements
+
+// * [ ] Create selectors by using any of the DOM element's methods
+// * [ ] Note that IDs have been used on all images. Use the IDs to update src path content
+
+// #### Update the HTML with the JSON data
+
+// * [x] Remember, NO direct updating of the HTML source is allowed.
+// * [x] Using your selectors, update the content to match the example file.
+// * [x] Remember to update the src attributes on images
+
+// #### Add new content
+
+// * [ ] Change the color of the navigation text to be green.
+// * [ ] Utilize `.appendChild()` and `.prepend()` to add two new items to the navigation system. You can call them whatever you want.
+// * [ ] Check your work by looking at the [original html](original.html) in the browser
+
+// ### Task 3: Stretch
+
+// * [ ] Update styles throughout the page as you see fit. Study what happens when you updated the DOM using style in JavaScript.  
+// * [ ] Study tomorrow's lesson on events and try to integrate a button that can update content on the site with a click of a button.  You could build a similar data object with new values to help you test the click event.
+
+// #### Stretch Project: Digital Timer
+
+// This project is heavier on logic but employs some DOM manipulation to achieve its goals.  Go check it out here: [stretch assignment](stretch-assignment) and see how far you can get.
+
+// ## Submission Format
+
+// **Follow these steps for completing your project.**
+
+// * [ ] Submit a Pull-Request to merge `<firstName-lastName>` Branch into `main` (student's  Repo). **Please don't merge your own pull request**
