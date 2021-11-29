7e2271f7cb276c4f27bc1008d63a8bad2d64a481
Leila <leila.alhemali@gmail.com>
2021-11-29 16:38:03 -0700

    finished assignment requirements

--- src/index.js
+++ src/index.js
@@ -39,4 +39,95 @@ const siteContent = { // DO NOT CHANGE THIS OBJECT
   },
 };
 
-console.log('project wired!')
+//navbar
+const nav = document.querySelectorAll('nav a');
+const linkTexts = Object.values(siteContent.nav)
+
+nav.forEach((link,idx) => 
+{
+  link.textContent = linkTexts[idx]
+})
+
+
+// const firstChild = nav.firstChild;
+// firstChild.textContent = siteContent['nav']['nav-item-1'];
+
+// const secondChild = firstChild.nextElementSibling;
+// secondChild.textContent = siteContent['nav']['nav-item-2'];
+// const thirdChild = secondChild.nextElementSibling;
+// thirdChild.textContent = siteContent['nav']['nav-item-3'];
+// const fourthChild = thirdChild.nextElementSibling;
+// fourthChild.textContent = siteContent['nav']['nav-item-4'];
+// const fifthChild = fourthChild.nextElementSibling;
+// fifthChild.textContent = siteContent['nav']['nav-item-5'];
+// const sixthChild = fifthChild.nextElementSibling;
+// sixthChild.textContent = siteContent['nav']['nav-item-6'];
+
+const logoTitle = document.querySelector('.logo');
+logoTitle.src = "http://localhost:9000/img/logo.png";
+
+
+//main section
+const domIsAwesome = document.querySelector('.cta-text h1');
+domIsAwesome.textContent = siteContent.cta.h1;
+
+const startedButton = document.querySelector('.cta-text button');
+startedButton.textContent = siteContent.cta.button;
+
+const ctaImg = document.querySelector('#cta-img');
+ctaImg.src = "http://localhost:9000/img/cta.png" ;
+
+
+//top text section
+const featuresContent = document.querySelector('.text-content h4');
+featuresContent.textContent = siteContent['main-content']['features-h4']; 
+
+const featuresContentText = document.querySelector('.text-content p');
+featuresContentText.textContent = siteContent['main-content']['features-content']; 
+
+const aboutContent = document.querySelector('.text-content:nth-of-type(2) h4');
+aboutContent.textContent = siteContent['main-content']['about-h4']; 
+
+const aboutContentText = document.querySelector('.text-content:nth-of-type(2) p');
+aboutContentText.textContent = siteContent['main-content']['about-content']; 
+
+//bottom text section
+const servicesContent = document.querySelector('.bottom-content .text-content h4');
+servicesContent.textContent = siteContent['main-content']['services-h4']; 
+
+const servicesContentText = document.querySelector('.bottom-content .text-content p');
+servicesContentText.textContent = siteContent['main-content']['services-content']; 
+
+const productContent = document.querySelector('.bottom-content .text-content:nth-of-type(2) h4');
+productContent.textContent = siteContent['main-content']['product-h4']; 
+
+const productContentText = document.querySelector('.bottom-content .text-content:nth-of-type(2) p');
+productContentText.textContent = siteContent['main-content']['product-content']; 
+
+const visionContent = document.querySelector('.bottom-content .text-content:nth-of-type(3) h4');
+visionContent.textContent = siteContent['main-content']['vision-h4']; 
+
+const visionContentText = document.querySelector('.bottom-content .text-content:nth-of-type(3) p');
+visionContentText.textContent = siteContent['main-content']['vision-content']; 
+
+const middleImg = document.querySelector('#middle-img');
+middleImg.src = "http://localhost:9000/img/accent.png"
+
+
+//contact section
+const contact = document.querySelector('.contact h4');
+contact.textContent = siteContent.contact['contact-h4'];
+
+const address = document.querySelector(".contact p");
+address.textContent = siteContent.contact.address;
+
+const phone = document.querySelector(".contact p:nth-of-type(2)");
+phone.textContent = siteContent.contact.phone;
+
+const email = document.querySelector(".contact p:nth-of-type(3)");
+email.textContent = siteContent.contact.email;
+
+
+//copyright
+const copyRight = document.querySelector('footer a');
+copyRight.textContent = siteContent.footer.copyright; 
