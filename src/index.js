9dc129348ce368e980e913e367621da006a772dd
Kim Nguyen <kimnivore@gmail.com>
2021-11-29 12:43:43 -0800

    completed footer

--- src/index.js
+++ src/index.js
@@ -42,25 +42,31 @@ const siteContent = { // DO NOT CHANGE THIS OBJECT
 console.log('project wired!')
 
 
-// const nav = document.querySelector('nav');
 const servicesNav = document.querySelector('nav a:nth-of-type(1)');
 servicesNav.textContent = siteContent['nav']['nav-item-1'];
-servicesNav.classList.add('italic');
+// servicesNav.classList.add('italic');
 const productNav = servicesNav.nextElementSibling;
 productNav.textContent = siteContent['nav']['nav-item-2'];
-productNav.classList.add('italic');
+// productNav.classList.add('italic');
 const visionNav = productNav.nextElementSibling;
 visionNav.textContent = siteContent['nav']['nav-item-3'];
-visionNav.classList.add('italic');
+// visionNav.classList.add('italic');
 const featuresNav = visionNav.nextElementSibling;
 featuresNav.textContent = siteContent['nav']['nav-item-4'];
-featuresNav.classList.add('italic');
+// featuresNav.classList.add('italic');
 const aboutNav = featuresNav.nextElementSibling;
 aboutNav.textContent = siteContent['nav']['nav-item-5'];
-aboutNav.classList.add('italic');
+// aboutNav.classList.add('italic');
 const contactNav = aboutNav.nextElementSibling;
 contactNav.textContent = siteContent['nav']['nav-item-6'];
-contactNav.classList.add('italic');
+// contactNav.classList.add('italic');
+
+const links = document.querySelectorAll('nav a');
+links.forEach(function(link) {
+  console.log(link);
+});
+const linksArray = Array.from(links);
+linksArray.classList.add('italic')
 
 const logo = document.getElementById('logo-img');
 logo.setAttribute('src', siteContent['images']['logo-img']);
@@ -73,3 +79,47 @@ button.textContent = siteContent['cta']['button'];
 
 const ctaLogo = document.getElementById('cta-img');
 ctaLogo.setAttribute('src', siteContent['images']['cta-img']);
+
+const featuresTitle = document.querySelector('.top-content .text-content:nth-of-type(1) h4');
+featuresTitle.textContent = siteContent['main-content']['features-h4'];
+const featuresText = document.querySelector('.top-content .text-content:nth-of-type(1) p');
+featuresText.textContent = siteContent['main-content']['features-content'];
+
+const aboutTitle = document.querySelector('.top-content .text-content:nth-of-type(2) h4');
+aboutTitle.textContent = siteContent['main-content']['about-h4'];
+const aboutText = document.querySelector('.top-content .text-content:nth-of-type(2) p');
+aboutText.textContent = siteContent['main-content']['about-content'];
+
+const accentLogo = document.getElementById('middle-img');
+accentLogo.setAttribute('src', siteContent['images']['accent-img']);
+
+const servicesTitle = document.querySelector('.bottom-content .text-content:nth-of-type(1) h4');
+servicesTitle.textContent = siteContent['main-content']['services-h4'];
+const servicesText = document.querySelector('.bottom-content .text-content:nth-of-type(1) p');
+servicesText.textContent = siteContent['main-content']['services-content'];
+
+const productTitle = document.querySelector('.bottom-content .text-content:nth-of-type(2) h4');
+productTitle.textContent = siteContent['main-content']['product-h4'];
+const productText = document.querySelector('.bottom-content .text-content:nth-of-type(2) p');
+productText.textContent = siteContent['main-content']['product-content'];
+
+const visionTitle = document.querySelector('.bottom-content .text-content:nth-of-type(3) h4');
+visionTitle.textContent = siteContent['main-content']['vision-h4'];
+const visionText = document.querySelector('.bottom-content .text-content:nth-of-type(3) p');
+visionText.textContent = siteContent['main-content']['vision-content'];
+
+const footerContact = document.querySelector('.contact h4');
+footerContact.textContent = siteContent['contact']['contact-h4'];
+
+const footerAddress = document.querySelector('.contact p:nth-of-type(1)');
+footerAddress.textContent = siteContent['contact']['address'];
+
+const footerPhone = document.querySelector('.contact p:nth-of-type(2)');
+footerPhone.textContent = siteContent['contact']['phone'];
+
+const footerEmail = document.querySelector('.contact p:nth-of-type(3)');
+footerEmail.textContent = siteContent['contact']['email'];
+
+const copyright = document.querySelector('footer a');
+copyright.textContent = siteContent['footer']['copyright'];
+copyright.classList.add('bold');
