f2d3128b7d1f92ccaef1dd6743d3c04991561102
Andrew Gary <andrew.gary91@gmail.com>
2021-11-29 15:05:55 -0600

    finished everything but cant seem to get images working: site will not connect

--- src/index.js
+++ src/index.js
@@ -39,4 +39,65 @@ const siteContent = { // DO NOT CHANGE THIS OBJECT
   },
 };
 
-console.log('project wired!')
+
+const links = document.querySelectorAll('header nav a');
+const realLinksArray = Array.from(links);
+
+console.log(siteContent.nav);
+realLinksArray[0].textContent = siteContent["nav"]["nav-item-1"];
+realLinksArray[1].textContent = siteContent["nav"]["nav-item-2"];
+realLinksArray[2].textContent = siteContent["nav"]["nav-item-3"];
+realLinksArray[3].textContent = siteContent["nav"]["nav-item-4"];
+realLinksArray[4].textContent = siteContent["nav"]["nav-item-5"];
+realLinksArray[5].textContent = siteContent["nav"]["nav-item-6"];
+
+
+const ctaText = document.querySelector('.cta-text h1');
+ctaText.textContent = siteContent['cta']['h1']
+
+const ctaButton = document.querySelector('.cta button');
+ctaButton.textContent = siteContent['cta']['button'];
+
+
+
+const topContentTitles = document.querySelectorAll('.top-content .text-content h4');
+console.log(topContentTitles);
+const realTopContentTitles = Array.from(topContentTitles);
+console.log(realTopContentTitles);
+
+realTopContentTitles[0].textContent = siteContent['main-content']['features-h4'];
+realTopContentTitles[1].textContent = siteContent['main-content']['about-h4'];
+
+const topContentContent = document.querySelectorAll('.top-content .text-content p');
+const realTopContentContent = Array.from(topContentContent);
+
+realTopContentContent[0].textContent = siteContent['main-content']['features-content'];
+realTopContentContent[1].textContent = siteContent['main-content']['about-content'];
+
+const bottomContentTitles = document.querySelectorAll('.bottom-content .text-content h4');
+const realBottomContentTitles = Array.from(bottomContentTitles);
+console.log(bottomContentTitles);
+console.log(realBottomContentTitles);
+
+realBottomContentTitles[0].textContent = siteContent['main-content']['services-h4'];
+realBottomContentTitles[1].textContent = siteContent['main-content']['product-h4'];
+realBottomContentTitles[2].textContent = siteContent['main-content']['vision-h4'];
+
+const bottomContentContent = document.querySelectorAll('.bottom-content .text-content p');
+const realBottomContentContent = Array.from(bottomContentContent);
+
+realBottomContentContent[0].textContent = siteContent['main-content']['services-content'];
+realBottomContentContent[1].textContent = siteContent['main-content']['product-content'];
+realBottomContentContent[2].textContent = siteContent['main-content']['vision-content'];
+
+const contact = document.querySelector('.contact h4');
+contact.textContent = siteContent['contact']['contact-h4'];
+
+const contactInfo = document.querySelectorAll('.contact p');
+const realContactInfo = Array.from(contactInfo);
+
+realContactInfo[0].textContent = siteContent['contact']['address'];
+realContactInfo[1].textContent = siteContent['contact']['phone'];
+realContactInfo[2].textContent = siteContent['contact']['email'];
+
+
