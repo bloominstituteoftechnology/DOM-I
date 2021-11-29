7b33dd5eb83a4ce90f5539d9d8ed92b257b927e6
kevnjs <91511588+kevnjs@users.noreply.github.com>
2021-11-29 17:41:04 -0600

    Finished most sections

--- src/index.js
+++ src/index.js
@@ -33,10 +33,66 @@ const siteContent = { // DO NOT CHANGE THIS OBJECT
     "copyright": "Copyright Great Idea! 2021",
   },
   "images": {
-    "logo-img": "http://localhost:9000/img/logo.png",
-    "cta-img": "http://localhost:9000/img/cta.png",
-    "accent-img": "http://localhost:9000/img/accent.png",
+    "logo-img": "mocks/img/logo.png",
+    "cta-img": "mocks/img/cta.png",
+    "accent-img": "mocks/img/accent.png",
   },
 };
 
-console.log('project wired!')
+console.log('project wired!');
+
+const navbar = document.querySelector('nav');
+navbar.style.fontStyle = "italic";
+const nav1 = document.querySelector('nav a:nth-of-type(1)');
+const nav2 = document.querySelector('nav a:nth-of-type(2)');
+const nav3 = document.querySelector('nav a:nth-of-type(3)');
+const nav4 = document.querySelector('nav a:nth-of-type(4)');
+const nav5 = document.querySelector('nav a:nth-of-type(5)');
+const nav6 = document.querySelector('nav a:nth-of-type(6)');
+nav1.textContent = siteContent["nav"]["nav-item-1"];
+nav2.textContent = siteContent["nav"]["nav-item-2"];
+nav3.textContent = siteContent["nav"]["nav-item-3"];
+nav4.textContent = siteContent["nav"]["nav-item-4"];
+nav5.textContent = siteContent["nav"]["nav-item-5"];
+nav6.textContent = siteContent["nav"]["nav-item-6"];
+
+const companyLogo = document.querySelector('#logo-img');
+companyLogo.src = siteContent["images"]["logo-img"];
+
+const ctaText = document.querySelector("h1");
+ctaText.textContent = siteContent["cta"]["h1"];
+const buttonText = document.querySelector("button");
+buttonText.textContent = siteContent["cta"]["button"];
+
+const ctaImage = document.querySelector("#cta-img");
+ctaImage.src = siteContent["images"]["cta-img"];
+
+const features = document.querySelector(".text-content:nth-of-type(1) h4");
+features.textContent = siteContent["main-content"]["features-h4"];
+const featuresPara = document.querySelector(".text-content:nth-of-type(1) p");
+featuresPara.textContent = siteContent["main-content"]["features-content"]
+
+const about = document.querySelector(".text-content:nth-of-type(2) h4");
+about.textContent = siteContent["main-content"]["about-h4"];
+const aboutPara = document.querySelector(".text-content:nth-of-type(2) p");
+aboutPara.textContent = siteContent["main-content"]["about-content"];
+
+const midImage = document.querySelector(".middle-img")
+midImage.src = siteContent["images"]["accent-img"];
+
+const services = document.querySelector(".text-content:nth-of-type(3) h4");
+services.textContent = siteContent["main-content"]["services-h4"];
+const servicesPara = document.querySelector(".text-content:nth-of-type(3) p");
+servicesPara.textContent = siteContent["main-content"]["services-content"];
+
+const product = document.querySelector(".bottom-content .text-content:nth-of-type(2) h4");
+product.textContent = siteContent["main-content"]["product-h4"];
+
+const productPara = document.querySelector(".bottom-content .text-content:nth-of-type(2) p");
+productPara.textContent = siteContent["main-content"]["product-content"];
+
+const vision = document.querySelector(".bottom-content .text-content:nth-of-type(3) h4:nth-of-type(3)");
+vision.textContent = siteContent["main-content"]["vision-h4"];
+
+
+
