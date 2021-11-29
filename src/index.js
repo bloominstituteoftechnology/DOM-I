7a81e164391d8e6e898f151a8ebba3c346ad11ef
Chris Kelley <chriskelley85@gmail.com>
2021-11-29 17:28:05 -0600

    thru mvp

--- src/index.js
+++ src/index.js
@@ -54,7 +54,34 @@ console.log('project wired!')
  //logo
  const logo = document.querySelector("#logo-img");
 
+ //cta 
+ const startButton = document.querySelector("button");
+ const startText = document.querySelector("h1");
+ const ctaImg = document.querySelector("#cta-img");
+
+ //main content
+const featureTitle = document.querySelector(".text-content:nth-of-type(1) h4");
+const featuresPara = document.querySelector(".text-content:nth-of-type(1) p");
+const aboutTitle = document.querySelector(".text-content:nth-of-type(2) h4");
+const aboutPara = document.querySelector(".text-content:nth-of-type(2) p");
+const servicesTitle = document.querySelector(".bottom-content .text-content:nth-of-type(1) h4");
+const servicesPara = document.querySelector(".bottom-content .text-content:nth-of-type(1) p");
+const productTitle = document.querySelector(".bottom-content .text-content:nth-of-type(2) h4");
+const productPara = document.querySelector(".bottom-content .text-content:nth-of-type(2) p");
+const visionTitle = document.querySelector(".bottom-content .text-content:nth-of-type(3) h4");
+const visionPara = document.querySelector(".bottom-content .text-content:nth-of-type(3) p");
+const accentImage = document.querySelector("#middle-img");
+
+const contact = document.querySelector(".contact h4");
+const address = document.querySelector(".contact p:nth-of-type(1)");
+const phone = document.querySelector(".contact p:nth-of-type(2)");
+const email = document.querySelector(".contact p:nth-of-type(3)");
+
+//contact
+const footer = document.querySelector("footer a")
+
 //add text
+  //nav
 nav1.textContent = siteContent["nav"]["nav-item-1"];
 nav2.textContent = siteContent["nav"]["nav-item-2"];
 nav3.textContent = siteContent["nav"]["nav-item-3"];
@@ -62,10 +89,39 @@ nav4.textContent = siteContent["nav"]["nav-item-4"];
 nav5.textContent = siteContent["nav"]["nav-item-5"];
 nav6.textContent = siteContent["nav"]["nav-item-6"];
 
+ //cta
+startText.textContent = siteContent["cta"]["h1"]
+startButton.textContent = siteContent["cta"]["button"]
+ // Titles
+featureTitle.textContent = siteContent["main-content"]["features-h4"];
+aboutTitle.textContent = siteContent["main-content"]["about-h4"];
+servicesTitle.textContent = siteContent["main-content"]["services-h4"];
+productTitle.textContent = siteContent["main-content"]["product-h4"];
+visionTitle.textContent = siteContent["main-content"]["vision-h4"];
+ // Paragraphs
+featuresPara.textContent = siteContent["main-content"]["features-content"];
+aboutPara.textContent = siteContent["main-content"]["about-content"];;
+servicesPara.textContent = siteContent["main-content"]["services-content"];
+productPara.textContent = siteContent["main-content"]["product-content"];
+visionPara.textContent = siteContent["main-content"]["vision-content"];
+ //contact
+contact.textContent = siteContent["contact"]["contact-h4"];
+address.textContent = siteContent["contact"]["address"];
+phone.textContent = siteContent["contact"]["phone"];
+email.textContent = siteContent["contact"]["email"];
+ //footer
+footer.textContent = siteContent["footer"]["copyright"];
+
  //class 
- nav1.classList.add('italic');
- nav2.classList.add('italic');
- nav3.classList.add('italic');
- nav4.classList.add('italic');
- nav5.classList.add('italic');
- nav6.classList.add('italic');
+nav1.classList.add("italic");
+nav2.classList.add("italic");
+nav3.classList.add("italic");
+nav4.classList.add("italic");
+nav5.classList.add("italic");
+nav6.classList.add("italic");
+
+footer.classList.add("bold");
+
+//image source
+ logo.src = siteContent["images"]["logo-img"]; 
+ ctaImg.src = siteContent["images"]["cta-img"]
