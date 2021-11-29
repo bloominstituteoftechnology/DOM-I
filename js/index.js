6a43d7e554dc21537630d96024b7cef50a992b1e
DayLaFavor <danelafavor1@gmail.com>
2021-11-29 15:33:52 -0600

    finished telling the rock computer what to do

--- js/index.js
+++ js/index.js
@@ -37,6 +37,68 @@ const siteContent = {
   },
 };
 
+const navItem1 = document.querySelector('.nav-item-1');
+const navItem2 = document.querySelector('.nav-item-2');
+const navItem3 = document.querySelector('.nav-item-3');
+const navItem4 = document.querySelector('.nav-item-4');
+const navItem5 = document.querySelector('.nav-item-5');
+const navItem6 = document.querySelector('.nav-item-6');
+const navImage = document.querySelector('.img-src');
+
+const ctaHeader = document.querySelector('.h1');
+const ctaButton = document.querySelector('.button');
+const ctaImage = document.querySelector('.img-src');
+
+const h4Features = document.querySelector('.features-h4');
+const featuresContent = document.querySelector('.features-content');
+const h4About = document.querySelector('.about-h4');
+const aboutContent = document.querySelector('.about-content');
+const contentImage = document.querySelector('.middle-img-src');
+const h4Services = document.querySelector('.services-h4');
+const servicesContent = document.querySelector('.services-content');
+const h4Product = document.querySelector('.product-h4');
+const productContent = document.querySelector('.product-content');
+const h4Vision = document.querySelector('.vision-h4');
+const visionContent = document.querySelector('.vision-content');
+
+const contactHeading = document.querySelector('.contact-h4');
+const address = document.querySelector('.address');
+const phone = document.querySelector('.phone');
+const email = document.querySelector('.email');
+
+const copyright = document.querySelector('.copyright');
+
+navItem1.textContent = siteContent['nav']['nav-item-1']
+navItem2.textContent = siteContent['nav']['nav-item-2']
+navItem3.textContent = siteContent['nav']['nav-item-3']
+navItem4.textContent = siteContent['nav']['nav-item-4']
+navItem5.textContent = siteContent['nav']['nav-item-5']
+navItem6.textContent = siteContent['nav']['nav-item-6']
+navImage.textContent = siteContent['nav']['img-src']
+
+ctaHeader.textContent = siteContent['cta']['h1']
+ctaButton.textContent = siteContent['cta']['button']
+ctaImage.textContent = siteContent['cta']['img-src']
+
+h4Features.textContent = siteContent['main-content']['features-h4']
+featuresContent.textContent = siteContent['main-content']['features-content']
+h4About.textContent = siteContent['main-content']['about-h4']
+aboutContent.textContent = siteContent['main-content']['about-content']
+contentImage.textContent = siteContent['main-content']['middle-img-src']
+h4Services.textContent = siteContent['main-content']['services-h4']
+servicesContent.textContent = siteContent['main-content']['services-content']
+h4Product.textContent = siteContent['main-content']['product-h4']
+productContent.textContent = siteContent['main-content']['product-content']
+h4Vision.textContent = siteContent['main-content']['vision-h4']
+visionContent.textContent = siteContent['main-content']['vision-content']
+
+contactHeading.textContent = siteContent['contact']['contact-h4'];
+address.textContent = siteContent['contact']['address'];
+phone.textContent = siteContent['contact']['phone'];
+email.textContent = siteContent['contact']['email'];
+
+copyright.textContent = siteContent['footer']['copyright'];
+
 // Example: Update the img src for the logo
 let logo = document.getElementById("logo-img");
 logo.setAttribute('src', siteContent["nav"]["img-src"])
