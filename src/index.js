0aaa9c6d06594c351cb87d6b5b922f932d19a085
scotty-yu <scotty.yu@yahoo.com>
2021-11-29 16:43:21 -0800

    progress

--- src/index.js
+++ src/index.js
@@ -40,3 +40,42 @@ const siteContent = { // DO NOT CHANGE THIS OBJECT
 };
 
 console.log('project wired!')
+
+//nav
+const nav = document.querySelectorAll('nav a');
+//nav[0].textContent = siteContent['main-content']['nav']['nav-content'];
+
+//top content
+const topContent = document.querySelector('.top-content');
+topContent.children[0].children[0].textContent = siteContent['main-content']['features-h4'];
+topContent.children[0].children[1].textContent = siteContent['main-content']['features-content'];
+topContent.children[1].children[0].textContent = siteContent['main-content']['about-h4'];
+topContent.children[1].children[1].textContent = siteContent['main-content']['about-content'];
+
+//bottom content
+const botContent = document.querySelector('.bottom-content');
+const h4Bottom = botContent.querySelectorAll('h4');
+h4Bottom[0].textContent = siteContent['main-content']['services-h4'];
+h4Bottom[1].textContent = siteContent['main-content']['product-h4']
+h4Bottom[2].textContent = siteContent['main-content']['vision-h4'];
+const pBottom = botContent.querySelectorAll('p');
+pBottom[0].textContent = siteContent['main-content']['services-content']
+pBottom[1].textContent = siteContent['main-content']['product-content'];
+pBottom[2].textContent = siteContent['main-content']['vision-content'];
+
+//img
+const logo = document.querySelector('#logo-img');
+logo.src = siteContent['images']['logo-img'];
+
+const ctaImg = document.querySelector('#cta-img');
+ctaImg.src = siteContent['images']['cta-img'];
+
+const midImage = document.querySelector('#middle-img');
+midImage.src = siteContent['images']['accent-img'];
+
+//Dom & button
+const ctaText = document.querySelector('.cta-text h1');
+ctaText.textContent = siteContent['cta']['h1'];
+const button = document.querySelector('.cta-text button');
+button.textContent = siteContent['cta']['button'];
+
