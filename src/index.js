c5232acb06db533068f499dcd5c7e86a6f3095e1
John S <johnc.sime@gmail.com>
2021-11-29 19:43:02 -0500

    fixed a typo, all test passing now

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+const { directive } = require("@babel/types");
 const concurrently = require("concurrently");
 
 const siteContent = { // DO NOT CHANGE THIS OBJECT
@@ -86,3 +87,14 @@ psBottom[2].textContent = siteContent['main-content']['vision-content']
 //CTA
 document.querySelector('.cta .cta-text h1').textContent = siteContent.cta.h1
 document.querySelector('.cta .cta-text button').textContent = siteContent.cta.button
+
+//Nav links
+const navLinks = document.querySelectorAll('header nav a')
+const navLinkTexts = Object.values(siteContent.nav)
+navLinks.forEach((link, idx) => {
+  link.textContent = navLinkTexts[idx]
+  link.classList.add('italic')
+})
+
+
+
