97465e64c76389db5b8f066d6fcfac66a235a183
bezzantsam <83626945+bezzantsam@users.noreply.github.com>
2021-11-29 19:23:01 -0800

    bottom-content

--- src/index.js
+++ src/index.js
@@ -101,3 +101,22 @@ topContent.children[1].children[0].textContent =
   siteContent["main-content"]["about-h4"];
 topContent.children[1].children[1].textContent =
   siteContent["main-content"]["about-content"];
+
+//bottom content
+const bottomContent = document.querySelector(".bottom-content");
+
+const h4sBottom = bottomContent.querySelectorAll("h4");
+
+h4sBottom[0].textContent = siteContent["main-content"]["services-h4"];
+
+h4sBottom[1].textContent = siteContent["main-content"]["product-h4"];
+
+h4sBottom[2].textContent = siteContent["main-content"]["vision-h4"];
+
+const psBottom = bottomContent.querySelectorAll("p");
+
+psBottom[0].textContent = siteContent["main-content"]["services-content"];
+
+psBottom[1].textContent = siteContent["main-content"]["product-content"];
+
+psBottom[2].textContent = siteContent["main-content"]["vision-content"];
