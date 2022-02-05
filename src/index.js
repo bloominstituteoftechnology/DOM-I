bf391878ad28926bb33c7c689e50bec37c49f57a
gregmcknight <gmcknight23@gmail.com>
2022-02-04 20:28:11 -0500

    bottom section done

--- src/index.js
+++ src/index.js
@@ -70,3 +70,12 @@ topContent.children[1].children[1].textContent = siteContent['main-content']['ab
 
 // BOTTOM CONTENT
 const bottomContent = document.querySelector('.bottom-content')
+const h4sBottom = bottomContent.querySelectorAll('h4')
+h4sBottom[0].textContent = siteContent['main-content']['services-h4']
+h4sBottom[1].textContent = siteContent['main-content']['product-h4']
+h4sBottom[2].textContent = siteContent['main-content']['vision-h4']
+
+const psBottom = bottomContent.querySelectorAll('p')
+psBottom[0].textContent = siteContent['main-content']['services-content']
+psBottom[1].textContent = siteContent['main-content']['product-content']
+psBottom[2].textContent = siteContent['main-content']['vision-content']
