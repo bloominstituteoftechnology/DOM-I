b24ba6120a0dcfea45aaa9d41990e949c81a203b
Rebecca Stone <you@example.com>
2022-02-07 17:18:23 -0600

    double checking my push

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
-const siteContent = { // DO NOT CHANGE THIS OBJECT
-  "nav": {
+const siteContent = {
+  // DO NOT CHANGE THIS OBJECT
+  nav: {
     "nav-item-1": "Services",
     "nav-item-2": "Product",
     "nav-item-3": "Vision",
@@ -7,39 +8,44 @@ const siteContent = { // DO NOT CHANGE THIS OBJECT
     "nav-item-5": "About",
     "nav-item-6": "Contact",
   },
-  "cta": {
-    "h1": "DOM Is Awesome",
-    "button": "Get Started",
+  cta: {
+    h1: "DOM Is Awesome",
+    button: "Get Started",
   },
   "main-content": {
     "features-h4": "Features",
-    "features-content": "Features content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.",
+    "features-content":
+      "Features content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.",
     "about-h4": "About",
-    "about-content": "About content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.",
+    "about-content":
+      "About content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.",
     "services-h4": "Services",
-    "services-content": "Services content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.",
+    "services-content":
+      "Services content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.",
     "product-h4": "Product",
-    "product-content": "Product content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.",
+    "product-content":
+      "Product content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.",
     "vision-h4": "Vision",
-    "vision-content": "Vision content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.",
+    "vision-content":
+      "Vision content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.",
   },
-  "contact": {
+  contact: {
     "contact-h4": "Contact",
-    "address": "123 Way 456 Street Somewhere, USA",
-    "phone": "1 (888) 888-8888",
-    "email": "sales@greatidea.io",
+    address: "123 Way 456 Street Somewhere, USA",
+    phone: "1 (888) 888-8888",
+    email: "sales@greatidea.io",
   },
-  "footer": {
-    "copyright": "Copyright Great Idea! 2021",
+  footer: {
+    copyright: "Copyright Great Idea! 2021",
   },
-  "images": {
+  images: {
     "logo-img": "http://localhost:9000/img/logo.png",
     "cta-img": "http://localhost:9000/img/cta.png",
     "accent-img": "http://localhost:9000/img/accent.png",
   },
 };
 
-console.log('project wired!')
+console.log("project wired!");
 
 //These are my const for the header
 const headerNav = document.querySelector("header nav");
@@ -52,7 +58,6 @@ const aboutHeader = featuresHeader.nextSibling;
 const contactHeader = aboutHeader.nextSibling;
 const headerImg = document.querySelector("header .logo");
 
-
 //These are to change the class and inner text of the header
 headerNavA.className = "italic";
 servicesHeader.textContent = "Services";
@@ -63,7 +68,7 @@ aboutHeader.textContent = "About";
 contactHeader.textContent = "Contact";
 headerImg.src = "http://localhost:9000/img/logo.png";
 
-//These are const for the cta section 
+//These are const for the cta section
 const ctaTextSection = document.querySelector(".cta-text");
 const cta_h1 = ctaTextSection.querySelector("h1");
 const cta_button = ctaTextSection.querySelector("button");
