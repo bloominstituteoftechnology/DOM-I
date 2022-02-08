412a81fd3a996ea848aebd47fe0340bde166801b
enhopkin@gmail.com <enhopkin@gmail.com>
2022-02-07 21:49:24 -0700

    ridiculously slow to complete this--ominous much?

--- src/index.js
+++ src/index.js
@@ -39,4 +39,86 @@ const siteContent = { // DO NOT CHANGE THIS OBJECT
   },
 };
 
-console.log('project wired!')
+// console.log('project wired!')
+// console.log('hello')
+
+const linkList= document.querySelectorAll('header nav a');
+// console.log(linkList)
+const linksArray= Array.from(linkList);
+// console.log(linksArray)
+linksArray.forEach(item=> {
+  item.classList.add('italic')
+})
+linksArray[0].textContent= "Service";
+linksArray[1].textContent= "Product";
+linksArray[2].textContent= "Vision";
+linksArray[3].textContent= "Features";
+linksArray[4].textContent= "About";
+linksArray[5].textContent= "Contact";
+
+const headerImg= document.querySelector('#logo-img');
+headerImg.src= siteContent.images['logo-img'];
+
+const ctaImg= document.querySelector('#cta-img')
+ctaImg.src = siteContent.images['cta-img']
+
+const middleImg= document.querySelector('#middle-img');
+middleImg.src=siteContent.images['accent-img'];
+
+const title= document.querySelector('h1')
+title.textContent= "DOM Is Awesome"
+const buttonText= document.querySelector('button').textContent= "Get Started";
+
+
+//THIS REFUSED TO WORK FOR ME...DON'T KNOW WHY
+// const mainContent= document.querySelectorAll('.text-content')
+// mainContent[0].textContent= 'Features content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.'
+// mainContent[1].textContent= 'About content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.'
+// mainContent[2].textContent= 'Services content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.'
+// mainContent[3].textContent= 'Product content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.'
+// mainContent[4].textContent= "Vision content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.";
+// // console.log(mainContent)
+// const h4Set= document.querySelectorAll('.main-content h4');
+// console.log(h4Set);
+// h4Set[0].textContent= 'features';
+// h4Set[1].textContent= 'about;
+// h4Set[2].textContent= 'Services';
+// h4Set[3].textContent= 'Product';
+// h4Set[4].textContent= 'Vision';
+// const mainSectionA= document.querySelector('.main-content .top-content')
+// console.log(mainSectionA)
+
+const topSection= document.querySelector('.top-content');
+topSection.children[0].children[0].textContent = siteContent['main-content']['features-h4'];
+topSection.children[0].children[1].textContent = siteContent['main-content'] ['features-content']
+topSection.children[1].children[0].textContent = siteContent['main-content']['about-h4']
+topSection.children[1].children[1].textContent = siteContent['main-content']['about-content']
+
+const bottomSection=document.querySelector('.bottom-content');
+bottomSection.children[0].children[0].textContent= siteContent['main-content']['services-h4'];
+bottomSection.children[0].children[1].textContent= siteContent['main-content']['services-content'];
+bottomSection.children[1].children[0].textContent= siteContent['main-content']['product-h4'];
+bottomSection.children[1].children[1].textContent= siteContent['main-content']['product-content'];
+bottomSection.children[2].children[0].textContent= siteContent['main-content']['vision-h4'];
+bottomSection.children[2].children[1].textContent= siteContent['main-content']['vision-content'];
+
+
+
+
+
+
+const contactInfo= document.querySelector('.contact')
+contactInfo.children[0].textContent=siteContent.contact['contact-h4'];
+contactInfo.children[1].textContent= siteContent.contact['address'];
+contactInfo.children[2].textContent= siteContent.contact['phone']
+contactInfo.children[3].textContent= siteContent.contact['email']
+// contactInfo.h4.textContent= "Contact";
+// const contactText= document.querySelectorAll('.contact p')
+// console.log(contactText)
+// contactText[0].textContent= "123 Way 456 Street Somewhere, USA";
+// contactText[1].textContent= "1 (888) 888-8888";
+// contactText[2].textContent= "sales@greatidea.io";
+
+const closingLink= document.querySelector('footer a')
+closingLink.textContent= siteContent.footer.copyright;
+closingLink.classList.add('bold');
