c7c7a54f98a1cc06b6438d00e817f3f391f8a469
Bennett Kautz <bennettkautz@gmail.com>
2022-03-07 18:12:01 -0500

    figuring header

--- src/index.js
+++ src/index.js
@@ -72,67 +72,58 @@ ctaButton.textContent = siteContent.cta.button;
 const ctaImg = document.querySelector("#cta-img");
 ctaImg.src = siteContent.images["cta-image"];
 
-//main-content h4 1
-const mainContentFirstHeader = document.querySelector("h4")
-mainContentFirstH4.textContent = siteContent["main-content"]["features-h4"];
+//Top content 
+const topContent = document.querySelector(".top-content");
 
-//main content p 1 
-const mainContentFirstP = document.querySelectorAll("p");
-mainContentFirstP.textContent = siteContent["main-content"]["features-content"];
+//H4 1
+topContent.children[0].children[0].textContent = siteContent["main-content"]["features-h4"];
 
-//main-content h4 2
-const mainContentSecondHeader = document.querySelectorAll("text content h4:nth-of-type(2)");
-console.log(mainContentSecondHeader);
-mainContentSecondH4.textContent = siteContent["main-content"]["about-h4"];
+//P 1
+topContent.children[0].childre3n[1].textContent = siteContent["main-content"]["features-content"];
 
-//main content p 2 
-const mainContentSecondP = document.querySelectorAll("p:nth-of-type(2)");
-console.log(mainContentSecondP);
-mainContentSecondP.textContent = siteContent["main-content"]["about-content"];
+//H4 2
+topContent.chidlren[1].children[0].textContent = siteContent["main-content"]["about-h4"];
+
+//P 2
+topContent.chidlren[1].chidlren[1].textContent =siteContent["main-content"]["about-content"];
 
 //middle image
 const accentImg = document.querySelector('#middle-img');
 accentImg.src = siteContent.images["accent-img"];
 
-//main-content h4 3
-const mainContentThirdHeader = document.querySelectorAll("h4:nth-of-type(3)");
-mainContentThirdH4.textContent = siteContent["main-content"]["services-h4"];
+//Bottom content
+const bottomContent = document.querySelector(".bottom-content");
 
-//main content p 3 
-const mainContentThirdP = document.querySelectorAll("p:nth-of-type(3)");
-mainContentThirdP.textContent = siteContent["main-content"]["services-content"];
+//H4 1
+bottomContent.children[0].children[0].textContent = siteContent["main-content"]["services-h4"];
 
-//main-content h4 4
-const mainContentFourthHeader = document.querySelectorAll("h4:nth-of-type(4)");
-mainContentFourthH4.textContent = siteContent["main-content"]["product-h4"];
+//P 1
+bottomContent.children[0].children[1].textContent = siteContent["main-content"]["services-content"];
 
-//main content p 4 
-const mainContentFourthP = document.querySelectorAll("p:nth-of-type(4)");
-mainContentFourthP.textContent = siteContent["main-content"]["product-content"];
+//H4 2
+bottomContent.children[1].children[0].textContent = siteContent["main-content"]["product-h4"];
 
-//main-content h4 5
-const mainContentFifthHeader = document.querySelectorAll("h4:nth-of-type(5)");
-mainContentFifthH4.textContent = siteContent["main-content"]["vision-h4"];
+//P 2
+bottomContent.children[1].children[1].textContent = siteContent["main-content"]["product-content"];
 
-//main content p 5
-const mainContentFifthP = document.querySelectorAll("p:nth-of-type(5)");
-mainContentFifthP.textContent = siteContent["main-content"]["vision-content"];
+//H4 3
+bottomContent.children[2].children[0].textContent = siteContent["main-content"]["vision-h4"];
+
+//P 3
+bottomContent.children[2].children[1].textContent = siteContent["main-content"]["vision-content"];
 
 //contact h4
-const contactH4 = document.querySelector("contact h4");
-contactH4.textContent = siteContent.contact["contact-h4"];
+const contact = document.querySelector("section.contact");
+contact.children[0].textContent = siteContent.contact["contact-h4"];
 
 //contact p1
-const contactAddress = document.querySelector("contact p"[0]);
-contactAddress.textContent = siteContent.contact.address;
+contact.children[1].textContent = siteContent.contact.address;
 
 //contact p2
-const contactPhone = document.querySelector("contact p"[1]);
-contactPhone.textContent = siteContent.contact.phone;
+contact.children[2].textContent= siteContent.contact.phone;
 
 //contact p3
-const contactEmail = document.querySelector("contact p"[2]);
-contactEmail.textContent = siteContent.contact.email;
+contact.children[3].textContent = siteContent.contact.email;
 
 //footer a
 const footerLink = document.querySelector('footer a');
