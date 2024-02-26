8724fc39a4081ba5045dc8801861813f0df86497
Marie Knowlton <mtknowlton67@gmail.com>
2024-02-26 11:10:43 -0800

    grabbed the h4 and nav item and rendered them onto the page

--- src/index.js
+++ src/index.js
@@ -38,5 +38,40 @@ const siteContent = { // DO NOT CHANGE THIS OBJECT
     "accent-img": "http://localhost:9000/img/accent.png",
   },
 };
+//images
+const logoImg = document.querySelector('#logo-img')
+logoImg.src = siteContent.images['logo-img']
+
+const ctaImg = document.querySelector('#cta-img')
+ctaImg.src = siteContent.images['cta-img']
+
+const midImg = document.querySelector('#middle-img')
+midImg.setAttribute('src', siteContent.images['accent-img'])
+
+const h1 = document.querySelector('h1');
+h1.textContent = siteContent['cta']['button'];
+
+const button = document.querySelector('button');
+button.textContent = siteContent['cta']['button']
+
+// Nav
+const nav = document.querySelectorAll('nav a');
+nav[0].textContent = siteContent['nav']['nav-item-1'];
+nav[1].textContent = siteContent['nav']['nav-item-2'];
+nav[2].textContent = siteContent['nav']['nav-item-3'];
+nav[3].textContent = siteContent['nav']['nav-item-4'];
+nav[4].textContent = siteContent['nav']['nav-item-5'];
+nav[5].textContent = siteContent['nav']['nav-item-6'];
+// Top Content
+const h4 = document.querySelectorAll('h4');
+h4[0].textContent = siteContent["main-content"]['features-h4'];
+h4[1].textContent = siteContent["main-content"]['about-h4'];
+h4[2].textContent = siteContent["main-content"]['services-h4'];
+h4[3].textContent = siteContent["main-content"]['product-h4'];
+h4[4].textContent = siteContent["main-content"]['vision-h4'];
+h4[5].textContent = siteContent["main-content"]['contact-h4'];
+
+
+
 
 console.log('project wired!')
